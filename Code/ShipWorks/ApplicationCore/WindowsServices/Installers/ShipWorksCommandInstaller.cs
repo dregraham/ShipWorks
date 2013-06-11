b5a7baad394f2fb2c3186e03406eaa8c7839fcc7
kcroke <k.croke@shipworks.com>
2013-06-11 17:19:11 -0500

    add service installer

---
+++ Code/ShipWorks/ApplicationCore/WindowsServices/Installers/ShipWorksCommandInstaller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using ShipWorks.ApplicationCore.Interaction;
+
+namespace ShipWorks.ApplicationCore.WindowsServices.Installers
+{
+    class ShipWorksCommandInstaller : ICommandLineCommandHandler
+    {
+        public string CommandName
+        {
+            get { return "installservice"; }
+        }
+
+        public void Execute(List<string> args)
+        {
+            InstallService();
+        }
+
+        public static void InstallService()
+        {
+
+            ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+
+            ServiceManager serviceManager = new ServiceManager(new ShipWorksSchedulerService());
+            serviceManager.StartService();
+        }
+    }
+}
---
+++ Code/ShipWorks/ApplicationCore/WindowsServices/ServiceManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using ShipWorks.Actions.Scheduling;
+using log4net;
+
+namespace ShipWorks.ApplicationCore.WindowsServices
+{
+    public class ServiceManager
+    {
+        static readonly ILog log = LogManager.GetLogger(typeof(ServiceManager));
+
+        private readonly ShipWorksSchedulerService schedulerService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceManager"/> class.
+        /// </summary>
+        /// <param name="schedulerService">The scheduler service.</param>
+        public ServiceManager(ShipWorksSchedulerService schedulerService)
+        {
+            this.schedulerService = schedulerService;
+        }
+
+        public void StartService()
+        {
+            ServiceController service = new ServiceController(schedulerService.ServiceName);
+            try
+            {
+                TimeSpan timeout = TimeSpan.FromMilliseconds(30000);
+
+                service.Start();
+                service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+            }
+            catch(Exception ex)
+            {
+                log.Error("Can't start service " + schedulerService.ServiceName, ex);
+
+                throw;
+            }
+        }
+
+    }
+}
