b598e0687336eb2b8ab5dfc999f920dcfdd86dea
kcroke <k.croke@shipworks.com>
2013-06-17 12:48:55 -0500

    Add ActionSchedule and ActionScheduleType

---
+++ Code/ShipWorks/Actions/Scheduling/ActionSchedules/ActionSchedule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.Actions.Scheduling.Enums;
+
+namespace ShipWorks.Actions.Scheduling.ActionSchedules
+{
+    /// <summary>
+    /// Abstract ActionSchedule type
+    /// </summary>
+    public abstract class ActionSchedule
+    {
+        /// <summary>
+        /// Gets the type of the schedule.
+        /// </summary>
+        /// <value>
+        /// The type of the schedule.
+        /// </value>
+        public abstract ActionScheduleType ScheduleType
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets or sets the start time.
+        /// </summary>
+        /// <value>
+        /// The start time.
+        /// </value>
+        public DateTime StartTime
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ Code/ShipWorks/Actions/Scheduling/Enums/ActionScheduleType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ShipWorks.Actions.Scheduling.Enums
+{
+     [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum ActionScheduleType
+    {
+        [Description("One Time")]
+        OneTime=0
+    }
+}
