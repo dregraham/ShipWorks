1414736da604118cbb90b4bdfef8503830ddffd2
Michael Petter <m.petter@shipworks.com>
2013-06-18 17:15:30 -0500

    Implemented ActionScheduleAdapter for OneTime schedule.

---
+++ Code/ShipWorks/Actions/Scheduling/QuartzNet/ActionScheduleAdapters/OneTimeActionScheduleAdapter.cs
@@ -0,0 +1,14 @@
+﻿using Quartz;
+using ShipWorks.Actions.Scheduling.ActionSchedules;
+
+
+namespace ShipWorks.Actions.Scheduling.QuartzNet.ActionScheduleAdapters
+{
+    public class OneTimeActionScheduleAdapter : ActionScheduleAdapter<OneTimeActionSchedule>
+    {
+        public override IScheduleBuilder Adapt(OneTimeActionSchedule schedule)
+        {
+            return SimpleScheduleBuilder.Create();
+        }
+    }
+}
