acb1a2295c6fda5770574db7b38333f58a565d85
Michael Petter <m.petter@shipworks.com>
2013-06-19 15:00:05 -0500

    Implemented ActionScheduleAdapter for Daily schedule.

---
+++ Code/ShipWorks/Actions/Scheduling/QuartzNet/ActionScheduleAdapters/DailyActionScheduleAdapter.cs
@@ -0,0 +1,15 @@
+﻿using Quartz;
+using ShipWorks.Actions.Scheduling.ActionSchedules;
+
+
+namespace ShipWorks.Actions.Scheduling.QuartzNet.ActionScheduleAdapters
+{
+    public class DailyActionScheduleAdapter : ActionScheduleAdapter<DailyActionSchedule>
+    {
+        public override IScheduleBuilder Adapt(DailyActionSchedule schedule)
+        {
+            return CalendarIntervalScheduleBuilder.Create()
+                .WithIntervalInDays(schedule.FrequencyInDays);
+        }
+    }
+}
