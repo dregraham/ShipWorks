d6a08643c83c3d6c05be499af2eb0d2bb22dfa5f
Michael Petter <m.petter@shipworks.com>
2013-06-19 17:29:30 -0500

    Add tests for Daily quartz adapter.

--- Code/ShipWorks.Tests/Actions/Scheduling/QuartzNet/ActionScheduleAdapters/DailyActionScheduleAdapterTests.cs
+++ Code/ShipWorks.Tests/Actions/Scheduling/QuartzNet/ActionScheduleAdapters/DailyActionScheduleAdapterTests.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShipWorks.Actions.Scheduling.ActionSchedules;
 using ShipWorks.Actions.Scheduling.QuartzNet.ActionScheduleAdapters;
+using System;
+using System.Linq;
 
 
 namespace ShipWorks.Tests.Actions.Scheduling.QuartzNet.ActionScheduleAdapters
@@ -13,6 +16,31 @@ namespace ShipWorks.Tests.Actions.Scheduling.QuartzNet.ActionScheduleAdapters
         public void Initialize()
         {
             target = new DailyActionScheduleAdapter();
+        }
+
+
+        [TestMethod]
+        public void FiresAtStartTime()
+        {
+            var schedule = new DailyActionSchedule { StartDateTimeInUtc = DateTime.UtcNow };
+
+            var fireTimes = schedule.ComputeFireTimes(target, 1);
+
+            Assert.AreEqual(schedule.StartDateTimeInUtc, fireTimes[0].DateTime);
+        }
+
+        [TestMethod]
+        public void FiresAtSpecifiedFrequency()
+        {
+            var schedule = new DailyActionSchedule { FrequencyInDays = 3 };
+
+            var fireTimes = schedule.ComputeFireTimes(target, 5).ToArray();
+
+            var intervals = fireTimes.Skip(1)
+                .Zip(fireTimes, (x, x0) => x - x0)
+                .ToArray();
+
+            Assert.IsTrue(intervals.All(x => x.TotalDays == schedule.FrequencyInDays));
         }
     }
 }
