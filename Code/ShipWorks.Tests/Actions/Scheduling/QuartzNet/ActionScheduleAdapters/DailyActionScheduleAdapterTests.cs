89a0d47556cf79e69eeaa600f3a25975c89d032e
Michael Petter <m.petter@shipworks.com>
2013-06-19 16:36:46 -0500

    Add tests for OneTime quartz adapter.

---
+++ Code/ShipWorks.Tests/Actions/Scheduling/QuartzNet/ActionScheduleAdapters/DailyActionScheduleAdapterTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShipWorks.Actions.Scheduling.QuartzNet.ActionScheduleAdapters;
+
+
+namespace ShipWorks.Tests.Actions.Scheduling.QuartzNet.ActionScheduleAdapters
+{
+    [TestClass]
+    public class DailyActionScheduleAdapterTests
+    {
+        DailyActionScheduleAdapter target;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            target = new DailyActionScheduleAdapter();
+        }
+    }
+}
---
+++ Code/ShipWorks.Tests/Actions/Scheduling/QuartzNet/ActionScheduleAdapters/Helpers/ActionScheduleAdapterExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Quartz;
+using Quartz.Spi;
+using ShipWorks.Actions.Scheduling.ActionSchedules;
+using ShipWorks.Actions.Scheduling.QuartzNet;
+using System;
+using System.Collections.Generic;
+
+
+namespace ShipWorks.Tests.Actions.Scheduling.QuartzNet.ActionScheduleAdapters
+{
+    public static class ActionScheduleExtensions
+    {
+        public static IList<DateTimeOffset> ComputeFireTimes<TSchedule, TAdapter>(this TSchedule schedule, TAdapter adapter, int maxTimes)
+            where TSchedule : ActionSchedule
+            where TAdapter : ActionScheduleAdapter<TSchedule>
+        {
+            var trigger = TriggerBuilder.Create()
+                .StartAt(schedule.StartDateTimeInUtc)
+                .WithSchedule(adapter.Adapt(schedule))
+                .Build();
+
+            return TriggerUtils.ComputeFireTimes((IOperableTrigger)trigger, null, maxTimes);
+        }
+    }
+}
