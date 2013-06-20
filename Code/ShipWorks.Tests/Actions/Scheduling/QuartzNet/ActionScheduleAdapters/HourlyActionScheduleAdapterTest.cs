b414c978b97218de7684d42e9362569e37713ede
thughes <t.hughes@shipworks.com>
2013-06-20 11:05:25 -0500

    Added HourlyActionScheduleAdapter and test.

---
+++ Code/ShipWorks.Tests/Actions/Scheduling/QuartzNet/ActionScheduleAdapters/HourlyActionScheduleAdapterTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShipWorks.Actions.Scheduling.ActionSchedules;
+using ShipWorks.Actions.Scheduling.QuartzNet.ActionScheduleAdapters;
+using System;
+using System.Linq;
+
+namespace ShipWorks.Tests.Actions.Scheduling.QuartzNet.ActionScheduleAdapters
+{
+    [TestClass]
+    public class HourlyActionScheduleAdapterTest
+    {
+        HourlyActionScheduleAdapter target;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            target = new HourlyActionScheduleAdapter();
+        }
+
+        [TestMethod]
+        public void FiresAtStartTime()
+        {
+            var schedule = new HourlyActionSchedule { StartDateTimeInUtc = DateTime.UtcNow };
+
+            var fireTimes = schedule.ComputeFireTimes(target, 1);
+
+            Assert.AreEqual(schedule.StartDateTimeInUtc, fireTimes[0].DateTime);
+        }
+
+        [TestMethod]
+        public void FiresAtSpecifiedFrequency()
+        {
+            var schedule = new HourlyActionSchedule
+                {
+                    StartDateTimeInUtc = DateTime.UtcNow, 
+                    FrequencyInHours = 3
+                };
+
+            var fireTimes = schedule.ComputeFireTimes(target, 5).ToArray();
+
+            var intervals = fireTimes.Skip(1)
+                .Zip(fireTimes, (x, x0) => x - x0)
+                .ToArray();
+
+            Assert.IsTrue(intervals.All(x => (int)x.TotalHours == schedule.FrequencyInHours));
+        }
+    }
+}
---
+++ Code/ShipWorks/Actions/Scheduling/QuartzNet/ActionScheduleAdapters/HourlyActionScheduleAdapter.cs
@@ -0,0 +1,20 @@
+﻿using Quartz;
+using ShipWorks.Actions.Scheduling.ActionSchedules;
+
+namespace ShipWorks.Actions.Scheduling.QuartzNet.ActionScheduleAdapters
+{
+    /// <summary>
+    /// Hourly schedule adatper to create the Quartz schedule based on the ShipWorks HourlyActionSchedule
+    /// </summary>
+    public class HourlyActionScheduleAdapter : ActionScheduleAdapter<HourlyActionSchedule>
+    {
+        /// <summary>
+        /// Adapts an HourlyActionSchedule to a Quartz schedule builder.
+        /// </summary>
+        public override IScheduleBuilder Adapt(HourlyActionSchedule schedule)
+        {
+            return CalendarIntervalScheduleBuilder.Create()
+                .WithIntervalInHours(schedule.FrequencyInHours);
+        }
+    }
+}
