9a48aa5480c38d22e8546df43e0b67457cf5edf2
John Yedlicka <j.yedlicka@shipworks.com>
2013-06-26 15:09:11 -0500

    Implement the dashboard item for when at least one ShipWorks service is stopped

---
+++ Code/ShipWorks.Tests/ApplicationCore/Dashboard/Content/DashboardServiceStoppedItemTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShipWorks.ApplicationCore.Dashboard;
+using ShipWorks.ApplicationCore.Dashboard.Content;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Tests.ApplicationCore.Dashboard.Content
+{
+    [TestClass]
+    public class DashboardServiceStoppedItemTest
+    {
+        private DashboardServiceStoppedItem testObject;
+
+        [TestMethod]
+        public void Initialize_CanUserDismiss_Test()
+        {
+            testObject = new DashboardServiceStoppedItem(new List<SchedulerEntity>());
+
+            testObject.Initialize(new DashboardBar());
+
+            Assert.IsFalse(testObject.DashboardBar.CanUserDismiss);
+        }
+
+        [TestMethod]
+        public void Initialize_PrimaryText_Test()
+        {
+            testObject = new DashboardServiceStoppedItem(new List<SchedulerEntity>());
+
+            testObject.Initialize(new DashboardBar());
+
+            Assert.AreEqual("Schedulers", testObject.DashboardBar.PrimaryText);
+        }
+
+        [TestMethod]
+        public void Initialize_SecondaryText_WhenMoreThanOneSchedulers_Test()
+        {
+            List<SchedulerEntity> schedulerEntities = new List<SchedulerEntity>
+            {
+                new SchedulerEntity(),
+                new SchedulerEntity(),
+                new SchedulerEntity()
+            };
+                
+            testObject = new DashboardServiceStoppedItem(schedulerEntities);
+
+            testObject.Initialize(new DashboardBar());
+
+            Assert.AreEqual("There are 3 ShipWorks scheduling services not running.", testObject.DashboardBar.SecondaryText);
+        }
+
+        [TestMethod]
+        public void Initialize_SecondaryText_WhenOneSchedulerTest()
+        {
+            List<SchedulerEntity> schedulerEntities = new List<SchedulerEntity>
+            {
+                new SchedulerEntity()
+            };
+
+            testObject = new DashboardServiceStoppedItem(schedulerEntities);
+
+            testObject.Initialize(new DashboardBar());
+
+            Assert.AreEqual("There is 1 ShipWorks scheduling service not running.", testObject.DashboardBar.SecondaryText);
+        }
+
+        [TestMethod]
+        public void Initialize_ImageIsNotNull_Test()
+        {
+            testObject = new DashboardServiceStoppedItem(new List<SchedulerEntity>());
+
+            testObject.Initialize(new DashboardBar());
+
+            Assert.IsNotNull(testObject.DashboardBar.Image);
+        }
+    }
+}
--- Code/ShipWorks/ApplicationCore/Dashboard/Content/DashboardServiceStoppedItem.cs
+++ Code/ShipWorks/ApplicationCore/Dashboard/Content/DashboardServiceStoppedItem.cs
@@ -2,31 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using ShipWorks.ApplicationCore.WindowsServices;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Properties;
 
 namespace ShipWorks.ApplicationCore.Dashboard.Content
 {
     public class DashboardServiceStoppedItem : DashboardItem
     {
-        private readonly ServiceStatusDialog statusDialog;
+        private readonly List<SchedulerEntity> schedulerEntities;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DashboardServiceStoppedItem"/> class.
+        /// Initializes a new instance of the <see cref="DashboardServiceStoppedItem" /> class.
         /// </summary>
-        /// <param name="statusDialog">The status dialog.</param>
-        public DashboardServiceStoppedItem(ServiceStatusDialog statusDialog)
+        /// <param name="schedulers">The schedulers that are not running.</param>
+        public DashboardServiceStoppedItem(IEnumerable<SchedulerEntity> schedulers)
         {
-            this.statusDialog = statusDialog;
+            schedulerEntities = new List<SchedulerEntity>(schedulers);
         }
 
         /// <summary>
-        /// Initialize the item with given bar that it will display its informaiton in
+        /// Initialize the item with given bar that it will display its information in
         /// </summary>
         /// <param name="dashboardBar">The UI element associated with ths dashboard item.</param>
         public override void Initialize(DashboardBar dashboardBar)
         {
             base.Initialize(dashboardBar);
-            dashboardBar.CanUserDismiss = false;
+            
+            DashboardBar.CanUserDismiss = false;
+
+            DashboardBar.Image = Resources.gear_stop_16;
+            DashboardBar.PrimaryText = "Schedulers";
+
+            bool usePlural = schedulerEntities.Count > 1;
+            DashboardBar.SecondaryText = string.Format("There {0} {1} ShipWorks scheduling service{2} not running.", usePlural ? "are" : "is", schedulerEntities.Count, usePlural ? "s" : string.Empty);
+
+            List<DashboardAction> dashboardActions = new List<DashboardAction> { new DashboardActionMethod("[link]More Info[/link]", OnMoreInfo) };
+            DashboardBar.ApplyActions(dashboardActions);
+        }
+
+        /// <summary>
+        /// Called when the [More Info] link of the DashboardBar is clicked to show the service status dialog.
+        /// </summary>
+        /// <param name="owner">The owner.</param>
+        /// <param name="userState">State of the user.</param>
+        private void OnMoreInfo(Control owner, object userState)
+        {
+          using (ServiceStatusDialog dialog = new ServiceStatusDialog())
+          {
+              dialog.ShowDialog(owner);
+          }
         }
     }
 }
