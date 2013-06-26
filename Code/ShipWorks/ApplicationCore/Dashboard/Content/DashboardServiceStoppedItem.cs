42340f1682c502136c4c14d3639c371e4f8c8ad3
John Yedlicka <j.yedlicka@shipworks.com>
2013-06-26 14:00:49 -0500

    Stub out dialog for ShipWorks Service Status (dependency of other tasks)

---
+++ Code/ShipWorks/ApplicationCore/Dashboard/Content/DashboardServiceStoppedItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.ApplicationCore.WindowsServices;
+
+namespace ShipWorks.ApplicationCore.Dashboard.Content
+{
+    public class DashboardServiceStoppedItem : DashboardItem
+    {
+        private readonly ShipWorksStatusDialog statusDialog;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DashboardServiceStoppedItem"/> class.
+        /// </summary>
+        /// <param name="statusDialog">The status dialog.</param>
+        public DashboardServiceStoppedItem(ShipWorksStatusDialog statusDialog)
+        {
+            this.statusDialog = statusDialog;
+        }
+
+        /// <summary>
+        /// Initialize the item with given bar that it will display its informaiton in
+        /// </summary>
+        /// <param name="dashboardBar">The UI element associated with ths dashboard item.</param>
+        public override void Initialize(DashboardBar dashboardBar)
+        {
+            base.Initialize(dashboardBar);
+            dashboardBar.CanUserDismiss = false;
+        }
+    }
+}
---
+++ Code/ShipWorks/ApplicationCore/WindowsServices/ShipWorksStatusDialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShipWorks.ApplicationCore.WindowsServices
+{
+    public partial class ShipWorksStatusDialog : Form
+    {
+        public ShipWorksStatusDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
