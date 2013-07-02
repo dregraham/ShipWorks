f29dac8efe43bd858df65f2a41344fe0cf5e67ac
Michael Petter <m.petter@shipworks.com>
2013-07-02 11:11:57 -0500

    Add computer limitation type enumeration.

---
+++ Code/ShipWorks/Actions/ComputerLimitationType.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+
+namespace ShipWorks.Actions
+{
+    /// <summary>
+    /// Specifies the type of computer limitation for an action.
+    /// </summary>
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum ComputerLimitationType
+    {
+        /// <summary>
+        /// The action may run on any computer.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The action may only run on the computer that triggered it.
+        /// </summary>
+        TriggeringComputer = 1,
+
+        /// <summary>
+        /// The action may only run on a named subset of computers.
+        /// </summary>
+        NamedList = 2
+    }
+}
