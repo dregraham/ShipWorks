786ae36f010ac2fe1135dbcbc2cf4c3138b8455d
kcroke <k.croke@shipworks.com>
2013-07-22 17:04:47 -0500

    Adding CleanupDatabaseType enumeration

---
+++ Code/ShipWorks/Actions/Tasks/Common/CleanupDatabaseType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ShipWorks.Actions.Tasks.Common
+{
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum CleanupDatabaseType
+    {
+        [Description("Audit")]
+        Audit = 0,
+
+        [Description("Email")]
+        Email = 1,
+
+        [Description("Label")]
+        Label = 2,
+
+        [Description("Print Job")]
+        Weekly = 3
+    }
+}
