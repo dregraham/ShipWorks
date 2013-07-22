aab6d6a746af60f438ff9a1c81d7b1bfdb7bc4e9
Michael Petter <m.petter@shipworks.com>
2013-07-22 14:52:01 -0500

    Implement background service registrar.

--- Code/ShipWorks/ApplicationCore/Services/Hosting/Background/BackgroundServiceRegistrar.cs
+++ Code/ShipWorks/ApplicationCore/Services/Hosting/Background/BackgroundServiceRegistrar.cs
@@ -1,4 +1,6 @@
 ﻿using log4net;
+using Microsoft.Win32;
+using System.Reflection;
 
 
 namespace ShipWorks.ApplicationCore.Services.Hosting.Background
@@ -7,14 +9,33 @@ namespace ShipWorks.ApplicationCore.Services.Hosting.Background
     {
         static readonly ILog log = LogManager.GetLogger(typeof(BackgroundServiceRegistrar));
 
+        const string RunKeyName = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+
         public void RegisterAll()
         {
             log.Info("Registering all services as background processes.");
+
+            using (var runKey = Registry.LocalMachine.OpenSubKey(RunKeyName, true))
+            {
+                foreach(var service in ShipWorksServiceBase.GetAllServices())
+                {
+                    var command = Assembly.GetExecutingAssembly().Location + " /s=" + service.ServiceType;
+                    runKey.SetValue(service.ServiceName, command);
+                }
+            }
         }
 
         public void UnregisterAll()
         {
             log.Info("Unregistering all background processes.");
+
+            using (var runKey = Registry.LocalMachine.OpenSubKey(RunKeyName, true))
+            {
+                foreach (var service in ShipWorksServiceBase.GetAllServices())
+                {
+                    runKey.DeleteValue(service.ServiceName, false);
+                }
+            }
         }
     }
 }
