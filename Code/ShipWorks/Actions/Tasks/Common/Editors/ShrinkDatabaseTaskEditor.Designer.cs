34e7d1b8147aee3947202dfe2447bde8d1be2599
thughes <t.hughes@shipworks.com>
2013-07-22 15:35:36 -0500

    Added Shrink DB task and editor.  Added redeploy assemblies command line option.  Merged in UPS Invoice total fix from trunk.

---
+++ Code/ShipWorks/Actions/Tasks/Common/Editors/ShrinkDatabaseTaskEditor.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ShipWorks.Actions.Tasks.Common.Editors
+{
+    partial class ShrinkDatabaseTaskEditor
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ShipWorks will try to reclaim unused database space.  ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ShipWorks.Properties.Resources.information16;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ShrinkDatabaseTaskEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ShrinkDatabaseTaskEditor";
+            this.Size = new System.Drawing.Size(405, 24);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+
+    }
+}
---
+++ Code/ShipWorks/Actions/Tasks/Common/Editors/ShrinkDatabaseTaskEditor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShipWorks.Actions.Tasks.Common.Editors
+{
+    /// <summary>
+    /// Task editor for shrinking a database.  It currently does nothing, but is required, so just set the Height to 0.
+    /// </summary>
+    public partial class ShrinkDatabaseTaskEditor : ActionTaskEditor
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ShrinkDatabaseTaskEditor(ShrinkDatabaseTask task)
+        {
+            InitializeComponent();
+        }
+    }
+}
