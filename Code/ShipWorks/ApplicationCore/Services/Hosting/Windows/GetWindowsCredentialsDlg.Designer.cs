2ea13d01a0c8954c30e0f0a75a82a7cb5b35ac51
kcroke <k.croke@shipworks.com>
2013-07-22 15:39:03 -0500

    Update to installer so it doesn't wait for BG process to complete

--- Code/ShipWorks/ApplicationCore/Services/Hosting/Windows/GetWindowsCredentialsDlg.Designer.cs
+++ Code/ShipWorks/ApplicationCore/Services/Hosting/Windows/GetWindowsCredentialsDlg.Designer.cs
@@ -160,6 +160,7 @@
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ShipWorks Service Credentials";
+            this.TopMost = true;
             this.ResumeLayout(false);
             this.PerformLayout();
 
