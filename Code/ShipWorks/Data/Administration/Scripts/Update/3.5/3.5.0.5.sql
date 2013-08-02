d280c822bd1e5e804a11cd0adef3b082757bfb23
kcroke <k.croke@shipworks.com>
2013-08-02 13:53:19 -0500

    Adding default email and print purge task to purge action.

--- Code/ShipWorks/Data/Administration/Scripts/Update/3.5/3.5.0.5.sql
+++ Code/ShipWorks/Data/Administration/Scripts/Update/3.5/3.5.0.5.sql
@@ -17,7 +17,7 @@ SET @NextFireTime = DATEADD(hh, DATEDIFF(hh, GETDATE(), GETUTCDATE()), @NextFire
 SET @NextFireTimeTicks = dbo.GetTicksFromDateTime(@NextFireTime)
 
 INSERT INTO [dbo].[Action] ([Name], [Enabled], [ComputerLimitedType], [ComputerLimitedList], [StoreLimited], [StoreLimitedList], [TriggerType], [TriggerSettings], [TaskSummary], [InternalOwner]) 
-VALUES (N'Delete old labels', 1, 0, '', 0, N'', 6, CONVERT(xml,N'<Settings><DailyActionSchedule><ScheduleType>2</ScheduleType><StartDateTimeInUtc>' + @StartDateTimeString  + '</StartDateTimeInUtc><FrequencyInDays>1</FrequencyInDays></DailyActionSchedule></Settings>',1), N'PurgeDatabase', NULL)
+VALUES (N'Delete Old Data', 1, 0, '', 0, N'', 6, CONVERT(xml,N'<Settings><DailyActionSchedule><ScheduleType>2</ScheduleType><StartDateTimeInUtc>' + @StartDateTimeString  + '</StartDateTimeInUtc><FrequencyInDays>1</FrequencyInDays></DailyActionSchedule></Settings>',1), N'PurgeDatabase', NULL)
 SELECT @ActionID = CONVERT(NVARCHAR(20), SCOPE_IDENTITY())
 
 PRINT(N'Add 1 row to [dbo].[Scheduling_JOB_DETAILS]')
@@ -27,7 +27,11 @@ VALUES (N'QuartzScheduler', @ActionID, N'DEFAULT', NULL, N'ShipWorks.Actions.Sch
 
 PRINT(N'Add 1 row to [dbo].[ActionTask]')
 INSERT INTO [dbo].[ActionTask] ([ActionID], [TaskIdentifier], [TaskSettings], [StepIndex], [InputSource], [InputFilterNodeID], [FilterCondition], [FilterConditionNodeID], [FlowSuccess], [FlowSkipped], [FlowError]) 
-VALUES (CONVERT(BIGINT, @ActionID), N'PurgeDatabase', CONVERT(xml,N'<Settings><CanTimeout value="True"/><TimeoutInHours value="6"/><RetentionPeriodInDays value="30"/><Purges><Item type="ShipWorks.Actions.Tasks.Common.PurgeDatabaseType" value="3"/></Purges></Settings>',1), 0, 1, -1, 0, -1, 0, 0, 0)
+VALUES (CONVERT(BIGINT, @ActionID), N'PurgeDatabase', CONVERT(xml,N'<Settings><CanTimeout value="True"/><TimeoutInHours value="3"/><RetentionPeriodInDays value="30"/><Purges><Item type="ShipWorks.Actions.Tasks.Common.PurgeDatabaseType" value="3"/></Purges></Settings>',1), 0, 1, -1, 0, -1, 0, 0, 0)
+ 
+PRINT (N'Add 1 row to [dbo].[ActionTask]')
+INSERT INTO [dbo].[ActionTask] ([ActionID], [TaskIdentifier], [TaskSettings], [StepIndex], [InputSource], [InputFilterNodeID], [FilterCondition], [FilterConditionNodeID], [FlowSuccess], [FlowSkipped], [FlowError])
+VALUES (CONVERT(BIGINT, @ActionID), N'PurgeDatabase', CONVERT(xml, N'<Settings><CanTimeout value="True"/><TimeoutInHours value="3"/><RetentionPeriodInDays value="180"/><Purges><Item type="ShipWorks.Actions.Tasks.Common.PurgeDatabaseType" value="2"/><Item type="ShipWorks.Actions.Tasks.Common.PurgeDatabaseType" value="4"/></Purges></Settings>', 1), 0, 1, -1, 0, -1, 0, 0, 0)
 
 PRINT(N'Add 1 row to [dbo].[Scheduling_TRIGGERS]')
 INSERT INTO [dbo].[Scheduling_TRIGGERS] ([SCHED_NAME], [TRIGGER_NAME], [TRIGGER_GROUP], [JOB_NAME], [JOB_GROUP], [DESCRIPTION], [NEXT_FIRE_TIME], [PREV_FIRE_TIME], [PRIORITY], [TRIGGER_STATE], [TRIGGER_TYPE], [START_TIME], [END_TIME], [CALENDAR_NAME], [MISFIRE_INSTR], [JOB_DATA]) 
@@ -36,10 +40,3 @@ VALUES (N'QuartzScheduler', @ActionID, N'DEFAULT', @ActionID, N'DEFAULT', NULL, 
 PRINT(N'Add 1 row to [dbo].[Scheduling_SIMPROP_TRIGGERS]')
 INSERT INTO [dbo].[Scheduling_SIMPROP_TRIGGERS] ([SCHED_NAME], [TRIGGER_NAME], [TRIGGER_GROUP], [STR_PROP_1], [STR_PROP_2], [STR_PROP_3], [INT_PROP_1], [INT_PROP_2], [LONG_PROP_1], [LONG_PROP_2], [DEC_PROP_1], [DEC_PROP_2], [BOOL_PROP_1], [BOOL_PROP_2]) 
 VALUES (N'QuartzScheduler', @ActionID, N'DEFAULT', N'Day', dbo.GetLocalTimezoneName(), NULL, 1, 0, 0, 0, 0.0000, 0.0000, 0, 0)
-
-
-
-
-
-
-
