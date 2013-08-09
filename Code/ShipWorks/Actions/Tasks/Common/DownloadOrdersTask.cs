914cf6df1b443f3ee3a19c4b40d72bc78e241053
John Yedlicka <j.yedlicka@shipworks.com>
2013-08-09 14:25:29 -0500

    Implement the "download orders" task
    - Download order task editor class
    
    - Download order task class
    
    - Fix bug in DownloadManager to use super user account when the download is
      initiated by ShipWorks

---
+++ Code/ShipWorks/Actions/Tasks/Common/DownloadOrdersTask.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.Actions.Tasks.Common.Editors;
+using ShipWorks.Data;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores;
+using ShipWorks.Stores.Communication;
+
+namespace ShipWorks.Actions.Tasks.Common
+{
+    /// <summary>
+    /// A task to automate order downloads to run on a scheduled basis.
+    /// </summary>
+    [ActionTask("Download orders", "DownloadOrders", ActionTriggerClassifications.Scheduled)]
+    public class DownloadOrdersTask : ActionTask
+    {
+        /// <summary>
+        /// Creates the editor that is used to edit the task.
+        /// </summary>
+        /// <returns></returns>
+        public override ActionTaskEditor CreateEditor()
+        {
+            return new DownloadOrdersTaskEditor();
+        }
+
+        /// <summary>
+        /// Indicates if the task requires input to function.  Such as the contents of a filter, or the item that caused the action.
+        /// </summary>
+        public override bool RequiresInput
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Run the task.
+        /// This should perform any long-running operations, but should NOT save to the database. This function is NOT within a transaction and should not be, as it's designed
+        /// for doing things like printing and connecting to external websites, which take too much time to be within a transaction.
+        /// </summary>
+        public override void Run(List<long> inputKeys, ActionStepContext context)
+        {
+            ActionEntity action = ActionManager.GetAction(context.Queue.ActionID);
+            if (action != null)
+            {
+                List<StoreEntity> storeEntities = new List<StoreEntity>();
+
+                foreach (long storeId in action.StoreLimitedList)
+                {
+                    StoreEntity storeEntity = StoreManager.GetStore(storeId);
+                    storeEntities.Add(storeEntity);
+                }
+
+                DownloadManager.StartDownload(storeEntities, DownloadInitiatedBy.ShipWorks);
+            }
+    }
+    }
+}
---
+++ Code/ShipWorks/Actions/Tasks/Common/Editors/DownloadOrdersTaskEditor.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ShipWorks.Actions.Tasks.Common.Editors
+{
+    partial class DownloadOrdersTaskEditor
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.taskDescription = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // taskDescription
+            // 
+            this.taskDescription.AutoSize = true;
+            this.taskDescription.Location = new System.Drawing.Point(4, 4);
+            this.taskDescription.Name = "taskDescription";
+            this.taskDescription.Size = new System.Drawing.Size(376, 13);
+            this.taskDescription.TabIndex = 0;
+            this.taskDescription.Text = "Use the Settings tab to select the stores that orders should be downloaded for.";
+            // 
+            // DownloadOrdersTaskEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.taskDescription);
+            this.Name = "DownloadOrdersTaskEditor";
+            this.Size = new System.Drawing.Size(397, 26);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label taskDescription;
+    }
+}
