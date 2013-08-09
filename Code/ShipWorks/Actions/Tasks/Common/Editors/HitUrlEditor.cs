ffca1a1cae7f39eb24dfee0d882fef8e6c249025
kcroke <k.croke@shipworks.com>
2013-08-09 14:30:19 -0500

    Added skeleton UrlEditor and put logic into the new HitUrlTask

---
+++ Code/ShipWorks/Actions/Tasks/Common/Editors/HitUrlEditor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShipWorks.Actions.Tasks.Common.Editors
+{
+    public partial class HitUrlEditor : TemplateBasedTaskEditor
+    {
+        private HitUrlTask task;
+
+        public HitUrlEditor(HitUrlTask task)
+        {
+            InitializeComponent();
+
+            this.task = task;
+        }
+
+    }
+}
---
+++ Code/ShipWorks/Actions/Tasks/Common/HitUrlTask.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Interapptive.Shared.Net;
+using ShipWorks.Actions.Tasks.Common.Editors;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Templates;
+using ShipWorks.Templates.Processing;
+using log4net;
+
+namespace ShipWorks.Actions.Tasks.Common
+{
+    /// <summary>
+    /// Task to hit a URL.
+    /// </summary>
+    [ActionTask("Hit URL", "HitUrl")]
+    public class HitUrlTask : TemplateBasedTask
+    {
+        // Logger
+        private static readonly ILog log = LogManager.GetLogger(typeof(HitUrlTask));
+
+        /// <summary>
+        /// Gets or sets the verb.
+        /// </summary>
+        public HttpVerb Verb
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the URL to hit.
+        /// </summary>
+        public string UrlToHit
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [use basic authentication].
+        /// </summary>
+        public bool UseBasicAuthentication
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the username.
+        /// </summary>
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the HTTP headers.
+        /// </summary>
+        public Dictionary<string, string> HttpHeaders
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether to postpone running or not.
+        /// </summary>
+        public override bool EnablePostpone
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Create the editor for the settings
+        /// </summary>
+        public override ActionTaskEditor CreateEditor()
+        {
+            return new HitUrlEditor(this);
+        }
+
+        /// <summary>
+        /// Sends template to URL (or just sends request if no Post Data)
+        /// </summary>
+        protected override void ProcessTemplateResults(TemplateEntity template, IList<TemplateResult> templateResults, ActionStepContext context)
+        {
+            if (Verb == HttpVerb.Get)
+            {
+                HttpRequestSubmitter request = new HttpTextPostRequestSubmitter("", GetContentType(TemplateOutputFormat.Text));
+                ProcessResult(request);
+            }
+            else
+            {
+                foreach (var templateResult in templateResults)
+                {
+                    HttpTextPostRequestSubmitter request = new HttpTextPostRequestSubmitter(templateResult.ReadResult(), GetContentType((TemplateOutputFormat) template.OutputFormat));
+                    ProcessResult(request);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Processes the result.
+        /// </summary>
+        private void ProcessResult(HttpRequestSubmitter request)
+        {
+            request.Uri = new Uri(UrlToHit);
+            request.Verb = Verb;
+
+            if (UseBasicAuthentication)
+            {
+                request.Credentials = new NetworkCredential(Username, Password);
+            }
+
+            foreach (var header in HttpHeaders)
+            {
+                request.Headers.Add(header.Key, header.Value);
+            }
+
+            request.GetResponse();
+        }
+
+        /// <summary>
+        /// Gets the type of the content.
+        /// </summary>
+        /// <exception cref="System.ArgumentOutOfRangeException"></exception>
+        private static string GetContentType(TemplateOutputFormat templateOutputFormat)
+        {
+            switch (templateOutputFormat)
+            {
+                case TemplateOutputFormat.Html:
+                    return "text/html";
+                case TemplateOutputFormat.Xml:
+                    return "text/xml";
+                case TemplateOutputFormat.Text:
+                    return "text/plain";
+                default:
+                    throw new ArgumentOutOfRangeException("templateOutputFormat", "Invalid template Output Format");
+            }
+        }
+    }
+}
