dab89e12a3d1a737bce48bb6a0cf3f62e02051ad
John Yedlicka <j.yedlicka@shipworks.com>
2013-08-13 17:15:31 -0500

    Update run command task editor to accept a command line and timeout options
    - Tokenized text box for capturing the command/script to run
    
    - Checkbox and numeric input field to indicate whether the command can timeout
      and how long the command can run before it times out respectively

--- Code/ShipWorks/Actions/Tasks/Common/Editors/RunCommandTaskEditor.Designer.cs
+++ Code/ShipWorks/Actions/Tasks/Common/Editors/RunCommandTaskEditor.Designer.cs
@@ -28,10 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            ShipWorks.Templates.Tokens.CommonTokenSuggestionsFactory commonTokenSuggestionsFactory1 = new ShipWorks.Templates.Tokens.CommonTokenSuggestionsFactory();
+            this.executeLabel = new System.Windows.Forms.Label();
+            this.tokenizedExecuteCommand = new ShipWorks.Templates.Tokens.TemplateTokenTextBox();
+            this.shouldTimeout = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timeoutInMinutes = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.timeoutInMinutes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // executeLabel
+            // 
+            this.executeLabel.AutoSize = true;
+            this.executeLabel.Location = new System.Drawing.Point(3, 3);
+            this.executeLabel.Name = "executeLabel";
+            this.executeLabel.Size = new System.Drawing.Size(137, 13);
+            this.executeLabel.TabIndex = 1;
+            this.executeLabel.Text = "With the program or script:";
+            // 
+            // tokenizedExecuteCommand
+            // 
+            this.tokenizedExecuteCommand.Location = new System.Drawing.Point(144, 0);
+            this.tokenizedExecuteCommand.MaxLength = 32767;
+            this.tokenizedExecuteCommand.Name = "tokenizedExecuteCommand";
+            this.tokenizedExecuteCommand.Size = new System.Drawing.Size(283, 22);
+            this.tokenizedExecuteCommand.TabIndex = 2;
+            this.tokenizedExecuteCommand.TokenSuggestionFactory = commonTokenSuggestionsFactory1;
+            // 
+            // shouldTimeout
+            // 
+            this.shouldTimeout.AutoSize = true;
+            this.shouldTimeout.Checked = true;
+            this.shouldTimeout.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.shouldTimeout.Location = new System.Drawing.Point(6, 35);
+            this.shouldTimeout.Name = "shouldTimeout";
+            this.shouldTimeout.Size = new System.Drawing.Size(211, 17);
+            this.shouldTimeout.TabIndex = 3;
+            this.shouldTimeout.Text = "Stop if the command takes longer than";
+            this.shouldTimeout.UseVisualStyleBackColor = true;
+            this.shouldTimeout.CheckedChanged += new System.EventHandler(this.OnShouldTimeoutChecked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "minutes";
+            // 
+            // timeoutInMinutes
+            // 
+            this.timeoutInMinutes.Location = new System.Drawing.Point(216, 31);
+            this.timeoutInMinutes.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.timeoutInMinutes.Name = "timeoutInMinutes";
+            this.timeoutInMinutes.Size = new System.Drawing.Size(42, 21);
+            this.timeoutInMinutes.TabIndex = 5;
+            this.timeoutInMinutes.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.timeoutInMinutes.ValueChanged += new System.EventHandler(this.OnTimeoutValueChanged);
+            // 
+            // RunCommandTaskEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.timeoutInMinutes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shouldTimeout);
+            this.Controls.Add(this.tokenizedExecuteCommand);
+            this.Controls.Add(this.executeLabel);
+            this.Name = "RunCommandTaskEditor";
+            this.Size = new System.Drawing.Size(435, 59);
+            ((System.ComponentModel.ISupportInitialize)(this.timeoutInMinutes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label executeLabel;
+        private Templates.Tokens.TemplateTokenTextBox tokenizedExecuteCommand;
+        private System.Windows.Forms.CheckBox shouldTimeout;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown timeoutInMinutes;
+
     }
 }
--- Code/ShipWorks/Actions/Tasks/Common/Editors/RunCommandTaskEditor.cs
+++ Code/ShipWorks/Actions/Tasks/Common/Editors/RunCommandTaskEditor.cs
@@ -11,9 +11,59 @@ namespace ShipWorks.Actions.Tasks.Common.Editors
 {
     public partial class RunCommandTaskEditor : ActionTaskEditor
     {
+        private readonly RunCommandTask task;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RunCommandTaskEditor"/> class.
+        /// </summary>
+        /// <param name="task">The task.</param>
         public RunCommandTaskEditor(RunCommandTask task)
         {
+            if (task == null)
+            {
+                throw new ArgumentNullException("task");
+            }
+
             InitializeComponent();
+            
+            this.task = task;
+            tokenizedExecuteCommand.Text = task.Command;
+            shouldTimeout.Checked = task.ShouldStopCommandOnTimeout;
+            timeoutInMinutes.Value = task.CommandTimeoutInMinutes;
+
+            tokenizedExecuteCommand.TextChanged += OnTokenizedExecuteCommandTextChanged;
+        }
+
+        /// <summary>
+        /// Called when the timeout checkbox is checked/unchecked to update the timeout 
+        /// property of the task and toggle the timeout values in minutes.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void OnShouldTimeoutChecked(object sender, EventArgs e)
+        {
+            task.ShouldStopCommandOnTimeout = shouldTimeout.Checked;
+            timeoutInMinutes.Enabled = shouldTimeout.Checked;
+        }
+
+        /// <summary>
+        /// Called when [timeout value changed] to update the task's timeout property.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void OnTimeoutValueChanged(object sender, EventArgs e)
+        {
+            task.CommandTimeoutInMinutes = (int)timeoutInMinutes.Value;
+        }
+
+        /// <summary>
+        /// Called when [tokenized execute command text changed] to update the command property of the task.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void OnTokenizedExecuteCommandTextChanged(object sender, EventArgs e)
+        {
+            task.Command = tokenizedExecuteCommand.Text;
         }
     }
 }
