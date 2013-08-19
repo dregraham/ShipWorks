13f3be624639701ec6807c0a6f9e406e5b5d76b6
kcroke <k.croke@shipworks.com>
2013-08-19 16:53:48 -0500

    WIP: file saving to temp directory

--- Code/ShipWorks/Actions/Tasks/Common/FtpFileTask.cs
+++ Code/ShipWorks/Actions/Tasks/Common/FtpFileTask.cs
@@ -1,40 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using ShipWorks.Actions.Tasks.Common.Editors;
+using ShipWorks.ApplicationCore;
 using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Templates;
 using ShipWorks.Templates.Processing;
+using ShipWorks.Templates.Saving;
 using log4net;
 
 namespace ShipWorks.Actions.Tasks.Common
 {
-
     /// <summary>
     /// Task for ftping a chosen template
     /// </summary>
     [ActionTask("Transfer file(s)", "Ftp")]
     public class FtpFileTask : TemplateBasedTask
     {
+        private string baseTempFileName;
+        //private int tempFileIndex = 0;
+
         // Logger
         static readonly ILog log = LogManager.GetLogger(typeof(FtpFileTask));
+
+        public FtpFileTask()
+        {
+            baseTempFileName = Path.Combine(DataPath.ShipWorksTemp, Guid.NewGuid().ToString());
+        }
 
         /// <summary>
         /// Gets or sets the FTP account unique identifier.
         /// </summary>
-
         public long? FtpAccountID { get; set; }
-        
+
         /// <summary>
         /// Gets or sets the FTP folder.
         /// </summary>
         public string FtpFolder { get; set; }
-        
+
         /// <summary>
         /// Gets or sets the name of the FTP file.
         /// </summary>
         public string FtpFileName { get; set; }
-
 
         /// <summary>
         /// The label that goes before what the data source for the task should be.
@@ -62,7 +69,40 @@ namespace ShipWorks.Actions.Tasks.Common
 
         protected override void ProcessTemplateResults(TemplateEntity template, IList<TemplateResult> templateResults, ActionStepContext context)
         {
-            throw new NotImplementedException();
+            TemplateTree editableTemplateTreeClone = TemplateManager.Tree.CreateEditableClone();
+            TemplateEntity editableCopyOfTemplate = editableTemplateTreeClone.GetTemplate(template.TemplateID);
+
+            // Create the print job using the default settings from the template
+            SaveWriter writer = SaveWriter.Create(editableCopyOfTemplate, templateResults);
+
+            try
+            {
+                editableCopyOfTemplate.SaveFileName = FtpFileName;
+                editableCopyOfTemplate.SaveFileFolder = baseTempFileName + "/" + FtpFolder;
+                editableCopyOfTemplate.SaveFilePrompt = (int)SavePromptWhen.Never;
+
+                writer.PromptForFile += OnSavePromptForFile;
+
+                writer.Save();
+            }
+            catch (SavingNoTemplateOutputException)
+            {
+                log.Info("SaveFileTask skipped due to no template output.");
+            }
+            catch (SaveException ex)
+            {
+                throw new ActionTaskRunException(ex.Message, ex);
+            }
+        }
+
+
+        private void OnSavePromptForFile(object sender, SavePromptForFileEventArgs e)
+        {
+            //string fileName = Path.Combine(baseTempFileName, FtpFolder);
+
+            //e.ResultName = fileName;
+
+            //tempFileIndex++;
         }
     }
 }
