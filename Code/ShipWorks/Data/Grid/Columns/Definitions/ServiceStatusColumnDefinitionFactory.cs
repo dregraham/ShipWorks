22f31907145a39fc25faaa86d1f1d4cd5d8232e4
John Yedlicka <j.yedlicka@shipworks.com>
2013-08-19 15:29:53 -0500

    Move the status column to first column in the service status dialog

--- Code/ShipWorks/Data/Grid/Columns/Definitions/ServiceStatusColumnDefinitionFactory.cs
+++ Code/ShipWorks/Data/Grid/Columns/Definitions/ServiceStatusColumnDefinitionFactory.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using Interapptive.Shared.Net;
-using Interapptive.Shared.Utility;
+﻿using Interapptive.Shared.Utility;
 using SD.LLBLGen.Pro.ORMSupportClasses;
 using ShipWorks.ApplicationCore.Services;
-using ShipWorks.ApplicationCore.Services.UI;
 using ShipWorks.Data.Grid.Columns.DisplayTypes;
 using ShipWorks.Data.Grid.Columns.DisplayTypes.Decorators;
 using ShipWorks.Data.Grid.Columns.SortProviders;
@@ -26,6 +23,13 @@ namespace ShipWorks.Data.Grid.Columns.Definitions
         {
             GridColumnDefinitionCollection definitions = new GridColumnDefinitionCollection
             {
+                new GridColumnDefinition("{F1979994-BB60-45D6-B2C1-18966AB15B65}", true,
+                    new GridEnumDisplayType<ServiceStatus>(EnumSortMethod.Value),
+                    "Status", "(icon)",
+                    new GridColumnFunctionValueProvider(x => ((ServiceStatusEntity)x).GetStatus()),
+                    null) 
+                    { DefaultWidth = 125 },
+
                 new GridColumnDefinition("{7BE7DBCE-3500-4B01-8594-471035C001AD}", true,
                     new GridComputerDisplayType(),
                     "Computer", @"\\ShippingPC",
@@ -39,22 +43,15 @@ namespace ShipWorks.Data.Grid.Columns.Definitions
                     ServiceStatusFields.ServiceType)
                     { DefaultWidth = 140 },
 
-                new GridColumnDefinition("{F1979994-BB60-45D6-B2C1-18966AB15B65}", true,
-                    new GridEnumDisplayType<ServiceStatus>(EnumSortMethod.Value),
-                    "Status", "(icon)",
-                    new GridColumnFunctionValueProvider(x => ((ServiceStatusEntity)x).GetStatus()),
-                    null) //new GridColumnSortProvider(CreateStatusSortField()))
-                    { DefaultWidth = 125 },
-
                 new GridColumnDefinition("{D5FCAF56-6931-4AA3-AD2D-3CCFB1D66098}", true, 
                     new GridDateDisplayType { UseDescriptiveDates = true },
                     "Last Started", DateTimeUtility.ParseEnUS("03/04/2012 1:30 PM").ToUniversalTime(),
-                    ServiceStatusFields.LastStartDateTime),
+                    ServiceStatusFields.LastStartDateTime) { DefaultWidth = 125 },
 
                 new GridColumnDefinition("{50003D6C-4082-4E52-9E23-F72E2CFBA924}", true, 
                     new GridDateDisplayType { UseDescriptiveDates = true }.Decorate(new GridServiceStopTimeDecorator()),
                     "Last Stopped", DateTimeUtility.ParseEnUS("03/16/2012 7:41 PM").ToUniversalTime(),
-                    ServiceStatusFields.LastStopDateTime)
+                    ServiceStatusFields.LastStopDateTime) { DefaultWidth = 125 }
             };
 
             return definitions;
