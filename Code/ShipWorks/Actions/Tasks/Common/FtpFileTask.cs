10290005422dc86910b42bf13ccb4a91a86ad281
kcroke <k.croke@shipworks.com>
2013-08-20 11:00:09 -0500

    FtpFileTask - Did some refactorring and fixed an upload issue.

--- Code/ShipWorks/Actions/Tasks/Common/FtpFileTask.cs
+++ Code/ShipWorks/Actions/Tasks/Common/FtpFileTask.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Rebex.Net;
 using ShipWorks.Actions.Tasks.Common.Editors;
 using ShipWorks.ApplicationCore;
 using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.FileTransfer;
 using ShipWorks.Templates;
 using ShipWorks.Templates.Processing;
 using ShipWorks.Templates.Saving;
@@ -17,15 +19,20 @@ namespace ShipWorks.Actions.Tasks.Common
     [ActionTask("Transfer file(s)", "Ftp")]
     public class FtpFileTask : TemplateBasedTask
     {
-        private string baseTempFileName;
-        //private int tempFileIndex = 0;
+        /// <summary>
+        /// The base temporary file name
+        /// </summary>
+        private readonly string rootTempFileName;
 
         // Logger
-        static readonly ILog log = LogManager.GetLogger(typeof(FtpFileTask));
+        private static readonly ILog log = LogManager.GetLogger(typeof(FtpFileTask));
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FtpFileTask"/> class.
+        /// </summary>
         public FtpFileTask()
         {
-            baseTempFileName = Path.Combine(DataPath.ShipWorksTemp, Guid.NewGuid().ToString());
+            rootTempFileName = Path.Combine(DataPath.ShipWorksTemp, Guid.NewGuid().ToString());
         }
 
         /// <summary>
@@ -53,7 +60,7 @@ namespace ShipWorks.Actions.Tasks.Common
                 return "Ftp file(s) using:";
             }
         }
-		
+
         /// <summary>
         /// Gets a value indicating whether to postpone running or not.
         /// </summary>
@@ -62,26 +69,67 @@ namespace ShipWorks.Actions.Tasks.Common
             get { return false; }
         }
 
+        /// <summary>
+        /// Create the editor
+        /// </summary>
+        /// <returns></returns>
         public override ActionTaskEditor CreateEditor()
         {
             return new FtpFileTaskEditor(this);
         }
 
+        /// <summary>
+        /// Most be overridden by derived classes to do the actual work of outputing template content
+        /// </summary>
+        /// <exception cref="ActionTaskRunException"></exception>
         protected override void ProcessTemplateResults(TemplateEntity template, IList<TemplateResult> templateResults, ActionStepContext context)
         {
-            TemplateTree editableTemplateTreeClone = TemplateManager.Tree.CreateEditableClone();
-            TemplateEntity editableCopyOfTemplate = editableTemplateTreeClone.GetTemplate(template.TemplateID);
+            SaveTemplateResultsToTempFile(template, templateResults);
 
-            // Create the print job using the default settings from the template
-            SaveWriter writer = SaveWriter.Create(editableCopyOfTemplate, templateResults);
+            UploadFiles();
+        }
+
+        /// <summary>
+        /// Uploads the files.
+        /// </summary>
+        /// <exception cref="ActionTaskRunException"></exception>
+        private void UploadFiles()
+        {
+            if (!FtpAccountID.HasValue)
+            {
+                throw new ActionTaskRunException("Ftp account not configured for this action.");
+            }
 
             try
             {
-                editableCopyOfTemplate.SaveFileName = FtpFileName;
-                editableCopyOfTemplate.SaveFileFolder = baseTempFileName + "/" + FtpFolder;
-                editableCopyOfTemplate.SaveFilePrompt = (int)SavePromptWhen.Never;
+                FtpAccountEntity ftpAccountEntity = FtpAccountManager.GetAccount(FtpAccountID.Value);
+                Ftp ftp = FtpUtility.LogonToFtp(ftpAccountEntity);
 
-                writer.PromptForFile += OnSavePromptForFile;
+                ftp.PutFiles(string.Format("{0}\\*", rootTempFileName), "/", FtpBatchTransferOptions.Recursive, FtpActionOnExistingFiles.OverwriteAll);
+            }
+            catch (FtpBatchTransferException ex)
+            {
+                log.Error("Error transferring File", ex);
+                throw new ActionTaskRunException(String.Format("Error uploading file. Ftp returned error: {0}", ex.Message));
+            }
+        }
+
+        /// <summary>
+        /// Saves the template results automatic temporary file.
+        /// </summary>
+        /// <exception cref="ActionTaskRunException"></exception>
+        private void SaveTemplateResultsToTempFile(TemplateEntity template, IList<TemplateResult> templateResults)
+        {
+            try
+            {
+                TemplateTree editableTemplateTreeClone = TemplateManager.Tree.CreateEditableClone();
+                TemplateEntity editableCopyOfTemplate = editableTemplateTreeClone.GetTemplate(template.TemplateID);
+
+                SaveWriter writer = SaveWriter.Create(editableCopyOfTemplate, templateResults);
+
+                editableCopyOfTemplate.SaveFileName = FtpFileName;
+                editableCopyOfTemplate.SaveFileFolder = string.Format("{0}/{1}", rootTempFileName, FtpFolder);
+                editableCopyOfTemplate.SaveFilePrompt = (int)SavePromptWhen.Never;
 
                 writer.Save();
             }
@@ -91,18 +139,10 @@ namespace ShipWorks.Actions.Tasks.Common
             }
             catch (SaveException ex)
             {
+                log.Error("Error saving file before FTP.", ex);
+
                 throw new ActionTaskRunException(ex.Message, ex);
             }
-        }
-
-
-        private void OnSavePromptForFile(object sender, SavePromptForFileEventArgs e)
-        {
-            //string fileName = Path.Combine(baseTempFileName, FtpFolder);
-
-            //e.ResultName = fileName;
-
-            //tempFileIndex++;
         }
     }
 }
