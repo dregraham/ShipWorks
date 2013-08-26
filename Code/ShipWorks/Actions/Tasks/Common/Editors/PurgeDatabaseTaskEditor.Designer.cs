6d446c5e9b91472a1fd18ebb7534f3291563b405
John Yedlicka <j.yedlicka@shipworks.com>
2013-08-26 13:47:02 -0500

    Modify "delete old data" task UI based on UI review
    - Adjust label/layout of retention setting
    
    - Replacing tooltips on checkboxes with with InfoTip controls

--- Code/ShipWorks/Actions/Tasks/Common/Editors/PurgeDatabaseTaskEditor.Designer.cs
+++ Code/ShipWorks/Actions/Tasks/Common/Editors/PurgeDatabaseTaskEditor.Designer.cs
@@ -32,7 +32,6 @@
             this.timeoutPurgeCheckbox = new System.Windows.Forms.CheckBox();
             this.timeoutHoursLabel = new System.Windows.Forms.Label();
             this.timeoutInHours = new System.Windows.Forms.NumericUpDown();
-            this.labelCleanup = new System.Windows.Forms.Label();
             this.retentionPeriodInDays = new System.Windows.Forms.NumericUpDown();
             this.retentionDaysLabel = new System.Windows.Forms.Label();
             this.email = new System.Windows.Forms.CheckBox();
@@ -46,6 +45,10 @@
             this.purgeToolTip = new System.Windows.Forms.ToolTip(this.components);
             this.retentionHeading = new System.Windows.Forms.Label();
             this.timeoutHelp = new ShipWorks.UI.Controls.InfoTip();
+            this.downloadHistoryHelp = new ShipWorks.UI.Controls.InfoTip();
+            this.emailContentHelp = new ShipWorks.UI.Controls.InfoTip();
+            this.shippingLabelsHelp = new ShipWorks.UI.Controls.InfoTip();
+            this.printJobHelp = new ShipWorks.UI.Controls.InfoTip();
             ((System.ComponentModel.ISupportInitialize)(this.timeoutInHours)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.retentionPeriodInDays)).BeginInit();
             this.SuspendLayout();
@@ -86,18 +89,9 @@
             0,
             0});
             // 
-            // labelCleanup
-            // 
-            this.labelCleanup.AutoSize = true;
-            this.labelCleanup.Location = new System.Drawing.Point(18, 20);
-            this.labelCleanup.Name = "labelCleanup";
-            this.labelCleanup.Size = new System.Drawing.Size(129, 13);
-            this.labelCleanup.TabIndex = 7;
-            this.labelCleanup.Text = "Delete records older than";
-            // 
             // retentionPeriodInDays
             // 
-            this.retentionPeriodInDays.Location = new System.Drawing.Point(149, 20);
+            this.retentionPeriodInDays.Location = new System.Drawing.Point(21, 18);
             this.retentionPeriodInDays.Maximum = new decimal(new int[] {
             2000,
             0,
@@ -120,7 +114,7 @@
             // retentionDaysLabel
             // 
             this.retentionDaysLabel.AutoSize = true;
-            this.retentionDaysLabel.Location = new System.Drawing.Point(202, 20);
+            this.retentionDaysLabel.Location = new System.Drawing.Point(74, 20);
             this.retentionDaysLabel.Name = "retentionDaysLabel";
             this.retentionDaysLabel.Size = new System.Drawing.Size(34, 13);
             this.retentionDaysLabel.TabIndex = 9;
@@ -157,7 +151,7 @@
             this.printJobs.TabIndex = 5;
             this.printJobs.Text = "Print Job Content";
             this.purgeToolTip.SetToolTip(this.printJobs, "The content of the print job is the only thing that will deleted. You will still " +
-        "be able to see your print history and filter against it.\r\n");
+        "be able to see your print history and filter against it.");
             this.printJobs.UseVisualStyleBackColor = true;
             // 
             // labels
@@ -180,8 +174,6 @@
             this.downloads.Size = new System.Drawing.Size(110, 17);
             this.downloads.TabIndex = 2;
             this.downloads.Text = "Download History";
-            this.purgeToolTip.SetToolTip(this.downloads, "This does not delete any of your orders, just the log of when they were downloade" +
-        "d.");
             this.downloads.UseVisualStyleBackColor = true;
             // 
             // label1
@@ -190,9 +182,10 @@
             this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(3, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.Size = new System.Drawing.Size(91, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Items to delete:";
+            this.label1.Text = "Data to delete:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label2
             // 
@@ -221,9 +214,9 @@
             this.retentionHeading.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.retentionHeading.Location = new System.Drawing.Point(3, 0);
             this.retentionHeading.Name = "retentionHeading";
-            this.retentionHeading.Size = new System.Drawing.Size(126, 13);
+            this.retentionHeading.Size = new System.Drawing.Size(151, 13);
             this.retentionHeading.TabIndex = 20;
-            this.retentionHeading.Text = "How far back to start";
+            this.retentionHeading.Text = "Delete records older than";
             // 
             // timeoutHelp
             // 
@@ -232,12 +225,56 @@
             this.timeoutHelp.Name = "timeoutHelp";
             this.timeoutHelp.Size = new System.Drawing.Size(12, 12);
             this.timeoutHelp.TabIndex = 21;
-            this.timeoutHelp.Title = "";
+            this.timeoutHelp.Title = "Deletion timeout";
+            // 
+            // downloadHistoryHelp
+            // 
+            this.downloadHistoryHelp.Caption = "This does not delete any of your orders, just the log of when they were downloade" +
+    "d.";
+            this.downloadHistoryHelp.Location = new System.Drawing.Point(127, 88);
+            this.downloadHistoryHelp.Name = "downloadHistoryHelp";
+            this.downloadHistoryHelp.Size = new System.Drawing.Size(12, 12);
+            this.downloadHistoryHelp.TabIndex = 22;
+            this.downloadHistoryHelp.Title = "Download History";
+            // 
+            // emailContentHelp
+            // 
+            this.emailContentHelp.Caption = "The message body is the only thing that will be deleted. You will still be able t" +
+    "o see if an email was sent, when, to whom, and the subject line.";
+            this.emailContentHelp.Location = new System.Drawing.Point(109, 111);
+            this.emailContentHelp.Name = "emailContentHelp";
+            this.emailContentHelp.Size = new System.Drawing.Size(12, 12);
+            this.emailContentHelp.TabIndex = 23;
+            this.emailContentHelp.Title = "Email Content";
+            // 
+            // shippingLabelsHelp
+            // 
+            this.shippingLabelsHelp.Caption = "This only deletes the actual label image. The shipment settings, tracking number," +
+    " and all other information is preserved.";
+            this.shippingLabelsHelp.Location = new System.Drawing.Point(149, 134);
+            this.shippingLabelsHelp.Name = "shippingLabelsHelp";
+            this.shippingLabelsHelp.Size = new System.Drawing.Size(12, 12);
+            this.shippingLabelsHelp.TabIndex = 24;
+            this.shippingLabelsHelp.Title = "Shipping Label Images";
+            // 
+            // printJobHelp
+            // 
+            this.printJobHelp.Caption = "The content of the print job is the only thing that will deleted. You will still " +
+    "be able to see your print history and filter against it.";
+            this.printJobHelp.Location = new System.Drawing.Point(128, 157);
+            this.printJobHelp.Name = "printJobHelp";
+            this.printJobHelp.Size = new System.Drawing.Size(12, 12);
+            this.printJobHelp.TabIndex = 25;
+            this.printJobHelp.Title = "Print Job Content";
             // 
             // PurgeDatabaseTaskEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.printJobHelp);
+            this.Controls.Add(this.shippingLabelsHelp);
+            this.Controls.Add(this.emailContentHelp);
+            this.Controls.Add(this.downloadHistoryHelp);
             this.Controls.Add(this.timeoutHelp);
             this.Controls.Add(this.retentionHeading);
             this.Controls.Add(this.reclaimDiskSpaceCheckbox);
@@ -250,7 +287,6 @@
             this.Controls.Add(this.email);
             this.Controls.Add(this.retentionDaysLabel);
             this.Controls.Add(this.retentionPeriodInDays);
-            this.Controls.Add(this.labelCleanup);
             this.Controls.Add(this.timeoutPurgeCheckbox);
             this.Controls.Add(this.timeoutHoursLabel);
             this.Controls.Add(this.timeoutInHours);
@@ -269,7 +305,6 @@
         private System.Windows.Forms.CheckBox timeoutPurgeCheckbox;
         private System.Windows.Forms.Label timeoutHoursLabel;
         private System.Windows.Forms.NumericUpDown timeoutInHours;
-        private System.Windows.Forms.Label labelCleanup;
         private System.Windows.Forms.NumericUpDown retentionPeriodInDays;
         private System.Windows.Forms.Label retentionDaysLabel;
         private System.Windows.Forms.CheckBox email;
@@ -283,5 +318,9 @@
         private System.Windows.Forms.ToolTip purgeToolTip;
         private System.Windows.Forms.Label retentionHeading;
         private ShipWorks.UI.Controls.InfoTip timeoutHelp;
+        private ShipWorks.UI.Controls.InfoTip downloadHistoryHelp;
+        private ShipWorks.UI.Controls.InfoTip emailContentHelp;
+        private ShipWorks.UI.Controls.InfoTip shippingLabelsHelp;
+        private ShipWorks.UI.Controls.InfoTip printJobHelp;
     }
 }
