9d4cf40284ae6460c04eb9b782727a1491a3706c
John Yedlicka <j.yedlicka@shipworks.com>
2013-08-28 08:37:43 -0500

    Refresh the data in the service status dialog every 10 seconds
    - Add a timer to the form to trigger the service data to be reloaded since
      the gateway used to populate the gird is no longer monitoring the database
      for changes

--- Code/ShipWorks/ApplicationCore/Services/UI/ServiceStatusDialog.cs
+++ Code/ShipWorks/ApplicationCore/Services/UI/ServiceStatusDialog.cs
@@ -23,6 +23,10 @@ using ShipWorks.Actions.Triggers;
 
 namespace ShipWorks.ApplicationCore.Services.UI
 {
+    /// <summary>
+    /// A dialog box showing the list of computers that require the ShipWorks background process/service to 
+    /// be running based on the configuration of any scheduled actions in the system.
+    /// </summary>
     public partial class ServiceStatusDialog : Form
     {
         static readonly Guid gridSettingsKey = new Guid("{53EE16A4-9315-4D22-B768-58613546476B}");
@@ -30,8 +34,7 @@ namespace ShipWorks.ApplicationCore.Services.UI
         private bool startingService;
         private Timer startingServiceTimer;
 
-        private LocalCollectionEntityGateway<ServiceStatusEntity> requiredServicesGateway;
-
+        private readonly Timer dataRefreshTimer;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ServiceStatusDialog"/> class.
@@ -41,8 +44,11 @@ namespace ShipWorks.ApplicationCore.Services.UI
             InitializeComponent();
 
             WindowStateSaver.Manage(this, WindowStateSaverOptions.SizeOnly);
-        }
 
+            // Keep the timer disabled until after the initial population of the grid
+            dataRefreshTimer = new Timer { Interval = 10000, Enabled = false };
+            dataRefreshTimer.Tick += OnDataRefreshTimerTick;
+        }
 
         /// <summary>
         /// Raises the <see cref="E:System.Windows.Forms.Form.Load" /> event.
@@ -64,13 +70,41 @@ namespace ShipWorks.ApplicationCore.Services.UI
             entityGrid.Renderer = AppearanceHelper.CreateWindowsRenderer();
             entityGrid.RowHighlightType = RowHighlightType.None;
 
-            //Open the data
-            requiredServicesGateway = new LocalCollectionEntityGateway<ServiceStatusEntity>(ServiceStatusManager.GetComputersRequiringShipWorksService());
-            entityGrid.OpenGateway(requiredServicesGateway);
+            // Load the data and start the timer to get data refreshed
+            LoadData();
+            dataRefreshTimer.Enabled = true;
 
             entityGrid.GridCellLinkClicked += OnGridCellLinkClicked;
         }
-        
+
+        /// <summary>
+        /// Called when [data refresh timer tick] to reload/refresh the data in the entity grid.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="eventArgs">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void OnDataRefreshTimerTick(object sender, EventArgs eventArgs)
+        {
+            // Disable the timer until after the data has been refreshed to avoid events queuing
+            // up if the database is slow to respond
+            dataRefreshTimer.Enabled = false;
+            LoadData();
+
+            dataRefreshTimer.Enabled = true;
+        }
+
+
+        /// <summary>
+        /// Loads the (static) list of computers/services that need to be running into the gateway and use it 
+        /// to populate the contents of the grid
+        /// </summary>
+        private void LoadData()
+        {
+            List<ServiceStatusEntity> entities = ServiceStatusManager.GetComputersRequiringShipWorksService();
+            LocalCollectionEntityGateway<ServiceStatusEntity> requiredServicesGateway = new LocalCollectionEntityGateway<ServiceStatusEntity>(entities);
+
+            entityGrid.OpenGateway(requiredServicesGateway);
+        }
+
         /// <summary>
         /// The start link was clicked on the grid
         /// </summary>
@@ -112,22 +146,27 @@ namespace ShipWorks.ApplicationCore.Services.UI
         }
 
         /// <summary>
-        /// Show or hide the ui that lets the user know that the service is starting
+        /// Show or hide the UI that lets the user know that the service is starting.
         /// </summary>
         /// <param name="isComplete">Is the process just starting or is it finished?</param>
         private void UpdateStartingServiceUI(bool isComplete)
         {
-            // We need to get rid of the timer if the startup is complete
+            // Enable/disable the timer based on whether the process is complete; we don't want the 
+            // data being refreshed while the service is still starting due to the 30 second wait
+            // to avoid confusion (i.e. the service is started, the data is reloaded causing the 
+            // row to be removed, but the "starting service..." UI is still visible)
+            dataRefreshTimer.Enabled = isComplete;
+
             if (isComplete)
             {
+                // We need to get rid of the timer if the startup is complete
                 startingServiceTimer.Tick -= StartingServiceTimerOnTick;
                 startingServiceTimer.Dispose();
                 startingServiceTimer = null;
             }
 
             // Refresh the data source of the grid
-            requiredServicesGateway = new LocalCollectionEntityGateway<ServiceStatusEntity>(ServiceStatusManager.GetComputersRequiringShipWorksService());
-            entityGrid.OpenGateway(requiredServicesGateway);
+            LoadData();
 
             entityGrid.Enabled = isComplete;
             startingServicePanel.Visible = !isComplete;
@@ -152,6 +191,12 @@ namespace ShipWorks.ApplicationCore.Services.UI
             {
                 startingServiceTimer.Tick -= StartingServiceTimerOnTick;
                 startingServiceTimer.Dispose();
+            }
+
+            if (dataRefreshTimer != null)
+            {
+                dataRefreshTimer.Tick -= OnDataRefreshTimerTick;
+                dataRefreshTimer.Dispose();
             }
         }
     }
