0385e1dac59215f8994153a935616a944421741c
Michael Petter <m.petter@shipworks.com>
2013-09-04 14:47:09 -0500

    Add upgrade script to set tasks with no input to 'Nothing' for InputSource.

---
+++ Code/ShipWorks/Data/Administration/Scripts/Update/3.5/3.5.0.6.sql
@@ -0,0 +1,9 @@
+﻿
+-- set input source to 'Nothing' for existing tasks and queue steps not requiring input
+UPDATE ActionTask
+SET InputSource = -1
+WHERE TaskIdentifier IN ('PlaySound');
+
+UPDATE ActionQueueStep
+SET InputSource = -1
+WHERE TaskIdentifier IN ('PlaySound');
