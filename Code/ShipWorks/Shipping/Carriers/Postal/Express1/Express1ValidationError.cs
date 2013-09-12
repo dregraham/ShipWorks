8edf46c9644ea83108ed03a883c7dbcde925050a
John Yedlicka <j.yedlicka@shipworks.com>
2013-09-12 12:57:13 -0500

    Start to add plumbing for Express1/Stamps validation
    - Defined the payment validator interface
    
    - Implemented the validation error class

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Express1/Express1ValidationError.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ShipWorks.Shipping.Carriers.Postal.Express1
+{
+    /// <summary>
+    /// A class to encapsulate any Express1 specific validation errors that may be triggered when 
+    /// validating information collected from a user.
+    /// </summary>
+    public class Express1ValidationError
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Express1ValidationError"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public Express1ValidationError(string message)
+        {
+            Message = message;
+        }
+
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Express1/IPaymentValidator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Express1
+{
+    /// <summary>
+    /// An interface for being able to validate payment information.
+    /// </summary>
+    public interface IPaymentValidator
+    {
+        /// <summary>
+        /// Validates payment information.
+        /// </summary>
+        /// <returns>A collection of Express1ValidationError objects. An empty collection indicates
+        /// the payment information is valid; a non-empty collection indicates that the payment info
+        /// did not pass validation.</returns>
+        IEnumerable<Express1ValidationError> ValidatePaymentInfo();
+    }
+}
