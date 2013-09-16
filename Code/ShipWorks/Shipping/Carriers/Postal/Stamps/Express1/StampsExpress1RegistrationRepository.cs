3ef871c728c0428b79825cf7d04217fa86deb499
John Yedlicka <j.yedlicka@shipworks.com>
2013-09-16 14:08:29 -0500

    Implement the registration repository for an Express1/Stamps account

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Stamps/Express1/StampsExpress1RegistrationRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Interapptive.Shared.Business;
+using Interapptive.Shared.Utility;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.Postal.Express1;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Stamps.Express1
+{
+    /// <summary>
+    /// An implementation of the IExpress1RegistrationRepository interface. This will use the StampsAccountManager 
+    /// to save an Express1 registration to the Stamps account table.
+    /// </summary>
+    public class StampsExpress1RegistrationRepository : IExpress1RegistrationRepository
+    {
+        /// <summary>
+        /// Saves the Express1 registration to the appropriate data source (Stamps account table or Endicia account table).
+        /// </summary>
+        /// <param name="registration">The registration object containing the Express1 account info being saved.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public void Save(Express1Registration registration)
+        {
+            if (registration == null)
+            {
+                throw new ArgumentNullException("registration");
+            }
+
+            // Create a new stamps account entity that will get saved to the database
+            StampsAccountEntity stampsAccount = new StampsAccountEntity();
+
+            // Initialize the nulls to default values and denote that the account is for Express1
+            stampsAccount.InitializeNullsToDefault();
+            stampsAccount.IsExpress1 = true;
+
+            // Translate the registration data into a Stamps account entity
+            stampsAccount.Username = registration.UserName;
+            stampsAccount.Password = SecureText.Encrypt(registration.Password, registration.UserName);
+
+            stampsAccount.FirstName = registration.PhysicalAddress.FirstName;
+            stampsAccount.MiddleName = registration.PhysicalAddress.MiddleName;
+            stampsAccount.LastName = registration.PhysicalAddress.LastName;
+            stampsAccount.Company = registration.PhysicalAddress.Company;
+
+            // Address info
+            stampsAccount.Street1 = registration.PhysicalAddress.Address1;
+            stampsAccount.Street2 = registration.PhysicalAddress.Address2;
+            stampsAccount.Street3 = registration.PhysicalAddress.Address3;
+            stampsAccount.City = registration.PhysicalAddress.City;
+            stampsAccount.PostalCode = registration.PhysicalAddress.PostalCode;
+            stampsAccount.CountryCode = Geography.GetStateProvCode(registration.PhysicalAddress.Country);
+            stampsAccount.StateProvCode = Geography.GetStateProvCode(registration.PhysicalAddress.State);
+            stampsAccount.MailingPostalCode = registration.MailingAddress.PostalCode;
+
+            // Contact information (website is not collected required by Express1, so the registration is not
+            // collecting this information)
+            stampsAccount.Phone = registration.PhysicalAddress.PhoneNumber;
+            stampsAccount.Email = registration.Email;
+            stampsAccount.Website = string.Empty;
+            
+            // Persist the account entity to the database
+            StampsAccountManager.SaveAccount(stampsAccount);
+        }
+    }
+}
