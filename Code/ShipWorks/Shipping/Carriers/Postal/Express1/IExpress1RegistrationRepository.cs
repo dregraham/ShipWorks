7523f3c923f4f4420e57d652d2ea49ed9afaee1b
John Yedlicka <j.yedlicka@shipworks.com>
2013-09-16 12:18:56 -0500

    Add interface for Express1 registration repository

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Express1/IExpress1RegistrationRepository.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ShipWorks.Shipping.Carriers.Postal.Express1
+{
+    /// <summary>
+    /// An interface for saving an Express1 registration that abstracts the details of the underlying data source.
+    /// This allows for ShipWorks to have one "registration" class even though the account data may actually get
+    /// saved in a different table by injecting the appropriate implementation of this interface.
+    /// </summary>
+    public interface IExpress1RegistrationRepository
+    {
+        /// <summary>
+        /// Saves the Express1 registration to the appropriate data source (Stamps account table or Endicia account table).
+        /// </summary>
+        /// <param name="registration">The registration object containing the Express1 account info being saved.</param>
+        void Save(Express1Registration registration);
+    }
+}
