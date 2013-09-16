c3fe15b8e2cbcfa9481cf558104037b37b1c9b00
kcroke <k.croke@shipworks.com>
2013-09-16 11:16:14 -0500

    Define IRegistrationValidator Interface

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Express1/IRegistrationValidator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Express1
+{
+    public interface IRegistrationValidator
+    {
+        List<Express1ValidationError> Validate(Express1Registration registration);
+    }
+}
