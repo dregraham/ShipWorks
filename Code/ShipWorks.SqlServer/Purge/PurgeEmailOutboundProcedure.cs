3f0e29c16e281fd64490a6195e24a15bfe6479b3
kcroke <k.croke@shipworks.com>
2013-09-17 12:17:01 -0500

    Fix Email Purge: Add HtmlEmail replacement instead of updating order.

--- Code/ShipWorks.SqlServer/Purge/PurgeEmailOutboundProcedure.cs
+++ Code/ShipWorks.SqlServer/Purge/PurgeEmailOutboundProcedure.cs
@@ -55,6 +55,28 @@ public partial class StoredProcedures
                     SET @deletedEmailResourceID = SCOPE_IDENTITY();
                 END;
 
+                -- find or create the 'deleted' HTML resource record
+                DECLARE @deletedHTMLEmailResourceID BIGINT = (
+                    SELECT ResourceID FROM [Resource] WHERE [Filename] = '__EmailCleanupHTML.swr'
+                );
+
+                IF @deletedHTMLEmailResourceID IS NULL
+                BEGIN
+                    INSERT INTO [Resource] (
+                        [Data], [Checksum], [Compressed], [Filename]
+                    )
+                    VALUES (
+                        0x1F8B08000000000004004D91514FC2301485DF49F80F377DE1C531301115BA258480920824B0C4E85BC7EE68E3D6CEED0E9DC6FF6ECB82D2873EDC9EF3DD93532E29CF422E512421274519863BA98A1C35C1DA904AD55E90329AFBED63B7C37324017BA3C96A0246F849BE834C602F4559210535A5DE1D03495478F85EAB63C066ADDC8B9A02998354D4585A072E4E6C92E60A48C419C237A4D6E1A522575933864848938B493BACD4178E61382868023F7F04EEB7C86EE71C518B1C03F6305FCFB7D368B365FF9957BBC768F56411FDC1A07F7F6DAFE1E8E676C442EE9F8AB00417064EC4802D36EBC8DB2D5FE7E7ADA7C162BA5A3EBD9CB3596F51E2A5A315436F66EA5261096BFCE8B13092AA02CC85CA408A0A62440D455D1E3081B80157FDB329DF2A103A01ABD40632A30FD62E8ED6E3BAE973DF6EB2515D4497D87DE02F7352D21AC7010000,
+                        0xF7DECD3292BB70389B1D1C229331B568331883BCE2BCAE01F1DC4E448F2395DB,
+                        1,
+                        '__EmailCleanupHTML.swr'
+                    );
+
+                    SET @deletedHTMLEmailResourceID = SCOPE_IDENTITY();
+                END;
+
+
+
                 -- create batch purge ID table
                 CREATE TABLE #EmailPurgeBatch (
                     EmailOutboundID BIGINT PRIMARY KEY
@@ -76,7 +98,8 @@ public partial class StoredProcedures
                     WHERE
                         e.SentDate < @olderThan AND
                         e.SendStatus = 1 AND  -- only purge emails that have been sent
-                        o.ObjectID <> @deletedEmailResourceID;
+                        o.ObjectID <> @deletedEmailResourceID AND
+                        o.ObjectID <> @deletedHTMLEmailResourceID;
 
                     SET @batchSize = @@ROWCOUNT;
                     IF @batchSize = 0
@@ -102,18 +125,21 @@ public partial class StoredProcedures
                     FROM ObjectReference o
                     INNER JOIN EmailOutbound e ON
                         e.EmailOutboundID = o.ConsumerID AND
-                        e.PlainPartResourceID = o.ObjectReferenceID
+                        (e.PlainPartResourceID = o.ObjectReferenceID)
                     INNER JOIN #EmailPurgeBatch p ON
                         p.EmailOutboundID = e.EmailOutboundID;
 
-                    -- update EmailOutbound.HtmlPartResourceID to point to null
-                    UPDATE EmailOutbound
-                    SET HtmlPartResourceID = NULL
-                    FROM EmailOutbound e
+                    UPDATE ObjectReference
+                    SET
+                        ObjectID = @deletedHTMLEmailResourceID,
+                        ReferenceKey = '#' + convert(VARCHAR, @deletedHTMLEmailResourceID)
+                    FROM ObjectReference o
+                    INNER JOIN EmailOutbound e ON
+                        e.EmailOutboundID = o.ConsumerID AND
+                        isnull(e.HtmlPartResourceID,-15834) = o.ObjectReferenceID
                     INNER JOIN #EmailPurgeBatch p ON
                         p.EmailOutboundID = e.EmailOutboundID;
-
-                    
+                   
                     -- delete ObjectReferences not explicitly pointed to by EmailOutbound (embedded email images)
                     DELETE ObjectReference
                     FROM ObjectReference o
@@ -121,7 +147,8 @@ public partial class StoredProcedures
                         e.EmailOutboundID = o.ConsumerID
                     INNER JOIN #EmailPurgeBatch p ON
                         p.EmailOutboundID = e.EmailOutboundID
-                    WHERE o.ObjectID <> @deletedEmailResourceID;
+                    WHERE o.ObjectID <> @deletedEmailResourceID
+                        AND o.ObjectID <> @deletedHTMLEmailResourceID;
 
                     COMMIT TRANSACTION;
 
