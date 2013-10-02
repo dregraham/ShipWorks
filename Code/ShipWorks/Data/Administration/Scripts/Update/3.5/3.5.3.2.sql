ab30648c962f7dfab818b5c041d24bf66b2d9feb
John Yedlicka <j.yedlicka@shipworks.com>
2013-10-02 08:37:47 -0500

    Add SQL update script for scan form batch ID updates
    - 3.5.3.2 reflects the next script version according to the current state of master
    
    - Adding scan form batch ID to EndiciaShipment and StampsShipment tables
    
    - Back-filling the scan form batch ID for any existing data
    
    - Dropping the scan form ID column from EndiciaShipment and StampsShipment tables

---
+++ Code/ShipWorks/Data/Administration/Scripts/Update/3.5/3.5.3.2.sql
@@ -0,0 +1,39 @@
+﻿-- Add the new ScanFormBatchID column to the stamps and endicia shipment tables
+ALTER TABLE dbo.StampsShipment ADD
+	ScanFormBatchID bigint NULL
+GO
+ALTER TABLE dbo.StampsShipment SET (LOCK_ESCALATION = TABLE)
+GO
+
+ALTER TABLE dbo.EndiciaShipment ADD
+	ScanFormBatchID bigint NULL
+GO
+ALTER TABLE dbo.EndiciaShipment SET (LOCK_ESCALATION = TABLE)
+GO
+
+-- Back-fill the batch ID for any existing shipment/scan form data
+UPDATE stampsShipment
+SET stampsShipment.ScanFormBatchID = StampsScanForm.ScanFormBatchID
+FROM StampsShipment
+INNER JOIN StampsScanForm
+on stampsShipment.ScanFormID = StampsScanForm.StampsScanFormID
+
+UPDATE endiciaShipment
+SET endiciaShipment.ScanFormBatchID = EndiciaScanForm.ScanFormBatchID
+FROM endiciaShipment
+INNER JOIN EndiciaScanForm
+on endiciaShipment.ScanFormID = EndiciaScanForm.EndiciaScanFormID
+
+-- Now that the batch ID has been back-filled we can drop the scan
+-- form ID column on the endicia and stamps shipment tables
+ALTER TABLE dbo.EndiciaShipment
+	DROP COLUMN ScanFormID
+GO
+ALTER TABLE dbo.EndiciaShipment SET (LOCK_ESCALATION = TABLE)
+GO
+
+ALTER TABLE dbo.StampsShipment
+	DROP COLUMN ScanFormID
+GO
+ALTER TABLE dbo.StampsShipment SET (LOCK_ESCALATION = TABLE)
+GO
