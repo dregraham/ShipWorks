09a79c847a69d763a4889d5932d81342a585d87d
Kevin Croke <k.croke@shipworks.com>
2013-10-03 14:08:32 -0500

    EndiciaApiClient change.

--- Code/ShipWorks/Shipping/Carriers/Postal/Endicia/EndiciaApiClient.cs
+++ Code/ShipWorks/Shipping/Carriers/Postal/Endicia/EndiciaApiClient.cs
@@ -874,7 +874,9 @@ namespace ShipWorks.Shipping.Carriers.Postal.Endicia
                         // We know error code 55001 maps to cubic pricing not being supported, but it also could mask other messages such as 
                         // an authentication error message in the response; do some fuzzy logic in case there are other errors when 
                         // rates are not found for cubic packages
-                        if (response.Status == 55001 && response.ErrorMessage.ToUpperInvariant().Contains("CUBIC"))
+                        if (response.Status == 55001 &&
+                            response.ErrorMessage.ToUpperInvariant().Contains("CUBIC") &&
+                            !response.ErrorMessage.ToUpperInvariant().StartsWith("DIMENSIONS"))
                         {
                             errorMessage = "The selected Express1 account does not support cubic pricing. Please contact Express1 to apply.";
                         }
