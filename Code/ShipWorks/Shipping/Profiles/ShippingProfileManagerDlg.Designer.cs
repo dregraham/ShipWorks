c883d495913fa74d08dafdd6b7c10b87265d6dc1
Brian Nottingham <brian@shipworks.com>
2013-10-09 09:41:18 -0500

    Making profile window wide enough to show longer shipment type names.

--- Code/ShipWorks/Shipping/Profiles/ShippingProfileManagerDlg.Designer.cs
+++ Code/ShipWorks/Shipping/Profiles/ShippingProfileManagerDlg.Designer.cs
@@ -40,9 +40,9 @@
             // 
             // close
             // 
-            this.close.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.close.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.close.Location = new System.Drawing.Point(373, 241);
+            this.close.Location = new System.Drawing.Point(489, 242);
             this.close.Name = "close";
             this.close.Size = new System.Drawing.Size(86, 23);
             this.close.TabIndex = 4;
@@ -52,18 +52,18 @@
             // sandGrid
             // 
             this.sandGrid.AllowMultipleSelection = false;
-            this.sandGrid.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.sandGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.sandGrid.ColumnClickBehavior = Divelements.SandGrid.ColumnClickBehavior.None;
             this.sandGrid.Columns.AddRange(new Divelements.SandGrid.GridColumn[] {
             this.gridColumnName});
-            this.sandGrid.Location = new System.Drawing.Point(138, 12);
+            this.sandGrid.Location = new System.Drawing.Point(194, 12);
             this.sandGrid.Name = "sandGrid";
             this.sandGrid.NullRepresentation = "";
             this.sandGrid.Renderer = windowsXPRenderer1;
             this.sandGrid.ShowColumnHeaders = false;
-            this.sandGrid.Size = new System.Drawing.Size(229, 213);
+            this.sandGrid.Size = new System.Drawing.Size(289, 214);
             this.sandGrid.TabIndex = 0;
             this.sandGrid.SelectionChanged += new Divelements.SandGrid.SelectionChangedEventHandler(this.OnChangeSelectedProfile);
             this.sandGrid.RowActivated += new Divelements.SandGrid.GridRowEventHandler(this.OnActivate);
@@ -80,10 +80,10 @@
             // 
             // delete
             // 
-            this.delete.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.delete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.delete.Image = global::ShipWorks.Properties.Resources.delete16;
             this.delete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.delete.Location = new System.Drawing.Point(373, 71);
+            this.delete.Location = new System.Drawing.Point(489, 71);
             this.delete.Name = "delete";
             this.delete.Size = new System.Drawing.Size(86, 23);
             this.delete.TabIndex = 3;
@@ -93,10 +93,10 @@
             // 
             // edit
             // 
-            this.edit.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.edit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.edit.Image = global::ShipWorks.Properties.Resources.edit16;
             this.edit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.edit.Location = new System.Drawing.Point(373, 12);
+            this.edit.Location = new System.Drawing.Point(489, 12);
             this.edit.Name = "edit";
             this.edit.Size = new System.Drawing.Size(86, 23);
             this.edit.TabIndex = 1;
@@ -106,10 +106,10 @@
             // 
             // add
             // 
-            this.add.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.add.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.add.Image = global::ShipWorks.Properties.Resources.add16;
             this.add.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.add.Location = new System.Drawing.Point(373, 41);
+            this.add.Location = new System.Drawing.Point(489, 41);
             this.add.Name = "add";
             this.add.Size = new System.Drawing.Size(86, 23);
             this.add.TabIndex = 6;
@@ -119,15 +119,15 @@
             // 
             // menuList
             // 
-            this.menuList.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.menuList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.menuList.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
             this.menuList.FormattingEnabled = true;
             this.menuList.IntegralHeight = false;
             this.menuList.ItemHeight = 26;
             this.menuList.Location = new System.Drawing.Point(12, 12);
             this.menuList.Name = "menuList";
-            this.menuList.Size = new System.Drawing.Size(120, 213);
+            this.menuList.Size = new System.Drawing.Size(176, 214);
             this.menuList.TabIndex = 5;
             this.menuList.SelectedIndexChanged += new System.EventHandler(this.OnChangeShipmentType);
             // 
@@ -136,14 +136,14 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.close;
-            this.ClientSize = new System.Drawing.Size(471, 276);
+            this.ClientSize = new System.Drawing.Size(587, 277);
             this.Controls.Add(this.add);
             this.Controls.Add(this.menuList);
             this.Controls.Add(this.delete);
             this.Controls.Add(this.edit);
             this.Controls.Add(this.sandGrid);
             this.Controls.Add(this.close);
-            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(352, 312);
