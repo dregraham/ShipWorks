e2d242d63625a790d7a2cd928956d3508982601c
Tim Hughes <t.hughes@shipworks.com>
2013-10-11 11:16:38 -0500

    Added SmartPost

--- Code/ShipWorks.Tests.Integration.MSTest/IntegrationTests.cs
+++ Code/ShipWorks.Tests.Integration.MSTest/IntegrationTests.cs
@@ -67,21 +67,27 @@ namespace ShipWorks.Tests.Integration.MSTest
         [DataSource("DataSource_Ship_FedExSmartPost")]
         [DeploymentItem("DataSources\\FedExAll.xlsx")]
         [TestMethod]
-        [Ignore]
+        //[Ignore]
         public void Ship_FedExSmartPost()
         {
             FedExSmartPostFixture testObject = new FedExSmartPostFixture();
             try
             {
-                System.Diagnostics.Debug.WriteLine(this.TestContext.DataRow["TestID"].ToString());
-                PopulateTestObject(testObject, null);
-
-                testObject.Ship();
+                if (PopulateTestObject(testObject, FedExSmartPostFixture.SmartPostMapping))
+                {
+                    testObject.Ship();
+                }
             }
             catch (Exception ex)
             {
-                string msg = ex.Message;
-                throw;
+                // The test framework doesn't seem to know when to stop...so if we don't have a SaveLabel populated, return with no error. 
+                if (string.IsNullOrWhiteSpace(TestContext.DataRow[0].ToString().Trim()))
+                {
+                    return;
+                }
+
+                string msg = string.Format("CustomerTransactionID: {0}, Message: {1}", TestContext.DataRow["ProcessShipmentRequest#TransactionDetail"], ex.Message);
+                throw new Exception(msg, ex);
             }
         }
 
