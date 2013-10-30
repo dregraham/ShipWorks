48cd846f3baf8a7ba7176a7034e03eb5434d40d6
John Yedlicka <j.yedlicka@shipworks.com>
2013-10-30 12:52:50 -0500

    Refactor automatic Express1 account for Endicia to use the native Express1 controls
    - Implemented the Express1 for Endicia settings facade
    
    - Using AutomaticExpress1ControlBase on the EndiciaSettingsControl to setup
      automatically use Express1 to save on postage costs
    
    - Using the "native" Express1 dialog to activate the Express1 discount from the
      endicia promotion footnoote and removed unused using statements
    
    - Removed the EndiciaExpress1ActivateDiscountDlg, EndiciaAutomaticExpress1Control,
      and Express1EndiciaSetupWizard classes from the project since they were no
      longer referenced because the analagous "native" Express1 classes are now
      being used in their place.
    
    - Updated the EndiciaShipmentType to use the "native" Express1 footnote controls
    
    - Removed the Endicia-Express1 footnote classes from the project file since they
      were no longer referenced anywhere
    
    - Removed the EndiciaExpress1ActualSavingsDlg class that was referenced from the
      Endicia-Express1 footnote classes

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Endicia/Express1/EndiciaExpress1SettingsFacade.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Interapptive.Shared.Business;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.Postal.Express1;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Endicia.Express1
+{
+    /// <summary>
+    /// An implementation of the IExpress1SettingsFacade that defines the way for interacting
+    /// with the Express1 for Endicia settings.
+    /// </summary>
+    public class EndiciaExpress1SettingsFacade : IExpress1SettingsFacade
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EndiciaExpress1SettingsFacade"/> class.
+        /// </summary>
+        /// <param name="settings">The shipping settings being used as the data source for the facade.</param>
+        public EndiciaExpress1SettingsFacade(ShippingSettingsEntity settings)
+        {
+            if (settings == null)
+            {
+                throw new ArgumentNullException("settings");
+            }
+
+            UseExpress1 = settings.EndiciaAutomaticExpress1;
+            Express1Account = settings.EndiciaAutomaticExpress1Account;
+        }
+
+        /// <summary>
+        /// Gets and sets whether Express1 should be used if possible
+        /// </summary>
+        /// <exception cref="System.NotImplementedException">
+        /// </exception>
+        public bool UseExpress1 { get; set; }
+
+        /// <summary>
+        /// Gets and sets the ID of the Express1 account to use
+        /// </summary>
+        public long Express1Account { get; set; }
+
+        /// <summary>
+        /// Gets and sets the shipment type associated with this account type
+        /// </summary>
+        public ShipmentType ShipmentType
+        {
+            get { return new Express1EndiciaShipmentType(); }
+        }
+
+        /// <summary>
+        /// Gets a list of account descriptions and IDs
+        /// </summary>
+        public ICollection<KeyValuePair<string, long>> Express1Accounts
+        {
+            get
+            {
+                return EndiciaAccountManager.GetAccounts(EndiciaReseller.Express1)
+                           .Select(a => new KeyValuePair<string, long>(a.Description, a.EndiciaAccountID))
+                           .ToList();
+            }
+        }
+
+        /// <summary>
+        /// Save the Express1 settings into the actual settings entity
+        /// </summary>
+        /// <param name="settings">Settings entity into which the settings should be saved</param>
+        /// <exception cref="System.ArgumentNullException">settings</exception>
+        public void SaveSettings(ShippingSettingsEntity settings)
+        {
+            if (settings == null)
+            {
+                throw new ArgumentNullException("settings");
+            }
+
+            settings.EndiciaAutomaticExpress1 = UseExpress1;
+            settings.EndiciaAutomaticExpress1Account = Express1Account;
+        }
+
+        /// <summary>
+        /// Gets a person to use as the default for new Express1 accounts
+        /// </summary>
+        public PersonAdapter DefaultAccountPerson
+        {
+            get
+            {
+                List<EndiciaAccountEntity> accounts = EndiciaAccountManager.GetAccounts(EndiciaReseller.None);
+                return accounts.Count == 1 ? new PersonAdapter(accounts.Single(), "") : null;
+            }
+        }
+    }
+}
