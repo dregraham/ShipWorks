374ff066da2c85a6322369e4493b1479425c91e4
John Yedlicka <j.yedlicka@shipworks.com>
2013-11-04 13:11:51 -0600

    Implement the NullShippingBroker class
    - An application of the null object pattern
    
    - Implements the IBestRateShippingBroker interface and is intended to be used
      for those shipping providers where best rate is not applicable
      (i.e. BestRateShipmentType) and/or not yet supported.
    
    - Unit tests for NullShippingBroker

---
+++ Code/ShipWorks/Shipping/Carriers/BestRate/NullShippingBroker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Editing;
+
+namespace ShipWorks.Shipping.Carriers.BestRate
+{
+    /// <summary>
+    /// An implementation of the IBestRateShippingBroker interface that is an application of the 
+    /// null object pattern. This is intended to be used for those shipping providers where 
+    /// best rate is not applicable and/or not yet supported. 
+    /// </summary>
+    public class NullShippingBroker : IBestRateShippingBroker
+    {
+        /// <summary>
+        /// Gets the single best rate for each of the accounts of a specific shipping provider based
+        /// on the configuration of the best rate shipment data.
+        /// </summary>
+        /// <param name="shipment">The shipment.</param>
+        /// <returns>An empty list of RateResult objects.</returns>
+        public List<RateResult> GetBestRates(ShipmentEntity shipment)
+        {
+            return new List<RateResult>();
+        }
+    }
+}
