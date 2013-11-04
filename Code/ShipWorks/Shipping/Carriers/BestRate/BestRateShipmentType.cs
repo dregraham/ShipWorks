5a505b7fe90e698bdcba83468c42340291799c21
John Yedlicka <j.yedlicka@shipworks.com>
2013-11-04 08:09:39 -0600

    Implement LoadShipmentData and ConfigureNewShipment methods of BestRateShipmentType

--- Code/ShipWorks/Shipping/Carriers/BestRate/BestRateShipmentType.cs
+++ Code/ShipWorks/Shipping/Carriers/BestRate/BestRateShipmentType.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Shipping.Settings;
 
 namespace ShipWorks.Shipping.Carriers.BestRate
 {
     public class BestRateShipmentType : ShipmentType
     {
+        /// <summary>
+        /// The ShipmentTypeCode represented by this ShipmentType
+        /// </summary>
         public override ShipmentTypeCode ShipmentTypeCode
         {
             get { return ShipmentTypeCode.BestRate; }
@@ -18,22 +19,45 @@ namespace ShipWorks.Shipping.Carriers.BestRate
             throw new NotImplementedException();
         }
 
-        public override void LoadShipmentData(Data.Model.EntityClasses.ShipmentEntity shipment, bool refreshIfPresent)
+        /// <summary>
+        /// Apply the configured defaults and profile rule settings to the given shipment
+        /// </summary>
+        public override void ConfigureNewShipment(ShipmentEntity shipment)
+        {
+            base.ConfigureNewShipment(shipment);
+
+            if (shipment.BestRateShipment != null)
+            {
+                shipment.BestRateShipment.DimsAddWeight = false;
+                shipment.BestRateShipment.DimsHeight = 0;
+                shipment.BestRateShipment.DimsLength = 0;
+                shipment.BestRateShipment.DimsWeight = 0;
+                shipment.BestRateShipment.DimsWidth = 0;
+            }
+        }
+
+        /// <summary>
+        /// Ensures that the carrier specific data for the shipment, such as the FedEx data, are loaded for the shipment.  If the data
+        /// already exists, nothing is done: it is not refreshed.  This method can throw SqlForeignKeyException if the root shipment
+        /// or order has been deleted, ORMConcurrencyException if the shipment had been edited elsewhere, and ObjectDeletedException if the shipment
+        /// had been deleted.
+        /// </summary>
+        public override void LoadShipmentData(ShipmentEntity shipment, bool refreshIfPresent)
+        {
+            ShipmentTypeDataService.LoadShipmentData(this, shipment, shipment, "BestRate", typeof(UpsShipmentEntity), refreshIfPresent);
+        }
+
+        public override string GetServiceDescription(ShipmentEntity shipment)
         {
             throw new NotImplementedException();
         }
 
-        public override string GetServiceDescription(Data.Model.EntityClasses.ShipmentEntity shipment)
+        public override Insurance.InsuranceChoice GetParcelInsuranceChoice(ShipmentEntity shipment, int parcelIndex)
         {
             throw new NotImplementedException();
         }
 
-        public override Insurance.InsuranceChoice GetParcelInsuranceChoice(Data.Model.EntityClasses.ShipmentEntity shipment, int parcelIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void ProcessShipment(Data.Model.EntityClasses.ShipmentEntity shipment)
+        public override void ProcessShipment(ShipmentEntity shipment)
         {
             throw new NotImplementedException();
         }
