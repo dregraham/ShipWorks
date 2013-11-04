2d0a2c5f89c83fac1a01392e0651b8fc749afb43
Kevin Croke <k.croke@shipworks.com>
2013-11-04 16:58:29 -0600

    Created BestRateTransitTimeRestrictionControl.

---
+++ Code/ShipWorks/Shipping/Carriers/BestRate/BestRateTransitTimeRestrictionControl.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ShipWorks.Shipping.Carriers.BestRate
+{
+    partial class BestRateTransitTimeRestrictionControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.anyTime = new System.Windows.Forms.RadioButton();
+            this.expected = new System.Windows.Forms.RadioButton();
+            this.expectedNumberOfDays = new ShipWorks.UI.Controls.NumericTextBox();
+            this.SuspendLayout();
+            // 
+            // anyTime
+            // 
+            this.anyTime.AutoSize = true;
+            this.anyTime.Checked = true;
+            this.anyTime.Location = new System.Drawing.Point(4, 9);
+            this.anyTime.Name = "anyTime";
+            this.anyTime.Size = new System.Drawing.Size(69, 17);
+            this.anyTime.TabIndex = 1;
+            this.anyTime.TabStop = true;
+            this.anyTime.Text = "Any Time";
+            this.anyTime.UseVisualStyleBackColor = true;
+            // 
+            // expected
+            // 
+            this.expected.AutoSize = true;
+            this.expected.Location = new System.Drawing.Point(4, 32);
+            this.expected.Name = "expected";
+            this.expected.Size = new System.Drawing.Size(148, 17);
+            this.expected.TabIndex = 2;
+            this.expected.Text = "Expected number of days:";
+            this.expected.UseVisualStyleBackColor = true;
+            // 
+            // numericTextBox1
+            // 
+            this.expectedNumberOfDays.Location = new System.Drawing.Point(152, 32);
+            this.expectedNumberOfDays.Name = "numericTextBox1";
+            this.expectedNumberOfDays.Size = new System.Drawing.Size(32, 20);
+            this.expectedNumberOfDays.TabIndex = 3;
+            // 
+            // BestRateTransitTimeRestictionControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.expectedNumberOfDays);
+            this.Controls.Add(this.expected);
+            this.Controls.Add(this.anyTime);
+            this.Name = "BestRateTransitTimeRestrictionControl";
+            this.Size = new System.Drawing.Size(201, 77);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton anyTime;
+        private System.Windows.Forms.RadioButton expected;
+        private UI.Controls.NumericTextBox expectedNumberOfDays;
+    }
+}
---
+++ Code/ShipWorks/Shipping/Carriers/BestRate/BestRateTransitTimeRestrictionControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ShipWorks.Shipping.Carriers.BestRate.Enums;
+using ShipWorks.UI.Controls;
+
+namespace ShipWorks.Shipping.Carriers.BestRate
+{
+    public partial class BestRateTransitTimeRestrictionControl : UserControl
+    {
+
+        /// <summary>
+        /// Gets or sets the expected number of days.
+        /// </summary>
+        public int ExpectedNumberOfDays
+        {
+            get
+            {
+                return expectedNumberOfDays.NumericValue.HasValue ? expectedNumberOfDays.NumericValue.Value : 0;
+            }
+            set
+            {
+                expectedNumberOfDays.Text = value.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the type of the transit time.
+        /// </summary>
+        public TransitTimeType TransitTimeType
+        {
+            get 
+            {
+                return anyTime.Checked ? TransitTimeType.Any : TransitTimeType.Expected;
+            }
+            set
+            {
+                anyTime.Checked = (value == TransitTimeType.Any);
+                expected.Checked = (value == TransitTimeType.Expected);
+            }
+        }
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BestRateTransitTimeRestrictionControl"/> class.
+        /// </summary>
+        public BestRateTransitTimeRestrictionControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
