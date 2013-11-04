997ae8647593e10b767a24ef249106c2b4ba22db
Kevin Croke <k.croke@shipworks.com>
2013-11-04 15:13:36 -0600

    Added TransitTimeType enumeration.

---
+++ Code/ShipWorks/Shipping/Carriers/BestRate/Enums/TransitTimeType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ShipWorks.Shipping.Carriers.BestRate.Enums
+{
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum TransitTimeType
+    {
+        [Description("Any")] 
+        Any = 0,
+
+        [Description("Expected")] 
+        Expected = 1
+    }
+}
