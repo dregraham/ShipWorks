2c743dfc8549ab0d4187cee29163ed8e0f2e4a96
John Yedlicka <j.yedlicka@shipworks.com>
2013-11-04 14:03:59 -0600

    Implement the IBestRateShippingBrokerFactory interface
    - This is the "live" implementation of the interface that will create the list
      of shipping brokers for all of the shipment types that have been activated and
      configured in ShipWorks

---
+++ Code/ShipWorks/Shipping/Carriers/BestRate/BestRateShippingBrokerFactory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Settings;
+
+namespace ShipWorks.Shipping.Carriers.BestRate
+{
+    /// <summary>
+    /// An implementation of the IBestRateShippingBrokerFactory interface that creates brokers for the
+    /// shipping providers that have been activated and configured in ShipWorks.
+    /// </summary>
+    public class BestRateShippingBrokerFactory : IBestRateShippingBrokerFactory
+    {
+        /// <summary>
+        /// Creates all of the best rate shipping brokers available in the system for the shipping
+        /// providers that are activated and configured.
+        /// </summary>
+        /// <returns>The shipping broker for all activated and configured shipment types.</returns>
+        public IEnumerable<IBestRateShippingBroker> CreateBrokers()
+        {
+            List<IBestRateShippingBroker> brokers = new List<IBestRateShippingBroker>();
+            ShippingSettingsEntity shippingSettings = ShippingSettings.Fetch();
+            
+            // Add a broker for every shipment type that has been activated and configured
+            foreach (ShipmentType shipmentType in ShipmentTypeManager.ShipmentTypes)
+            {
+                int shipmentTypeCodeValue = (int)shipmentType.ShipmentTypeCode;
+
+                if (shippingSettings.ActivatedTypes.Contains(shipmentTypeCodeValue) && shippingSettings.ConfiguredTypes.Contains(shipmentTypeCodeValue))
+                {
+                    // This shipment type is activated and configured, so add it to our list of brokers
+                    brokers.Add(shipmentType.GetShippingBroker());
+                }
+            }
+
+            return brokers;
+        }
+    }
+}
