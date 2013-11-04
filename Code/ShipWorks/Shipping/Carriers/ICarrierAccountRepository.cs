d8da65d16df9991a37c1f0dfe5372aff37daab9d
Tim Hughes <t.hughes@shipworks.com>
2013-11-04 09:54:51 -0600

    Added ICarrierAccountRepository and UpsAccountRepository

---
+++ Code/ShipWorks/Shipping/Carriers/ICarrierAccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+
+namespace ShipWorks.Shipping.Carriers
+{
+    public interface ICarrierAccountRepository
+    {
+        /// <summary>
+        /// Returns a list of accounts for the carrier.
+        /// </summary>
+        IEnumerable<IEntity2> Accounts { get; }
+
+        /// <summary>
+        /// Returns a carrier account for the provided accountID.
+        /// </summary>
+        /// <param name="accountID">The account ID for which to return an account.</param>
+        /// <returns>The matching account as IEntity2.</returns>
+        IEntity2 GetAccount(long accountID);
+    }
+}
---
+++ Code/ShipWorks/Shipping/Carriers/UPS/BestRate/UpsAccountRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.BestRate;
+
+namespace ShipWorks.Shipping.Carriers.UPS.BestRate
+{
+    public class UpsAccountRepository : ICarrierAccountRepository
+    {
+        /// <summary>
+        /// Returns a list of accounts for the carrier.
+        /// </summary>
+        public IEnumerable<IEntity2> Accounts
+        {
+            get
+            {
+                return UpsAccountManager.Accounts;
+            }
+        }
+
+        /// <summary>
+        /// Returns a carrier account for the provided accountID.
+        /// </summary>
+        /// <param name="accountID">The account ID for which to return an account.</param>
+        /// <returns>The matching account as IEntity2.</returns>
+        public IEntity2 GetAccount(long accountID)
+        {
+            return UpsAccountManager.GetAccount(accountID);
+        }
+    }
+}
