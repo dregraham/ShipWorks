86dc2c28ee3f84c7c98e81e4023ba578eabfceb3
Kevin Croke <k.croke@shipworks.com>
2013-11-07 14:11:48 -0600

    Add Best Rate Profile to Upgrade script

--- Code/ShipWorks/Data/Administration/Scripts/Update/4.0/4.0.0.0.sql
+++ Code/ShipWorks/Data/Administration/Scripts/Update/4.0/4.0.0.0.sql
@@ -31,9 +31,30 @@ GO
 ALTER TABLE [dbo].[BestRateShipment] ADD CONSTRAINT [PK_BestRateShipment] PRIMARY KEY CLUSTERED  ([ShipmentID])
 GO
 
+CREATE TABLE [dbo].[BestRateProfile]
+(
+[ShippingProfileID] [bigint] NOT NULL,
+[DimsProfileID] [bigint] NULL,
+[DimsLength] [float] NULL,
+[DimsWidth] [float] NULL,
+[DimsHeight] [float] NULL,
+[DimsWeight] [float] NULL,
+[DimsAddWeight] [bit] NULL,
+[Weight] [float] NULL,
+[ServiceLevel] [int] NULL
+)
+GO
+PRINT N'Creating primary key [PK_BestRateProfile] on [dbo].[BestRateProfile]'
+GO
+ALTER TABLE [dbo].[BestRateProfile] ADD CONSTRAINT [PK_BestRateProfile] PRIMARY KEY CLUSTERED  ([ShippingProfileID])
+GO
 -- Create default best rate profile
 INSERT INTO [dbo].[ShippingProfile] ([Name], [ShipmentType], [ShipmentTypePrimary], [OriginID], [Insurance], [InsuranceInitialValueSource], [InsuranceInitialValueAmount], [ReturnShipment])
 VALUES ('Defaults - Best rate', 14, 1, 0, 0, 0, 0.00, 0)
+GO
+PRINT N'Adding foreign keys to [dbo].[BestRateProfile]'
+GO
+ALTER TABLE [dbo].[BestRateProfile] ADD CONSTRAINT [FK_BestRateProfile_ShippingProfile] FOREIGN KEY ([ShippingProfileID]) REFERENCES [dbo].[ShippingProfile] ([ShippingProfileID]) ON DELETE CASCADE
 GO
 
 INSERT INTO [dbo].[BestRateProfile] ([ShippingProfileID], [DimsProfileID], [DimsLength], [DimsWidth], [DimsHeight], [DimsWeight], [DimsAddWeight], [Weight], [ServiceLevel])
