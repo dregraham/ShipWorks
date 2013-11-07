3a7b56aaebf1ecd518a9cd59f6746a42d767da37
Andrew Benz <a.benz@shipworks.com>
2013-11-07 13:28:52 -0600

    Created the NonCompetitiveRateResult to potentially mask rate service names

---
+++ Code/ShipWorks.Tests/Shipping/Editing/NonCompetitiveRateResultTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShipWorks.Shipping.Editing;
+using ShipWorks.Shipping.Editing.Enums;
+
+namespace ShipWorks.Tests.Shipping.Editing
+{
+    [TestClass]
+    public class NonCompetitiveRateResultTest
+    {
+        [TestMethod]
+        public void Constructor_CopiesAllProperties()
+        {
+            RateResult originalRate = new RateResult("Test", "3", 12.1m, "Foo") {ServiceLevel = ServiceLevelType.ThreeDays};
+            NonCompetitiveRateResult testObject = new NonCompetitiveRateResult(originalRate);
+
+            Assert.AreEqual("Test", testObject.Description);
+            Assert.AreEqual("3", testObject.Days);
+            Assert.AreEqual(12.1m, testObject.Amount);
+            Assert.AreEqual("Foo", testObject.Tag);
+            Assert.AreEqual(ServiceLevelType.ThreeDays, testObject.ServiceLevel);
+        }
+
+        [TestMethod]
+        public void MaskDescription_ShowsDescription_WhenAllResultsAreNonCompetitive()
+        {
+            NonCompetitiveRateResult testObject = new NonCompetitiveRateResult(new RateResult("Bar", "5"));
+            testObject.MaskDescription(new List<RateResult>{testObject, new NonCompetitiveRateResult(new RateResult("Foo", "3"))});
+
+            Assert.AreEqual("Bar", testObject.Description);
+        }
+
+        [TestMethod]
+        public void MaskDescription_MasksDescription_WhenOneResultIsNotNonCompetitive()
+        {
+            NonCompetitiveRateResult testObject = new NonCompetitiveRateResult(new RateResult("Bar", "5") { ServiceLevel = ServiceLevelType.ThreeDays});
+            testObject.MaskDescription(new List<RateResult> { testObject, new RateResult("Foo", "3") });
+
+            Assert.AreEqual("Undisclosed Three Days", testObject.Description);
+        }
+
+        [TestMethod]
+        public void MaskDescription_MasksDescription_WhenNoResultsAreNonCompetitive()
+        {
+            NonCompetitiveRateResult testObject = new NonCompetitiveRateResult(new RateResult("Bar", "5") { ServiceLevel = ServiceLevelType.ThreeDays });
+            testObject.MaskDescription(new List<RateResult> { new RateResult("Baz", "7"), new RateResult("Foo", "3") });
+
+            Assert.AreEqual("Undisclosed Three Days", testObject.Description);
+        }
+    }
+}
---
+++ Code/ShipWorks/Shipping/Editing/NonCompetitiveRateResult.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Interapptive.Shared.Utility;
+
+namespace ShipWorks.Shipping.Editing
+{
+    /// <summary>
+    /// Defines a rate result that has non-compete rules for displaying service type around other carriers
+    /// </summary>
+    public class NonCompetitiveRateResult : RateResult
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="originalRate">RateResult from which to create this result</param>
+        public NonCompetitiveRateResult(RateResult originalRate) :
+            base(originalRate.Description, originalRate.Days, originalRate.Amount, originalRate.Tag)
+        {
+            AmountFootnote = originalRate.AmountFootnote;
+            ServiceLevel = originalRate.ServiceLevel;
+        }
+
+        /// <summary>
+        /// Mask the description of the rate, if necessary
+        /// </summary>
+        /// <param name="rates">Collection of all rates, including this one</param>
+        public override void MaskDescription(System.Collections.Generic.IEnumerable<RateResult> rates)
+        {
+            if (!rates.All(x => x is NonCompetitiveRateResult))
+            {
+                Description = "Undisclosed " + EnumHelper.GetDescription(ServiceLevel);
+            }
+        }
+    }
+}
