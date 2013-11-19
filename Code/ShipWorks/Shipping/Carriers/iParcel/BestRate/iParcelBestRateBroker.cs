a36fa2f4c97b13c4f5e28c032ee21243060c25f3
Andrew Benz <a.benz@shipworks.com>
2013-11-19 11:01:27 -0600

    Refactor iParcelBestRateBroker to use new base class

--- Code/ShipWorks/Shipping/Carriers/iParcel/BestRate/iParcelBestRateBroker.cs
+++ Code/ShipWorks/Shipping/Carriers/iParcel/BestRate/iParcelBestRateBroker.cs
@@ -1,205 +1,114 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Interapptive.Shared.Business;
-using ShipWorks.Data;
+﻿using System.Collections.Generic;
+using SD.LLBLGen.Pro.ORMSupportClasses;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Shipping.Carriers.BestRate;
 using ShipWorks.Shipping.Carriers.iParcel.Enums;
-using ShipWorks.Shipping.Editing;
-using ShipWorks.Shipping.Settings.Origin;
 
 namespace ShipWorks.Shipping.Carriers.iParcel.BestRate
 {
-    class iParcelBestRateBroker : IBestRateShippingBroker
+    class iParcelBestRateBroker : PackageBasedBestRateBroker<IParcelAccountEntity, IParcelPackageEntity>
     {
-        private readonly iParcelShipmentType shipmentType;
-        private readonly ICarrierAccountRepository<IParcelAccountEntity> accountRepository;
-
         /// <summary>
-        /// Initializes a new instance of the <see cref="iParcelBestRateBroker"/> class.
+        /// Creates a broker with the default shipment type and account repository
         /// </summary>
-        public iParcelBestRateBroker() : this(new iParcelShipmentType(), new iParcelAccountRepository())
-        {}
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="shipmentType">Type of the shipment.</param>
-        /// <param name="accountRepository">The account repository.</param>
-        private iParcelBestRateBroker(iParcelShipmentType shipmentType, ICarrierAccountRepository<IParcelAccountEntity> accountRepository)
+        /// <remarks>This is designed to be used within ShipWorks</remarks>
+        public iParcelBestRateBroker()
+            : this(new iParcelShipmentType(), new iParcelAccountRepository())
         {
-            this.shipmentType = shipmentType;
-            this.accountRepository = accountRepository;
+
         }
 
         /// <summary>
-        /// Gets a value indicating whether there any accounts available to a broker.
+        /// Creates a broker with the specified shipment type and account repository
         /// </summary>
-        /// <value>
-        /// <c>true</c> if the broker [has accounts]; otherwise, <c>false</c>.
-        /// </value>
-        public bool HasAccounts
+        /// <param name="shipmentType">Instance of a IParcel shipment type that will be used to get rates</param>
+        /// <param name="accountRepository">Instance of an account repository that will get IParcel accounts</param>
+        /// <remarks>This is designed to be used by tests</remarks>
+        public iParcelBestRateBroker(ShipmentType shipmentType, ICarrierAccountRepository<IParcelAccountEntity> accountRepository) :
+            base(shipmentType, accountRepository, "iParcel")
         {
-            get { return accountRepository.Accounts.Any(); }
+
         }
 
         /// <summary>
-        /// Gets the rates for each of the accounts of a specific shipping provider based
-        /// on the configuration of the best rate shipment data.
+        /// Applies IParcel specific data to the specified shipment
         /// </summary>
-        /// <param name="shipment">The shipment.</param>
-        /// <param name="exceptionHandler"></param>
-        /// <returns>
-        /// A list of RateResults for each account of a specific shipping provider (i.e. if two accounts 
-        /// are registered for a single provider, the list of rates would have two entries if both 
-        /// accounts returned rates).
-        /// </returns>
-        public List<RateResult> GetBestRates(ShipmentEntity shipment, Action<ShippingException> exceptionHandler)
+        /// <param name="currentShipment">Shipment that will be modified</param>
+        /// <param name="originalShipment">Shipment that contains original data for copying</param>
+        /// <param name="account">Account that will be attached to the shipment</param>
+        protected override void UpdateChildShipmentSettings(ShipmentEntity currentShipment, ShipmentEntity originalShipment, IParcelAccountEntity account)
         {
-            if (shipment == null)
-            {
-                throw new ArgumentNullException("shipment");
-            }
+            base.UpdateChildShipmentSettings(currentShipment, originalShipment, account);
 
-            List<RateResult> allRates = new List<RateResult>();
-
-            List<IParcelAccountEntity> accounts = accountRepository.Accounts.ToList();
-
-            Dictionary<RateResult, IParcelShipmentEntity> rateShipments = new Dictionary<RateResult, IParcelShipmentEntity>();
-
-            // Create a clone so we don't have to worry about modifying the original shipment
-            ShipmentEntity testRateShipment = EntityUtility.CloneEntity(shipment);
-            testRateShipment.ShipmentType = (int)ShipmentTypeCode.iParcel;
-
-            foreach (IParcelAccountEntity account in accounts)
-            {
-                testRateShipment.IParcel = new IParcelShipmentEntity();
-
-                shipmentType.ConfigureNewShipment(testRateShipment);
-                UpdateShipmentSettings(testRateShipment, shipment, account);
-
-                try
-                {
-                    IEnumerable<RateResult> results = shipmentType.GetRates(testRateShipment).Rates
-                                                                  .Where(r => r.Tag != null)
-                                                                  .Where(r => r.Amount > 0);
-
-                    // Save a mapping between the rate and the shipment used to get the rate
-                    foreach (RateResult result in results)
-                    {
-                        rateShipments.Add(result, testRateShipment.IParcel);
-                    }
-
-                    allRates.AddRange(results);
-                }
-                catch (ShippingException ex)
-                {
-                    // Offload exception handling to the passed in exception handler
-                    exceptionHandler(ex);
-                }
-            }
-
-            // Return all the rates, then group by iParcelServiceType and ServiceLevel
-            List<RateResult> filteredRates = allRates
-                .GroupBy(r => ((iParcelRateSelection)r.Tag).ServiceType)
-                .SelectMany(RateResultsByServiceLevel)
-                .ToList();
-
-            foreach (RateResult rate in filteredRates)
-            {
-                // Replace the service type with a function that will select the correct shipment type
-                rate.Tag = CreateRateSelectionFunction(rateShipments[rate], rate.Tag);
-                rate.Description = rate.Description.Contains("iParcel") ? rate.Description : "iParcel " + rate.Description;
-            }
-
-            return filteredRates.ToList();
-        }
-
-        /// <summary>
-        /// Gets a list of rates by iParcelServiceType
-        /// </summary>
-        /// <param name="typeGroup">Group </param>
-        /// <returns></returns>
-        private static IEnumerable<RateResult> RateResultsByServiceLevel(IGrouping<iParcelServiceType, RateResult> typeGroup)
-        {
-            return typeGroup
-                .GroupBy(r => r.ServiceLevel)
-                .Select(serviceLevelRate => serviceLevelRate.OrderBy(rateToOrder => rateToOrder.Amount)
-                    .FirstOrDefault());
-        }
-
-        /// <summary>
-        /// Creates a function that can be used to select a specific rate
-        /// </summary>
-        /// <param name="rateShipment">iParcelShipment that was used to get the rate</param>
-        /// <param name="originalTag">iParcelServiceType associated with the specific rate</param>
-        /// <returns>A function that, when executed, will convert the passed in shipment to a iParcel shipment
-        /// used to create the rate.</returns>
-        private static Action<ShipmentEntity> CreateRateSelectionFunction(IParcelShipmentEntity rateShipment, object originalTag)
-        {
-            return selectedShipment =>
-            {
-                rateShipment.Service = (int)((iParcelRateSelection)originalTag).ServiceType;
-                selectedShipment.ShipmentType = (int)ShipmentTypeCode.iParcel;
-                ShippingManager.EnsureShipmentLoaded(selectedShipment);
-
-                if (selectedShipment.IParcel == null)
-                {
-                    selectedShipment.IParcel = rateShipment;
-                }
-                else
-                {
-                    // Grab the original iParcel package so we can get it's iParcelPackageID, as we'll need to set it on the
-                    // cloned package.  There's probably a better way, so need to check with Brian.
-                    IParcelPackageEntity selectedPackageEntity = selectedShipment.IParcel.Packages[0];
-                    long originalPackageID = selectedPackageEntity.IParcelPackageID;
-
-                    // Set the rated shipment as the iParcel shipment
-                    selectedShipment.IParcel = rateShipment;
-
-                    // Update the first package iParcelPackgeID to be that of the original persisted package.  If this isn't 
-                    // done, we get an ORM exception.  There's probably a better way, so need to check with Brian.
-                    selectedShipment.IParcel.Packages[0].IParcelPackageID = originalPackageID;
-
-                    // Set the shipment and package to be not new so a copy isn't persisted.
-                    selectedShipment.IParcel.Packages[0].IsNew = false;
-                    selectedShipment.IParcel.IsNew = false;
-
-                }
-            };
-        }
-
-        /// <summary>
-        /// Updates the shipment settings.
-        /// </summary>
-        /// <param name="testRateShipment">The test rate shipment.</param>
-        /// <param name="originalShipment">The original shipment.</param>
-        /// <param name="account">The account.</param>
-        private void UpdateShipmentSettings(ShipmentEntity testRateShipment, ShipmentEntity originalShipment, IParcelAccountEntity account)
-        {
-            testRateShipment.OriginOriginID = originalShipment.OriginOriginID;
-
-            // Set the address of the shipment to either the UPS account, or the address of the original shipment
-            if (testRateShipment.OriginOriginID == (int)ShipmentOriginSource.Account)
-            {
-                PersonAdapter.Copy(account, "", testRateShipment, "Origin");
-            }
-            else
-            {
-                PersonAdapter.Copy(originalShipment, testRateShipment, "Origin");
-            }
-
-            testRateShipment.IParcel.Packages[0].DimsHeight = testRateShipment.BestRate.DimsHeight;
-            testRateShipment.IParcel.Packages[0].DimsWidth = testRateShipment.BestRate.DimsWidth;
-            testRateShipment.IParcel.Packages[0].DimsLength = testRateShipment.BestRate.DimsLength;
+            currentShipment.IParcel.Packages[0].DimsHeight = originalShipment.BestRate.DimsHeight;
+            currentShipment.IParcel.Packages[0].DimsWidth = originalShipment.BestRate.DimsWidth;
+            currentShipment.IParcel.Packages[0].DimsLength = originalShipment.BestRate.DimsLength;
 
             // ConfigureNewShipment sets these fields, but we need to make sure they're what we expect
-            testRateShipment.IParcel.Packages[0].Weight = originalShipment.ContentWeight;
-            testRateShipment.IParcel.Packages[0].DimsAddWeight = false;
-            testRateShipment.IParcel.Service = (int)iParcelServiceType.Saver;
-            testRateShipment.IParcel.IParcelAccountID = account.IParcelAccountID;
+            currentShipment.IParcel.Packages[0].Weight = originalShipment.ContentWeight;
+            currentShipment.IParcel.Packages[0].DimsAddWeight = false;
+            currentShipment.IParcel.Service = (int)iParcelServiceType.Saver;
+            currentShipment.IParcel.IParcelAccountID = account.IParcelAccountID;
+        }
+
+        /// <summary>
+        /// Checks whether the service type specified in the rate should be excluded from best rate consideration
+        /// </summary>
+        /// <param name="tag">IParcel service type from the rate tag</param>
+        /// <returns></returns>
+        protected override bool IsExcludedServiceType(object tag)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Creates and attaches a new instance of a IParcelShipment to the specified shipment
+        /// </summary>
+        protected override void CreateShipmentChild(ShipmentEntity shipment)
+        {
+            shipment.IParcel = new IParcelShipmentEntity();
+        }
+
+        /// <summary>
+        /// Sets the service type on the IParcel shipment from the value in the rate tag
+        /// </summary>
+        /// <param name="shipment">Shipment that will be updated</param>
+        /// <param name="tag">Rate tag that represents the service type</param>
+        protected override void SetServiceTypeFromTag(ShipmentEntity shipment, object tag)
+        {
+            shipment.IParcel.Service = (int) tag;
+        }
+
+        /// <summary>
+        /// Gets the current entity state for the specified shipment's child
+        /// </summary>
+        protected override EntityState ChildShipmentEntityState(ShipmentEntity shipment)
+        {
+            return shipment.IParcel.Fields.State;
+        }
+
+        /// <summary>
+        /// Gets a collection of packages from the specified shipment
+        /// </summary>
+        protected override IList<IParcelPackageEntity> Packages(ShipmentEntity shipment)
+        {
+            return shipment.IParcel.Packages;
+        }
+
+        /// <summary>
+        /// Gets the id for the specified package
+        /// </summary>
+        protected override long PackageId(IParcelPackageEntity package)
+        {
+            return package.IParcelPackageID;
+        }
+
+        /// <summary>
+        /// Sets the id on the specified package
+        /// </summary>
+        protected override void SetPackageId(IParcelPackageEntity package, long packageId)
+        {
+            package.IParcelPackageID = packageId;
         }
     }
 }
