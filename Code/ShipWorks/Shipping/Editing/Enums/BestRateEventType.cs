bdbaef535224b4694540b4e812dbe5dedce26a79
unknown <XSarco@PETTER-PC.interapptive.local>
2013-11-22 08:18:02 -0600

    Create BestRateEventType flags enum

---
+++ Code/ShipWorks/Shipping/Editing/Enums/BestRateEventType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ShipWorks.Shipping.Editing.Enums
+{
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    [Flags]
+    public enum BestRateEventType
+    {
+        [Description("None")]
+        None = 0,
+
+        [Description("Rates Compared")]
+        RatesCompared = 1,
+
+        [Description("Rate Selected")]
+        RateSelected = 2,
+
+        [Description("Rate Auto Selected and Processed")]
+        RateAutoSelectedAndProcessed  = 4
+    }
+}
