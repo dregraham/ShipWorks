5f6c25c1d15d0044c6e5651b2aee68d862c0a1db
Kevin Croke <k.croke@shipworks.com>
2013-12-03 12:56:13 -0600

    Fix issue where best rate filter is not updating when processing with bestrate.

--- Code/ShipWorks/Shipping/Carriers/BestRate/CoreExtensions/Filters/BestRateProcessedCondition.cs
+++ Code/ShipWorks/Shipping/Carriers/BestRate/CoreExtensions/Filters/BestRateProcessedCondition.cs
@@ -1,50 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using ShipWorks.Data.Model.HelperClasses;
+﻿using ShipWorks.Data.Model.HelperClasses;
 using ShipWorks.Filters.Content;
 using ShipWorks.Filters.Content.Conditions;
 using ShipWorks.Filters.Content.SqlGeneration;
-using ShipWorks.Shipping.Editing.Enums;
 
 namespace ShipWorks.Shipping.Carriers.BestRate.CoreExtensions.Filters
 {
-
     /// <summary>
-    /// Filter for BestRateProcessedCondition
+    ///     Filter for BestRateProcessedCondition
     /// </summary>
     [ConditionElement("Best rate used", "Shipment.BestRateProcessed")]
-    class BestRateProcessedCondition : BooleanCondition
+    internal class BestRateProcessedCondition : BooleanCondition
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="BestRateProcessedCondition"/> class.
+        ///     Initializes a new instance of the <see cref="BestRateProcessedCondition" /> class.
         /// </summary>
         public BestRateProcessedCondition()
             : base("Yes", "No")
-        { }
+        {}
 
         /// <summary>
-        /// Generate the SQL for the condition clement
+        ///     Generate the SQL for the condition clement
         /// </summary>
-        /// <param name="context"></param>
-        /// <returns></returns>
         public override string GenerateSql(SqlGenerationContext context)
         {
-
-            return string.Format("BestRateEvents & {0} {1} {0}", (int)BestRateEventTypes.RateAutoSelectedAndProcessed,
+            return string.Format("{0} & {1} {2} {1}",
+                                 context.GetColumnReference(ShipmentFields.BestRateEvents),
+                                 (int)BestRateEventTypes.RateAutoSelectedAndProcessed,
                                  GetSqlOperator());
         }
 
         /// <summary>
-        /// Gets the SQL operator.
+        ///     Gets the SQL operator.
         /// </summary>
         protected override string GetSqlOperator()
         {
             // When user selects Equals, True and False should be = and != respectively.
             // When user selects not equals, that is flip-flopped.
             return Operator == EqualityOperator.Equals ?
-                (Value ? "=" : "!=") : (Value ? "!=" : "=");
+                       (Value ? "=" : "!=") : (Value ? "!=" : "=");
         }
     }
 }
