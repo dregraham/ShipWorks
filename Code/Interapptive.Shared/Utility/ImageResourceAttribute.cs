dregraham/ShipWorks

commit 37e5b3fe15863f382b1c1a3312b7f6a83c91308a

Detect if a unit test is being ran in mstest or visual studio before attempting to access assembly resources.

Code/Interapptive.Shared/Utility/ImageResourceAttribute.cs
@@ -14,8 +14,8 @@ namespace Interapptive.Shared.Utility
     [AttributeUsage(AttributeTargets.All)]
     public class ImageResourceAttribute : Attribute
     {
-        string resourceKey;
-        string resourceSet;
+        private string resourceKey;
+        private string resourceSet;
 
         /// <summary>
         /// Constructor
@@ -38,14 +38,8 @@ namespace Interapptive.Shared.Utility
         /// </summary>
         public string ResourceSet
         {
-            get
-            {
-                return resourceSet;
-            }
-            set
-            {
-                resourceSet = value;
-            }
+            get { return resourceSet; }
+            set { resourceSet = value; }
         }
 
         /// <summary>
@@ -55,10 +49,11 @@ namespace Interapptive.Shared.Utility
         {
             get
             {
+
                 Assembly primaryAssembly = Assembly.GetEntryAssembly();
-                
+
                 // The only reason this will be null is if we are in a unit test.
-                if (primaryAssembly == null)
+                if (primaryAssembly == null || primaryAssembly.FullName.ToLower().Contains("mstest"))
                 {
                     return new Bitmap(1, 1);
                 }
@@ -73,7 +68,7 @@ namespace Interapptive.Shared.Utility
                     throw new NotFoundException(string.Format("The resource '{0}' could not be found in '{1}'.", resourceKey, primaryResources));
                 }
 
-                return (Image) result;
+                return (Image)result;
             }
         }
     }

commit 8ac6442f23af4b90d2d88e6f07fc8b761b4eea69

Fix code analysis problem introduced in commit 37e5b3fe15863f382b1c1a3312b7f6a83c91308a

Code/Interapptive.Shared/Utility/ImageResourceAttribute.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Drawing;
 using System.Resources;
 using System.Reflection;
+using System.Globalization;
 
 namespace Interapptive.Shared.Utility
 {
@@ -53,7 +54,7 @@ namespace Interapptive.Shared.Utility
                 Assembly primaryAssembly = Assembly.GetEntryAssembly();
 
                 // The only reason this will be null is if we are in a unit test.
-                if (primaryAssembly == null || primaryAssembly.FullName.ToLower().Contains("mstest"))
+                if (primaryAssembly == null || primaryAssembly.FullName.ToUpper(CultureInfo.InvariantCulture).Contains("MSTEST"))
                 {
                     return new Bitmap(1, 1);
                 }