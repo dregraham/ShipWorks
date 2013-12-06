674f346e1143cb28f22d3225dab0e884ed032a4e
Brian Nottingham <brian@shipworks.com>
2013-12-06 08:27:28 -0600

    Moving WorldShip update script so it gets properly applied to those who haven't had it, and ignores for those who have.

---
+++ Code/ShipWorks/Data/Administration/Scripts/Update/3.7/3.7.0.1.sql
@@ -0,0 +1,7 @@
+﻿PRINT N'Altering [dbo].[WorldShipProcessed]'
+GO
+
+IF COL_LENGTH('WorldShipProcessed','ShipmentIdCalculated') IS NULL
+	ALTER TABLE [dbo].[WorldShipProcessed] ADD
+	[ShipmentIdCalculated] AS (case when isnumeric([ShipmentID]+'.e0')=(1) then CONVERT([bigint],[ShipmentID],(0))  end) PERSISTED
+	GO
