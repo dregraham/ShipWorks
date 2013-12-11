ba4f9399bc0746bf899cb445812556ffcf52ce11
John Yedlicka <j.yedlicka@shipworks.com>
2013-12-11 16:34:05 -0600

    Remove short-circuiting the check for customs requirement when best-rate shipment type is used

--- Code/ShipWorks/Shipping/CustomsManager.cs
+++ Code/ShipWorks/Shipping/CustomsManager.cs
@@ -53,11 +53,6 @@ namespace ShipWorks.Shipping
         /// </summary>
         private static bool IsCustomsRequiredByShipment(ShipmentEntity shipment)
         {
-            if (ShipmentTypeManager.IsBestRate((ShipmentTypeCode) shipment.ShipmentType))
-            {
-                return false;
-            }
-
             bool requiresCustoms = !ShipmentType.IsDomestic(shipment);
 
             if (shipment.ShipCountryCode == "US")
