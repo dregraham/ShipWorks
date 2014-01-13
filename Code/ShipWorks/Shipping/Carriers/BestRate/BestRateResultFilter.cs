d07011bdc8397480acda5dbdd22096d9b92397f2
Tim Hughes <t.hughes@shipworks.com>
2014-01-13 14:37:23 -0600

    Added stubbed BestRateResultFilter.  Throws NotImplemented for now.

---
+++ Code/ShipWorks/Shipping/Carriers/BestRate/BestRateResultFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.Shipping.Editing;
+
+namespace ShipWorks.Shipping.Carriers.BestRate
+{
+    /// <summary>
+    /// Implementation of IBestRateResultFilter.  Used to filter Rate Results.
+    /// </summary>
+    public class BestRateResultFilter : IBestRateResultFilter
+    {
+        /// <summary>
+        /// Method that filters rate results and returns a new list of the filtered rate results.
+        /// </summary>
+        public IEnumerable<RateResult> FilterRates(List<RateResult> rateResults)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
