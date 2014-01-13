77db7bc53410e0c56a2aa7ec04f2d0f927eda8bc
Andrew Benz <a.benz@shipworks.com>
2014-01-13 16:41:07 -0600

    Add BestRateResultTag class

---
+++ Code/ShipWorks/Shipping/Carriers/BestRate/BestRateResultTag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping.Carriers.BestRate
+{
+    /// <summary>
+    /// Extra information for a best rate rate result
+    /// </summary>
+    public class BestRateResultTag
+    {
+        /// <summary>
+        /// Original value of the rate result's tag
+        /// </summary>
+        public object OriginalTag { get; set; }
+
+        /// <summary>
+        /// Identifier for the best rate rate result carrier and service type
+        /// </summary>
+        public string ResultKey { get; set; }
+
+        /// <summary>
+        /// Delegate that will be executed when a rate is selected
+        /// </summary>
+        public Action<ShipmentEntity> RateSelectionDelegate { get; set; } 
+    }
+}
