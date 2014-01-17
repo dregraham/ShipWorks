ee06356ada828694582a99c66a20cbd1fb468b47
Andrew Benz <a.benz@shipworks.com>
2014-01-17 09:47:20 -0600

    Add Express1DiscountedRateFootnoteFactory

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Express1/Express1DiscountedRateFootnoteFactory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using ShipWorks.Shipping.Editing;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Express1
+{
+    /// <summary>
+    /// Creates Express1 Rate Discounted footnote controls
+    /// </summary>
+    public class Express1DiscountedRateFootnoteFactory : IRateFootnoteFactory
+    {
+        private readonly List<RateResult> originalRates;
+        private readonly List<RateResult> discountedRates;
+
+        /// <summary>
+        /// Construct a new Express1DiscountedRateFootnoteFactory object
+        /// </summary>
+        /// <param name="shipmentType">Type of shipment that instantiated this factory</param>
+        /// <param name="originalRates">Original rates from the carrier</param>
+        /// <param name="discountedRates">Express1 rates</param>
+        public Express1DiscountedRateFootnoteFactory(ShipmentType shipmentType, List<RateResult> originalRates, List<RateResult> discountedRates)
+        {
+            this.originalRates = originalRates;
+            this.discountedRates = discountedRates;
+            ShipmentType = shipmentType;
+        }
+
+        /// <summary>
+        /// Gets the corresponding shipment type for the factory.
+        /// </summary>
+        public ShipmentType ShipmentType { get; private set; }
+
+        /// <summary>
+        /// Create an Express1 rate discounted control
+        /// </summary>
+        /// <returns></returns>
+        public RateFootnoteControl CreateFootnote()
+        {
+            return new Express1RateDiscountedFootnote(originalRates, discountedRates);
+        }
+    }
+}
