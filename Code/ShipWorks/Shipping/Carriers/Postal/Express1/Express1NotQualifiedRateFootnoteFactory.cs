87355ce1018e79fe4c042daa013cd7bc04f67fbb
Andrew Benz <a.benz@shipworks.com>
2014-01-17 09:56:19 -0600

    Add Express1NotQualifiedRateFootnoteFactory

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Express1/Express1NotQualifiedRateFootnoteFactory.cs
@@ -0,0 +1,33 @@
+﻿using ShipWorks.Shipping.Editing;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Express1
+{
+    /// <summary>
+    /// Creates Express1 Rate Not Qualified footnote controls
+    /// </summary>
+    public class Express1NotQualifiedRateFootnoteFactory : IRateFootnoteFactory
+    {
+        /// <summary>
+        /// Construct a new Express1NotQualifiedRateFootnoteFactory object
+        /// </summary>
+        /// <param name="shipmentType">Type of shipment that instantiated this factory</param>
+        public Express1NotQualifiedRateFootnoteFactory(ShipmentType shipmentType)
+        {
+            ShipmentType = shipmentType;
+        }
+
+        /// <summary>
+        /// Gets the corresponding shipment type for the factory.
+        /// </summary>
+        public ShipmentType ShipmentType { get; private set; }
+
+        /// <summary>
+        /// Create an Express1 rate not qualified control
+        /// </summary>
+        /// <returns></returns>
+        public RateFootnoteControl CreateFootnote()
+        {
+            return new Express1RateNotQualifiedFootnote();
+        }
+    }
+}
