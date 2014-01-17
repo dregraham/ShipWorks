82bb2069d1e127b583bc03ea3fad5fa44395be40
John Yedlicka <j.yedlicka@shipworks.com>
2014-01-17 07:59:01 -0600

    Slightly change the architecture for creating footnote controls
    - A factory interface will be used to instantiate the footnote controls
      rather than continuing with the lambda expression paradigm. The thought
      is that this will be a little more straight forward when filtering out the
      Express1 promo footers in best-rate since we don't have to instantiate a
      control to find out what it actually is, but can rather just perform a simple
      LINQ query to find out which footnote factories are in the rate group.

---
+++ Code/ShipWorks/Shipping/Editing/IRateFootnoteFactory.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ShipWorks.Shipping.Editing
+{
+    /// <summary>
+    /// A factory interface for being able to create footnote controls. 
+    /// </summary>
+    public interface IRateFootnoteFactory
+    {
+        /// <summary>
+        /// Creates a footnote control.
+        /// </summary>
+        /// <returns>A instance of a RateFoonoteControl.</returns>
+        RateFootnoteControl CreateFootnote();
+    }
+}
