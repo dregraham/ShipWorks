fc2615fc6d624b439f30193d0fcba645a08fe84c
Andrew Benz <a.benz@shipworks.com>
2014-01-28 08:27:13 -0600

    Add Express1StampsCounterAccountRepository

---
+++ Code/ShipWorks.Tests/Shipping/Carriers/Postal/Stamps/Express1/BestRate/Express1StampsCounterAccountRepositoryTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers;
+using ShipWorks.Shipping.Carriers.Postal.Stamps.Express1.BestRates;
+
+namespace ShipWorks.Tests.Shipping.Carriers.Postal.Stamps.Express1.BestRate
+{
+    [TestClass]
+    public class Express1StampsCounterAccountRepositoryTest
+    {
+        [TestMethod]
+        public void GetAccount_GetsCredentialsFromStore()
+        {
+            var store = new Mock<ICounterRatesCredentialStore>();
+            var testObject = new Express1StampsCounterAccountRepository(store.Object);
+
+            testObject.GetAccount(0);
+
+            store.Verify(x => x.Express1StampUsername);
+            store.Verify(x => x.Express1StampsPassword);
+        }
+
+        [TestMethod]
+        public void GetAccount_ReturnsValidCounterAccount()
+        {
+            var store = new Mock<ICounterRatesCredentialStore>();
+            store.Setup(x => x.Express1StampUsername).Returns("Foo");
+            store.Setup(x => x.Express1StampsPassword).Returns("Bar");
+
+            var testObject = new Express1StampsCounterAccountRepository(store.Object);
+
+            StampsAccountEntity account = testObject.GetAccount(0);
+
+            Assert.AreEqual("Foo", account.Username);
+            Assert.AreEqual("Bar", account.Password);
+            Assert.IsTrue(account.IsExpress1);
+        }
+    }
+}
---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Stamps/Express1/BestRates/Express1StampsCounterAccountRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Stamps.Express1.BestRates
+{
+    /// <summary>
+    /// Gets account data for Express1 Stamps counter rates
+    /// </summary>
+    public class Express1StampsCounterAccountRepository : ICarrierAccountRepository<StampsAccountEntity>
+    {
+        private readonly ICounterRatesCredentialStore credentialStore;
+        private readonly Lazy<List<StampsAccountEntity>> accounts;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="credentialStore"></param>
+        public Express1StampsCounterAccountRepository(ICounterRatesCredentialStore credentialStore)
+        {
+            this.credentialStore = credentialStore;
+            accounts = new Lazy<List<StampsAccountEntity>>(ConvertCredentialsToStampsAccountEntities);
+        }
+
+        /// <summary>
+        /// Gets a list of counter rate accounts
+        /// </summary>
+        public IEnumerable<StampsAccountEntity> Accounts
+        {
+            get
+            {
+                return accounts.Value;
+            }
+        }
+
+        /// <summary>
+        /// Gets a counter rate account
+        /// </summary>
+        public StampsAccountEntity GetAccount(long accountID)
+        {
+            return accounts.Value.First();
+        }
+
+        /// <summary>
+        /// Create a list of accounts from the credential store
+        /// </summary>
+        private List<StampsAccountEntity> ConvertCredentialsToStampsAccountEntities()
+        {
+            StampsAccountEntity account = new StampsAccountEntity
+            {
+                Username = credentialStore.Express1StampUsername,
+                Password = credentialStore.Express1StampsPassword,
+                IsExpress1 = true
+            };
+
+            return new List<StampsAccountEntity> { account };
+        }
+    }
+}
