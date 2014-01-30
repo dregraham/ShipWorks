cb51d02035ffea8cfb92156c91e5944c1450406d
Tim Hughes <t.hughes@shipworks.com>
2014-01-30 09:23:40 -0600

    Fix for FreshDesk 515584, SellerVantage URL Not Updating with Update.  Added sql to update the ModuleUrl for existing SellerVantage users.

---
+++ Code/ShipWorks/Data/Administration/Scripts/Update/3.7/3.7.0.4.sql
@@ -0,0 +1,5 @@
+﻿
+
+-- Update the SellerVantage ModuleUrl
+update GenericModuleStore set ModuleUrl = 'http://app.sellervantage.com/shipworksv3/' where StoreID in (select StoreID from Store where TypeCode = 23)
+
