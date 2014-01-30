128085e70d76a2246bd8fb5d0e30558b5d75a21b
John Yedlicka <j.yedlicka@shipworks.com>
2014-01-30 07:11:24 -0600

    Add unit tests for counter rate origin address validation

---
+++ Code/ShipWorks.Tests/Shipping/Carriers/BestRate/CounterRatesOriginAddressValidatorTest.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.BestRate;
+
+namespace ShipWorks.Tests.Shipping.Carriers.BestRate
+{
+    [TestClass]
+    public class CounterRatesOriginAddressValidatorTest
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(CounterRatesOriginAddressException))]
+        public void Validate_ThrowsCounterRatesOriginAddressException_WhenOriginStreet1IsEmpty_Test()
+        {
+            ShipmentEntity shipment = CreateShipment();
+            shipment.OriginStreet1 = string.Empty;
+
+            CounterRatesOriginAddressValidator.Validate(shipment);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CounterRatesOriginAddressException))]
+        public void Validate_ThrowsCounterRatesOriginAddressException_WhenOriginCityIsEmpty_Test()
+        {
+            ShipmentEntity shipment = CreateShipment();
+            shipment.OriginCity = string.Empty;
+
+            CounterRatesOriginAddressValidator.Validate(shipment);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CounterRatesOriginAddressException))]
+        public void Validate_ThrowsCounterRatesOriginAddressException_WhenOriginStateIsEmpty_Test()
+        {
+            ShipmentEntity shipment = CreateShipment();
+            shipment.OriginStateProvCode = string.Empty;
+
+            CounterRatesOriginAddressValidator.Validate(shipment);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CounterRatesOriginAddressException))]
+        public void Validate_ThrowsCounterRatesOriginAddressException_WhenOriginPostalCode1IsEmpty_Test()
+        {
+            ShipmentEntity shipment = CreateShipment();
+            shipment.OriginPostalCode = string.Empty;
+
+            CounterRatesOriginAddressValidator.Validate(shipment);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CounterRatesOriginAddressException))]
+        public void Validate_ThrowsCounterRatesOriginAddressException_WhenOriginCountryCodeIsEmpty_Test()
+        {
+            ShipmentEntity shipment = CreateShipment();
+            shipment.OriginCountryCode = string.Empty;
+
+            CounterRatesOriginAddressValidator.Validate(shipment);
+        }
+
+        private ShipmentEntity CreateShipment()
+        {
+            ShipmentEntity shipment = new ShipmentEntity
+            {
+                OriginStreet1 = "1 Memorial Drive",
+                OriginCity = "St. Louis",
+                OriginStateProvCode = "MO",
+                OriginPostalCode = "63102",
+                OriginCountryCode = "US"
+            };
+
+            return shipment;
+        }
+    }
+}
