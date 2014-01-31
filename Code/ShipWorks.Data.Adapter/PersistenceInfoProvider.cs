50e5e23ba077bb06731e05ef3543fbe218165221
Brian Nottingham <brian@shipworks.com>
2014-01-31 09:33:26 -0600

    Fix for crash when processing International MailInnovations with WorldShip

--- Code/ShipWorks.Data.Adapter/PersistenceInfoProvider.cs
+++ Code/ShipWorks.Data.Adapter/PersistenceInfoProvider.cs
@@ -2831,17 +2831,18 @@ namespace ShipWorks.Data.Adapter
 		/// <summary>Inits WorldShipGoodsEntity's mappings</summary>
 		private void InitWorldShipGoodsEntityMappings()
 		{
-			base.AddElementMapping( "WorldShipGoodsEntity", "ShipWorksLocal", @"dbo", "WorldShipGoods", 10 );
-			base.AddElementFieldMapping( "WorldShipGoodsEntity", "ShipmentCustomsItemID", "ShipmentCustomsItemID", false, (int)SqlDbType.BigInt, 0, 0, 19, false, "", null, typeof(System.Int64), 0 );
+			base.AddElementMapping( "WorldShipGoodsEntity", "ShipWorksLocal", @"dbo", "WorldShipGoods", 11 );
+			base.AddElementFieldMapping( "WorldShipGoodsEntity", "WorldShipGoodsID", "WorldShipGoodsID", false, (int)SqlDbType.BigInt, 0, 0, 19, true, "SCOPE_IDENTITY()", null, typeof(System.Int64), 0 );
 			base.AddElementFieldMapping( "WorldShipGoodsEntity", "ShipmentID", "ShipmentID", false, (int)SqlDbType.BigInt, 0, 0, 19, false, "", null, typeof(System.Int64), 1 );
-			base.AddElementFieldMapping( "WorldShipGoodsEntity", "Description", "Description", false, (int)SqlDbType.NVarChar, 150, 0, 0, false, "", null, typeof(System.String), 2 );
-			base.AddElementFieldMapping( "WorldShipGoodsEntity", "TariffCode", "TariffCode", false, (int)SqlDbType.VarChar, 15, 0, 0, false, "", null, typeof(System.String), 3 );
-			base.AddElementFieldMapping( "WorldShipGoodsEntity", "CountryOfOrigin", "CountryOfOrigin", false, (int)SqlDbType.VarChar, 50, 0, 0, false, "", null, typeof(System.String), 4 );
-			base.AddElementFieldMapping( "WorldShipGoodsEntity", "Units", "Units", false, (int)SqlDbType.Int, 0, 0, 10, false, "", null, typeof(System.Int32), 5 );
-			base.AddElementFieldMapping( "WorldShipGoodsEntity", "UnitOfMeasure", "UnitOfMeasure", false, (int)SqlDbType.VarChar, 5, 0, 0, false, "", null, typeof(System.String), 6 );
-			base.AddElementFieldMapping( "WorldShipGoodsEntity", "UnitPrice", "UnitPrice", false, (int)SqlDbType.Money, 0, 4, 19, false, "", null, typeof(System.Decimal), 7 );
-			base.AddElementFieldMapping( "WorldShipGoodsEntity", "Weight", "Weight", false, (int)SqlDbType.Float, 0, 0, 38, false, "", null, typeof(System.Double), 8 );
-			base.AddElementFieldMapping( "WorldShipGoodsEntity", "InvoiceCurrencyCode", "InvoiceCurrencyCode", true, (int)SqlDbType.VarChar, 3, 0, 0, false, "", null, typeof(System.String), 9 );
+			base.AddElementFieldMapping( "WorldShipGoodsEntity", "ShipmentCustomsItemID", "ShipmentCustomsItemID", false, (int)SqlDbType.BigInt, 0, 0, 19, false, "", null, typeof(System.Int64), 2 );
+			base.AddElementFieldMapping( "WorldShipGoodsEntity", "Description", "Description", false, (int)SqlDbType.NVarChar, 150, 0, 0, false, "", null, typeof(System.String), 3 );
+			base.AddElementFieldMapping( "WorldShipGoodsEntity", "TariffCode", "TariffCode", false, (int)SqlDbType.VarChar, 15, 0, 0, false, "", null, typeof(System.String), 4 );
+			base.AddElementFieldMapping( "WorldShipGoodsEntity", "CountryOfOrigin", "CountryOfOrigin", false, (int)SqlDbType.VarChar, 50, 0, 0, false, "", null, typeof(System.String), 5 );
+			base.AddElementFieldMapping( "WorldShipGoodsEntity", "Units", "Units", false, (int)SqlDbType.Int, 0, 0, 10, false, "", null, typeof(System.Int32), 6 );
+			base.AddElementFieldMapping( "WorldShipGoodsEntity", "UnitOfMeasure", "UnitOfMeasure", false, (int)SqlDbType.VarChar, 5, 0, 0, false, "", null, typeof(System.String), 7 );
+			base.AddElementFieldMapping( "WorldShipGoodsEntity", "UnitPrice", "UnitPrice", false, (int)SqlDbType.Money, 0, 4, 19, false, "", null, typeof(System.Decimal), 8 );
+			base.AddElementFieldMapping( "WorldShipGoodsEntity", "Weight", "Weight", false, (int)SqlDbType.Float, 0, 0, 38, false, "", null, typeof(System.Double), 9 );
+			base.AddElementFieldMapping( "WorldShipGoodsEntity", "InvoiceCurrencyCode", "InvoiceCurrencyCode", true, (int)SqlDbType.VarChar, 3, 0, 0, false, "", null, typeof(System.String), 10 );
 		}
 		/// <summary>Inits WorldShipPackageEntity's mappings</summary>
 		private void InitWorldShipPackageEntityMappings()
--- Code/ShipWorks/Data/Administration/Scripts/Update/3.7/3.7.0.4.sql
+++
@@ -1,5 +0,0 @@
-﻿
-
--- Update the SellerVantage ModuleUrl
-update GenericModuleStore set ModuleUrl = 'http://app.sellervantage.com/shipworksv3/' where StoreID in (select StoreID from Store where TypeCode = 23)
-
