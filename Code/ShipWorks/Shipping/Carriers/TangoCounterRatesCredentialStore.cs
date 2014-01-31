2a6208e69e30badc5a880843996fe36d277d6bb5
John Yedlicka <j.yedlicka@shipworks.com>
2014-01-31 08:58:25 -0600

    Incorporate failure delay when getting production account credentials from Tango

--- Code/ShipWorks/Shipping/Carriers/TangoCounterRatesCredentialStore.cs
+++ Code/ShipWorks/Shipping/Carriers/TangoCounterRatesCredentialStore.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using ShipWorks.ApplicationCore.Licensing;
 using ShipWorks.Shipping.Carriers.FedEx.Api.Environment;
 using ShipWorks.Shipping.Carriers.Postal.Endicia.Express1;
 using ShipWorks.Shipping.Carriers.Postal.Stamps.Express1;
 using ShipWorks.Shipping.Carriers.UPS.OnLineTools.Api;
+using log4net;
 
 namespace ShipWorks.Shipping.Carriers
 {
@@ -15,81 +15,64 @@ namespace ShipWorks.Shipping.Carriers
     /// </summary>
     public class TangoCounterRatesCredentialStore : ICounterRatesCredentialStore
     {
-        private static FedExSettingsRepository fedExSettingsRepo = new FedExSettingsRepository();
+        private static readonly ILog log = LogManager.GetLogger(typeof(TangoCounterRatesCredentialStore));
+
+        private static readonly FedExSettingsRepository fedExSettingsRepo = new FedExSettingsRepository();
+        
+        private static DateTime lastFailure;
 
         // The lazy loaded singleton instance variable.
-        private static readonly Lazy<TangoCounterRatesCredentialStore> lazyInstance = 
+        private static readonly Lazy<TangoCounterRatesCredentialStore> lazyInstance =
             new Lazy<TangoCounterRatesCredentialStore>(() => new TangoCounterRatesCredentialStore());
 
-        // Test credentials
-        private const string testCredentialFedExAccountNumber = "603103343";
-        private const string testCredentialFedExMeterNumber = "118601174";
-        private const string testCredentialFedExUsername = "51LPnQ5iP1uPARkC";
-        private const string testCredentialFedExPassword = "VYnYkYqui2OIux8DO+Po0YQKvySBei17NsODezd5bRY=";
-        private const string testCredentialUpsUserId = "6863e0f62cdd4a1b";
-        private const string testCredentialUpsPassword = "cf9e7473";
-        private const string testCredentialUpsAccessKey = "YbeKtEkBXqxQYcW0MonRIXPCPFKuLQ6l";
-        private const string testCredentialExpress1EndiciaAccountNumber = "ba66e5d7-5224-4273-a7e4-6176e2b06d7b";
-        private const string testCredentialExpress1EndiciaPassPhrase = "Y71yGErhEfgAqBkCprcEXA==";
-        private const string testCredentialExpress1StampsUsername = "759cc789-25ab-4701-b791-b0c7d4b47701";
-        private const string testCredentialExpress1StampsPassword = "nqsNMvjHqa3u3qX1qav5BldJ+6deGykO4i/B3T3YR/1PTXRSkBcTfA==";
+        // The lock used when populating our credential dictionary
+        private static readonly object lockObject = new object();
 
-        // Production credentials
-        private string fedExAccountNumber = "";
-        private string fedExMeterNumber = "";
-        private string fedExUsername = "";
-        private string fedExPassword = "";
-        private string upsUserId = "";
-        private string upsPassword = "";
-        private string upsAccessKey = "";
-        private string express1EndiciaAccountNumber = "";
-        private string express1EndiciaPassPhrase = "";
-        private string express1StampsUsername = "";
-        private string express1StampsPassword = "";
+        // A dictionary used for storing the account credentials for providers' production accounts
+        private static volatile Dictionary<string, string> productionCredentials;
 
+        // Credentials to use when ShipWorks is using a provider's test environment
+        private const string TestCredentialFedExAccountNumber = "603103343";
+        private const string TestCredentialFedExMeterNumber = "118601174";
+        private const string TestCredentialFedExUsername = "51LPnQ5iP1uPARkC";
+        private const string TestCredentialFedExPassword = "VYnYkYqui2OIux8DO+Po0YQKvySBei17NsODezd5bRY=";
+
+        private const string TestCredentialUpsUserId = "6863e0f62cdd4a1b";
+        private const string TestCredentialUpsPassword = "cf9e7473";
+        private const string TestCredentialUpsAccessKey = "YbeKtEkBXqxQYcW0MonRIXPCPFKuLQ6l";
+
+        private const string TestCredentialExpress1EndiciaAccountNumber = "ba66e5d7-5224-4273-a7e4-6176e2b06d7b";
+        private const string TestCredentialExpress1EndiciaPassPhrase = "Y71yGErhEfgAqBkCprcEXA==";
+
+        private const string TestCredentialExpress1StampsUsername = "759cc789-25ab-4701-b791-b0c7d4b47701";
+        private const string TestCredentialExpress1StampsPassword = "nqsNMvjHqa3u3qX1qav5BldJ+6deGykO4i/B3T3YR/1PTXRSkBcTfA==";
+       
+        // Key names of credential values in the dictionary 
+        private const string FedExAccountNumberKeyName = "FedExAccountNumber";
+        private const string FedExMeterNumberKeyName = "FedExMeterNumber";
+        private const string FedExUserNameKeyName = "FedExUsername";
+        private const string FedExPasswordKeyName = "FedExPassword";
+
+        private const string UpsUserIdKeyName = "UpsUserId";
+        private const string UpsPasswordKeyName = "UpsPassword";
+        private const string UpsAccessKeyKeyName = "UpsAccessKey";
+
+        private const string Express1EndiciaAccountNumberKeyName = "Express1EndiciaAccountNumber";
+        private const string Express1EndiciaPassPhraseKeyName = "Express1EndiciaPassPhrase";
+
+        private const string Express1StampsUsernameKeyName = "Express1StampsUsername";
+        private const string Express1StampsPasswordKeyName = "Express1StampsPassword";
+        
+        
         /// <summary>
         /// Private constructor.
         /// </summary>
         private TangoCounterRatesCredentialStore()
         {
+            lastFailure = DateTime.MinValue;
+            productionCredentials = new Dictionary<string, string>();
         }
-
-        /// <summary>
-        /// Calls Tango to get counter rates credentials
-        /// </summary>
-        private void LoadTangoProductionCredentials()
-        {
-            Dictionary<string, string> creds;
-
-            // Try to get the counter rates credentials from Tango
-            try
-            {
-                creds = TangoWebClient.GetCounterRatesCredentials();
-            }
-            catch (TangoException)
-            {
-                throw new ShippingException("Unable to get counter rates.");
-            }
-
-            // Check the result...make sure it's not null, has more than 0 entries, and at least has the FedExAccountNumber
-            if (creds == null || creds.Count == 0 || !creds.ContainsKey("FedExAccountNumber"))
-            {
-                throw new ShippingException("Unable to get counter rates.");
-            }
-
-            fedExAccountNumber = creds["FedExAccountNumber"];
-            fedExMeterNumber = creds["FedExMeterNumber"];
-            fedExUsername = creds["FedExUsername"];
-            fedExPassword = creds["FedExPassword"];
-            upsUserId = creds["UpsUserId"];
-            upsPassword = creds["UpsPassword"];
-            upsAccessKey = creds["UpsAccessKey"];
-            express1EndiciaAccountNumber = creds["Express1EndiciaAccountNumber"];
-            express1EndiciaPassPhrase = creds["Express1EndiciaPassPhrase"];
-            express1StampsUsername = creds["Express1StampsUsername"];
-            express1StampsPassword = creds["Express1StampsPassword"];
-        }
-
+        
         /// <summary>
         /// The instance of this Tangle Credential store singleton.
         /// </summary>
@@ -108,7 +91,8 @@ namespace ShipWorks.Shipping.Carriers
         {
             get
             {
-                return fedExSettingsRepo.UseTestServer ? testCredentialFedExAccountNumber : fedExAccountNumber;
+                return fedExSettingsRepo.UseTestServer ? 
+                    TestCredentialFedExAccountNumber : GetCredentialValue(FedExAccountNumberKeyName);
             }
         }
 
@@ -119,18 +103,20 @@ namespace ShipWorks.Shipping.Carriers
         {
             get
             {
-                return fedExSettingsRepo.UseTestServer ? testCredentialFedExMeterNumber : fedExMeterNumber;
+                return fedExSettingsRepo.UseTestServer ? 
+                    TestCredentialFedExMeterNumber : GetCredentialValue(FedExMeterNumberKeyName);
             }
         }
 
         /// <summary>
-        /// The ShipWorks FedEx username
+        /// The ShipWorks FedEx user name
         /// </summary>
         public string FedExUsername
         {
             get
             {
-                return fedExSettingsRepo.UseTestServer ? testCredentialFedExUsername : fedExUsername;
+                return fedExSettingsRepo.UseTestServer ? 
+                    TestCredentialFedExUsername : GetCredentialValue(FedExUserNameKeyName);
             }
         }
 
@@ -141,7 +127,8 @@ namespace ShipWorks.Shipping.Carriers
         {
             get
             {
-                return fedExSettingsRepo.UseTestServer ? testCredentialFedExPassword : fedExPassword;
+                return fedExSettingsRepo.UseTestServer ? 
+                    TestCredentialFedExPassword : GetCredentialValue(FedExPasswordKeyName);
             }
         }
 
@@ -152,7 +139,8 @@ namespace ShipWorks.Shipping.Carriers
         {
             get
             {
-                return UpsWebClient.UseTestServer ? testCredentialUpsUserId : upsUserId;
+                return UpsWebClient.UseTestServer ? 
+                    TestCredentialUpsUserId : GetCredentialValue(UpsUserIdKeyName);
             }
         }
 
@@ -163,7 +151,8 @@ namespace ShipWorks.Shipping.Carriers
         {
             get
             {
-                return UpsWebClient.UseTestServer ? testCredentialUpsPassword : upsPassword;
+                return UpsWebClient.UseTestServer ? 
+                    TestCredentialUpsPassword : GetCredentialValue(UpsPasswordKeyName);
             }
         }
 
@@ -174,7 +163,8 @@ namespace ShipWorks.Shipping.Carriers
         {
             get
             {
-                return UpsWebClient.UseTestServer ? testCredentialUpsAccessKey : upsAccessKey;
+                return UpsWebClient.UseTestServer ? 
+                    TestCredentialUpsAccessKey : GetCredentialValue(UpsAccessKeyKeyName);
             }
         }
 
@@ -185,7 +175,8 @@ namespace ShipWorks.Shipping.Carriers
         {
             get
             {
-                return Express1EndiciaUtility.UseTestServer ? testCredentialExpress1EndiciaAccountNumber : express1EndiciaAccountNumber;
+                return Express1EndiciaUtility.UseTestServer ? 
+                    TestCredentialExpress1EndiciaAccountNumber : GetCredentialValue(Express1EndiciaAccountNumberKeyName);
             }
         }
 
@@ -196,7 +187,8 @@ namespace ShipWorks.Shipping.Carriers
         {
             get
             {
-                return Express1EndiciaUtility.UseTestServer ? testCredentialExpress1EndiciaPassPhrase : express1EndiciaPassPhrase;
+                return Express1EndiciaUtility.UseTestServer ? 
+                    TestCredentialExpress1EndiciaPassPhrase : GetCredentialValue(Express1EndiciaPassPhraseKeyName);
             }
         }
         /// <summary>
@@ -206,7 +198,8 @@ namespace ShipWorks.Shipping.Carriers
         {
             get
             {
-                return Express1StampsConnectionDetails.UseTestServer ? testCredentialExpress1StampsUsername : express1StampsUsername;
+                return Express1StampsConnectionDetails.UseTestServer ? 
+                    TestCredentialExpress1StampsUsername : GetCredentialValue(Express1StampsUsernameKeyName);
             }
         }
 
@@ -217,8 +210,86 @@ namespace ShipWorks.Shipping.Carriers
         {
             get
             {
-                return Express1StampsConnectionDetails.UseTestServer ? testCredentialExpress1StampsPassword : express1StampsPassword;
+                return Express1StampsConnectionDetails.UseTestServer ? 
+                    TestCredentialExpress1StampsPassword : GetCredentialValue(Express1StampsPasswordKeyName);
             }
+        }
+
+        /// <summary>
+        /// Uses the key name provided to get the corresponding value from the credential dictionary.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>The value for the given key.</returns>
+        private string GetCredentialValue(string key)
+        {
+            try
+            {
+                if (!productionCredentials.Any())
+                {
+                    // The credentials haven't been populated yet, so we need to get them
+                    // from Tango
+                    LoadTangoProductionCredentials();
+                }
+
+                return productionCredentials[key];
+            }
+            catch (Exception ex)
+            {
+                throw new ShippingException("Unable to get counter rates.", ex);
+            }
+        }
+
+        /// <summary>
+        /// Makes a request to Tango to obtain the account credentials that should be
+        /// used for requesting counter rates in a provider's production environment.
+        /// </summary>
+        private void LoadTangoProductionCredentials()
+        {
+            if (!productionCredentials.Any())
+            {
+                if (DateTime.UtcNow.Subtract(lastFailure).TotalSeconds < 30)
+                {
+                    // The 30 second delay from the last failed request hasn't elapsed , so just throw the
+                    // shipping exception here.
+                    throw new ShippingException("Unable to get counter rates.");
+                }
+
+                lock (lockObject)
+                {
+                    if (!productionCredentials.Any())
+                    {
+                        try
+                        {
+                            // Try to get the counter rates credentials from Tango
+                            productionCredentials = TangoWebClient.GetCounterRatesCredentials();
+                        }
+                        catch (TangoException ex)
+                        {
+                            log.Error(ex);
+                            ThrowShippingException(ex);
+                        }
+
+                        // Check the result...make sure it's not null, has more than 0 entries, and at least has the FedExAccountNumber
+                        if (!productionCredentials.Any() || !productionCredentials.ContainsKey(FedExAccountNumberKeyName))
+                        {
+                            // We still don't have any credentials
+                            ThrowShippingException(null);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Throws a shipping exception.
+        /// </summary>
+        /// <exception cref="ShippingException">Unable to get counter rates.</exception>
+        private void ThrowShippingException(Exception innerException)
+        {
+            // Note the time of the failure, so we can throttle any subsequent 
+            // calls to Tango before throwing the exception;
+            lastFailure = DateTime.UtcNow;
+            throw new ShippingException("Unable to get counter rates.", innerException);
         }
     }
 }
