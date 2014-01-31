dd3d7cd5e430ec8f53a0f8996a9faef9ca4a3c64
Kevin Croke <k.croke@shipworks.com>
2014-01-31 10:44:07 -0600

    In UpdgradePath, initialize FromVersion to an empty list and added ToString Logic.

--- Code/ShipWorks/Data/Administration/Versioning/UpgradePath.cs
+++ Code/ShipWorks/Data/Administration/Versioning/UpgradePath.cs
@@ -1,25 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
-namespace ShipWorks.Data.Administration
+namespace ShipWorks.Data.Administration.Versioning
 {
     /// <summary>
     /// Class represents possible upgrade paths to the ToVersion.
     /// </summary>
     public class UpgradePath
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpgradePath"/> class.
+        /// </summary>
+        public UpgradePath()
+        {
+            FromVersion=new List<string>();    
+        }
+
+        /// <summary>
+        /// The version to Upgrade To.
+        /// </summary>
         public string ToVersion
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// The version we will upgrade From.
+        /// </summary>
         public List<string> FromVersion
         {
             get;
             set;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
         }
     }
 }
