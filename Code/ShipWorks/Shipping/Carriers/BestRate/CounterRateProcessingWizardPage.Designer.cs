c512d0222ad539934a0a2a0a754cf5f5519cae05
John Yedlicka <j.yedlicka@shipworks.com>
2014-02-04 12:46:27 -0600

    Add logic to counter rate processing wizard page for selecting correct rate

--- Code/ShipWorks/Shipping/Carriers/BestRate/CounterRateProcessingWizardPage.Designer.cs
+++ Code/ShipWorks/Shipping/Carriers/BestRate/CounterRateProcessingWizardPage.Designer.cs
@@ -28,18 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CounterRateProcessingWizardPage));
+            this.availableRatePanel = new System.Windows.Forms.Panel();
+            this.ignoreCounterRates = new System.Windows.Forms.CheckBox();
+            this.moreExpensiveAvailableRate = new System.Windows.Forms.Label();
+            this.useAvailableRateLink = new System.Windows.Forms.LinkLabel();
+            this.availableRateDescription = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.Label();
+            this.heading = new System.Windows.Forms.Label();
+            this.availableRatePanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // availableRatePanel
+            // 
+            this.availableRatePanel.Controls.Add(this.ignoreCounterRates);
+            this.availableRatePanel.Controls.Add(this.moreExpensiveAvailableRate);
+            this.availableRatePanel.Controls.Add(this.useAvailableRateLink);
+            this.availableRatePanel.Controls.Add(this.availableRateDescription);
+            this.availableRatePanel.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.availableRatePanel.Location = new System.Drawing.Point(10, 109);
+            this.availableRatePanel.Name = "availableRatePanel";
+            this.availableRatePanel.Size = new System.Drawing.Size(477, 43);
+            this.availableRatePanel.TabIndex = 0;
+            // 
+            // ignoreCounterRates
+            // 
+            this.ignoreCounterRates.AutoSize = true;
+            this.ignoreCounterRates.Location = new System.Drawing.Point(6, 21);
+            this.ignoreCounterRates.Name = "ignoreCounterRates";
+            this.ignoreCounterRates.Size = new System.Drawing.Size(358, 17);
+            this.ignoreCounterRates.TabIndex = 3;
+            this.ignoreCounterRates.Text = "Don\'t help me save money for the rest of the shipments in this batch.";
+            this.ignoreCounterRates.UseVisualStyleBackColor = true;
+            // 
+            // moreExpensiveAvailableRate
+            // 
+            this.moreExpensiveAvailableRate.AutoSize = true;
+            this.moreExpensiveAvailableRate.Location = new System.Drawing.Point(52, 5);
+            this.moreExpensiveAvailableRate.Name = "moreExpensiveAvailableRate";
+            this.moreExpensiveAvailableRate.Size = new System.Drawing.Size(417, 13);
+            this.moreExpensiveAvailableRate.TabIndex = 1;
+            this.moreExpensiveAvailableRate.Text = "if you would rather use your existing {ProviderName} account that costs $x.xx mor" +
+    "e.";
+            // 
+            // useAvailableRateLink
+            // 
+            this.useAvailableRateLink.AutoSize = true;
+            this.useAvailableRateLink.Location = new System.Drawing.Point(27, 5);
+            this.useAvailableRateLink.Name = "useAvailableRateLink";
+            this.useAvailableRateLink.Size = new System.Drawing.Size(29, 13);
+            this.useAvailableRateLink.TabIndex = 2;
+            this.useAvailableRateLink.TabStop = true;
+            this.useAvailableRateLink.Text = "here";
+            this.useAvailableRateLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.OnUseAvailableRateLinkClicked);
+            // 
+            // availableRateDescription
+            // 
+            this.availableRateDescription.AutoSize = true;
+            this.availableRateDescription.Location = new System.Drawing.Point(3, 5);
+            this.availableRateDescription.Name = "availableRateDescription";
+            this.availableRateDescription.Size = new System.Drawing.Size(28, 13);
+            this.availableRateDescription.TabIndex = 0;
+            this.availableRateDescription.Text = "Click";
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(13, 38);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(469, 52);
+            this.description.TabIndex = 1;
+            this.description.Text = resources.GetString("description.Text");
+            // 
+            // heading
+            // 
+            this.heading.AutoSize = true;
+            this.heading.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heading.Location = new System.Drawing.Point(10, 14);
+            this.heading.Name = "heading";
+            this.heading.Size = new System.Drawing.Size(252, 13);
+            this.heading.TabIndex = 2;
+            this.heading.Text = "ShipWorks found a way to save you money!";
             // 
             // CounterRateProcessingWizardPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.heading);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.availableRatePanel);
             this.Name = "CounterRateProcessingWizardPage";
-            this.Size = new System.Drawing.Size(464, 349);
+            this.Size = new System.Drawing.Size(501, 349);
+            this.availableRatePanel.ResumeLayout(false);
+            this.availableRatePanel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel availableRatePanel;
+        private System.Windows.Forms.CheckBox ignoreCounterRates;
+        private System.Windows.Forms.Label moreExpensiveAvailableRate;
+        private System.Windows.Forms.LinkLabel useAvailableRateLink;
+        private System.Windows.Forms.Label availableRateDescription;
+        private System.Windows.Forms.Label description;
+        private System.Windows.Forms.Label heading;
     }
 }
--- Code/ShipWorks/Shipping/Carriers/BestRate/CounterRateProcessingWizardPage.cs
+++ Code/ShipWorks/Shipping/Carriers/BestRate/CounterRateProcessingWizardPage.cs
@@ -14,37 +14,38 @@ namespace ShipWorks.Shipping.Carriers.BestRate
 {
     public partial class CounterRateProcessingWizardPage : WizardPage
     {
-        private readonly WizardForm host;
         private readonly IEnumerable<ShipmentEntity> shipmentsInBatch;
+        private bool usingAvailableRate;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CounterRateProcessingWizardPage" /> class.
         /// </summary>
         /// <param name="filteredRates">The filtered rates.</param>
         /// <param name="allRates">All rates.</param>
-        /// <param name="host">The Wizard form that is hosting this page.</param>
         /// <param name="shipmentsInBatch">All of the shipments being processed in the current batch.</param>
-        public CounterRateProcessingWizardPage(RateGroup filteredRates, RateGroup allRates, WizardForm host, IEnumerable<ShipmentEntity> shipmentsInBatch)
+        public CounterRateProcessingWizardPage(RateGroup filteredRates, RateGroup allRates, IEnumerable<ShipmentEntity> shipmentsInBatch)
         {
             InitializeComponent();
             
             FilteredRates = filteredRates;
             AllRates = allRates;
 
-            this.host = host;
             this.shipmentsInBatch = shipmentsInBatch;
+            usingAvailableRate = false;
 
             if (this.shipmentsInBatch.Count() > 1)
             {
-                // TODO: remove the UI for the user to ignore counter rates for the rest of the batch
+                // There's only one shipment, so it doesn't make sense to show the UI 
+                // for the user to ignore counter rates for the rest of the batch
+                ignoreCounterRates.Visible = false;
             }
 
             if (AllRates.Rates.All(r => r.IsCounterRate))
             {
-                // TODO: remove the UI for the user choose to use an available rate
+                // There aren't any rates for an existing account, so remove 
+                // the UI for the user choose to use an available rate
+                availableRatePanel.Visible = false;
             }
-
-            this.host.FormClosing += FinalizeValues;
         }
 
         /// <summary>
@@ -60,22 +61,52 @@ namespace ShipWorks.Shipping.Carriers.BestRate
         /// <summary>
         /// Gets the selected rate.
         /// </summary>
-        public RateResult SelectedRate { get; private set; }
+        public RateResult SelectedRate
+        {
+            get
+            {
+                RateResult selectedRate = FilteredRates.Rates.First();
+
+                if (usingAvailableRate)
+                {
+                    // The user opted to use a rate from an existing account, so grab the first 
+                    // non-counter rate from the list containing all of the rates. All rates is 
+                    // used as the source, so any service types that were filtered out based on 
+                    // the cheapest counter rate still applied appropriately
+                    selectedRate = AllRates.Rates.First(r => !r.IsCounterRate);
+                }
+
+                return selectedRate;
+            }
+        }
 
         /// <summary>
         /// Gets a value indicating whether the user chose to [ignore all counter rates] for
         /// the batch of shipments being processed.
         /// </summary>
-        public bool IgnoreAllCounterRates { get; private set; }
+        public bool IgnoreAllCounterRates
+        {
+            get
+            {
+                // Set the property to ignore all counter rates to false if the user did not 
+                // choose the option to use an available rate in addition to checking the box.
+                // This avoids the incorrect value being returned when the user has the ignore
+                // option checked, but continued to setup an account anyway
+                return usingAvailableRate && ignoreCounterRates.Checked;
+            }
+        }
 
         /// <summary>
-        /// Finalizes the values.
+        /// Called when [use available rate link clicked].
         /// </summary>
         /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="FormClosingEventArgs"/> instance containing the event data.</param>
-        private void FinalizeValues(object sender, FormClosingEventArgs e)
+        /// <param name="e">The <see cref="LinkLabelLinkClickedEventArgs"/> instance containing the event data.</param>
+        private void OnUseAvailableRateLinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            throw new NotImplementedException();
+            // Note that the user chose to use the available rate and close the wizard
+            // hosting this page.
+            usingAvailableRate = true;            
+            Wizard.Close();
         }
     }
 }
