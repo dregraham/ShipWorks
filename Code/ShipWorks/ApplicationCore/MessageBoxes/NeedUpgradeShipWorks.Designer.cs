815c96a4bb9b92c0a85bedfbc9406674d08890bb
Brian Nottingham <brian@shipworks.com>
2014-02-20 08:21:42 -0600

    Provide better message and link to the software when they are required to upgrade.

---
+++ Code/ShipWorks/ApplicationCore/MessageBoxes/NeedUpgradeShipWorks.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ShipWorks.ApplicationCore.MessageBoxes
+{
+    partial class NeedUpgradeShipWorks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.sp2Link = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.close = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(255, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "ShipWorks won\'t be able to open until you upgrade.";
+            // 
+            // sp2Link
+            // 
+            this.sp2Link.Location = new System.Drawing.Point(29, 123);
+            this.sp2Link.Name = "sp2Link";
+            this.sp2Link.Size = new System.Drawing.Size(244, 23);
+            this.sp2Link.TabIndex = 16;
+            this.sp2Link.TabStop = true;
+            this.sp2Link.Text = "Get the latest version of ShipWorks";
+            this.sp2Link.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.OnClickDownloadLink);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(29, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(395, 42);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "ShipWorks needs every computer to be running the same version of ShipWorks.  Sinc" +
+    "e ShipWorks has been upgraded on another computer, you\'ll need to upgrade it on " +
+    "this computer too.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ShipWorks.Properties.Resources.exclamation16;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(379, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Another computer has upgraded to a newer version of ShipWorks.";
+            // 
+            // close
+            // 
+            this.close.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.close.Location = new System.Drawing.Point(349, 149);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(75, 23);
+            this.close.TabIndex = 18;
+            this.close.Text = "Close";
+            this.close.UseVisualStyleBackColor = true;
+            // 
+            // NeedUpgradeShipWorks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 189);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sp2Link);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "NeedUpgradeShipWorks";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ShipWorks";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel sp2Link;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button close;
+    }
+}
