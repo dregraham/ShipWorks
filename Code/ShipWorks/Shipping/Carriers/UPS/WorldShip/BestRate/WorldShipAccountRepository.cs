e0b85248ada85e5109da663f908adc0b135e0245
Tim Hughes <t.hughes@shipworks.com>
2014-03-10 12:32:06 -0500

    UPS and WorldShip implementation for DefaultProfileAccount

--- Code/ShipWorks/Shipping/Carriers/UPS/WorldShip/BestRate/WorldShipAccountRepository.cs
+++ Code/ShipWorks/Shipping/Carriers/UPS/WorldShip/BestRate/WorldShipAccountRepository.cs
@@ -5,9 +5,20 @@ using ShipWorks.Shipping.Carriers.UPS.BestRate;
 namespace ShipWorks.Shipping.Carriers.UPS.WorldShip.BestRate
 {
     /// <summary>
-    /// Implementation of ICarrierAccountRepository for WorldShip.  It will just default down to UpsAccountRepository.
+    /// Implementation of ICarrierAccountRepository for WorldShip.  
     /// </summary>
     public class WorldShipAccountRepository : UpsAccountRepository
     {
+        /// <summary>
+        ///  Returns the default account as defined by the primary profile
+        ///  </summary>
+        public override UpsAccountEntity DefaultProfileAccount
+        {
+            get
+            {
+                long? accountID = new WorldShipShipmentType().GetPrimaryProfile().Ups.UpsAccountID;
+                return GetProfileAccount(ShipmentTypeCode.UpsWorldShip, accountID);
+            }
+        }
     }
 }
