52be52fd4cc099109c6f0edbd743edd34181ebe2
John Yedlicka <j.yedlicka@shipworks.com>
2014-03-11 17:02:19 -0500

    Add ICertificateInspector interface and CertificateSecurityLevel enumeration
    - Building blocks for attempting to secure the credentials that ShipWorks uses
      to obtain counter rates from various carriers from man in the
      middle situations.

---
+++ Code/Interapptive.Shared/Net/ICertificateInspector.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace Interapptive.Shared.Net
+{
+    /// <summary>
+    /// An interface intended to inspect the certificate associated with a request
+    /// to determine the security level.
+    /// </summary>
+    public interface ICertificateInspector
+    {
+        /// <summary>
+        /// Inspects the certificate of the given request to determine the security
+        /// level of the certificate.
+        /// </summary>
+        CertificateSecurityLevel Inspect(HttpRequest request);
+    }
+}
