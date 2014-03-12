dbdfb64e7d617dc16d0990bc4372c1472b0dd324
John Yedlicka <j.yedlicka@shipworks.com>
2014-03-12 14:24:48 -0500

    Add additional value to the test credentials' FedEx certificate verfication data

--- Code/ShipWorks/Shipping/Carriers/TangoCounterRatesCredentialStore.cs
+++ Code/ShipWorks/Shipping/Carriers/TangoCounterRatesCredentialStore.cs
@@ -38,7 +38,7 @@ namespace ShipWorks.Shipping.Carriers
         private const string TestCredentialFedExMeterNumber = "118601174";
         private const string TestCredentialFedExUsername = "51LPnQ5iP1uPARkC";
         private const string TestCredentialFedExPassword = "VYnYkYqui2OIux8DO+Po0YQKvySBei17NsODezd5bRY=";
-        private const string TestCredentialFedExCertificateVerificationData = "<Subject><Value>wsbeta.fedex.com</Value></Subject>";
+        private const string TestCredentialFedExCertificateVerificationData = "<Subject><Value>wsbeta.fedex.com</Value><Value>OU=EIS-WSAS</Value></Subject>";
 
         private const string TestCredentialUpsUserId = "6863e0f62cdd4a1b";
         private const string TestCredentialUpsPassword = "cf9e7473";
