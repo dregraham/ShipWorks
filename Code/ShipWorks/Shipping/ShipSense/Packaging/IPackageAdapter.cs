252d7333d2f0a4e19a68e324a2dfc905de6f4f3a
John Yedlicka <j.yedlicka@shipworks.com>
2014-03-20 14:58:08 -0500

    Defined the IPacakgeAdapter interface
    - This is an adapter intended to be used for shuffling package data between
      a ShipSense knowledge base entry and carriers.

---
+++ Code/ShipWorks/Shipping/ShipSense/Packaging/IPackageAdapter.cs
@@ -0,0 +1,41 @@
+﻿using Interapptive.Shared.Enums;
+
+namespace ShipWorks.Shipping.ShipSense.Packaging
+{
+    /// <summary>
+    /// An interface intended to be used for shuffling package data between 
+    /// a ShipSense knowledge base entry and carriers.
+    /// </summary>
+    public interface IPackageAdapter
+    {
+        /// <summary>
+        /// Gets or sets the length.
+        /// </summary>
+        double Length { get; set; }
+
+        /// <summary>
+        /// Gets or sets the width.
+        /// </summary>
+        double Width { get; set; }
+
+        /// <summary>
+        /// Gets or sets the height.
+        /// </summary>
+        double Height { get; set; }
+
+        /// <summary>
+        /// Gets or sets the weight.
+        /// </summary>
+        double Weight { get; set; }
+
+        /// <summary>
+        /// Gets or sets the weight unit of measure.
+        /// </summary>
+        WeightUnitOfMeasure WeightUnitOfMeasure { get; set; }
+
+        /// <summary>
+        /// Gets or sets the additional weight.
+        /// </summary>
+        double AdditionalWeight { get; set; }
+    }
+}
