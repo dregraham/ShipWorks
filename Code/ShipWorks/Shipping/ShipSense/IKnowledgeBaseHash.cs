cc222ea3c1ef65d294f82b76ec699ecbf030a70e
John Yedlicka <j.yedlicka@shipworks.com>
2014-03-20 11:46:50 -0500

    Add interface for computing the knowledge base hash

---
+++ Code/ShipWorks/Shipping/ShipSense/IKnowledgeBaseHash.cs
@@ -0,0 +1,15 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping.ShipSense
+{
+    public interface IKnowledgeBaseHash
+    {
+        /// <summary>
+        /// Uses the data in the order to compute a hash to identify an entry in the 
+        /// ShipSense knowledge base.
+        /// </summary>
+        /// <param name="order">The order.</param>
+        /// <returns>A string value of the computed hash.</returns>
+        string ComputeHash(OrderEntity order);
+    }
+}
