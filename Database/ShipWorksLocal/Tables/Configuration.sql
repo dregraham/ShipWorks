24fad91ccb93641c622d7c35b21040cc4b51f1ca
Kevin Croke <k.croke@shipworks.com>
2014-03-24 14:59:23 -0500

    Adding ShipworksLocal sql project and update script database

---
+++ Database/ShipWorksLocal/Tables/Configuration.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Configuration] (
+    [ConfigurationID]        BIT        NOT NULL,
+    [RowVersion]             ROWVERSION NOT NULL,
+    [LogOnMethod]            INT        NOT NULL,
+    [AddressCasing]          BIT        NOT NULL,
+    [CustomerCompareEmail]   BIT        NOT NULL,
+    [CustomerCompareAddress] BIT        NOT NULL,
+    [CustomerUpdateBilling]  BIT        NOT NULL,
+    [CustomerUpdateShipping] BIT        NOT NULL,
+    [AuditNewOrders]         BIT        NOT NULL,
+    [AuditDeletedOrders]     BIT        NOT NULL,
+    CONSTRAINT [PK_Configuration] PRIMARY KEY CLUSTERED ([ConfigurationID] ASC)
+);
+
---
+++ Database/ShipWorksLocal/Tables/FilterNodeUpdatePending.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[FilterNodeUpdatePending] (
+    [FilterNodeContentID] BIGINT         NOT NULL,
+    [FilterTarget]        INT            NOT NULL,
+    [ColumnMask]          VARBINARY (75) NOT NULL,
+    [JoinMask]            INT            NOT NULL,
+    [Position]            INT            NOT NULL
+);
+
