39683abbd79bd21c2c0110eec743b3f090fbe059
John Yedlicka <j.yedlicka@shipworks.com>
2014-03-26 12:12:57 -0500

    Create a condition for filtering on ShipSense status

---
+++ Code/ShipWorks/Filters/Content/Conditions/Shipments/ShipSenseStatusCondition.cs
@@ -0,0 +1,26 @@
+﻿using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content.SqlGeneration;
+using ShipWorks.Shipping.ShipSense;
+
+namespace ShipWorks.Filters.Content.Conditions.Shipments
+{
+    [ConditionElement("ShipSense Status", "Shipment.ShipSenseStatus")]
+    public class ShipSenseStatusCondition : EnumCondition<ShipSenseStatus>
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ShipSenseStatusCondition()
+        {
+            Value = ShipSenseStatus.NotApplied;
+        }
+
+        /// <summary>
+        /// Generate the sql
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            return GenerateSql(context.GetColumnReference(ShipmentFields.ShipSenseStatus), context);
+        }
+    }
+}
