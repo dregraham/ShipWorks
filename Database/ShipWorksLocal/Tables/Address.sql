a899605fad206aae642bba208dd2b25cb558d50c
Kevin Croke <k.croke@shipworks.com>
2014-03-28 16:14:17 -0500

    Data changes to address validation.

---
+++ Database/ShipWorksLocal/Tables/Address.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[AddressID] BIGINT NOT NULL IDENTITY(1101, 1000),
+	[Street1]           NVARCHAR (60)  NOT NULL,
+    [Street2]           NVARCHAR (60)  NOT NULL,
+    [Street3]           NVARCHAR (60)  NOT NULL,
+    [City]              NVARCHAR (50)  NOT NULL,
+    [StateProvCode]     NVARCHAR (50)  NOT NULL,
+    [PostalCode]        NVARCHAR (20)  NOT NULL,
+    [CountryCode]       NVARCHAR (50)  NOT NULL,
+	CONSTRAINT [PK_Address] PRIMARY KEY CLUSTERED ([AddressID] ASC)
+)
---
+++ Database/ShipWorksLocal/Tables/ValidatedAddress.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ValidatedAddress]
+(
+	[ValidatedAddressID] BIGINT NOT NULL IDENTITY(1100, 1000),
+	[ConsumerID] BIGINT NOT NULL, 
+    [IsOriginal] BIT NOT NULL, 
+    CONSTRAINT [PK_ValidatedAddress] PRIMARY KEY CLUSTERED ([ValidatedAddressID] ASC)
+)
