c2849038107d209c66e6841460bf1d8c05ff2881
Andrew Benz <a.benz@shipworks.com>
2014-03-31 10:18:33 -0500

    Add AddressValidationException

---
+++ Code/ShipWorks/AddressValidation/AddressValidationException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ShipWorks.AddressValidation
+{
+    /// <summary>
+    /// Thrown when a known error occurs during address validation
+    /// </summary>
+    [Serializable]
+    public class AddressValidationException : Exception
+    {
+        /// <summary>
+        /// Creates a new AddressValidationException
+        /// </summary>
+        public AddressValidationException()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new AddressValidationException with the specified message
+        /// </summary>
+        public AddressValidationException(string message)
+            : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new AddressValidationException with the specified message an inner exception
+        /// </summary>
+        public AddressValidationException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+    }
+}
