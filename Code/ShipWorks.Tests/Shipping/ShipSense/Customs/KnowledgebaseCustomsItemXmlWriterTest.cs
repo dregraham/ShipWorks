288c3512e8912f8458740b16138f4e72c0aae88b
Tim Hughes <t.hughes@shipworks.com>
2014-03-31 08:19:45 -0500

    Added KnowledgeBaseCustomsItemWriter and test class

---
+++ Code/ShipWorks.Tests/Shipping/ShipSense/Customs/KnowledgebaseCustomsItemXmlWriterTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShipWorks.Shipping.ShipSense.Customs;
+using ShipWorks.Shipping.ShipSense.Packaging;
+
+namespace ShipWorks.Tests.Shipping.ShipSense.Customs
+{
+    [TestClass]
+    public class KnowledgebaseCustomsItemXmlWriterTest
+    {
+        private KnowledgebaseCustomsItemXmlWriter testObject;
+
+        private List<KnowledgebaseCustomsItem> beforeCustomsItems;
+        private List<KnowledgebaseCustomsItem> afterCustomsItems;
+        private XElement changeSetElement;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            beforeCustomsItems = new List<KnowledgebaseCustomsItem>
+            {
+                new KnowledgebaseCustomsItem() { CountryOfOrigin = "US", Description = "Item1 Desc", HarmonizedCode = "hc1", NumberOfPieces = 1, Weight = 1, Quantity = 1, UnitPriceAmount = 1, UnitValue = 1},
+                new KnowledgebaseCustomsItem() { CountryOfOrigin = "US", Description = "Item2 Desc", HarmonizedCode = "hc2", NumberOfPieces = 2, Weight = 2, Quantity = 2, UnitPriceAmount = 2, UnitValue = 2},
+                new KnowledgebaseCustomsItem() { CountryOfOrigin = "US", Description = "Item3 Desc", HarmonizedCode = "hc3", NumberOfPieces = 3, Weight = 3, Quantity = 3, UnitPriceAmount = 3, UnitValue = 3}
+            };
+
+            afterCustomsItems = new List<KnowledgebaseCustomsItem>
+            {
+                new KnowledgebaseCustomsItem() { CountryOfOrigin = "US", Description = "Item1 Desc", HarmonizedCode = "hc1", NumberOfPieces = 1, Weight = 1, Quantity = 1, UnitPriceAmount = 1, UnitValue = 1},
+                new KnowledgebaseCustomsItem() { CountryOfOrigin = "US", Description = "Item2 Desc", HarmonizedCode = "hc2", NumberOfPieces = 2, Weight = 2.2, Quantity = 2, UnitPriceAmount = 2, UnitValue = 2},
+                new KnowledgebaseCustomsItem() { CountryOfOrigin = "US", Description = "Item3 Desc", HarmonizedCode = "hc3", NumberOfPieces = 3, Weight = 3.3, Quantity = 3, UnitPriceAmount = 3, UnitValue = 3},
+                new KnowledgebaseCustomsItem() { CountryOfOrigin = "US", Description = "Item4 Desc", HarmonizedCode = "hc4", NumberOfPieces = 4, Weight = 4.4, Quantity = 4, UnitPriceAmount = 4, UnitValue = 4}
+            };
+
+            changeSetElement = new XElement("ChangeSet");
+        }
+
+        [TestMethod]
+        public void Write_AddsCustomsItemsNodeToChangeSet_Test()
+        {
+            testObject = new KnowledgebaseCustomsItemXmlWriter(beforeCustomsItems, afterCustomsItems);
+            testObject.Write(changeSetElement);
+
+            Assert.AreEqual(1, changeSetElement.Descendants("CustomsItems").Count());
+        }
+
+        [TestMethod]
+        public void Write_AddsBeforeAfterNodesToCustomsItems_Test()
+        {
+            testObject = new KnowledgebaseCustomsItemXmlWriter(beforeCustomsItems, afterCustomsItems);
+            testObject.Write(changeSetElement);
+
+            Assert.AreEqual(1, changeSetElement.Descendants("Before").Count());
+            Assert.AreEqual(1, changeSetElement.Descendants("After").Count());
+        }
+
+        [TestMethod]
+        public void Write_BeforeItemsMatchXml_Test()
+        {
+            testObject = new KnowledgebaseCustomsItemXmlWriter(beforeCustomsItems, afterCustomsItems);
+            testObject.Write(changeSetElement);
+
+            for (int i = 0; i < beforeCustomsItems.Count; i++)
+            {
+                XElement beforeXElement = changeSetElement.Descendants("Before").Descendants("CustomsItem").ElementAt(i);
+
+                Assert.AreEqual(beforeCustomsItems[i].CountryOfOrigin, beforeXElement.Descendants("CountryOfOrigin").First().Value);
+                Assert.AreEqual(beforeCustomsItems[i].Description, beforeXElement.Descendants("Description").First().Value);
+                Assert.AreEqual(beforeCustomsItems[i].HarmonizedCode, beforeXElement.Descendants("HarmonizedCode").First().Value);
+                Assert.AreEqual(beforeCustomsItems[i].NumberOfPieces.ToString(), beforeXElement.Descendants("NumberOfPieces").First().Value);
+                Assert.AreEqual(beforeCustomsItems[i].Quantity.ToString(), beforeXElement.Descendants("Quantity").First().Value);
+                Assert.AreEqual(beforeCustomsItems[i].UnitPriceAmount.ToString(), beforeXElement.Descendants("UnitPriceAmount").First().Value);
+                Assert.AreEqual(beforeCustomsItems[i].UnitValue.ToString(), beforeXElement.Descendants("UnitValue").First().Value);
+                Assert.AreEqual(beforeCustomsItems[i].Weight.ToString(), beforeXElement.Descendants("Weight").First().Value);    
+            }
+        }
+
+        [TestMethod]
+        public void Write_AfterItemsMatchXml_Test()
+        {
+            testObject = new KnowledgebaseCustomsItemXmlWriter(beforeCustomsItems, afterCustomsItems);
+            testObject.Write(changeSetElement);
+
+            for (int i = 0; i < afterCustomsItems.Count; i++)
+            {
+                XElement afterXElement = changeSetElement.Descendants("After").Descendants("CustomsItem").ElementAt(i);
+
+                Assert.AreEqual(afterCustomsItems[i].CountryOfOrigin, afterXElement.Descendants("CountryOfOrigin").First().Value);
+                Assert.AreEqual(afterCustomsItems[i].Description, afterXElement.Descendants("Description").First().Value);
+                Assert.AreEqual(afterCustomsItems[i].HarmonizedCode, afterXElement.Descendants("HarmonizedCode").First().Value);
+                Assert.AreEqual(afterCustomsItems[i].NumberOfPieces.ToString(), afterXElement.Descendants("NumberOfPieces").First().Value);
+                Assert.AreEqual(afterCustomsItems[i].Quantity.ToString(), afterXElement.Descendants("Quantity").First().Value);
+                Assert.AreEqual(afterCustomsItems[i].UnitPriceAmount.ToString(), afterXElement.Descendants("UnitPriceAmount").First().Value);
+                Assert.AreEqual(afterCustomsItems[i].UnitValue.ToString(), afterXElement.Descendants("UnitValue").First().Value);
+                Assert.AreEqual(afterCustomsItems[i].Weight.ToString(), afterXElement.Descendants("Weight").First().Value);
+            }
+        }
+
+        [TestMethod]
+        public void Write_DoesNotWriteAnyHash_Test()
+        {
+            testObject = new KnowledgebaseCustomsItemXmlWriter(beforeCustomsItems, afterCustomsItems);
+            testObject.Write(changeSetElement);
+
+            Assert.AreEqual(0, changeSetElement.Descendants("CustomsItems").Descendants("Hash").Count());
+        }
+    }
+}
---
+++ Code/ShipWorks/Shipping/ShipSense/Customs/KnowledgebaseCustomsItemXmlWriter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ShipWorks.Shipping.ShipSense.Customs
+{
+    /// <summary>
+    /// Xml write for customs items
+    /// </summary>
+    public class KnowledgebaseCustomsItemXmlWriter : IChangeSetXmlWriter
+    {
+        private IEnumerable<KnowledgebaseCustomsItem> before;
+        private IEnumerable<KnowledgebaseCustomsItem> after;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="before">List of kb customs items before any changes.</param>
+        /// <param name="after">List of kb customs items after changes applied.</param>
+        public KnowledgebaseCustomsItemXmlWriter(IEnumerable<KnowledgebaseCustomsItem> before, IEnumerable<KnowledgebaseCustomsItem> after)
+        {
+            this.before = before;
+            this.after = after;
+        }
+
+        /// <summary>
+        /// This method appends the XML representation of the ChangeSet to the given XElement.
+        /// </summary>
+        public void Write(XElement changeSetXElement)
+        {
+            // Create the before customs items
+            XElement beforeCustomsElements = new XElement("Before",
+                from kbciBefore in before
+                select CreateCustomsItem(kbciBefore));
+
+            // Create the after customs items
+            XElement afterCustomsElements = new XElement("After",
+                from kbciBefore in after
+                select CreateCustomsItem(kbciBefore));
+
+            // Create the before CustomsItems element with the before and after customs
+            XElement customsItems = new XElement("CustomsItems", beforeCustomsElements, afterCustomsElements);
+
+            changeSetXElement.Add(customsItems);
+        }
+
+        /// <summary>
+        /// Creates an XElement based on the given KnowledgebaseCustomsItem
+        /// </summary>
+        private static XElement CreateCustomsItem(KnowledgebaseCustomsItem knowledgebaseCustomsItem)
+        {
+            return new XElement("CustomsItem",
+                new XElement("Description", knowledgebaseCustomsItem.Description),
+                new XElement("CountryOfOrigin", knowledgebaseCustomsItem.CountryOfOrigin),
+                new XElement("HarmonizedCode", knowledgebaseCustomsItem.HarmonizedCode),
+                new XElement("NumberOfPieces", knowledgebaseCustomsItem.NumberOfPieces),
+                new XElement("Quantity", knowledgebaseCustomsItem.Quantity),
+                new XElement("UnitPriceAmount", knowledgebaseCustomsItem.UnitPriceAmount),
+                new XElement("UnitValue", knowledgebaseCustomsItem.UnitValue),
+                new XElement("Weight", knowledgebaseCustomsItem.Weight)
+                );
+        }
+    }
+}
