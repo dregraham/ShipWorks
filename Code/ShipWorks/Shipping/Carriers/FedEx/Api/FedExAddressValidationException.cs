4a36b77165b6b3ee1e31bf99063b606a320c32cb
Kevin Croke <k.croke@shipworks.com>
2014-04-01 17:31:51 -0500

    Fix crash when FedEx cannot determine residential or commercial.

---
+++ Code/ShipWorks/Shipping/Carriers/FedEx/Api/FedExAddressValidationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShipWorks.Shipping.Carriers.FedEx.Api
+{
+    public class FedExAddressValidationException : ShippingException
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FedExAddressValidationException"/> class.
+        /// </summary>
+        public FedExAddressValidationException(string message, Exception ex) : base(message, ex)
+        {}
+    }
+}
