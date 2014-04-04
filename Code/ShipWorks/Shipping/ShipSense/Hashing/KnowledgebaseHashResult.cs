5b2b93e386af932cb4f33eb386d259b758fc55ac
John Yedlicka <j.yedlicka@shipworks.com>
2014-04-04 08:54:30 -0500

    Ignore an order from the ShipSense knowledge base if any of its items do not have a valid uniqueness key
    - Refactored the IKnowledgebaseHash interface to return a KnowledgebaseHashResult
      that has an IsValid property to communicate the status of the result, so the
      Knowledgebase class doesn't have to try to infer what is and is not a valid
      hash based on the hashed string value.

---
+++ Code/ShipWorks/Shipping/ShipSense/Hashing/KnowledgebaseHashResult.cs
@@ -0,0 +1,34 @@
+﻿namespace ShipWorks.Shipping.ShipSense.Hashing
+{
+    /// <summary>
+    /// A class representing the result of hashing of a knowledge base entry. This
+    /// is to account for cases where there was insufficient data to accurately compute
+    /// the hash; consumers of the hash can easily tell whether the result is valid or not.
+    /// </summary>
+    public class KnowledgebaseHashResult
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KnowledgebaseHashResult"/> class.
+        /// </summary>
+        /// <param name="isValid">if set to <c>true</c> [is valid].</param>
+        /// <param name="hashValue">The hash value.</param>
+        public KnowledgebaseHashResult(bool isValid, string hashValue)
+        {
+            IsValid = isValid;
+            HashValue = hashValue;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the hash value is considered valid.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is valid; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsValid { get; private set; }
+
+        /// <summary>
+        /// Gets the hash value.
+        /// </summary>
+        public string HashValue { get; private set; }
+    }
+}
