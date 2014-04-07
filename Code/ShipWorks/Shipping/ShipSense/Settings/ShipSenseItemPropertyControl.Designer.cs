fa2361a9ae74f478b821243a9b3ac5f8ff2bcccc
John Yedlicka <j.yedlicka@shipworks.com>
2014-04-07 17:00:21 -0500

    Stub in the ShipSenesItemPropertyControl
    - Just adding the skeleton for the control, so the dialog can be hook into it

---
+++ Code/ShipWorks/Shipping/ShipSense/Settings/ShipSenseItemPropertyControl.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ShipWorks.Shipping.ShipSense.Settings
+{
+    partial class ShipSenseItemPropertyControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.itemSku = new System.Windows.Forms.CheckBox();
+            this.itemUnitCost = new System.Windows.Forms.CheckBox();
+            this.itemUnitPrice = new System.Windows.Forms.CheckBox();
+            this.itemLocation = new System.Windows.Forms.CheckBox();
+            this.itemISBN = new System.Windows.Forms.CheckBox();
+            this.itemDescription = new System.Windows.Forms.CheckBox();
+            this.itemUPC = new System.Windows.Forms.CheckBox();
+            this.itemName = new System.Windows.Forms.CheckBox();
+            this.itemCode = new System.Windows.Forms.CheckBox();
+            this.itemLocalStatus = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // itemSku
+            // 
+            this.itemSku.AutoSize = true;
+            this.itemSku.Location = new System.Drawing.Point(3, 3);
+            this.itemSku.Name = "itemSku";
+            this.itemSku.Size = new System.Drawing.Size(48, 17);
+            this.itemSku.TabIndex = 0;
+            this.itemSku.Text = "SKU";
+            this.itemSku.UseVisualStyleBackColor = true;
+            // 
+            // itemUnitCost
+            // 
+            this.itemUnitCost.AutoSize = true;
+            this.itemUnitCost.Location = new System.Drawing.Point(3, 187);
+            this.itemUnitCost.Name = "itemUnitCost";
+            this.itemUnitCost.Size = new System.Drawing.Size(69, 17);
+            this.itemUnitCost.TabIndex = 1;
+            this.itemUnitCost.Text = "Unit Cost";
+            this.itemUnitCost.UseVisualStyleBackColor = true;
+            // 
+            // itemUnitPrice
+            // 
+            this.itemUnitPrice.AutoSize = true;
+            this.itemUnitPrice.Location = new System.Drawing.Point(3, 164);
+            this.itemUnitPrice.Name = "itemUnitPrice";
+            this.itemUnitPrice.Size = new System.Drawing.Size(72, 17);
+            this.itemUnitPrice.TabIndex = 2;
+            this.itemUnitPrice.Text = "Unit Price";
+            this.itemUnitPrice.UseVisualStyleBackColor = true;
+            // 
+            // itemLocation
+            // 
+            this.itemLocation.AutoSize = true;
+            this.itemLocation.Location = new System.Drawing.Point(3, 141);
+            this.itemLocation.Name = "itemLocation";
+            this.itemLocation.Size = new System.Drawing.Size(67, 17);
+            this.itemLocation.TabIndex = 3;
+            this.itemLocation.Text = "Location";
+            this.itemLocation.UseVisualStyleBackColor = true;
+            // 
+            // itemISBN
+            // 
+            this.itemISBN.AutoSize = true;
+            this.itemISBN.Location = new System.Drawing.Point(3, 118);
+            this.itemISBN.Name = "itemISBN";
+            this.itemISBN.Size = new System.Drawing.Size(51, 17);
+            this.itemISBN.TabIndex = 4;
+            this.itemISBN.Text = "ISBN";
+            this.itemISBN.UseVisualStyleBackColor = true;
+            // 
+            // itemDescription
+            // 
+            this.itemDescription.AutoSize = true;
+            this.itemDescription.Location = new System.Drawing.Point(3, 72);
+            this.itemDescription.Name = "itemDescription";
+            this.itemDescription.Size = new System.Drawing.Size(79, 17);
+            this.itemDescription.TabIndex = 5;
+            this.itemDescription.Text = "Description";
+            this.itemDescription.UseVisualStyleBackColor = true;
+            // 
+            // itemUPC
+            // 
+            this.itemUPC.AutoSize = true;
+            this.itemUPC.Location = new System.Drawing.Point(3, 95);
+            this.itemUPC.Name = "itemUPC";
+            this.itemUPC.Size = new System.Drawing.Size(48, 17);
+            this.itemUPC.TabIndex = 6;
+            this.itemUPC.Text = "UPC";
+            this.itemUPC.UseVisualStyleBackColor = true;
+            // 
+            // itemName
+            // 
+            this.itemName.AutoSize = true;
+            this.itemName.Location = new System.Drawing.Point(3, 49);
+            this.itemName.Name = "itemName";
+            this.itemName.Size = new System.Drawing.Size(54, 17);
+            this.itemName.TabIndex = 7;
+            this.itemName.Text = "Name";
+            this.itemName.UseVisualStyleBackColor = true;
+            // 
+            // itemCode
+            // 
+            this.itemCode.AutoSize = true;
+            this.itemCode.Location = new System.Drawing.Point(3, 26);
+            this.itemCode.Name = "itemCode";
+            this.itemCode.Size = new System.Drawing.Size(51, 17);
+            this.itemCode.TabIndex = 8;
+            this.itemCode.Text = "Code";
+            this.itemCode.UseVisualStyleBackColor = true;
+            // 
+            // itemLocalStatus
+            // 
+            this.itemLocalStatus.AutoSize = true;
+            this.itemLocalStatus.Location = new System.Drawing.Point(3, 210);
+            this.itemLocalStatus.Name = "itemLocalStatus";
+            this.itemLocalStatus.Size = new System.Drawing.Size(85, 17);
+            this.itemLocalStatus.TabIndex = 9;
+            this.itemLocalStatus.Text = "Local Status";
+            this.itemLocalStatus.UseVisualStyleBackColor = true;
+            // 
+            // ShipSenseItemPropertyControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.itemLocalStatus);
+            this.Controls.Add(this.itemCode);
+            this.Controls.Add(this.itemName);
+            this.Controls.Add(this.itemUPC);
+            this.Controls.Add(this.itemDescription);
+            this.Controls.Add(this.itemISBN);
+            this.Controls.Add(this.itemLocation);
+            this.Controls.Add(this.itemUnitPrice);
+            this.Controls.Add(this.itemUnitCost);
+            this.Controls.Add(this.itemSku);
+            this.Name = "ShipSenseItemPropertyControl";
+            this.Size = new System.Drawing.Size(187, 235);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox itemSku;
+        private System.Windows.Forms.CheckBox itemUnitCost;
+        private System.Windows.Forms.CheckBox itemUnitPrice;
+        private System.Windows.Forms.CheckBox itemLocation;
+        private System.Windows.Forms.CheckBox itemISBN;
+        private System.Windows.Forms.CheckBox itemDescription;
+        private System.Windows.Forms.CheckBox itemUPC;
+        private System.Windows.Forms.CheckBox itemName;
+        private System.Windows.Forms.CheckBox itemCode;
+        private System.Windows.Forms.CheckBox itemLocalStatus;
+    }
+}
---
+++ Code/ShipWorks/Shipping/ShipSense/Settings/ShipSenseItemPropertyControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShipWorks.Shipping.ShipSense.Settings
+{
+    public partial class ShipSenseItemPropertyControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShipSenseItemPropertyControl"/> class.
+        /// </summary>
+        public ShipSenseItemPropertyControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Loads the selected properties.
+        /// </summary>
+        /// <param name="selectedProperties">The selected properties.</param>
+        public void LoadSelectedProperties(IEnumerable<string> selectedProperties)
+        { }
+
+        /// <summary>
+        /// Gets the selected item properties.
+        /// </summary>
+        public List<string> SelectedItemProperties
+        {
+            get
+            {
+                return new List<string>();
+            }
+        }
+    }
+}
