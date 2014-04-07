ebd4c232ad08a3ad7574389270c986c300cf8d00
John Yedlicka <j.yedlicka@shipworks.com>
2014-04-07 12:55:55 -0500

    Add ShipSenseOrderItemKey class
    - Implemented the class with a slight difference in the way the key is being
      built than what was originally discussed. The key is including the name and
      value of the property/attribute that was added to avoid potential collisions
      due to different data missing across items
    
    - For example, say item 1 has a missing SKU but the item code is 123 and
      item 2 (a separate item) has a missing code but a SKU of 123. Previously, this
      would have resulted in the same Key value being generated for both items: 123.
      Now, the key will accurately reflect the items: [SKU,123]|[Code,] for item 1
      and [SKU,]|[Code,123] for item 2

---
+++ Code/ShipWorks/Shipping/ShipSense/Hashing/ShipSenseOrderItemKey.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace ShipWorks.Shipping.ShipSense.Hashing
+{
+    /// <summary>
+    /// A class intended to uniquely identify an order item based on the quantity and a set of
+    /// property and attribute values.
+    /// </summary>
+    public class ShipSenseOrderItemKey
+    {
+        private readonly Dictionary<string, string> itemPropertyAttributeKeys;
+        private readonly double quantity;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShipSenseOrderItemKey" /> class.
+        /// </summary>
+        /// <param name="quantity">The quantity of a particular order item.</param>
+        public ShipSenseOrderItemKey(double quantity)
+        {
+            itemPropertyAttributeKeys = new Dictionary<string, string>();
+            this.quantity = quantity;
+        }
+        
+        /// <summary>
+        /// Gets a value that uniquely represents this instance of an order item based on the
+        /// identifier values and the quantity.
+        /// </summary>
+        public string Value
+        {
+            get
+            {
+                // Build a pipe delimited string from the identifier values and the quantity
+                string identiferSection = string.Join("|", itemPropertyAttributeKeys.Select(pair => string.Format("[{0},{1}]", pair.Key, pair.Value)));
+                return string.Join("|", identiferSection, quantity.ToString(CultureInfo.InvariantCulture));
+            }
+        }
+
+        /// <summary>
+        /// Adds a name/value pair that should be used to when constructing the key value for this instance.
+        /// </summary>
+        public void Add(string key, string value)
+        {
+            itemPropertyAttributeKeys[key] = value;
+        }
+
+        /// <summary>
+        /// Determines whether this instance is valid.
+        /// </summary>
+        public bool IsValid()
+        {
+            // For a key to be valid, at least one entry in the dictionary needs to have a value
+            return itemPropertyAttributeKeys.Any(pair => !string.IsNullOrWhiteSpace(pair.Value));
+        }
+    }
+}
