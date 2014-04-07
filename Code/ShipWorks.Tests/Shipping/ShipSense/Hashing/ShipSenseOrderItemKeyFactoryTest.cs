93f276adb2462ead21a2cdc56f599ec541f52802
John Yedlicka <j.yedlicka@shipworks.com>
2014-04-07 14:28:38 -0500

    Add ShipSenseOrderItemKeyFactory class
    - This is intended to encapsulate the logic for create instance of a
      ShipSenseOrderItemKey and will eventually be wired into the Knowledgebase
      class as a mechanism to hand off a collection of ShipSenseOrderItemKey values
      to be used when creating the hash.

---
+++ Code/ShipWorks.Tests/Shipping/ShipSense/Hashing/ShipSenseOrderItemKeyFactoryTest.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.ShipSense.Hashing;
+
+namespace ShipWorks.Tests.Shipping.ShipSense.Hashing
+{
+    [TestClass]
+    public class ShipSenseOrderItemKeyFactoryTest
+    {
+        private readonly ShipSenseOrderItemKeyFactory testObject;
+        
+        private List<OrderItemEntity> orderItems;
+
+        public ShipSenseOrderItemKeyFactoryTest()
+        {
+            testObject = new ShipSenseOrderItemKeyFactory();
+
+            orderItems = CreateOrderItems();
+            foreach (OrderItemEntity item in orderItems)
+            {
+                AddAttributes(item);
+            }
+        }
+
+        [TestMethod]
+        public void GetKeys_ReturnsKeyForEachOrderItem_WithAllPropertiesAdded_Test()
+        {
+            List<ShipSenseOrderItemKey> keys = testObject.GetKeys(orderItems, GetPropertyNames(), GetAttributeNames()).ToList();
+
+            Assert.AreEqual(3, keys.Count);
+        }
+
+        [TestMethod]
+        public void GetKeys_ReturnsKeys_WithAllPropertiesInSpecifiedPropertyListAdded_Test()
+        {
+            List<ShipSenseOrderItemKey> keys = testObject.GetKeys(orderItems, GetPropertyNames(), GetAttributeNames()).ToList();
+
+            Assert.AreEqual(3, keys.Count(k => k.Value.Contains("SKU")));
+            Assert.AreEqual(3, keys.Count(k => k.Value.Contains("Code")));
+            Assert.AreEqual(3, keys.Count(k => k.Value.Contains("Name")));
+        }
+
+        [TestMethod]
+        public void GetKeys_ReturnsKeys_WithProperties_OrderedByPropertyName_Test()
+        {
+            List<ShipSenseOrderItemKey> keys = testObject.GetKeys(orderItems, GetPropertyNames(), GetAttributeNames()).ToList();
+
+            // Properties should be in the order of Code, Name, SKU
+            int codeIndex = keys[0].Value.IndexOf("Code");
+            int nameIndex = keys[0].Value.IndexOf("Name");
+            int skuIndex = keys[0].Value.IndexOf("SKU");
+
+            Assert.IsTrue(codeIndex < nameIndex && nameIndex < skuIndex);
+        }
+
+        [TestMethod]
+        public void GetKeys_ReturnsKeys_WithPropertyValues_Test()
+        {
+            List<ShipSenseOrderItemKey> keys = testObject.GetKeys(orderItems, GetPropertyNames(), GetAttributeNames()).ToList();
+
+            List<string> values = keys.Select(k => k.Value).ToList();
+
+            // Keys were sorted by SKU in alpha
+            Assert.IsTrue(values[0].Contains("ABC"));
+            Assert.IsTrue(values[1].Contains("JKL"));
+            Assert.IsTrue(values[2].Contains("XYZ"));
+        }
+
+        [TestMethod]
+        public void GetKeys_ReturnsKeys_WithOnlyAttributesSpecifiedInAttributeListAdded_Test()
+        {
+            List<ShipSenseOrderItemKey> keys = testObject.GetKeys(orderItems, GetPropertyNames(), GetAttributeNames()).ToList();
+
+            Assert.AreEqual(3, keys.Count(k => k.Value.Contains("COLOR")));
+            Assert.AreEqual(3, keys.Count(k => k.Value.Contains("SIZE")));
+            Assert.AreEqual(0, keys.Count(k => k.Value.Contains("EDITION")));
+        }
+
+        [TestMethod]
+        public void GetKeys_ReturnsKeys_WithAttributeValues_Test()
+        {
+            List<ShipSenseOrderItemKey> keys = testObject.GetKeys(orderItems, GetPropertyNames(), GetAttributeNames()).ToList();
+
+            List<string> values = keys.Select(k => k.Value).ToList();
+
+            // Keys were sorted by SKU in alpha
+            Assert.AreEqual(3, values.Count(v => v.Contains("MEDIUM")));
+            Assert.AreEqual(3, values.Count(v => v.Contains("RED")));
+        }
+
+        [TestMethod]
+        public void GetKeys_ReturnsKeys_WithAttributes_OrderedByPropertyName_Test()
+        {
+            List<ShipSenseOrderItemKey> keys = testObject.GetKeys(orderItems, GetPropertyNames(), GetAttributeNames()).ToList();
+
+            // Properties should be in the order of Code, Name, SKU
+            int sizeIndex = keys[0].Value.IndexOf("SIZE");
+            int colorIndex = keys[0].Value.IndexOf("COLOR");
+
+            Assert.IsTrue(colorIndex < sizeIndex);
+        }
+
+        private List<string> GetPropertyNames()
+        {
+            return new List<string> { "SKU", "Code", "Name" };
+        }
+
+        private List<string> GetAttributeNames()
+        {
+            return new List<string> { "Size", "Color" };
+        }
+
+        private List<OrderItemEntity> CreateOrderItems()
+        {
+            return new List<OrderItemEntity>
+            {
+                new OrderItemEntity { SKU = "ABC", Code = "123", Name = "Item 1", Description = "The first item", ISBN = "110-123-324", UnitCost = 2.3M },
+                new OrderItemEntity { SKU = "XYZ", Code = "QRS", Name = "Another item", Description = "The second item", ISBN = "", UnitCost = 12.3M },
+                new OrderItemEntity { SKU = "JKL", Code = "TUV123", Name = "One more item", Description = "The third item", ISBN = "", UnitCost = 42.3M }
+            };
+        }
+
+        private void AddAttributes(OrderItemEntity orderItem)
+        {
+            OrderItemAttributeEntity sizeAttribute = new OrderItemAttributeEntity { Name = "Size", Description = "Medium" };
+            orderItem.OrderItemAttributes.Add(sizeAttribute);
+
+            OrderItemAttributeEntity colorAttribute = new OrderItemAttributeEntity { Name = "Color", Description = "Red" };
+            orderItem.OrderItemAttributes.Add(colorAttribute);
+
+            OrderItemAttributeEntity editionAttribute = new OrderItemAttributeEntity { Name = "Edition", Description = "Enterprise" };
+            orderItem.OrderItemAttributes.Add(editionAttribute);
+        }
+    }
+}
---
+++ Code/ShipWorks/Shipping/ShipSense/Hashing/ShipSenseOrderItemKeyFactory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping.ShipSense.Hashing
+{
+    /// <summary>
+    /// A factory for creating ShipSenseOrderItemKey objects.
+    /// </summary>
+    public class ShipSenseOrderItemKeyFactory : IShipSenseOrderItemKeyFactory
+    {
+        /// <summary>
+        /// Creates a list of ShipSenseOrderItemKey instances for the given order items. The ShipSenseOrderItemKey instances
+        /// will be configured with the names/values of the properties and attributes provided.
+        /// </summary>
+        public IEnumerable<ShipSenseOrderItemKey> GetKeys(IEnumerable<OrderItemEntity> orderItems, List<string> propertyNames, List<string> attributeNames)
+        {
+            List<ShipSenseOrderItemKey> keys = new List<ShipSenseOrderItemKey>();
+            
+            // Sort the order items, so the same keys are generated regardless of the sequence the order items are in
+            IEnumerable<OrderItemEntity> sortedItems = GetSortedOrderItems(orderItems);
+            foreach (OrderItemEntity item in sortedItems)
+            {
+                ShipSenseOrderItemKey key = new ShipSenseOrderItemKey(item.Quantity);
+
+                foreach (string property in propertyNames.OrderBy(p => p.ToUpperInvariant()))
+                {
+                    key.Add(property, item.Fields[property].CurrentValue.ToString());
+                }
+                
+                // Find the item attributes that match the list of attribute names provided.
+                List<OrderItemAttributeEntity> matchingOrderItemAttributes = (item.OrderItemAttributes
+                                                                                  .Join(attributeNames, oia => oia.Name.ToUpperInvariant(), name => name.ToUpperInvariant(), (oia, name) => oia))
+                                                                                  .ToList();
+
+                // Now that we have the matching attributes, we need to sort the attributes by name (so we always have the
+                // same order) and add each attribute name and description to our key
+                foreach (OrderItemAttributeEntity orderItemAttributeEntity in matchingOrderItemAttributes.OrderBy(attribute => attribute.Name))
+                {
+                    key.Add(orderItemAttributeEntity.Name.ToUpperInvariant(), orderItemAttributeEntity.Description.ToUpperInvariant());
+                }
+
+                keys.Add(key);
+            }
+
+            return keys;
+        }
+
+        /// <summary>
+        /// Gets the sorted order items. The actual sequence the criteria is applied is not really important 
+        /// here; we just need the items being ordered in a consistent manner.
+        /// </summary>
+        private static IEnumerable<OrderItemEntity> GetSortedOrderItems(IEnumerable<OrderItemEntity> orderItems)
+        {
+            return new List<OrderItemEntity>(orderItems).OrderBy(i => i.SKU)
+                                                        .ThenBy(i => i.Code).ThenBy(i => i.Name).ThenBy(i => i.ISBN).ThenBy(i => i.Description)
+                                                        .ThenBy(i => i.UPC).ThenBy(i => i.Location).ThenBy(i => i.UnitPrice).ThenBy(i => i.UnitCost)
+                                                        .ThenBy(i => i.LocalStatus);
+        }
+    }
+}
