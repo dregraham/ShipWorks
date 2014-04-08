0b733f9649539c8e58dd14a5d4844e779323c34f
John Yedlicka <j.yedlicka@shipworks.com>
2014-04-08 07:54:30 -0500

    Fill in the logic loading/returning the selected properties in the ShipSenseItemPropertyControl

--- Code/ShipWorks/Shipping/ShipSense/Settings/ShipSenseItemPropertyControl.cs
+++ Code/ShipWorks/Shipping/ShipSense/Settings/ShipSenseItemPropertyControl.cs
@@ -9,31 +9,70 @@ using System.Windows.Forms;
 
 namespace ShipWorks.Shipping.ShipSense.Settings
 {
+    /// <summary>
+    /// A UserControl for selecting which properties of an OrderItemEntity should be used when
+    /// determining unique ShipSense knowledge base entries.
+    /// </summary>
     public partial class ShipSenseItemPropertyControl : UserControl
     {
+        private Dictionary<string, CheckBox> itemPropertyCheckboxMap;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ShipSenseItemPropertyControl"/> class.
         /// </summary>
         public ShipSenseItemPropertyControl()
         {
             InitializeComponent();
+
+            InitializeDictionary();
         }
 
         /// <summary>
-        /// Loads the selected properties.
+        /// Initializes the entries in the dictionary, so that each check box matches up 
+        /// with the name of the corresponding property of the OrderItemEntity.
         /// </summary>
-        /// <param name="selectedProperties">The selected properties.</param>
-        public void LoadSelectedProperties(IEnumerable<string> selectedProperties)
-        { }
+        private void InitializeDictionary()
+        {
+            itemPropertyCheckboxMap = new Dictionary<string, CheckBox>();
+
+            // The dictionary key needs to match the name of the corresponding 
+            // property of the OrderEntityItem
+            itemPropertyCheckboxMap.Add("SKU", itemSku);
+            itemPropertyCheckboxMap.Add("Code", itemCode);
+            itemPropertyCheckboxMap.Add("Name", itemName);
+            itemPropertyCheckboxMap.Add("Description", itemDescription);
+            itemPropertyCheckboxMap.Add("UPC", itemUPC);
+            itemPropertyCheckboxMap.Add("ISBN", itemISBN);
+            itemPropertyCheckboxMap.Add("Location", itemLocation);
+            itemPropertyCheckboxMap.Add("UnitPrice", itemUnitPrice);
+            itemPropertyCheckboxMap.Add("UnitCost", itemUnitPrice);
+            itemPropertyCheckboxMap.Add("LocalStatus", itemLocalStatus);
+        }
 
         /// <summary>
-        /// Gets the selected item properties.
+        /// Uses the collection of property names to determine which check boxes are 
+        /// selected/checked.
         /// </summary>
-        public List<string> SelectedItemProperties
+        /// <param name="propertyNames">The properties names that should be selected/checked.</param>
+        public void LoadSelectedProperties(IEnumerable<string> propertyNames)
+        {
+            foreach (string property in propertyNames)
+            {
+                if (itemPropertyCheckboxMap.ContainsKey(property))
+                {
+                    itemPropertyCheckboxMap[property].Checked = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the corresponding OrderItemEntity property names that have been selected.
+        /// </summary>
+        public IEnumerable<string> SelectedItemProperties
         {
             get
             {
-                return new List<string>();
+                return itemPropertyCheckboxMap.Where(pair => pair.Value.Checked).Select(pair => pair.Key);
             }
         }
     }
