424b6cb43795e912e4488d5b26ceb9b0414097bd
Tim Hughes <t.hughes@shipworks.com>
2014-04-17 11:54:56 -0500

    Added Order table trigger, OrderShipmentShipSenseStatusUpdate Implement the matrix for updating the Shipment.ShipSenseStatus value for an order's unprocessed shipments.
                                        Matches KB Hash                        Does NOT Match KB Hash
    ------------------------------------------------------------------------------------------------
    
    Matches                         SSS = Applied                                     SSS = Not Applied
    compressed
    
    ------------------------------------------------------------------------------------------------
    
    Does not                      SSS = Overwritten                               SSS = Not Applied
    match
    compressed

---
+++ Code/ShipWorks.SqlServer/Shipping/OrderShipmentShipSenseStatusUpdate.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Server;
+using ShipWorks.SqlServer.General;
+using ShipWorks.SqlServer.Common.Data;
+
+public partial class Triggers
+{
+    [SqlTrigger(Target = "Order", Event = "FOR UPDATE")]
+    public static void OrderShipmentShipSenseStatusUpdate()
+    {
+        // Attach to the connection
+        using (SqlConnection con = new SqlConnection("Context connection = true"))
+        {
+            con.Open();
+
+            string sql = @"
+	            -- If we do not match a ShipSenseKnowledgeBase Hash, then always update the shipment shipsense status to 0, Not Applied
+	            -- for all shipments assigned to the inserted order
+	            if not exists (select * from ShipSenseKnowledgeBase sskb, inserted i where sskb.[Hash] = i.ShipSenseHashKey)
+	            begin
+		            update s
+		            set s.ShipSenseStatus = 0
+		            from Shipment s, inserted i
+		            where s.OrderID = i.OrderID
+		                and s.Processed = 0
+		                and s.ShipSenseStatus <> 0
+	            end
+	            else
+	            begin
+		            -- We have a ShipSenseKnowledgeBase entry based on Hash, so now we check shipments for the inserted order
+		            update s
+		            set s.ShipSenseStatus =
+			            case
+				            when sskb.Entry = s.ShipSenseEntry then 1
+				            else 2
+			            end
+		            from Shipment s
+			            join
+			            inserted i on i.OrderID = s.OrderID
+			            join
+			            ShipSenseKnowledgeBase sskb with(nolock) on sskb.Hash = i.ShipSenseHashKey
+		            where s.Processed = 0
+		                and s.ShipSenseStatus <> 0
+	            end
+                        ";
+
+            using (SqlCommand sqlCommand = con.CreateCommand())
+            {
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandText = sql;
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
