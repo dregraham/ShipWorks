9096440671e6a403536e4a175a2c29ea20bbcb4b
Tim Hughes <t.hughes@shipworks.com>
2014-04-18 09:22:38 -0500

    Added ShipSensible condition for filters.

---
+++ Code/ShipWorks/Filters/Content/Conditions/Orders/OrderShipSensibleCondition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+using ShipWorks.Filters.Content.SqlGeneration;
+
+namespace ShipWorks.Filters.Content.Conditions.Orders
+{
+    /// <summary>
+    ///     Filter for OrderShipSensibleCondition
+    /// </summary>
+    [ConditionElement("ShipSensible", "Order.ShipSensible")]
+    internal class OrderShipSensibleCondition : BooleanCondition
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="OrderShipSensibleCondition" /> class.
+        /// </summary>
+        public OrderShipSensibleCondition()
+            : base("Yes", "No")
+        {
+            Value = false;
+        }
+
+        /// <summary>
+        ///     Generate the SQL for the condition clement
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context", "context required");
+            }
+
+            return base.GenerateSql(context.GetColumnReference(OrderFields.ShipSensible), context);
+        }
+    }
+}
