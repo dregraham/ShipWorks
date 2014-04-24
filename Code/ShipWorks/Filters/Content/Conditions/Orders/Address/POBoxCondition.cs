76d1a7d22786d71f59835171eee3edf0f49b151c
Andrew Benz <a.benz@shipworks.com>
2014-04-24 14:02:25 -0500

    Add filter conditions for the extra address detail fields

---
+++ Code/ShipWorks/Filters/Content/Conditions/Orders/Address/POBoxCondition.cs
@@ -0,0 +1,29 @@
+﻿using ShipWorks.AddressValidation;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content.SqlGeneration;
+
+namespace ShipWorks.Filters.Content.Conditions.Orders.Address
+{
+    /// <summary>
+    /// Condition that compares against the PO Box field of an order's shipping address
+    /// </summary>
+    [ConditionElement("PO Box", "Order.Address.POBox")]
+    public class POBoxCondition : EnumCondition<POBoxType>
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public POBoxCondition()
+        {
+            Value = POBoxType.Unknown;
+        }
+
+        /// <summary>
+        /// Generate the SQL
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            return GenerateSql(context.GetColumnReference(OrderFields.ShipPOBox), context);
+        }
+    }
+}
