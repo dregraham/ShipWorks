9f812766797fdfeebc0df49cf043a075a1743df4
Andrew Benz <a.benz@shipworks.com>
2014-04-24 14:43:53 -0500

    Update UPS and OnTrac to use 'From Address Validation' as the default residential determination

--- Code/ShipWorks/Data/Administration/Scripts/Update/3.9/BestRateToAddressValidation.Data.sql
+++ Code/ShipWorks/Data/Administration/Scripts/Update/3.9/BestRateToAddressValidation.Data.sql
@@ -8,6 +8,22 @@ WHERE TypeCode = 1 OR TypeCode = 10 OR TypeCode = 18
 
 GO
 
+-- Update UPS profiles to use address validation for residential determination
+-- if it was using the default
+UPDATE UpsProfile
+SET ResidentialDetermination = 4
+WHERE ResidentialDetermination = 0
+
+GO
+
+-- Update OnTrac profiles to use address validation for residential determination
+-- if it was using the default
+UPDATE OnTracProfile
+SET ResidentialDetermination = 4
+WHERE ResidentialDetermination = 0
+
+GO
+
 PRINT N'Update data complete.';
 
 GO
