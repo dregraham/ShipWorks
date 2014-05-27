45362b99ce3a37573e2dc038f01cd1feab5f30d2
Tim Hughes <t.hughes@shipworks.com>
2014-05-27 11:36:30 -0500

    Added new FedEx package types

--- Code/ShipWorks.Tests/Shipping/Carriers/FedEx/Api/Shipping/Request/Manipulators/FedExPackagingTypeManipulatorTest.cs
+++ Code/ShipWorks.Tests/Shipping/Carriers/FedEx/Api/Shipping/Request/Manipulators/FedExPackagingTypeManipulatorTest.cs
@@ -38,5 +38,45 @@ namespace ShipWorks.Tests.Shipping.Carriers.FedEx.Api.Shipping.Request.Manipulat
             // Make sure we got a the same values back
             Assert.AreEqual(nativeRequest.RequestedShipment.PackagingType, PackagingType.FEDEX_BOX);
         }
+
+        [TestMethod]
+        public void Manipulate_FedExPackagingTypeManipulator_ReturnsExtraLargeBoxPackagingType_Test()
+        {
+            shipmentEntity.FedEx.PackagingType = (int)FedExPackagingType.ExtraLargeBox;
+            testObject.Manipulate(carrierRequest.Object);
+
+            // Make sure we got a the same values back
+            Assert.AreEqual(nativeRequest.RequestedShipment.PackagingType, PackagingType.FEDEX_EXTRA_LARGE_BOX);
+        }
+
+        [TestMethod]
+        public void Manipulate_FedExPackagingTypeManipulator_ReturnsSmallBoxPackagingType_Test()
+        {
+            shipmentEntity.FedEx.PackagingType = (int)FedExPackagingType.SmallBox;
+            testObject.Manipulate(carrierRequest.Object);
+
+            // Make sure we got a the same values back
+            Assert.AreEqual(nativeRequest.RequestedShipment.PackagingType, PackagingType.FEDEX_SMALL_BOX);
+        }
+
+        [TestMethod]
+        public void Manipulate_FedExPackagingTypeManipulator_ReturnsLargeBoxPackagingType_Test()
+        {
+            shipmentEntity.FedEx.PackagingType = (int)FedExPackagingType.LargeBox;
+            testObject.Manipulate(carrierRequest.Object);
+
+            // Make sure we got a the same values back
+            Assert.AreEqual(nativeRequest.RequestedShipment.PackagingType, PackagingType.FEDEX_LARGE_BOX);
+        }
+
+        [TestMethod]
+        public void Manipulate_FedExPackagingTypeManipulator_ReturnsMediumPackagingType_Test()
+        {
+            shipmentEntity.FedEx.PackagingType = (int)FedExPackagingType.MediumBox;
+            testObject.Manipulate(carrierRequest.Object);
+
+            // Make sure we got a the same values back
+            Assert.AreEqual(nativeRequest.RequestedShipment.PackagingType, PackagingType.FEDEX_MEDIUM_BOX);
+        }
     }
 }
