d12201863b00c1259298d840b1120c1468ea72c2
John Yedlicka <j.yedlicka@shipworks.com>
2014-06-02 10:09:19 -0500

    Remove file containing commented out class

--- Code/ShipWorks/Shipping/Carriers/FedEx/Api/Shipping/Request/CreatePendingShipmentRequest.cs
+++
@@ -1,8 +0,0 @@
-//using ShipWorks.Shipping.Carriers.FedEx.Api.Shipping.Request;
-
-//namespace ShipWorks.Shipping.Carriers.FedEx.WebServices.OpenShip
-//{
-//    public partial class CreatePendingShipmentRequest : IFedExNativeShipmentRequest
-//    {
-//    }
-//}
