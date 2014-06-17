1d0875e67775f6074f6552b26f8fa0b18525d760
Kevin Croke <k.croke@shipworks.com>
2014-06-17 08:18:59 -0500

    Update size and growth of initial database and tempdb when updating sql database.

--- Code/ShipWorks/Data/Administration/SqlSchemaUpdater.cs
+++ Code/ShipWorks/Data/Administration/SqlSchemaUpdater.cs
@@ -146,23 +146,99 @@ namespace ShipWorks.Data.Administration
 
                             ExistingConnectionScope.Commit();
 
-                            // If we were upgrading from 3.1.21 or before we adjust the FILEGROW settings.  Can't be in a transaction, so has to be here.
-                            if (installed < new Version(3, 1, 21, 0))
+                            // If we were upgrading from 3.9.0.4 or before we adjust the FILEGROW settings.  Can't be in a transaction, so has to be here.
+                            if (installed <= new Version(3, 9, 0, 4))
                             {
                                 ExistingConnectionScope.ExecuteWithCommand(cmd =>
                                 {
                                     cmd.CommandText = @"
 
                                         DECLARE @dbName nvarchar(100)
-                                        DECLARE @dataName nvarchar(100)
-                                        DECLARE @logName nvarchar(100)
+                                        DECLARE @isAutoShrink int
 
                                         SET @dbName = DB_NAME()
-                                        SELECT @dataName = name FROM sys.database_files WHERE type = 0
-                                        SELECT @logName = name FROM sys.database_files WHERE type = 1
 
-                                        EXECUTE ('ALTER DATABASE ' + @dbName + ' MODIFY FILE ( NAME = N''' + @dataName + ''', FILEGROWTH = 100MB)' )
-                                        EXECUTE ('ALTER DATABASE ' + @dbName + ' MODIFY FILE ( NAME = N''' + @logName + ''', FILEGROWTH = 100MB)' )";
+                                        SELECT @isAutoShrink = CONVERT(int,DATABASEPROPERTYEX([Name] , 'IsAutoShrink'))
+                                        FROM master.dbo.sysdatabases
+                                        where name = @dbName
+
+                                        IF(@isAutoShrink = 1)
+	                                        EXECUTE ('ALTER DATABASE ' + @dbName + ' SET AUTO_SHRINK OFF')";
+
+                                    cmd.ExecuteNonQuery();
+                                });
+
+                                // Update size and growth of shipworks database
+                                ExistingConnectionScope.ExecuteWithCommand(cmd =>
+                                {
+                                    cmd.CommandText = @"
+                                        DECLARE @logSize int 
+                                        DECLARE @dataSize int 
+                                        DECLARE @dataFileGrowth int 
+                                        DECLARE @logFileGrowth int
+                                        DECLARE @dataName nvarchar(100) 
+                                        DECLARE @logName nvarchar(100)
+                                        DECLARE @dbName nvarchar(100)
+
+                                        SET @dbName = DB_NAME()
+
+                                        SELECT @dataSize = SUM(CASE WHEN type_desc = 'ROWS' THEN size END),
+                                               @dataName = MAX(CASE WHEN type_desc = 'ROWS' THEN name END),
+                                               @dataFileGrowth = SUM(CASE WHEN type_desc = 'ROWS' AND is_percent_growth=1 THEN growth ELSE 0 END),
+                                               @logSize = SUM(CASE WHEN type_desc = 'LOG' THEN size END),
+                                               @logName = MAX(CASE WHEN type_desc = 'LOG' THEN name END),
+                                               @logFileGrowth = SUM(CASE WHEN type_desc = 'LOG' AND is_percent_growth=1 THEN growth ELSE 0 END)
+                                        FROM sys.master_files 
+                                        where DB_NAME(database_id) = @dbName
+
+                                        IF (@logSize < 25600)
+                                            EXECUTE ('ALTER DATABASE ' + @dbName + ' MODIFY FILE ( NAME = N''' + @logName + ''', SIZE = 200MB)' )
+
+                                        IF (@dataSize < 25600)
+                                            EXECUTE ('ALTER DATABASE ' + @dbName + ' MODIFY FILE ( NAME = N''' + @dataName + ''', SIZE = 200MB)' )
+
+                                        IF (@dataFileGrowth < 25600 OR @dataFileGrowth >= 64000)
+                                            EXECUTE ('ALTER DATABASE ' + @dbName + ' MODIFY FILE ( NAME = N''' + @dataName + ''', FILEGROWTH = 200MB)' )
+
+                                        IF (@logFileGrowth < 25600 OR @logFileGrowth >= 64000)
+                                            EXECUTE ('ALTER DATABASE ' + @dbName + ' MODIFY FILE ( NAME = N''' + @logName + ''', FILEGROWTH = 200MB)' )
+                                    ";
+
+                                    cmd.ExecuteNonQuery();
+                                });
+
+                                // Update size and growth of tempdb
+                                ExistingConnectionScope.ExecuteWithCommand(cmd =>
+                                {
+                                    cmd.CommandText = @"
+                                        DECLARE @logSize int 
+                                        DECLARE @dataSize int 
+                                        DECLARE @dataFileGrowth int 
+                                        DECLARE @logFileGrowth int
+                                        DECLARE @dataName nvarchar(100) 
+                                        DECLARE @logName nvarchar(100)
+
+                                        SELECT @dataSize = SUM(CASE WHEN type_desc = 'ROWS' THEN size END),
+	                                           @dataName = MAX(CASE WHEN type_desc = 'ROWS' THEN name END),
+	                                           @dataFileGrowth = SUM(CASE WHEN type_desc = 'ROWS' AND is_percent_growth=1 THEN growth ELSE 0 END),
+	                                           @logSize = SUM(CASE WHEN type_desc = 'LOG' THEN size END),
+	                                           @logName = MAX(CASE WHEN type_desc = 'LOG' THEN name END),
+	                                           @logFileGrowth = SUM(CASE WHEN type_desc = 'LOG' AND is_percent_growth=1 THEN growth ELSE 0 END)
+                                        FROM sys.master_files 
+                                        where DB_NAME(database_id) = 'tempdb'
+	 
+                                        IF (@logSize < 25600)
+                                            EXECUTE ('ALTER DATABASE tempdb MODIFY FILE ( NAME = N''' + @logName + ''', SIZE = 200MB)' )
+
+                                        IF (@dataSize < 25600)
+                                            EXECUTE ('ALTER DATABASE tempdb MODIFY FILE ( NAME = N''' + @dataName + ''', SIZE = 200MB)' )
+
+                                        IF (@dataFileGrowth < 25600)
+                                            EXECUTE ('ALTER DATABASE tempdb MODIFY FILE ( NAME = N''' + @dataName + ''', FILEGROWTH = 200MB)' )
+
+                                        IF (@logFileGrowth < 25600)
+                                            EXECUTE ('ALTER DATABASE tempdb MODIFY FILE ( NAME = N''' + @logName + ''', FILEGROWTH = 200MB)' )
+                                    ";
 
                                     cmd.ExecuteNonQuery();
                                 });
