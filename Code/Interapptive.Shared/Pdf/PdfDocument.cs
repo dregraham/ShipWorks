ba101f53aba9cce392f234ae9677eee9c3659eca
Tim Hughes <t.hughes@shipworks.com>
2014-06-19 16:41:06 -0500

    Added Apitron PDF Rastorizer dll Added IPdfDocument and PdfDocument to Interapptive.Shared.  It is used to convert a pdf into image(s)

---
+++ Code/Interapptive.Shared/Pdf/PdfDocument.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Apitron.PDF.Rasterizer;
+using Apitron.PDF.Rasterizer.Configuration;
+
+namespace Interapptive.Shared.Pdf
+{
+    /// <summary>
+    /// Implementation of IPdfDocument
+    /// </summary>
+    public class PdfDocument : IPdfDocument, IDisposable
+    {
+        private Document pdfDocument;
+        private List<Stream> images = new List<Stream>();
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="pdf">Stream representation of the pdf</param>
+        public PdfDocument(Stream pdf)
+        {
+            pdfDocument = new Document(pdf);
+        }
+
+        /// <summary>
+        /// Iterates through each page of the PDF and converts each page to an image.
+        /// </summary>
+        /// <returns>List of streams, for each page image.</returns>
+        public IEnumerable<Stream> ToImages()
+        {
+            // Iterate through each page, rendering each to an image
+            foreach (Page currentPage in pdfDocument.Pages)
+            {
+                // We use original page's width and height for image as well as default rendering settings
+                using (Bitmap bitmap = currentPage.Render((int)currentPage.Width, (int)currentPage.Height, new RenderingSettings()))
+                {
+                    Stream imageMemoryStream = new MemoryStream();
+                    bitmap.Save(imageMemoryStream, ImageFormat.Png);
+
+                    images.Add(imageMemoryStream);
+                }
+            }
+
+            return images;
+        }
+
+        /// <summary>
+        /// Dispose of managed resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Dispose of managed resources if disposing.
+        /// </summary>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // free managed resources
+                if (pdfDocument != null)
+                {
+                    pdfDocument.Dispose();
+                    pdfDocument = null;
+                }
+
+                if (images != null && images.Any())
+                {
+                    images.ForEach(image => image.Dispose());
+                }
+            }
+        }
+    }
+}
---
+++ Code/ShipWorks.Tests/Interapptive.Shared/Pdf/PdfDocumentTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Interapptive.Shared.Pdf;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rebex.Mail;
+
+namespace ShipWorks.Tests.Interapptive.Shared.Pdf
+{
+    /// <summary>
+    /// Test class to verify that the WeightUtility method(s) work correctly.
+    /// </summary>
+    [TestClass]
+    public class PdfDocumentTest
+    {
+        // Verify that a one to one conversion works correctly
+        [TestMethod]
+        public void Convert_SinglePagePdf_ReturnsOneImageStream_Test()
+        {
+            List<Stream> images;
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "ShipWorks.Tests.Resources.SinglePagePdf.pdf";
+    
+            using (Stream pdfFileStream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (PdfDocument pdfDocument = new PdfDocument(pdfFileStream))
+                {
+                    images = pdfDocument.ToImages().ToList();
+
+                    // Grab the first one and see if we can make it a bitmap.
+                    Bitmap bitmap = new Bitmap(images.First());
+                    bitmap.Dispose();
+                }
+            }
+
+            Assert.AreEqual(1, images.Count);
+        }
+
+        // Verify that a multipage conversion works correctly
+        [TestMethod]
+        public void Convert_MultiPagePdf_ReturnsMultipleImageStreams_Test()
+        {
+            List<Stream> images;
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "ShipWorks.Tests.Resources.MultiPagePdf.pdf";
+
+            using (Stream pdfFileStream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (PdfDocument pdfDocument = new PdfDocument(pdfFileStream))
+                {
+                    images = pdfDocument.ToImages().ToList();
+
+                    foreach (Stream imageStream in images)
+                    {
+                        // See if we can make the image into a bitmap.
+                        Bitmap bitmap = new Bitmap(images.First());
+                        bitmap.Dispose();
+                    }
+                }
+            }
+
+            Assert.AreEqual(9, images.Count);
+        }
+    }
+}
