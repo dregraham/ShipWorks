5ee0de97cb71f5eb2fb53ec55a9b3e812ba69d04
Kevin Croke <k.croke@shipworks.com>
2014-06-30 13:40:22 -0500

    Add FoxyCart Store Type

---
+++ Code/ShipWorks/Stores/Platforms/FoxyCart/FoxyCartStoreType.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.ApplicationCore.Logging;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.GenericModule;
+
+namespace ShipWorks.Stores.Platforms.FoxyCart
+{
+    /// <summary>
+    /// FoxyCart Store Type
+    /// </summary>
+    public class FoxyCartStoreType : GenericModuleStoreType
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FoxyCartStoreType"/> class.
+        /// </summary>
+        /// <param name="store"></param>
+        public FoxyCartStoreType(StoreEntity store)
+            : base(store)
+        {
+        }
+
+        /// <summary>
+        /// Gets the type code for this store
+        /// </summary>
+        public override StoreTypeCode TypeCode
+        {
+            get
+            {
+                return StoreTypeCode.FoxyCart;
+            }
+        }
+
+        /// <summary>
+        /// Get the log source
+        /// </summary>
+        public override ApiLogSource LogSource
+        {
+            get
+            {
+                return ApiLogSource.FoxyCart;
+            }
+        }
+    }
+}
