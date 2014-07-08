74d3b1fa003557a44e08ef766d0eae5b4dcd2b2d
Kevin Croke <k.croke@shipworks.com>
2014-07-08 13:17:40 -0500

    Install and Upgrade scripts for scheduling a reindex action.

--- Code/ShipWorks/Data/Administration/Scripts/Update/3.10/3.10.0.0.sql
+++ Code/ShipWorks/Data/Administration/Scripts/Update/3.10/3.10.0.0.sql
@@ -125,43 +125,6 @@ GO
 
 DECLARE @ActionID NVARCHAR(20)
 
--- Get dates in UTC
-DECLARE @StartDeleteTime DATETIME
-DECLARE @StartDeleteTimeString NVARCHAR(50)
-DECLARE @NextDeleteFireTime DATETIME
-DECLARE @NextDeleteFireTimeTicks BIGINT
-SET @StartDeleteTime = DATEADD(hour, 23, CONVERT(DATETIME, CONVERT(DATE, GETDATE())))
-SET @NextDeleteFireTime = DATEADD(d, 1, @StartDeleteTime)
-
--- Convert to UTC
-SET @StartDeleteTime = DATEADD(hh, DATEDIFF(hh, GETDATE(), GETUTCDATE()), @StartDeleteTime)
-SET @StartDeleteTimeString = CONVERT(NVARCHAR(50), @StartDeleteTime, 127) + 'Z'
-
--- Convert to UTC, and get ticks
-SET @NextDeleteFireTime = DATEADD(hh, DATEDIFF(hh, GETDATE(), GETUTCDATE()), @NextDeleteFireTime)
-SET @NextDeleteFireTimeTicks = dbo.GetTicksFromDateTime(@NextDeleteFireTime)
-
-INSERT INTO [dbo].[Action] ([Name], [Enabled], [ComputerLimitedType], [ComputerLimitedList], [StoreLimited], [StoreLimitedList], [TriggerType], [TriggerSettings], [TaskSummary], [InternalOwner]) 
-VALUES (N'Delete Old Data', 1, 0, '', 0, N'', 6, CONVERT(xml,N'<Settings><DailyActionSchedule><ScheduleType>2</ScheduleType><StartDateTimeInUtc>' + @StartDeleteTimeString  + '</StartDateTimeInUtc><FrequencyInDays>1</FrequencyInDays></DailyActionSchedule></Settings>',1), N'PurgeDatabase', NULL)
-SELECT @ActionID = CONVERT(NVARCHAR(20), SCOPE_IDENTITY())
-
-PRINT(N'Add 1 row to [dbo].[Scheduling_JOB_DETAILS]')
-INSERT INTO [dbo].[Scheduling_JOB_DETAILS] 
-([SCHED_NAME], [JOB_NAME], [JOB_GROUP], [DESCRIPTION], [JOB_CLASS_NAME], [IS_DURABLE], [IS_NONCONCURRENT], [IS_UPDATE_DATA], [REQUESTS_RECOVERY], [JOB_DATA]) 
-VALUES (N'QuartzScheduler', @ActionID, N'DEFAULT', NULL, N'ShipWorks.Actions.Scheduling.QuartzNet.ActionJob, ShipWorks', 0, 0, 0, 0, null)
-
-PRINT (N'Add 1 row to [dbo].[ActionTask]')
-INSERT INTO [dbo].[ActionTask] ([ActionID], [TaskIdentifier], [TaskSettings], [StepIndex], [InputSource], [InputFilterNodeID], [FilterCondition], [FilterConditionNodeID], [FlowSuccess], [FlowSkipped], [FlowError])
-VALUES (CONVERT(BIGINT, @ActionID), N'PurgeDatabase', CONVERT(xml, N'<Settings><CanTimeout value="True"/><TimeoutInHours value="6"/><RetentionPeriodInDays value="180"/><Purges><Item value="2"/><Item value="3"/><Item value="4"/></Purges><ReclaimDiskSpace value="True" /></Settings>', 1), 0, -1, -1, 0, -1, 0, 0, 0)
-
-PRINT(N'Add 1 row to [dbo].[Scheduling_TRIGGERS]')
-INSERT INTO [dbo].[Scheduling_TRIGGERS] ([SCHED_NAME], [TRIGGER_NAME], [TRIGGER_GROUP], [JOB_NAME], [JOB_GROUP], [DESCRIPTION], [NEXT_FIRE_TIME], [PREV_FIRE_TIME], [PRIORITY], [TRIGGER_STATE], [TRIGGER_TYPE], [START_TIME], [END_TIME], [CALENDAR_NAME], [MISFIRE_INSTR], [JOB_DATA]) 
-VALUES (N'QuartzScheduler', @ActionID, N'DEFAULT', @ActionID, N'DEFAULT', NULL, @NextDeleteFireTimeTicks, NULL, 5, N'WAITING', N'CAL_INT', @NextDeleteFireTimeTicks, NULL, NULL, 0, NULL)
-
-PRINT(N'Add 1 row to [dbo].[Scheduling_SIMPROP_TRIGGERS]')
-INSERT INTO [dbo].[Scheduling_SIMPROP_TRIGGERS] ([SCHED_NAME], [TRIGGER_NAME], [TRIGGER_GROUP], [STR_PROP_1], [STR_PROP_2], [STR_PROP_3], [INT_PROP_1], [INT_PROP_2], [LONG_PROP_1], [LONG_PROP_2], [DEC_PROP_1], [DEC_PROP_2], [BOOL_PROP_1], [BOOL_PROP_2]) 
-VALUES (N'QuartzScheduler', @ActionID, N'DEFAULT', N'Day', dbo.GetLocalTimezoneName(), NULL, 1, 0, 0, 0, 0.0000, 0.0000, 0, 0)
-GO
 
 -- Create RebuildTableIndex Action
 DECLARE @StartReindexTime DATETIME
@@ -191,7 +154,7 @@ VALUES (N'QuartzScheduler', @ActionID, N'DEFAULT', NULL, N'ShipWorks.Actions.Sch
 
 PRINT (N'Add 1 row to [dbo].[ActionTask]')
 INSERT INTO [dbo].[ActionTask] ([ActionID], [TaskIdentifier], [TaskSettings], [StepIndex], [InputSource], [InputFilterNodeID], [FilterCondition], [FilterConditionNodeID], [FlowSuccess], [FlowSkipped], [FlowError])
-VALUES (CONVERT(BIGINT, @ActionID), N'RebuildTableIndex', CONVERT(xml, N'<Settings><TimeoutInMinutes value="120"/></Settings>', 1), 0, -1, -1, 0, -1, 0, 0, 0)
+VALUES (CONVERT(BIGINT, @ActionID), N'RebuildTableIndex', CONVERT(xml, N'<Settings><TimeoutInHours value="120"/></Settings>', 1), 0, -1, -1, 0, -1, 0, 0, 0)
 
 PRINT(N'Add 1 row to [dbo].[Scheduling_TRIGGERS]')
 INSERT INTO [dbo].[Scheduling_TRIGGERS] ([SCHED_NAME], [TRIGGER_NAME], [TRIGGER_GROUP], [JOB_NAME], [JOB_GROUP], [DESCRIPTION], [NEXT_FIRE_TIME], [PREV_FIRE_TIME], [PRIORITY], [TRIGGER_STATE], [TRIGGER_TYPE], [START_TIME], [END_TIME], [CALENDAR_NAME], [MISFIRE_INSTR], [JOB_DATA]) 
@@ -200,16 +163,6 @@ VALUES (N'QuartzScheduler', @ActionID, N'DEFAULT', @ActionID, N'DEFAULT', NULL, 
 PRINT(N'Add 1 row to [dbo].[Scheduling_SIMPROP_TRIGGERS]')
 INSERT INTO [dbo].[Scheduling_SIMPROP_TRIGGERS] ([SCHED_NAME], [TRIGGER_NAME], [TRIGGER_GROUP], [STR_PROP_1], [STR_PROP_2], [STR_PROP_3], [INT_PROP_1], [INT_PROP_2], [LONG_PROP_1], [LONG_PROP_2], [DEC_PROP_1], [DEC_PROP_2], [BOOL_PROP_1], [BOOL_PROP_2]) 
 VALUES (N'QuartzScheduler', @ActionID, N'DEFAULT', N'Day', dbo.GetLocalTimezoneName(), NULL, 1, 0, 0, 0, 0.0000, 0.0000, 0, 0)
-GO
-
-
--- Create default best rate profile
-INSERT INTO [dbo].[ShippingProfile] ([Name], [ShipmentType], [ShipmentTypePrimary], [OriginID], [Insurance], [InsuranceInitialValueSource], [InsuranceInitialValueAmount], [ReturnShipment])
-VALUES ('Defaults - Best rate', 14, 1, 0, 0, 0, 0.00, 0)
-GO
-
-INSERT INTO [dbo].[BestRateProfile] ([ShippingProfileID], [DimsProfileID], [DimsLength], [DimsWidth], [DimsHeight], [DimsWeight], [DimsAddWeight], [Weight], [ServiceLevel])
-SELECT TOP 1 ShippingProfileID, 0, 0, 0, 0, 0, 0, 0, 0  FROM ShippingProfile WHERE ShipmentType = 14
 GO
 
 -- Cleanup functions
