0f0aa664a709c186357766d26c2e93a9f479c09b
Kevin Croke <k.croke@shipworks.com>
2014-07-08 10:45:45 -0500

    create TableIndex DTO

---
+++ Code/ShipWorks/Data/Administration/Indexing/TableIndex.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShipWorks.Data.Administration.Indexing
+{
+    /// <summary>
+    /// DTO to hold TableIndex Information.
+    /// </summary>
+    public class TableIndex
+    {
+        /// <summary>
+        /// Gets or sets the name of the table.
+        /// </summary>
+        public string TableName
+        {
+            get; 
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the index.
+        /// </summary>
+        public string IndexName
+        {
+            get; 
+            set;
+        }
+    }
+}
