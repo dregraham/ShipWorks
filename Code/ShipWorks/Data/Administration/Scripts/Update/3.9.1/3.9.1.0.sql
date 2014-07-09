20932ed43673d0c638575197bfe62ccec7239128
Tim Hughes <t.hughes@shipworks.com>
2014-07-09 08:32:36 -0500

    Rename a few indexes that had misleading names.

---
+++ Code/ShipWorks/Data/Administration/Scripts/Update/3.9.1/3.9.1.0.sql
@@ -0,0 +1,19 @@
+﻿
+
+if exists(select index_id from sys.indexes where name = 'IX_GridLayoutColumn' and object_id = OBJECT_ID('GridColumnPosition'))
+BEGIN
+	EXEC sp_rename N'dbo.[GridColumnPosition].[IX_GridLayoutColumn]', N'IX_GridColumnPosition_GridColumnLayoutIdColumn', N'INDEX';
+END
+GO
+
+if exists(select index_id from sys.indexes where name = 'PK_GridColumnLayout' and object_id = OBJECT_ID('GridColumnPosition'))
+BEGIN
+	EXEC sp_rename N'dbo.[GridColumnPosition].[PK_GridColumnLayout]', N'PK_GridColumnPosition', N'INDEX';
+END
+GO
+
+if exists(select index_id from sys.indexes where name = 'PK_GridLayout' and object_id = OBJECT_ID('GridColumnLayout'))
+BEGIN
+	EXEC sp_rename N'dbo.GridColumnLayout.PK_GridLayout', N'PK_GridColumnLayout', N'INDEX';
+END
+GO
