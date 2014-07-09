3fa07dd312a04339fbdc41b3032da002785f0b4e
Tim Hughes <t.hughes@shipworks.com>
2014-07-09 09:14:01 -0500

    Move 3.9.1 sql to the already existing 3.10.0.0

--- Code/ShipWorks/Data/Administration/Scripts/Update/3.10/3.10.0.0.sql
+++ Code/ShipWorks/Data/Administration/Scripts/Update/3.10/3.10.0.0.sql
@@ -173,6 +173,24 @@ DROP FUNCTION [dbo].[MonthToDays]
 DROP FUNCTION [dbo].[MonthToDays366]
 DROP FUNCTION [dbo].[MonthToDays365]
 DROP FUNCTION dbo.GetLocalTimezoneName
+GO
 
+if exists(select index_id from sys.indexes where name = 'IX_GridLayoutColumn' and object_id = OBJECT_ID('GridColumnPosition'))
+BEGIN
+	EXEC sp_rename N'dbo.[GridColumnPosition].[IX_GridLayoutColumn]', N'IX_GridColumnPosition_GridColumnLayoutIdColumn', N'INDEX';
+END
+GO
+
+if exists(select index_id from sys.indexes where name = 'PK_GridColumnLayout' and object_id = OBJECT_ID('GridColumnPosition'))
+BEGIN
+	EXEC sp_rename N'dbo.[GridColumnPosition].[PK_GridColumnLayout]', N'PK_GridColumnPosition', N'INDEX';
+END
+GO
+
+if exists(select index_id from sys.indexes where name = 'PK_GridLayout' and object_id = OBJECT_ID('GridColumnLayout'))
+BEGIN
+	EXEC sp_rename N'dbo.GridColumnLayout.PK_GridLayout', N'PK_GridColumnLayout', N'INDEX';
+END
+GO
 
 
