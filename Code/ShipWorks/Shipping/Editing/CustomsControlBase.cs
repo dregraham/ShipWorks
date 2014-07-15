3172713b816f31f04069c2063cf1407331f73f21
John Yedlicka <j.yedlicka@shipworks.com>
2014-07-15 09:56:42 -0500

    Fix for FD #538168
    - After ShipSense gets applied, the customs control is reloaded to keep
      the data in sync since the customs control does not have a reference to
      the actual shipments. Whenever customs items get reloaded, the first
      item in the list was getting  automatically selected. This is fine in most
      cases, because you don't want the user to have to select an item before
      editing it in the case where there is only one customs item. The problem
      arises when there are two or more customs items, and item 2...n is being
      edited; the shipment gets synced with ShipSense and reloads the customs
      items resulting the selection going back to the first item in the list.
    
    - Overloaded the LoadShipments method to allow the option to not
      automatically select the first row in the grid.
    
    - Added a method (RefreshItems) to the CustomsControl to just do an in-place refresh
      of the shipments/customs items already loaded in the control and
      setting the selection back to the previously selected rows.
    
    - Calling the RefreshItems method from the
      ShippingDlg.SynchornizeWithShipSense  instead of the LoadShipments
      method

--- Code/ShipWorks/Shipping/Editing/CustomsControlBase.cs
+++ Code/ShipWorks/Shipping/Editing/CustomsControlBase.cs
@@ -85,9 +85,18 @@ namespace ShipWorks.Shipping.Editing
         }
 
         /// <summary>
-        /// Load the given shipments customs information into the control
+        /// Load the given shipments customs information into the control and resets the selection
+        /// to the first item in the list (if there are any).
         /// </summary>
         public virtual void LoadShipments(IEnumerable<ShipmentEntity> shipments, bool enableEditing)
+        {
+            LoadShipments(shipments, enableEditing, true);
+        }
+
+        /// <summary>
+        /// Load the given shipments customs information into the control
+        /// </summary>
+        private void LoadShipments(IEnumerable<ShipmentEntity> shipments, bool enableEditing, bool resetSelection)
         {
             SuspendShipSenseFieldChangeEvent();
 
@@ -213,7 +222,10 @@ namespace ShipWorks.Shipping.Editing
 
             if (sandGrid.Rows.Count > 0)
             {
-                sandGrid.Rows[0].Selected = true;
+                if (resetSelection)
+                {
+                    sandGrid.Rows[0].Selected = true;
+                }
             }
             else
             {
@@ -221,6 +233,29 @@ namespace ShipWorks.Shipping.Editing
             }
 
             ResumeShipSenseFieldChangeEvent();
+        }
+
+        /// <summary>
+        /// Refreshes the grid contents using the shipments already loaded in the control. Any items
+        /// that were previously selected are retained.
+        /// </summary>
+        public void RefreshItems()
+        {
+            // Helps to retain selections when a shipment is synched with ShipSense; this retains
+            // any rows that were selected prior to the sync.
+
+            // Make note of any rows that are selected in the grid, so we can re-select them
+            // after loading the customs items
+            List<int> selectedIndexesInGrid = selectedRows.Select(r => r.IndexInGrid).ToList();
+            LoadShipments(loadedShipments, loadedShipments.All(s => !s.Processed), false);
+
+            // Select the rows that were originally selected. The row counts should
+            // not have changed, so we shouldn't get index out of range exceptions
+            selectedIndexesInGrid.ForEach(i => 
+            { 
+                selectedRows.Add(sandGrid.Rows[i]);
+                sandGrid.Rows[i].Selected = true;
+            });
         }
 
         /// <summary>
