fee36f1a623399296604034218ab3d6dbd8d0e06
Tim Hughes <t.hughes@shipworks.com>
2014-07-15 15:51:03 -0500

    Added IInsureShipResponse

---
+++ Code/ShipWorks/Shipping/Insurance/InsureShip/IInsureShipResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShipWorks.Shipping.Insurance.InsureShip
+{
+    /// <summary>
+    /// Interface for working with InsureShip responses
+    /// </summary>
+    public interface IInsureShipResponse
+    {
+        /// <summary>
+        /// Process a response from InsureShip
+        /// </summary>
+        void Process();
+    }
+}
