52fd2032bc4d86b21d1f42bd38be02f541d2adb9
Kevin Croke <k.croke@shipworks.com>
2014-07-16 14:20:59 -0500

    Add InsureShipRequestFactory.

---
+++ Code/ShipWorks/Shipping/Insurance/InsureShip/IInsureShipRequestFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping.Insurance.InsureShip
+{
+    public interface IInsureShipRequestFactory
+    {
+        void CreateInsureShipmentRequest(ShipmentEntity shipmentEntity, InsureShipAffiliate insureShipAffiliate);
+    }
+}
