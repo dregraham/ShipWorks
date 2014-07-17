5b042f579bd5e2e108e9cfc9b0e31a562a60f597
Kevin Croke <k.croke@shipworks.com>
2014-07-17 12:17:53 -0500

    Add InsureShipResponseFactory

---
+++ Code/ShipWorks/Shipping/Insurance/InsureShip/InsureShipResponseFactory.cs
@@ -0,0 +1,18 @@
+﻿using log4net;
+
+namespace ShipWorks.Shipping.Insurance.InsureShip
+{
+    /// <summary>
+    /// Factory to create an InsureShipResponse
+    /// </summary>
+    public class InsureShipResponseFactory: IInsureShipResponseFactory
+    {
+        /// <summary>
+        /// Creates the insure shipment response.
+        /// </summary>
+        public IInsureShipResponse CreateInsureShipmentResponse(InsureShipRequestBase insureShipRequestBase)
+        {
+            return new InsureShipmentResponse(insureShipRequestBase, LogManager.GetLogger(typeof(InsureShipmentResponse)));
+        }
+    }
+}
