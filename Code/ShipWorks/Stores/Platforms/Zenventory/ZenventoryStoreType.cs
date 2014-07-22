367f8f0e14f41563a7d9c1fa00211e2a21c71d89
Kevin Croke <k.croke@shipworks.com>
2014-07-22 16:46:17 -0500

    Add Store Wizard and Settings Control

--- Code/ShipWorks/Stores/Platforms/Zenventory/ZenventoryStoreType.cs
+++ Code/ShipWorks/Stores/Platforms/Zenventory/ZenventoryStoreType.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using ShipWorks.ApplicationCore.Logging;
 using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Management;
 using ShipWorks.Stores.Platforms.GenericModule;
+using ShipWorks.UI.Wizard;
 
 namespace ShipWorks.Stores.Platforms.Zenventory
 {
@@ -41,5 +43,24 @@ namespace ShipWorks.Stores.Platforms.Zenventory
                 return ApiLogSource.Zenventory;
             }
         }
+
+        /// <summary>
+        /// Create the user control used in the Store Manager window.
+        /// </summary>
+        public override AccountSettingsControlBase CreateAccountSettingsControl()
+        {
+            return new ZenventoryStoreAccountSettingsControl();
+        }
+
+        /// <summary>
+        /// Create the Wizard pages used in the setup wizard to configure the store.
+        /// </summary>
+        public override List<WizardPage> CreateAddStoreWizardPages()
+        {
+            List<WizardPage> pages = new List<WizardPage> {new ZenventoryAddStoreWizardPage()};
+
+            return pages;
+        }
+
     }
 }
