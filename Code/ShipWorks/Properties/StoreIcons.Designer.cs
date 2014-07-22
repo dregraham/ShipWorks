ec9adc595ef26f6bc48b7ffca1817b782139ab1e
Kevin Croke <k.croke@shipworks.com>
2014-07-22 15:40:57 -0500

    Add Zenventory favicon.

--- Code/ShipWorks/Properties/StoreIcons.Designer.cs
+++ Code/ShipWorks/Properties/StoreIcons.Designer.cs
@@ -509,5 +509,15 @@ namespace ShipWorks.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap zenventory {
+            get {
+                object obj = ResourceManager.GetObject("zenventory", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
