a85f9aa08e10867aa4701844f38e326c3329cc07
Tim Hughes <t.hughes@shipworks.com>
2014-07-22 07:49:27 -0500

    The only unique thing in the module url is the value of the token param.   So for the InternalLicenseIdentifier we will get that value, hash it, and return a Base64 string of it.

--- Code/ShipWorks/Stores/Platforms/SureDone/SureDoneStoreType.cs
+++ Code/ShipWorks/Stores/Platforms/SureDone/SureDoneStoreType.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Web;
 using ShipWorks.ApplicationCore.Logging;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Stores.Platforms.GenericModule;
@@ -40,6 +43,36 @@ namespace ShipWorks.Stores.Platforms.SureDone
             get
             {
                 return ApiLogSource.SureDone;
+            }
+        }
+
+        /// <summary>
+        /// Return value that uniquely identifies this store instance
+        /// 
+        /// The only unique thing in the module url is the value of the token param.
+        /// So we will get that value, hash it, and return a Base64 string of it.
+        /// </summary>
+        protected override string InternalLicenseIdentifier
+        {
+            get
+            {
+                GenericModuleStoreEntity genericStore = (GenericModuleStoreEntity)Store;
+
+                string moduleUrl = genericStore.ModuleUrl;
+                Uri moduleUri = new Uri(moduleUrl);
+
+                // Parse the query string variables into a NameValueCollection.
+                NameValueCollection queryStringParams = HttpUtility.ParseQueryString(moduleUri.Query);
+                string token = queryStringParams["token"];
+
+                // We can't use the actual token, because its secure.
+                byte[] bytes = Encoding.UTF8.GetBytes(token);
+
+                using (MD5 md5 = new MD5CryptoServiceProvider())
+                {
+                    // Generate the hash
+                    return Convert.ToBase64String(md5.ComputeHash(bytes));
+                }
             }
         }
     }
