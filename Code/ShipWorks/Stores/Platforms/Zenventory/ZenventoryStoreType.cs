723c84a793cfa00b68ad920f98e95cf889f96186
Kevin Croke <k.croke@shipworks.com>
2014-07-22 15:53:36 -0500

    Add ZenventoryStoreType

---
+++ Code/ShipWorks/Stores/Platforms/Zenventory/ZenventoryStoreType.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.ApplicationCore.Logging;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.GenericModule;
+
+namespace ShipWorks.Stores.Platforms.Zenventory
+{
+    public class ZenventoryStoreType : GenericModuleStoreType
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ZenventoryStoreType"/> class.
+        /// </summary>
+        public ZenventoryStoreType(StoreEntity store) 
+            : base(store)
+        {
+
+        }
+
+        /// <summary>
+        /// Gets the type code for this store
+        /// </summary>
+        public override StoreTypeCode TypeCode
+        {
+            get
+            {
+                return StoreTypeCode.Zenventory;
+            }
+        }
+
+
+        /// <summary>
+        /// Get the log source
+        /// </summary>
+        public override ApiLogSource LogSource
+        {
+            get
+            {
+                return ApiLogSource.Zenventory;
+            }
+        }
+    }
+}
