07c456a2fe480495943ed82f9bb41072fb849d6c
Tim Hughes <t.hughes@shipworks.com>
2014-07-30 07:50:37 -0500

    Refactored a bit and added additional check for not showing the edit or view controls if the submit claim wait time has not fully passed.

--- Code/ShipWorks/Shipping/Insurance/InsuranceTabControl.cs
+++ Code/ShipWorks/Shipping/Insurance/InsuranceTabControl.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 using ShipWorks.Data;
 using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Insurance.InsureShip;
 
 namespace ShipWorks.Shipping.Insurance
 {
@@ -34,42 +35,14 @@ namespace ShipWorks.Shipping.Insurance
         /// <param name="shipments"></param>
         public void LoadClaim(List<ShipmentEntity> shipments)
         {
-            if (shipments.Count > 1)
+            if (!IsValid(shipments))
             {
-                messageLabel.Text = "Multiple shipments are selected. Select a single shipment to view insurance information.";
-                messageLabel.Visible = true;
-                insuranceViewClaimControl.Visible = false;
-                return;
-            }
-            
-            ShipmentEntity shipment = shipments.First();
-
-            if (!shipment.Processed)
-            {
-                messageLabel.Text = "The selected shipment has not been processed.";
-                messageLabel.Visible = true;
-                insuranceViewClaimControl.Visible = false;
-                return;
-            }
-
-            if (shipment.Voided)
-            {
-                messageLabel.Text = "This shipment has been voided and has no insurance claims.";
-                messageLabel.Visible = true;
-                insuranceViewClaimControl.Visible = false;
-                return;
-            }
-
-            if (!shipment.Insurance)
-            {
-                messageLabel.Text = "This shipment has not been insured.";
-                messageLabel.Visible = true;
-                insuranceViewClaimControl.Visible = false;
                 return;
             }
 
             messageLabel.Visible = false;
-            
+
+            ShipmentEntity shipment = shipments.First();
             ShipmentTypeDataService.LoadInsuranceData(shipment);
 
             if (shipment.InsurancePolicy != null)
@@ -83,12 +56,66 @@ namespace ShipWorks.Shipping.Insurance
         }
 
         /// <summary>
+        /// Determines if the shipment(s) is valid.  Sets UI message text appropriately.
+        /// </summary>
+        /// <param name="shipments">The list of shipments</param>
+        /// <returns>True if the the view or edit control can be shown.  False otherwise.</returns>
+        private bool IsValid(List<ShipmentEntity> shipments)
+        {
+            if (shipments.Count > 1)
+            {
+                messageLabel.Text = "Multiple shipments are selected. Select a single shipment to view insurance information.";
+                messageLabel.Visible = true;
+                insuranceViewClaimControl.Visible = false;
+                return false;
+            }
+
+            ShipmentEntity shipment = shipments.First();
+
+            if (!shipment.Processed)
+            {
+                messageLabel.Text = "The selected shipment has not been processed.";
+                messageLabel.Visible = true;
+                insuranceViewClaimControl.Visible = false;
+                return false;
+            }
+
+            if (shipment.Voided)
+            {
+                messageLabel.Text = "This shipment has been voided and has no insurance claims.";
+                messageLabel.Visible = true;
+                insuranceViewClaimControl.Visible = false;
+                return false;
+            }
+
+            if (!shipment.Insurance)
+            {
+                messageLabel.Text = "This shipment has not been insured.";
+                messageLabel.Visible = true;
+                insuranceViewClaimControl.Visible = false;
+                return false;
+            }
+
+            InsureShipSettings insureShipSettings = new InsureShipSettings();
+            DateTime allowedSubmitClaimDateUtc = shipment.ProcessedDate.Value + insureShipSettings.ClaimSubmissionWaitingPeriod;
+
+            if (DateTime.UtcNow < allowedSubmitClaimDateUtc)
+            {
+                messageLabel.Text = string.Format("You may submit a claim on or after {0}.", allowedSubmitClaimDateUtc.ToLocalTime());
+                messageLabel.Visible = true;
+                insuranceViewClaimControl.Visible = false;
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
         /// Show the readonly view of the claim
         /// </summary>
         private void ShowViewClaim(ShipmentEntity shipment)
         {
             insuranceViewClaimControl.LoadClaim(shipment);
-            
         }
 
         /// <summary>
