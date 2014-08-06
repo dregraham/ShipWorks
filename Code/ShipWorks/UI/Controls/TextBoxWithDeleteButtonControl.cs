f8f5b3e20af61bcfb6fd03c3b43075eeb5d07c15
Kevin Croke <k.croke@shipworks.com>
2014-08-06 17:18:41 -0500

    Create StringListControl and replace similar controls in ShipSenseHashConfigurationControl

--- Code/ShipWorks/UI/Controls/TextBoxWithDeleteButtonControl.cs
+++ Code/ShipWorks/UI/Controls/TextBoxWithDeleteButtonControl.cs
@@ -2,38 +2,37 @@
 using System.Windows.Forms;
 using ShipWorks.UI.Utility;
 
-namespace ShipWorks.Shipping.ShipSense.Settings
+namespace ShipWorks.UI.Controls
 {
     /// <summary>
     /// 
     /// </summary>
-    public partial class ShipSenseItemAttributeControl : UserControl
+    public partial class TextBoxWithDeleteButton : UserControl
     {
         /// <summary>
         /// Occurs when the delete/remove button is clicked to notify interested listeners.
         /// </summary>
-        public event EventHandler DeleteAttributeClick;
+        public event EventHandler DeleteClick;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ShipSenseItemAttributeControl"/> class.
+        /// Initializes a new instance of the <see cref="TextBoxWithDeleteButton"/> class.
         /// </summary>
-        /// <param name="attributeName">Name of the attribute.</param>
-        public ShipSenseItemAttributeControl(string attributeName)
+        public TextBoxWithDeleteButton(string value)
         {
             InitializeComponent();
 
-            attribute.Text = attributeName;
+            textBox.Text = value;
 
             // Get rid of the ugly bottom border on the toolstrip control
             toolStripDelete.Renderer = new NoBorderToolStripRenderer();
         }
 
         /// <summary>
-        /// Gets the name of the attribute in the text box.
+        /// Gets the value in the value box.
         /// </summary>
-        public string AttributeName
+        public string Value
         {
-            get { return attribute.Text; }
+            get { return textBox.Text; }
         }
 
         /// <summary>
@@ -43,9 +42,9 @@ namespace ShipWorks.Shipping.ShipSense.Settings
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         public void OnDelete(object sender, EventArgs e)
         {
-            if (DeleteAttributeClick != null)
+            if (DeleteClick != null)
             {
-                DeleteAttributeClick(this, EventArgs.Empty);
+                DeleteClick(this, EventArgs.Empty);
             }
         }
     }
