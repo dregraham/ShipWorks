a4baebcfcb4def54429ef5aebf85235e45f84371
Kevin Croke <k.croke@shipworks.com>
2014-08-18 14:04:58 -0500

    Add Cloud Conversion favicon.

--- Code/ShipWorks/Properties/StoreIcons.Designer.cs
+++ Code/ShipWorks/Properties/StoreIcons.Designer.cs
@@ -143,6 +143,16 @@ namespace ShipWorks.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap cloudconversion {
+            get {
+                object obj = ResourceManager.GetObject("cloudconversion", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap commerceinterface {
             get {
                 object obj = ResourceManager.GetObject("commerceinterface", resourceCulture);
