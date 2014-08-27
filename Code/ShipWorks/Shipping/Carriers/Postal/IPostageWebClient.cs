0d801205af4a5a5932cd546a41b2dceb477ce879
Kevin Croke <k.croke@shipworks.com>
2014-08-27 16:17:36 -0500

    Add IPostageWebClient

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/IPostageWebClient.cs
@@ -0,0 +1,14 @@
+﻿namespace ShipWorks.Shipping.Carriers.Postal
+{
+    /// <summary>
+    /// Interface to Get Postal Balance and Purchase Postage.
+    /// </summary>
+    public interface IPostageWebClient
+    {
+        ShipmentTypeCode ShipmentTypeCode { get; }
+        string AccountIdentifier { get; }
+
+        decimal GetBalance();
+        void Purchase(decimal amount);
+    }
+}
