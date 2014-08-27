6c00bffe08d1d15f03e2f46adb136877a2796940
Kevin Croke <k.croke@shipworks.com>
2014-08-27 16:40:18 -0500

    Wrap TangoWebClient and generate ITangoWebClient

---
+++ Code/ShipWorks/ApplicationCore/Licensing/TangoWebClientWrapper.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using Interapptive.Shared.Business;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping;
+using ShipWorks.Shipping.Carriers.Postal.Endicia.Account;
+
+namespace ShipWorks.ApplicationCore.Licensing
+{
+    /// <summary>
+    /// Interface for working with the interapptive license server
+    /// </summary>
+    public class TangoWebClientWrapper : ITangoWebClient
+    {
+        /// <summary>
+        /// Activate the given license key to the specified store identifier
+        /// </summary>
+        public LicenseAccountDetail ActivateLicense(string licenseKey, StoreEntity store)
+        {
+            return TangoWebClient.ActivateLicense(licenseKey, store);
+        }
+
+        /// <summary>
+        /// Get the status of the specified license
+        /// </summary>
+        public LicenseAccountDetail GetLicenseStatus(string licenseKey, StoreEntity store)
+        {
+            return TangoWebClient.GetLicenseStatus(licenseKey, store);
+        }
+
+        /// <summary>
+        /// Get the status of the specified license
+        /// </summary>
+        public Dictionary<string, string> GetCounterRatesCredentials(StoreEntity store)
+        {
+            return TangoWebClient.GetCounterRatesCredentials(store);
+        }
+
+        /// <summary>
+        /// Get the status of the specified license
+        /// </summary>
+        public Dictionary<string, string> GetCarrierCertificateVerificationData(StoreEntity store)
+        {
+            return TangoWebClient.GetCarrierCertificateVerificationData(store);
+        }
+
+        /// <summary>
+        /// Request a trial for use with the specified store. If a trial already exists, a new one will not be created.
+        /// </summary>
+        public TrialDetail GetTrial(StoreEntity store)
+        {
+            return TangoWebClient.GetTrial(store);
+        }
+
+        /// <summary>
+        /// Extend the trial for the given store
+        /// </summary>
+        public TrialDetail ExtendTrial(StoreEntity store)
+        {
+            return TangoWebClient.ExtendTrial(store);
+        }
+
+        /// <summary>
+        /// Send the user their username using the specified email address
+        /// </summary>
+        public void SendAccountUsername(string email, string username)
+        {
+            TangoWebClient.SendAccountUsername(email, username);
+        }
+
+        /// <summary>
+        /// Sends Postal balances for postal services.
+        /// </summary>
+        public void LogPostageEvent(double balance, double purchaseAmount, ShipmentTypeCode shipmentTypeCode, string accountIdentifier)
+        {
+            TangoWebClient.LogPostageEvent(balance, purchaseAmount, shipmentTypeCode, accountIdentifier);
+        }
+
+        /// <summary>
+        /// Log the given processed shipment to Tango.  isRetry is only for internal interapptive purposes to handle rare cases where shipments a customer
+        /// insured did not make it up into tango, but the shipment did actually process.
+        /// </summary>
+        public void LogShipment(StoreEntity store, ShipmentEntity shipment, bool isRetry = false)
+        {
+            TangoWebClient.LogShipment(store, shipment, isRetry);
+        }
+
+        /// <summary>
+        /// Void the given processed shipment to Tango
+        /// </summary>
+        public void VoidShipment(StoreEntity store, ShipmentEntity shipment)
+        {
+            TangoWebClient.VoidShipment(store, shipment);
+        }
+
+        /// <summary>
+        /// Records License agreement and generates an Interapptive-tracked policy number.
+        /// </summary>
+        public string GenerateInsurancePolicyNumber(StoreEntity store)
+        {
+            return TangoWebClient.GenerateInsurancePolicyNumber(store);
+        }
+
+        /// <summary>
+        /// Get the freemium status of the given store
+        /// </summary>
+        public LicenseAccountDetail GetFreemiumStatus(EbayStoreEntity store)
+        {
+            return TangoWebClient.GetFreemiumStatus(store);
+        }
+
+        /// <summary>
+        /// Create a new freemium store in tango
+        /// </summary>
+        public void CreateFreemiumStore(StoreEntity store, PersonAdapter accountAddress, EndiciaPaymentInfo paymentInfo, string dazzleAccount, bool validateOnly)
+        {
+            TangoWebClient.CreateFreemiumStore(store, accountAddress, paymentInfo, dazzleAccount, validateOnly);
+        }
+
+        /// <summary>
+        /// Associate the given freemium account# with the given store
+        /// </summary>
+        public void SetFreemiumAccountNumber(StoreEntity store, string freemiumAccount)
+        {
+            TangoWebClient.SetFreemiumAccountNumber(store, freemiumAccount);
+        }
+
+        /// <summary>
+        /// Update the platform, developer, and version info for the given generic store
+        /// </summary>
+        public void UpdateGenericModuleInfo(GenericModuleStoreEntity store, string platform, string developer, string version)
+        {
+            TangoWebClient.UpdateGenericModuleInfo(store, platform, developer, version);
+        }
+
+        /// <summary>
+        /// Upgrade the given store to the 'Paid' version of freemium with the given endicia service plan.
+        /// </summary>
+        public void UpgradeFreemiumStore(StoreEntity store, int endiciaServicePlan)
+        {
+            TangoWebClient.UpgradeFreemiumStore(store, endiciaServicePlan);
+        }
+
+        /// <summary>
+        /// Upgrade the given trial to not be in an 'Edition' mode
+        /// </summary>
+        public void UpgradeEditionTrial(StoreEntity store)
+        {
+            TangoWebClient.UpgradeEditionTrial(store);
+        }
+
+        /// <summary>
+        /// Ensure the connection to the given URI is a valid interapptive secure connection
+        /// </summary>
+        public void ValidateSecureConnection(Uri uri)
+        {
+            TangoWebClient.ValidateSecureConnection(uri);
+        }
+    }
+}
