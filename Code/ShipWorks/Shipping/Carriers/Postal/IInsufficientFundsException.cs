222b2d036c2517eeef9cce1b0fd15ac3232ab117
Andrew Benz <a.benz@shipworks.com>
2014-09-04 17:09:47 -0500

    Implement stamps postage dialog

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/IInsufficientFundsException.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+
+namespace ShipWorks.Shipping.Carriers.Postal
+{
+    /// <summary>
+    /// Insufficient funds interface
+    /// </summary>
+    public interface IInsufficientFundsException
+    {
+        /// <summary>
+        /// Name of the provider associated with the exception
+        /// </summary>
+        string Provider { get; }
+
+        /// <summary>
+        /// Identifier of the account
+        /// </summary>
+        string AccountIdentifier { get; }
+
+        /// <summary>
+        /// Create a dialog that will allow a customer to purchase more postage
+        /// </summary>
+        Form CreatePostageDialog();
+    }
+}
