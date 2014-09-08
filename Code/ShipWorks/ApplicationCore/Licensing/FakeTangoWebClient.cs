ffc7ffe8f701f7588ee04032c1fde553e08b5e24
John Yedlicka <j.yedlicka@shipworks.com>
2014-09-08 11:05:25 -0500

    Create an inteface around the TangoWebClient and add Nudge methods
    - Most of this code was pulled over from the feature-log-postage-to-tango
      branch that has not yet been merged into staging.
    
    - Methods for retrieving nudges from Tango and logging a nudge option back
      to Tango were added to the interface.
    
    - A FakeTangoWebClient was added for internal testing purposes to simulate
      calls to Tango that may not yet be implemented and/or Tango sending down
      data for specific test cases.

---
+++ Code/ShipWorks/ApplicationCore/Licensing/FakeTangoWebClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using ShipWorks.ApplicationCore.Nudges;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.ApplicationCore.Licensing
+{
+    /// <summary>
+    /// A fake web client for internal testing purposes to simulate calls to Tango that may not yet be 
+    /// implemented on the Tango side. This is just to streamline the development side of things on the
+    /// ShipWorks side without having to mess with Fiddler and all of the certificate inspection that
+    /// goes along with it when trying to setup specific test cases.
+    /// </summary>
+    public class FakeTangoWebClient : TangoWebClientWrapper, ITangoWebClient
+    {
+        /// <summary>
+        /// Gets the nudges.
+        /// </summary>
+        /// <returns>A couple of fake nudges for testing purposes.</returns>
+        public override IEnumerable<Nudge> GetNudges(IEnumerable<StoreEntity> stores)
+        {
+            // Build up a couple of dummy nudges for testing purposes. Null is being configured as the INudgeAction 
+            // until the actual implementations are ready. Null is a good test to ensure that this is accounted 
+            // for, however.
+            List<Nudge> nudges = new List<Nudge>
+            {
+                new Nudge(1, NudgeTypes.ShipWorksUpgrade, new Uri("http://www.shipworks.com"), new List<NudgeOption> { new NudgeOption(0, "Close", null) }, new Size(400, 600)),
+                new Nudge(2, NudgeTypes.ShipWorksUpgrade, new Uri("http://www.google.com"), new List<NudgeOption> { new NudgeOption(0, "Close", null) }, new Size(300, 500))
+            };
+
+            return nudges;
+        }
+
+        /// <summary>
+        /// Logs the nudge option back to Tango. Intended to record which option was selected by the user.
+        /// </summary>
+        public override void LogNudgeOption(NudgeOption option)
+        {
+            // Do nothing to simulate a successful nudge being logged to Tango
+        }
+    }
+}
---
+++ Code/ShipWorks/ApplicationCore/Licensing/TangoWebClientWrapper.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using Interapptive.Shared.Business;
+using ShipWorks.ApplicationCore.Nudges;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping;
+using ShipWorks.Shipping.Carriers.Postal.Endicia.Account;
+
+namespace ShipWorks.ApplicationCore.Licensing
+{
+    /// <summary>
+    /// Interface for working with the interapptive license server
+    /// </summary>
+    public class TangoWebClientWrapper : ITangoWebClient
+    {
+        /// <summary>
+        /// Activate the given license key to the specified store identifier
+        /// </summary>
+        public virtual LicenseAccountDetail ActivateLicense(string licenseKey, StoreEntity store)
+        {
+            return TangoWebClient.ActivateLicense(licenseKey, store);
+        }
+
+        /// <summary>
+        /// Get the status of the specified license
+        /// </summary>
+        public virtual LicenseAccountDetail GetLicenseStatus(string licenseKey, StoreEntity store)
+        {
+            return TangoWebClient.GetLicenseStatus(licenseKey, store);
+        }
+
+        /// <summary>
+        /// Get the status of the specified license
+        /// </summary>
+        public virtual Dictionary<string, string> GetCounterRatesCredentials(StoreEntity store)
+        {
+            return TangoWebClient.GetCounterRatesCredentials(store);
+        }
+
+        /// <summary>
+        /// Get the status of the specified license
+        /// </summary>
+        public virtual Dictionary<string, string> GetCarrierCertificateVerificationData(StoreEntity store)
+        {
+            return TangoWebClient.GetCarrierCertificateVerificationData(store);
+        }
+
+        /// <summary>
+        /// Request a trial for use with the specified store. If a trial already exists, a new one will not be created.
+        /// </summary>
+        public virtual TrialDetail GetTrial(StoreEntity store)
+        {
+            return TangoWebClient.GetTrial(store);
+        }
+
+        /// <summary>
+        /// Extend the trial for the given store
+        /// </summary>
+        public virtual TrialDetail ExtendTrial(StoreEntity store)
+        {
+            return TangoWebClient.ExtendTrial(store);
+        }
+
+        /// <summary>
+        /// Send the user their username using the specified email address
+        /// </summary>
+        public virtual void SendAccountUsername(string email, string username)
+        {
+            TangoWebClient.SendAccountUsername(email, username);
+        }
+        
+        /// <summary>
+        /// Log the given processed shipment to Tango.  isRetry is only for internal interapptive purposes to handle rare cases where shipments a customer
+        /// insured did not make it up into tango, but the shipment did actually process.
+        /// </summary>
+        public virtual void LogShipment(StoreEntity store, ShipmentEntity shipment, bool isRetry = false)
+        {
+            TangoWebClient.LogShipment(store, shipment, isRetry);
+        }
+
+        /// <summary>
+        /// Void the given processed shipment to Tango
+        /// </summary>
+        public virtual void VoidShipment(StoreEntity store, ShipmentEntity shipment)
+        {
+            TangoWebClient.VoidShipment(store, shipment);
+        }
+
+        /// <summary>
+        /// Records License agreement and generates an Interapptive-tracked policy number.
+        /// </summary>
+        public virtual string GenerateInsurancePolicyNumber(StoreEntity store)
+        {
+            return TangoWebClient.GenerateInsurancePolicyNumber(store);
+        }
+
+        /// <summary>
+        /// Get the freemium status of the given store
+        /// </summary>
+        public LicenseAccountDetail GetFreemiumStatus(EbayStoreEntity store)
+        {
+            return TangoWebClient.GetFreemiumStatus(store);
+        }
+
+        /// <summary>
+        /// Create a new freemium store in tango
+        /// </summary>
+        public virtual void CreateFreemiumStore(StoreEntity store, PersonAdapter accountAddress, EndiciaPaymentInfo paymentInfo, string dazzleAccount, bool validateOnly)
+        {
+            TangoWebClient.CreateFreemiumStore(store, accountAddress, paymentInfo, dazzleAccount, validateOnly);
+        }
+
+        /// <summary>
+        /// Associate the given freemium account# with the given store
+        /// </summary>
+        public virtual void SetFreemiumAccountNumber(StoreEntity store, string freemiumAccount)
+        {
+            TangoWebClient.SetFreemiumAccountNumber(store, freemiumAccount);
+        }
+
+        /// <summary>
+        /// Update the platform, developer, and version info for the given generic store
+        /// </summary>
+        public virtual void UpdateGenericModuleInfo(GenericModuleStoreEntity store, string platform, string developer, string version)
+        {
+            TangoWebClient.UpdateGenericModuleInfo(store, platform, developer, version);
+        }
+
+        /// <summary>
+        /// Upgrade the given store to the 'Paid' version of freemium with the given endicia service plan.
+        /// </summary>
+        public virtual void UpgradeFreemiumStore(StoreEntity store, int endiciaServicePlan)
+        {
+            TangoWebClient.UpgradeFreemiumStore(store, endiciaServicePlan);
+        }
+
+        /// <summary>
+        /// Upgrade the given trial to not be in an 'Edition' mode
+        /// </summary>
+        public virtual void UpgradeEditionTrial(StoreEntity store)
+        {
+            TangoWebClient.UpgradeEditionTrial(store);
+        }
+
+        /// <summary>
+        /// Ensure the connection to the given URI is a valid interapptive secure connection
+        /// </summary>
+        public virtual void ValidateSecureConnection(Uri uri)
+        {
+            TangoWebClient.ValidateSecureConnection(uri);
+        }
+
+        /// <summary>
+        /// Gets the nudges.
+        /// </summary>
+        public virtual IEnumerable<Nudge> GetNudges(IEnumerable<StoreEntity> stores)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logs the nudge option back to Tango. Intended to record which option was selected by the user.
+        /// </summary>
+        public virtual void LogNudgeOption(NudgeOption option)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
