bd0f309d74b34eccc7fd24e00103f76fe162b63f
John Yedlicka <j.yedlicka@shipworks.com>
2014-09-08 15:06:30 -0500

    Implement the NudgeManager class for serving as a repository of nudges obtained from Tango
    - Initialize method is clearing out any existing nudges and using the
      TangoWebClientFactory/ITangoWebClient to fetch the nudge data.
    
    - Stubbed in the NudgeDlg since the task called for a ShowNudge method on
      the NudgeManager (which probably should be moved out of the
      NudgeManager)

---
+++ Code/ShipWorks/ApplicationCore/Nudges/NudgeDlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Interapptive.Shared.Utility;
+
+namespace ShipWorks.ApplicationCore.Nudges
+{
+    public partial class NudgeDlg : Form
+    {
+        private readonly Nudge nudge;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NudgeDlg"/> class.
+        /// </summary>
+        /// <param name="nudge">The nudge.</param>
+        public NudgeDlg(Nudge nudge)
+        {
+            InitializeComponent();
+
+            this.nudge = nudge;
+        }
+
+        /// <summary>
+        /// Called when the form is loaded.
+        /// </summary>
+        private void OnLoad(object sender, EventArgs e)
+        {
+            Text = EnumHelper.GetDescription(nudge.NudgeType);
+        }
+    }
+}
---
+++ Code/ShipWorks/ApplicationCore/Nudges/NudgeManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using ShipWorks.ApplicationCore.Licensing;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.ApplicationCore.Nudges
+{
+    /// <summary>
+    /// Manager of all the Nudges that are available in ShipWorks. Nudges are retreived from Tango
+    /// and stored in memory.
+    /// </summary>
+    public static class NudgeManager
+    {
+        private static List<Nudge> nudges = new List<Nudge>();
+        private static object lockObject = new object();
+
+        /// <summary>
+        /// Initializes this instance by clearing out any previous nudges and refreshing the
+        /// nudges stored in memory for the given list of stores.
+        /// </summary>
+        /// <param name="stores">The stores.</param>
+        public static void Initialize(IEnumerable<StoreEntity> stores)
+        {
+            lock (lockObject)
+            {
+                nudges.Clear();
+
+                ITangoWebClient tangoWebClient = new TangoWebClientFactory().CreateWebClient();
+                nudges = tangoWebClient.GetNudges(stores).ToList();
+            }
+        }
+
+        /// <summary>
+        /// Gets the nudges.
+        /// </summary>
+        public static IEnumerable<Nudge> Nudges
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    // Return a new list, so our internal list isn't altered by any consumers
+                    return nudges.ToList();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Shows any nudges that need to be seen.
+        /// </summary>
+        /// <param name="owner">The owner.</param>
+        /// <param name="nudge">The nudge.</param>
+        public static void ShowNudge(IWin32Window owner, Nudge nudge)
+        {
+            // TODO: Move this elsewhere since it doesn't necessarily belong in the NudgeManager
+            // as far as SRP goes...
+            using (NudgeDlg nudgeDialog = new NudgeDlg(nudge))
+            {
+                nudgeDialog.ShowDialog(owner);
+            }
+        }
+    }
+}
