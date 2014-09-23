e9c47a3b44ddfe25c5133f7f835054bb09ee2924
John Yedlicka <j.yedlicka@shipworks.com>
2014-09-23 08:03:36 -0500

    Add PostalAccountContractType enumeration

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/PostalAccountContractType.cs
@@ -0,0 +1,31 @@
+﻿
+namespace ShipWorks.Shipping.Carriers.Postal
+{
+    public enum PostalAccountContractType
+    {
+        /// <summary>
+        /// The contract type is unknown at the moment but can/should be obtained from the postal
+        /// provder's API (most likely because ShipWorks has not requested this information 
+        /// from the postal provider yet).
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// The contract type is not applicable (most likely due to the postal provider's
+        /// API not offering a way to obtain this information). This should also be used
+        /// for accounts from postal providers where ShipWorks is not concerned with the type
+        /// of postal account.
+        /// </summary>
+        NotApplicable = 1,
+        
+        /// <summary>
+        /// Commercial Based Pricing (CBP)
+        /// </summary>
+        Commercial = 2,
+
+        /// <summary>
+        /// Commercial Plus Pricing/Negotiated Service Agreement (CPP/NSA)
+        /// </summary>
+        CommercialPlus = 3
+    }
+}
