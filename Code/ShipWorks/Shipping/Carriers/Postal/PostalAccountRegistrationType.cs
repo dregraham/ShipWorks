12f17a990ed9f7c4ff53fe7bc5b6ee5b9ad09646
John Yedlicka <j.yedlicka@shipworks.com>
2014-09-29 07:23:21 -0500

    Define IRegistrationPromotion interface for Stamps.com

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/PostalAccountRegistrationType.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ShipWorks.Shipping.Carriers.Postal
+{
+    /// <summary>
+    /// An enumeration for the type of postal account being registered/created.
+    /// </summary>
+    public enum PostalAccountRegistrationType
+    {
+        /// <summary>
+        /// An expedited postal account
+        /// </summary>
+        Expedited,
+
+        /// <summary>
+        /// A standard, non-expedited postal account
+        /// </summary>
+        Standard
+    }
+}
---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Stamps/Registration/IRegistrationPromotion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Stamps.Registration
+{
+    /// <summary>
+    /// An interface intended to obtain the promotion code that should be used when registering
+    /// an account with Stamps.com. The promotion code could differ based on the context that the 
+    /// account is being registered within. For example, the promo code could differ based on the
+    /// account being registered (Expedited vs Standard), whether a customer is migrating from 
+    /// another carrier, etc.
+    /// </summary>
+    public interface IRegistrationPromotion
+    {
+        /// <summary>
+        /// Gets the available account registration types. These values could differ across implementations
+        /// based on factors such as whether the account is a brand new account, being migrated from another
+        /// postal carrier, etc.
+        /// </summary>
+        IEnumerable<PostalAccountRegistrationType> AvailableAccountTypes { get; }
+
+        /// <summary>
+        /// Gets the promo code to use when registering an account with Stamps.com based on the 
+        /// type of account being registered.
+        /// </summary>
+        /// <param name="registrationType">The type of account being registered.</param>
+        /// <returns>The promotion code to be used during registration.</returns>
+        string GetPromoCode(PostalAccountRegistrationType registrationType);
+    }
+}
