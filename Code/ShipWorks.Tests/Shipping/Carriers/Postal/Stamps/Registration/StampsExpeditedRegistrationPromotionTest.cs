c2a091303f50e3190dd44b81816993dfd6dc91ae
John Yedlicka <j.yedlicka@shipworks.com>
2014-09-29 07:41:25 -0500

    Implement StampsExpeditedRegistrationPromotion

---
+++ Code/ShipWorks.Tests/Shipping/Carriers/Postal/Stamps/Registration/StampsExpeditedRegistrationPromotionTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShipWorks.Shipping.Carriers.Postal;
+using ShipWorks.Shipping.Carriers.Postal.Stamps.Registration;
+
+namespace ShipWorks.Tests.Shipping.Carriers.Postal.Stamps.Registration
+{
+    [TestClass]
+    public class StampsExpeditedRegistrationPromotionTest
+    {
+        private readonly StampsExpeditedRegistrationPromotion testObject;
+
+        public StampsExpeditedRegistrationPromotionTest()
+        {
+            testObject = new StampsExpeditedRegistrationPromotion();
+        }
+
+        [TestMethod]
+        public void AvailableAvailableAccountTypes_ReturnsListWithSingleItem_Test()
+        {
+            IEnumerable<PostalAccountRegistrationType> registrationAccountTypes = testObject.AvailableAccountTypes;
+
+            Assert.AreEqual(1, registrationAccountTypes.Count());
+        }
+
+        [TestMethod]
+        public void AvailableAvailableAccountTypes_ContainsExpeditedRegistrationType_Test()
+        {
+            IEnumerable<PostalAccountRegistrationType> registrationAccountTypes = testObject.AvailableAccountTypes;
+
+            Assert.AreEqual(PostalAccountRegistrationType.Expedited, registrationAccountTypes.First());
+        }
+
+        [TestMethod]
+        public void GetPromoCode_ReturnsShipWorks3_WhenRegistrationTypeIsExpedited_Test()
+        {
+            string promo = testObject.GetPromoCode(PostalAccountRegistrationType.Expedited);
+
+            Assert.AreEqual("shipworks3", promo);
+        }
+
+        [TestMethod]
+        public void GetPromoCode_ReturnsShipWorks3_WhenRegistrationTypeIsStandard_Test()
+        {
+            string promo = testObject.GetPromoCode(PostalAccountRegistrationType.Standard);
+
+            Assert.AreEqual("shipworks3", promo);
+        }
+    }
+}
