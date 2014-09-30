ac306438d6cf69711eed328a63ec3184c1a42aa6
Tim Hughes <t.hughes@shipworks.com>
2014-09-30 09:42:00 -0500

    Initial addition of UspsShipmentType

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Usps/UspsShipmentType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.ApplicationCore.Logging;
+using ShipWorks.Shipping.Carriers.Postal.Stamps;
+using ShipWorks.Shipping.Settings;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Usps
+{
+    public class UspsShipmentType : StampsShipmentType
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UspsShipmentType"/> class.
+        /// </summary>
+        public UspsShipmentType()
+        {
+            ShouldRetrieveExpress1Rates = false;
+
+            // Use the "live" versions by default
+            AccountRepository = new StampsAccountRepository();
+            LogEntryFactory = new LogEntryFactory();
+        }
+
+        /// <summary>
+        /// The ShipmentTypeCode enumeration value
+        /// </summary>
+        public override ShipmentTypeCode ShipmentTypeCode
+        {
+            get { return ShipmentTypeCode.Usps; }
+        }
+
+        /// <summary>
+        /// Create the settings control for stamps.com
+        /// </summary>
+        public override SettingsControlBase CreateSettingsControl()
+        {
+            return new StampsSettingsControl(StampsResellerType.StampsExpedited);
+        } 
+    }
+}
