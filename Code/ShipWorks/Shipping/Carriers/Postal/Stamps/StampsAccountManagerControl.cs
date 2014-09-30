832b98f780cfb5240c29c9d19ced9420f8206a6e
Tim Hughes <t.hughes@shipworks.com>
2014-09-30 12:27:43 -0500

    Added missing comment

--- Code/ShipWorks/Shipping/Carriers/Postal/Stamps/StampsAccountManagerControl.cs
+++ Code/ShipWorks/Shipping/Carriers/Postal/Stamps/StampsAccountManagerControl.cs
@@ -52,6 +52,9 @@ namespace ShipWorks.Shipping.Carriers.Postal.Stamps
         /// </summary>
         public StampsResellerType StampsResellerType { get; set; }
 
+        /// <summary>
+        /// Determines if an Express1 account is being managed.
+        /// </summary>
         private bool IsExpress1
         {
             get { return StampsResellerType == StampsResellerType.Express1; }
