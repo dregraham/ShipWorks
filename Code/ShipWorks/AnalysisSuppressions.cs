1aa5cee6537805b708d902826f275263f2ac5ac8
John Yedlicka <j.yedlicka@shipworks.com>
2014-10-22 12:26:39 -0500

    Revert "Update UspsCounterRateBroker to inherit from StampsCounterRatesBroker."
    - This reverts commit 319fc2bb15ac26433281f1ea010f85af07b0b87d. The
      reasoning for no longer inheriting from the StampsCounterRatesBroker is
      that we need to explicitly specify the UspsShipmentType, so it can be
      used to create the appropriate setup wizard and promo code combination.

--- Code/ShipWorks/AnalysisSuppressions.cs
+++ Code/ShipWorks/AnalysisSuppressions.cs
@@ -13744,3 +13744,4 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2240:ImplementISerializableCorrectly", Scope = "type", Target = "ShipWorks.Shipping.Carriers.UPS.OpenAccount.UpsOpenAccountBusinessAddressException")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2240:ImplementISerializableCorrectly", Scope = "type", Target = "ShipWorks.Shipping.Carriers.UPS.OpenAccount.UpsOpenAccountSoapException")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2240:ImplementISerializableCorrectly", Scope = "type", Target = "ShipWorks.Users.Security.PermissionException")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1702:CompoundWordsShouldBeCasedCorrectly", MessageId = "Username", Scope = "member", Target = "ShipWorks.Shipping.Carriers.TangoCounterRatesCredentialStore.#StampsUsername")]
--- Code/ShipWorks/Shipping/Carriers/Postal/Usps/BestRate/UspsCounterRatesBroker.cs
+++ Code/ShipWorks/Shipping/Carriers/Postal/Usps/BestRate/UspsCounterRatesBroker.cs
@@ -1,7 +1,6 @@
 ﻿using System.Linq;
 using ShipWorks.Data.Model.Custom.EntityClasses;
 using ShipWorks.Data.Model.EntityClasses;
-using ShipWorks.Shipping.Carriers.Postal.Stamps.BestRate;
 using ShipWorks.Shipping.Carriers.Postal.WebTools.BestRate;
 
 namespace ShipWorks.Shipping.Carriers.Postal.Usps.BestRate
@@ -9,7 +8,7 @@ namespace ShipWorks.Shipping.Carriers.Postal.Usps.BestRate
     /// <summary>
     /// Counter rate broker that converts shipments to USPS (Stamps.com Expedited)
     /// </summary>
-    public class UspsCounterRatesBroker : StampsCounterRatesBroker
+    public class UspsCounterRatesBroker : WebToolsCounterRatesBroker
     {
         private readonly ICarrierAccountRepository<StampsAccountEntity> stampsAccountRepository;
 
@@ -18,7 +17,7 @@ namespace ShipWorks.Shipping.Carriers.Postal.Usps.BestRate
         /// </summary>
         /// <param name="accountRepository">Repository that will be used to access Stamps accounts</param>
         public UspsCounterRatesBroker(ICarrierAccountRepository<StampsAccountEntity> accountRepository) :
-            base(accountRepository)
+            base(new UspsShipmentType())
         {
             stampsAccountRepository = accountRepository;
         }
