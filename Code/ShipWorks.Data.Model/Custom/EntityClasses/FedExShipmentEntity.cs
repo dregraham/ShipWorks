4c34b4b82a188d6c8eda73c3376047d9b7a7db8b
Tim Hughes <t.hughes@shipworks.com>
2014-10-30 13:57:36 -0500

    Added custom entity classes for saving RequestedLabelFormat of the child shipment entities up to the shipment entity.

---
+++ Code/ShipWorks.Data.Model/Custom/EntityClasses/FedExShipmentEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Runtime.Serialization;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+
+namespace ShipWorks.Data.Model.EntityClasses
+{
+    /// <summary>
+    /// Partial class extention of the LLBLGen FedExShipmentEntity
+    /// </summary>
+    public partial class FedExShipmentEntity
+    {
+        /// <summary>
+        /// Field value is changing
+        /// </summary>
+        protected override void OnFieldValueChanged(object originalValue, IEntityField2 field)
+        {
+            // If the FedEx RequestedLabelFormat changed, update the ShipmentEntity's version
+            if (field.FieldIndex == (int)FedExShipmentFieldIndex.RequestedLabelFormat)
+            {
+                Shipment.RequestedLabelFormat = this.RequestedLabelFormat;
+            }
+
+            base.OnFieldValueChanged(originalValue, field);
+        }
+    }
+}
---
+++ Code/ShipWorks.Data.Model/Custom/EntityClasses/OtherShipmentEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Runtime.Serialization;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+
+namespace ShipWorks.Data.Model.EntityClasses
+{
+    /// <summary>
+    /// Partial class extention of the LLBLGen OtherShipmentEntity
+    /// </summary>
+    public partial class OtherShipmentEntity
+    {
+        /// <summary>
+        /// Field value is changing
+        /// </summary>
+        protected override void OnFieldValueChanged(object originalValue, IEntityField2 field)
+        {
+            // If the RequestedLabelFormat changed, update the ShipmentEntity's version
+            if (field.FieldIndex == (int)OtherShipmentFieldIndex.RequestedLabelFormat)
+            {
+                Shipment.RequestedLabelFormat = this.RequestedLabelFormat;
+            }
+
+            base.OnFieldValueChanged(originalValue, field);
+        }
+    }
+}
