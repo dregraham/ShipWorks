24faca42d0548f16cfb5aff880a0c657779957ea
Kevin Croke <k.croke@shipworks.com>
2014-11-12 15:04:01 -0600

    Add obfuscation attribute to MapPanelType (Thanks, Tim!)

--- Code/ShipWorks/Stores/Content/Panels/MapPanelType.cs
+++ Code/ShipWorks/Stores/Content/Panels/MapPanelType.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace ShipWorks.Stores.Content.Panels
@@ -8,6 +9,7 @@ namespace ShipWorks.Stores.Content.Panels
     /// <summary>
     /// Enum that controls what type of image the MapPanel gets.
     /// </summary>
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]    
     public enum MapPanelType
     {
         Satellite,
