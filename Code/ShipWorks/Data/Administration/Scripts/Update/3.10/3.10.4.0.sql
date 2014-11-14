91f13d866e616ce11a2ebe47f9c8b143291e9f51
John Yedlicka <j.yedlicka@shipworks.com>
2014-11-14 08:32:23 -0600

    Remove unnecessary script to reset contract types
    - Now that we're grabbing the contract type from Stamps on an ongoing
      basis, we no longer need to reset the contract types of Stamps accounts
      in ShipWorks.

--- Code/ShipWorks/Data/Administration/Scripts/Update/3.10/3.10.4.0.sql
+++
@@ -1,3 +0,0 @@
-﻿-- Reset the contract type to unknown, so we get updated information (and don't show the
--- footnote to users that have already converted their accounts outside of ShipWorks)
-UPDATE StampsAccount SET ContractType = 0
