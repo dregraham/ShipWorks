1e0f5751b3111918ce0f6caf8975f33508af5ddd
Kevin Croke <k.croke@shipworks.com>
2015-01-20 09:27:08 -0600

    Add BestRateUpsRestrictionShippingPolicy

---
+++ Code/ShipWorks/Shipping/Policies/BestRateUpsRestrictionShippingPolicy.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.Shipping.Carriers.BestRate;
+using ShipWorks.Shipping.Carriers.UPS.BestRate;
+using ShipWorks.Shipping.Carriers.UPS.WorldShip.BestRate;
+using ShipWorks.Shipping.Editing.Rating;
+
+namespace ShipWorks.Shipping.Policies
+{
+    /// <summary>
+    /// If Tango says we can't use UPS for BestRate, filter out UPS
+    /// </summary>
+    public class BestRateUpsRestrictionShippingPolicy : IShippingPolicy
+    {
+        private bool isRestricted = false;
+
+        /// <summary>
+        /// Uses the configuration data provided to configure the shipping policy.
+        /// </summary>
+        /// <param name="configuration">The configuration data.</param>
+        /// <exception cref="System.ArgumentException">configuration</exception>
+        public void Configure(string configuration)
+        {
+            bool parsedConfiguration;
+
+            if (bool.TryParse(configuration, out parsedConfiguration))
+            {
+                if (parsedConfiguration)
+                {
+                    isRestricted = true;
+                }
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Unknown configuration value '{0}.' Expected 'true' or 'false.'", configuration), "configuration");
+            }
+        }
+
+        /// <summary>
+        /// If rategroup, we can act on it. return true
+        /// </summary>
+        public bool IsApplicable(object target)
+        {
+            return isRestricted && target is List<IBestRateShippingBroker>;
+        }
+
+        /// <summary>
+        /// Filter out 
+        /// </summary>
+        public void Apply(object target)
+        {
+            if (!isRestricted)
+            {
+                return;
+            }
+
+            if (target == null)
+            {
+                throw new ArgumentNullException("target");
+            }
+
+            List<IBestRateShippingBroker> brokers = target as List<IBestRateShippingBroker>;
+
+            if (brokers == null)
+            {
+                throw new ArgumentException("target not of type  List<IBestRateShippingBroker>", "target");
+            }
+
+            RemoveUPSRateGroups(brokers);
+        }
+
+        /// <summary>
+        /// Removes the ups rate groups.
+        /// </summary>
+        private void RemoveUPSRateGroups(List<IBestRateShippingBroker> brokers)
+        {
+            int originalNumberOfRateGroups = brokers.Count;
+
+            for (int i = originalNumberOfRateGroups - 1; i >= 0; i--)
+            {
+                IBestRateShippingBroker broker = brokers[i];
+
+                if (IsUpsBroker(broker))
+                {
+                    brokers.RemoveAt(i);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Determines whether broker is a UPS broker type.
+        /// </summary>
+        private static bool IsUpsBroker(IBestRateShippingBroker broker)
+        {
+            Type type = broker.GetType();
+
+            return type == typeof(WorldShipBestRateBroker) ||
+                   type == typeof(UpsBestRateBroker) ||
+                   type == typeof(UpsCounterRatesBroker);
+        }
+    }
+}
