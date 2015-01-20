352a48c60fd53226547d6801ba79dbd3866a9208
John Yedlicka <j.yedlicka@shipworks.com>
2015-01-20 07:21:11 -0600

    Define the IShippingPolicy interface and the ShippingPolicyType enumeration
    - The interface is intended to be an extension point to dynamically alter
      behavior at run-time on various aspects of shipping.
    
    - The enumeration describes the different types of policies that can be
      added. Each enumeration value basically equates to a topic in which
      the normal behavior could be altered.

---
+++ Code/ShipWorks/Shipping/Policies/ShippingPolicyType.cs
@@ -0,0 +1,24 @@
+﻿using Interapptive.Shared.Utility;
+
+namespace ShipWorks.Shipping.Policies
+{
+    /// <summary>
+    /// An enumeration for the different type of shipping policies/extension points where
+    /// behavior can be dynamically altered.
+    /// </summary>
+    public enum ShippingPolicyType
+    {
+        /// <summary>
+        /// A policy type indicating that the quantity of rate results that get displayed
+        /// for a shipment type should be altered.
+        /// </summary>
+        [ApiValue("RateResultCount")]
+        RateResultCount,
+
+        /// <summary>
+        /// A policy type to indicate whether UPS should be used in the BestRate shipment type.
+        /// </summary>
+        [ApiValue("BestRateUpsRestriction")]
+        BestRateUpsRestriction
+    }
+}
