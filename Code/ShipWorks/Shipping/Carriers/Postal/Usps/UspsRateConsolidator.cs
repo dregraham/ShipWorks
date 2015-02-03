df0aad3b24d1458625a30f7eaa764a0e3959d46b
Kevin Croke <k.croke@shipworks.com>
2015-02-03 13:01:59 -0600

    Create rate consolidator

--- Code/ShipWorks/Shipping/Carriers/Postal/Usps/UspsRateConsolidator.cs
+++ Code/ShipWorks/Shipping/Carriers/Postal/Usps/UspsRateConsolidator.cs
@@ -14,8 +14,64 @@ namespace ShipWorks.Shipping.Carriers.Postal.Usps
         /// </summary>
         public List<RateResult> Consolidate(IEnumerable<List<RateResult>> rates)
         {
-            //TODO: Implement correct merging logic; this just flattens the list
-            return rates.SelectMany(x => x).ToList();
+            List<RateResult> consolidatedRates = new List<RateResult>();
+
+            foreach (RateResult rateResult in rates.SelectMany(r => r))
+            {
+                RateResult foundConsolidatedRate = consolidatedRates.SingleOrDefault(consolidatedRate => ServiceMatches(consolidatedRate, rateResult));
+
+                if (foundConsolidatedRate == null)
+                {
+                    consolidatedRates.Add(rateResult);
+                }
+                else if (foundConsolidatedRate.Amount > rateResult.Amount)
+                {
+                    int indexOfFoundConsolidatedRate = consolidatedRates.IndexOf(foundConsolidatedRate);
+                    consolidatedRates[indexOfFoundConsolidatedRate] = rateResult;
+                }
+                else if (foundConsolidatedRate.Amount == rateResult.Amount)
+                {
+                    AddAccounts(foundConsolidatedRate, rateResult);
+                }
+            }
+
+            return consolidatedRates
+                .OrderBy(GetServiceType)
+                .ThenBy(r => r.Selectable)
+                .ToList();
+        }
+
+        /// <summary>
+        /// Gets the type of the service.
+        /// </summary>
+        private PostalServiceType GetServiceType(RateResult rateResult)
+        {
+            UspsPostalRateSelection rateResultTag = (UspsPostalRateSelection)rateResult.Tag;
+
+            return rateResultTag.ServiceType;
+        }
+
+        /// <summary>
+        /// Adds the accounts specified in the sourceRate to the targetRate's accounts.
+        /// </summary>
+        private void AddAccounts(RateResult targetRate, RateResult sourceRate)
+        {
+            UspsPostalRateSelection targetRateTag = (UspsPostalRateSelection)targetRate.Tag;
+            UspsPostalRateSelection sourceRateTag = (UspsPostalRateSelection)sourceRate.Tag;
+
+            targetRateTag.Accounts.AddRange(sourceRateTag.Accounts);
+        }
+
+        /// <summary>
+        /// If the ConfirmationType and ServiceType of both rates match, return true
+        /// </summary>
+        public bool ServiceMatches(RateResult rateA, RateResult rateB)
+        {
+            UspsPostalRateSelection rateATag = (UspsPostalRateSelection)rateA.Tag;
+            UspsPostalRateSelection rateBTag = (UspsPostalRateSelection)rateB.Tag;
+
+            return rateATag.ServiceType == rateBTag.ServiceType &&
+                rateATag.ConfirmationType == rateBTag.ConfirmationType;
         }
     }
 }
