ee0a14295f92b7f5a38f926e51894ccb889312db
Kevin Croke <k.croke@shipworks.com>
2015-02-05 10:11:29 -0600

    Fix spacing in error messages from Best Rate.

--- Code/ShipWorks/Shipping/Carriers/Postal/Stamps/StampsApiSession.cs
+++ Code/ShipWorks/Shipping/Carriers/Postal/Stamps/StampsApiSession.cs
@@ -394,7 +394,7 @@ namespace ShipWorks.Shipping.Carriers.Postal.Stamps
                 {
                     // Provide a little more context as to which user name/password was incorrect in the case
                     // where there's multiple accounts or Express1 for Stamps is being used to compare rates
-                    string message = string.Format("ShipWorks was unable to connect to {0} with account {1}.{2}{2}Check that your account credentials are correct.",
+                    string message = string.Format("ShipWorks was unable to connect to {0} with account {1}.{2}Check that your account credentials are correct.",
                                     StampsAccountManager.GetResellerName((StampsResellerType)account.StampsReseller),
                                     account.Username,
                                     Environment.NewLine);
@@ -671,7 +671,7 @@ namespace ShipWorks.Shipping.Carriers.Postal.Stamps
                 {
                     // Provide a little more context as to which user name/password was incorrect in the case
                     // where there's multiple accounts or Express1 for Stamps is being used to compare rates
-                    string message = string.Format("ShipWorks was unable to connect to {0} with account {1}.{2}{2}Check that your account credentials are correct.",
+                    string message = string.Format("ShipWorks was unable to connect to {0} with account {1}.{2}Check that your account credentials are correct.",
                                     StampsAccountManager.GetResellerName((StampsResellerType)account.StampsReseller),
                                     account.Username,
                                     Environment.NewLine);
