8f224a9650ec236073104e652821a421b57caed1
John Yedlicka <j.yedlicka@shipworks.com>
2015-02-16 12:49:01 -0600

    Move/rename StampsExpress1PasswordEncryptionStrategy to Usps namespace

--- Code/ShipWorks/Shipping/Carriers/Postal/Usps/Express1/UspsExpress1PasswordEncryptionStrategy.cs
+++ Code/ShipWorks/Shipping/Carriers/Postal/Usps/Express1/UspsExpress1PasswordEncryptionStrategy.cs
@@ -2,13 +2,13 @@
 using Interapptive.Shared.Utility;
 using ShipWorks.Shipping.Carriers.Postal.Express1.Registration;
 
-namespace ShipWorks.Shipping.Carriers.Postal.Stamps.Express1
+namespace ShipWorks.Shipping.Carriers.Postal.Usps.Express1
 {
     /// <summary>
     /// An implementation of the IExpress1PasswordEncryptionStrategy interface that is specific to the 
     /// registration of an Express1 for Stamps.com account.
     /// </summary>
-    public class StampsExpress1PasswordEncryptionStrategy : IExpress1PasswordEncryptionStrategy
+    public class UspsExpress1PasswordEncryptionStrategy : IExpress1PasswordEncryptionStrategy
     {
         /// <summary>
         /// Encrypts the plain text password of the Express1 registration by using the user name
