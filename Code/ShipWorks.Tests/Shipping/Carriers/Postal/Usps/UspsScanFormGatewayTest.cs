8a8f3aeb0975cdff43f42117189a19850743cdbf
Kevin Croke <k.croke@shipworks.com>
2015-02-16 11:26:03 -0600

    Renaming cooresponding test

---
+++ Code/ShipWorks.Tests/Shipping/Carriers/Postal/Usps/UspsScanFormGatewayTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShipWorks.Shipping.Carriers.Postal.Stamps;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.ScanForms;
+using Moq;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Shipping.Carriers.Postal.Usps;
+using ShipWorks.Shipping.Carriers.Postal.Usps.Api.Net;
+
+namespace ShipWorks.Tests.Shipping.Carriers.Postal.Usps
+{
+    [TestClass]
+    public class UspsScanFormGatewayTest
+    {
+        private ScanFormBatch scanFormBatch;
+        private Mock<IScanFormCarrierAccount> carrierAccount;
+        
+        private UspsScanFormGateway testObject;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            carrierAccount = new Mock<IScanFormCarrierAccount>();
+            carrierAccount.Setup(c => c.GetAccountEntity()).Returns(new UspsAccountEntity());
+
+            scanFormBatch = new ScanFormBatch(carrierAccount.Object, null, null);
+
+            testObject = new UspsScanFormGateway(new UspsWebClient(UspsResellerType.None));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UspsException))]
+        public void CreateScanForms_ThrowsStampsException_WhenAccountEntityIsNull_Test()
+        {
+            // Setup the GetAccountEntity method to return a null value
+            carrierAccount.Setup(c => c.GetAccountEntity()).Returns((IEntity2)null);
+
+            testObject.CreateScanForms(scanFormBatch, new List<ShipmentEntity>());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UspsException))]
+        public void CreateScanForms_ThrowsStampsException_WhenShipmentsContainNonStampsShipment_Test()
+        {
+            // Create an Endicia shipment to get the gateway to throw an exception
+            List<ShipmentEntity> shipments = new List<ShipmentEntity>()
+            {
+                new ShipmentEntity()
+                {
+                    Postal = new PostalShipmentEntity() { Endicia = new EndiciaShipmentEntity() }
+                }
+            };
+
+            testObject.CreateScanForms(scanFormBatch, shipments);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UspsException))]
+        public void CreateScanForms_ThrowsStampsException_WhenShipmentsIsNull_Test()
+        {
+            testObject.CreateScanForms(scanFormBatch, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UspsException))]
+        public void CreateScanForms_ThrowsStampsException_WhenShipmentsIsEmpty_Test()
+        {
+            testObject.CreateScanForms(scanFormBatch, new List<ShipmentEntity>());
+        }
+
+        // Can't effectively unit test the rest of this class since it is calling into 
+        // an external dependency that cannot be abstracted
+    }
+}
