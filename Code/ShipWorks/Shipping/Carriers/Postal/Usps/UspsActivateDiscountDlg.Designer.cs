b28a1229489e1fc2f39007578ecbb2efbb4c7869
Andrew Benz <a.benz@shipworks.com>
2015-02-18 08:29:33 -0600

    Fix seed data and align Expedited Account dialog contents

--- Code/ShipWorks/Shipping/Carriers/Postal/Usps/UspsActivateDiscountDlg.Designer.cs
+++ Code/ShipWorks/Shipping/Carriers/Postal/Usps/UspsActivateDiscountDlg.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace ShipWorks.Shipping.Carriers.Postal.Usps
+﻿using System;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Usps
 {
     partial class UspsActivateDiscountDlg
     {
@@ -31,16 +33,16 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UspsActivateDiscountDlg));
             this.close = new System.Windows.Forms.Button();
             this.labelDiscountedPostage = new System.Windows.Forms.Label();
-            this.expeditedLearnMore = new ShipWorks.UI.Controls.LinkControl();
             this.labelDiscountInfo = new System.Windows.Forms.Label();
             this.expeditedSignup = new System.Windows.Forms.Button();
+            this.expeditedLearnMore = new ShipWorks.UI.Controls.LinkControl();
             this.SuspendLayout();
             // 
             // close
             // 
             this.close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.close.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.close.Location = new System.Drawing.Point(353, 161);
+            this.close.Location = new System.Drawing.Point(353, 174);
             this.close.Name = "close";
             this.close.Size = new System.Drawing.Size(75, 23);
             this.close.TabIndex = 1;
@@ -51,36 +53,23 @@
             // 
             this.labelDiscountedPostage.AutoSize = true;
             this.labelDiscountedPostage.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelDiscountedPostage.Location = new System.Drawing.Point(7, 9);
+            this.labelDiscountedPostage.Location = new System.Drawing.Point(10, 12);
             this.labelDiscountedPostage.Name = "labelDiscountedPostage";
             this.labelDiscountedPostage.Size = new System.Drawing.Size(105, 13);
             this.labelDiscountedPostage.TabIndex = 15;
             this.labelDiscountedPostage.Text = "Postage Discount";
             // 
-            // expeditedLearnMore
+            // labelDiscountInfo
             // 
-            this.expeditedLearnMore.AutoSize = true;
-            this.expeditedLearnMore.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.expeditedLearnMore.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Underline);
-            this.expeditedLearnMore.ForeColor = System.Drawing.Color.Blue;
-            this.expeditedLearnMore.Location = new System.Drawing.Point(121, 127);
-            this.expeditedLearnMore.Name = "expeditedLearnMore";
-            this.expeditedLearnMore.Size = new System.Drawing.Size(69, 13);
-            this.expeditedLearnMore.TabIndex = 14;
-            this.expeditedLearnMore.Text = "(Learn more)";
-            this.expeditedLearnMore.Click += new System.EventHandler(this.OnExpedited1LearnMore);
-            // 
-            // labelDiscountInfo1
-            // 
-            this.labelDiscountInfo.Location = new System.Drawing.Point(19, 32);
+            this.labelDiscountInfo.Location = new System.Drawing.Point(29, 37);
             this.labelDiscountInfo.Name = "labelDiscountInfo";
-            this.labelDiscountInfo.Size = new System.Drawing.Size(417, 87);
+            this.labelDiscountInfo.Size = new System.Drawing.Size(379, 101);
             this.labelDiscountInfo.TabIndex = 16;
-            this.labelDiscountInfo.Text = resources.GetString("labelDiscountInfo1.Text");
+            this.labelDiscountInfo.Text = resources.GetString("labelDiscountInfo.Text");
             // 
             // expeditedSignup
             // 
-            this.expeditedSignup.Location = new System.Drawing.Point(22, 122);
+            this.expeditedSignup.Location = new System.Drawing.Point(31, 141);
             this.expeditedSignup.Name = "expeditedSignup";
             this.expeditedSignup.Size = new System.Drawing.Size(94, 23);
             this.expeditedSignup.TabIndex = 13;
@@ -88,13 +77,26 @@
             this.expeditedSignup.UseVisualStyleBackColor = true;
             this.expeditedSignup.Click += new System.EventHandler(this.OnSignup);
             // 
+            // expeditedLearnMore
+            // 
+            this.expeditedLearnMore.AutoSize = true;
+            this.expeditedLearnMore.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.expeditedLearnMore.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Underline);
+            this.expeditedLearnMore.ForeColor = System.Drawing.Color.Blue;
+            this.expeditedLearnMore.Location = new System.Drawing.Point(130, 146);
+            this.expeditedLearnMore.Name = "expeditedLearnMore";
+            this.expeditedLearnMore.Size = new System.Drawing.Size(69, 13);
+            this.expeditedLearnMore.TabIndex = 14;
+            this.expeditedLearnMore.Text = "(Learn more)";
+            this.expeditedLearnMore.Click += new System.EventHandler(this.OnExpedited1LearnMore);
+            // 
             // UspsActivateDiscountDlg
             // 
             this.AcceptButton = this.close;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.close;
-            this.ClientSize = new System.Drawing.Size(443, 192);
+            this.ClientSize = new System.Drawing.Size(443, 205);
             this.Controls.Add(this.labelDiscountedPostage);
             this.Controls.Add(this.expeditedLearnMore);
             this.Controls.Add(this.labelDiscountInfo);
