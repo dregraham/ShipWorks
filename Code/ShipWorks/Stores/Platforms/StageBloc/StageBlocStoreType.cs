715ea429803bd3488ece705850b9f8cde068e41a
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2015-02-19 10:10:18 -0600

    Add StageBloc storetype to ShipWorks

---
+++ Code/ShipWorks/Stores/Platforms/StageBloc/StageBlocStoreType.cs
@@ -0,0 +1,51 @@
+﻿using ShipWorks.ApplicationCore.Logging;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.GenericModule;
+
+namespace ShipWorks.Stores.Platforms.StageBloc
+{
+    /// <summary>
+    /// Store specific integration into ShipWorks
+    /// </summary>
+    public class StageBlocStoreType : GenericModuleStoreType
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public StageBlocStoreType(StoreEntity store) :
+            base(store)
+        {
+
+        }
+
+        /// <summary>
+        /// StoreType enum value
+        /// </summary>
+        public override StoreTypeCode TypeCode
+        {
+            get
+            {
+                return StoreTypeCode.StageBloc;
+            }
+        }
+
+        /// <summary>
+        /// Log request/responses as StageBloc
+        /// </summary>
+        public override ApiLogSource LogSource
+        {
+            get
+            {
+                return ApiLogSource.StageBloc;
+            }
+        }
+
+        /// <summary>
+        /// Gets the help URL to use in the account settings.
+        /// </summary>
+        public override string AccountSettingsHelpUrl
+        {
+            get { return "http://support.shipworks.com/support/solutions/articles/4000045732-connecting-stagebloc-with-shipworks"; }
+        }
+    }
+}
