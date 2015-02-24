dcc0c789fc95e57466fc607ae86c7c4d64e2908e
Tim Hughes <t.hughes@shipworks.com>
2015-02-24 16:51:48 -0600

    Added indexes for the new address validation columns.

--- Code/ShipWorks/Data/Administration/Scripts/Installation/CreateSchema.sql
+++ Code/ShipWorks/Data/Administration/Scripts/Installation/CreateSchema.sql
@@ -600,6 +600,58 @@ PRINT N'Adding [Order].[IX_Auto_ShipSenseHashKey] Index'
 GO
 CREATE NONCLUSTERED INDEX [IX_Auto_ShipSenseHashKey] ON [dbo].[Order] ([ShipSenseHashKey])
 GO
+PRINT N'Creating index [IX_Order_BillAddressValidationStatus] on [dbo].[Order]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Order_BillAddressValidationStatus] ON [dbo].[Order] ([BillAddressValidationStatus] DESC)
+GO
+PRINT N'Creating index [IX_Order_BillMilitaryAddress] on [dbo].[Order]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Order_BillMilitaryAddress] ON [dbo].[Order] ([BillMilitaryAddress] DESC)
+GO
+PRINT N'Creating index [IX_Order_BillPOBox] on [dbo].[Order]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Order_BillPOBox] ON [dbo].[Order] ([BillPOBox] DESC)
+GO
+PRINT N'Creating index [IX_Order_BillResidentialStatus] on [dbo].[Order]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Order_BillResidentialStatus] ON [dbo].[Order] ([BillResidentialStatus] DESC)
+GO
+PRINT N'Creating index [IX_Order_BillUSTerritory] on [dbo].[Order]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Order_BillUSTerritory] ON [dbo].[Order] ([BillUSTerritory] DESC)
+GO
+PRINT N'Creating index [IX_Order_ShipAddressValidationStatus] on [dbo].[Order]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Order_ShipAddressValidationStatus] ON [dbo].[Order] ([ShipAddressValidationStatus] DESC)
+GO
+PRINT N'Creating index [IX_Auto_ShipFirstName] on [dbo].[Order]'
+GO
+-- *********************
+-- Purposely using If Not Exists here because this index may already exist in some db's and we dont' want
+-- to fail on upgrade.
+-- *********************
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[Order]') AND name = N'IX_Auto_ShipFirstName')
+CREATE NONCLUSTERED INDEX [IX_Auto_ShipFirstName] ON [dbo].[Order]
+(
+	[ShipFirstName] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+PRINT N'Creating index [IX_Order_ShipMilitaryAddress] on [dbo].[Order]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Order_ShipMilitaryAddress] ON [dbo].[Order] ([ShipMilitaryAddress] DESC)
+GO
+PRINT N'Creating index [IX_Order_ShipPOBox] on [dbo].[Order]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Order_ShipPOBox] ON [dbo].[Order] ([ShipPOBox] DESC)
+GO
+PRINT N'Creating index [IX_Order_ShipResidentialStatus] on [dbo].[Order]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Order_ShipResidentialStatus] ON [dbo].[Order] ([ShipResidentialStatus] DESC)
+GO
+PRINT N'Creating index [IX_Order_ShipUSTerritory] on [dbo].[Order]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Order_ShipUSTerritory] ON [dbo].[Order] ([ShipUSTerritory] DESC)
+GO
 PRINT N'Adding [Order].[IX_Store_OrderNumberComplete_IsManual] Index'
 GO
 CREATE UNIQUE NONCLUSTERED INDEX [IX_Store_OrderNumberComplete_IsManual] ON [dbo].[Order]
@@ -1010,6 +1062,26 @@ GO
 PRINT N'Creating index [IX_Shipment_ActualLabelFormat] on [dbo].[Shipment]'
 GO
 CREATE NONCLUSTERED INDEX [IX_Shipment_ActualLabelFormat] ON [dbo].[Shipment] ([ActualLabelFormat])
+GO
+PRINT N'Creating index [IX_Shipment_ShipAddressValidationStatus] on [dbo].[Shipment]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Shipment_ShipAddressValidationStatus] ON [dbo].[Shipment] ([ShipAddressValidationStatus] DESC) INCLUDE ([Processed])
+GO
+PRINT N'Creating index [IX_Shipment_ShipMilitaryAddress] on [dbo].[Shipment]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Shipment_ShipMilitaryAddress] ON [dbo].[Shipment] ([ShipMilitaryAddress] DESC)
+GO
+PRINT N'Creating index [IX_Shipment_ShipPOBox] on [dbo].[Shipment]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Shipment_ShipPOBox] ON [dbo].[Shipment] ([ShipPOBox] DESC)
+GO
+PRINT N'Creating index [IX_Shipment_ShipResidentialStatus] on [dbo].[Shipment]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Shipment_ShipResidentialStatus] ON [dbo].[Shipment] ([ShipResidentialStatus] DESC)
+GO
+PRINT N'Creating index [IX_Shipment_ShipUSTerritory] on [dbo].[Shipment]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Shipment_ShipUSTerritory] ON [dbo].[Shipment] ([ShipUSTerritory] DESC)
 GO
 ALTER TABLE [dbo].[Shipment] ENABLE CHANGE_TRACKING
 GO
