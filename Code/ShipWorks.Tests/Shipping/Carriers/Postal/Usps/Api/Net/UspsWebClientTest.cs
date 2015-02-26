8846659b63db881b803dd6035ea71451acb02a50
Tim Hughes <t.hughes@shipworks.com>
2015-02-26 09:24:02 -0600

    Updated to support 3 memo fields, concatenating them with escape sequences from Stamps so that they display on multiple lines.

---
+++ Code/ShipWorks.Tests/Shipping/Carriers/Postal/Usps/Api/Net/UspsWebClientTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.Postal;
+using ShipWorks.Shipping.Carriers.Postal.Usps.Api.Net;
+using ShipWorks.Shipping.Carriers.Postal.Usps.Registration;
+
+namespace ShipWorks.Tests.Shipping.Carriers.Postal.Usps.Api.Net
+{
+    [TestClass]
+    public class UspsWebClientTest
+    {
+        PostalShipmentEntity postalShipment = new PostalShipmentEntity();
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            postalShipment.Memo1 = "memo1";
+            postalShipment.Memo2 = "memo2";
+            postalShipment.Memo3 = "memo3";
+        }
+
+        [TestMethod]
+        public void BuildMemoField_StartsWithMultilineChar_WhenOneMemoFieldHasContent_Test()
+        {
+            postalShipment.Memo1 = "";
+            postalShipment.Memo2 = "";
+            string apiMemoText = UspsWebClient.BuildMemoField(postalShipment);
+
+            Debug.Assert(Regex.Matches(apiMemoText, "\x09").Count == 1);
+        }
+
+        [TestMethod]
+        public void BuildMemoField_StartsWithMultilineChar_WhenTwoMemoFieldsHaveContent_Test()
+        {
+            postalShipment.Memo1 = "";
+            string apiMemoText = UspsWebClient.BuildMemoField(postalShipment);
+
+            Debug.Assert(Regex.Matches(apiMemoText, "\x09").Count == 1);
+        }
+
+        [TestMethod]
+        public void BuildMemoField_StartsWithMultilineChar_WhenThreeMemoFieldsHaveContent_Test()
+        {
+            string apiMemoText = UspsWebClient.BuildMemoField(postalShipment);
+
+            Debug.Assert(Regex.Matches(apiMemoText, "\x09").Count == 1);
+        }
+
+        [TestMethod]
+        public void BuildMemoField_ReturnsTwoWrapChars_WhenThreeMemoFieldsHaveContent_Test()
+        {
+            string apiMemoText = UspsWebClient.BuildMemoField(postalShipment);
+
+            Debug.Assert(Regex.Matches(apiMemoText, "\r\n").Count == 2);
+        }
+
+        [TestMethod]
+        public void BuildMemoField_ReturnsTwoWrapChars_WhenOnlyMemo3FieldHasContent_Test()
+        {
+            postalShipment.Memo1 = "";
+            postalShipment.Memo2 = "";
+            string apiMemoText = UspsWebClient.BuildMemoField(postalShipment);
+
+            Debug.Assert(Regex.Matches(apiMemoText, "\r\n").Count == 2);
+        }
+
+        [TestMethod]
+        public void BuildMemoField_ReturnsOnlyUpTo200Chars_WhenMemoFieldsGreaterThan200CharsEach_Test()
+        {
+            postalShipment.Memo1 = "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789_____";
+            postalShipment.Memo2 = "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789_____";
+            postalShipment.Memo3 = "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789_____";
+            string apiMemoText = UspsWebClient.BuildMemoField(postalShipment);
+
+            Debug.Assert(apiMemoText.Length <= 200);
+        }
+    }
+}
--- Code/ShipWorks/Shipping/Carriers/Postal/Usps/Api/Net/UspsWebClient.cs
+++ Code/ShipWorks/Shipping/Carriers/Postal/Usps/Api/Net/UspsWebClient.cs
@@ -105,7 +105,7 @@ namespace ShipWorks.Shipping.Carriers.Postal.Usps.Api.Net
             {
                 Url = ServiceUrl
             };
-
+            
             return webService;
         }
 
@@ -657,7 +657,6 @@ namespace ShipWorks.Shipping.Carriers.Postal.Usps.Api.Net
             RateV15 rate = CreateRateForProcessing(shipment, account);
             CustomsV3 customs = CreateCustoms(shipment);
             Usps.WebServices.PostageBalance postageBalance;
-            string memo = TemplateTokenProcessor.ProcessTokens(shipment.Postal.Memo1, shipment.ShipmentID).Truncate(200);
 
             // USPS requires that the address in the Rate match that of the request.  Makes sense - but could be different if they auto-cleansed the address.
             rate.ToState = toAddress.State;
@@ -731,7 +730,7 @@ namespace ShipWorks.Shipping.Carriers.Postal.Usps.Api.Net
                         isSampleOnly,
                         thermalType == null ? ImageType.Png : ((thermalType == ThermalLanguage.EPL) ? ImageType.Epl : ImageType.Zpl),
                         EltronPrinterDPIType.Default,
-                        memo, // Memo
+                        BuildMemoField(shipment.Postal), // Memo
                         0, // Cost Code
                         false, // delivery notify
                         null,  // shipment notification
@@ -771,6 +770,21 @@ namespace ShipWorks.Shipping.Carriers.Postal.Usps.Api.Net
 
             string[] labelUrls = labelUrl.Split(' ');
             SaveLabels(shipment, labelUrls);
+        }
+
+        /// <summary>
+        /// Builds the string to send to USPS as the memo.  If multiple memo fields have content, the wrap character is injected
+        /// between each.
+        /// </summary>
+        public static string BuildMemoField(PostalShipmentEntity postalShipment)
+        {
+            string memo1 = TemplateTokenProcessor.ProcessTokens(postalShipment.Memo1, postalShipment.ShipmentID).Truncate(200);
+            string memo2 = TemplateTokenProcessor.ProcessTokens(postalShipment.Memo2, postalShipment.ShipmentID).Truncate(200);
+            string memo3 = TemplateTokenProcessor.ProcessTokens(postalShipment.Memo3, postalShipment.ShipmentID).Truncate(200);
+
+            var memo = string.Format("\x09{0}\r\n{1}\r\n{2}", memo1, memo2, memo3).Truncate(200);
+            
+            return memo;
         }
 
         /// <summary>
