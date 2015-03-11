383812a8521b6f22405ad0174a7e8a0221ca9792
Tim Hughes <t.hughes@shipworks.com>
2015-03-11 12:15:51 -0500

    Changed MwsClient.GetCarrierName to be public so that we could write unit tests.  Added unit tests for Usps/Endicia/Other with DHL/Consolidators/FirstClass

--- Code/ShipWorks.Tests/Stores/Amazon/MwsClientTests.cs
+++ Code/ShipWorks.Tests/Stores/Amazon/MwsClientTests.cs
@@ -6,12 +6,28 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ShipWorks.Stores.Platforms.Amazon.Mws;
 using ShipWorks.Data.Model.EntityClasses;
 using Interapptive.Shared.Utility;
+using ShipWorks.Shipping;
+using ShipWorks.Shipping.Carriers.Postal;
 
 namespace ShipWorks.Tests.Stores.Amazon
 {
     [TestClass]
     public class MwsClientTests
     {
+        private ShipmentEntity shipmentEntity;
+        private AmazonOrderEntity orderEntity;
+        private PostalShipmentEntity postalShipmentEntity;
+        private OtherShipmentEntity otherShipmentEntity;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            orderEntity = new AmazonOrderEntity { OrderNumber = 123456 };
+            postalShipmentEntity = new PostalShipmentEntity { Service = (int)PostalServiceType.FirstClass };
+            otherShipmentEntity = new OtherShipmentEntity { Carrier = "Some other carrier", Service = "Fast Ground" };
+            shipmentEntity = new ShipmentEntity { Order = orderEntity, TrackingNumber = "ABCD1234", ShipDate = DateTime.UtcNow, ShipmentType = (int)ShipmentTypeCode.Usps, Postal = postalShipmentEntity };
+        }
+
         [TestMethod]
         public void ClockSyncTest()
         {
@@ -31,5 +47,68 @@ namespace ShipWorks.Tests.Stores.Amazon
 
             Assert.AreEqual(signature, signed);
         }
+
+        [TestMethod]
+        public void GetCarrierName_ReturnsUsps_WhenUspsAndFirstClass_Test()
+        {
+            shipmentEntity.ShipmentType = (int)ShipmentTypeCode.Usps;
+            string carrierName = AmazonMwsClient.GetCarrierName(shipmentEntity, ShipmentTypeCode.Usps);
+
+            Assert.AreEqual("USPS", carrierName);
+        }
+
+        [TestMethod]
+        public void GetCarrierName_ReturnsUsps_WhenEndiciaAndFirstClass_Test()
+        {
+            shipmentEntity.ShipmentType = (int)ShipmentTypeCode.Endicia;
+            string carrierName = AmazonMwsClient.GetCarrierName(shipmentEntity, ShipmentTypeCode.Usps);
+
+            Assert.AreEqual("USPS", carrierName);
+        }
+
+        [TestMethod]
+        public void GetCarrierName_ReturnsDhlGlobalMail_WhenEndiciaAndDhl_Test()
+        {
+            shipmentEntity.ShipmentType = (int)ShipmentTypeCode.Endicia;
+            postalShipmentEntity.Service = (int)PostalServiceType.DhlParcelStandard;
+
+            string carrierName = AmazonMwsClient.GetCarrierName(shipmentEntity, ShipmentTypeCode.Endicia);
+
+            Assert.AreEqual("DHL Global Mail", carrierName);
+        }
+
+        [TestMethod]
+        public void GetCarrierName_ReturnsDhlGlobalMail_WhenUspsAndDhl_Test()
+        {
+            shipmentEntity.ShipmentType = (int)ShipmentTypeCode.Usps;
+            postalShipmentEntity.Service = (int)PostalServiceType.DhlParcelStandard;
+
+            string carrierName = AmazonMwsClient.GetCarrierName(shipmentEntity, ShipmentTypeCode.Usps);
+
+            Assert.AreEqual("DHL Global Mail", carrierName);
+        }
+
+        [TestMethod]
+        public void GetCarrierName_ReturnsConsolidator_WhenEndiciaAndConsolidator_Test()
+        {
+            shipmentEntity.ShipmentType = (int) ShipmentTypeCode.Endicia;
+            postalShipmentEntity.Service = (int)PostalServiceType.ConsolidatorDomestic;
+
+            string carrierName = AmazonMwsClient.GetCarrierName(shipmentEntity, ShipmentTypeCode.Endicia);
+
+            Assert.AreEqual("Consolidator", carrierName);
+        }
+
+        [TestMethod]
+        public void GetCarrierName_ReturnsOtherCarrierDesc_WhenOther_Test()
+        {
+            shipmentEntity.ShipmentType = (int)ShipmentTypeCode.Other;
+            shipmentEntity.Other = otherShipmentEntity;
+
+            string carrierName = AmazonMwsClient.GetCarrierName(shipmentEntity, ShipmentTypeCode.Endicia);
+
+            Assert.AreEqual("Some other carrier", carrierName);
+        }
+
     }
 }
