dcf78774d4a636a053e4f911ac6bfd3d4f328191
John Yedlicka <j.yedlicka@shipworks.com>
2015-03-16 16:20:18 -0500

    Map columns to attributes on the order items portion of the mapping control

--- Code/ShipWorks/Data/Import/Spreadsheet/OrderSchema/GenericSpreadsheetOrderItemsSchemaGroup.cs
+++ Code/ShipWorks/Data/Import/Spreadsheet/OrderSchema/GenericSpreadsheetOrderItemsSchemaGroup.cs
@@ -39,12 +39,11 @@ namespace ShipWorks.Data.Import.Spreadsheet.OrderSchema
             GenericSpreadsheetOrderMapSettings settings = (GenericSpreadsheetOrderMapSettings) _settings;
 
             int fieldSets = (settings.MultiItemStrategy == GenericSpreadsheetOrderMultipleItemStrategy.SingleLine) ? settings.SingleLineCount : 1;
-
             List<GenericSpreadsheetTargetField> fields = new List<GenericSpreadsheetTargetField>();
 
             for (int i = 0; i < fieldSets; i++)
             {
-                foreach (GenericSpreadsheetTargetField baseField in baseFields)
+                foreach (GenericSpreadsheetTargetField baseField in baseFields.Where(f => f.Identifier != "Item.Attribute.Name"))
                 {
                     string display = baseField.DisplayName;
 
@@ -56,6 +55,17 @@ namespace ShipWorks.Data.Import.Spreadsheet.OrderSchema
 
                     fields.Add(new GenericSpreadsheetTargetField(string.Format("{0}.{1}", baseField.Identifier, (i + 1)), display, baseField.DataType));
                 }
+            }
+
+            // Attributes - treat these differently since they are just an add-on to the items and really aren't mapped to 
+            // a column on the order item itself. 
+            // TODO: Maybe go back and remove the attribute from the target field group entirely
+            for (int i = 0; i < settings.AttributeCountPerLine; i++)
+            {
+                fields.Add(new GenericSpreadsheetTargetField(
+                    string.Format("{0}.{1}", "Item.Attribute.Name", (i + 1)),       // Item.Attribute.Name.1
+                    string.Format("Attribute {0}", (i + 1)),                        // Attribute 1
+                    typeof (string)));
             }
 
             return fields;
