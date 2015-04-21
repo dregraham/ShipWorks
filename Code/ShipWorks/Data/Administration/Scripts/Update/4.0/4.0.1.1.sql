fb20e6addb1464b9bebf5d5a2ad5ee06e3a061b4
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2015-04-21 08:00:53 -0500

    Update existing NoMoreRack store URL to latest api

---
+++ Code/ShipWorks/Data/Administration/Scripts/Update/4.0/4.0.1.1.sql
@@ -0,0 +1,4 @@
+﻿UPDATE       GenericModuleStore
+SET                ModuleUrl = REPLACE(ModuleUrl, 'vendor-api.nomorerack.com', 'vendor-api.choxi.com')
+WHERE        (ModuleUrl LIKE '%vendor-api.nomorerack.com%') AND (ModulePlatform = 'Nomorerack')
+GO
