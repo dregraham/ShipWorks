76354a6c7e173e2dd73432cc9c6fc0f0283889ef
Kevin Croke <k.croke@shipworks.com>
2015-06-17 15:31:08 -0500

    Comments and cleanup

--- Code/ShipWorks/Common/Net/ExtendedWebBrowser.cs
+++ Code/ShipWorks/Common/Net/ExtendedWebBrowser.cs
@@ -6,12 +6,18 @@ using System.Windows.Forms;
 
 namespace ShipWorks.Common.Net
 {
+    /// <summary>
+    /// Extends Web Browser to support custom UserAgent.
+    /// </summary>
     public class ExtendedWebBrowser : WebBrowser
     {
         bool renavigating = false;
 
         public string UserAgent { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExtendedWebBrowser"/> class.
+        /// </summary>
         public ExtendedWebBrowser()
         {
             DocumentCompleted += SetupBrowser;
@@ -20,19 +26,19 @@ namespace ShipWorks.Common.Net
             Navigate("about:blank");
         }
 
+        /// <summary>
+        /// Setups the browser.
+        /// </summary>
         void SetupBrowser(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             DocumentCompleted -= SetupBrowser;
             SHDocVw.WebBrowser xBrowser = (SHDocVw.WebBrowser)ActiveXInstance;
             xBrowser.BeforeNavigate2 += BeforeNavigate;
-            DocumentCompleted += PageLoaded;
         }
 
-        void PageLoaded(object sender, WebBrowserDocumentCompletedEventArgs e)
-        {
-
-        }
-
+        /// <summary>
+        /// Befores the navigate.
+        /// </summary>
         void BeforeNavigate(object pDisp, ref object url, ref object flags, ref object targetFrameName,
             ref object postData, ref object headers, ref bool cancel)
         {
