cd1f58bae2577700cba8744915d5e6fbff9c16b6
Kevin Croke <k.croke@shipworks.com>
2015-06-24 09:17:49 -0500

    Create StoreFilterRepsoitorySaveResult

---
+++ Code/ShipWorks/Stores/StoreFilterRepositorySaveResult.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Stores
+{
+    /// <summary>
+    /// The result of creating filters
+    /// </summary>
+    public class StoreFilterRepositorySaveResult
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether [folder created].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [folder created]; otherwise, <c>false</c>.
+        /// </value>
+        public bool FolderCreated { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the store folder.
+        /// </summary>
+        /// <value>
+        /// The name of the store folder.
+        /// </value>
+        public string StoreFolderName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created filters.
+        /// </summary>
+        /// <value>
+        /// The created filters.
+        /// </value>
+        public List<FilterEntity> CreatedFilters { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collision filters - These were not created
+        /// </summary>
+        /// <value>
+        /// The collision filters that couldn't be created
+        /// </value>
+        public List<FilterEntity> CollisionFilters { get; set; }
+    }
+}
