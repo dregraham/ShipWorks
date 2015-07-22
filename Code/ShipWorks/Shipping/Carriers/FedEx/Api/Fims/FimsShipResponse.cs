ec7c94a6d1d909ec5faa902ffb1f32afc15f7a12
Tim Hughes <t.hughes@shipworks.com>
2015-07-22 14:45:50 -0500

    Added web client interface, IFimsWebClient for processing FedEx FIMS shipments.  Also added FIMS request/response interfaces/concrete classes.

---
+++ Code/ShipWorks/Shipping/Carriers/FedEx/Api/Fims/FimsShipResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShipWorks.Shipping.Carriers.FedEx.Api.Fims
+{
+    /// <summary>
+    /// Response data from a FIMS ship request.
+    /// </summary>
+    public class FimsShipResponse : IFimsShipResponse
+    {
+    }
+}
---
+++ Code/ShipWorks/Shipping/Carriers/FedEx/Api/Fims/IFimsShipResponse.cs
@@ -0,0 +1,9 @@
+namespace ShipWorks.Shipping.Carriers.FedEx.Api.Fims
+{
+    /// <summary>
+    /// Response data from a FIMS ship request.
+    /// </summary>
+    public interface IFimsShipResponse
+    {
+    }
+}
