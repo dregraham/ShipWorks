2ae1c79ab28635e23f1c92d2427a56126f804b4c
Andrew Benz <a.benz@shipworks.com>
2015-08-05 10:06:52 -0500

    Add AmazonShipmentType and type code

---
+++ Code/ShipWorks/Shipping/Carriers/Amazon/AmazonShipmentType.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.BestRate;
+using ShipWorks.Shipping.Editing;
+using ShipWorks.Shipping.Editing.Rating;
+using ShipWorks.Shipping.ShipSense.Packaging;
+
+namespace ShipWorks.Shipping.Carriers.Amazon
+{
+    /// <summary>
+    /// Amazon implementation of shipment type
+    /// </summary>
+    public class AmazonShipmentType : ShipmentType
+    {
+        /// <summary>
+        /// Shipment type code
+        /// </summary>
+        public override ShipmentTypeCode ShipmentTypeCode
+        {
+            get
+            {
+                return ShipmentTypeCode.Amazon;
+            }
+        }
+
+        /// <summary>
+        /// Creates the UserControl that is used to edit service options for the shipment type
+        /// </summary>
+        /// <param name="rateControl">A handle to the rate control so the selected rate can be updated when
+        /// a change to the shipment, such as changing the service type, matches a rate in the control</param>
+        protected override ServiceControlBase InternalCreateServiceControl(RateControl rateControl)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the package adapter for the shipment.
+        /// </summary>
+        public override IEnumerable<IPackageAdapter> GetPackageAdapters(ShipmentEntity shipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Ensures that the carrier specific data for the shipment, such as the FedEx data, are loaded for the shipment.  If the data
+        /// already exists, nothing is done: it is not refreshed.  This method can throw SqlForeignKeyException if the root shipment
+        /// or order has been deleted, ORMConcurrencyException if the shipment had been edited elsewhere, and ObjectDeletedException if the shipment
+        /// had been deleted.
+        /// </summary>
+        public override void LoadShipmentData(ShipmentEntity shipment, bool refreshIfPresent)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Get the carrier specific description of the shipping service used. The carrier specific data must already exist
+        /// when this method is called.
+        /// </summary>
+        public override string GetServiceDescription(ShipmentEntity shipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Get detailed information about the parcel in a generic way that can be used accross shipment types
+        /// </summary>
+        public override ShipmentParcel GetParcelDetail(ShipmentEntity shipment, int parcelIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Process the shipment
+        /// </summary>
+        public override void ProcessShipment(ShipmentEntity shipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets an instance to the best rate shipping broker for a provider based on the shipment configuration.
+        /// </summary>
+        /// <param name="shipment">The shipment.</param>
+        /// <returns>An instance of an IBestRateShippingBroker.</returns>
+        public override IBestRateShippingBroker GetShippingBroker(ShipmentEntity shipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the processing synchronizer to be used during the PreProcessing of a shipment.
+        /// </summary>
+        public override IShipmentProcessingSynchronizer GetProcessingSynchronizer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
