8deb3beecb119e2421649949b8ac347dac959e5a
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2015-08-10 14:43:00 -0500

    Update AmazonShippingWebClint to validate credentials via API
    Use AmazonMwsClient to test credentials

--- Code/ShipWorks/Shipping/Carriers/Amazon/Api/AmazonShippingWebClient.cs
+++ Code/ShipWorks/Shipping/Carriers/Amazon/Api/AmazonShippingWebClient.cs
@@ -1,4 +1,8 @@
-﻿namespace ShipWorks.Shipping.Carriers.Amazon.Api
+﻿using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.Amazon;
+using ShipWorks.Stores.Platforms.Amazon.Mws;
+
+namespace ShipWorks.Shipping.Carriers.Amazon.Api
 {
     /// <summary>
     /// Amazon shipping api client
@@ -10,7 +14,31 @@
         /// </summary>
         public AmazonValidateCredentialsResponse ValidateCredentials(string merchantId, string authToken)
         {
-            return AmazonValidateCredentialsResponse.Succeeded();
+            // Create a fake store instance because the current
+            // AmazonMwsClient requires a store to be passed
+            // use US api 
+            AmazonStoreEntity fakeStore = new AmazonStoreEntity() 
+            {
+                MerchantID = merchantId, 
+                AuthToken = authToken,
+                AmazonApiRegion = "US"
+            };
+
+            using (AmazonMwsClient client = new AmazonMwsClient(fakeStore))
+            {
+                try
+                {
+                    // Request a list of marketplaces to test credentials
+                    client.GetMarketplaces();
+                    return AmazonValidateCredentialsResponse.Succeeded();
+                }
+                catch (AmazonException ex)
+                {
+                    // Something must be wrong with the credentails 
+                    return AmazonValidateCredentialsResponse.Failed(ex.Message);
+                }
+            }
+
         }
     }
 }
