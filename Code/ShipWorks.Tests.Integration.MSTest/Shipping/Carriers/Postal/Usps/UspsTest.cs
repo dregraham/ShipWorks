d531f4c0d8768d184d449a75806819f558066fb2
Andrew Benz <a.benz@shipworks.com>
2015-08-14 15:48:08 -0500

    Update USPS integration tests to use the xUnit constructs

--- Code/ShipWorks.Tests.Integration.MSTest/Shipping/Carriers/Postal/Usps/UspsTest.cs
+++ Code/ShipWorks.Tests.Integration.MSTest/Shipping/Carriers/Postal/Usps/UspsTest.cs
@@ -10,248 +10,146 @@ using ShipWorks.Shipping.Carriers.Postal.Usps.Express1.Net;
 using ShipWorks.Shipping.Carriers.Postal.WebTools;
 using ShipWorks.Shipping.Editing.Rating;
 using System.Data;
+using Xunit.Abstractions;
 
 namespace ShipWorks.Tests.Integration.MSTest.Shipping.Carriers.Postal.Usps
 {
     public class UspsTest : DataDrivenIntegrationTestBase
     {
         private const bool justLabels = true;
+        private readonly ITestOutputHelper output;
 
-        //// [DataSource("DataSource_Ship_Stamps")]
-        //// [DeploymentItem("DataSources\\Usps.xlsx")]
+        public UspsTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        // [DataSource("DataSource_Ship_Stamps")]
+        [ExcelData("DataSources\\Usps.xlsx", "Stamps")]
         [Trait("Category", "USPS")]
         [Theory]
         public void ProcessBatch_UspsAndExpress1Usps_Test(DataRow row)
         {
             UspsMapping testObject = new UspsMapping();
 
-            try
+            PostalWebUtility.UseTestServer = true;
+            UspsWebClient.UseTestServer = true;
+            Express1UspsWebClient.UseTestServer = true;
+
+            if (PopulateTestObject(row, testObject, UspsMapping.Mapping) &&
+                (testObject.IsSaveLabel || !justLabels))
             {
-                PostalWebUtility.UseTestServer = true;
-                UspsWebClient.UseTestServer = true;
-                Express1UspsWebClient.UseTestServer = true;
+                output.WriteLine(@"{0}{0}--------------------------------------------------------------------------------", Environment.NewLine);
+                output.WriteLine(string.Format("Executing Test ID {0}", row["TestID"]));
+                output.WriteLine(@"--------------------------------------------------------------------------------{0}{0}", Environment.NewLine);
 
-                if (PopulateTestObject(row, testObject, UspsMapping.Mapping) &&
-                    (testObject.IsSaveLabel || !justLabels))
+                UspsResellerType resellerType;
+
+                if (testObject.ShipmentType == "15")
                 {
-                    Console.WriteLine(@"{0}{0}--------------------------------------------------------------------------------", Environment.NewLine);
-                    Console.WriteLine(string.Format("Executing Test ID {0}", row["TestID"]));
-                    Console.WriteLine(@"--------------------------------------------------------------------------------{0}{0}", Environment.NewLine);
-
-                    UspsResellerType resellerType;
-
-                    if (testObject.ShipmentType == "15")
-                    {
-                        resellerType = UspsResellerType.None;
-                    }
-                    else
-                    {
-                        resellerType = UspsResellerType.Express1;
-                    }
-
-                    Exception exception = null;
-
-                    // Yes, this looks stupid, but we get a debug assert if we aren't in a transaction...
-                    using (SqlAdapter adapter = new SqlAdapter(true))
-                    {
-                        try
-                        {
-                            testObject.Ship(resellerType);
-                        }
-                        catch (Exception ex)
-                        {
-                            exception = ex;
-                        }
-                    }
-
-                    if (exception != null)
-                    {
-                        throw exception;
-                    }
+                    resellerType = UspsResellerType.None;
                 }
-            }
-            catch (Exception ex)
-            {
-                if (string.IsNullOrWhiteSpace(row[0].ToString().Trim()))
+                else
                 {
-                    // The test framework doesn't seem to know when to stop...so if we don't have a SaveLabel populated, return with no error. 
-                    return;
+                    resellerType = UspsResellerType.Express1;
                 }
 
-                Console.WriteLine(string.Format("Error running Test ID {0}.  Error message: {1}", row["TestID"], ex.Message));
-
-                // We have a legitimate exception
-                throw;
+                // Yes, this looks stupid, but we get a debug assert if we aren't in a transaction...
+                using (SqlAdapter adapter = new SqlAdapter(true))
+                {
+                    testObject.Ship(resellerType);
+                }
             }
         }
 
-        // [DataSource("DataSource_Ship_Stamps")]
-        // [DeploymentItem("DataSources\\Usps.xlsx")]
+        [ExcelData("DataSources\\Usps.xlsx", "Stamps")]
         [Trait("Category", "USPS")]
         [Theory]
         public void GetRates_UspsAndExpress1Usps_Test(DataRow row)
         {
             UspsMapping testObject = new UspsMapping();
 
-            try
+            PostalWebUtility.UseTestServer = true;
+            UspsWebClient.UseTestServer = true;
+            Express1UspsWebClient.UseTestServer = true;
+
+            if (PopulateTestObject(row, testObject, UspsMapping.Mapping) &&
+                (testObject.IsSaveLabel || !justLabels))
             {
-                PostalWebUtility.UseTestServer = true;
-                UspsWebClient.UseTestServer = true;
-                Express1UspsWebClient.UseTestServer = true;
+                output.WriteLine(@"{0}{0}--------------------------------------------------------------------------------", Environment.NewLine);
+                output.WriteLine(string.Format("Executing Test ID {0}", row["TestID"]));
+                output.WriteLine(@"--------------------------------------------------------------------------------{0}{0}", Environment.NewLine);
 
-                if (PopulateTestObject(row, testObject, UspsMapping.Mapping) &&
-                    (testObject.IsSaveLabel || !justLabels))
+                UspsResellerType resellerType;
+
+                if (testObject.ShipmentType == "15")
                 {
-                    Console.WriteLine(@"{0}{0}--------------------------------------------------------------------------------", Environment.NewLine);
-                    Console.WriteLine(string.Format("Executing Test ID {0}", row["TestID"]));
-                    Console.WriteLine(@"--------------------------------------------------------------------------------{0}{0}", Environment.NewLine);
-
-                    UspsResellerType resellerType;
-
-                    if (testObject.ShipmentType == "15")
-                    {
-                        resellerType = UspsResellerType.None;
-                    }
-                    else
-                    {
-                        resellerType = UspsResellerType.Express1;
-                    }
-
-                    List<RateResult> rateResults = testObject.GetRates(resellerType);
-
-                    Debug.Assert(rateResults != null);
-                    Debug.Assert(rateResults.Any());
+                    resellerType = UspsResellerType.None;
                 }
-            }
-            catch (Exception ex)
-            {
-                if (string.IsNullOrWhiteSpace(row[0].ToString().Trim()))
+                else
                 {
-                    // The test framework doesn't seem to know when to stop...so if we don't have a SaveLabel populated, return with no error. 
-                    return;
+                    resellerType = UspsResellerType.Express1;
                 }
 
-                Console.WriteLine(string.Format("Error running Test ID {0}.  Error message: {1}", row["TestID"], ex.Message));
+                List<RateResult> rateResults = testObject.GetRates(resellerType);
 
-                // We have a legitimate exception
-                throw;
+                Assert.NotNull(rateResults);
+                Assert.True(rateResults.Any());
             }
         }
 
-        // [DataSource("DataSource_Ship_PurchasePostage")]
-        // [DeploymentItem("DataSources\\Usps.xlsx")]
+        [ExcelData("DataSources\\Usps.xlsx", "PurchasePostage")]
         [Trait("Category", "USPS")]
         [Theory]
         public void PurchasePostage_UspsAndExpress1Usps_Test(DataRow row)
         {
             UspsMapping testObject = new UspsMapping();
 
-            try
+            PostalWebUtility.UseTestServer = true;
+            UspsWebClient.UseTestServer = true;
+            Express1UspsWebClient.UseTestServer = true;
+
+            if (PopulateTestObject(row, testObject, UspsMapping.Mapping) &&
+                (testObject.IsSaveLabel || !justLabels))
             {
-                PostalWebUtility.UseTestServer = true;
-                UspsWebClient.UseTestServer = true;
-                Express1UspsWebClient.UseTestServer = true;
+                output.WriteLine(@"{0}{0}--------------------------------------------------------------------------------", Environment.NewLine);
+                output.WriteLine(string.Format("Executing Test ID {0}", row["TestID"]));
+                output.WriteLine(@"--------------------------------------------------------------------------------{0}{0}", Environment.NewLine);
 
-                if (PopulateTestObject(row, testObject, UspsMapping.Mapping) &&
-                    (testObject.IsSaveLabel || !justLabels))
+                // Yes, this looks stupid, but we get a debug assert if we aren't in a transaction...
+                using (SqlAdapter adapter = new SqlAdapter(true))
                 {
-                    Console.WriteLine(@"{0}{0}--------------------------------------------------------------------------------", Environment.NewLine);
-                    Console.WriteLine(string.Format("Executing Test ID {0}", row["TestID"]));
-                    Console.WriteLine(@"--------------------------------------------------------------------------------{0}{0}", Environment.NewLine);
-
-                    Exception exception = null;
-
-                    // Yes, this looks stupid, but we get a debug assert if we aren't in a transaction...
-                    using (SqlAdapter adapter = new SqlAdapter(true))
-                    {
-                        try
-                        {
-                            testObject.PurchasePostage(25M);
-                        }
-                        catch (Exception ex)
-                        {
-                            exception = ex;
-                        }
-                    }
-
-                    if (exception != null)
-                    {
-                        throw exception;
-                    }
+                    testObject.PurchasePostage(25M);
                 }
-            }
-            catch (Exception ex)
-            {
-                if (string.IsNullOrWhiteSpace(row[0].ToString().Trim()))
-                {
-                    // The test framework doesn't seem to know when to stop...so if we don't have a SaveLabel populated, return with no error. 
-                    return;
-                }
-
-                Console.WriteLine(string.Format("Error running Test ID {0}.  Error message: {1}", row["TestID"], ex.Message));
-
-                // We have a legitimate exception
-                throw;
             }
         }
 
-        // [DataSource("DataSource_Ship_PurchasePostage")]
-        // [DeploymentItem("DataSources\\Usps.xlsx")]
+        [ExcelData("DataSources\\Usps.xlsx", "PurchasePostage")]
         [Trait("Category", "USPS")]
         [Theory]
         public void CheckPostage_UspsAndExpress1Usps_Test(DataRow row)
         {
             UspsMapping testObject = new UspsMapping();
 
-            try
+            PostalWebUtility.UseTestServer = true;
+            UspsWebClient.UseTestServer = true;
+            Express1UspsWebClient.UseTestServer = true;
+
+            if (PopulateTestObject(row, testObject, UspsMapping.Mapping) &&
+                (testObject.IsSaveLabel || !justLabels))
             {
-                PostalWebUtility.UseTestServer = true;
-                UspsWebClient.UseTestServer = true;
-                Express1UspsWebClient.UseTestServer = true;
-
-                if (PopulateTestObject(row, testObject, UspsMapping.Mapping) &&
-                    (testObject.IsSaveLabel || !justLabels))
+                output.WriteLine(@"{0}{0}--------------------------------------------------------------------------------", Environment.NewLine);
+                output.WriteLine(string.Format("Executing Test ID {0}", row["TestID"]));
+                output.WriteLine(@"--------------------------------------------------------------------------------{0}{0}", Environment.NewLine);
+                
+                // Yes, this looks stupid, but we get a debug assert if we aren't in a transaction...
+                using (SqlAdapter adapter = new SqlAdapter(true))
                 {
-                    Console.WriteLine(@"{0}{0}--------------------------------------------------------------------------------", Environment.NewLine);
-                    Console.WriteLine(string.Format("Executing Test ID {0}", row["TestID"]));
-                    Console.WriteLine(@"--------------------------------------------------------------------------------{0}{0}", Environment.NewLine);
+                    decimal balance = -1;
+                    balance = testObject.CheckPostage();
 
-                    Exception exception = null;
-
-                    // Yes, this looks stupid, but we get a debug assert if we aren't in a transaction...
-                    using (SqlAdapter adapter = new SqlAdapter(true))
-                    {
-                        try
-                        {
-                            decimal balance = -1;
-                            balance = testObject.CheckPostage();
-
-                            Debug.Assert(balance != -1);
-                        }
-                        catch (Exception ex)
-                        {
-                            exception = ex;
-                        }
-                    }
-
-                    if (exception != null)
-                    {
-                        throw exception;
-                    }
+                    Assert.NotEqual(-1, balance);
                 }
-            }
-            catch (Exception ex)
-            {
-                if (string.IsNullOrWhiteSpace(row[0].ToString().Trim()))
-                {
-                    // The test framework doesn't seem to know when to stop...so if we don't have a SaveLabel populated, return with no error. 
-                    return;
-                }
-
-                Console.WriteLine(string.Format("Error running Test ID {0}.  Error message: {1}", row["TestID"], ex.Message));
-
-                // We have a legitimate exception
-                throw;
             }
         }
     }
