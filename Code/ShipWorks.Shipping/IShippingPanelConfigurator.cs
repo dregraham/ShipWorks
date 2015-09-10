394e5a9eec544f26558ce2a4de608edf18bdd329
Tim Hughes <t.hughes@shipworks.com>
2015-09-10 10:10:08 -0500

    Initial implementation for the shipping panel configurator

---
+++ Code/ShipWorks.Shipping/IShippingPanelConfigurator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Users.Security;
+
+namespace ShipWorks.Shipping
+{
+    /// <summary>
+    /// Interface for settings needed by the Shipping Panel
+    /// </summary>
+    public interface IShippingPanelConfigurator
+    {
+        /// <summary>
+        /// Wraps a call to determine if a user has permission on an entity
+        /// </summary>
+        bool UserHasPermission(PermissionType permissionType, long entityID);
+
+        /// <summary>
+        /// TBD
+        /// </summary>
+        /// <param name="shipment"></param>
+        /// <returns></returns>
+        bool GetAddressValidation(ShipmentEntity shipment);
+
+        /// <summary>
+        /// Wraps ShippingSettings.AutoCreateShipments
+        /// </summary>
+        bool AutoCreateShipments { get; }
+    }
+}
---
+++ Code/ShipWorks.Shipping/ShippingPanelConfigurator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Settings;
+using ShipWorks.Users;
+using ShipWorks.Users.Security;
+
+namespace ShipWorks.Shipping
+{
+    /// <summary>
+    /// Interface for settings needed by the Shipping Panel
+    /// </summary>
+    public class ShippingPanelConfigurator : IShippingPanelConfigurator
+    {
+        /// <summary>
+        /// Wraps a call to determine if a user has permission on an entity
+        /// </summary>
+        public bool UserHasPermission(PermissionType permissionType, long orderID)
+        {
+            return UserSession.Security.HasPermission(permissionType, orderID);
+        }
+
+        /// <summary>
+        /// TBD
+        /// </summary>
+        /// <param name="shipment"></param>
+        /// <returns></returns>
+        public bool GetAddressValidation(ShipmentEntity shipment)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Wraps ShippingSettings.AutoCreateShipments
+        /// </summary>
+        public bool AutoCreateShipments
+        {
+            get
+            {
+                return ShippingSettings.Fetch().AutoCreateShipments;
+            }
+        }
+    }
+}
