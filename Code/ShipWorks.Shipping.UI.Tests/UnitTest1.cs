2230c69ec70e3175f1b668da6dc539a8fc9e275f
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2015-09-18 15:33:34 -0500

    Add ShipWorks.Shipping.UI.Tests

---
+++ Code/ShipWorks.Shipping.UI.Tests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping;
+using Xunit;
+using Autofac.Extras.Moq;
+using ShipWorks.Shipping.Carriers.Other;
+using ShipWorks.Shipping.UI;
+using ShipWorks.Shipping.Carriers;
+using ShipWorks.Shipping.Tests;
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ShipWorks.Shipping.UI.Tests
+{
+    public class UnitTest1
+    {
+        ShipmentEntity shipment = new ShipmentEntity();
+
+        [Fact]
+        public void Save_UpdatesShipmentEntity_WhenTotalWeightChanged_Test()
+        {
+            using (var mock = AutoMockExtensions.GetLooseThatReturnsMocks())
+            {
+                ShipmentViewModel testObject = mock.Create<ShipmentViewModel>();
+                testObject.Load(shipment);
+
+                testObject.TotalWeight = 2.93;
+                testObject.Save(shipment);
+
+                Assert.Equal(2.93, shipment.TotalWeight);
+            }
+        }
+    }
+}
---
+++ Code/ShipWorks.Shipping.UI.Tests/ValueConverters/DoubleToWeightStringConverterTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using ShipWorks.Shipping.UI.ValueConverters;
+using Xunit;
+
+namespace ShipWorks.Shipping.UI.Tests.ValueConverters
+{
+    class DoubleToWeightStringConverterTests
+    {
+        //[Fact]
+        //public void Convert_DoubleToStringWeight_Test()
+        //{
+        //    DoubleToWeightStringConverter converter = new DoubleToWeightStringConverter();
+
+        //    converter.Convert(1, null, null, CultureInfo.CurrentCulture);
+            
+        //}
+    }
+}
