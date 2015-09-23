ef2a4d60690a4c0edef4fc1a222356ce853dea93
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2015-09-23 14:34:12 -0500

    Add ValidationRule and error template to ShipmentControl

---
+++ Code/ShipWorks.Shipping.UI/ValidationRules/WeightValidationRule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace ShipWorks.Shipping.UI.ValidationRules
+{
+    public class WeightValidationRule : ValidationRule
+    {
+        // Match any integer or floating point number
+        static string numberRegex = @"-?([0-9]+(\.[0-9]*)?|\.[0-9]+)";
+
+        // Match the case where both pounds and ounces are present
+        readonly Regex poundsOzRegex = new Regex(
+            @"^(?<Pounds>" + numberRegex + @")\s*(lbs.|lbs|lb.|lb|l|pounds|pound)?\s+" +
+            @"(?<Ounces>" + numberRegex + @")\s*(ounces|ounce|oz.|oz|o)?\s*$",
+            RegexOptions.IgnorePatternWhitespace | RegexOptions.IgnoreCase);
+
+        // Match the case were only ounces is present
+        readonly Regex ouncesRegex = new Regex(
+            @"^(?<Ounces>" + numberRegex + @")\s*(ounces|ounce|oz.|oz|o)\s*$",
+            RegexOptions.IgnorePatternWhitespace | RegexOptions.IgnoreCase);
+
+        // Match the case were only pounds is present
+        readonly Regex poundsRegex = new Regex(
+            @"^(?<Pounds>" + numberRegex + @")\s*(lbs.|lbs|lb.|lb|l|pounds|pound)?\s*$",
+            RegexOptions.IgnorePatternWhitespace | RegexOptions.IgnoreCase);
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            // Check to see if value is not null and if it matches one of the tree Regex from above
+            // if we find a match then the validation rule is satisfied and returns true
+            // if no match is found or value is null we return false
+            bool result = value != null &&
+                (poundsOzRegex.Match((string) value).Success || ouncesRegex.Match((string) value).Success ||
+                 poundsRegex.Match((string) value).Success);
+
+            return new ValidationResult(result, $"{(string)value}{" is not a valid weight"}");
+            
+        }
+    }
+}
