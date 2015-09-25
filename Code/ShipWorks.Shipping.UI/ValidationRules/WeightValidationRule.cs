ae6ca612f9a7225f58f2de34db5605b7740966e5
Andrew Benz <a.benz@shipworks.com>
2015-09-25 08:03:49 -0500

    Remove extraneous code from and add comments to the WeightValidationRule

--- Code/ShipWorks.Shipping.UI/ValidationRules/WeightValidationRule.cs
+++ Code/ShipWorks.Shipping.UI/ValidationRules/WeightValidationRule.cs
@@ -4,6 +4,9 @@ using System.Windows.Controls;
 
 namespace ShipWorks.Shipping.UI.ValidationRules
 {
+    /// <summary>
+    /// Validate a weight entered with lbs or oz
+    /// </summary>
     public class WeightValidationRule : ValidationRule
     {
         // Match any integer or floating point number
@@ -25,22 +28,29 @@ namespace ShipWorks.Shipping.UI.ValidationRules
             @"^(?<Pounds>" + numberRegex + @")\s*(lbs.|lbs|lb.|lb|l|pounds|pound)?\s*$",
             RegexOptions.IgnorePatternWhitespace | RegexOptions.IgnoreCase);
 
+        /// <summary>
+        /// Perform the validation
+        /// </summary>
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
+            string valueAsString = value as string;
+
             // Check to see if value is not null and if it matches one of the tree Regex from above
             // if we find a match then the validation rule is satisfied and returns true
             // if no match is found or value is null we return false
-            bool result = value != null &&
-                (poundsOzRegex.Match((string) value).Success || ouncesRegex.Match((string) value).Success ||
-                 poundsRegex.Match((string) value).Success);
+            bool result = valueAsString != null &&
+                (poundsOzRegex.Match(valueAsString).Success || 
+                ouncesRegex.Match(valueAsString).Success ||
+                 poundsRegex.Match(valueAsString).Success);
 
             // Weight is valid, check to see if someone tried to be smart
             // and enter a negative number 
-            if (result && ((string)value).Contains("-"))
+            if (result && valueAsString.Contains("-"))
             {
                 return new ValidationResult(false, "The weight cannot be negative.");
             }
-            return new ValidationResult(result, $"{(string)value}{" is not a valid weight"}");
+
+            return new ValidationResult(result, $"{valueAsString} is not a valid weight");
         }
     }
 }
