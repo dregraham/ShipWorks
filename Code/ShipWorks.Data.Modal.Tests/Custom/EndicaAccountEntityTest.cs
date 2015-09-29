619ab237b4ee2b9244bf207cbf802ad198bf0c7b
Kevin Croke <k.croke@shipworks.com>
2015-09-29 09:04:29 -0500

    Add unit test project ShipWorks.Data.Modal.Tests to solution with 1 unit test. Still need to create account apply tests for other carriers

---
+++ Code/ShipWorks.Data.Modal.Tests/Custom/EndicaAccountEntityTest.cs
@@ -0,0 +1,29 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+using Xunit;
+
+namespace ShipWorks.Data.Modal.Tests.Custom
+{
+    public class EndicaAccountEntityTest
+    {
+        [Fact]
+        public void ApplyTo_AccountIdIsSet()
+        {
+            ShipmentEntity shipment = new ShipmentEntity()
+            {
+                Postal = new PostalShipmentEntity()
+                {
+                    Endicia = new EndiciaShipmentEntity()
+                    {
+                        EndiciaAccountID = 7
+                    }
+                }
+            };
+
+            EndiciaAccountEntity testObject = new EndiciaAccountEntity(42);
+
+            testObject.ApplyTo(shipment);
+
+            Assert.Equal(42, shipment.Postal.Endicia.EndiciaAccountID);
+        }
+    }
+}
