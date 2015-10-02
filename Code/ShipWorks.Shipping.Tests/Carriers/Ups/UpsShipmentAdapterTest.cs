f5233b9b7ddf5fa82ffdd28e239ed2077f119e2f
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2015-10-02 15:17:05 -0500

    Create UpsShipmentAdapterTest and fix FedExShipmentAdapterTest

--- Code/ShipWorks.Shipping.Tests/Carriers/Ups/UpsShipmentAdapterTest.cs
+++ Code/ShipWorks.Shipping.Tests/Carriers/Ups/UpsShipmentAdapterTest.cs
@@ -1,39 +1,39 @@
 ﻿using ShipWorks.Data.Model.EntityClasses;
-using ShipWorks.Shipping.Carriers.FedEx;
+using ShipWorks.Shipping.Carriers.Ups;
 using System;
 using Xunit;
 
-namespace ShipWorks.Shipping.Tests.Carriers.FedEx
+namespace ShipWorks.Shipping.Tests.Carriers.Ups
 {
-    public class FedExShipmentAdapterTest
+    public class UpsShipmentAdapterTest
     {
         readonly ShipmentEntity shipment;
 
-        public FedExShipmentAdapterTest()
+        public UpsShipmentAdapterTest()
         {
             shipment = new ShipmentEntity
             {
-                FedEx = new FedExShipmentEntity()
+                Ups = new UpsShipmentEntity()
             };
         }
 
         [Fact]
         public void Constructor_ThrowsArgumentNullExcpetion_WhenShipmentIsNull()
         {
-            Assert.Throws<ArgumentNullException>(() => new FedExShipmentAdapter(null));
+            Assert.Throws<ArgumentNullException>(() => new UpsShipmentAdapter(null));
         }
 
         [Fact]
-        public void Constructor_ThrowsArgumentNullExcpetion_WhenFedExShipmentIsNull()
+        public void Constructor_ThrowsArgumentNullExcpetion_WhenPostalShipmentIsNull()
         {
-            Assert.Throws<ArgumentNullException>(() => new FedExShipmentAdapter(new ShipmentEntity()));
+            Assert.Throws<ArgumentNullException>(() => new UpsShipmentAdapter(new ShipmentEntity()));
         }
 
         [Fact]
         public void AccountId_ReturnsShipmentValue()
         {
-            shipment.FedEx.FedExAccountID = 12;
-            var testObject = new FedExShipmentAdapter(shipment);
+            shipment.Ups.UpsAccountID = 12;
+            var testObject = new UpsShipmentAdapter(shipment);
             Assert.Equal(12, testObject.AccountId);
         }
 
@@ -43,17 +43,17 @@ namespace ShipWorks.Shipping.Tests.Carriers.FedEx
         [InlineData(10009238)]
         public void AccountId_StoresSpecifiedValue_WhenValueIsValid(long value)
         {
-            var testObject = new FedExShipmentAdapter(shipment);
+            var testObject = new UpsShipmentAdapter(shipment);
             testObject.AccountId = value;
-            Assert.Equal(value, shipment.FedEx.FedExAccountID);
+            Assert.Equal(value, shipment.Ups.UpsAccountID);
         }
 
         [Fact]
         public void AccountId_StoresZero_WhenValueIsNull()
         {
-            var testObject = new FedExShipmentAdapter(shipment);
+            var testObject = new UpsShipmentAdapter(shipment);
             testObject.AccountId = null;
-            Assert.Equal(0, shipment.FedEx.FedExAccountID);
+            Assert.Equal(0, shipment.Ups.UpsAccountID);
         }
     }
 }
