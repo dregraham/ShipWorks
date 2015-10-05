5258fce1d76bcfee8c965389022810b5c19759c1
Kevin Croke <k.croke@shipworks.com>
2015-10-05 14:11:18 -0500

    Add PromoActivation class

---
+++ Code/ShipWorks.Tests/Shipping/Carriers/UPS/Promo/Api/PromoActiviationTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShipWorks.Shipping.Carriers.UPS.Promo.API;
+using ShipWorks.Shipping.Carriers.UPS.WebServices.Promo;
+using Xunit;
+
+namespace ShipWorks.Tests.Shipping.Carriers.UPS.Promo.Api
+{
+    public class PromoActiviationTest
+    {
+        [Fact]
+        public void PromoActivation_Info_ContainsFirstAlertValue()
+        {
+            string desc = "stupid description";
+
+            PromoDiscountResponse response = new PromoDiscountResponse()
+            {
+                Response = new ResponseType()
+                {
+                    Alert = new[]
+                    {
+                        new CodeDescriptionType()
+                        {
+                            Description = desc
+                        }
+                    }
+                }
+            };
+
+            PromoActivation testObject = new PromoActivation(response);
+
+            Assert.Equal(desc, testObject.Info);
+        }
+
+        [Fact]
+        public void PromoActivation_Info_ContainsEmptyStringWhenAlertIsNull()
+        {
+            PromoDiscountResponse response = new PromoDiscountResponse()
+            {
+                Response = new ResponseType()
+            };
+
+            PromoActivation testObject = new PromoActivation(response);
+
+            Assert.Equal(string.Empty, testObject.Info);
+        }
+
+        [Fact]
+        public void PromoActivation_IsSuccessful_IsTrueWhenStatusCodeIs1()
+        {
+            PromoDiscountResponse response = new PromoDiscountResponse()
+            {
+                Response = new ResponseType()
+                {
+                    ResponseStatus = new CodeDescriptionType()
+                    {
+                        Code = "1"
+                    }
+                }
+            };
+
+            PromoActivation testObject = new PromoActivation(response);
+
+            Assert.True(testObject.IsSuccessful);
+        }
+
+        [Fact]
+        public void PromoActivation_IsSuccessful_IsFalseWhenStatusCodeIsNot1()
+        {
+            PromoDiscountResponse response = new PromoDiscountResponse()
+            {
+                Response = new ResponseType()
+                {
+                    ResponseStatus = new CodeDescriptionType()
+                    {
+                        Code = "0"
+                    }
+                }
+            };
+
+            PromoActivation testObject = new PromoActivation(response);
+
+            Assert.False(testObject.IsSuccessful);
+        }
+
+        [Fact]
+        public void PromoActivation_IsSuccessful_IsFalseWhenResponseStatusIsNull()
+        {
+            PromoDiscountResponse response = new PromoDiscountResponse()
+            {
+                Response = new ResponseType()
+            };
+
+            PromoActivation testObject = new PromoActivation(response);
+
+            Assert.False(testObject.IsSuccessful);
+        }
+    }
+}
--- Code/ShipWorks/Shipping/Carriers/UPS/Promo/API/PromoActivation.cs
+++ Code/ShipWorks/Shipping/Carriers/UPS/Promo/API/PromoActivation.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using ShipWorks.Shipping.Carriers.UPS.WebServices.Promo;
 
 namespace ShipWorks.Shipping.Carriers.UPS.Promo.API
 {
@@ -7,10 +9,23 @@ namespace ShipWorks.Shipping.Carriers.UPS.Promo.API
     /// </summary>
     public class PromoActivation
     {
-        public PromoActivation()
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public PromoActivation(PromoDiscountResponse upsResponse)
         {
-            throw new NotImplementedException();
+            IsSuccessful = ((upsResponse?.Response?.ResponseStatus?.Code ?? "0") == "1");
+            Info = upsResponse?.Response?.Alert?.FirstOrDefault()?.Description ?? string.Empty;
         }
 
+        /// <summary>
+        /// Gets any information if there were any alerts
+        /// </summary>
+        public string Info { get; }
+
+        /// <summary>
+        /// Returns true if activation was sucessful.
+        /// </summary>
+        public bool IsSuccessful { get; }
     }
 }
