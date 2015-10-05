f17c56e2163f093f9583d110d81e111e037d7fd0
John Yedlicka <j.yedlicka@shipworks.com>
2015-10-05 14:03:11 -0500

    Implement UpsPromoWebClientFactory

---
+++ Code/ShipWorks.Tests/Shipping/Carriers/UPS/Promo/Api/UpsPromoWebClientFactoryTest.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers;
+using ShipWorks.Shipping.Carriers.UPS.Promo.API;
+using Xunit;
+
+namespace ShipWorks.Tests.Shipping.Carriers.UPS.Promo.Api
+{
+    public class UpsPromoWebClientFactoryTest
+    {
+        private readonly UpsPromoWebClientFactory testObject;
+
+        public UpsPromoWebClientFactoryTest()
+        {
+            testObject = new UpsPromoWebClientFactory();
+        }
+
+        [Fact]
+        public void CreatePromoClient_ReturnsUpsPromoApiClient_Test()
+        {
+            UpsPromo promo = new UpsPromo(1, string.Empty, new Mock<ICarrierAccountRepository<UpsAccountEntity>>().Object, new Mock<IPromoClientFactory>().Object);
+
+            IUpsApiPromoClient client = testObject.CreatePromoClient(promo);
+
+            Assert.IsType<UpsApiPromoClient>(client);
+        }
+
+        [Fact]
+        public void CreatePromoClient_PromoApiClientIsNotNull_Test()
+        {
+            UpsPromo promo = new UpsPromo(1, string.Empty, new Mock<ICarrierAccountRepository<UpsAccountEntity>>().Object, new Mock<IPromoClientFactory>().Object);
+
+            IUpsApiPromoClient client = testObject.CreatePromoClient(promo);
+
+            Assert.NotNull(client);
+        }
+    }
+}
---
+++ Code/ShipWorks/Shipping/Carriers/UPS/Promo/API/UpsPromoWebClientFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ShipWorks.Shipping.Carriers.UPS.Promo.API
+{
+    /// <summary>
+    /// An implementation of the IPromoClientFactory that creates the "live" implementation
+    /// of the IUpsApiPromoClient.
+    /// </summary>
+    public class UpsPromoWebClientFactory : IPromoClientFactory
+    {
+        /// <summary>
+        /// Uses the UpsPromo provided to create a web client for communicating with 
+        /// the UPS Promo API.
+        /// </summary>
+        /// <param name="promo">The UPS promo that the client is being created for.</param>
+        /// <returns>A concrete UpsApiPromoClient instance.</returns>
+        public IUpsApiPromoClient CreatePromoClient(UpsPromo promo)
+        {
+            return new UpsApiPromoClient(promo);
+        }
+    }
+}
