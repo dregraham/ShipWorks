32b6dfa5277566023c9706c0a67202c4a6e81d18
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2015-10-05 12:13:01 -0500

    Create UpsPromoStatus Enum

---
+++ Code/ShipWorks/Shipping/Carriers/UPS/Enums/UpsPromoStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace ShipWorks.Shipping.Carriers.UPS.Enums
+{
+    /// <summary>
+    /// UPS Promo Status
+    /// </summary>
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum UpsPromoStatus
+    {
+        None = 0,
+        Applied = 1
+    }
+}
