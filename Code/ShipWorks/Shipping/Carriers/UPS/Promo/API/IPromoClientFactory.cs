78e16a7668a8bf5acba1363cb8022bce40df1977
John Yedlicka <j.yedlicka@shipworks.com>
2015-10-05 13:36:18 -0500

    Add the IPromoClientFactory interface

--- Code/ShipWorks/Shipping/Carriers/UPS/Promo/API/IPromoClientFactory.cs
+++ Code/ShipWorks/Shipping/Carriers/UPS/Promo/API/IPromoClientFactory.cs
@@ -1,6 +1,16 @@
 ﻿namespace ShipWorks.Shipping.Carriers.UPS.Promo.API
 {
+    /// <summary>
+    /// A factory interface for creating IUpsApiPromoClient instances.
+    /// </summary>
     public interface IPromoClientFactory
     {
+        /// <summary>
+        /// Uses the UpsPromo provided to create a web client for communicating with 
+        /// the UPS Promo API.
+        /// </summary>
+        /// <param name="promo">The UPS promo that the client is being created for.</param>
+        /// <returns>An IUpsApiPromoClient instance.</returns>
+        IUpsApiPromoClient CreatePromoClient(UpsPromo promo);
     }
 }
