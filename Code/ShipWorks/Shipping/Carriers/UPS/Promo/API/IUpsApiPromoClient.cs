e7d5b71af67f8e5c8cffcda9d0e20cd976c33891
Kevin Croke <k.croke@shipworks.com>
2015-10-07 09:10:38 -0500

    Made FakeUpsApiPromoClient configurable. (see desc) Copy \Code\ShipWorks\Shipping\Carriers\UPS\Promo\API\FakeUpsApiPromoClient.json to c:\. Edit it for the desired behavior.

--- Code/ShipWorks/Shipping/Carriers/UPS/Promo/API/IUpsApiPromoClient.cs
+++ Code/ShipWorks/Shipping/Carriers/UPS/Promo/API/IUpsApiPromoClient.cs
@@ -1,4 +1,7 @@
-﻿using log4net;
+﻿using System.IO;
+using log4net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using ShipWorks.Shipping.Carriers.UPS.WebServices.Promo;
 
 namespace ShipWorks.Shipping.Carriers.UPS.Promo.API
@@ -19,17 +22,38 @@ namespace ShipWorks.Shipping.Carriers.UPS.Promo.API
         PromoAcceptanceTerms GetAgreement();
     }
 
-    // Todo: get rid of this
+    
+    /// <summary>
+    /// Todo: get rid of this
+    /// This relies on a file at c:\FakeUpsApiPromoClient.json. 
+    /// An example of this file is in this directory.
+    /// </summary>
     class FakeUpsApiPromoClient : IUpsApiPromoClient
     {
         private ILog log;
         private UpsPromo promo;
+
+        private bool GetAgreementThrowsError = false;
+        private bool GetAgreementIsSuccessful = true;
+        private bool ActivateThrowsError = false;
+        private bool ActivateIsSuccessful = true;
 
         public FakeUpsApiPromoClient(UpsPromo promo)
         {
             log = LogManager.GetLogger(typeof(FakeUpsApiPromoClient));
             log.Info($"FakeUpsApiPromoClient recieved a promo with account {promo.AccountNumber}.");
 
+            const string fakeConfigFileName = @"c:\FakeUpsApiPromoClient.json";
+            if (File.Exists(fakeConfigFileName))
+            {
+                JObject fakeConfig = JObject.Parse(File.ReadAllText(fakeConfigFileName));
+
+                GetAgreementThrowsError = fakeConfig["GetAgreement"]["ThrowsError"].Value<bool>();
+                GetAgreementIsSuccessful = fakeConfig["GetAgreement"]["IsSuccessful"].Value<bool>();
+                ActivateThrowsError = fakeConfig["Activate"]["ThrowsError"].Value<bool>();
+                ActivateIsSuccessful = fakeConfig["Activate"]["IsSuccessful"].Value<bool>();
+            }
+            
             this.promo = promo;
         }
 
@@ -37,13 +61,18 @@ namespace ShipWorks.Shipping.Carriers.UPS.Promo.API
         {
             log.Info($"FakeUpsApiPromoClient.Activate({acceptanceCode})");
 
+            if (ActivateThrowsError)
+            {
+                throw new UpsPromoException("Throwing because of FakeUpsApiPromoClient.json says so.");
+            }
+
             return new PromoActivation(new PromoDiscountResponse()
             {
                 Response = new ResponseType()
                 {
                     ResponseStatus = new CodeDescriptionType()
                     {
-                        Code = "1"
+                        Code = ActivateIsSuccessful ? "1" : "0"
                     }
                 }
             });
@@ -54,6 +83,11 @@ namespace ShipWorks.Shipping.Carriers.UPS.Promo.API
             string acceptanceCode = "42";
             log.Info($"FakeUpsApiPromoClient.GetAgreement() returning acceptance code of {acceptanceCode}, AgreementURL of www.google.com and PromoDescription of \"Mocked Descriptoin\"");
 
+            if (GetAgreementThrowsError)
+            {
+                throw new UpsPromoException("Throwing because of FakeUpsApiPromoClient.json says so.");
+            }
+
             return new PromoAcceptanceTerms(new PromoDiscountAgreementResponse()
             {
                 PromoAgreement = new PromoAgreementType()
@@ -61,7 +95,13 @@ namespace ShipWorks.Shipping.Carriers.UPS.Promo.API
                     AgreementURL = "www.google.com",
                     AcceptanceCode = acceptanceCode
                 },
-                PromoDescription = "Mocked Description"
+                PromoDescription = "Mocked Description", Response = new ResponseType()
+                {
+                    ResponseStatus = new CodeDescriptionType()
+                    {
+                        Code = GetAgreementIsSuccessful ? "1" : "0"
+                    }
+                }
             });
         }
     }
