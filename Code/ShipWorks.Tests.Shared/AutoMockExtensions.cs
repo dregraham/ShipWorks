0408dd84d982ccfdaeaa5123a63af691cc270ba9
Kevin Croke <k.croke@shipworks.com>
2015-10-09 14:53:00 -0500

    Copy Shipworks.Tests.Shared from feature-shipping-panel

---
+++ Code/ShipWorks.Tests.Shared/AutoMockExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Autofac.Extras.Moq;
+using Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping;
+using System;
+
+namespace ShipWorks.Tests.Shared
+{
+    /// <summary>
+    /// Additional methods for AutoMocks
+    /// </summary>
+    public static class AutoMockExtensions
+    {
+        /// <summary>
+        /// Get an AutoMock that is loose and will try and return meaningful defaults instead of null
+        /// </summary>
+        /// <returns></returns>
+        public static AutoMock GetLooseThatReturnsMocks() =>
+            AutoMock.GetFromRepository(new MockRepository(MockBehavior.Loose) { DefaultValue = DefaultValue.Mock });
+
+        /// <summary>
+        /// Configure a shipment type that will be returned by an instance of IShipmentTypeFactory
+        /// </summary>
+        public static void WithShipmentTypeFromFactory(this AutoMock mock, Action<Mock<ShipmentType>> shipmentTypeConfiguration)
+        {
+            var shipmentTypeMock = mock.MockRepository.Create<ShipmentType>();
+            shipmentTypeConfiguration(shipmentTypeMock);
+
+            mock.Mock<IShipmentTypeFactory>()
+                .Setup(x => x.Get(It.IsAny<ShipmentTypeCode>()))
+                .Returns(shipmentTypeMock.Object);
+
+            mock.Mock<IShipmentTypeFactory>()
+                .Setup(x => x.Get(It.IsAny<ShipmentEntity>()))
+                .Returns(shipmentTypeMock.Object);
+        }
+
+        /// <summary>
+        /// Configure a shipment type that will be injected directly; not through a factory
+        /// </summary>
+        public static void WithShipmentType<T>(this AutoMock mock, Action<Mock<T>> shipmentTypeConfiguration) where T : ShipmentType
+        {
+            var shipmentTypeMock = mock.MockRepository.Create<T>();
+            shipmentTypeConfiguration(shipmentTypeMock);
+            mock.Provide(shipmentTypeMock.Object);
+        }
+    }
+}
