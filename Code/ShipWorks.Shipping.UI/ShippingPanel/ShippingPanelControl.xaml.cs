b4e77e806b5a9f2cf22d70a882f81b723fddedf4
Tim Hughes <t.hughes@shipworks.com>
2015-10-13 12:02:37 -0500

    To support selecting all text in the text box when tabbing into it, register the got focus event to select all text for text boxes

--- Code/ShipWorks.Shipping.UI/ShippingPanel/ShippingPanelControl.xaml.cs
+++ Code/ShipWorks.Shipping.UI/ShippingPanel/ShippingPanelControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace ShipWorks.Shipping.UI.ShippingPanel
 {
@@ -15,6 +16,20 @@ namespace ShipWorks.Shipping.UI.ShippingPanel
             InitializeComponent();
 
             DataContext = viewModel;
+
+            // To support selecting all text in the text box when tabbing into it, register the got focus event
+            // to select all text for text boxes
+            EventManager.RegisterClassHandler(typeof(TextBox),
+                TextBox.GotFocusEvent,
+                new RoutedEventHandler(OnTextBoxGotFocus));
+        }
+
+        /// <summary>
+        /// Event fired when a text box gets focus.  Selects all the text in the box.
+        /// </summary>
+        private void OnTextBoxGotFocus(object sender, RoutedEventArgs e)
+        {
+            (sender as TextBox)?.SelectAll();
         }
     }
 }
