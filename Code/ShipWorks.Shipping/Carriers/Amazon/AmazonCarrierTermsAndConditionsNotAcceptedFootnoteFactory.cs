ce90e2ea241b97a9546bd010c79a4b42da6244cf
Tim Hughes <t.hughes@shipworks.com>
2015-10-27 13:46:16 -0500

    Initial AmazonCarrierTermsAndConditionsNotAcceptedFootnoteFactory

---
+++ Code/ShipWorks.Shipping/Carriers/Amazon/AmazonCarrierTermsAndConditionsNotAcceptedFootnoteFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipWorks.Shipping.Editing.Rating;
+using Interapptive.Shared.Utility;
+
+namespace ShipWorks.Shipping.Carriers.Amazon
+{
+    /// <summary>
+    /// Footnote factory for creating Amazon carrier terms and conditions footnote
+    /// </summary>
+    public class AmazonCarrierTermsAndConditionsNotAcceptedFootnoteFactory : IRateFootnoteFactory
+    {
+        private List<string> carrierNames;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AmazonCarrierTermsAndConditionsNotAcceptedFootnoteFactory(ShipmentType shipmentType, List<string> carrierNames)
+        {
+            MethodConditions.EnsureArgumentIsNotNull(shipmentType, nameof(shipmentType));
+            MethodConditions.EnsureArgumentIsNotNull(carrierNames, nameof(carrierNames));
+
+            this.ShipmentType = shipmentType;
+            this.carrierNames = carrierNames;
+        }
+
+        /// <summary>
+        /// Gets the corresponding shipment type for the factory.
+        /// </summary>
+        public ShipmentType ShipmentType { get; }
+
+        /// <summary>
+        /// Creates a footnote control.
+        /// </summary>
+        public RateFootnoteControl CreateFootnote(FootnoteParameters parameters)
+        {
+            // TODO: Create and return actual footnote
+            return new RateFootnoteControl();
+        }
+
+        /// <summary>
+        /// Notes that this factory should or should not be used in BestRate
+        /// For example, when using BestRate, we do not want Usps promo footnotes to display, so this will be set to false.
+        /// </summary>
+        public bool AllowedForBestRate => false;
+    }
+}
