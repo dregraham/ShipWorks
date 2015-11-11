95077837865f205ceed76837fc797580f453f868
Kevin Croke <k.croke@shipworks.com>
2015-11-11 13:12:34 -0600

    Remove insurance settings from AmazonSettingsControl

--- Code/ShipWorks.Shipping.UI/Carriers/Amazon/AmazonSettingsControl.Designer.cs
+++ Code/ShipWorks.Shipping.UI/Carriers/Amazon/AmazonSettingsControl.Designer.cs
@@ -30,41 +30,8 @@ namespace ShipWorks.Shipping.UI.Carriers.Amazon
         /// </summary>
         private void InitializeComponent()
         {
-            this.labelShipmentProtection = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.insuranceProviderChooser = new ShipWorks.Shipping.Insurance.InsuranceProviderChooser();
             this.amazonOptionsControl1 = new ShipWorks.Shipping.Carriers.Amazon.AmazonOptionsControl();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // labelShipmentProtection
-            // 
-            this.labelShipmentProtection.AutoSize = true;
-            this.labelShipmentProtection.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelShipmentProtection.Location = new System.Drawing.Point(5, 5);
-            this.labelShipmentProtection.Name = "labelShipmentProtection";
-            this.labelShipmentProtection.Size = new System.Drawing.Size(123, 13);
-            this.labelShipmentProtection.TabIndex = 22;
-            this.labelShipmentProtection.Text = "Shipment Protection";
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.insuranceProviderChooser);
-            this.panel2.Controls.Add(this.labelShipmentProtection);
-            this.panel2.Location = new System.Drawing.Point(3, 52);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(425, 57);
-            this.panel2.TabIndex = 24;
-            // 
-            // insuranceProviderChooser
-            // 
-            this.insuranceProviderChooser.CarrierMessage = "";
-            this.insuranceProviderChooser.CarrierProviderName = "Amazon Insurance";
-            this.insuranceProviderChooser.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.insuranceProviderChooser.Location = new System.Drawing.Point(19, 21);
-            this.insuranceProviderChooser.Name = "insuranceProviderChooser";
-            this.insuranceProviderChooser.Size = new System.Drawing.Size(375, 30);
-            this.insuranceProviderChooser.TabIndex = 25;
             // 
             // amazonOptionsControl1
             // 
@@ -79,19 +46,13 @@ namespace ShipWorks.Shipping.UI.Carriers.Amazon
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.amazonOptionsControl1);
-            this.Controls.Add(this.panel2);
             this.Name = "AmazonSettingsControl";
             this.Size = new System.Drawing.Size(445, 119);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Label labelShipmentProtection;
-        private System.Windows.Forms.Panel panel2;
-        private Insurance.InsuranceProviderChooser insuranceProviderChooser;
         private AmazonOptionsControl amazonOptionsControl1;
     }
 }
--- Code/ShipWorks.Shipping.UI/Carriers/Amazon/AmazonSettingsControl.cs
+++ Code/ShipWorks.Shipping.UI/Carriers/Amazon/AmazonSettingsControl.cs
@@ -9,27 +9,5 @@ namespace ShipWorks.Shipping.UI.Carriers.Amazon
         {
             InitializeComponent();
         }
-        
-        /// <summary>
-        /// Load the settings into the control
-        /// </summary>
-        public override void LoadSettings()
-        {                        
-            //TODO Set insurance provider based on database field for Amazon Insurance
-            //ShippingSettingsEntity settings = ShippingSettings.Fetch();
-            //insuranceProviderChooser.InsuranceProvider = (InsuranceProvider)settings.;
-        }
-
-        /// <summary>
-        /// Save the shipping settings for Amazon
-        /// </summary>
-        /// <param name="settings"></param>
-        public override void SaveSettings(ShippingSettingsEntity settings)
-        {
-            base.SaveSettings(settings);
-
-            //TODO save amazon insurance provider
-            //settings.AmazonInsuranceProvider = (int)insuranceProviderChooser.InsuranceProvider;
-        }
     }
 }
