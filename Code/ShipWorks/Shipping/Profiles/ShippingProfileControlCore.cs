cba2f18eff60b84a986d08ebc2d566ef31d4e70e
Kevin Croke <k.croke@shipworks.com>
2015-11-24 14:44:39 -0600

    When WorldShip initial profile first loads in the profile control, PayorDutiesCountry was blank. Prior to f33d2db, OnCheckChange was called after setting the value and this selected the first item in the combo if nothing was selected (Which is the case in an initial profile). After setting the value, I am not calling OnCheckChanged.
    This is now being called twice.

--- Code/ShipWorks/Shipping/Profiles/ShippingProfileControlCore.cs
+++ Code/ShipWorks/Shipping/Profiles/ShippingProfileControlCore.cs
@@ -66,6 +66,7 @@ namespace ShipWorks.Shipping.Profiles
             if (checkBox.Checked)
             {
                 ReadFieldValue(entity, field, dataControl);
+                OnStateCheckChanged(checkBox, EventArgs.Empty);
             }
         }
 
