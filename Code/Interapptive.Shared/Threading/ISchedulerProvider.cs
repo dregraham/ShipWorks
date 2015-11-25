7f73542f35d09787b7701b46e956cc40cd378b12
Andrew Benz <a.benz@shipworks.com>
2015-11-25 14:30:12 -0600

    Add scheduler provider

---
+++ Code/Interapptive.Shared/Threading/ISchedulerProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.Reactive.Concurrency;
+
+namespace Interapptive.Shared.Threading
+{
+    /// <summary>
+    /// Provide all the various schedulers
+    /// </summary>
+    public interface ISchedulerProvider
+    {
+        /// <summary>
+        /// Current thread scheduler
+        /// </summary>
+        IScheduler CurrentThread { get; }
+
+        /// <summary>
+        /// Dispatcher scheduler
+        /// </summary>
+        IScheduler Dispatcher { get; }
+
+        /// <summary>
+        /// Immediate scheduler
+        /// </summary>
+        IScheduler Immediate { get; }
+
+        /// <summary>
+        /// New thread scheduler
+        /// </summary>
+        IScheduler NewThread { get; }
+
+        /// <summary>
+        /// Thread pool scheduler
+        /// </summary>
+        IScheduler ThreadPool { get; }
+
+        /// <summary>
+        /// Task pool scheduler
+        /// </summary>
+        IScheduler TaskPool { get; }
+    }
+}
---
+++ Code/ShipWorks/Messaging/SchedulerProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.Reactive.Concurrency;
+
+namespace Interapptive.Shared.Threading
+{
+    /// <summary>
+    /// Provide all the various schedulers
+    /// </summary>
+    public sealed class SchedulerProvider : ISchedulerProvider
+    {
+        /// <summary>
+        /// Current thread scheduler
+        /// </summary>
+        public IScheduler CurrentThread => CurrentThreadScheduler.Instance;
+
+        /// <summary>
+        /// Dispatcher scheduler
+        /// </summary>
+        public IScheduler Dispatcher => DispatcherScheduler.Current;
+
+        /// <summary>
+        /// Immediate scheduler
+        /// </summary>
+        public IScheduler Immediate => ImmediateScheduler.Instance;
+
+        /// <summary>
+        /// New thread scheduler
+        /// </summary>
+        public IScheduler NewThread => NewThreadScheduler.Default;
+
+        /// <summary>
+        /// Thread pool scheduler
+        /// </summary>
+        public IScheduler ThreadPool => ThreadPoolScheduler.Instance;
+
+        /// <summary>
+        /// Task pool scheduler
+        /// </summary>
+        public IScheduler TaskPool => TaskPoolScheduler.Default;
+    }
+}
