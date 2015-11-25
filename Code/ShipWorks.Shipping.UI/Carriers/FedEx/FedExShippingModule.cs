dff3f95063223e40224f52d8d02a605820d392d0
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2015-11-25 10:00:00 -0600

    create FedExLabelService

---
+++ Code/ShipWorks.Shipping.UI/Carriers/FedEx/FedExShippingModule.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Api;
+using ShipWorks.Shipping.Carriers.Api;
+using ShipWorks.Shipping.Carriers.FedEx.Api;
+using ShipWorks.Shipping.Carriers.FedEx.Api.Environment;
+
+namespace ShipWorks.Shipping.Carriers.FedEx
+{
+    public class FedExShippingModule : Module
+    {
+        /// <summary>
+        /// Load the module configuration
+        /// </summary>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<FedExLabelService>()
+                .Keyed<ILabelService>(ShipmentTypeCode.FedEx);
+            
+            builder.RegisterType<FedExSettingsRepository>()
+                .Keyed<ICarrierSettingsRepository>(ShipmentTypeCode.FedEx)
+                .AsSelf();
+
+            builder.Register<IShippingClerk>((container, parameters) =>
+            {
+                return FedExShippingClerkFactory.CreateShippingClerk(
+                    parameters.TypedAs<ShipmentEntity>(),
+                    container.ResolveKeyed<ICarrierSettingsRepository>(ShipmentTypeCode.FedEx));
+            });
+        }
+    }
+}
---
+++ Code/ShipWorks.Shipping/Carriers/FedEx/FedExLabelService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.Api;
+using ShipWorks.Shipping.Carriers.FedEx.Api;
+
+namespace ShipWorks.Shipping.Carriers.FedEx
+{
+    /// <summary>
+    /// Label Service for the FedEx carrier
+    /// </summary>
+    public class FedExLabelService : ILabelService
+    {
+        private readonly Func<ShipmentEntity, IShippingClerk> shippingClerkFactory;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public FedExLabelService(Func<ShipmentEntity,IShippingClerk> shippingClerkFactory)
+        {
+            this.shippingClerkFactory = shippingClerkFactory;
+        }
+
+        /// <summary>
+        /// Creates the label
+        /// </summary>
+        public void Create(ShipmentEntity shipment)
+        {
+            try
+            {
+                IShippingClerk shippingClerk = shippingClerkFactory(shipment);
+                shippingClerk.Ship(shipment);
+            }
+            catch (FedExException ex)
+            {
+                throw new ShippingException(ex.Message, ex);
+            }
+        }
+
+        /// <summary>
+        /// Voids the label
+        /// </summary>
+        public void Void(ShipmentEntity shipment)
+        {
+            try
+            {
+                IShippingClerk shippingClerk = shippingClerkFactory(shipment);
+                shippingClerk.Void(shipment);
+            }
+            catch (FedExException ex)
+            {
+                throw new ShippingException(ex.Message, ex);
+            }
+        }
+    }
+}
