f39f0efc3688face9064ee65849c61899509d664
Kevin Croke <k.croke@shipworks.com>
2015-12-02 13:56:52 -0600

    Create NullRatingService

---
+++ Code/ShipWorks/Shipping/NullRatingService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Editing.Rating;
+
+namespace ShipWorks.Shipping
+{
+    /// <summary>
+    /// Null Rating Service
+    /// </summary>
+    public class NullRatingService : IRatingService
+    {
+        /// <summary>
+        /// GetRates not supported by carrier
+        /// </summary>
+        public RateGroup GetRates(ShipmentEntity shipment)
+        {
+            throw new InvalidOperationException("Should not be called.");
+        }
+    }
+}
