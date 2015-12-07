e3f7349037ab34af69cd1af06565e99626b01ab4
Andrew Benz <a.benz@shipworks.com>
2015-12-07 15:50:10 -0600

    Use common assembly info in the UI project

--- Code/ShipWorks.UI/Properties/AssemblyInfo.cs
+++ Code/ShipWorks.UI/Properties/AssemblyInfo.cs
@@ -1,23 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ShipWorks.UI")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ShipWorks.UI")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1f6a1dc6-dd72-4279-8747-6789f83711ff")]
 
