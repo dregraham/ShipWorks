48a090f654be5920ce216aac3558167d522a8a0c
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2015-12-14 10:58:20 -0600

    Add enum description to fix unit test

--- Code/ShipWorks/Shipping/Carriers/BestRate/BestRateConsolidatePostalRates.cs
+++ Code/ShipWorks/Shipping/Carriers/BestRate/BestRateConsolidatePostalRates.cs
@@ -1,11 +1,14 @@
-﻿using System.Reflection;
+﻿using System.ComponentModel;
+using System.Reflection;
 
 namespace ShipWorks.Shipping.Carriers.BestRate
 {
     [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
     public enum BestRateConsolidatePostalRates
     {
+        [Description("No")]
         No = 0,
+        [Description("Yes")]
         Yes = 1
     }
 }
