27a79793d4089714ce10f20da63f02f653376de7
Kevin Croke <k.croke@shipworks.com>
2016-01-07 13:48:50 -0600

    Add ILicense

---
+++ Code/ShipWorks/ApplicationCore/Licensing/ILicense.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShipWorks.ApplicationCore.Licensing
+{
+    /// <summary>
+    /// License describes the capabilities of the customer's license.
+    /// </summary>
+    public interface ILicense
+    {
+        /// <summary>
+        /// Refresh the License capabilities from Tango
+        /// </summary>
+        void Refresh();
+
+        /// <summary>
+        /// Reason the license is Disabled
+        /// </summary>
+        string DisabledReason { get; set; }
+
+        /// <summary>
+        /// Is the license Disabled
+        /// </summary>
+        bool IsDisabled { get; set; }
+    }
+}
