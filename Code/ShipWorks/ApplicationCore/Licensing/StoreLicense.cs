b012ed19a1f000f164e10521562c3b58ccb32d11
Kevin Croke <k.croke@shipworks.com>
2016-01-07 14:39:23 -0600

    WIP

--- Code/ShipWorks/ApplicationCore/Licensing/StoreLicense.cs
+++ Code/ShipWorks/ApplicationCore/Licensing/StoreLicense.cs
@@ -1,21 +1,58 @@
 ﻿using System;
+using ShipWorks.Data.Model.EntityClasses;
 
 namespace ShipWorks.ApplicationCore.Licensing
 {
     public class StoreLicense : ILicense
     {
+        private readonly StoreEntity store;
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public StoreLicense(StoreEntity store)
+        {
+            this.store = store;
+        }
+
         public void Activate(string email, string password)
         {
-            throw new NotImplementedException();
+            // no need to activate for legacy
         }
 
-        public string Key { get; set; }
+        /// <summary>
+        /// The store license key
+        /// </summary>
+        public string Key
+        {
+            get { return store.License; }
+            set { store.License = value; }
+        }
+
+        /// <summary>
+        /// Refreshes store license data.
+        /// </summary>
         public void Refresh()
         {
-            throw new NotImplementedException();
+            try
+            {
+                LicenseActivationHelper.EnsureActive(store);
+                DisabledReason = string.Empty;
+            }
+            catch (ShipWorksLicenseException ex)
+            {
+                DisabledReason = ex.Message;
+            }
         }
 
+        /// <summary>
+        /// The reason the store is disabled.
+        /// </summary>
         public string DisabledReason { get; set; }
-        public bool IsDisabled { get; set; }
+
+        /// <summary>
+        /// Is the store disabled?
+        /// </summary>
+        public bool IsDisabled => !string.IsNullOrEmpty(DisabledReason);
     }
 }
