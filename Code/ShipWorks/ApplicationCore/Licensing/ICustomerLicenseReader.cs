63b7fc9283e6395f24e41e61f72cd13f0587e5da
Chris Hicks <c.hicks@shipworks.com>
2016-01-07 13:41:46 -0600

    Create ICustomerLicenseReader

---
+++ Code/ShipWorks/ApplicationCore/Licensing/ICustomerLicenseReader.cs
@@ -0,0 +1,13 @@
+﻿namespace ShipWorks.ApplicationCore.Licensing
+{
+    /// <summary>
+    /// Interface for reading the license information
+    /// </summary>
+    public interface ICustomerLicenseReader
+    {
+        /// <summary>
+        /// Reads this License
+        /// </summary>
+        string Read();
+    }
+}
