c87347ae4486f46380840f6ede478a51336735ca
Kevin Croke <k.croke@shipworks.com>
2016-01-07 14:04:43 -0600

    Created shell StoreLicense

---
+++ Code/ShipWorks/ApplicationCore/Licensing/StoreLicense.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ShipWorks.ApplicationCore.Licensing
+{
+    public class StoreLicense : ILicense
+    {
+        public void Activate(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Key { get; set; }
+        public void Refresh()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DisabledReason { get; set; }
+        public bool IsDisabled { get; set; }
+    }
+}
