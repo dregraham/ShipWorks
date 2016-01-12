3e517fce1f35d4192ae67e1a9f27f907d4ba56ff
Chris Hicks <c.hicks@shipworks.com>
2016-01-12 13:17:00 -0600

    Implement UserSessionWrapper

---
+++ Code/ShipWorks/Users/UserSessionWrapper.cs
@@ -0,0 +1,17 @@
+﻿using ShipWorks.Users.Logon;
+
+namespace ShipWorks.Users
+{
+    public class UserSessionWrapper : IUserSessionWrapper
+    {
+        public bool Logon(LogonCredentials credentials)
+        {
+            return UserSession.Logon(credentials.Username, credentials.Password, credentials.Remember);
+        }
+
+        public bool LogonLastUser()
+        {
+            return UserSession.LogonLastUser();
+        }
+    }
+}
