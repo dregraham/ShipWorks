c7750abca155c20ab2516d6c5b91bde279d03493
Chris Hicks <c.hicks@shipworks.com>
2016-01-12 13:04:15 -0600

    Create Credentials class

---
+++ Code/ShipWorks/Users/Logon/Credentials.cs
@@ -0,0 +1,23 @@
+﻿namespace ShipWorks.Users.Logon
+{
+    /// <summary>
+    /// Credentials used for logon
+    /// </summary>
+    public class Credentials
+    {
+        /// <summary>
+        /// The username to logon
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The password to logon with
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Whether or not to remember the username and password for auto logon
+        /// </summary>
+        public bool Remember { get; set; }
+    }
+}
