250c58b3814d4cc9fb839118c11556bf5892c385
Kevin Croke <k.croke@shipworks.com>
2016-01-12 13:26:55 -0600

    Add Logon methods to IUserService

--- Code/ShipWorks/Users/UserService.cs
+++ Code/ShipWorks/Users/UserService.cs
@@ -1,4 +1,6 @@
-﻿using ShipWorks.Data.Model.EntityClasses;
+﻿using Interapptive.Shared.Utility;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Users.Logon;
 
 namespace ShipWorks.Users
 {
@@ -13,6 +15,22 @@ namespace ShipWorks.Users
         public UserEntity CreateUser(string username, string password, bool isAdmin)
         {
             return UserUtility.CreateUser(username, username, password, isAdmin);
+        }
+
+        /// <summary>
+        /// Logs the user in
+        /// </summary>
+        public GenericResult<LogonCredentials> Logon(LogonCredentials credentials)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logs the user in using their saved credentials
+        /// </summary>
+        public GenericResult<LogonCredentials> Logon()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
