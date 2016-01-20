5856f13dadbc680a39a49e6c163196a2bbafb06d
Kevin Croke <k.croke@shipworks.com>
2016-01-20 09:59:53 -0600

    Update GetDatabaseGuid to return a guid

--- Code/ShipWorks/Data/Administration/Scripts/Installation/InitialData.sql
+++ Code/ShipWorks/Data/Administration/Scripts/Installation/InitialData.sql
@@ -226,5 +226,5 @@ DROP FUNCTION dbo.GetLocalTimezoneName
 
 DECLARE @ExecStatement NVARCHAR(4000), @SQLString NVARCHAR(4000)
 SET @ExecStatement = 'EXEC sp_executesql @SQLString'
-SET @SQLString = N'CREATE PROCEDURE [GetDatabaseGuid] WITH ENCRYPTION AS SELECT ''' + CONVERT(varchar(255),NEWID()) + ''''
+SET @SQLString = N'CREATE PROCEDURE [GetDatabaseGuid] WITH ENCRYPTION AS SELECT convert(uniqueidentifier,''' + CONVERT(varchar(255),NEWID()) + ''')'
 EXEC sp_executesql  @ExecStatement, N'@SQLString NVARCHAR(4000)', @SQLString=@SQLString
