166f60c66fe594eb922184fa4eda4ec3a430ac28
Chris Hicks <c.hicks@shipworks.com>
2016-01-20 09:15:26 -0600

    Implement LicenseEncryptionProvider and create tests for it

---
+++ Code/ShipWorks.Tests/ApplicationCore/Licensing/LicenseEncryptionProviderTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Autofac.Extras.Moq;
+using ShipWorks.ApplicationCore.Licensing;
+using Xunit;
+
+namespace ShipWorks.Tests.ApplicationCore.Licensing
+{
+    public class LicenseEncryptionProviderTest
+    {
+        private readonly LicenseEncryptionProvider testObject;
+
+        public LicenseEncryptionProviderTest()
+        {
+
+            Guid guid = new Guid("CF136821-5D3C-4237-ABFB-F5560C65A3D0");
+
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IDatabaseIdentifier>().Setup(x => x.Get()).Returns(guid);
+
+                testObject = new LicenseEncryptionProvider(mock.Mock<IDatabaseIdentifier>().Object);
+            }
+        }
+
+        [Fact]
+        public void Encrypt_ReturnsEncryptedString_WhenGivenDecryptedString_Test()
+        {
+            string encryptedText = testObject.Encrypt("Some decrypted text");
+
+            Assert.Equal("qRiG4SmDSj9lM0HX4hltSQr1joOCJ+eN", encryptedText);
+        }
+
+        [Fact]
+        public void Encrypt_ReturnsEncryptedString_WhenGivenEmptyString_Test()
+        {
+            string encryptedText = testObject.Encrypt("");
+
+            Assert.Equal("jojgM63ozWI=", encryptedText);
+        }
+
+        [Fact]
+        public void Decrypt_ReturnsDecryptedString_WhenGivenEncryptedString_Test()
+        {
+            string decryptedText = testObject.Decrypt("qRiG4SmDSj9lM0HX4hltSQr1joOCJ+eN");
+
+            Assert.Equal("Some decrypted text", decryptedText);
+        }
+
+        [Fact]
+        public void Decrypt_ThrowsArgumentException_WhenGivenEmptyString_Test()
+        {
+            Assert.Throws<ArgumentException>(() => testObject.Decrypt(""));
+        }
+    }
+}
---
+++ Code/ShipWorks/ApplicationCore/Licensing/LicenseEncryptionProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Interapptive.Shared.Utility;
+
+namespace ShipWorks.ApplicationCore.Licensing
+{
+    /// <summary>
+    /// Class for encrypting and decrypting license keys
+    /// </summary>
+    public class LicenseEncryptionProvider : IEncryptionProvider
+    {
+        private readonly IDatabaseIdentifier databaseID;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LicenseEncryptionProvider"/> class.
+        /// </summary>
+        /// <param name="databaseID">The database identifier.</param>
+        public LicenseEncryptionProvider(IDatabaseIdentifier databaseID)
+        {
+            this.databaseID = databaseID;
+        }
+
+        /// <summary>
+        /// Encrypts the given decrypted text.
+        /// </summary>
+        public string Encrypt(string decryptedText)
+        {
+            return SecureText.Encrypt(decryptedText, databaseID.Get().ToString().ToUpperInvariant());
+        }
+
+        /// <summary>
+        /// Decrypts the given encrypted text.
+        /// </summary>
+        public string Decrypt(string encryptedText)
+        {
+            if (string.IsNullOrWhiteSpace(encryptedText))
+            {
+                throw new ArgumentException("Cannot decrypt an empty string");
+            }
+
+            return SecureText.Decrypt(encryptedText, databaseID.Get().ToString().ToUpperInvariant());
+        }
+    }
+}
