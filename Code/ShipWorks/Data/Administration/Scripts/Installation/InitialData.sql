8a4d47d4ce69aa77024a5dd8abb5738355be81b1
Kevin Croke <k.croke@shipworks.com>
2016-01-20 08:31:37 -0600

    Adding GetDatabaseGuid Stored Proc

--- Code/ShipWorks/Data/Administration/Scripts/Installation/InitialData.sql
+++ Code/ShipWorks/Data/Administration/Scripts/Installation/InitialData.sql
@@ -224,3 +224,7 @@ DROP FUNCTION dbo.GetLocalTimezoneName
 
 
 
+DECLARE @ExecStatement NVARCHAR(4000), @SQLString NVARCHAR(4000)
+SET @ExecStatement = 'EXEC sp_executesql @SQLString'
+SET @SQLString = N'CREATE PROCEDURE [GetDatabaseGuid] WITH ENCRYPTION AS SELECT ''' + CONVERT(varchar(255),NEWID()) + ''''
+EXEC sp_executesql  @ExecStatement, N'@SQLString NVARCHAR(4000)', @SQLString=@SQLString
---
+++ Code/ShipWorks/Data/Administration/Scripts/Update/4.9/4.9.0.1.sql
@@ -0,0 +1,4 @@
+﻿DECLARE @ExecStatement NVARCHAR(4000), @SQLString NVARCHAR(4000)
+SET @ExecStatement = 'EXEC sp_executesql @SQLString'
+SET @SQLString = N'CREATE PROCEDURE [GetDatabaseGuid] WITH ENCRYPTION AS SELECT ''' + CONVERT(varchar(255),NEWID()) + ''''
+EXEC sp_executesql  @ExecStatement, N'@SQLString NVARCHAR(4000)', @SQLString=@SQLString
