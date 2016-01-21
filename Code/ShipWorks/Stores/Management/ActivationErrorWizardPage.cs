ebc6c193ef2a28605f100fd18804cd3b0fd52501
Kevin Croke <k.croke@shipworks.com>
2016-01-21 16:34:13 -0600

    Add ActivationErrorWizardPage - still need to work out flow, but it makes sense to do this as part of the OnSteppingIntoComplete task

---
+++ Code/ShipWorks/Stores/Management/ActivationErrorWizardPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ShipWorks.UI.Wizard;
+
+namespace ShipWorks.Stores.Management
+{
+    /// <summary>
+    /// A page for use in the AddStoreWizard - Show license activation errors
+    /// </summary>
+    public partial class ActivationErrorWizardPage : AddStoreWizardPage
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ActivationErrorWizardPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ErrorMessage to display.
+        /// </summary>
+        public string ErrorMessage
+        {
+            get { return errorMessage.Text; }
+            set { errorMessage.Text = value; }
+        }
+    }
+}
