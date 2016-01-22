305afe5c38ce9df93fe7eba550b4de989ffa86c0
Kevin Croke <k.croke@shipworks.com>
2016-01-22 08:14:19 -0600

    Add AddStoreResponse

---
+++ Code/ShipWorks/ApplicationCore/Licensing/AddStoreResponse.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml;
+using Interapptive.Shared.Utility;
+
+namespace ShipWorks.ApplicationCore.Licensing
+{
+    /// <summary>
+    /// Response from TangoWebClient.AddStore 
+    /// </summary>
+    public class AddStoreResponse
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AddStoreResponse(XmlDocument xmlResponse)
+        {
+            XPathNamespaceNavigator xpath = new XPathNamespaceNavigator(xmlResponse);
+            Error = XPathUtility.Evaluate(xpath, "//*[local-name()='Error']", "");
+            Key = XPathUtility.Evaluate(xpath, "//*[local-name()='LicenseKey']", "");
+
+            Success = string.IsNullOrEmpty(Error);
+        }
+
+        /// <summary>
+        /// License Key
+        /// </summary>
+        public string Key { get; }
+
+        /// <summary>
+        /// Error Message
+        /// </summary>
+        public string Error { get; }
+
+        /// <summary>
+        /// Success!
+        /// </summary>
+        public bool Success { get; }
+    }
+}
