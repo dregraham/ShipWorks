8c31fc849dd3ffd39c2ec9ad3fe0a79b5ba59ae6
Chris Hicks <c.hicks@shipworks.com>
2016-01-25 15:27:01 -0600

    Create ActiveStore class

---
+++ Code/ShipWorks/ApplicationCore/Licensing/ActiveStore.cs
@@ -0,0 +1,18 @@
+﻿namespace ShipWorks.ApplicationCore.Licensing
+{
+    /// <summary>
+    /// An active store retrieved from Tango
+    /// </summary>
+    public class ActiveStore
+    {
+        /// <summary>
+        /// The store license key.
+        /// </summary>
+        public string StoreLicenseKey { get; set; }
+
+        /// <summary>
+        /// The store name
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
