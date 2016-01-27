2101ec363855a5f6c8a5498eb56e3de692db6ee9
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-01-27 14:12:36 -0600

    Fix merge conflict

--- Code/ShipWorks.UI/Controls/ChannelLimit/ChannelLimitDlg.xaml.cs
+++ Code/ShipWorks.UI/Controls/ChannelLimit/ChannelLimitDlg.xaml.cs
@@ -18,7 +18,7 @@ namespace ShipWorks.UI.Controls.ChannelLimit
     /// <summary>
     /// Interaction logic for ChannelLimitDlg.xaml
     /// </summary>
-    public partial class ChannelLimitDlg : Window, IChannelLimitDlg
+    public partial class ChannelLimitDlg : IChannelLimitDlg
     {
         private readonly ChannelLimitViewModel viewModel;
 
