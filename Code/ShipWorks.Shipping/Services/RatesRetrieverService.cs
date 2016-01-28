60b1c29a353568ed6a2c7c14416e1b7185d28a13
Andrew Benz <a.benz@shipworks.com>
2016-01-28 14:47:57 -0600

    Add initial loading popup to the rating panel

--- Code/ShipWorks.Shipping/Services/RatesRetrieverService.cs
+++ Code/ShipWorks.Shipping/Services/RatesRetrieverService.cs
@@ -2,7 +2,9 @@
 using System.Linq;
 using System.Reactive.Linq;
 using Autofac.Features.Indexed;
+using Interapptive.Shared.Collections;
 using Interapptive.Shared.Threading;
+using log4net;
 using ShipWorks.ApplicationCore;
 using ShipWorks.Core.Messaging;
 using ShipWorks.Messaging.Messages;
@@ -19,6 +21,7 @@ namespace ShipWorks.Shipping.Services
         private readonly IRatesRetriever ratesRetriever;
         private readonly IIndex<ShipmentTypeCode, IRateHashingService> rateHashingServiceLookup;
         private readonly ISchedulerProvider schedulerProvider;
+        private readonly ILog log;
         private IDisposable subscription;
 
         /// <summary>
@@ -26,12 +29,13 @@ namespace ShipWorks.Shipping.Services
         /// </summary>
         public RatesRetrieverService(IMessenger messenger, IRatesRetriever ratesRetriever,
             IIndex<ShipmentTypeCode, IRateHashingService> rateHashingServiceLookup,
-            ISchedulerProvider schedulerProvider)
+            ISchedulerProvider schedulerProvider, Func<Type, ILog> logFactory)
         {
             this.messenger = messenger;
             this.ratesRetriever = ratesRetriever;
             this.rateHashingServiceLookup = rateHashingServiceLookup;
             this.schedulerProvider = schedulerProvider;
+            log = logFactory(typeof(RatesRetrieverService));
         }
 
         /// <summary>
@@ -46,7 +50,17 @@ namespace ShipWorks.Shipping.Services
                 .Do(x => messenger.Send(new RatesRetrievingMessage(this, x.RatingHash)))
                 .Throttle(TimeSpan.FromMilliseconds(250), schedulerProvider.Default)
                 .Select(x => new { x.ShipmentAdapter, x.RatingHash, Rates = ratesRetriever.GetRates(x.ShipmentAdapter.Shipment) })
-                .Subscribe(x => messenger.Send(new RatesRetrievedMessage(this, x.RatingHash, x.Rates, x.ShipmentAdapter)));
+                .SubscribeWithRetry(x => messenger.Send(new RatesRetrievedMessage(this, x.RatingHash, x.Rates, x.ShipmentAdapter)), HandleException);
+        }
+
+        /// <summary>
+        /// Handle exceptions generated while getting rates
+        /// </summary>
+        private bool HandleException(Exception ex)
+        {
+            log.Warn(ex);
+
+            return true;
         }
 
         /// <summary>
