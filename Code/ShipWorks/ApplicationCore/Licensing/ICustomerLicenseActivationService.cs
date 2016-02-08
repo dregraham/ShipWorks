8c890a38aaaab3b60a97b60146c5ac4175717ecb
Chris Hicks <c.hicks@shipworks.com>
2016-02-08 16:46:24 -0600

    Create ICustomeLicenseActivationService

---
+++ Code/ShipWorks/ApplicationCore/Licensing/ICustomerLicenseActivationService.cs
@@ -0,0 +1,15 @@
+﻿namespace ShipWorks.ApplicationCore.Licensing
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public interface ICustomerLicenseActivationService
+    {
+        /// <summary>
+        /// Activates a CustomerLicense
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        ICustomerLicense Activate(string username, string password);
+    }
+}
