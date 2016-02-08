59c6a7d371c8412f31b8601dc21ca7beac7940cb
Andrew Benz <a.benz@shipworks.com>
2016-02-08 16:15:47 -0600

    Fix crash when shipping dlg is disposed before rate timer fires

--- Code/ShipWorks/Shipping/ShippingDlg.cs
+++ Code/ShipWorks/Shipping/ShippingDlg.cs
@@ -2306,6 +2306,8 @@ namespace ShipWorks.Shipping
         /// </summary>
         private void OnClosing(object sender, FormClosingEventArgs e)
         {
+            getRatesTimer.Stop();
+
             // Disable the ShipSense timer and make one final call to synchronize to make sure we have
             // everything matching that should be matching; otherwise changing a value and closing
             // the dialog before the timer kicks would result in shipments being out of sync
