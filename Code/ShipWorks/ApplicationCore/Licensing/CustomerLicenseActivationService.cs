c9d7efe0ac18f43f03e718f78d178b461c7c8e4f
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-02-11 09:53:56 -0600

    Continue activation process if Usps server returns an error
    in CustomerLicenseActivationService log error from usps and continue activation
    instead of throwing

--- Code/ShipWorks/ApplicationCore/Licensing/CustomerLicenseActivationService.cs
+++ Code/ShipWorks/ApplicationCore/Licensing/CustomerLicenseActivationService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using Interapptive.Shared.Utility;
+using log4net;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Shipping.Carriers;
 using ShipWorks.Shipping.Carriers.Postal.Usps;
@@ -18,6 +19,7 @@ namespace ShipWorks.ApplicationCore.Licensing
         private readonly IUspsWebClient uspsWebClient;
         private readonly ICarrierAccountRepository<UspsAccountEntity> uspsAccountRepository;
         private readonly Func<string, ICustomerLicense> licenseFactory;
+        private readonly ILog log;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CustomerLicenseActivationService"/> class.
@@ -26,12 +28,17 @@ namespace ShipWorks.ApplicationCore.Licensing
         /// <param name="uspsWebClientFactory"></param>
         /// <param name="uspsAccountRepository">The usps account repository.</param>
         /// <param name="licenseFactory">The license factory.</param>
-        public CustomerLicenseActivationService(ITangoWebClient tangoWebClient, Func<UspsResellerType, IUspsWebClient> uspsWebClientFactory, ICarrierAccountRepository<UspsAccountEntity> uspsAccountRepository, Func<string, ICustomerLicense> licenseFactory)
+        /// <param name="logFactory">Logging factory</param>
+        public CustomerLicenseActivationService(ITangoWebClient tangoWebClient,
+            Func<UspsResellerType, IUspsWebClient> uspsWebClientFactory,
+            ICarrierAccountRepository<UspsAccountEntity> uspsAccountRepository,
+            Func<string, ICustomerLicense> licenseFactory, Func<Type, ILog> logFactory)
         {
             this.tangoWebClient = tangoWebClient;
-            this.uspsWebClient = uspsWebClientFactory(UspsResellerType.None);
+            uspsWebClient = uspsWebClientFactory(UspsResellerType.None);
             this.uspsAccountRepository = uspsAccountRepository;
             this.licenseFactory = licenseFactory;
+            log = logFactory(typeof (CustomerLicenseActivationService));
         }
 
         /// <summary>
@@ -50,14 +57,23 @@ namespace ShipWorks.ApplicationCore.Licensing
             string associatedStampsUserName = activateLicenseResponse.Context.AssociatedStampsUserName;
             if (!uspsAccountRepository.Accounts.Any() && !string.IsNullOrWhiteSpace(associatedStampsUserName))
             {
-                UspsAccountEntity uspsAccount = new UspsAccountEntity()
+                UspsAccountEntity uspsAccount = new UspsAccountEntity
                 {
                     Username = associatedStampsUserName,
                     Password = SecureText.Encrypt(password, associatedStampsUserName)
                 };
 
-                uspsWebClient.PopulateUspsAccountEntity(uspsAccount);
-                uspsAccountRepository.Save(uspsAccount);
+                try
+                {
+                    uspsWebClient.PopulateUspsAccountEntity(uspsAccount);
+                    uspsAccountRepository.Save(uspsAccount);
+                }
+                catch (Exception ex) when (ex is UspsApiException || ex is UspsException)
+                {
+                    // Populating the account information failed, due to an issue with USPS
+                    // log the error and continue with the activation process
+                    log.Error($"Error when populating USPS account information: {ex.Message}");
+                }
             }
 
             ICustomerLicense license = licenseFactory(activateLicenseResponse.Context.Key);
