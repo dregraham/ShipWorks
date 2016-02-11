f92f5ba88968f083ca9e1af4e262c519b1b98339
Chris Hicks <c.hicks@shipworks.com>
2016-02-11 09:37:44 -0600

    Remove default value from PendingInitialAccount column in UspsAccount

--- Code/ShipWorks/Data/Administration/Scripts/Installation/CreateSchema.sql
+++ Code/ShipWorks/Data/Administration/Scripts/Installation/CreateSchema.sql
@@ -4730,7 +4730,7 @@ CREATE TABLE [dbo].[UspsAccount]
 [UspsReseller] [int] NOT NULL,
 [ContractType] [int] NOT NULL,
 [CreatedDate] [datetime] NOT NULL,
-[PendingInitialAccount] [bit] NOT NULL DEFAULT ((0))
+[PendingInitialAccount] [bit] NOT NULL
 )
 GO
 PRINT N'Creating primary key [PK_PostalUspsAccount] on [dbo].[UspsAccount]'
