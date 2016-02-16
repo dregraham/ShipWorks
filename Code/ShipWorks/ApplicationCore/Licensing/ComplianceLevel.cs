3b1369ba4975b5bed042b6e10de285573cc809d0
Chris Hicks <c.hicks@shipworks.com>
2016-02-16 15:20:52 -0600

    Add comments, obfuscation attributes, and description to ComplianceLevel

--- Code/ShipWorks/ApplicationCore/Licensing/ComplianceLevel.cs
+++ Code/ShipWorks/ApplicationCore/Licensing/ComplianceLevel.cs
@@ -1,8 +1,18 @@
-﻿namespace ShipWorks.ApplicationCore.Licensing
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace ShipWorks.ApplicationCore.Licensing
 {
+    /// <summary>
+    /// Compliance state of a license
+    /// </summary>
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = true, StripAfterObfuscation = false)]
     public enum ComplianceLevel
     {
+        [Description("Not Compliant")]
         NotCompliant = 0,
+
+        [Description("Not Compliant")]
         Compliant = 1
     }
 }
