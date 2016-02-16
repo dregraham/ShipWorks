aad742eae6ebbc3cf124780ad80dfd607f189cf0
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-02-16 15:13:07 -0600

    Create EnforcementContext Enum

---
+++ Code/ShipWorks/ApplicationCore/Licensing/LicenseEnforcement/EnforcementContext.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace ShipWorks.ApplicationCore.Licensing.LicenseEnforcement
+{ 
+    /// <summary>
+    /// Enum for license enforcement context
+    /// </summary>
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum EnforcementContext
+    {
+        [Description("Logging into ShipWorks")]
+        Login = 0,
+
+        [Description("Creating a label")]
+        CreateLabel = 1,
+
+        [Description("Downloading orders")]
+        Download = 2
+    }
+}
