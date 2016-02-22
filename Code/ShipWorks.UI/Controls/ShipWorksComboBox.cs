b97bc8f9e4516a55735c02c65e1d5e8f48a0f471
Andrew Benz <a.benz@shipworks.com>
2016-02-22 11:46:40 -0600

    Move enum into its own file

--- Code/ShipWorks.UI/Controls/ShipWorksComboBox.cs
+++ Code/ShipWorks.UI/Controls/ShipWorksComboBox.cs
@@ -1,34 +1,11 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Linq;
-using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 
 namespace ShipWorks.UI.Controls
 {
-    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = true, StripAfterObfuscation = false)]
-    /// Named locations in a list
-    /// </summary>
-    public enum RelativeIndex
-    {
-        [Description("None")]
-        /// Don't select a location
-        /// </summary>
-        None,
-
-        [Description("First")]
-        /// Select the first item in a list
-        /// </summary>
-        First,
-
-        [Description("Last")]
-        /// Select the last item in a list
-        /// </summary>
-        Last
-    }
-
     /// <summary>
     /// Combo box that has ShipWorks customizations
     /// </summary>
