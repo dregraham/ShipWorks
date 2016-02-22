82f42218849baba7a0a3e6a11163ff5914df266b
Chris Hicks <c.hicks@shipworks.com>
2016-02-22 10:26:06 -0600

    Create ApproachingShipmentLimitEnforcer that displays a message warning the user when they are approaching their shipment limit on login

---
+++ Code/ShipWorks.Tests/ApplicationCore/Licensing/LicenseEnforcement/ApproachingShipmentLimitEnforcerTest.cs
@@ -0,0 +1,185 @@
+﻿using System.Windows.Forms;
+using Autofac.Extras.Moq;
+using Interapptive.Shared.Utility;
+using Moq;
+using ShipWorks.ApplicationCore.Licensing;
+using ShipWorks.ApplicationCore.Licensing.LicenseEnforcement;
+using ShipWorks.Editions;
+using Xunit;
+
+namespace ShipWorks.Tests.ApplicationCore.Licensing.LicenseEnforcement
+{
+    public class ApproachingShipmentLimitEnforcerTest
+    {
+        [Fact]
+        public void Priority_ReturnsLow()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                ApproachingShipmentLimitEnforcer testObject = mock.Create<ApproachingShipmentLimitEnforcer>();
+
+                Assert.Equal(EnforcementPriority.Low, testObject.Priority);
+            }
+        }
+
+        [Fact]
+        public void EditionFeature_ReturnsShipmentCount()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                ApproachingShipmentLimitEnforcer testObject = mock.Create<ApproachingShipmentLimitEnforcer>();
+
+                Assert.Equal(EditionFeature.ShipmentCount, testObject.EditionFeature);
+            }
+        }
+
+        [Fact]
+        public void Enforce_ReturnsCompliantWithWarningMessage_WhenOverShipmentLimitWarningThreshold()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                ApproachingShipmentLimitEnforcer testObject = mock.Create<ApproachingShipmentLimitEnforcer>();
+
+                Mock<ILicenseCapabilities> licenseCapabilities = mock.Mock<ILicenseCapabilities>();
+
+                licenseCapabilities.Setup(l => l.ProcessedShipments).Returns(9);
+                licenseCapabilities.Setup(l => l.ShipmentLimit).Returns(10);
+
+                EnumResult<ComplianceLevel> result = testObject.Enforce(licenseCapabilities.Object, EnforcementContext.Login);
+
+                Assert.Equal(ComplianceLevel.Compliant, result.Value);
+                Assert.Equal("You are approaching your shipment limit for the current billing cycle", result.Message);
+            }
+        }
+
+        [Fact]
+        public void Enforce_ReturnsCompliantWithWarningMessage_WhenAtShipmentLimitWarningThreshold()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                ApproachingShipmentLimitEnforcer testObject = mock.Create<ApproachingShipmentLimitEnforcer>();
+
+                Mock<ILicenseCapabilities> licenseCapabilities = mock.Mock<ILicenseCapabilities>();
+
+                licenseCapabilities.Setup(l => l.ProcessedShipments).Returns(8);
+                licenseCapabilities.Setup(l => l.ShipmentLimit).Returns(10);
+
+                EnumResult<ComplianceLevel> result = testObject.Enforce(licenseCapabilities.Object, EnforcementContext.Login);
+
+                Assert.Equal(ComplianceLevel.Compliant, result.Value);
+                Assert.Equal("You are approaching your shipment limit for the current billing cycle", result.Message);
+            }
+        }
+
+        [Fact]
+        public void Enforce_ReturnsCompliantWithNoMessage_WhenUnderShipmentLimitWarningThreshold()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                ApproachingShipmentLimitEnforcer testObject = mock.Create<ApproachingShipmentLimitEnforcer>();
+
+                Mock<ILicenseCapabilities> licenseCapabilities = mock.Mock<ILicenseCapabilities>();
+
+                licenseCapabilities.Setup(l => l.ProcessedShipments).Returns(7);
+                licenseCapabilities.Setup(l => l.ShipmentLimit).Returns(10);
+
+                EnumResult<ComplianceLevel> result = testObject.Enforce(licenseCapabilities.Object, EnforcementContext.Login);
+
+                Assert.Equal(ComplianceLevel.Compliant, result.Value);
+                Assert.Equal(string.Empty, result.Message);
+            }
+        }
+
+        [Fact]
+        public void Enforce_ReturnsCompliantWithNoMessage_WhenIsInTrialAndOverShipmentLimitWarningThreshold()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                ApproachingShipmentLimitEnforcer testObject = mock.Create<ApproachingShipmentLimitEnforcer>();
+
+                Mock<ILicenseCapabilities> licenseCapabilities = mock.Mock<ILicenseCapabilities>();
+
+                licenseCapabilities.Setup(l => l.IsInTrial).Returns(true);
+                licenseCapabilities.Setup(l => l.ProcessedShipments).Returns(9);
+                licenseCapabilities.Setup(l => l.ShipmentLimit).Returns(10);
+
+                EnumResult<ComplianceLevel> result = testObject.Enforce(licenseCapabilities.Object, EnforcementContext.Login);
+
+                Assert.Equal(ComplianceLevel.Compliant, result.Value);
+                Assert.Equal(string.Empty, result.Message);
+            }
+        }
+
+        [Fact]
+        public void Enforce_ShowsDialogWithShipmentLimitWarningMessage_WhenContextIsLoginAndOverShipmentLimitWarningThreshold()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                Mock<IUpgradePlanDlgFactory> dlgFactory = mock.Mock<IUpgradePlanDlgFactory>();
+                Mock<IDialog> dlg = mock.Mock<IDialog>();
+
+                dlgFactory.Setup(f => f.Create(It.IsAny<string>(), It.IsAny<IWin32Window>())).Returns(dlg.Object);
+
+                ApproachingShipmentLimitEnforcer testObject = mock.Create<ApproachingShipmentLimitEnforcer>();
+
+                Mock<ILicenseCapabilities> licenseCapabilities = mock.Mock<ILicenseCapabilities>();
+
+                licenseCapabilities.Setup(l => l.ProcessedShipments).Returns(9);
+                licenseCapabilities.Setup(l => l.ShipmentLimit).Returns(10);
+
+                testObject.Enforce(licenseCapabilities.Object, EnforcementContext.Login, null);
+
+                dlg.Verify(d => d.ShowDialog(), Times.Once);
+                dlgFactory.Verify(d =>
+                    d.Create("You are approaching your shipment limit for the current billing cycle", null),
+                    Times.Once);
+            }
+        }
+
+        [Fact]
+        public void Enforce_DoesNotShowsDialog_WhenContextIsNotLogin()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                Mock<IUpgradePlanDlgFactory> dlgFactory = mock.Mock<IUpgradePlanDlgFactory>();
+                Mock<IDialog> dlg = mock.Mock<IDialog>();
+
+                dlgFactory.Setup(f => f.Create(It.IsAny<string>(), It.IsAny<IWin32Window>())).Returns(dlg.Object);
+
+                ApproachingShipmentLimitEnforcer testObject = mock.Create<ApproachingShipmentLimitEnforcer>();
+
+                Mock<ILicenseCapabilities> licenseCapabilities = mock.Mock<ILicenseCapabilities>();
+
+                licenseCapabilities.Setup(l => l.ProcessedShipments).Returns(9);
+                licenseCapabilities.Setup(l => l.ShipmentLimit).Returns(10);
+
+                testObject.Enforce(licenseCapabilities.Object, EnforcementContext.CreateLabel, null);
+
+                dlg.Verify(d => d.ShowDialog(), Times.Never);
+            }
+        }
+
+        [Fact]
+        public void Enforce_DoesNotShowsDialog_WhenUnderShipmentLimitWarningThreshold()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                Mock<IUpgradePlanDlgFactory> dlgFactory = mock.Mock<IUpgradePlanDlgFactory>();
+                Mock<IDialog> dlg = mock.Mock<IDialog>();
+
+                dlgFactory.Setup(f => f.Create(It.IsAny<string>(), It.IsAny<IWin32Window>())).Returns(dlg.Object);
+
+                ApproachingShipmentLimitEnforcer testObject = mock.Create<ApproachingShipmentLimitEnforcer>();
+
+                Mock<ILicenseCapabilities> licenseCapabilities = mock.Mock<ILicenseCapabilities>();
+
+                licenseCapabilities.Setup(l => l.ProcessedShipments).Returns(7);
+                licenseCapabilities.Setup(l => l.ShipmentLimit).Returns(10);
+
+                testObject.Enforce(licenseCapabilities.Object, EnforcementContext.Login, null);
+
+                dlg.Verify(d => d.ShowDialog(), Times.Never);
+            }
+        }
+    }
+}
