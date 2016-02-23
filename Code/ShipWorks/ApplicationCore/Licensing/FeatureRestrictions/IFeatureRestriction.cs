2f264727a4bfd0813c18487a688e8e9746e7e3d5
Kevin Croke <k.croke@shipworks.com>
2016-02-23 15:28:11 -0600

    Create IFeatureRestriction

---
+++ Code/ShipWorks/ApplicationCore/Licensing/FeatureRestrictions/IFeatureRestriction.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+using ShipWorks.Editions;
+
+namespace ShipWorks.ApplicationCore.Licensing.FeatureRestrictions
+{
+    public interface IFeatureRestriction
+    {
+        /// <summary>
+        /// Gets the edition feature.
+        /// </summary>
+        EditionFeature EditionFeature { get; }
+
+        /// <summary>
+        /// Checks the restriction for this feature
+        /// </summary>
+        EditionRestrictionLevel Check(ILicenseCapabilities capabilities, object data);
+
+        /// <summary>
+        /// Handles the restriction for this feature
+        /// </summary>
+        bool Handle(IWin32Window owner, object data);
+    }
+}
