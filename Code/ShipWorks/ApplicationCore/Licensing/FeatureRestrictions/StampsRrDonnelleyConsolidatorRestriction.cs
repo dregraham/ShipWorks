1a873c9ef38ddec7b9410817a0a875d7d007058b
Chris Hicks <c.hicks@shipworks.com>
2016-02-25 11:00:05 -0600

    Implement StampsRrDonnelleyConsolidatorRestriction

---
+++ Code/ShipWorks/ApplicationCore/Licensing/FeatureRestrictions/StampsRrDonnelleyConsolidatorRestriction.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+using ShipWorks.Editions;
+
+namespace ShipWorks.ApplicationCore.Licensing.FeatureRestrictions
+{
+    public class StampsRrDonnelleyConsolidatorRestriction : IFeatureRestriction
+    {
+        /// <summary>
+        /// The edition feature
+        /// </summary>
+        public EditionFeature EditionFeature => EditionFeature.StampsRrDonnelleyConsolidator;
+
+        /// <summary>
+        /// Checks the license capabilities to see if StampsRrDonnelleyConsolidator is enabled
+        /// </summary>
+        public EditionRestrictionLevel Check(ILicenseCapabilities capabilities, object data)
+        {
+            return capabilities.StampsRrDonnelleyConsolidator ? EditionRestrictionLevel.None : EditionRestrictionLevel.Hidden;
+        }
+
+        /// <summary>
+        /// Nothing to handle, return false
+        /// </summary>
+        public bool Handle(IWin32Window owner, object data) => false;
+    }
+}
