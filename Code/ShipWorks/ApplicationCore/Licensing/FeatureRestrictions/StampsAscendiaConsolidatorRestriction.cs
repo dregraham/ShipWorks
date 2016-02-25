2595376df2cad263c02523d790510f5232fef074
Chris Hicks <c.hicks@shipworks.com>
2016-02-25 10:49:12 -0600

    Implement StampsAscendiaConsolidatorRestriction

---
+++ Code/ShipWorks/ApplicationCore/Licensing/FeatureRestrictions/StampsAscendiaConsolidatorRestriction.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+using ShipWorks.Editions;
+
+namespace ShipWorks.ApplicationCore.Licensing.FeatureRestrictions
+{
+    public class StampsAscendiaConsolidatorRestriction : IFeatureRestriction
+    {
+        /// <summary>
+        /// The edition feature
+        /// </summary>
+        public EditionFeature EditionFeature => EditionFeature.StampsAscendiaConsolidator;
+
+        /// <summary>
+        /// Checks the license capabilities to see if StampsAscendiaConsolidator is enabled
+        /// </summary>
+        public EditionRestrictionLevel Check(ILicenseCapabilities capabilities, object data)
+        {
+            return capabilities.StampsAscendiaConsolidator ? EditionRestrictionLevel.None : EditionRestrictionLevel.Hidden;
+        }
+
+        /// <summary>
+        /// Nothing to handle, return false
+        /// </summary>
+        public bool Handle(IWin32Window owner, object data) => false;
+    }
+}
