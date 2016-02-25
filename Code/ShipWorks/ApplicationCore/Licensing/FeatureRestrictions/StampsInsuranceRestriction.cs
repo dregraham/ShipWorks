d43e29db31b6116c659dab8a3736c1ccaeea9528
Chris Hicks <c.hicks@shipworks.com>
2016-02-25 10:15:36 -0600

    Implement StampsInsuranceRestriction and replace EditionManger calls with ILicenseService calls

---
+++ Code/ShipWorks/ApplicationCore/Licensing/FeatureRestrictions/StampsInsuranceRestriction.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+using ShipWorks.Editions;
+
+namespace ShipWorks.ApplicationCore.Licensing.FeatureRestrictions
+{
+    public class StampsInsuranceRestriction : IFeatureRestriction
+    {
+        /// <summary>
+        /// The edition feature
+        /// </summary>
+        public EditionFeature EditionFeature => EditionFeature.StampsInsurance;
+
+        /// <summary>
+        /// Checks the license capabilities to see if StampsInsurance is enabled
+        /// </summary>
+        public EditionRestrictionLevel Check(ILicenseCapabilities capabilities, object data)
+        {
+            return capabilities.StampsInsurance ? EditionRestrictionLevel.None : EditionRestrictionLevel.Hidden;
+        }
+
+        /// <summary>
+        /// Nothing to handle, return false
+        /// </summary>
+        public bool Handle(IWin32Window owner, object data) => false;
+    }
+}
