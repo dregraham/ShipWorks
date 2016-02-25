5a09d63c446bb70e82f61bd448c8915e69388ad9
Chris Hicks <c.hicks@shipworks.com>
2016-02-25 10:58:51 -0600

    Implement StampsDhlConsolidatorRestriction

---
+++ Code/ShipWorks/ApplicationCore/Licensing/FeatureRestrictions/StampsDhlConsolidatorRestriction.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+using ShipWorks.Editions;
+
+namespace ShipWorks.ApplicationCore.Licensing.FeatureRestrictions
+{
+    public class StampsDhlConsolidatorRestriction : IFeatureRestriction
+    {
+        /// <summary>
+        /// The edition feature
+        /// </summary>
+        public EditionFeature EditionFeature => EditionFeature.StampsDhlConsolidator;
+
+        /// <summary>
+        /// Checks the license capabilities to see if StampsDhlConsolidator is enabled
+        /// </summary>
+        public EditionRestrictionLevel Check(ILicenseCapabilities capabilities, object data)
+        {
+            return capabilities.StampsDhlConsolidator ? EditionRestrictionLevel.None : EditionRestrictionLevel.Hidden;
+        }
+
+        /// <summary>
+        /// Nothing to handle, return false
+        /// </summary>
+        public bool Handle(IWin32Window owner, object data) => false;
+    }
+}
