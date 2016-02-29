b0ade93d8382329a4931c976cc07665d586ab207
Andrew Benz <a.benz@shipworks.com>
2016-02-29 12:33:49 -0600

    Extract insurance UI into a new XAML file

---
+++ Code/ShipWorks.Shipping.UI/ShippingPanel/Insurance/InsurancePromoControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ShipWorks.Shipping.UI.ShippingPanel.Insurance
+{
+    /// <summary>
+    /// Interaction logic for InsurancePromoControl.xaml
+    /// </summary>
+    public partial class InsurancePromoControl : UserControl
+    {
+        public InsurancePromoControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
