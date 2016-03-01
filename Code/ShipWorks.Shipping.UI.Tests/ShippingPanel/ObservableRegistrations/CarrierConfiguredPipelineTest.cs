08300d7bfdf0b05929018b1b76fa97e90d71a787
Andrew Benz <a.benz@shipworks.com>
2016-03-01 13:02:34 -0600

    Update shipping panel when carrier is configured
    When a user configures a carrier, there was a chance that the shipment's label format could get modified which would cause the shipping panel to display the dialog box about someone else changing the shipment.  Now, we'll update the shipment in the shipping panel, if necessary

---
+++ Code/ShipWorks.Shipping.UI.Tests/ShippingPanel/ObservableRegistrations/CarrierConfiguredPipelineTest.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
+using Interapptive.Shared.Threading;
+using Moq;
+using ShipWorks.Core.Messaging;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Messaging.Messages;
+using ShipWorks.Shipping.Services;
+using ShipWorks.Shipping.UI.ShippingPanel;
+using ShipWorks.Shipping.UI.ShippingPanel.ObservableRegistrations;
+using ShipWorks.Tests.Shared;
+using Xunit;
+
+namespace ShipWorks.Shipping.UI.Tests.ShippingPanel.ObservableRegistrations
+{
+    public class CarrierConfiguredPipelineTest : IDisposable
+    {
+        readonly AutoMock mock;
+        readonly TestSchedulerProvider schedulerProvider;
+        readonly Mock<ShippingPanelViewModel> viewModel;
+        readonly Subject<IShipWorksMessage> messages;
+
+        public CarrierConfiguredPipelineTest()
+        {
+            schedulerProvider = new TestSchedulerProvider();
+            messages = new Subject<IShipWorksMessage>();
+
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+            mock.Provide<ISchedulerProvider>(schedulerProvider);
+            mock.Provide<IObservable<IShipWorksMessage>>(messages);
+
+            viewModel = mock.CreateMock<ShippingPanelViewModel>();
+        }
+
+        [Fact]
+        public void Register_DoesNotUpdateAllowEditing_WhenChangedCarrierDoesNotMatchViewModel()
+        {
+            viewModel.Setup(x => x.ShipmentType).Returns(ShipmentTypeCode.Usps);
+            var testObject = mock.Create<CarrierConfiguredPipeline>();
+            testObject.Register(viewModel.Object);
+
+            messages.OnNext(new CarrierConfiguredMessage(this, ShipmentTypeCode.FedEx));
+
+            viewModel.VerifySet(x => x.AllowEditing = false, Times.Never);
+        }
+
+        [Fact]
+        public void Register_DoesNotDelegateToShippingManager_WhenSchedulerHasNotAdvanced()
+        {
+            var testObject = mock.Create<CarrierConfiguredPipeline>();
+            testObject.Register(viewModel.Object);
+
+            messages.OnNext(new CarrierConfiguredMessage(this, viewModel.Object.ShipmentType));
+
+            mock.Mock<IShippingManager>().Verify(x => x.GetShipment(It.IsAny<long>()), Times.Never);
+        }
+
+        [Fact]
+        public void Register_DelegatesToShippingManager_OnTaskPool()
+        {
+            viewModel.Setup(x => x.Shipment).Returns(new ShipmentEntity { ShipmentID = 123 });
+            var testObject = mock.Create<CarrierConfiguredPipeline>();
+            testObject.Register(viewModel.Object);
+
+            messages.OnNext(new CarrierConfiguredMessage(this, viewModel.Object.ShipmentType));
+            schedulerProvider.TaskPool.Start();
+
+            mock.Mock<IShippingManager>().Verify(x => x.GetShipment(123));
+        }
+
+        [Fact]
+        public void Register_DoesNotLoadShipment_WhenViewModelNoLongerShowsPreviousShipment()
+        {
+            var shipmentAdapter = mock.CreateMock<ICarrierShipmentAdapter>();
+            shipmentAdapter.Setup(x => x.Shipment).Returns(new ShipmentEntity { ShipmentID = 123 });
+
+            mock.Mock<IShippingManager>()
+                .Setup(x => x.GetShipment(It.IsAny<long>()))
+                .Returns(shipmentAdapter.Object);
+            var testObject = mock.Create<CarrierConfiguredPipeline>();
+            testObject.Register(viewModel.Object);
+
+            messages.OnNext(new CarrierConfiguredMessage(this, viewModel.Object.ShipmentType));
+            schedulerProvider.TaskPool.Start();
+            viewModel.Setup(x => x.Shipment).Returns(new ShipmentEntity { ShipmentID = 999 });
+            schedulerProvider.Dispatcher.Start();
+
+            viewModel.Verify(x => x.Populate(It.IsAny<ICarrierShipmentAdapter>()), Times.Never);
+        }
+
+        [Fact]
+        public void Register_DoesNotLoadShipment_WhenDispatcherSchedulerHasNotElapsed()
+        {
+            var shipment = new ShipmentEntity { ShipmentID = 123 };
+            var shipmentAdapter = mock.CreateMock<ICarrierShipmentAdapter>();
+            shipmentAdapter.Setup(x => x.Shipment).Returns(shipment);
+
+            mock.Mock<IShippingManager>()
+                .Setup(x => x.GetShipment(It.IsAny<long>()))
+                .Returns(shipmentAdapter.Object);
+            var testObject = mock.Create<CarrierConfiguredPipeline>();
+            testObject.Register(viewModel.Object);
+
+            messages.OnNext(new CarrierConfiguredMessage(this, viewModel.Object.ShipmentType));
+            schedulerProvider.TaskPool.Start();
+            viewModel.Setup(x => x.Shipment).Returns(shipment);
+
+            viewModel.Verify(x => x.Populate(It.IsAny<ICarrierShipmentAdapter>()), Times.Never);
+        }
+
+        [Fact]
+        public void Register_LoadsShipment_WhenDispatcherSchedulerHasElapsed()
+        {
+            var shipment = new ShipmentEntity { ShipmentID = 123 };
+            var shipmentAdapter = mock.CreateMock<ICarrierShipmentAdapter>();
+            shipmentAdapter.Setup(x => x.Shipment).Returns(shipment);
+
+            mock.Mock<IShippingManager>()
+                .Setup(x => x.GetShipment(It.IsAny<long>()))
+                .Returns(shipmentAdapter.Object);
+            var testObject = mock.Create<CarrierConfiguredPipeline>();
+            testObject.Register(viewModel.Object);
+
+            messages.OnNext(new CarrierConfiguredMessage(this, viewModel.Object.ShipmentType));
+            schedulerProvider.TaskPool.Start();
+            viewModel.Setup(x => x.Shipment).Returns(shipment);
+            schedulerProvider.Dispatcher.Start();
+
+            viewModel.Verify(x => x.Populate(shipmentAdapter.Object));
+        }
+
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+    }
+}
