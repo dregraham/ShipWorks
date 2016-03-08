a97f2b71fb560cb8e314b68b56c5e4eb9da99e64
Kevin Croke <k.croke@shipworks.com>
2016-03-08 09:59:30 -0600

    Add AssociateShipworksWithItselfResponseType

---
+++ Code/ShipWorks/ApplicationCore/Licensing/AssociateShipWorksWithItselfResponseType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ShipWorks.ApplicationCore.Licensing
+{
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum AssociateShipWorksWithItselfResponseType
+    {
+        [Description("Success")]
+        Success = 0,
+
+        [Description("Unknown")]
+        UnknownError = 1,
+
+        [Description("PO Box Not Allowed")]
+        POBoxNotAllowed = 2
+    }
+}
