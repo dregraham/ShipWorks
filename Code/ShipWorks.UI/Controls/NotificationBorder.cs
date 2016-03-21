61114f2012bd19ee75097f62ed87ee626e93a9bf
Andrew Benz <a.benz@shipworks.com>
2016-03-21 16:57:20 -0500

    Extract the processed and void details container into a new reusable border control

---
+++ Code/ShipWorks.UI/Controls/NotificationBorder.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ShipWorks.UI.Controls
+{
+    /// <summary>
+    /// Border used for notification that can display a character icon
+    /// </summary>
+    public class NotificationBorder : ContentControl
+    {
+        public static readonly DependencyProperty IconCharacterProperty =
+            DependencyProperty.Register("IconCharacter",
+                typeof(string),
+                typeof(NotificationBorder));
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        static NotificationBorder()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NotificationBorder), new FrameworkPropertyMetadata(typeof(NotificationBorder)));
+        }
+
+        /// <summary>
+        /// Character to use for the icon
+        /// </summary>
+        public string IconCharacter
+        {
+            get { return (string) GetValue(IconCharacterProperty); }
+            set { SetValue(IconCharacterProperty, value); }
+        }
+    }
+}
