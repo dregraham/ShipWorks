5a6763e8f5b80302ad1a1087de3daf31242648da
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-03-23 15:19:05 -0500

    stub out spark pay store type

---
+++ Code/ShipWorks/Stores/Platforms/SparkPay/SparkPayDownloader.cs
@@ -0,0 +1,21 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShipWorks.Stores.Platforms.SparkPay
+{
+    class SparkPayDownloader : StoreDownloader
+    {
+        public SparkPayDownloader(StoreEntity store) : base(store)
+        {
+
+        }
+        protected override void Download()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Code/ShipWorks/Stores/Platforms/SparkPay/SparkPayStoreType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Communication;
+using ShipWorks.Stores.Content;
+
+namespace ShipWorks.Stores.Platforms.SparkPay
+{
+    class SparkPayStoreType : StoreType
+    {
+        public SparkPayStoreType(StoreEntity store) : base(store)
+        {
+
+        }
+
+        public override StoreTypeCode TypeCode => StoreTypeCode.AmeriCommerce;
+
+        protected override string InternalLicenseIdentifier
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override StoreDownloader CreateDownloader()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override OrderIdentifier CreateOrderIdentifier(OrderEntity order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override StoreEntity CreateStoreInstance()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
