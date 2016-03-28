92f08cd3cb6e77495f519b3fca5a2a55be7eaaf9
John Yedlicka <j.yedlicka@shipworks.com>
2016-03-28 16:25:23 -0500

    Exclude UPS from the Best Rate setting regarding providers to use when rating

--- Code/ShipWorks/Shipping/Carriers/BestRate/BestRateSettingsControl.cs
+++ Code/ShipWorks/Shipping/Carriers/BestRate/BestRateSettingsControl.cs
@@ -11,7 +11,7 @@ namespace ShipWorks.Shipping.Carriers.BestRate
     /// </summary>
     public partial class BestRateSettingsControl : SettingsControlBase
     {
-        private static readonly List<ShipmentTypeCode> nonCarrierShipmentTypes = new List<ShipmentTypeCode>
+        private static readonly List<ShipmentTypeCode> NonCarrierShipmentTypes = new List<ShipmentTypeCode>
         {
             ShipmentTypeCode.None,
             ShipmentTypeCode.BestRate,
@@ -19,7 +19,9 @@ namespace ShipWorks.Shipping.Carriers.BestRate
             ShipmentTypeCode.PostalWebTools,
             ShipmentTypeCode.Endicia,
             ShipmentTypeCode.Express1Endicia,
-            ShipmentTypeCode.Express1Usps
+            ShipmentTypeCode.Express1Usps,
+            ShipmentTypeCode.UpsOnLineTools,
+            ShipmentTypeCode.UpsWorldShip
         };
 
         private bool isDirty;
@@ -69,7 +71,7 @@ namespace ShipWorks.Shipping.Carriers.BestRate
             // Finally, remove explicitly included shipment types to remove previously excluded types that are now shown globally
             settings.BestRateExcludedTypes = panelProviders.UnselectedShipmentTypes.Select(x => (int) x.ShipmentTypeCode)
                 .Union(settings.BestRateExcludedTypes)
-                .Union(nonCarrierShipmentTypes.Cast<int>())
+                .Union(NonCarrierShipmentTypes.Cast<int>())
                 .Except(panelProviders.SelectedShipmentTypes.Select(x => (int) x.ShipmentTypeCode))
                 .ToArray();
 
@@ -99,7 +101,7 @@ namespace ShipWorks.Shipping.Carriers.BestRate
         /// <param name="shipmentType">The shipment type to test</param>
         private static bool IsCarrierShippingType(ShipmentType shipmentType)
         {
-            return !nonCarrierShipmentTypes.Contains(shipmentType.ShipmentTypeCode);
+            return !NonCarrierShipmentTypes.Contains(shipmentType.ShipmentTypeCode);
         }
     }
 }
