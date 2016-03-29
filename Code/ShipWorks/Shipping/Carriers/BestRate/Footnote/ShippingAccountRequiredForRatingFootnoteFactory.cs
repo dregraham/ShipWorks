cc5b5b02e69424303b786e78a1d72a790563fa13
John Yedlicka <j.yedlicka@shipworks.com>
2016-03-29 12:13:29 -0500

    Clean up comments in ShippingAccountRequiredForRatingFootnoteFactory

--- Code/ShipWorks/Shipping/Carriers/BestRate/Footnote/ShippingAccountRequiredForRatingFootnoteFactory.cs
+++ Code/ShipWorks/Shipping/Carriers/BestRate/Footnote/ShippingAccountRequiredForRatingFootnoteFactory.cs
@@ -2,11 +2,17 @@
 
 namespace ShipWorks.Shipping.Carriers.BestRate.Footnote
 {
+    /// <summary>
+    /// An implementation of the IRateFootnoteFactory interface intended to be used to display a footnote
+    /// indicating that a shipping account is required to get rates.
+    /// </summary>
+    /// <seealso cref="ShipWorks.Shipping.Editing.Rating.IRateFootnoteFactory" />
     public class ShippingAccountRequiredForRatingFootnoteFactory : IRateFootnoteFactory
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="InformationFootnoteFactory"/> class.
+        /// Initializes a new instance of the <see cref="ShippingAccountRequiredForRatingFootnoteFactory"/> class.
         /// </summary>
+        /// <param name="shipmentTypeCode">The shipment type code that the footnote pertains to.</param>
         public ShippingAccountRequiredForRatingFootnoteFactory(ShipmentTypeCode shipmentTypeCode)
         {
             ShipmentTypeCode = shipmentTypeCode;
@@ -21,7 +27,7 @@ namespace ShipWorks.Shipping.Carriers.BestRate.Footnote
         /// Creates a footnote control.
         /// </summary>
         /// <param name="parameters">Parameters that allow footnotes to interact with the rates grid</param>
-        /// <returns>A instance of a RateFoonoteControl.</returns>
+        /// <returns>A instance of a RateFootnoteControl.</returns>
         public RateFootnoteControl CreateFootnote(FootnoteParameters parameters)
         {
             return new ShippingAccountRequiredForRatingFootnoteControl();
