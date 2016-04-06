7c78db68062a190ed2d385c4b2f9bfcd126b5163
Chris Hicks <c.hicks@shipworks.com>
2016-04-06 10:33:54 -0500

    TP#12981: Remove registration call from the invoice control, because it will be moved to the UpsSetupWizard

--- Code/ShipWorks/Shipping/Carriers/UPS/UpsAccountRateTypeControl.cs
+++ Code/ShipWorks/Shipping/Carriers/UPS/UpsAccountRateTypeControl.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using Interapptive.Shared.UI;
 using ShipWorks.Data.Model.EntityClasses;
-using ShipWorks.Shipping.Carriers.Api;
 using ShipWorks.Shipping.Carriers.UPS.Enums;
 using Interapptive.Shared.Utility;
-using ShipWorks.Shipping.Carriers.UPS.OnLineTools.Api;
 
 namespace ShipWorks.Shipping.Carriers.UPS
 {
@@ -20,8 +11,6 @@ namespace ShipWorks.Shipping.Carriers.UPS
     /// </summary>
     public partial class UpsAccountRateTypeControl : UserControl
     {
-        UpsAccountEntity account;
-
         private bool isInitialized = false;
 
         /// <summary>
@@ -33,26 +22,13 @@ namespace ShipWorks.Shipping.Carriers.UPS
         }
 
         /// <summary>
-        /// Gets or sets a value indicating whether user is creating a new UPS account
-        /// </summary>
-        /// <value>
-        /// <c>true</c> if this instance is creating new UPS account; otherwise, <c>false</c>.
-        /// </value>
-        private bool isCreatingNewUpsAccount;
-       
-
-        /// <summary>
         /// Initialize the control with the settings from the given account
         /// </summary>
         public void Initialize(UpsAccountEntity account, bool isCreatingNewUpsAccount)
         {
-            this.account = account;
-
             if (!isInitialized)
             {
                 isInitialized = true;
-
-                this.isCreatingNewUpsAccount = isCreatingNewUpsAccount;
 
                 if (isCreatingNewUpsAccount)
                 {
@@ -70,63 +46,12 @@ namespace ShipWorks.Shipping.Carriers.UPS
                         authorizationControl.Visible = true;
                     }
                 }
+
                 EnumHelper.BindComboBox<UpsRateType>(rateType);
 
                 rateType.SelectedValue = (UpsRateType) account.RateType;
 
                 HideOrShowNegotiatedRates();
-            }
-        }
-
-        /// <summary>
-        /// Save the UI selection to the account entity
-        /// </summary>
-        /// <exception cref="CarrierException">
-        /// Negotiated Rate not valid for new account.
-        /// or
-        /// Invoice Number required.
-        /// </exception>
-        public void RegisterAndSaveToEntity()
-        {
-            UpsRateType selectedRateType = (UpsRateType) rateType.SelectedValue;
-
-            if (selectedRateType == UpsRateType.Negotiated)
-            {
-                if (isCreatingNewUpsAccount)
-                {
-                    throw new CarrierException("Negotiated Rate not valid for new account.");
-                }
-
-                if (!account.InvoiceAuth)
-                {
-                    if (string.IsNullOrWhiteSpace(authorizationControl.InvoiceAuthorizationData.InvoiceNumber))
-                    {
-                        throw new CarrierException("Invoice Number required.");
-                    }
-
-                    RegisterAccount();
-                }
-            }
-
-            account.RateType = (int)selectedRateType;
-        }
-
-        /// <summary>
-        /// Registers the account.
-        /// </summary>
-        /// <exception cref="CarrierException"></exception>
-        private void RegisterAccount()
-        {
-            try
-            {
-                UpsClerk clerk = new UpsClerk(account);
-
-                clerk.RegisterAccount(account, authorizationControl.InvoiceAuthorizationData);
-            }
-            catch (UpsWebServiceException ex)
-            {
-                string errorMessage = ex.Message + Environment.NewLine + Environment.NewLine + "Note: UPS will lock out accounts for a 24 hour period if your invoice information cannot be authenticated after two attempts.";
-                throw new CarrierException(errorMessage,ex);
             }
         }
 
