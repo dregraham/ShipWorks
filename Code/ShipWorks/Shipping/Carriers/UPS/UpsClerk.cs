73cb5fa34fe50dd0d7bc24bd18750a766f525ad0
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-04-06 10:55:12 -0500

    update UpsClerk and IUpsClerk to return UpsRegistrationStatus when registering

--- Code/ShipWorks/Shipping/Carriers/UPS/UpsClerk.cs
+++ Code/ShipWorks/Shipping/Carriers/UPS/UpsClerk.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Text;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Shipping.Carriers.Api;
 using ShipWorks.Shipping.Carriers.UPS.InvoiceRegistration;
 using ShipWorks.Shipping.Carriers.UPS.OnLineTools;
 using ShipWorks.Shipping.Carriers.UPS.OnLineTools.Api;
-using ShipWorks.Shipping.Carriers.UPS.OpenAccount;
 using ShipWorks.Shipping.Carriers.UPS.OpenAccount.Api;
 using ShipWorks.Shipping.Carriers.UPS.WebServices.OpenAccount;
 
@@ -48,22 +45,30 @@ namespace ShipWorks.Shipping.Carriers.UPS
 
             ICarrierResponse carrierResponse = openAccountRequest.Submit();
             carrierResponse.Process();
-            
+
             OpenAccountResponse nativeResponse = (OpenAccountResponse)carrierResponse.NativeResponse;
-            
+
             return nativeResponse;
         }
 
         /// <summary>
-        /// Registers the account using the invoice information provided to get account based rates.
+        /// Registers the account without invoice information.
+        /// </summary>
+        /// <param name="accountEntity">The account entity.</param>
+        /// <exception cref="UpsException">A unique UserID could not be generated.  Please try again.</exception>
+        public UpsRegistrationStatus RegisterAccount(UpsAccountEntity accountEntity)
+        {
+            return RegisterAccount(accountEntity, null);
+        }
+
+        /// <summary>
+        /// Registers the account using the invoice information provided.
         /// </summary>
         /// <param name="accountEntity">The account entity.</param>
         /// <param name="invoiceAuthorizationData">The invoice authorization data.</param>
         /// <exception cref="UpsException">A unique UserID could not be generated.  Please try again.</exception>
-        public void RegisterAccount(UpsAccountEntity accountEntity, UpsOltInvoiceAuthorizationData invoiceAuthorizationData)
+        public UpsRegistrationStatus RegisterAccount(UpsAccountEntity accountEntity, UpsOltInvoiceAuthorizationData invoiceAuthorizationData)
         {
-            ValidateInvoiceData(invoiceAuthorizationData);
-
             bool isAccountCreated = false;
             UpsWebServiceException lastUniqueException = null;
 
@@ -78,19 +83,25 @@ namespace ShipWorks.Shipping.Carriers.UPS
                     response.Process();
 
                     isAccountCreated = true;
+                    return UpsRegistrationStatus.Success;
                 }
                 catch (UpsWebServiceException upsEx)
                 {
-                    if (upsEx.Code != "9570100")
-                    {
-                        // An exception occurred not related to the uniqueness of the User ID
-                        throw;
-                    }
-                    else
+                    if (upsEx.Code == "9570100")
                     {
                         // The user ID provided is already taken; make a note of it so it can
                         // be used if the account was not created after three attempts
                         lastUniqueException = upsEx;
+                    }
+                    else if (upsEx.Code == "0495")
+                    {
+                        // Ups requires invoice information
+                        return UpsRegistrationStatus.InvoiceAuthenticationRequired;
+                    }
+                    else
+                    {
+                        // An exception occurred not related to the uniqueness of the User ID
+                        throw;
                     }
                 }
             }
@@ -99,24 +110,8 @@ namespace ShipWorks.Shipping.Carriers.UPS
             {
                 throw new UpsException("A unique User ID could not be generated.  Please try again.", lastUniqueException);
             }
-        }
 
-        /// <summary>
-        /// Validates the invoice data - only validates that an invlice number is provided.
-        /// </summary>
-        /// <param name="invoiceAuthorizationData">The invoice authorization data.</param>
-        /// <exception cref="UpsException"></exception>
-        private static void ValidateInvoiceData(UpsOltInvoiceAuthorizationData invoiceAuthorizationData)
-        {
-            if (invoiceAuthorizationData == null)
-            {
-                throw new UpsException("Invoice authorization cannot be null.");
-            }
-
-            if (string.IsNullOrWhiteSpace(invoiceAuthorizationData.InvoiceNumber))
-            {
-                throw new UpsException("An invoice number is required.");
-            }
+            return UpsRegistrationStatus.Failed;
         }
     }
 }
