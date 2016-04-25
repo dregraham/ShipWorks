409b5848aa68a8930782929eed7c7f96b2c6277b
Kevin Croke <k.croke@shipworks.com>
2016-04-25 16:06:10 -0500

    TP#13107 Add IOdbcDataSourceRepository

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/IOdbcDataSourceRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ShipWorks.Stores.Platforms.Odbc
+{
+    /// <summary>
+    /// Repository for OdbcDataSources
+    /// </summary>
+    public interface IOdbcDataSourceRepository
+    {
+        /// <summary>
+        /// Gets the available data sources.
+        /// </summary>
+        IEnumerable<OdbcDataSource> GetDataSources();
+    }
+}
