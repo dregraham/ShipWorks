4806ad8d2f72734cbfee3dfadb6672e74bc5c6f5
Kevin Croke <k.croke@shipworks.com>
2016-05-02 12:08:35 -0500

    TP#13172 - Updating SearsCredentials for new authentication method

--- Code/ShipWorks.Stores.Tests/Platforms/Sears/SearsCredentialsTest.cs
+++ Code/ShipWorks.Stores.Tests/Platforms/Sears/SearsCredentialsTest.cs
@@ -41,7 +41,6 @@ namespace ShipWorks.Stores.Tests.Platforms.Sears
         {
             using (var mock = AutoMock.GetLoose())
             {
-                Mock<IDateTimeProvider> dateTime = mock.Mock<IDateTimeProvider>();
                 var testObject = mock.Create<SearsCredentials>();
 
                 string email = "abcfake@email.com";
@@ -62,9 +61,266 @@ namespace ShipWorks.Stores.Tests.Platforms.Sears
         }
 
         [Fact]
+        public void AddCredentials_DoesNotAddSellerIdVariable_WhenLegacyStore()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                var testObject = mock.Create<SearsCredentials>();
+
+                string email = "abcfake@email.com";
+                string password = "fakePassword123";
+
+                var store = new SearsStoreEntity
+                {
+                    Email = email,
+                    Password = SecureText.Encrypt(password, email)
+                };
+
+                var request = new HttpVariableRequestSubmitter();
+                testObject.AddCredentials(store, request);
+                HttpVariable sellerIdVariable = request.Variables.FirstOrDefault(v => v.Name == "sellerId");
+
+                Assert.Null(sellerIdVariable);
+            }
+        }
+
+        [Fact]
+        public void AddCredentials_AddsSellerIdToVariable_WhenNewStore()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IEncryptionProvider>()
+                    .Setup(e => e.Decrypt(It.IsAny<string>())).Returns("blah");
+
+                var testObject = mock.Create<SearsCredentials>();
+
+                string sellerId = "mySellerId";
+
+                var store = new SearsStoreEntity
+                {
+                    SellerID = sellerId
+                };
+
+                var request = new HttpVariableRequestSubmitter();
+                testObject.AddCredentials(store, request);
+                HttpVariable sellerIdVariable = request.Variables.Single(v => v.Name == "sellerId");
+
+                Assert.Equal(sellerId, sellerIdVariable.Value);
+            }
+        }
+
+        [Fact]
+        public void AddCredentials_DoesNotAddEmailRequestVariable_WhenNewStore()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IEncryptionProvider>()
+                    .Setup(e => e.Decrypt(It.IsAny<string>())).Returns("blah");
+
+                var testObject = mock.Create<SearsCredentials>();
+
+                string sellerId = "mySellerId";
+
+                var store = new SearsStoreEntity
+                {
+                    SellerID = sellerId
+                };
+
+                var request = new HttpVariableRequestSubmitter();
+                testObject.AddCredentials(store, request);
+                HttpVariable emailVariable = request.Variables.SingleOrDefault(v => v.Name == "email");
+
+                Assert.Null(emailVariable);
+            }
+        }
+
+        [Fact]
+        public void AddCredentials_AddsAuthorizationHeader_WhenNewStore()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IEncryptionProvider>()
+                    .Setup(e => e.Decrypt(It.IsAny<string>())).Returns("blah");
+
+                var testObject = mock.Create<SearsCredentials>();
+
+                string sellerId = "mySellerId";
+
+                var store = new SearsStoreEntity
+                {
+                    SellerID = sellerId
+                };
+
+                var request = new HttpVariableRequestSubmitter();
+                testObject.AddCredentials(store, request);
+
+                Assert.NotNull(request.Headers["authorization"]);
+            }
+        }
+
+        [Fact]
+        public void AddCredentials_AuthHeaderSignatureIsCorrect_WhenNewStore()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                string secretKey = "syZJ+ak2Qhta3HvYig+3vCjg1Al0b8cqdVqaZr9EbFU=";
+                string sellerId = "10173601";
+                string emailAddress = "wes@shipworks.com";
+                string expectedHash = "d847e945dda8574cdfeaedb69670b3357123af4078fa9bda64651571b4055d9a";
+                var now = new DateTime(2016, 5, 2, 16, 20, 00, DateTimeKind.Utc);
+
+                mock.Mock<IDateTimeProvider>()
+                    .Setup(d => d.UtcNow)
+                    .Returns(now);
+
+                mock.Mock<IEncryptionProvider>()
+                    .Setup(e => e.Decrypt(It.IsAny<string>())).Returns(secretKey);
+
+                var testObject = mock.Create<SearsCredentials>();
+
+                var store = new SearsStoreEntity
+                {
+                    SellerID = sellerId,
+                    SecretKey = secretKey,
+                    Email = emailAddress
+                };
+
+                var request = new HttpVariableRequestSubmitter();
+                testObject.AddCredentials(store, request);
+
+                string headerText = request.Headers["authorization"];
+                string actualHash = headerText.Split(',')[2].Split('=')[1];
+
+                Assert.Equal(expectedHash, actualHash);
+            }
+        }
+
+        [Fact]
+        public void AddCredentials_AuthHeaderStartsWithHashType()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IEncryptionProvider>()
+                    .Setup(e => e.Decrypt(It.IsAny<string>())).Returns("blah");
+
+                var testObject = mock.Create<SearsCredentials>();
+
+                string sellerId = "mySellerId";
+
+                var store = new SearsStoreEntity
+                {
+                    SellerID = sellerId
+                };
+
+                var request = new HttpVariableRequestSubmitter();
+                testObject.AddCredentials(store, request);
+
+                Assert.StartsWith("HMAC-SHA256 ", request.Headers["authorization"]);
+            }
+        }
+
+        [Fact]
+        public void AddCredentials_AuthHeaderEmailAddressIsStoreEmailAddress()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                string storeEmail = "blah@shipworks.com";
+
+                mock.Mock<IEncryptionProvider>()
+                    .Setup(e => e.Decrypt(It.IsAny<string>())).Returns("blah");
+
+                var testObject = mock.Create<SearsCredentials>();
+
+                string sellerId = "mySellerId";
+
+                var store = new SearsStoreEntity
+                {
+                    SellerID = sellerId,
+                    Email = storeEmail
+                };
+
+                var request = new HttpVariableRequestSubmitter();
+                testObject.AddCredentials(store, request);
+
+                string headerValue = request.Headers["authorization"];
+                string headerEmail = headerValue
+                    .Split(' ')[1] // everything after the space
+                    .Split(',')[0] // everything before the comma
+                    .Split('=')[1]; // the email address
+                
+                Assert.Equal(storeEmail, headerEmail);
+            }
+        }
+
+        [Fact]
+        public void AddCredentials_AuthHeaderContainsTimestampsFromProvider()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                var now = new DateTime(2016, 5, 2, 16, 20, 00, DateTimeKind.Utc);
+
+                mock.Mock<IDateTimeProvider>()
+                    .Setup(d => d.UtcNow)
+                    .Returns(now);
+
+                mock.Mock<IEncryptionProvider>()
+                    .Setup(e => e.Decrypt(It.IsAny<string>())).Returns("blah");
+
+                var testObject = mock.Create<SearsCredentials>();
+
+                string sellerId = "mySellerId";
+
+                var store = new SearsStoreEntity
+                {
+                    SellerID = sellerId
+                };
+
+                var request = new HttpVariableRequestSubmitter();
+                testObject.AddCredentials(store, request);
+
+                string headerValue = request.Headers["authorization"];
+                string timeStamp = headerValue.Split(',')[1].Split('=')[1];
+
+                Assert.Equal(now, DateTime.Parse(timeStamp).ToUniversalTime());
+            }
+        }
+
+
+        [Fact]
+        public void AddCredentials_DelegatesToIEncryptionProvider()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                var now = new DateTime(2016, 5, 2, 16, 20, 00, DateTimeKind.Utc);
+
+                mock.Mock<IDateTimeProvider>()
+                    .Setup(d => d.UtcNow)
+                    .Returns(now);
+
+                var encryptionProvider = mock.Mock<IEncryptionProvider>();
+                encryptionProvider.Setup(e => e.Decrypt(It.IsAny<string>())).Returns("blah");
+
+                var testObject = mock.Create<SearsCredentials>();
+
+                string sellerId = "mySellerId";
+                string secretKey = "mySecretKey";
+                var store = new SearsStoreEntity
+                {
+                    SellerID = sellerId,
+                    SecretKey = secretKey
+                };
+
+                var request = new HttpVariableRequestSubmitter();
+                testObject.AddCredentials(store, request);
+
+                encryptionProvider.Verify(p => p.Decrypt(It.Is<string>(s => s == secretKey)));
+            }
+        }
+
+        [Fact]
         public void SearsCredentials_WithNullDateTimeProvider_ThrowsArgumentNullException()
         {
-            Assert.Throws<ArgumentNullException>(() => new SearsCredentials(null));
+            Assert.Throws<ArgumentNullException>(() => new SearsCredentials(null, null));
         }
     }
 }
--- Code/ShipWorks/Stores/Platforms/Sears/SearsCredentials.cs
+++ Code/ShipWorks/Stores/Platforms/Sears/SearsCredentials.cs
@@ -1,7 +1,11 @@
 ﻿using Interapptive.Shared.Net;
 using Interapptive.Shared.Utility;
 using ShipWorks.Data.Model.EntityClasses;
+using System;
+using System.Globalization;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ShipWorks.Stores.Platforms.Sears
 {
@@ -11,15 +15,17 @@ namespace ShipWorks.Stores.Platforms.Sears
     public class SearsCredentials
     {
         private readonly IDateTimeProvider dateTimeProvider;
+        private readonly IEncryptionProvider encryptionProvider;
 
         /// <summary>
         /// Constructor
         /// </summary>
-        public SearsCredentials(IDateTimeProvider dateTimeProvider)
+        public SearsCredentials(IDateTimeProvider dateTimeProvider, IEncryptionProvider encryptionProvider)
         {
             MethodConditions.EnsureArgumentIsNotNull(dateTimeProvider);
 
             this.dateTimeProvider = dateTimeProvider;
+            this.encryptionProvider = encryptionProvider;
         }
 
         /// <summary>
@@ -28,7 +34,43 @@ namespace ShipWorks.Stores.Platforms.Sears
         public void AddCredentials(SearsStoreEntity store, HttpVariableRequestSubmitter request)
         {
             GetCredentialsHttpVariables(store).ToList().ForEach(v => request.Variables.Add(v));
+            AddCredentialsHeader(store, request);
         }
+
+        /// <summary>
+        /// Adds the credentials header to the request for authenticating
+        /// </summary>
+        private void AddCredentialsHeader(SearsStoreEntity store, HttpVariableRequestSubmitter request)
+        {
+            if (!string.IsNullOrEmpty(store.SellerID))
+            {
+                // They are using the current authentication method
+                string timeStamp = dateTimeProvider.UtcNow.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'",
+                    CultureInfo.InvariantCulture);
+                string toHash = $"{store.SellerID}:{store.Email}:{timeStamp}";
+                string signature = HashSignature(toHash, encryptionProvider.Decrypt(store.SecretKey));
+                string headerValue =
+                    $"HMAC-SHA256 emailaddress={store.Email},timestamp={timeStamp},signature={signature}";
+
+                request.Headers.Add("authorization", headerValue);
+            }
+        }
+
+        /// <summary>
+        /// Hashes toHash using HMACSHA256 and secretKey
+        /// </summary>
+        private string HashSignature(string toHash, string secretKey)
+        {
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] keyByte = encoding.GetBytes(secretKey);
+            byte[] messageBytes = encoding.GetBytes(toHash);
+            using (HMACSHA256 hmacSha256 = new HMACSHA256(keyByte))
+            {
+                byte[] hash = hmacSha256.ComputeHash(messageBytes);
+                return BitConverter.ToString(hash).Replace("-", "").ToLower();
+            }
+        }
+
 
         /// <summary>
         /// Return the collection of HTTP variables for authenticating
@@ -36,8 +78,18 @@ namespace ShipWorks.Stores.Platforms.Sears
         private static HttpVariableCollection GetCredentialsHttpVariables(SearsStoreEntity store)
         {
             HttpVariableCollection credentials = new HttpVariableCollection();
-            credentials.Add("email", store.Email);
-            credentials.Add("password", SecureText.Decrypt(store.Password, store.Email));
+
+            if (string.IsNullOrEmpty(store.SellerID))
+            {
+                // They are using the "old" authentication method
+                credentials.Add("email", store.Email);
+                credentials.Add("password", SecureText.Decrypt(store.Password, store.Email));
+            }
+            else
+            {
+                // They are using the current authentication method
+                credentials.Add("sellerId", store.SellerID);
+            }
 
             return credentials;
         }
