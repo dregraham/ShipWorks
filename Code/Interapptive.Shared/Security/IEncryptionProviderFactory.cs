63056c910c315054d7cf7220e3dc8ee0da8e6465
Kevin Croke <k.croke@shipworks.com>
2016-05-05 08:11:30 -0500

    TP#13050 - Adding comments to IEncryptionProviderFactory per code review

--- Code/Interapptive.Shared/Security/IEncryptionProviderFactory.cs
+++ Code/Interapptive.Shared/Security/IEncryptionProviderFactory.cs
@@ -1,11 +1,23 @@
 ﻿namespace Interapptive.Shared.Security
 {
+    /// <summary>
+    /// Interface for a factory that creates Encryption Providers
+    /// </summary>
     public interface IEncryptionProviderFactory
     {
+        /// <summary>
+        /// Creates the license encryption provider.
+        /// </summary>
         IEncryptionProvider CreateLicenseEncryptionProvider();
 
+        /// <summary>
+        /// Creates the sears encryption provider.
+        /// </summary>
         IEncryptionProvider CreateSearsEncryptionProvider();
 
+        /// <summary>
+        /// Creates the secure text encryption provider.
+        /// </summary>
         IEncryptionProvider CreateSecureTextEncryptionProvider(string salt);
     }
 }
