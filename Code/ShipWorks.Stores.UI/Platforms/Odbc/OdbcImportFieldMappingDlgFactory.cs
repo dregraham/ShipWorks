1e893373d1b8830581cbd572ebb0135e76f33198
Chris Hicks <c.hicks@shipworks.com>
2016-05-11 15:55:04 -0500

    TP#12930: Create OdbcImportFieldMappingDlgFactory

---
+++ Code/ShipWorks.Stores.UI/Platforms/Odbc/OdbcImportFieldMappingDlgFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using ShipWorks.ApplicationCore.Licensing;
+using ShipWorks.ApplicationCore.Licensing.LicenseEnforcement;
+using ShipWorks.Editions;
+
+namespace ShipWorks.Stores.UI.Platforms.Odbc
+{
+    public class OdbcImportFieldMappingDlgFactory
+    {
+        private readonly Func<string, IDialog> dialogFactory;
+        private readonly IOdbcImportFieldMappingDlgViewModel viewModel;
+
+        public OdbcImportFieldMappingDlgFactory(Func<string, IDialog> dialogFactory, IOdbcImportFieldMappingDlgViewModel viewModel)
+        {
+            this.dialogFactory = dialogFactory;
+            this.viewModel = viewModel;
+        }
+
+        /// <summary>
+        /// Gets the channel limit dialog.
+        /// </summary>
+        public IDialog GetOdbcImportFieldMappingDlg(IWin32Window owner)
+        {
+            // Get the dialog
+            IDialog dialog = dialogFactory("OdbcImportFieldMappingDlg");
+            dialog.LoadOwner(owner);
+            dialog.DataContext = viewModel;
+
+            return dialog;
+        }
+    }
+}
