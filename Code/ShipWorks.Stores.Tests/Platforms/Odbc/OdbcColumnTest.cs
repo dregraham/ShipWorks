f0161f0f08adf74a2ee83d403330047920067916
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-05-11 13:09:22 -0500

    TP#12930 Write unit test for OdbcColumn

---
+++ Code/ShipWorks.Stores.Tests/Platforms/Odbc/OdbcColumnTest.cs
@@ -0,0 +1,15 @@
+﻿using ShipWorks.Stores.Platforms.Odbc;
+using Xunit;
+
+namespace ShipWorks.Stores.Tests.Platforms.Odbc
+{
+    public class OdbcColumnTest
+    {
+        [Fact]
+        public void Ctor_SetsName()
+        {
+            OdbcColumn testObject = new OdbcColumn("SomeName");
+            Assert.Equal("SomeName", testObject.Name);
+        }
+    }
+}
