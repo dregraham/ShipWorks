ffe65f8b7673e415fbb053f2dffee3ae71e14450
Chris Hicks <c.hicks@shipworks.com>
2016-05-12 14:34:40 -0500

    TP#13050: Rename email on the sears store entity to SearsEmail.

--- Code/ShipWorks.Data.Model/EntityClasses/SearsStoreEntity.cs
+++ Code/ShipWorks.Data.Model/EntityClasses/SearsStoreEntity.cs
@@ -560,7 +560,7 @@ namespace ShipWorks.Data.Model.EntityClasses
 
 		/// <summary> The Email property of the Entity SearsStore<br/><br/>
 		/// </summary>
-		/// <remarks>Mapped on  table field: "SearsStore"."Email"<br/>
+		/// <remarks>Mapped on  table field: "SearsStore"."SearsEmail"<br/>
 		/// Table field type characteristics (type, precision, scale, length): NVarChar, 0, 0, 75<br/>
 		/// Table field behavior characteristics (is nullable, is PK, is identity): false, false, false</remarks>
 		public override System.String Email
