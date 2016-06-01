2c8b0f117824a3529867fe61efa669a5b415164b
Kevin Croke <k.croke@shipworks.com>
2016-06-01 12:47:41 -0500

    TP#13599 Add OdbcRecord

---
+++ Code/ShipWorks.Stores.Tests/Platforms/Odbc/OdbcRecordTests.cs
@@ -0,0 +1,37 @@
+﻿using Autofac.Extras.Moq;
+using ShipWorks.Stores.Platforms.Odbc;
+using Xunit;
+
+namespace ShipWorks.Stores.Tests.Platforms.Odbc
+{
+    public class OdbcRecordTests
+    {
+        [Fact]
+        public void GetValue_ReturnsNull_WhenValueNotFound()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                var testObject = mock.Create<OdbcRecord>();
+                var result = testObject.GetValue("test");
+
+                Assert.Null(result);
+            }
+        }
+
+        [Fact]
+        public void GetValue_ReturnsValue_WhenValueFound()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                var testObject = mock.Create<OdbcRecord>();
+
+                var objectToAdd = new object();
+                testObject.AddField("test", objectToAdd);
+
+                var result = testObject.GetValue("test");
+
+                Assert.Equal(objectToAdd, result);
+            }
+        }
+    }
+}
---
+++ Code/ShipWorks.Stores/Platforms/Odbc/OdbcRecord.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace ShipWorks.Stores.Platforms.Odbc
+{
+    /// <summary>
+    /// Contains data for an ODBCRecord
+    /// </summary>
+    public class OdbcRecord
+    {
+        private readonly Dictionary<string, object> fields = new Dictionary<string, object>();
+
+        /// <summary>
+        /// Gets the value of the field - null if not set
+        /// </summary>
+        public object GetValue(string fieldName)
+        {
+            object value;
+            fields.TryGetValue(fieldName, out value);
+
+            return value;
+        }
+
+        /// <summary>
+        /// Adds or updates the value of the field.
+        /// </summary>
+        public void AddField(string columnName, object value)
+        {
+            fields[columnName] = value;
+        }
+    }
+}
