934801607122848920c2d204a552153fe23dba97
Chris Hicks <c.hicks@shipworks.com>
2016-06-03 14:12:39 -0500

    TP#13668: Create IOdbcOrderLoader

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/IOdbcOrderLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.Odbc.Mapping;
+
+namespace ShipWorks.Stores.Platforms.Odbc
+{
+    public interface IOdbcOrderLoader
+    {
+        void Load(OdbcFieldMap map, OrderEntity order, IEnumerable<OdbcRecord> records);
+    }
+}
