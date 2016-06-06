bc55baf0495b8c4dfdd1759156a2f40dbb438515
Chris Hicks <c.hicks@shipworks.com>
2016-06-06 10:55:37 -0500

    TP#13671: Fix failed unit tests

--- Code/ShipWorks.Stores.Tests/Platforms/Odbc/OdbcOrderPaymentLoaderTest.cs
+++ Code/ShipWorks.Stores.Tests/Platforms/Odbc/OdbcOrderPaymentLoaderTest.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Autofac.Extras.Moq;
+using Moq;
+using SD.LLBLGen.Pro.ORMSupportClasses;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Stores.Platforms.Odbc;
 using ShipWorks.Stores.Platforms.Odbc.Mapping;
@@ -14,18 +17,24 @@ namespace ShipWorks.Stores.Tests.Platforms.Odbc
         {
             using (var mock = AutoMock.GetLoose())
             {
-                OdbcFieldMap map = mock.Create<OdbcFieldMap>();
+                var map = mock.Mock<IOdbcFieldMap>();
+
                 var shipworksfield = mock.Mock<IShipWorksOdbcMappableField>();
                 shipworksfield.Setup(e => e.GetQualifiedName()).Returns("OrderPaymentDetail.Value");
                 shipworksfield.Setup(e => e.DisplayName).Returns("Payment Method");
                 shipworksfield.Setup(e => e.Value).Returns("Credit Card");
+
                 var mapEntry = mock.Mock<IOdbcFieldMapEntry>();
                 mapEntry.Setup(e => e.ShipWorksField).Returns(shipworksfield.Object);
-                map.AddEntry(mapEntry.Object);
-                OrderEntity order = new OrderEntity();
-                OdbcOrderPaymentLoader testObject = new OdbcOrderPaymentLoader();
 
-                testObject.Load(map, order);
+                map.Object.AddEntry(mapEntry.Object);
+                map.Setup(m => m.FindEntriesBy(It.IsAny<EntityField2>())).
+                    Returns(new List<IOdbcFieldMapEntry> { mapEntry.Object });
+
+                OdbcOrderPaymentLoader testObject = new OdbcOrderPaymentLoader();
+                OrderEntity order = new OrderEntity();
+
+                testObject.Load(map.Object, order);
 
                 Assert.Equal("Payment Method", order.OrderPaymentDetails.First().Label);
                 Assert.Equal("Credit Card", order.OrderPaymentDetails.First().Value);
@@ -37,12 +46,13 @@ namespace ShipWorks.Stores.Tests.Platforms.Odbc
         {
             using (var mock = AutoMock.GetLoose())
             {
-                OdbcFieldMap map = mock.Create<OdbcFieldMap>();
+                var map = mock.Mock<IOdbcFieldMap>();
 
                 var shipworksfield = mock.Mock<IShipWorksOdbcMappableField>();
                 shipworksfield.Setup(e => e.GetQualifiedName()).Returns("OrderPaymentDetail.Value");
                 shipworksfield.Setup(e => e.DisplayName).Returns("Payment Method");
                 shipworksfield.Setup(e => e.Value).Returns("Credit Card");
+
                 var mapEntry = mock.Mock<IOdbcFieldMapEntry>();
                 mapEntry.Setup(e => e.ShipWorksField).Returns(shipworksfield.Object);
 
@@ -50,15 +60,19 @@ namespace ShipWorks.Stores.Tests.Platforms.Odbc
                 shipworksfield2.Setup(e => e.GetQualifiedName()).Returns("OrderPaymentDetail.Value");
                 shipworksfield2.Setup(e => e.DisplayName).Returns("Credit Card Type");
                 shipworksfield2.Setup(e => e.Value).Returns("Visa");
+
                 var mapEntry2 = mock.Mock<IOdbcFieldMapEntry>();
                 mapEntry2.Setup(e => e.ShipWorksField).Returns(shipworksfield2.Object);
 
-                map.AddEntry(mapEntry.Object);
-                map.AddEntry(mapEntry2.Object);
-                OrderEntity order = new OrderEntity();
-                OdbcOrderPaymentLoader testObject = new OdbcOrderPaymentLoader();
+                map.Object.AddEntry(mapEntry.Object);
+                map.Object.AddEntry(mapEntry2.Object);
+                map.Setup(m => m.FindEntriesBy(It.IsAny<EntityField2>())).
+                    Returns(new List<IOdbcFieldMapEntry> { mapEntry.Object, mapEntry2.Object });
 
-                testObject.Load(map, order);
+                OdbcOrderPaymentLoader testObject = new OdbcOrderPaymentLoader();
+                OrderEntity order = new OrderEntity();
+
+                testObject.Load(map.Object, order);
 
                 Assert.NotNull(order.OrderPaymentDetails.Where(x => x.Label == "Payment Method"));
                 Assert.NotNull(order.OrderPaymentDetails.Where(x => x.Value == "Credit Card"));
@@ -72,16 +86,20 @@ namespace ShipWorks.Stores.Tests.Platforms.Odbc
         {
             using (var mock = AutoMock.GetLoose())
             {
-                OdbcFieldMap map = mock.Create<OdbcFieldMap>();
+                var map = mock.Mock<IOdbcFieldMap>();
+
                 var shipworksfield = mock.Mock<IShipWorksOdbcMappableField>();
                 shipworksfield.Setup(e => e.GetQualifiedName()).Returns("Blah");
+
                 var mapEntry = mock.Mock<IOdbcFieldMapEntry>();
                 mapEntry.Setup(e => e.ShipWorksField).Returns(shipworksfield.Object);
-                map.AddEntry(mapEntry.Object);
-                OrderEntity order = new OrderEntity();
-                OdbcOrderPaymentLoader testObject = new OdbcOrderPaymentLoader();
 
-                testObject.Load(map, order);
+                map.Object.AddEntry(mapEntry.Object);
+
+                OdbcOrderPaymentLoader testObject = new OdbcOrderPaymentLoader();
+                OrderEntity order = new OrderEntity();
+
+                testObject.Load(map.Object, order);
 
                 Assert.Equal(0, order.OrderPaymentDetails.Count);
             }
