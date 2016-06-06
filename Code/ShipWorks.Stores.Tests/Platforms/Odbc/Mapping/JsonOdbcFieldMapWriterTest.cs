584ffecea1e1ef05ec9c7a28e6eab116bc0b835e
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-06-06 08:59:58 -0500

    TP#6153 Fix broken unit test for JsonOdbcFieldMapWriterTest

--- Code/ShipWorks.Stores.Tests/Platforms/Odbc/Mapping/JsonOdbcFieldMapWriterTest.cs
+++ Code/ShipWorks.Stores.Tests/Platforms/Odbc/Mapping/JsonOdbcFieldMapWriterTest.cs
@@ -36,8 +36,7 @@ namespace ShipWorks.Stores.Tests.Platforms.Odbc.Mapping
         [Fact]
         public void Write_WritesSerializedMapToStream()
         {
-            string expectedResult =
-                "{\"Entries\":[{\"ShipWorksField\":{\"ContainingObjectName\":\"OrderEntity\",\"Name\":\"OrderNumber\",\"DisplayName\":\"Order Number\"},\"ExternalField\":{\"Table\":{\"Name\":\"some table\"},\"Column\":{\"Name\":\"OrderNumberColumn\"},\"DisplayName\":\"some table OrderNumberColumn\"}}],\"ExternalTableName\":\"some external tablename\"}";
+            string expectedResult = "{\"Entries\":[{\"ShipWorksField\":{\"TypeName\":\"System.Int64\",\"ContainingObjectName\":\"OrderEntity\",\"Name\":\"OrderNumber\",\"DisplayName\":\"Order Number\"},\"ExternalField\":{\"Table\":{\"Name\":\"some table\"},\"Column\":{\"Name\":\"OrderNumberColumn\"},\"DisplayName\":\"some table OrderNumberColumn\"}}],\"ExternalTableName\":\"some external tablename\"}";
 
             Mock<IOdbcFieldMapIOFactory> ioFactory = mock.Mock<IOdbcFieldMapIOFactory>();
             OdbcFieldMap map = new OdbcFieldMap(ioFactory.Object);
@@ -56,7 +55,7 @@ namespace ShipWorks.Stores.Tests.Platforms.Odbc.Mapping
             map.ExternalTableName = "some external tablename";
 
             map.AddEntry(entry);
-            
+
             using (MemoryStream stream = new MemoryStream())
             {
                 testObject.Write(stream);
