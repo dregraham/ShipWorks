1c89d3e64e6f8b2654cdf10702dc7084b42afae6
Chris Hicks <c.hicks@shipworks.com>
2016-06-06 10:26:14 -0500

    TP#13671: Implement OdbcOrderPaymentLoader

---
+++ Code/ShipWorks.Stores.Tests/Platforms/Odbc/OdbcOrderPaymentLoaderTest.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using Autofac.Extras.Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.Odbc;
+using ShipWorks.Stores.Platforms.Odbc.Mapping;
+using Xunit;
+
+namespace ShipWorks.Stores.Tests.Platforms.Odbc
+{
+    public class OdbcOrderPaymentLoaderTest
+    {
+        [Fact]
+        public void Load_AddsPaymentDetailsToOrder_WhenMapContainsPaymentFields()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                OdbcFieldMap map = mock.Create<OdbcFieldMap>();
+                var shipworksfield = mock.Mock<IShipWorksOdbcMappableField>();
+                shipworksfield.Setup(e => e.GetQualifiedName()).Returns("OrderPaymentDetail.Value");
+                shipworksfield.Setup(e => e.DisplayName).Returns("Payment Method");
+                shipworksfield.Setup(e => e.Value).Returns("Credit Card");
+                var mapEntry = mock.Mock<IOdbcFieldMapEntry>();
+                mapEntry.Setup(e => e.ShipWorksField).Returns(shipworksfield.Object);
+                map.AddEntry(mapEntry.Object);
+                OrderEntity order = new OrderEntity();
+                OdbcOrderPaymentLoader testObject = new OdbcOrderPaymentLoader();
+
+                testObject.Load(map, order);
+
+                Assert.Equal("Payment Method", order.OrderPaymentDetails.First().Label);
+                Assert.Equal("Credit Card", order.OrderPaymentDetails.First().Value);
+            }
+        }
+
+        [Fact]
+        public void Load_AddsMultiplePaymentDetailsToOrder_WhenMapContainsMultiplePaymentFields()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                OdbcFieldMap map = mock.Create<OdbcFieldMap>();
+
+                var shipworksfield = mock.Mock<IShipWorksOdbcMappableField>();
+                shipworksfield.Setup(e => e.GetQualifiedName()).Returns("OrderPaymentDetail.Value");
+                shipworksfield.Setup(e => e.DisplayName).Returns("Payment Method");
+                shipworksfield.Setup(e => e.Value).Returns("Credit Card");
+                var mapEntry = mock.Mock<IOdbcFieldMapEntry>();
+                mapEntry.Setup(e => e.ShipWorksField).Returns(shipworksfield.Object);
+
+                var shipworksfield2 = mock.Mock<IShipWorksOdbcMappableField>();
+                shipworksfield2.Setup(e => e.GetQualifiedName()).Returns("OrderPaymentDetail.Value");
+                shipworksfield2.Setup(e => e.DisplayName).Returns("Credit Card Type");
+                shipworksfield2.Setup(e => e.Value).Returns("Visa");
+                var mapEntry2 = mock.Mock<IOdbcFieldMapEntry>();
+                mapEntry2.Setup(e => e.ShipWorksField).Returns(shipworksfield2.Object);
+
+                map.AddEntry(mapEntry.Object);
+                map.AddEntry(mapEntry2.Object);
+                OrderEntity order = new OrderEntity();
+                OdbcOrderPaymentLoader testObject = new OdbcOrderPaymentLoader();
+
+                testObject.Load(map, order);
+
+                Assert.NotNull(order.OrderPaymentDetails.Where(x => x.Label == "Payment Method"));
+                Assert.NotNull(order.OrderPaymentDetails.Where(x => x.Value == "Credit Card"));
+                Assert.NotNull(order.OrderPaymentDetails.Where(x => x.Label == "Credit Card Type"));
+                Assert.NotNull(order.OrderPaymentDetails.Where(x => x.Value == "Visa"));
+            }
+        }
+
+        [Fact]
+        public void Load_DoesNotAddPaymentDetailsToOrder_WhenMapDoesNotContainPaymentFields()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                OdbcFieldMap map = mock.Create<OdbcFieldMap>();
+                var shipworksfield = mock.Mock<IShipWorksOdbcMappableField>();
+                shipworksfield.Setup(e => e.GetQualifiedName()).Returns("Blah");
+                var mapEntry = mock.Mock<IOdbcFieldMapEntry>();
+                mapEntry.Setup(e => e.ShipWorksField).Returns(shipworksfield.Object);
+                map.AddEntry(mapEntry.Object);
+                OrderEntity order = new OrderEntity();
+                OdbcOrderPaymentLoader testObject = new OdbcOrderPaymentLoader();
+
+                testObject.Load(map, order);
+
+                Assert.Equal(0, order.OrderPaymentDetails.Count);
+            }
+        }
+    }
+}
---
+++ Code/ShipWorks.Stores/Platforms/Odbc/OdbcOrderPaymentLoader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Stores.Platforms.Odbc.Mapping;
+
+namespace ShipWorks.Stores.Platforms.Odbc
+{
+    /// <summary>
+    /// Loads payment details into an order
+    /// </summary>
+    public class OdbcOrderPaymentLoader : IOdbcOrderDetailLoader
+    {
+        /// <summary>
+        /// Load the order payment details from the given map into the given entity
+        /// </summary>
+        public void Load(IOdbcFieldMap map, OrderEntity order)
+        {
+            if (order != null)
+            {
+                IEnumerable<IOdbcFieldMapEntry> paymentEntries = map.FindEntriesBy(OrderPaymentDetailFields.Value);
+
+                foreach (IOdbcFieldMapEntry entry in paymentEntries)
+                {
+                    AddPaymentDetailToOrder(order, entry);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add the individual payment detail to the order entity
+        /// </summary>
+        private void AddPaymentDetailToOrder(OrderEntity order, IOdbcFieldMapEntry chargeEntry)
+        {
+            OrderPaymentDetailEntity paymentDetail = new OrderPaymentDetailEntity();
+            paymentDetail.Order = order;
+
+            paymentDetail.Label = chargeEntry.ShipWorksField.DisplayName;
+            paymentDetail.Value = chargeEntry.ShipWorksField.Value.ToString();
+        }
+    }
+}
