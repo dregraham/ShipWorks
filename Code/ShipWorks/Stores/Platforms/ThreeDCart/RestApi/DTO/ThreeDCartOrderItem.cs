2b4d8a55f9c95e8abb166744383b76186e32e930
Kevin Croke <k.croke@shipworks.com>
2016-06-14 08:29:06 -0500

    TP#13794 SW-CR-104 Removed JsonProperty attributes from DTO's that are now deobfuscated.

--- Code/ShipWorks/Stores/Platforms/ThreeDCart/RestApi/DTO/ThreeDCartOrderItem.cs
+++ Code/ShipWorks/Stores/Platforms/ThreeDCart/RestApi/DTO/ThreeDCartOrderItem.cs
@@ -1,67 +1,47 @@
 ﻿using System;
 using System.Reflection;
-using Newtonsoft.Json;
 
 namespace ShipWorks.Stores.Platforms.ThreeDCart.RestApi.DTO
 {
     [Obfuscation(Exclude = true, ApplyToMembers = true)]
     public class ThreeDCartOrderItem
     {
-        [JsonProperty("CatalogID")]
         public int CatalogID { get; set; }
 
-        [JsonProperty("ItemIndexID")]
         public int ItemIndexID { get; set; }
 
-        [JsonProperty("ItemID")]
         public string ItemID { get; set; }
 
-        [JsonProperty("ItemShipmentID")]
         public int ItemShipmentID { get; set; }
 
-        [JsonProperty("ItemQuantity")]
         public double ItemQuantity { get; set; }
 
-        [JsonProperty("ItemWarehouseID")]
         public int ItemWarehouseID { get; set; }
 
-        [JsonProperty("ItemDescription")]
         public string ItemDescription { get; set; }
 
-        [JsonProperty("ItemUnitPrice")]
         public decimal ItemUnitPrice { get; set; }
 
-        [JsonProperty("ItemWeight")]
         public double ItemWeight { get; set; }
 
-        [JsonProperty("ItemOptionPrice")]
         public decimal ItemOptionPrice { get; set; }
 
-        [JsonProperty("ItemAdditionalField1")]
         public string ItemAdditionalField1 { get; set; }
 
-        [JsonProperty("ItemAdditionalField2")]
         public string ItemAdditionalField2 { get; set; }
 
-        [JsonProperty("ItemAdditionalField3")]
         public string ItemAdditionalField3 { get; set; }
 
-        [JsonProperty("ItemPageAdded")]
         public string ItemPageAdded { get; set; }
 
-        [JsonProperty("ItemDateAdded")]
         public DateTime ItemDateAdded { get; set; }
 
-        [JsonProperty("ItemUnitCost")]
         public decimal ItemUnitCost { get; set; }
 
-        [JsonProperty("ItemUnitStock")]
         public int ItemUnitStock { get; set; }
 
-        [JsonProperty("ItemOptions")]
         public string ItemOptions { get; set; }
 
-        [JsonProperty("ItemCatalogIDOptions")]
         public string ItemCatalogIDOptions { get; set; }
     }
 }
--- Code/ShipWorks/Stores/Platforms/ThreeDCart/RestApi/DTO/ThreeDCartQuestion.cs
+++ Code/ShipWorks/Stores/Platforms/ThreeDCart/RestApi/DTO/ThreeDCartQuestion.cs
@@ -1,33 +1,24 @@
 ﻿using System.Reflection;
-using Newtonsoft.Json;
 
 namespace ShipWorks.Stores.Platforms.ThreeDCart.RestApi.DTO
 {
     [Obfuscation(Exclude = true, ApplyToMembers = true)] 
     public class ThreeDCartQuestion
     {
-        [JsonProperty("OrderID")]
         public int OrderID { get; set; }
 
-        [JsonProperty("QuestionID")]
         public int QuestionID { get; set; }
 
-        [JsonProperty("QuestionTitle")]
         public string QuestionTitle { get; set; }
 
-        [JsonProperty("QuestionAnswer")]
         public string QuestionAnswer { get; set; }
 
-        [JsonProperty("QuestionType")]
         public string QuestionType { get; set; }
 
-        [JsonProperty("QuestionCheckoutStep")]
         public int QuestionCheckoutStep { get; set; }
 
-        [JsonProperty("QuestionSorting")]
         public int QuestionSorting { get; set; }
 
-        [JsonProperty("QuestionDiscountGroup")]
         public int QuestionDiscountGroup { get; set; }
     }
 }
