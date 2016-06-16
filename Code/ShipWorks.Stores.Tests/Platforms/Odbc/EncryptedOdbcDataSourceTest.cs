8ec6daffe1a068f0e159a18a0d833a8d3baf8f1a
Kevin Croke <k.croke@shipworks.com>
2016-06-16 12:16:45 -0500

    TP#6153 - Added unit tests to test EncryptedDataSource.

---
+++ Code/ShipWorks.Stores.Tests/Platforms/Odbc/EncryptedOdbcDataSourceTest.cs
@@ -0,0 +1,101 @@
+﻿using Autofac.Extras.Moq;
+using Interapptive.Shared.Security;
+using Moq;
+using ShipWorks.Stores.Platforms.Odbc;
+using Xunit;
+
+namespace ShipWorks.Stores.Tests.Platforms.Odbc
+{
+    public class EncryptedOdbcDataSourceTest
+    {
+        [Fact]
+        public void Restore_DelegatesToDecrypt()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                string json = "{\"Name\":\"Custom...\",\"Username\":null,\"Password\":null,\"IsCustom\":true,\"ConnectionString\":\"CustomConnectionString\"}";
+                string encryptedJson = "encrypted";
+
+                var encryptionProvider = mock.Mock<IEncryptionProvider>();
+                encryptionProvider.Setup(p => p.Decrypt(encryptedJson)).Returns(json);
+
+                var encryptionProviderFactory = mock.Mock<IEncryptionProviderFactory>();
+                encryptionProviderFactory.Setup(f => f.CreateOdbcEncryptionProvider())
+                    .Returns(encryptionProvider.Object);
+
+                EncryptedOdbcDataSource testObject = mock.Create<EncryptedOdbcDataSource>();
+
+                testObject.Restore(encryptedJson);
+
+                encryptionProvider.Verify(p=>p.Decrypt(encryptedJson));
+            }
+        }
+
+        [Fact]
+        public void Restore_NameIsSet()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                string sourceName = "my data source name";
+                string json = $"{{\"Name\":\"{sourceName}\",\"Username\":null,\"Password\":null,\"IsCustom\":true,\"ConnectionString\":\"CustomConnectionString\"}}";
+                string encryptedJson = "encrypted";
+
+                var encryptionProvider = mock.Mock<IEncryptionProvider>();
+                encryptionProvider.Setup(p => p.Decrypt(encryptedJson)).Returns(json);
+
+                var encryptionProviderFactory = mock.Mock<IEncryptionProviderFactory>();
+                encryptionProviderFactory.Setup(f => f.CreateOdbcEncryptionProvider())
+                    .Returns(encryptionProvider.Object);
+
+                EncryptedOdbcDataSource testObject = mock.Create<EncryptedOdbcDataSource>();
+
+                testObject.Restore(encryptedJson);
+
+                Assert.Equal(sourceName, testObject.Name);
+            }
+        }
+
+        [Fact]
+        public void Restore_ShipWorksOdbcExceptionThrown_WhenDecryptThrowsEncryptionException()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                string encryptedJson = "encrypted";
+
+                var encryptionProvider = mock.Mock<IEncryptionProvider>();
+                encryptionProvider.Setup(p => p.Decrypt(encryptedJson)).Throws<EncryptionException>();
+
+                var encryptionProviderFactory = mock.Mock<IEncryptionProviderFactory>();
+                encryptionProviderFactory.Setup(f => f.CreateOdbcEncryptionProvider())
+                    .Returns(encryptionProvider.Object);
+
+                EncryptedOdbcDataSource testObject = mock.Create<EncryptedOdbcDataSource>();
+
+                Assert.Throws<ShipWorksOdbcException>(() => testObject.Restore(encryptedJson));
+            }
+        }
+
+        [Fact]
+        public void Serialize_ReturnsEncryptedResults()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                string encryptedJson = "encrypted";
+
+                var encryptionProvider = mock.Mock<IEncryptionProvider>();
+                encryptionProvider.Setup(p => p.Encrypt(It.IsAny<string>())).Returns(encryptedJson);
+
+                var encryptionProviderFactory = mock.Mock<IEncryptionProviderFactory>();
+                encryptionProviderFactory.Setup(f => f.CreateOdbcEncryptionProvider())
+                    .Returns(encryptionProvider.Object);
+
+                var testObject = mock.Create<EncryptedOdbcDataSource>();
+
+                string serializationResult = testObject.Serialize();
+
+                Assert.Equal(encryptedJson, serializationResult);
+            }
+        }
+
+    }
+}
--- Code/ShipWorks.Stores/Platforms/Odbc/EncryptedOdbcDataSource.cs
+++ Code/ShipWorks.Stores/Platforms/Odbc/EncryptedOdbcDataSource.cs
@@ -2,10 +2,16 @@
 
 namespace ShipWorks.Stores.Platforms.Odbc
 {
+    /// <summary>
+    /// OdbcDatasource that inputs and outputs encrypted json.
+    /// </summary>
     public class EncryptedOdbcDataSource : OdbcDataSource
     {
         private readonly IEncryptionProvider odbcEncryptionProvider;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EncryptedOdbcDataSource"/> class.
+        /// </summary>
         public EncryptedOdbcDataSource(IShipWorksDbProviderFactory odbcProvider, IEncryptionProviderFactory encryptionProviderFactory) : base(odbcProvider)
         {
             odbcEncryptionProvider = encryptionProviderFactory.CreateOdbcEncryptionProvider();
@@ -16,7 +22,14 @@ namespace ShipWorks.Stores.Platforms.Odbc
         /// </summary>
         public override void Restore(string encryptedJson)
         {
-            base.Restore(odbcEncryptionProvider.Decrypt(encryptedJson));
+            try
+            {
+                base.Restore(odbcEncryptionProvider.Decrypt(encryptedJson));
+            }
+            catch (EncryptionException ex)
+            {
+                throw new ShipWorksOdbcException("Failed to restore data source", ex);
+            }
         }
 
         /// <summary>
@@ -24,7 +37,14 @@ namespace ShipWorks.Stores.Platforms.Odbc
         /// </summary>
         public override string Serialize()
         {
-            return odbcEncryptionProvider.Encrypt(base.Serialize());
+            try
+            {
+                return odbcEncryptionProvider.Encrypt(base.Serialize());
+            }
+            catch (EncryptionException ex)
+            {
+                throw new ShipWorksOdbcException("Failed to restore data source", ex);
+            }
         }
     }
 }
