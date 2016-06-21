b12f1500c191e2485f919e394423ace4e66a9774
Tim Hughes <t.hughes@shipworks.com>
2016-06-21 09:45:06 -0500

    TP#13900: Added File CipherContext enum

--- Code/ShipWorks/ApplicationCore/Security/CipherContext.cs
+++ Code/ShipWorks/ApplicationCore/Security/CipherContext.cs
@@ -6,13 +6,16 @@ namespace ShipWorks.ApplicationCore.Security
     /// <summary>
     /// Context of ciphers
     /// </summary>
-    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false, ApplyToMembers = true)]
     public enum CipherContext
     {
         [Description("Sears Context")]
         Sears,
 
         [Description("License Context")]
-        License 
+        License,
+
+        [Description("File Context")]
+        File
     }
 }
