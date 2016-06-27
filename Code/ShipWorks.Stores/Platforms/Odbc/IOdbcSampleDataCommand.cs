d346c706a55270d0f5941e49433d7b4061ae6b7f
Kevin Croke <k.croke@shipworks.com>
2016-06-27 09:52:39 -0500

    TP#13940 Add IOdbcSampleDataCommand

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/IOdbcSampleDataCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace ShipWorks.Stores.Platforms.Odbc
+{
+    /// <summary>
+    /// Interface to get a sample of the results of this query.
+    /// </summary>
+    public interface IOdbcSampleDataCommand
+    {
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        DataTable Execute(IOdbcDataSource dataSource, string query);
+    }
+}
---
+++ Code/ShipWorks.Stores/Platforms/Odbc/OdbcSampleDataCommand.cs
@@ -0,0 +1,25 @@
+﻿#region
+
+using System;
+using System.Data;
+
+#endregion
+
+namespace ShipWorks.Stores.Platforms.Odbc
+{
+    public class OdbcSampleDataCommand : IOdbcSampleDataCommand
+    {
+        private readonly IShipWorksDbProviderFactory dbProviderFactory;
+
+        private OdbcSampleDataCommand(IShipWorksDbProviderFactory dbProviderFactory)
+        {
+            this.dbProviderFactory = dbProviderFactory;
+        }
+
+
+        public DataTable Execute(IOdbcDataSource dataSource, string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
