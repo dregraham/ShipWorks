3f7456c2cf12490be9cfb733aa083ce41b081269
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-06-27 14:39:00 -0500

    TP#13917 Create OdbcSampleDataCommand

--- Code/ShipWorks.Stores/Platforms/Odbc/OdbcSampleDataCommand.cs
+++ Code/ShipWorks.Stores/Platforms/Odbc/OdbcSampleDataCommand.cs
@@ -1,25 +1,74 @@
-﻿#region
-
-using System;
-using System.Data;
-
-#endregion
+﻿using System.Data;
+using System.Data.Common;
+using System.Linq;
 
 namespace ShipWorks.Stores.Platforms.Odbc
 {
+    /// <summary>
+    /// Command to retrieve sample data from the results of a query.
+    /// </summary>
     public class OdbcSampleDataCommand : IOdbcSampleDataCommand
     {
         private readonly IShipWorksDbProviderFactory dbProviderFactory;
 
-        private OdbcSampleDataCommand(IShipWorksDbProviderFactory dbProviderFactory)
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        private OdbcSampleDataCommand(IShipWorksDbProviderFactory dbProviderFactory, OdbcColumnSource)
         {
             this.dbProviderFactory = dbProviderFactory;
         }
 
-
+        /// <summary>
+        /// Execute the query and return sample data
+        /// </summary>
         public DataTable Execute(IOdbcDataSource dataSource, string query)
         {
-            throw new NotImplementedException();
+            using (DbConnection connection = dataSource.CreateConnection())
+            {
+                connection.Open();
+
+                using (IShipWorksOdbcCommand cmd = dbProviderFactory.CreateOdbcCommand(query, connection))
+                {
+                    using (DbDataReader reader = cmd.ExecuteReader())
+                    {
+                        DataTable resultTable = GetEmptyResultTable(reader);
+
+                        int resultCount = 0;
+                        while (reader.Read() && resultCount < 4)
+                        {
+                            DataRow row = resultTable.NewRow();
+
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                row[reader.GetName(i)] = reader.GetValue(i);
+                            }
+
+                            resultTable.Rows.Add(row);
+
+                            resultCount++;
+                        }
+
+                        return resultTable;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates an empty result table from the reader
+        /// </summary>
+        private static DataTable GetEmptyResultTable(DbDataReader reader)
+        {
+            DataTable schemaTable = reader.GetSchemaTable();
+            DataTable result = new DataTable();
+
+            foreach (DataRow row in schemaTable.Rows.OfType<DataRow>())
+            {
+                result.Columns.Add(row["BaseColumnName"].ToString());
+            }
+
+            return result;
         }
     }
 }
