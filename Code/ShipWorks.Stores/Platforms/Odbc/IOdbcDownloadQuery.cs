d2bdab5271d8395368f9ff124ccd2842d0f17f2a
Kevin Croke <k.croke@shipworks.com>
2016-06-28 15:43:03 -0500

    TP#13993 Ad IOdbcDownloadQuery

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/IOdbcDownloadQuery.cs
@@ -0,0 +1,13 @@
+﻿namespace ShipWorks.Stores.Platforms.Odbc
+{
+    /// <summary>
+    /// Generates a context specific download query
+    /// </summary>
+    public interface IOdbcDownloadQuery
+    {
+        /// <summary>
+        /// Generates the Sql to download orders.
+        /// </summary>
+        string GenerateSql();
+    }
+}
