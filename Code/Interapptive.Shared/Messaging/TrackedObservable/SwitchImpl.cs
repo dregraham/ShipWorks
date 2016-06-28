a7a1f6e9d2799937def1e20745c5ea36439f7408
Andrew Benz <a.benz@shipworks.com>
2016-06-28 08:46:21 -0500

    TP#13928: Remove the refresh rates pipeline because it was causing 'someone else modified shipment' errors.
    The funtionality seems to also exist in the SHipmentViewModelBase, so this should not affect anything

--- Code/Interapptive.Shared/Messaging/TrackedObservable/SwitchImpl.cs
+++ Code/Interapptive.Shared/Messaging/TrackedObservable/SwitchImpl.cs
@@ -5,8 +5,14 @@ using System.Runtime.CompilerServices;
 
 namespace Interapptive.Shared.Messaging.TrackedObservable
 {
+    /// <summary>
+    /// Impelementation of the trackable switch statement
+    /// </summary>
     public static class SwitchImpl
     {
+        /// <summary>
+        /// Implementation of the trackable switch statement
+        /// </summary>
         public static IObservable<IMessageTracker<TSource>> Switch<TSource>(this IObservable<IMessageTracker<IObservable<IMessageTracker<TSource>>>> sources,
             object listener, [CallerMemberName] string callerName = "")
         {
