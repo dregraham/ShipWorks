3300a890a8859097ffa1ff58f4fe5955e4db94bf
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-07-01 15:06:19 -0500

    TP#14040 CreateOdbcDownloadStrategy enum

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/OdbcDownloadstrategy.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace ShipWorks.Stores.Platforms.Odbc
+{
+    /// <summary>
+    /// Configuration option for how the Store downloads orders from the Odbc Source
+    /// </summary>
+    [Obfuscation(Exclude = true)]
+    public enum OdbcDownloadStrategy
+    {
+        all = 0,
+        ByModifiedTime = 1
+    }
+}
