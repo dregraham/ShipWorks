a7d6564aa2740e5b87a1818641b3f27598eff9a2
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-07-05 10:29:37 -0500

    Write unit test for OdbcLastModifiedDownloadQuery sorting

--- Code/ShipWorks.Stores.Tests/Platforms/Odbc/OdbcLastModifiedDownloadQueryTest.cs
+++ Code/ShipWorks.Stores.Tests/Platforms/Odbc/OdbcLastModifiedDownloadQueryTest.cs
@@ -83,7 +83,42 @@ namespace ShipWorks.Stores.Tests.Platforms.Odbc
 
             OdbcLastModifiedDownloadQuery testObject = new OdbcLastModifiedDownloadQuery(downloadQuery.Object, onlineLastModifiedStartingPoint, fieldMap.Object, dbProviderFactory.Object, dataSource.Object);
 
-            Assert.Contains(onlineLastModifiedStartingPoint.ToString("u"), testObject.GenerateSql());
+            Assert.Contains(onlineLastModifiedStartingPoint.ToString("yyyy-MM-ddTHH\\:mm\\:ss.fff"), testObject.GenerateSql());
+        }
+
+        [Fact]
+        public void GenerateSql_GeneratesSqlThatContainsOrderBy()
+        {
+            string originalDownloadQuery = "SELECT * FROM FOO";
+            DateTime onlineLastModifiedStartingPoint = DateTime.UtcNow;
+            OdbcColumn column = new OdbcColumn("OnlineLastModified");
+
+            Mock<IOdbcDownloadQuery> downloadQuery = mock.Mock<IOdbcDownloadQuery>();
+            downloadQuery.Setup(d => d.GenerateSql()).Returns(originalDownloadQuery);
+
+            Mock<IExternalOdbcMappableField> externalField = mock.Mock<IExternalOdbcMappableField>();
+            externalField.SetupGet(e => e.Column).Returns(column);
+
+            Mock<IOdbcFieldMapEntry> entry = mock.Mock<IOdbcFieldMapEntry>();
+            entry.SetupGet(e => e.ExternalField).Returns(externalField.Object);
+
+            Mock<IOdbcFieldMap> fieldMap = mock.Mock<IOdbcFieldMap>();
+            fieldMap.Setup(f => f.FindEntriesBy(It.IsAny<EntityField2>())).Returns(new[] { entry.Object });
+
+            Mock<IShipWorksOdbcCommandBuilder> cmdBuilder = mock.Mock<IShipWorksOdbcCommandBuilder>();
+            cmdBuilder.Setup(c => c.QuoteIdentifier(It.IsAny<string>())).Returns(column.Name);
+
+            Mock<IShipWorksDbProviderFactory> dbProviderFactory = mock.Mock<IShipWorksDbProviderFactory>();
+            dbProviderFactory.Setup(d => d.CreateShipWorksOdbcCommandBuilder(It.IsAny<IShipWorksOdbcDataAdapter>())).Returns(cmdBuilder.Object);
+
+            Mock<DbConnection> connection = mock.Mock<DbConnection>();
+
+            Mock<IOdbcDataSource> dataSource = mock.Mock<IOdbcDataSource>();
+            dataSource.Setup(d => d.CreateConnection()).Returns(connection.Object);
+
+            OdbcLastModifiedDownloadQuery testObject = new OdbcLastModifiedDownloadQuery(downloadQuery.Object, onlineLastModifiedStartingPoint, fieldMap.Object, dbProviderFactory.Object, dataSource.Object);
+
+            Assert.EndsWith("ORDER BY OnlineLastModified ASC", testObject.GenerateSql());
         }
 
         [Fact]
