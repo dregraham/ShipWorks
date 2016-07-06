041868f8cbb1f7d51acef299c5e593ade66f167c
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-07-06 09:51:51 -0500

    Create OdbcColumnSourceType

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/DataSource/Schema/OdbcColumnSourceType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace ShipWorks.Stores.Platforms.Odbc.DataSource.Schema
+{
+    [Obfuscation(Exclude = true)]
+    public enum OdbcColumnSourceType
+    {
+        [Description("The column source is a table.")]
+        Table = 0,
+
+        [Description("The column source is a custom query.")]
+        CustomQuery = 1
+    }
+}
