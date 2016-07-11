d26eff1471c1b6b790a33367e82a41e508407b54
Chris Hicks <c.hicks@shipworks.com>
2016-07-11 10:04:11 -0500

    TP#14130: Create enum OdbcShipmentUploadStrategy

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/Upload/OdbcShipmentUploadStrategy.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace ShipWorks.Stores.Platforms.Odbc.Upload
+{
+    /// <summary>
+    /// Strategy on how to upload shipment details for Odbc
+    /// </summary>
+    public enum OdbcShipmentUploadStrategy
+    {
+        [Description("Do not upload")]
+        DoNotUpload = 0,
+
+        [Description("Upload to import data source")]
+        UseImportDataSource = 1,
+
+        [Description("Upload to shipment data source")]
+        UseShipmentDataSource = 2
+    }
+}
