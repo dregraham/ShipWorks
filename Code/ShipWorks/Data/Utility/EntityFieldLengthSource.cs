385644914e06309f614073afbeae51e5821f3ce0
Kevin Croke <k.croke@shipworks.com>
2016-07-14 12:09:44 -0500

    Bug#14183 Limit # of characters in custom query ui field.

--- Code/ShipWorks/Data/Utility/EntityFieldLengthSource.cs
+++ Code/ShipWorks/Data/Utility/EntityFieldLengthSource.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
+﻿using System.Reflection;
 
 namespace ShipWorks.Data.Utility
 {
@@ -168,5 +164,7 @@ namespace ShipWorks.Data.Utility
         AmazonShipmentShippingServiceName,
         AmazonShipmentShippingServiceID,
         AmazonShipmentShippingServiceOfferID,
+
+        OdbcStoreCustomQuery
     }
 }
