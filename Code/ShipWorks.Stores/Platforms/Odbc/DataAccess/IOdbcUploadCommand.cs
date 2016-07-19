d56b98b097719d98f36be6783dc91d6e85ac49ae
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-07-19 15:42:40 -0500

    TP#14220 create IOdbcUploadCommand and OdbcUploadCommand

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/DataAccess/IOdbcUploadCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace ShipWorks.Stores.Platforms.Odbc.DataAccess
+{
+    /// <summary>
+    /// Interface for uploading data to ODBC
+    /// </summary>
+    public interface IOdbcUploadCommand
+    {
+        /// <summary>
+        /// Execute the command
+        /// </summary>
+        int Execute();
+    }
+}
---
+++ Code/ShipWorks.Stores/Platforms/Odbc/DataAccess/OdbcUploadCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace ShipWorks.Stores.Platforms.Odbc.DataAccess
+{
+    /// <summary>
+    /// Command to upload shipment data to ODBC
+    /// </summary>
+    public class OdbcUploadCommand : IOdbcUploadCommand
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public OdbcUploadCommand()
+        {
+
+        }
+
+        /// <summary>
+        /// Execute the command
+        /// </summary>
+        public int Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
