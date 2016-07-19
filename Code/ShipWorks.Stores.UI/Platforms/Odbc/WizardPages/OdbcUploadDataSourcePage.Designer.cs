5bc50acc50e5e7d6aa1c729f2b5924094d330eab
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-07-19 09:49:15 -0500

    fix missing label after merge

--- Code/ShipWorks.Stores.UI/Platforms/Odbc/WizardPages/OdbcUploadDataSourcePage.Designer.cs
+++ Code/ShipWorks.Stores.UI/Platforms/Odbc/WizardPages/OdbcUploadDataSourcePage.Designer.cs
@@ -2,12 +2,12 @@
 {
     partial class OdbcUploadDataSourcePage
     {
-        /// <summary> 
+        /// <summary>
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
+        /// <summary>
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -22,8 +22,8 @@
 
         #region Component Designer generated code
 
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
+        /// <summary>
+        /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
@@ -31,18 +31,18 @@
             this.odbcDataSourceControl = new ShipWorks.Stores.UI.Platforms.Odbc.OdbcDataSourceControl();
             this.uploadsettings = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
+            //
             // odbcDataSourceControl
-            // 
+            //
             this.odbcDataSourceControl.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.odbcDataSourceControl.Location = new System.Drawing.Point(30, 22);
             this.odbcDataSourceControl.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.odbcDataSourceControl.Name = "odbcDataSourceControl";
             this.odbcDataSourceControl.Size = new System.Drawing.Size(487, 209);
             this.odbcDataSourceControl.TabIndex = 1;
-            // 
+            //
             // uploadsettings
-            // 
+            //
             this.uploadsettings.AutoSize = true;
             this.uploadsettings.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
             this.uploadsettings.Location = new System.Drawing.Point(20, 10);
@@ -50,11 +50,12 @@
             this.uploadsettings.Size = new System.Drawing.Size(96, 13);
             this.uploadsettings.TabIndex = 2;
             this.uploadsettings.Text = "Upload Settings";
-            // 
+            //
             // OdbcUploadDataSourcePage
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.uploadsettings);
             this.Controls.Add(this.odbcDataSourceControl);
             this.Description = "Setup the ODBC data source used for uploading your shipments.";
             this.Name = "OdbcUploadDataSourcePage";
