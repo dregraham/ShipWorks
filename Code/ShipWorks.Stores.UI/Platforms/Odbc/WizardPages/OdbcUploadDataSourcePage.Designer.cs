2a884561e1d7698e2667e5c63b90620dd0b15aa9
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-07-19 09:40:30 -0500

    TP#14236 fix alignment on odbc setup wizard pages

--- Code/ShipWorks.Stores.UI/Platforms/Odbc/WizardPages/OdbcUploadDataSourcePage.Designer.cs
+++ Code/ShipWorks.Stores.UI/Platforms/Odbc/WizardPages/OdbcUploadDataSourcePage.Designer.cs
@@ -29,16 +29,27 @@
         private void InitializeComponent()
         {
             this.odbcDataSourceControl = new ShipWorks.Stores.UI.Platforms.Odbc.OdbcDataSourceControl();
+            this.uploadsettings = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // odbcDataSourceControl
             // 
             this.odbcDataSourceControl.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.odbcDataSourceControl.Location = new System.Drawing.Point(10, 0);
+            this.odbcDataSourceControl.Location = new System.Drawing.Point(30, 22);
             this.odbcDataSourceControl.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.odbcDataSourceControl.Name = "odbcDataSourceControl";
             this.odbcDataSourceControl.Size = new System.Drawing.Size(487, 209);
             this.odbcDataSourceControl.TabIndex = 1;
+            // 
+            // uploadsettings
+            // 
+            this.uploadsettings.AutoSize = true;
+            this.uploadsettings.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
+            this.uploadsettings.Location = new System.Drawing.Point(20, 10);
+            this.uploadsettings.Name = "uploadsettings";
+            this.uploadsettings.Size = new System.Drawing.Size(96, 13);
+            this.uploadsettings.TabIndex = 2;
+            this.uploadsettings.Text = "Upload Settings";
             // 
             // OdbcUploadDataSourcePage
             // 
@@ -47,16 +58,18 @@
             this.Controls.Add(this.odbcDataSourceControl);
             this.Description = "Setup the ODBC data source used for uploading your shipments.";
             this.Name = "OdbcUploadDataSourcePage";
-            this.Size = new System.Drawing.Size(621, 231);
+            this.Size = new System.Drawing.Size(547, 291);
             this.Title = "Setup Upload Data Source";
             this.StepNext += new System.EventHandler<ShipWorks.UI.Wizard.WizardStepEventArgs>(this.OnStepNext);
             this.SteppingInto += new System.EventHandler<ShipWorks.UI.Wizard.WizardSteppingIntoEventArgs>(this.OnSteppingInto);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private OdbcDataSourceControl odbcDataSourceControl;
+        private System.Windows.Forms.Label uploadsettings;
     }
 }
