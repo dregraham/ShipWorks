0fc5f0c31a7b576552a2fb5aa848d57bdbbdc81a
Kevin Croke <k.croke@shipworks.com>
2016-07-20 10:45:20 -0500

    TP#14216 WIP: Adding OdbcUploader

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/Upload/IOdbcUploader.cs
@@ -0,0 +1,11 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+using System.Collections.Generic;
+
+namespace ShipWorks.Stores.Platforms.Odbc.Upload
+{
+    public interface IOdbcUploader
+    {
+        void UploadLatestShipment(OdbcStoreEntity store, long orderid);
+        void UploadShipments(OdbcStoreEntity store, IEnumerable<long> shipmentIds);
+    }
+}
---
+++ Code/ShipWorks.Stores/Platforms/Odbc/Upload/OdbcUploader.cs
@@ -0,0 +1,38 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping;
+using ShipWorks.Shipping.Services;
+using System;
+using System.Collections.Generic;
+
+namespace ShipWorks.Stores.Platforms.Odbc.Upload
+{
+    public class OdbcUploader : IOdbcUploader
+    {
+        private readonly IShippingManager shippingManager;
+
+        public OdbcUploader(IShippingManager shippingManager)
+        {
+            this.shippingManager = shippingManager;
+        }
+
+        public void UploadShipments(OdbcStoreEntity store, IEnumerable<long> shipmentIds)
+        {
+            foreach (long shipmentId in shipmentIds)
+            {
+                ICarrierShipmentAdapter carrierShipmentAdapter = shippingManager.GetShipment(shipmentId);
+                Upload(carrierShipmentAdapter.Shipment);
+            }
+        }
+
+        public void UploadLatestShipment(OdbcStoreEntity store, long orderid)
+        {
+            ShipmentEntity shipment = shippingManager.GetLatestActiveShipment(orderid);
+            Upload(shipment);
+        }
+
+        private void Upload(ShipmentEntity shipment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
