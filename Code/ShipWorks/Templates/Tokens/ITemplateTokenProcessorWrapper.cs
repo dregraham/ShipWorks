91e5c2ea855e45ea3b464405d5d55b7bb89671f0
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-07-22 10:59:57 -0500

    TP#14286 Create the TemplateTokenProcessorWrapper and interface

---
+++ Code/ShipWorks/Templates/Tokens/ITemplateTokenProcessorWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using ShipWorks.Templates.Processing.TemplateXml;
+
+namespace ShipWorks.Templates.Tokens
+{
+    /// <summary>
+    /// Interface that represents the TemplateTokenProcessor
+    /// </summary>
+    public interface ITemplateTokenProcessorWrapper
+    {
+        /// <summary>
+        /// Process the tokens in the token string for the given TemplateXPathNavigator as input
+        /// </summary>
+        string ProcessTokens(string tokenText, TemplateXPathNavigator templateXPath, bool stripNewLines = true);
+
+        /// <summary>
+        /// Process the tokens in the token string for the given entity ID
+        /// </summary>
+        string ProcessTokens(string tokenText, long entityID, bool stripNewLines = true);
+
+        /// <summary>
+        /// Process tokens in the token string with the given list of entity IDs
+        /// </summary>
+        string ProcessTokens(string tokenText, List<long> idList, bool stripNewLines = true);
+    }
+}
---
+++ Code/ShipWorks/Templates/Tokens/TemplateTokenProcessorWrapper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using ShipWorks.Templates.Processing.TemplateXml;
+
+namespace ShipWorks.Templates.Tokens
+{
+    /// <summary>
+    /// Wrapper for the static TemplateTokenProcessor
+    /// </summary>
+    public class TemplateTokenProcessorWrapper : ITemplateTokenProcessorWrapper
+    {
+        /// <summary>
+        /// Process the tokens in the token string for the given entity ID
+        /// </summary>
+        public string ProcessTokens(string tokenText, long entityID, bool stripNewLines = true)
+        {
+            return TemplateTokenProcessor.ProcessTokens(tokenText, entityID, stripNewLines);
+        }
+
+        /// <summary>
+        /// Process tokens in the token string with the given list of entity IDs
+        /// </summary>
+        public string ProcessTokens(string tokenText, List<long> idList, bool stripNewLines = true)
+        {
+            return TemplateTokenProcessor.ProcessTokens(tokenText, idList, stripNewLines);
+        }
+
+        /// <summary>
+        /// Process the tokens in the token string for the given TemplateXPathNavigator as input
+        /// </summary>
+        public string ProcessTokens(string tokenText, TemplateXPathNavigator templateXPath, bool stripNewLines = true)
+        {
+            return TemplateTokenProcessor.ProcessTokens(tokenText, templateXPath, stripNewLines);
+        }
+    }
+}
