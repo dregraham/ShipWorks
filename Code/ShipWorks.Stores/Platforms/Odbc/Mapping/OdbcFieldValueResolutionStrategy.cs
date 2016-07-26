bd179b20a3cb09a47298b21a413c6b80c79c456e
Kevin Croke <k.croke@shipworks.com>
2016-07-26 14:02:25 -0500

    TP#14392 Add OdbcFieldValueResolutionStrategy

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/Mapping/OdbcFieldValueResolutionStrategy.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace ShipWorks.Stores.Platforms.Odbc.Mapping
+{
+    [Obfuscation(Exclude = true)]
+    public enum OdbcFieldValueResolutionStrategy
+    {
+        [Description("Default")]
+        Default,
+
+        [Description("Shipping Carrier")]
+        ShippingCarrier,
+
+        [Description("Shipping Provider")]
+        ShippingService
+    }
+}
