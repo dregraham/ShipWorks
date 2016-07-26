79a814f8c505c67b62e0c1b71fe22422d2ca5f30
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-07-26 13:55:33 -0500

    TP#14388 create IOdbcFieldValueResolver interface

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/Upload/IOdbcFieldValueResolver.cs
@@ -0,0 +1,16 @@
+﻿using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Stores.Platforms.Odbc.Mapping;
+
+namespace ShipWorks.Stores.Platforms.Odbc.Upload
+{
+    /// <summary>
+    /// Represents an OdbcField Value Resolver
+    /// </summary>
+    public interface IOdbcFieldValueResolver
+    {
+        /// <summary>
+        /// Get a value for the specific field from the given entity
+        /// </summary>
+        object GetValue(IShipWorksOdbcMappableField field, IEntity2 entity);
+    }
+}
