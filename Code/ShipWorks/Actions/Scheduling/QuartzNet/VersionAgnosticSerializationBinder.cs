64634700f68a5d76b0dae8fc370294518e860325
Andrew Benz <a.benz@shipworks.com>
2016-07-28 16:26:28 -0500

    TP#14522: Add logging if specified assembly couldn't be loaded

--- Code/ShipWorks/Actions/Scheduling/QuartzNet/VersionAgnosticSerializationBinder.cs
+++ Code/ShipWorks/Actions/Scheduling/QuartzNet/VersionAgnosticSerializationBinder.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
+using log4net;
 
 namespace ShipWorks.Actions.Scheduling.QuartzNet
 {
@@ -10,15 +11,23 @@ namespace ShipWorks.Actions.Scheduling.QuartzNet
     /// </summary>
     internal class VersionAgnosticSerializationBinder : SerializationBinder
     {
+        private static ILog log = LogManager.GetLogger(typeof(VersionAgnosticSerializationBinder));
+
         /// <summary>
         /// Bind the specified type
         /// </summary>
+        /// <remarks>If we return null here, we'll fall back to the default assembly loading behavior</remarks>
         public override Type BindToType(string assemblyName, string typeName)
         {
             if (assemblyName.StartsWith("ShipWorks"))
             {
                 Assembly assembly = Assembly.Load(assemblyName.Split(',').First());
-                return FormatterServices.GetTypeFromAssembly(assembly, typeName);
+                if (assembly != null)
+                {
+                    return FormatterServices.GetTypeFromAssembly(assembly, typeName);
+                }
+
+                log.Warn($"Could not load assembly {assemblyName}");
             }
 
             return null;
