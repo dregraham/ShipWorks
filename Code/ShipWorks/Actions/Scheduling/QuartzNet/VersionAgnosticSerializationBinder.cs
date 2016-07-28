9e9ea13ba849de6bab42c45158c3eadf37c1f391
Andrew Benz <a.benz@shipworks.com>
2016-07-28 14:39:00 -0500

    TP#14522: Update serializer to ignore version of ShipWorks.Core

---
+++ Code/ShipWorks/Actions/Scheduling/QuartzNet/VersionAgnosticSerializationBinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace ShipWorks.Actions.Scheduling.QuartzNet
+{
+    /// <summary>
+    /// Serialization binder that is version agnostic
+    /// </summary>
+    internal class VersionAgnosticSerializationBinder : SerializationBinder
+    {
+        /// <summary>
+        /// Bind the specified type
+        /// </summary>
+        public override Type BindToType(string assemblyName, string typeName)
+        {
+            if (assemblyName.StartsWith("ShipWorks"))
+            {
+                Assembly assembly = Assembly.Load(assemblyName.Split(',').First());
+                return FormatterServices.GetTypeFromAssembly(assembly, typeName);
+            }
+
+            return null;
+        }
+    }
+}
