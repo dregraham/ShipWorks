5437dff1d362305d9d6c2b5b31d90bb936e1cae1
Kevin Croke <k.croke@shipworks.com>
2016-08-01 09:25:36 -0500

    tp#13073 Fix: Override equals in OdbcColumn so OdbcMappingControl selected value is read from the map

--- Code/ShipWorks.Stores/Platforms/Odbc/DataSource/Schema/OdbcColumn.cs
+++ Code/ShipWorks.Stores/Platforms/Odbc/DataSource/Schema/OdbcColumn.cs
@@ -6,7 +6,7 @@ namespace ShipWorks.Stores.Platforms.Odbc.DataSource.Schema
     /// Represents a column from an ODBC table
     /// </summary>
 	public class OdbcColumn
-	{
+    {
         /// <summary>
         /// Constructor
         /// </summary>
@@ -19,6 +19,24 @@ namespace ShipWorks.Stores.Platforms.Odbc.DataSource.Schema
         /// The column name
         /// </summary>
         [Obfuscation(Exclude = true)]
-	    public string Name { get; set; }
-	}
+	    public string Name { get; }
+
+        /// <summary>
+        /// Determines whether the specified Object is equal to this instance.
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            OdbcColumn odbcColumnToCompare = obj as OdbcColumn;
+
+            return odbcColumnToCompare != null && Name == odbcColumnToCompare.Name;
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        public override int GetHashCode()
+        {
+            return Name?.GetHashCode() ?? 0;
+        }
+    }
 }
