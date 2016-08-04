8eceacd0409e3390f5b03c5335b8da36c392b56b
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-08-04 16:12:59 -0500

    fix issue where ODBC store type can be added when it should be restricted

---
+++ Code/ShipWorks.UI/Controls/ChannelLimit/ChannelLimitBehavior/OdbcBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using ShipWorks.ApplicationCore.Licensing;
+using ShipWorks.Editions;
+using ShipWorks.Stores;
+
+namespace ShipWorks.UI.Controls.ChannelLimit.ChannelLimitBehavior
+{
+    /// <summary>
+    /// Channel limit behavior for the ODBC
+    /// </summary>
+    public class OdbcBehavior : IChannelLimitBehavior
+    {
+        /// <summary>
+        /// Populates the channels.
+        /// </summary>
+        public void PopulateChannels(ObservableCollection<StoreTypeCode> channels,
+            StoreTypeCode? channelToAdd)
+        {
+            channels.Clear();
+
+            channels.Add(StoreTypeCode.Odbc);
+        }
+
+        /// <summary>
+        /// Gets the edition feature.
+        /// </summary>
+        public EditionFeature EditionFeature => EditionFeature.Odbc;
+
+        /// <summary>
+        /// Title
+        /// </summary>
+        public string Title => "ODBC not allowed";
+    }
+}
