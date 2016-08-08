3e484413f4e5dd24abfc46412cb1f17811704c3c
Kevin Croke <k.croke@shipworks.com>
2016-08-08 14:32:26 -0500

    TP#14633 IFileDialog with comments

--- Code/Interapptive.Shared/UI/IFileDialog.cs
+++ Code/Interapptive.Shared/UI/IFileDialog.cs
@@ -2,12 +2,38 @@
 
 namespace Interapptive.Shared.UI
 {
+    /// <summary>
+    /// Interface to get a file from the user
+    /// </summary>
     public interface IFileDialog
     {
+        /// <summary>
+        /// Shows the open file Dialog box
+        /// </summary>
+        /// <remarks>Sets FileName property if dialog result is OK</remarks>
+        /// <param name="filter">
+        /// The file name filter string, which determines the choices that appear in the "Files of type" box in the dialog box.
+        /// </param>
+        /// <returns>DialogResult</returns>
         DialogResult ShowOpenFile(string filter);
 
+        /// <summary>
+        /// Shows the save file dialog box.
+        /// </summary>
+        /// <remarks>Sets FileName property if dialog result is OK</remarks>
+        /// <param name="filter"> 
+        /// The file name filter string, which determines the choices that appear in the "Save as file type" box in the dialog box.
+        /// </param>
+        /// <param name="defaultExtension">The default file extension.</param>
+        /// <param name="initialFileName">
+        /// A string containing the file name selected in the file dialog box.
+        /// </param>
+        /// <returns>DialogResult</returns>
         DialogResult ShowSaveFile(string filter, string defaultExtension, string initialFileName);
 
+        /// <summary>
+        /// Gets the name of the file - Empty string if not set.
+        /// </summary>
         string FileName { get; }
     }
 }
