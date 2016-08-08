62dee8f2b26e9fc90aefbfe0c3ae882b04e1a783
Kevin Croke <k.croke@shipworks.com>
2016-08-08 14:19:12 -0500

    TP#14633 IFileDialog stubbed out. Need to add comments...

---
+++ Code/Interapptive.Shared/UI/IFileDialog.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace Interapptive.Shared.UI
+{
+    public interface IFileDialog
+    {
+        DialogResult ShowOpenFile(string filter);
+
+        DialogResult ShowSaveFile(string filter, string defaultExtension, string initialFileName);
+
+        string FileName { get; }
+    }
+}
