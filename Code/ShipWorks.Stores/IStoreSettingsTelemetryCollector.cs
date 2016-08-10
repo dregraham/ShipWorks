41c1493883449809a03e2f69a99f66275f13a32c
Kevin Croke <k.croke@shipworks.com>
2016-08-10 12:45:29 -0500

    TP#14675 Add IStoreSettingsTelemetryCollector

---
+++ Code/ShipWorks.Stores/IStoreSettingsTelemetryCollector.cs
@@ -0,0 +1,15 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Stores
+{
+    /// <summary>
+    /// Collects telemetry data for a store
+    /// </summary>
+    public interface IStoreSettingsTelemetryCollector
+    {
+        /// <summary>
+        /// Collects telemetry for a store
+        /// </summary>
+        void CollectTelemetry(StoreEntity store, StoreSettingsTrackedDurationEvent storeSettingsTrackedDurationEvent);
+    }
+}
---
+++ Code/ShipWorks.Stores/StoreSettingsTrackedDurationEvent.cs
@@ -0,0 +1,14 @@
+﻿using Interapptive.Shared.Metrics;
+
+namespace ShipWorks.Stores
+{
+    /// <summary>
+    /// Utility class for tracking the duration of a Store Settings event, along with other metric info
+    /// </summary>
+    public class StoreSettingsTrackedDurationEvent : TrackedDurationEvent
+    {
+        public StoreSettingsTrackedDurationEvent() : base(string.Empty)
+        {
+        }
+    }
+}
