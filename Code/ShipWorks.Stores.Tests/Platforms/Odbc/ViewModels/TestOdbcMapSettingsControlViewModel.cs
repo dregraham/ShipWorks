1e9cea5e80b29691304b5f5e6cd32a70159702e6
Kevin Croke <k.croke@shipworks.com>
2016-08-12 09:34:03 -0500

    TP#14738 Fixed build. Fake class for testing wasn't implementing CustomQueryColumnSourceName

--- Code/ShipWorks.Stores.Tests/Platforms/Odbc/ViewModels/TestOdbcMapSettingsControlViewModel.cs
+++ Code/ShipWorks.Stores.Tests/Platforms/Odbc/ViewModels/TestOdbcMapSettingsControlViewModel.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using Interapptive.Shared.UI;
+﻿using Interapptive.Shared.UI;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Stores.Platforms.Odbc.DataSource.Schema;
 using ShipWorks.Stores.UI.Platforms.Odbc.ViewModels;
+using System;
 
 namespace ShipWorks.Stores.Tests.Platforms.Odbc.ViewModels
 {
@@ -23,5 +23,7 @@ namespace ShipWorks.Stores.Tests.Platforms.Odbc.ViewModels
         {
             // not implemented for testing the abstract class
         }
+
+        public override string CustomQueryColumnSourceName { get; }
     }
 }
