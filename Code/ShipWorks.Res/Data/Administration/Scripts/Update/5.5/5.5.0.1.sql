a57ac221857e585e83d2c91ec42eea6e6cb6447c
Tim Hughes <t.hughes@shipworks.com>
2016-08-15 09:31:58 -0500

    TP#14649: After testing with all indexes, it looks like having the multiple order item indexes caused queries (that compared SKU and Code for example) to run slower.  So removing the order item indexes for now.  Full text indexes should make these queries faster anyhow.

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.5/5.5.0.1.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.5/5.5.0.1.sql
@@ -1,5 +1,4 @@
-﻿
-PRINT N'Dropping index [IX_Auto_OrderDate] from [dbo].[Order]'
+﻿PRINT N'Dropping index [IX_Auto_OrderDate] from [dbo].[Order]'
 GO
 IF EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'IX_Auto_OrderDate' AND object_id = OBJECT_ID(N'[dbo].[Order]'))
 DROP INDEX [IX_Auto_OrderDate] ON [dbo].[Order]
@@ -13,33 +12,6 @@ PRINT N'Dropping index [IX_Auto_StoreID] from [dbo].[Order]'
 GO
 IF EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'IX_Auto_StoreID' AND object_id = OBJECT_ID(N'[dbo].[Order]'))
 DROP INDEX [IX_Auto_StoreID] ON [dbo].[Order]
-GO
-
-/*  Modify OrderItem table indexes  */
-PRINT N'Creating index [IX_OrderItem_Code] on [dbo].[OrderItem]'
-GO
-IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'IX_OrderItem_Code' AND object_id = OBJECT_ID(N'[dbo].[OrderItem]'))
-CREATE NONCLUSTERED INDEX [IX_OrderItem_Code] ON [dbo].[OrderItem] ([Code]) INCLUDE ([OrderID])
-GO
-PRINT N'Creating index [IX_OrderItem_Name] on [dbo].[OrderItem]'
-GO
-IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'IX_OrderItem_Name' AND object_id = OBJECT_ID(N'[dbo].[OrderItem]'))
-CREATE NONCLUSTERED INDEX [IX_OrderItem_Name] ON [dbo].[OrderItem] ([Name]) INCLUDE ([OrderID])
-GO
-PRINT N'Creating index [IX_OrderItem_Quantity] on [dbo].[OrderItem]'
-GO
-IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'IX_OrderItem_Quantity' AND object_id = OBJECT_ID(N'[dbo].[OrderItem]'))
-CREATE NONCLUSTERED INDEX [IX_OrderItem_Quantity] ON [dbo].[OrderItem] ([Quantity]) INCLUDE ([OrderID])
-GO
-PRINT N'Creating index [IX_OrderItem_Sku] on [dbo].[OrderItem]'
-GO
-IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'IX_OrderItem_Sku' AND object_id = OBJECT_ID(N'[dbo].[OrderItem]'))
-CREATE NONCLUSTERED INDEX [IX_OrderItem_Sku] ON [dbo].[OrderItem] ([SKU]) INCLUDE ([OrderID])
-GO
-PRINT N'Creating index [IX_OrderItem_Weight] on [dbo].[OrderItem]'
-GO
-IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'IX_OrderItem_Weight' AND object_id = OBJECT_ID(N'[dbo].[OrderItem]'))
-CREATE NONCLUSTERED INDEX [IX_OrderItem_Weight] ON [dbo].[OrderItem] ([Weight]) INCLUDE ([OrderID])
 GO
 
 /*  Modify Order table indexes  */
