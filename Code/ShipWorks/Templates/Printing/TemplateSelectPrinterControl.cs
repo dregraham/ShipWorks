ce214dd2a61ceb3f1808b9f25c2ffaaecc010174
Tim Hughes <t.hughes@shipworks.com>
2016-09-02 08:38:31 -0500

    TP#15001: Getting printer settings from Windows was taking a lot of time.  So now caching those and the PrinterSettingsProxy objects.

--- Code/ShipWorks/Templates/Printing/TemplateSelectPrinterControl.cs
+++ Code/ShipWorks/Templates/Printing/TemplateSelectPrinterControl.cs
@@ -87,7 +87,7 @@ namespace ShipWorks.Templates.Printing
         private void OnUseDefault(object sender, EventArgs e)
         {
             var templateSettings = TemplateHelper.GetComputerSettings(template);
-            var printerSettings = new PrinterSettings();
+            var printerSettings = PrinterSettingFactory.GetDefaultPrinterSettings();
 
             if (printerSettings.IsValid)
             {
