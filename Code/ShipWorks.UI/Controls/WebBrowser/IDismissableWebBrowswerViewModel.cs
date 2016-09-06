b99347919ef415af5eadc55ebb3c15ffcfbcbc6c
Chris Hicks <c.hicks@shipworks.com>
2016-09-06 14:59:17 -0500

    Create IDismissableWebBrowserViewModel and IGlobalPostLabelNotification

---
+++ Code/ShipWorks.UI/Controls/WebBrowser/IDismissableWebBrowswerViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ShipWorks.UI.Controls.WebBrowser
+{
+    /// <summary>
+    /// View Model for DismissableWebBrowser
+    /// </summary>
+    /// <seealso cref="ShipWorks.UI.Controls.WebBrowser.IWebBrowserDlgViewModel" />
+    public interface IDismissableWebBrowswerViewModel : IWebBrowserDlgViewModel
+    {
+        /// <summary>
+        /// Dismisses this instance
+        /// </summary>
+        void Dismiss();
+    }
+}
---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Usps/IGlobalPostLabelNotification.cs
@@ -0,0 +1,15 @@
+﻿namespace ShipWorks.Shipping.Carriers.Postal.Usps
+{
+    public interface IGlobalPostLabelNotification
+    {
+        /// <summary>
+        /// Check to see if we should show the notification based on the current user session
+        /// </summary>
+        bool AppliesToSession();
+
+        /// <summary>
+        /// Show the notification and save result in UserEntity NextGlobapPostNotificationDate
+        /// </summary>
+        void Show();
+    }
+}
