cce16b2a220ca296f578b3a866a4dc4a4de6aafe
Chris Hicks <c.hicks@shipworks.com>
2016-09-06 10:11:11 -0500

    comments

--- Code/ShipWorks/Stores/Platforms/ThreeDCart/ThreeDCartOptionNode.cs
+++ Code/ShipWorks/Stores/Platforms/ThreeDCart/ThreeDCartOptionNode.cs
@@ -2,10 +2,19 @@
 
 namespace ShipWorks.Stores.Platforms.ThreeDCart
 {
+    /// <summary>
+    /// DTO for 3dcart production option nodes
+    /// </summary>
     public class ThreeDCartOptionNode
     {
+        /// <summary>
+        /// Gets or sets the option type node.
+        /// </summary>
         public XElement OptionTypeNode { get; set; }
 
+        /// <summary>
+        /// Gets or sets the option value node.
+        /// </summary>
         public XElement OptionValueNode { get; set; }
     }
 }
