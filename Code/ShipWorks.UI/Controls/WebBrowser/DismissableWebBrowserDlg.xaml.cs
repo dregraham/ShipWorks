50a01cba28f23a3a21ab029f3923536f1aef42d4
Kevin Croke <k.croke@shipworks.com>
2016-09-06 16:26:39 -0500

    TP#15140 and TP#15141 DismissableWebBrowserDlg and ViewModel
    (cherry picked from commit b36c179aaaa57a86654b215faefffbd06473e429)

---
+++ Code/ShipWorks.UI/Controls/WebBrowser/DismissableWebBrowserDlg.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Interapptive.Shared.UI;
+using System;
+using System.Windows;
+using System.Windows.Interop;
+using IWin32Window = System.Windows.Forms.IWin32Window;
+
+namespace ShipWorks.UI.Controls.WebBrowser
+{
+    /// <summary>
+    /// Interaction logic for DismissableWebBrowserDlg.xaml
+    /// </summary>
+    public partial class DismissableWebBrowserDlg : IWin32Window, IDialog
+    {
+        public DismissableWebBrowserDlg()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Window handle.
+        /// </summary>
+        public IntPtr Handle { get; set; }
+
+        /// <summary>
+        /// Called when [click close].
+        /// </summary>
+        private void OnClickClose(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Loads the owner.
+        /// </summary>
+        public void LoadOwner(IWin32Window owner)
+        {
+            Handle = owner.Handle;
+
+            new WindowInteropHelper(this)
+            {
+                Owner = owner.Handle
+            };
+        }
+    }
+}
---
+++ Code/ShipWorks.UI/Controls/WebBrowser/IDismissableWebBrowserDlgViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ShipWorks.UI.Controls.WebBrowser
+{
+    /// <summary>
+    /// Interface for ViewModel for DismissableWebBrowserDlg
+    /// </summary>
+    public interface IDismissableWebBrowserDlgViewModel
+    {
+        /// <summary>
+        /// Loads the specified URL.
+        /// </summary>
+        void Load(Uri url, string title, string moreInfoUrl);
+
+        /// <summary>
+        /// Gets or sets a value indicating the client doesn't want to see the message again.
+        /// </summary>
+        bool Dissmissed { get; set; }
+
+        /// <summary>
+        /// Command that display moreInfoUrl in default browser
+        /// </summary>
+        ICommand MoreInfoClickCommand { get; }
+    }
+}
