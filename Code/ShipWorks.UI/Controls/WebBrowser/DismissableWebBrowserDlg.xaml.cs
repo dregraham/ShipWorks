faa4cdde3b2659fddb9e8e22b12d76f16520c17a
Kevin Croke <k.croke@shipworks.com>
2016-09-08 15:37:06 -0500

    TP#14715 SW-CR-236 Removed code from codebehind and moved it into behaviors.
    (cherry picked from commit acdc84fa191089634f401bddbea538369d864f6e)

--- Code/ShipWorks.UI/Controls/WebBrowser/DismissableWebBrowserDlg.xaml.cs
+++ Code/ShipWorks.UI/Controls/WebBrowser/DismissableWebBrowserDlg.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using Interapptive.Shared.UI;
 using System;
-using System.Reflection;
 using System.Windows;
 using System.Windows.Interop;
-using System.Windows.Navigation;
 
 
 namespace ShipWorks.UI.Controls.WebBrowser
@@ -20,17 +18,7 @@ namespace ShipWorks.UI.Controls.WebBrowser
         {
             InitializeComponent();
         }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether [hide scrollbars].
-        /// </summary>
-        public bool HideScrollbars { get; set; } = true;
-
-        /// <summary>
-        /// Gets or sets a value indicating whether [suppress popups] - this includes script errors.
-        /// </summary>
-        public bool SuppressPopups { get; set; } = true;
-        
+       
         /// <summary>
         /// Window handle.
         /// </summary>
@@ -55,41 +43,6 @@ namespace ShipWorks.UI.Controls.WebBrowser
             {
                 Owner = owner.Handle
             };
-        }
-
-        /// <summary>
-        /// Called when [browser load completed].
-        /// </summary>
-        void OnBrowserLoadCompleted(object sender, NavigationEventArgs e)
-        {
-            if (HideScrollbars)
-            {
-                System.Windows.Controls.WebBrowser browser = (System.Windows.Controls.WebBrowser)sender;
-
-                string script = "document.documentElement.style.overflow ='hidden'";
-                browser.InvokeScript("execScript", script, "JavaScript");
-            }
-        }
-
-        /// <summary>
-        /// Called when [browser navigated].
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="NavigationEventArgs"/> instance containing the event data.</param>
-        void OnBrowserNavigated(object sender, NavigationEventArgs e)
-        {
-            if (SuppressPopups)
-            {
-                System.Windows.Controls.WebBrowser browser = (System.Windows.Controls.WebBrowser) sender;
-
-                FieldInfo webBrowserInfo = browser.GetType()
-                    .GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
-
-                object comWebBrowser = webBrowserInfo?.GetValue(browser);
-
-                comWebBrowser?.GetType()
-                    .InvokeMember("Silent", BindingFlags.SetProperty, null, comWebBrowser, new object[] {SuppressPopups});
-            }
         }
     }
 }
