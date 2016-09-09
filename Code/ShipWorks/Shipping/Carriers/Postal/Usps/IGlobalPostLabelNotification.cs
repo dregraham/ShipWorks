112e1dd79407cf1ff4cb46ed81e62bf253218a17
Chris Hicks <c.hicks@shipworks.com>
2016-09-09 08:26:01 -0500

    SW-CR-236: Fix issue with comment
    (cherry picked from commit 8fc0cf1345fb4bf7bb563d6958a2635e3dfb8609)

--- Code/ShipWorks/Shipping/Carriers/Postal/Usps/IGlobalPostLabelNotification.cs
+++ Code/ShipWorks/Shipping/Carriers/Postal/Usps/IGlobalPostLabelNotification.cs
@@ -3,8 +3,7 @@
     public interface IGlobalPostLabelNotification
     {
         /// <summary>
-        /// Check to see if we should show the notification based on the current user. If the user
-        /// dismisses the notification, don't show again. If not, show once a day.
+        /// Check to see if we should show the notification based on the current user.
         /// </summary>
         bool AppliesToCurrentUser();
 
