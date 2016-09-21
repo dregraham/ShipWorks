a448e97992ddfcc052095f3242316420af315410
Chris Hicks <c.hicks@shipworks.com>
2016-09-21 16:25:12 -0500

    TP#15178: Add script to update RollupItemQuantity for existing orders with multiple items

---
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.7/5.7.0.0.sql
@@ -0,0 +1,18 @@
+﻿disable trigger FilterDirtyOrder ON [Order];
+disable trigger OrderAuditTrigger ON [Order];
+disable trigger OrderLabelTrigger ON [Order];
+disable trigger OrderRollupTrigger ON [Order];
+disable trigger OrderShipmentShipSenseStatusTrigger ON [Order];
+disable trigger OrderShipSenseRecognitionStatusTrigger ON [Order];
+GO
+UPDATE orderTable
+SET orderTable.RollupItemQuantity = (Select SUM(Quantity) from [OrderItem] oi WHERE orderTable.OrderID = oi.OrderID)
+From [Order] as orderTable;
+GO
+enable trigger FilterDirtyOrder ON [Order];
+enable trigger OrderAuditTrigger ON [Order];
+enable trigger OrderLabelTrigger ON [Order];
+enable trigger OrderRollupTrigger ON [Order];
+enable trigger OrderShipmentShipSenseStatusTrigger ON [Order];
+enable trigger OrderShipSenseRecognitionStatusTrigger ON [Order];
+GO
