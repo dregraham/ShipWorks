1cff7b7bcb596510a3310f8c0e7a3955e26de075
Chris Hicks <c.hicks@shipworks.com>
2016-10-19 16:36:20 -0500

    TP#15372: Update GrouponCarrier to upload carrier information, based on our latest Groupon documentation.

---
+++ Code/ShipWorks.Stores.Tests/Platforms/Groupon/GrouponCarrierTest.cs
@@ -0,0 +1,124 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping;
+using ShipWorks.Shipping.Carriers.FedEx.Enums;
+using ShipWorks.Shipping.Carriers.Postal;
+using ShipWorks.Shipping.Carriers.UPS.Enums;
+using ShipWorks.Stores.Platforms.Groupon;
+using Xunit;
+
+namespace ShipWorks.Stores.Tests.Platforms.Groupon
+{
+    public class GrouponCarrierTest
+    {
+
+        [Fact]
+        public void GetCarrierCode_ReturnsUsps_WhenCarrierIsUspsAndServiceIsNotMediaMail()
+        {
+            ShipmentEntity shipment = new ShipmentEntity();
+            shipment.Postal = new PostalShipmentEntity();
+            shipment.Postal.Service = (int)PostalServiceType.PriorityMail;
+            shipment.ShipmentType = (int)ShipmentTypeCode.Usps;
+
+            Assert.Equal("usps", GrouponCarrier.GetCarrierCode(shipment));
+        }
+
+        [Fact]
+        public void GetCarrierCode_ReturnsUspsmm_WhenCarrierIsUspsAndServiceIsMediaMail()
+        {
+            ShipmentEntity shipment = new ShipmentEntity();
+            shipment.Postal = new PostalShipmentEntity();
+            shipment.Postal.Service = (int)PostalServiceType.MediaMail;
+            shipment.ShipmentType = (int)ShipmentTypeCode.Usps;
+
+            Assert.Equal("uspsmm", GrouponCarrier.GetCarrierCode(shipment));
+        }
+
+        [Fact]
+        public void GetCarrierCode_ReturnsDhlsm_WhenCarrierIsDhlAndServiceIsSmartMail()
+        {
+            ShipmentEntity shipment = new ShipmentEntity();
+            shipment.Postal = new PostalShipmentEntity();
+            shipment.Postal.Service = (int) PostalServiceType.DhlParcelPlusExpedited;
+            shipment.ShipmentType = (int) ShipmentTypeCode.Endicia;
+
+            Assert.Equal("dhlsm", GrouponCarrier.GetCarrierCode(shipment));
+        }
+
+        [Fact]
+        public void GetCarrierCode_ReturnsFedex_WhenCarrierIsFedex()
+        {
+            ShipmentEntity shipment = new ShipmentEntity();
+            shipment.FedEx = new FedExShipmentEntity();
+            shipment.FedEx.Service = (int) FedExServiceType.FedEx2Day;
+            shipment.ShipmentType = (int)ShipmentTypeCode.FedEx;
+
+            Assert.Equal("fedex", GrouponCarrier.GetCarrierCode(shipment));
+        }
+
+        [Fact]
+        public void GetCarrierCode_ReturnsFedexhd_WhenCarrierIsFedexAndServiceIsHomeDelivery()
+        {
+            ShipmentEntity shipment = new ShipmentEntity();
+            shipment.FedEx = new FedExShipmentEntity();
+            shipment.FedEx.Service = (int)FedExServiceType.GroundHomeDelivery;
+            shipment.ShipmentType = (int)ShipmentTypeCode.FedEx;
+
+            Assert.Equal("Fedexhd", GrouponCarrier.GetCarrierCode(shipment));
+        }
+
+        [Fact]
+        public void GetCarrierCode_ReturnsFedexsp_WhenCarrierIsFedexAndServiceIsSmartPost()
+        {
+            ShipmentEntity shipment = new ShipmentEntity();
+            shipment.FedEx = new FedExShipmentEntity();
+            shipment.FedEx.Service = (int)FedExServiceType.SmartPost;
+            shipment.ShipmentType = (int)ShipmentTypeCode.FedEx;
+
+            Assert.Equal("fedexsp", GrouponCarrier.GetCarrierCode(shipment));
+        }
+
+        [Fact]
+        public void GetCarrierCode_ReturnsFedexsp_WhenCarrierIsFedexAndServiceIsPriority()
+        {
+            ShipmentEntity shipment = new ShipmentEntity();
+            shipment.FedEx = new FedExShipmentEntity();
+            shipment.FedEx.Service = (int)FedExServiceType.PriorityOvernight;
+            shipment.ShipmentType = (int)ShipmentTypeCode.FedEx;
+
+            Assert.Equal("Fexp", GrouponCarrier.GetCarrierCode(shipment));
+        }
+
+        [Fact]
+        public void GetCarrierCode_ReturnsUps_WhenCarrierIsUpsAndServiceIsNotMI()
+        {
+            ShipmentEntity shipment = new ShipmentEntity();
+            shipment.Ups = new UpsShipmentEntity();
+            shipment.Ups.Service = (int)UpsServiceType.Ups2DayAir;
+            shipment.ShipmentType = (int)ShipmentTypeCode.UpsOnLineTools;
+
+            Assert.Equal("ups", GrouponCarrier.GetCarrierCode(shipment));
+        }
+
+        [Fact]
+        public void GetCarrierCode_ReturnsUpsmi_WhenCarrierIsUpsAndServiceIsMI()
+        {
+            ShipmentEntity shipment = new ShipmentEntity();
+            shipment.Ups = new UpsShipmentEntity();
+            shipment.Ups.Service = (int)UpsServiceType.UpsMailInnovationsExpedited;
+            shipment.ShipmentType = (int)ShipmentTypeCode.UpsOnLineTools;
+
+            Assert.Equal("upsmi", GrouponCarrier.GetCarrierCode(shipment));
+        }
+
+        [Fact]
+        public void GetCarrierCode_ReturnsOtherCarrierName_WhenCarrierIsOther()
+        {
+            ShipmentEntity shipment = new ShipmentEntity();
+            shipment.Other = new OtherShipmentEntity();
+            shipment.Other.Carrier = "Other Carrier";
+            shipment.ShipmentType = (int)ShipmentTypeCode.Other;
+
+            Assert.Equal("Other Carrier", GrouponCarrier.GetCarrierCode(shipment));
+        }
+    }
+}
--- Code/ShipWorks/Stores/Platforms/Groupon/GrouponCarrier.cs
+++ Code/ShipWorks/Stores/Platforms/Groupon/GrouponCarrier.cs
@@ -1,6 +1,8 @@
 ﻿using ShipWorks.Shipping;
 using ShipWorks.Data.Model.EntityClasses;
 using System.Diagnostics;
+using Interapptive.Shared.Utility;
+using ShipWorks.Shipping.Carriers.FedEx.Enums;
 using ShipWorks.Shipping.Carriers.Postal;
 using ShipWorks.Shipping.Carriers.UPS;
 using ShipWorks.Shipping.Carriers.UPS.Enums;
@@ -22,45 +24,19 @@ namespace ShipWorks.Stores.Platforms.Groupon
                 case ShipmentTypeCode.Express1Endicia:
                 case ShipmentTypeCode.Express1Usps:
                 case ShipmentTypeCode.PostalWebTools:
-                    carrierCode = "USPS";
-                    break;
-
                 case ShipmentTypeCode.Usps:
                 case ShipmentTypeCode.Endicia:
-                    // The shipment is an Endicia shipment, check to see if it's DHL
-                    if (shipmentEntity.Postal != null && ShipmentTypeManager.IsDhl((PostalServiceType)shipmentEntity.Postal.Service))
-                    {
-                        // The DHL carrier for Endicia is:
-                        carrierCode = "DHL";
-                    }
-                    else if (shipmentEntity.Postal != null && ShipmentTypeManager.IsConsolidator((PostalServiceType)shipmentEntity.Postal.Service))
-                    {
-                        carrierCode = "USPS";
-                    }
-                    else
-                    {
-                        // Use the default carrier for other Endicia types
-                        carrierCode = "USPS";
-                    }
+                    carrierCode = GetPostalCarrierCode(shipmentEntity);
                     break;
 
                 case ShipmentTypeCode.FedEx:
-                    carrierCode = "FedEx";
+                    carrierCode = GetFedexCarrierCode(shipmentEntity);
                     break;
 
                 case ShipmentTypeCode.UpsOnLineTools:
                 case ShipmentTypeCode.UpsWorldShip:
-                    ShippingManager.EnsureShipmentLoaded(shipmentEntity);
                     //The shipment is a UPS shipment, check to see if it is UPS-MI
-                    if (UpsUtility.IsUpsMiService((UpsServiceType)shipmentEntity.Ups.Service))
-                    {
-                        carrierCode = "upsmi";
-
-                    }
-                    else
-                    {
-                        carrierCode = "UPS";
-                    }
+                    carrierCode = UpsUtility.IsUpsMiService((UpsServiceType)shipmentEntity.Ups.Service) ? "upsmi" : "ups";
                     break;
 
                 case ShipmentTypeCode.OnTrac:
@@ -72,7 +48,6 @@ namespace ShipWorks.Stores.Platforms.Groupon
                     break;
 
                 case ShipmentTypeCode.Other:
-                    ShippingManager.EnsureShipmentLoaded(shipmentEntity);
                     carrierCode = shipmentEntity.Other.Carrier;
                     break;
 
@@ -80,6 +55,69 @@ namespace ShipWorks.Stores.Platforms.Groupon
                     Debug.Fail("Unhandled ShipmentTypeCode in Groupon.GetCarrierCode");
                     carrierCode = "Other";
                     break;
+            }
+
+            return carrierCode;
+        }
+
+        /// <summary>
+        /// Gets the fedex carrier code.
+        /// </summary>
+        private static string GetFedexCarrierCode(ShipmentEntity shipmentEntity)
+        {
+            string carrierCode = string.Empty;
+
+            if (shipmentEntity.FedEx != null)
+            {
+                FedExServiceType service = (FedExServiceType) shipmentEntity.FedEx.Service;
+                if (service == FedExServiceType.PriorityOvernight)
+                {
+                    carrierCode = "Fexp";
+                }
+                else if (service == FedExServiceType.GroundHomeDelivery)
+                {
+                    carrierCode = "Fedexhd";
+                }
+                else if (service == FedExServiceType.SmartPost)
+                {
+                    carrierCode = "fedexsp";
+                }
+                else
+                {
+                    carrierCode = "fedex";
+                }
+            }
+            return carrierCode;
+        }
+
+        /// <summary>
+        /// Gets the carrier code for Usps shipments
+        /// </summary>
+        private static string GetPostalCarrierCode(ShipmentEntity shipmentEntity)
+        {
+            string carrierCode;
+
+            // The shipment is an Endicia shipment, check to see if it's DHL
+            if (shipmentEntity.Postal != null && ShipmentTypeManager.IsDhl((PostalServiceType) shipmentEntity.Postal.Service))
+            {
+                carrierCode = ShipmentTypeManager.IsDhlSmartMail((PostalServiceType) shipmentEntity.Postal.Service) ?
+                    "dhlsm" :
+                    "dhl";
+            }
+            else if (shipmentEntity.Postal != null &&
+                     ShipmentTypeManager.IsConsolidator((PostalServiceType) shipmentEntity.Postal.Service))
+            {
+                carrierCode = "usps";
+            }
+            else if (shipmentEntity.Postal != null &&
+                     (PostalServiceType) shipmentEntity.Postal.Service == PostalServiceType.MediaMail)
+            {
+                carrierCode = "uspsmm";
+            }
+            else
+            {
+                // Use the default carrier for other Endicia types
+                carrierCode = "usps";
             }
 
             return carrierCode;
