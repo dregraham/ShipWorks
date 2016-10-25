28309d0a2079bb3b6f2610c62488d2b0ddb885c6
Tim Hughes <t.hughes@shipworks.com>
2016-10-25 10:11:51 -0500

    TP#15012: Originally was using a temp table to store the data to insert into each filter node content dirty table.  During perf testing, it was found that this caused importing to go slower.  Removed the temp table, and importing orders was just as fast as before.

--- Code/ShipWorks.SqlServer/Filters/DirtyCounts/FilterNodeContentDirtyUtility.cs
+++ Code/ShipWorks.SqlServer/Filters/DirtyCounts/FilterNodeContentDirtyUtility.cs
@@ -166,18 +166,11 @@ namespace ShipWorks.SqlServer.Filters.DirtyCounts
             using (SqlCommand cmd = con.CreateCommand())
             {
                 cmd.CommandText = string.Format(@"
-                        IF OBJECT_ID('tempdb..#FNCDTemp') IS NOT NULL DROP TABLE #FNCDTemp
+                    INSERT INTO FilterNodeContentDirty (ObjectID, ParentID, ObjectType, ComputerID, ColumnsUpdated)
+                      SELECT {0}, {1}, @type, @computerID, @columns FROM {2}
 
-                        SELECT {0} as [ObjectID], {1} as [ParentID], @type as [ObjectType], @computerID as [ComputerID], @columns as [ColumnsUpdated] 
-                            INTO #FNCDTemp FROM {2} 
-
-                        INSERT INTO FilterNodeContentDirty (ObjectID, ParentID, ObjectType, ComputerID, ColumnsUpdated)
-                          SELECT [ObjectID], [ParentID], [ObjectType], [ComputerID], [ColumnsUpdated]  FROM #FNCDTemp
-
-                        INSERT INTO QuickFilterNodeContentDirty (ObjectID, ParentID, ObjectType, ComputerID, ColumnsUpdated)
-                          SELECT [ObjectID], [ParentID], [ObjectType], [ComputerID], [ColumnsUpdated]  FROM #FNCDTemp
-            
-                        DROP TABLE tempdb..#FNCDTemp
+                    INSERT INTO QuickFilterNodeContentDirty (ObjectID, ParentID, ObjectType, ComputerID, ColumnsUpdated)
+                      SELECT {0}, {1}, @type, @computerID, @columns FROM {2}
                     ",
                     primaryKey, parentID, table);
 
