927a84891b4bc5ff3a2c4c9480000a4db01a95f4
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-11-07 16:24:22 -0600

    TP#15512 create GlobalPostServiceAvailability enum

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Usps/GlobalPostServiceAvailability.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Usps
+{
+    /// <summary>
+    /// GlobalPost services we support
+    /// </summary>
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum GlobalPostServiceAvailability
+    {
+        None = 0,
+        GlobalPost = 1,
+        SmartSaver = 2
+    }
+}
