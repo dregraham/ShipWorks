9eed5e716014773a4c2fdf6a857d85cb17b6a8b0
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-11-15 15:47:52 -0600

    TP#15584 create IMagentoProbe

---
+++ Code/ShipWorks.Stores/Platforms/Magento/IMagentoProbe.cs
@@ -0,0 +1,15 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Stores.Platforms.Magento
+{
+    /// <summary>
+    /// Interface that represents a probe of a Magento store for compatibility
+    /// </summary>
+    public interface IMagentoProbe
+    {
+        /// <summary>
+        /// Probe the given Magento store to see if it is compatible
+        /// </summary>
+        bool IsCompatible(MagentoStoreEntity store);
+    }
+}
