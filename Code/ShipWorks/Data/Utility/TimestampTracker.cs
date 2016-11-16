1125beeb8169f243d44fd2cd7d9d0036411496fd
Andrew Benz <a.benz@shipworks.com>
2016-11-16 11:32:29 -0600

    TP#13595: Fix crash when updating timestamp during lost SQL connection

--- Code/ShipWorks/Data/Utility/TimestampTracker.cs
+++ Code/ShipWorks/Data/Utility/TimestampTracker.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Data.Common;
 using System.Data.SqlClient;
 using Interapptive.Shared.Data;
+using log4net;
 using ShipWorks.Data.Administration.Retry;
 using ShipWorks.Data.Connection;
 
@@ -11,31 +13,18 @@ namespace ShipWorks.Data.Utility
     /// </summary>
     public class TimestampTracker
     {
+        static ILog log = LogManager.GetLogger(typeof(TimestampTracker));
         long timestamp = 0;
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public TimestampTracker()
-        {
-
-        }
 
         /// <summary>
         /// String representation of the class is the timestamp value.
         /// </summary>
-        public override string ToString()
-        {
-            return Timestamp.ToString();
-        }
+        public override string ToString() => Timestamp.ToString();
 
         /// <summary>
         /// The current timestamp value
         /// </summary>
-        public long Timestamp
-        {
-            get { return timestamp; }
-        }
+        public long Timestamp => timestamp;
 
         /// <summary>
         /// Gets the latest timestamp value from the database, and returns true if it is different.
@@ -45,17 +34,18 @@ namespace ShipWorks.Data.Utility
             using (DbConnection con = SqlSession.Current.OpenConnection())
             {
                 long current = 0;
-                object value = null;
 
-                SqlAdapterRetry<SqlException> sqlAdapterRetry = new SqlAdapterRetry<SqlException>(3, -5, "TimestapTracker.CheckForChange");
-                sqlAdapterRetry.ExecuteWithRetry(() => value = DbCommandProvider.ExecuteScalar(con, "SELECT CAST(@@DBTS AS BIGINT)"));
-
-                // During a reconnect or after an upgrade, the timestamp is sometimes returned as null
-                // If that happens, we'll assume no changes for the moment and let the next run through
-                // of the heartbeat pick it up.
-                if (value is long)
+                try
                 {
-                    current = (long) value;
+                    SqlAdapterRetry<SqlException> sqlAdapterRetry = new SqlAdapterRetry<SqlException>(3, -5, "TimestapTracker.CheckForChange");
+                    sqlAdapterRetry.ExecuteWithRetry(() => current = DbCommandProvider.ExecuteScalar<long>(con, "SELECT CAST(@@DBTS AS BIGINT)"));
+                }
+                catch (SystemException ex)
+                {
+                    // During a reconnect or after an upgrade, ExecuteScalar can throw an
+                    // InvalidOperationException or InvalidCastException. If that happens, we'll assume no changes
+                    // for the moment and let the next run through of the heartbeat pick it up.
+                    log.Error("Could not update timestamp", ex);
                 }
 
                 if (current > timestamp)
@@ -63,10 +53,8 @@ namespace ShipWorks.Data.Utility
                     timestamp = current;
                     return true;
                 }
-                else
-                {
-                    return false;
-                }
+
+                return false;
             }
         }
 
