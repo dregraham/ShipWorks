8e23bf7fba91d006be9fc600781180e2015c962a
Chris Hicks <c.hicks@shipworks.com>
2016-12-02 13:40:07 -0600

    TP#14207: Fix style issue in MagentoAccountSettingsControl and MagentoStoreSetupControl. Making clicking help link in Magento wizard page actually open link.

--- Code/ShipWorks.Stores.UI/Platforms/Magento/WizardPages/MagentoStoreSetupControl.xaml.cs
+++ Code/ShipWorks.Stores.UI/Platforms/Magento/WizardPages/MagentoStoreSetupControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace ShipWorks.Stores.UI.Platforms.Magento.WizardPages
+﻿using System.Diagnostics;
+using System.Windows.Navigation;
+
+namespace ShipWorks.Stores.UI.Platforms.Magento.WizardPages
 {
     /// <summary>
     /// Interaction logic for MagentoStoreSetupWizardPage.xaml
@@ -8,6 +11,15 @@
         public MagentoStoreSetupControl()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Opens the link in browser
+        /// </summary>
+        private void HyperlinkOnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
