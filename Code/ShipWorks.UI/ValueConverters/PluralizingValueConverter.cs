262c26a6fd1d25a28334db805090ff6767a637e3
Andrew Benz <a.benz@shipworks.com>
2016-12-06 12:02:27 -0600

    TP#15611: Add missing comments to pluralizing value converter

--- Code/ShipWorks.UI/ValueConverters/PluralizingValueConverter.cs
+++ Code/ShipWorks.UI/ValueConverters/PluralizingValueConverter.cs
@@ -4,8 +4,18 @@ using System.Windows.Data;
 
 namespace ShipWorks.UI.ValueConverters
 {
+    /// <summary>
+    /// Return the parameter, adding an 's' if value is greater than 1
+    /// </summary>
+    /// <remarks>
+    /// This is a very simplistic pluralizer. There are packages that will do a much better job with words that
+    /// need something other than an 's' appended, but for the immediate need, that was out of scope.
+    /// </remarks>
     public class PluralizingValueConverter : IValueConverter
     {
+        /// <summary>
+        /// Convert the number into a possibly pluralized string
+        /// </summary>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             long numericValue = System.Convert.ToInt64(value);
@@ -14,6 +24,9 @@ namespace ShipWorks.UI.ValueConverters
             return numericValue > 1 ? text + "s" : text;
         }
 
+        /// <summary>
+        /// Don't support converting back
+        /// </summary>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException("Cannot convert back");
