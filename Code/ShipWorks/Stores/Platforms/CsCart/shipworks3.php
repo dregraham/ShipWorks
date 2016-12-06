18e895b1e2b4df4a53d06b194aec59618a402b12
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-12-06 12:34:41 -0600

    UP#SW-CR-335 pass message to die so that we dont have to use echo

--- Code/ShipWorks/Stores/Platforms/CsCart/shipworks3.php
+++ Code/ShipWorks/Stores/Platforms/CsCart/shipworks3.php
@@ -23,8 +23,7 @@ define('REQUIRE_SECURE', true);
 
 $php_value = phpversion();
 if (version_compare($php_value, '5.3.0') == -1) {
-    echo 'Currently installed PHP version (' . $php_value . ') is not supported. Minimal required PHP version is  5.3.0.';
-    die();
+    die('Currently installed PHP version (' . $php_value . ') is not supported. Minimal required PHP version is  5.3.0.');
 }
 
 define('AREA', 'A');
