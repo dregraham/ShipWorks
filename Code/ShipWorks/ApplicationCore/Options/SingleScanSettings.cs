c004a6dbcfb46fc2a72e7a8b86cb8ac3ef8e0006
Chris Hicks <c.hicks@shipworks.com>
2016-12-09 09:28:04 -0600

    TP#15681: Create SingleScanSettings enum

---
+++ Code/ShipWorks/ApplicationCore/Options/SingleScanSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace ShipWorks.ApplicationCore.Options
+{
+    public enum SingleScanSettings
+    {
+        [Description("Single scan disabled")]
+        Disabled = 0,
+
+        [Description("Single scan enabled")]
+        Scan = 1,
+
+        [Description("Single scan with auto print enabled")]
+        AutoPrint = 2
+    }
+}
