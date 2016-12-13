556d75a20c23884768965ab3b67c34ccd292a170
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-12-13 13:03:14 -0600

    Fix Magento Connect issue when url has a trailing slash

--- Code/ShipWorks/Stores/Platforms/Magento/MagentoConnectWebClient.cs
+++ Code/ShipWorks/Stores/Platforms/Magento/MagentoConnectWebClient.cs
@@ -86,7 +86,7 @@ namespace ShipWorks.Stores.Platforms.Magento
         private string GetServiceUrl()
         {
             Uri storeUri = new Uri(Store.ModuleUrl);
-            return $"{storeUri.AbsoluteUri}index.php/api/soap/index/";
+            return $"{storeUri.AbsoluteUri.TrimEnd('/')}/index.php/api/soap/index/";
         }
 
         /// <summary>
