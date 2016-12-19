841bec07007c39fc65d4bc063b0e58f99b63545d
Chris Hicks <c.hicks@shipworks.com>
2016-12-19 15:18:56 -0600

    TP#15957: Create IFilterDefinitionProvider

---
+++ Code/ShipWorks/Filters/Search/IFilterDefinitionProvider.cs
@@ -0,0 +1,15 @@
+﻿using ShipWorks.Filters.Content;
+
+namespace ShipWorks.Filters.Search
+{
+    /// <summary>
+    /// Class for creating filter definitions
+    /// </summary>
+    public interface IFilterDefinitionProvider
+    {
+        /// <summary>
+        /// Gets a filter definition based on the provided quick search string.
+        /// </summary>
+        FilterDefinition GetDefinition(string quickSearchString);
+    }
+}
