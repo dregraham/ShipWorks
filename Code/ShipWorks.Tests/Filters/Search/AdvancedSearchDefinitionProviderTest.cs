65b27e808a64bb5e3f272edb52a1b43f13a8d0d7
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-12-20 09:43:22 -0600

    TP#15960 Create AdvancedSearchDefinitionProvider and tests

---
+++ Code/ShipWorks.Tests/Filters/Search/AdvancedSearchDefinitionProviderTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using Autofac.Extras.Moq;
+using Moq;
+using ShipWorks.Filters;
+using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+using ShipWorks.Filters.Content.Conditions.Orders;
+using ShipWorks.Filters.Search;
+using ShipWorks.Tests.Shared;
+using Xunit;
+
+namespace ShipWorks.Tests.Filters.Search
+{
+    public class AdvancedSearchDefinitionProviderTest : IDisposable
+    {
+        private readonly AutoMock mock;
+
+        public AdvancedSearchDefinitionProviderTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+        }
+
+        [Fact]
+        public void GetDefinition_ReturnsNull_WhenAdvancedFilterDefinitionIsEmpty()
+        {
+            AdvancedSearchDefinitionProvider testObject = new AdvancedSearchDefinitionProvider(new FilterDefinition(FilterTarget.Orders), null);
+
+            Assert.Null(testObject.GetDefinition(""));
+        }
+
+        [Fact]
+        public void GetDefinition_GetsQuickSearchFilterDefinitionFromQuickSearchDefinitionProvider()
+        {
+            Mock<IFilterDefinitionProvider> quickFilterDefinitionProvider = mock.Mock<IFilterDefinitionProvider>();
+            quickFilterDefinitionProvider.Setup(q => q.GetDefinition(It.IsAny<string>()))
+                .Returns(new FilterDefinition(FilterTarget.Orders));
+
+            FilterDefinition advancedDefinitions = new FilterDefinition(FilterTarget.Orders)
+            {
+                RootContainer = new ConditionGroupContainer(new ConditionGroup())
+            };
+
+            advancedDefinitions.RootContainer.FirstGroup.Conditions.Add(new OrderNumberCondition
+            {
+                IsNumeric = false,
+                StringOperator = StringOperator.BeginsWith,
+                StringValue = "something"
+            });
+
+            AdvancedSearchDefinitionProvider testObject = mock.Create<AdvancedSearchDefinitionProvider>(new TypedParameter(typeof(FilterDefinition), advancedDefinitions));
+            testObject.GetDefinition("something");
+
+            quickFilterDefinitionProvider.Verify(q => q.GetDefinition("something"));
+        }
+
+        public void Dispose()
+        {
+            mock?.Dispose();
+        }
+    }
+}
---
+++ Code/ShipWorks/Filters/Search/AdvancedSearchDefinitionProvider.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+
+namespace ShipWorks.Filters.Search
+{
+    /// <summary>
+    /// Advanced Search Definition Provider
+    /// </summary>
+    public class AdvancedSearchDefinitionProvider : IFilterDefinitionProvider
+    {
+        private readonly FilterDefinition advancedFilterDefinition;
+        private readonly IFilterDefinitionProvider quickFilterDefinitionProvider;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AdvancedSearchDefinitionProvider(FilterDefinition advancedFilterDefinition,
+            IFilterDefinitionProvider quickFilterDefinitionProvider)
+        {
+            this.advancedFilterDefinition = advancedFilterDefinition;
+            this.quickFilterDefinitionProvider = quickFilterDefinitionProvider;
+        }
+
+        /// <summary>
+        /// Get the Advanced AND Quick search definition combined in a single FilterDefinition
+        /// </summary>
+        public FilterDefinition GetDefinition(string quickSearchString)
+        {
+            // If the filter definition is empty (no criteria has been selected) don't return
+            // the definition. An empty definition returns all orders or customers, which is
+            // irrelevant
+            if (advancedFilterDefinition.IsEmpty())
+            {
+                return null;
+            }
+
+            FilterDefinition quickFilterDefinition = quickFilterDefinitionProvider.GetDefinition(quickSearchString);
+
+            List<Condition> quickSearchConditions = new List<Condition>();
+            if (quickFilterDefinition?.RootContainer?.FirstGroup?.Conditions != null)
+            {
+                quickSearchConditions.AddRange(quickFilterDefinition.RootContainer.FirstGroup.Conditions);
+            }
+
+            // When quick searching the customer grid, there is no second group.
+            if (quickFilterDefinition?.RootContainer?.SecondGroup?.FirstGroup?.Conditions != null)
+            {
+                quickSearchConditions.AddRange(quickFilterDefinition.RootContainer.SecondGroup.FirstGroup.Conditions);
+            }
+
+            ConditionGroup quickSearchConditionGroup = new ConditionGroup();
+            quickSearchConditions.ForEach(quickSearchConditionGroup.Conditions.Add);
+            quickSearchConditionGroup.JoinType = ConditionJoinType.Any;
+
+            FilterDefinition combinedFilter = new FilterDefinition(advancedFilterDefinition.FilterTarget)
+            {
+                RootContainer =
+                {
+                    FirstGroup = quickSearchConditionGroup,
+                    SecondGroup = advancedFilterDefinition.RootContainer,
+                    JoinType = ConditionGroupJoinType.And
+                }
+            };
+
+            return combinedFilter;
+        }
+    }
+}
