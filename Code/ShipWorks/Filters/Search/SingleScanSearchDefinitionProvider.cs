2084d98c58776bdb1807c19dc1f7fddeb70d7479
Kevin Croke <k.croke@shipworks.com>
2016-12-21 15:28:05 -0600

    WIP: TP#15965 Created skeleton of SingleScanSearchDefinitionProvider so the next task can be worked on

---
+++ Code/ShipWorks/Filters/Search/SingleScanSearchDefinitionProvider.cs
@@ -0,0 +1,12 @@
+﻿using ShipWorks.Filters.Content;
+
+namespace ShipWorks.Filters.Search
+{
+    public class SingleScanSearchDefinitionProvider:ISearchDefinitionProvider
+    {
+        public FilterDefinition GetDefinition(string quickSearchString)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
