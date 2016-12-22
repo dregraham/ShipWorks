6ea80c7cabca9ddf27d25149efed9d6307984276
Chris Hicks <c.hicks@shipworks.com>
2016-12-22 11:26:38 -0600

    TP#15965: Implement SingleScanDefinitionProvider

--- Code/ShipWorks/Filters/Search/SingleScanSearchDefinitionProvider.cs
+++ Code/ShipWorks/Filters/Search/SingleScanSearchDefinitionProvider.cs
@@ -1,12 +1,46 @@
 ﻿using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+using ShipWorks.Filters.Content.Conditions.Orders;
 
 namespace ShipWorks.Filters.Search
 {
-    public class SingleScanSearchDefinitionProvider:ISearchDefinitionProvider
+    /// <summary>
+    /// Provides the search definition used when single scan is enabled
+    /// </summary>
+    /// <seealso cref="ShipWorks.Filters.Search.ISearchDefinitionProvider" />
+    public class SingleScanSearchDefinitionProvider : ISearchDefinitionProvider
     {
+        /// <summary>
+        /// Gets a filter definition that only searches order number
+        /// </summary>
         public FilterDefinition GetDefinition(string quickSearchString)
         {
-            throw new System.NotImplementedException();
+            FilterDefinition definition = new FilterDefinition(FilterTarget.Orders);
+
+            OrderNumberCondition condition;
+            long orderNumber;
+            if (long.TryParse(quickSearchString, out orderNumber))
+            {
+                condition = new OrderNumberCondition
+                {
+                    IsNumeric = true,
+                    Operator = NumericOperator.Equal,
+                    Value1 = orderNumber
+                };
+            }
+            else
+            {
+                condition = new OrderNumberCondition
+                {
+                    IsNumeric = false,
+                    StringOperator = StringOperator.Equals,
+                    StringValue = quickSearchString,
+                };
+            }
+            
+            definition.RootContainer.FirstGroup.Conditions.Add(condition);
+
+            return definition;
         }
     }
 }
