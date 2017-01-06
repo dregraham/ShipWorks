01ae8092cd83dafb5e95bea75a7e298665ea03d2
Andrew Benz <a.benz@shipworks.com>
2017-01-06 12:20:59 -0600

    TP#15606: Add more interfaces and stub shared concrete classes

---
+++ Code/Interapptive.Shared/Win32/User32Devices.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Interapptive.Shared.Win32.Native;
+
+namespace Interapptive.Shared.Win32
+{
+    /// <summary>
+    /// User32 wrapper for managing devices
+    /// </summary>
+    public class User32Devices : IUser32Devices
+    {
+        /// <summary>
+        /// Get a list of all raw devices
+        /// </summary>
+        public RawInputDeviceListItem[] GetAllRawDevices()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Get information about a device
+        /// </summary>
+        public RawInputDeviceInfo GetDeviceInfo(RawInputDeviceListItem device)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Code/ShipWorks/Common/IO/Hardware/Scanner/ScannerService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using ShipWorks.ApplicationCore;
+
+namespace ShipWorks.Common.IO.Hardware.Scanner
+{
+    /// <summary>
+    /// Main entry point for interacting with scanners
+    /// </summary>
+    public class ScannerService : IScannerService, IInitializeForCurrentUISession
+    {
+        /// <summary>
+        /// Get the state of the current scanner
+        /// </summary>
+        public ScannerState CurrentScannerState
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Begin finding a current scanner
+        /// </summary>
+        public void BeginFindScanner()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Disable scanner handling
+        /// </summary>
+        public void Disable()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Enable scanner handling
+        /// </summary>
+        public void Enable()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// End finding the current scanner
+        /// </summary>
+        public void EndFindScanner()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// End the current session
+        /// </summary>
+        public void EndSession()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initialize for the current session
+        /// </summary>
+        public void InitializeForCurrentSession()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Dispose resources
+        /// </summary>
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
