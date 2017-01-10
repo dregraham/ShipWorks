32240d4d543a3fbd0d0bd842b3cd9b066fb4b3bc
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-01-10 14:30:08 -0600

    TP#SW-CR-364 add method comments

--- Code/ShipWorks.SingleScan/ScannerConfigurationRepositoryException.cs
+++ Code/ShipWorks.SingleScan/ScannerConfigurationRepositoryException.cs
@@ -7,22 +7,27 @@ namespace ShipWorks.SingleScan
     /// </summary>
     public class ScannerConfigurationRepositoryException : Exception
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public ScannerConfigurationRepositoryException()
         {
-
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public ScannerConfigurationRepositoryException(string message)
             : base(message)
         {
-
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public ScannerConfigurationRepositoryException(string message, Exception innerException)
             : base(message, innerException)
         {
-
-
         }
     }
 }
