6c8defb09450933ccd073abea95180f1768489af
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-01-19 11:08:28 -0600

    remove unneeded obfuscation attribute

--- Code/ShipWorks.SingleScan/ScannerRegistrationDialog.cs
+++ Code/ShipWorks.SingleScan/ScannerRegistrationDialog.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-using System.Windows;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using ShipWorks.ApplicationCore.ComponentRegistration;
 using ShipWorks.Common.IO.Hardware.Scanner;
 
@@ -9,7 +7,6 @@ namespace ShipWorks.SingleScan
     /// <summary>
     /// Dialog to prompt user to register scanner.
     /// </summary>
-    [Obfuscation(Exclude = true)]
     [NamedComponent("ScannerRegistrationDialog", typeof(Form))]
     public partial class ScannerRegistrationDialog : Form
     {
