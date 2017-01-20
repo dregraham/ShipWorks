775a9954f2800aca0976f200202fd5552780a8a2
Chris Hicks <c.hicks@shipworks.com>
2017-01-20 09:07:42 -0600

    TP#16375: TP#16376: Create ISingleScanOrderPrefix and SingleScanOrderPrefix.

---
+++ Code/ShipWorks.SingleScan/SingleScanOrderPrefix.cs
@@ -0,0 +1,31 @@
+﻿using ShipWorks.Filters.Search;
+
+namespace ShipWorks.SingleScan
+{
+    /// <summary>
+    /// Prefix that identifies a scan result as a ShipWorks order
+    /// </summary>
+    /// <seealso cref="ShipWorks.Filters.Search.ISingleScanOrderPrefix" />
+    public class SingleScanOrderPrefix : ISingleScanOrderPrefix
+    {
+        public const string Prefix = "SW-O-";
+
+        /// <summary>
+        /// Scan result that is displayed to user
+        /// </summary>
+        public string DisplayText { get; set; }
+
+        /// <summary>
+        /// Actual scan result
+        /// </summary>
+        public string OriginalSearchText { get; set; }
+
+        /// <summary>
+        /// Whether or not the scan result begins with the ShipWorks order prefix
+        /// </summary>
+        public bool Contains()
+        {
+            return OriginalSearchText.StartsWith(Prefix);
+        }
+    }
+}
---
+++ Code/ShipWorks/Filters/Search/ISingleScanOrderPrefix.cs
@@ -0,0 +1,23 @@
+﻿namespace ShipWorks.Filters.Search
+{
+    /// <summary>
+    /// Prefix that identifies a scan result as a ShipWorks order
+    /// </summary>
+    public interface ISingleScanOrderPrefix
+    {
+        /// <summary>
+        /// Scan result that is displayed to user
+        /// </summary>
+        string DisplayText { get; set; }
+
+        /// <summary>
+        /// Actual scan result
+        /// </summary>
+        string OriginalSearchText { get; set; }
+
+        /// <summary>
+        /// Whether or not the scan result starts with the ShipWorks order prefix
+        /// </summary>
+        bool Contains();
+    }
+}
