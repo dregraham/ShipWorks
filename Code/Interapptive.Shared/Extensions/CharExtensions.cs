ea7cc9a31ba278c219519887c6c41fce886be588
Chris Hicks <c.hicks@shipworks.com>
2017-01-23 12:22:57 -0600

    TP#16269: Add method to StringUtility for removing symbols. Use new method to remove symbols from the service in SparkPayShipmentFactory. Fix spelling error in method name.

---
+++ Code/Interapptive.Shared/Extensions/CharExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Interapptive.Shared.Extensions
+{
+    /// <summary>
+    /// Char type extension methods
+    /// </summary>
+    public static class CharExtensions
+    {
+        /// <summary>
+        /// Returns a string representation of the given chars
+        /// </summary>
+        public static string CreateString(this IEnumerable<char> value) => new string(value.ToArray());
+    }
+}
