8e1b7734b73bfb54af00096e5fe1c8c5354857c0
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-01-25 13:01:27 -0600

    TP#16459 Create IAutoPrintConfirmationDlgFactory and register ISingleScanShipmentConfirmationService

--- Code/ShipWorks/Shipping/Services/IAutoPrintConfirmationDlgFactory.cs
+++ Code/ShipWorks/Shipping/Services/IAutoPrintConfirmationDlgFactory.cs
@@ -2,6 +2,9 @@
 
 namespace ShipWorks.Shipping.Services
 {
+    /// <summary>
+    /// Interface that represents a factory for creating the AutoPrintConfirmationDlg
+    /// </summary>
     public interface IAutoPrintConfirmationDlgFactory
     {
         /// <summary>
@@ -9,6 +12,7 @@ namespace ShipWorks.Shipping.Services
         /// </summary>
         /// <param name="scanMessageText">The scan message that the dialog is listening for to confirm and close</param>
         /// <param name="displayText">The text that is displayed to the user</param>
-        IDialog Create(string scanMessageText, string displayText);
+        /// <param name="title">The title of the dialog</param>
+        IDialog Create(string scanMessageText, string displayText, string title);
     }
 }
--- Code/ShipWorks/Shipping/Services/ISingleScanShipmentConfirmationService.cs
+++ Code/ShipWorks/Shipping/Services/ISingleScanShipmentConfirmationService.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using ShipWorks.ApplicationCore.ComponentRegistration;
 using ShipWorks.Data.Model.EntityClasses;
 
 namespace ShipWorks.Shipping.Services
 {
+    [Service]
     public interface ISingleScanShipmentConfirmationService
     {
         Task<IEnumerable<ShipmentEntity>> GetShipments(long OrderId, string scannedBarcode);
