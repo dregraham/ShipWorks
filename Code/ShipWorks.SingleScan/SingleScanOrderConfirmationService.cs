27e62acabb012fd7d3fb11ed0330245f87381be4
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-01-30 13:53:21 -0600

    TP#16522 Create SingleScanOrderConfirmationService

---
+++ Code/ShipWorks.SingleScan/SingleScanOrderConfirmationService.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows.Forms;
+using Interapptive.Shared.UI;
+using ShipWorks.ApplicationCore.ComponentRegistration;
+using ShipWorks.Shipping.Services;
+using ShipWorks.Stores;
+
+namespace ShipWorks.SingleScan
+{
+    /// <summary>
+    /// Confirm what the user wants to do when auto printing and multiple matching orders are found
+    /// </summary>
+    [Component]
+    public class SingleScanOrderConfirmationService : ISingleScanOrderConfirmationService
+    {
+        private readonly IAutoPrintConfirmationDlgFactory dlgFactory;
+        private readonly IMessageHelper messageHelper;
+        private readonly IStoreManager storeManager;
+
+        public SingleScanOrderConfirmationService(IAutoPrintConfirmationDlgFactory dlgFactory, IMessageHelper messageHelper, IStoreManager storeManager)
+        {
+            this.dlgFactory = dlgFactory;
+            this.messageHelper = messageHelper;
+            this.storeManager = storeManager;
+        }
+
+        /// <summary>
+        /// Confirms that the order with the given orderId should be printed.
+        /// </summary>
+        public bool Confirm(long orderId, int numberOfMatchedOrders, string scanText)
+        {
+            if (numberOfMatchedOrders > 1)
+            {
+                return messageHelper.ShowDialog(() => dlgFactory.Create(scanText, GetMessageingText(orderId, numberOfMatchedOrders))) == DialogResult.OK;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Generate the message displayed to the user
+        /// </summary>
+        private MessagingText GetMessageingText(long orderId, int numberOfMatchedOrders)
+        {
+            string store = storeManager.GetRelatedStore(orderId)?.StoreName;
+
+            return new MessagingText
+            {
+                Title = "Multiple Matches Found",
+                Body = $"Your order number matches {numberOfMatchedOrders} orders. The most recent order is from store '{store}'. Scan the barcode again or click 'Use Most Recent Order'",
+                Continue = "Use Most Recent Order"
+            };
+
+        }
+    }
+}
