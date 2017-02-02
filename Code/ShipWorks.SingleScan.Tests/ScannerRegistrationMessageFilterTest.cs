98ecb3d080278f5c099c08f2570aa769c790b63d
Tim Hughes <t.hughes@shipworks.com>
2017-02-02 10:03:54 -0600

    TP#15609: One of the scanners was sending control characters before and after the actual scan text, so we are now ignoring those.

--- Code/ShipWorks.SingleScan.Tests/ScannerRegistrationMessageFilterTest.cs
+++ Code/ShipWorks.SingleScan.Tests/ScannerRegistrationMessageFilterTest.cs
@@ -76,7 +76,7 @@ namespace ShipWorks.SingleScan.Tests
             var message = new Message { Msg = (int) WindowsMessage.INPUT };
             testObject.PreFilterMessage(ref message);
 
-            mock.Mock<IUser32Input>().Verify(x => x.GetCharactersFromKeys(VirtualKeys.X, false, false));
+            mock.Mock<IUser32Input>().Verify(x => x.GetCharactersFromKeys(VirtualKeys.X, false, false, false));
         }
 
         [Fact]
@@ -91,7 +91,22 @@ namespace ShipWorks.SingleScan.Tests
             testObject.PreFilterMessage(ref message);
             testObject.PreFilterMessage(ref message);
 
-            mock.Mock<IUser32Input>().Verify(x => x.GetCharactersFromKeys(VirtualKeys.X, true, false));
+            mock.Mock<IUser32Input>().Verify(x => x.GetCharactersFromKeys(VirtualKeys.X, true, false, false));
+        }
+
+        [Fact]
+        public void PreFilterMessage_DelegatesToUser32Input_ToGetCharactersWhenControlIsHeld()
+        {
+            mock.Mock<IUser32Input>()
+                .SetupSequence(x => x.GetRawInputData(It.IsAny<IntPtr>(), It.IsAny<RawInputCommand>()))
+                .Returns(CreateRawInputData(WindowsMessage.KEYFIRST, VirtualKeys.Control))
+                .Returns(CreateRawInputData(WindowsMessage.KEYFIRST, VirtualKeys.X));
+
+            var message = new Message { Msg = (int)WindowsMessage.INPUT };
+            testObject.PreFilterMessage(ref message);
+            testObject.PreFilterMessage(ref message);
+
+            mock.Mock<IUser32Input>().Verify(x => x.GetCharactersFromKeys(VirtualKeys.X, false, true, false));
         }
 
         [Fact]
@@ -101,7 +116,7 @@ namespace ShipWorks.SingleScan.Tests
                 .Setup(x => x.GetRawInputData(It.IsAny<IntPtr>(), It.IsAny<RawInputCommand>()))
                 .Returns(CreateRawInputData(WindowsMessage.KEYFIRST, VirtualKeys.X));
             mock.Mock<IUser32Input>()
-                .Setup(x => x.GetCharactersFromKeys(It.IsAny<VirtualKeys>(), It.IsAny<bool>(), false))
+                .Setup(x => x.GetCharactersFromKeys(It.IsAny<VirtualKeys>(), It.IsAny<bool>(), It.IsAny<bool>(), false))
                 .Returns("X");
 
             var message = new Message { Msg = (int) WindowsMessage.INPUT };
