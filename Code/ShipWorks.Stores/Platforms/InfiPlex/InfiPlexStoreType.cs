a6691f743a75198e4793b5c4fffcdc4e374fe14d
Tim Hughes <t.hughes@shipworks.com>
2017-02-07 16:27:12 -0600

    TP#16565: Added InfiPlexStoreType and registration module.

---
+++ Code/ShipWorks.Stores/Platforms/InfiPlex/InfiPlexStoreType.cs
@@ -0,0 +1,35 @@
+﻿using ShipWorks.ApplicationCore.Logging;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.GenericModule;
+
+namespace ShipWorks.Stores.Platforms.InfiPlex
+{
+    /// <summary>
+    /// InfiPlex store specific integration into ShipWorks
+    /// </summary>
+    public class InfiPlexStoreType : GenericModuleStoreType
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public InfiPlexStoreType(StoreEntity store) : base(store)
+        {
+        }
+
+        /// <summary>
+        /// StoreType enum value
+        /// </summary>
+        public override StoreTypeCode TypeCode => StoreTypeCode.InfiPlex;
+
+        /// <summary>
+        /// Log request/responses as InfiPlex
+        /// </summary>
+        public override ApiLogSource LogSource => ApiLogSource.InfiPlex;
+
+        /// <summary>
+        /// Gets the account settings help URL.
+        /// </summary>
+        public override string AccountSettingsHelpUrl => 
+            "http://support.shipworks.com/support/solutions/articles/4000097090-adding-an-infiplex-store";
+    }
+}
