d0a0ee2ae27c53abc553c6e2895e39eb59765679
Tim Hughes <t.hughes@shipworks.com>
2017-02-07 15:21:16 -0600

    TP#16450: Added SellerCloudStoreType

---
+++ Code/ShipWorks.Stores/Platforms/SellerCloud/SellerCloudStoreType.cs
@@ -0,0 +1,34 @@
+﻿using ShipWorks.ApplicationCore.Logging;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.GenericModule;
+
+namespace ShipWorks.Stores.Platforms.SellerCloud
+{
+    /// <summary>
+    /// SellerCloud generic module store implementation
+    /// </summary>
+    public class SellerCloudStoreType : GenericModuleStoreType
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public SellerCloudStoreType(StoreEntity store) : base(store)
+        {
+        }
+
+        /// <summary>
+        /// StoreTypeCode enum value
+        /// </summary>
+        public override StoreTypeCode TypeCode => StoreTypeCode.SellerCloud;
+
+        /// <summary>
+        /// Log request/responses as SellerCloud
+        /// </summary>
+        public override ApiLogSource LogSource => ApiLogSource.SellerCloud;
+
+        /// <summary>
+        /// Gets the account settings help URL.
+        /// </summary>
+        public override string AccountSettingsHelpUrl => "http://support.shipworks.com/support/solutions/articles/4000097089-adding-a-sellercloud-store";
+    }
+}
