f68e76e54e32f5946edf6d8a92c34af48a3384c4
Chris Hicks <c.hicks@shipworks.com>
2017-02-07 16:50:14 -0600

    SW-CR-406: Class comments

--- Code/ShipWorks.SingleScan/SingleScanOrderConfirmationServiceTelemetryWrapper.cs
+++ Code/ShipWorks.SingleScan/SingleScanOrderConfirmationServiceTelemetryWrapper.cs
@@ -3,15 +3,26 @@ using ShipWorks.Shipping.Services;
 
 namespace ShipWorks.SingleScan
 {
+    /// <summary>
+    /// Collects telemetry data from SingleScanOrderConfirmationService
+    /// </summary>
+    /// <seealso cref="ShipWorks.Shipping.Services.ISingleScanOrderConfirmationService" />
     public class SingleScanOrderConfirmationServiceTelemetryDecorator : ISingleScanOrderConfirmationService
     {
         private readonly ISingleScanOrderConfirmationService singleScanOrderConfirmationService;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SingleScanOrderConfirmationServiceTelemetryDecorator"/> class.
+        /// </summary>
+        /// <param name="singleScanOrderConfirmationService">The single scan order confirmation service.</param>
         public SingleScanOrderConfirmationServiceTelemetryDecorator(ISingleScanOrderConfirmationService singleScanOrderConfirmationService)
         {
             this.singleScanOrderConfirmationService = singleScanOrderConfirmationService;
         }
 
+        /// <summary>
+        /// Confirms that the order with the given orderId should be printed, collecting desired telemetry data
+        /// </summary>
         public bool Confirm(long orderId, int numberOfMatchedOrders, string scanText)
         {
             using (TrackedDurationEvent orderCollisionTrackedDurationEvent =
