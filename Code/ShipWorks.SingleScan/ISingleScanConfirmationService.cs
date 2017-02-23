9494da3fdcbc1c8a7811201247a826ec0e4abff2
Kevin Croke <k.croke@shipworks.com>
2017-02-23 17:03:51 -0600

    TP#1496 Created SingleScanConrirmationService - A facade for all the SS services.

---
+++ Code/ShipWorks.SingleScan/ISingleScanConfirmationService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Interapptive.Shared.Metrics;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.SingleScan
+{
+    public interface ISingleScanConfirmationService
+    {
+        Task<bool> Apply(IEnumerable<ShipmentEntity> shipments, ITrackedDurationEvent trackedDurationEvent);
+        bool ConfirmOrders(long orderId, int numberOfMatchedOrders, string scanText);
+        Task<IEnumerable<ShipmentEntity>> GetShipments(long orderId, string scannedBarcode);
+    }
+}
