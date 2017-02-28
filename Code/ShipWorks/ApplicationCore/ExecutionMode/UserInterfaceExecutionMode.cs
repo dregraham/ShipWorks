49c744a62c1b5e47205820bbabd2f223ba27a75c
Kevin Croke <k.croke@shipworks.com>
2017-02-28 10:13:58 -0600

    TP#1496 Add comment per code review

--- Code/ShipWorks/ApplicationCore/ExecutionMode/UserInterfaceExecutionMode.cs
+++ Code/ShipWorks/ApplicationCore/ExecutionMode/UserInterfaceExecutionMode.cs
@@ -148,6 +148,8 @@ namespace ShipWorks.ApplicationCore.ExecutionMode
             // First do base/common initialization
             base.Initialize();
 
+            // This kicks off scale detection when SW starts instead of waiting for the the scale to initialize the first time 
+            // it is used which can take upwards to 1 minute.
             ScaleReader.Initialize();
 
             // Initialize window state
