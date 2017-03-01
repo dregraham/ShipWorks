199aa7ab72bda93bed7c567a657e19b89342a411
Kevin Croke <k.croke@shipworks.com>
2017-03-01 14:29:12 -0600

    TP#1496 Fixed unit tests

--- Code/ShipWorks.SingleScan.Tests/LoggableAutoPrintServiceTest.cs
+++ Code/ShipWorks.SingleScan.Tests/LoggableAutoPrintServiceTest.cs
@@ -1,64 +1,65 @@
-﻿#pragma warning disable S125 // Sections of code should not be "commented out"
-//using Autofac.Extras.Moq;
-//using Interapptive.Shared.Utility;
-//using log4net;
-//using Moq;
-//using ShipWorks.Messaging.Messages.SingleScan;
-//using ShipWorks.Tests.Shared;
-//using System;
-//using System.Threading.Tasks;
-//using Xunit;
+﻿using Autofac.Extras.Moq;
+using Interapptive.Shared.Utility;
+using log4net;
+using Moq;
+using ShipWorks.Messaging.Messages.SingleScan;
+using ShipWorks.Tests.Shared;
+using System;
+using System.Threading.Tasks;
+using Xunit;
 
-//namespace ShipWorks.SingleScan.Tests
-//{
-//    public class LoggableAutoPrintServiceTest : IDisposable
-//    {
-//        readonly AutoMock mock;
-//        readonly Mock<IAutoPrintService> autoPrintService;
-//        readonly Mock<ILog> logger;
-//        readonly LoggableAutoPrintService testObject;
+namespace ShipWorks.SingleScan.Tests
+{
+    public class LoggableAutoPrintServiceTest : IDisposable
+    {
+        readonly AutoMock mock;
+        readonly Mock<IAutoPrintService> autoPrintService;
+        readonly Mock<ILog> logger;
+        readonly LoggableAutoPrintService testObject;
 
-//        public LoggableAutoPrintServiceTest()
-//        {
-//            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
-//            autoPrintService = mock.Mock<IAutoPrintService>();
-//            logger = mock.Mock<ILog>();
+        public LoggableAutoPrintServiceTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+            autoPrintService = mock.Mock<IAutoPrintService>();
+            logger = mock.Mock<ILog>();
 
-//            testObject = mock.Create<LoggableAutoPrintService>();
-//        }
+            testObject = mock.Create<LoggableAutoPrintService>();
+        }
 
-//        [Fact]
-//        public void AllowAutoPrint_DelegatesToAutoPrintService_AllowAutoPrint()
-//        {
-//            ScanMessage scanMessage = new ScanMessage();
-//            testObject.AllowAutoPrint(scanMessage);
+        [Fact]
+        public void AllowAutoPrint_DelegatesToAutoPrintService_AllowAutoPrint()
+        {
+            ScanMessage scanMessage = new ScanMessage();
+            testObject.AllowAutoPrint(scanMessage);
 
-//            autoPrintService.Verify(a => a.AllowAutoPrint(scanMessage));
-//        }
+            autoPrintService.Verify(a => a.AllowAutoPrint(scanMessage));
+        }
 
-//        [Fact]
-//        public async void Print_DelegatesToAutoPrintService_Prit()
-//        {
-//            AutoPrintServiceDto autoprintdto = new AutoPrintServiceDto();
-//            await testObject.Print(new AutoPrintServiceDto());
+        [Fact]
+        public async void Print_DelegatesToAutoPrintService_Prit()
+        {
+            AutoPrintServiceDto autoprintdto = new AutoPrintServiceDto();
+            await testObject.Print(new AutoPrintServiceDto());
 
-//            autoPrintService.Verify(a => a.Print(autoprintdto));
-//        }
+            autoPrintService.Verify(a => a.Print(autoprintdto));
+        }
 
-//        [Fact]
-//        public async void Print_LogsError_WhenAutoPrintServiceReturnsFailure()
-//        {
-//            string error = "something went wrong";
-//            autoPrintService.Setup(a => a.Print(It.IsAny<AutoPrintServiceDto>())).Returns(Task.FromResult(GenericResult.FromError<string>(error)));
+        [Fact]
+        public async void Print_LogsError_WhenAutoPrintServiceDoesNotSendProcessShipmentsMessage()
+        {
+            string error = "something went wrong";
+            autoPrintService
+                .Setup(a => a.Print(It.IsAny<AutoPrintServiceDto>()))
+                .ReturnsAsync(new AutoPrintResult("blah", 42, error, false));
 
-//            await testObject.Print(new AutoPrintServiceDto());
+            await testObject.Print(new AutoPrintServiceDto());
 
-//            logger.Verify(l => l.Error(error));
-//        }
+            logger.Verify(l => l.Error(error));
+        }
 
-//        public void Dispose()
-//        {
-//            mock?.Dispose();
-//        }
-//    }
-//}
+        public void Dispose()
+        {
+            mock?.Dispose();
+        }
+    }
+}
