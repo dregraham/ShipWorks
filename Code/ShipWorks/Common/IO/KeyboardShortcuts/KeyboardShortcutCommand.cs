0abdb40aad8eb1512cb4a1fc317aab38548d038d
Andrew Benz <a.benz@shipworks.com>
2017-03-07 12:55:21 -0600

    TP#16826: Add keyboard shortcut command enum

---
+++ Code/ShipWorks/Common/IO/KeyboardShortcuts/KeyboardShortcutCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Common.IO.KeyboardShortcuts
+{
+    /// <summary>
+    /// List of keyboard shortcut commands
+    /// </summary>
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum KeyboardShortcutCommand
+    {
+        /// <summary>
+        /// Apply the weight in a scale control
+        /// </summary>
+        [Description("Apply weight")]
+        ApplyWeight
+    }
+}
