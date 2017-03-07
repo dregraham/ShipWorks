d1793484950b86a607808164402db40f20848a4c
Andrew Benz <a.benz@shipworks.com>
2017-03-07 13:11:15 -0600

    TP#16826: Add keyboard shortcut translator

---
+++ Code/ShipWorks/Common/IO/KeyboardShortcuts/IKeyboardShortcutTranslator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Interapptive.Shared.Messaging;
+using Interapptive.Shared.Win32.Native;
+
+namespace ShipWorks.Common.IO.KeyboardShortcuts
+{
+    /// <summary>
+    /// Translate keyboard shortcut commands and keys
+    /// </summary>
+    public interface IKeyboardShortcutTranslator
+    {
+        /// <summary>
+        /// Get a list of commands for the given keys
+        /// </summary>
+        IEnumerable<Func<object, IShipWorksMessage>> GetCommands(IEnumerable<VirtualKeys> keys);
+
+        /// <summary>
+        /// Get a list of shortcuts for the given command
+        /// </summary>
+        IEnumerable<string> GetShortcuts(KeyboardShortcutCommand command);
+    }
+}
