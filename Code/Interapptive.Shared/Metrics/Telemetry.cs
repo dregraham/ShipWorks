0b4bc243d7e4d17083bc3852efda2e3f75665fe9
Tim Hughes <t.hughes@shipworks.com>
2017-03-27 15:32:25 -0500

    TP#15005: Added parallel action queue telemetry

--- Code/Interapptive.Shared/Metrics/Telemetry.cs
+++ Code/Interapptive.Shared/Metrics/Telemetry.cs
@@ -29,6 +29,7 @@ namespace Interapptive.Shared.Metrics
         private static readonly TelemetryClient telemetryClient;
         public const string TotalShipmentsKey = "TotalShipments";
         public const string TotalSuccessfulShipmentsKey = "TotalSuccessfulShipments";
+        public const string ParallelActionQueueUsed = "ParallelActionQueueUsed";
 
         /// <summary>
         /// Static constructor
