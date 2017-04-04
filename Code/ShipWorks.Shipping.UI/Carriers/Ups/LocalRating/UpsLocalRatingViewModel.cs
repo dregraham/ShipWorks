47ff3577ffc53fa15feb6013b8644f005f1a3f69
Kevin Croke <k.croke@shipworks.com>
2017-04-04 13:35:48 -0500

    TP#16422: WIP: Add UpsLocalRatingViewModel
    Checking in early as Mirza could use the LocalRatingEnabled and UploadRatingFile command

--- Code/ShipWorks.Shipping.UI/Carriers/Ups/LocalRating/UpsLocalRatingViewModel.cs
+++ Code/ShipWorks.Shipping.UI/Carriers/Ups/LocalRating/UpsLocalRatingViewModel.cs
@@ -12,27 +12,67 @@ using ShipWorks.Shipping.Carriers.UPS.LocalRating;
 
 namespace ShipWorks.Shipping.UI.Carriers.Ups.LocalRating
 {
+    /// <summary>
+    /// ViewModel to allow users to set up LocalRating
+    /// </summary>
     [Component]
     public class UpsLocalRatingViewModel : IUpsLocalRatingViewModel
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpsLocalRatingViewModel"/> class.
+        /// </summary>
         public UpsLocalRatingViewModel()
         {
             DownloadSampleFile = new RelayCommand(DownloadSampleFileAccount);
+            UploadRatingFile = new RelayCommand(UploadRatingFileCommand);
         }
 
+        /// <summary>
+        /// Command to download the sample file
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public ICommand DownloadSampleFile { get; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [local rating enabled].
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public bool LocalRatingEnabled { get; set; }
+
+
+        /// <summary>
+        /// Gets the upload rating file.
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public ICommand UploadRatingFile { get; }
+
+        /// <summary>
+        /// Loads the UpsAccount information to the view model
+        /// </summary>
         public void Load(UpsAccountEntity upsAccount)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Saves view model information to the UpsAccount
+        /// </summary>
         public bool Save(UpsAccountEntity upsAccount)
         {
             throw new NotImplementedException();
         }
 
-        [Obfuscation(Exclude = true)]
-        public ICommand DownloadSampleFile { get; }
+        /// <summary>
+        /// Uploads the rating File.
+        /// </summary>
+        private void UploadRatingFileCommand()
+        {
+            throw new NotImplementedException();
+        }
 
+        /// <summary>
+        /// Downloads the sample file account.
+        /// </summary>
         private void DownloadSampleFileAccount()
         {
             
