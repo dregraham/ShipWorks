3fdfec3489b1774961ed447dca1a5c26e2e96a51
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-04-04 13:54:50 -0500

    TP#16422 create UpsLocalRatingControl

---
+++ Code/ShipWorks.Shipping.UI/Carriers/Ups/LocalRating/UpsLocalRatingControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShipWorks.Shipping.UI.Carriers.Ups
+{
+    /// <summary>
+    /// Interaction logic for UpsLocalRatesControl.xaml
+    /// </summary>
+    public partial class UpsLocalRatingControl : UserControl
+    {
+        public UpsLocalRatingControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
