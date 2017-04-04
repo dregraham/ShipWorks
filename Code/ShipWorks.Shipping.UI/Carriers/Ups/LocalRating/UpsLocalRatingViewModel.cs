c46572f1bfd08389b979cce6b0bb46c8266bdabc
Kevin Croke <k.croke@shipworks.com>
2017-04-04 13:26:38 -0500

    TP#16422: WIP: Add UpsLocalRatingViewModel
    Checking in early as Mirza could use the DownloadSampleFile command

---
+++ Code/ShipWorks.Shipping.UI/Carriers/Ups/LocalRating/UpsLocalRatingViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.CommandWpf;
+using ShipWorks.ApplicationCore.ComponentRegistration;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.UPS.LocalRating;
+
+namespace ShipWorks.Shipping.UI.Carriers.Ups.LocalRating
+{
+    [Component]
+    public class UpsLocalRatingViewModel : IUpsLocalRatingViewModel
+    {
+        public UpsLocalRatingViewModel()
+        {
+            DownloadSampleFile = new RelayCommand(DownloadSampleFileAccount);
+        }
+
+        public void Load(UpsAccountEntity upsAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(UpsAccountEntity upsAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Obfuscation(Exclude = true)]
+        public ICommand DownloadSampleFile { get; }
+
+        private void DownloadSampleFileAccount()
+        {
+            
+        }
+    }
+}
