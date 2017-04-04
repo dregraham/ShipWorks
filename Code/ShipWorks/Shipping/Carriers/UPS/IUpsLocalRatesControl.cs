c22a97ab59ad570d3d6da1547e2fa694e9e8ae9f
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-04-04 12:59:09 -0500

    TP#17480 Create IUpsLocalRatesControl

---
+++ Code/ShipWorks/Shipping/Carriers/UPS/IUpsLocalRatesControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Shipping.Carriers.UPS
+{
+    /// <summary>
+    /// Represents a Ups Local Rates Control
+    /// </summary>
+    public interface IUpsLocalRatesControl
+    {
+        /// <summary>
+        /// The DataContext of the control
+        /// </summary>
+        object DataContet { get; set; }
+    }
+}
