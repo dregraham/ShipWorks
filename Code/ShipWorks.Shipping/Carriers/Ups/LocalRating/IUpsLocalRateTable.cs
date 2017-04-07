e831c86f42998d1f17dcc9d8122d1abee65f73a5
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-04-07 11:06:48 -0500

    TP#16424 add comments and add to csproj

--- Code/ShipWorks.Shipping/Carriers/Ups/LocalRating/IUpsLocalRateTable.cs
+++ Code/ShipWorks.Shipping/Carriers/Ups/LocalRating/IUpsLocalRateTable.cs
@@ -8,14 +8,29 @@ using System.Threading.Tasks;
 
 namespace ShipWorks.Shipping.Carriers.Ups.LocalRating
 {
+    /// <summary>
+    /// Interface that represents a Ups local rate table
+    /// </summary>
     public interface IUpsLocalRateTable
     {
+        /// <summary>
+        /// Load the rate table froma stream
+        /// </summary>
         void Load(Stream stream);
 
+        /// <summary>
+        /// Save the rate table
+        /// </summary>
+        void Save(UpsAccountEntity entity);
+
+        /// <summary>
+        /// Add a rates collection to the rate table
+        /// </summary>
         void AddRates(IEnumerable<UpsLocalRateEntity> rates);
 
+        /// <summary>
+        /// Add a surcharge collection to the rate table
+        /// </summary>
         void AddSurcharges(IEnumerable<UpsLocalRateSurchargeEntity> surcharges);
-
-        void Save(UpsAccountEntity entity);
     }
 }
