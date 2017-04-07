f2cbc3c240b14702573d06705f69d4efa354b735
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-04-07 11:11:13 -0500

    TP#16424 create IUpsLocalRateTableRepo

---
+++ Code/ShipWorks.Shipping/Carriers/Ups/LocalRating/IUpsLocalRateTableRepo.cs
@@ -0,0 +1,25 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating
+{
+    /// <summary>
+    /// Represents a repository for LocalRateTable
+    /// </summary>
+    public interface IUpsLocalRateTableRepo
+    {
+        /// <summary>
+        /// Save the rate table and update the account to use the given rate table
+        /// </summary>
+        void Save(IUpsLocalRateTable rateTable, UpsAccountEntity account);
+
+        /// <summary>
+        /// Remove rate tables that are not associated with a UpsAccountEntity
+        /// </summary>
+        void CleanupRates();
+    }
+}
