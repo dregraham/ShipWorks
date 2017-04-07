0f4f936f29d68c59954a05a5ec25615c3fc2e6ab
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-04-07 11:04:53 -0500

    TP#17488 Create IUpsLocalRateTable

---
+++ Code/ShipWorks.Shipping/Carriers/Ups/LocalRating/IUpsLocalRateTable.cs
@@ -0,0 +1,21 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating
+{
+    public interface IUpsLocalRateTable
+    {
+        void Load(Stream stream);
+
+        void AddRates(IEnumerable<UpsLocalRateEntity> rates);
+
+        void AddSurcharges(IEnumerable<UpsLocalRateSurchargeEntity> surcharges);
+
+        void Save(UpsAccountEntity entity);
+    }
+}
