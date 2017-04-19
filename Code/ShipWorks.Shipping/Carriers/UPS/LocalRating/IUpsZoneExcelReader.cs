af1ebc194fa3e442a0c4b45cd9a0d4da1e7b0113
Kevin Croke <k.croke@shipworks.com>
2017-04-19 11:08:07 -0500

    TP#17551: Add IUpsZoneExcelReader

---
+++ Code/ShipWorks.Shipping/Carriers/UPS/LocalRating/IUpsZoneExcelReader.cs
@@ -0,0 +1,17 @@
+﻿using ShipWorks.ApplicationCore.ComponentRegistration;
+using Syncfusion.XlsIO;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating
+{
+    /// <summary>
+    /// Reads the ups zone excel work sheets and stores the zone info in to the UpsLocalRateTable
+    /// </summary>
+    [Service]
+    public interface IUpsZoneExcelReader
+    {
+        /// <summary>
+        ///  Reads the ups zone excel work sheets and stores the zone info in to the UpsLocalRateTable
+        /// </summary>
+        void Read(IWorksheets zoneWorksheets, IUpsLocalRateTable upsLocalRateTable);
+    }
+}
