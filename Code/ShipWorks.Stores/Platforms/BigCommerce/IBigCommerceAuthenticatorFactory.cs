ffbcc2d3c885d2d9797899d43bbe6adf47cfa9af
Andrew Benz <a.benz@shipworks.com>
2017-04-20 11:37:01 -0500

    TP#17549: Create IBigCommerceAuthenticatorFactory

---
+++ Code/ShipWorks.Stores/Platforms/BigCommerce/IBigCommerceAuthenticatorFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using ShipWorks.Data.Model.EntityInterfaces;
+
+namespace ShipWorks.Stores.Platforms.BigCommerce
+{
+    /// <summary>
+    /// Factory for creating web client authenticators
+    /// </summary>
+    public interface IBigCommerceAuthenticatorFactory
+    {
+        /// <summary>
+        /// Create the correct authenticator based on the store
+        /// </summary>
+        IAuthenticator Create(IBigCommerceStoreEntity store);
+    }
+}
