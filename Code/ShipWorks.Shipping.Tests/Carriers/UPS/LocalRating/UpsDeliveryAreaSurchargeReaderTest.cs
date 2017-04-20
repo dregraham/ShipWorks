fb4b340e18ea7ba3167ed5cccf9086c3086ee6d1
Kevin Croke <k.croke@shipworks.com>
2017-04-20 11:42:51 -0500

    TP#17551: Add DeliveryAreaSurchargeUpsZoneExcelReader

---
+++ Code/ShipWorks.Shipping.Tests/Carriers/UPS/LocalRating/UpsDeliveryAreaSurchargeReaderTest.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac.Extras.Moq;
+using Interapptive.Shared.Utility;
+using Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.Ups.LocalRating;
+using ShipWorks.Tests.Shared;
+using Syncfusion.XlsIO;
+using Xunit;
+
+namespace ShipWorks.Shipping.Tests.Carriers.UPS.LocalRating
+{
+    public class UpsDeliveryAreaSurchargeReaderTest : IDisposable
+    {
+        readonly AutoMock mock;
+        private readonly ExcelEngine excelEngine;
+
+        private IEnumerable<UpsLocalRatingDeliveryAreaSurchargeEntity> readSurcharges;
+        private readonly Mock<IUpsLocalRateTable> rateTable;
+        private readonly DeliveryAreaSurchargeUpsZoneExcelReader testObject;
+
+        private readonly string expectedUs48DasHeader = "US 48 DAS";
+        private readonly string expectedUs48DasExtendedHeader = "US 48 DAS Extended";
+        private readonly string expectedRemoteHiHeader = "Remote HI";
+        private readonly string expectedRemoteAkHeader = "Remote AK";
+
+
+        public UpsDeliveryAreaSurchargeReaderTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+
+            rateTable = mock.CreateMock<IUpsLocalRateTable>(table =>
+            {
+                table.Setup(t => t.ReplaceDeliveryAreaSurcharges(
+                        It.IsAny<IEnumerable<UpsLocalRatingDeliveryAreaSurchargeEntity>>()))
+                    .Callback<IEnumerable<UpsLocalRatingDeliveryAreaSurchargeEntity>>(read => readSurcharges = read);
+            });
+
+            testObject = mock.Create<DeliveryAreaSurchargeUpsZoneExcelReader>();
+
+            excelEngine = new ExcelEngine();
+        }
+
+        [Theory]
+        [InlineData("00001", UpsDeliveryAreaSurchargeType.Us48Das)]
+        [InlineData("00002", UpsDeliveryAreaSurchargeType.Us48DasExtended)]
+        [InlineData("00003", UpsDeliveryAreaSurchargeType.UsRemoteHi)]
+        [InlineData("00004", UpsDeliveryAreaSurchargeType.UsRemoteAk)]
+        public void Read_FirstRowRead_WhenReadingSingleRowDocument(string zip, UpsDeliveryAreaSurchargeType dasType)
+        {
+            IWorksheets sheets = SetupSheetWithOneZipForEachDas();
+            testObject.Read(sheets, rateTable.Object);
+
+            Assert.Equal(1, readSurcharges.Count(s => s.DeliveryAreaType == (int) dasType));
+            Assert.Equal(1, readSurcharges.Count(s => s.DeliveryAreaType == (int) dasType && s.DestinationZip == int.Parse(zip)));
+        }
+
+        [Fact]
+        public void Read_ThrowsLocalRatingException_WhenNoDasZipsTab()
+        {
+            IWorksheets sheets = SetupSheetWithOneZipForEachDas();
+            sheets[0].Name = "60000-70000";
+            Exception exception = Record.Exception(() => testObject.Read(sheets, rateTable.Object));
+
+            Assert.NotNull(exception);
+            Assert.IsType<UpsLocalRatingException>(exception);
+            Assert.Equal(DeliveryAreaSurchargeUpsZoneExcelReader.MissingDasTab, exception.Message);
+        }
+
+        [Fact]
+        public void Read_ThrowsLocalRatingException_WhenDuplicateColumns()
+        {
+            IWorksheets sheets = SetupSheetWithOneZipForEachDas();
+            sheets[0]["B1"].Text = expectedUs48DasHeader;
+
+            string expectedErrorMessage = string.Format(DeliveryAreaSurchargeUpsZoneExcelReader.DuplicateColumns,
+                expectedUs48DasHeader);
+
+            Exception exception = Record.Exception(() => testObject.Read(sheets, rateTable.Object));
+
+            Assert.NotNull(exception);
+            Assert.IsType<UpsLocalRatingException>(exception);
+            Assert.Equal(expectedErrorMessage, exception.Message);
+        }
+
+        [Theory]
+        [InlineData("E1")]
+        [InlineData("E2")]
+        public void Read_ThrowsLocalRatingException_WhenMoreThanFourColumns(string columnWithExtraText)
+        {
+            IWorksheets sheets = SetupSheetWithOneZipForEachDas();
+            sheets[0].Range[columnWithExtraText].Text = "Extra";
+
+            Exception exception = Record.Exception(() => testObject.Read(sheets, rateTable.Object));
+
+            Assert.NotNull(exception);
+            Assert.IsType<UpsLocalRatingException>(exception);
+            Assert.Equal(DeliveryAreaSurchargeUpsZoneExcelReader.TooManyColumns, exception.Message);
+        }
+
+        [Fact]
+        public void Read_ThrowsLocalRatingException_WhenZipcodeIsNotFiveDigits()
+        {
+            IWorksheets sheets = SetupSheetWithOneZipForEachDas();
+            sheets[0].Range["A2"].Text = "BadZip";
+
+            string expectedExceptionMessage = string.Format(DeliveryAreaSurchargeUpsZoneExcelReader.InvalidZipCode,
+                expectedUs48DasHeader,
+                2);
+
+            Exception exception = Record.Exception(() => testObject.Read(sheets, rateTable.Object));
+
+            Assert.NotNull(exception);
+            Assert.IsType<UpsLocalRatingException>(exception);
+            Assert.Equal(expectedExceptionMessage, exception.Message);
+        }
+
+        [Theory]
+        [InlineData(UpsDeliveryAreaSurchargeType.Us48Das)]
+        [InlineData(UpsDeliveryAreaSurchargeType.Us48DasExtended)]
+        [InlineData(UpsDeliveryAreaSurchargeType.UsRemoteAk)]
+        [InlineData(UpsDeliveryAreaSurchargeType.UsRemoteHi)]
+        public void Read_ThrowsLocalRatingException_WhenMissingDasType(UpsDeliveryAreaSurchargeType missingDasType)
+        {
+            var sheets = SetupSheetWithMissingColumn(missingDasType);
+            Exception exception = Record.Exception(() => testObject.Read(sheets, rateTable.Object));
+
+            string expectedExceptionMessage = string.Format(DeliveryAreaSurchargeUpsZoneExcelReader.MissingColumn,
+                EnumHelper.GetApiValue(missingDasType));
+
+            Assert.NotNull(exception);
+            Assert.IsType<UpsLocalRatingException>(exception);
+            Assert.Equal(expectedExceptionMessage, exception.Message);
+        }
+
+        private IWorksheets SetupSheetWithMissingColumn(UpsDeliveryAreaSurchargeType missingDasType)
+        {
+            IWorkbook workbook = excelEngine.Excel.Workbooks.Create(1);
+            IWorksheets sheets = workbook.Worksheets;
+            IWorksheet sheet = sheets[0];
+
+            sheet.Name = DeliveryAreaSurchargeUpsZoneExcelReader.DasZipsTabName;
+
+            var dasTypes = EnumHelper.GetEnumList<UpsDeliveryAreaSurchargeType>(type => type != missingDasType).ToList();
+            Assert.Equal(3, dasTypes.Count);
+
+            sheet.Range["A1"].Text = dasTypes[0].ApiValue;
+            sheet.Range["B1"].Text = dasTypes[1].ApiValue;
+            sheet.Range["C1"].Text = dasTypes[2].ApiValue;
+            
+            sheet.Range["A2"].Text = "00001";
+            sheet.Range["B2"].Text = "00002";
+            sheet.Range["C2"].Text = "00003";
+
+            return sheets;
+        }
+
+        private IWorksheets SetupSheetWithOneZipForEachDas()
+        {
+            IWorkbook workbook = excelEngine.Excel.Workbooks.Create(1);
+            IWorksheets sheets = workbook.Worksheets;
+            IWorksheet sheet = sheets[0];
+
+            sheet.Name = DeliveryAreaSurchargeUpsZoneExcelReader.DasZipsTabName;
+            sheet.Range["A1"].Text = expectedUs48DasHeader;
+            sheet.Range["B1"].Text = expectedUs48DasExtendedHeader;
+            sheet.Range["C1"].Text = expectedRemoteHiHeader;
+            sheet.Range["D1"].Text = expectedRemoteAkHeader;
+
+            sheet.Range["A2"].Text = "00001";
+            sheet.Range["B2"].Text = "00002";
+            sheet.Range["C2"].Text = "00003";
+            sheet.Range["D2"].Text = "00004";
+            return sheets;
+        }
+
+        public void Dispose()
+        {
+            mock.Dispose();
+            excelEngine.Dispose();
+        }
+
+    }
+}
---
+++ Code/ShipWorks.Shipping/Carriers/UPS/LocalRating/DeliveryAreaSurchargeUpsZoneExcelReader.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Interapptive.Shared.Collections;
+using Interapptive.Shared.Utility;
+using ShipWorks.Data.Model.EntityClasses;
+using Syncfusion.XlsIO;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating
+{
+    /// <summary>
+    /// Reads Delivery Area Surcharges from an excel document and saves them via
+    /// the UpsLocalRateTable.
+    /// </summary>
+    public class DeliveryAreaSurchargeUpsZoneExcelReader : IUpsZoneExcelReader
+    {
+        public const string MissingColumn = "DASzips missing '{0}' column.";
+        public const string MissingDasTab = "Spreadsheet missing DASzips Tab";
+        public const string TooManyColumns = "DASzips can only have 4 columns. There may be a cell with text outside of the expected 4 columns.";
+        public const string DasZipsTabName = "DASzips";
+        public const string InvalidZipCode = "DASzips has an invalid zip code for column {0} row {1}.";
+        public const string DuplicateColumns = "DASzips can only have 1 '{0}' column.";
+
+        /// <summary>
+        /// Reads the worksheets in a UPS zone excel document, creates a list of  
+        /// UpsLocalRatingDeliveryAreaSurchargeEntity objects, and passes the
+        /// list to upsLocalRateTable.ReplaceDeliveryAreaSurcharges
+        /// </summary>
+        public void Read(IWorksheets zoneWorksheets, IUpsLocalRateTable upsLocalRateTable)
+        {
+            IWorksheet zoneWorksheet = zoneWorksheets[DasZipsTabName];
+            if (zoneWorksheet == null)
+            {
+                throw new UpsLocalRatingException(MissingDasTab);
+            }
+
+            if (zoneWorksheet.Columns.Length>4)
+            {
+                throw new UpsLocalRatingException(TooManyColumns);
+            }
+
+            List<UpsLocalRatingDeliveryAreaSurchargeEntity> readSurcharges = new List<UpsLocalRatingDeliveryAreaSurchargeEntity>();
+
+            foreach (EnumEntry<UpsDeliveryAreaSurchargeType> enumEntry in EnumHelper.GetEnumList<UpsDeliveryAreaSurchargeType>())
+            {
+                ProcessColumn(zoneWorksheet, enumEntry, readSurcharges);
+            }
+
+            upsLocalRateTable.ReplaceDeliveryAreaSurcharges(readSurcharges);
+        }
+
+        /// <summary>
+        /// Processes the column.
+        /// </summary>
+        private void ProcessColumn(IWorksheet zoneWorksheet,
+            EnumEntry<UpsDeliveryAreaSurchargeType> dasTypeEntry,
+            List<UpsLocalRatingDeliveryAreaSurchargeEntity> readSurcharges)
+        {
+            List<IRange> matchingColumns = zoneWorksheet.Columns.Where(c => c.Cells[0].Text.Trim() == dasTypeEntry.ApiValue).ToList();
+            if (matchingColumns.None())
+            {
+                throw new UpsLocalRatingException(string.Format(MissingColumn, dasTypeEntry.ApiValue));
+            }
+            if (matchingColumns.Count != 1)
+            {
+                throw new UpsLocalRatingException(string.Format(DuplicateColumns, dasTypeEntry.ApiValue));
+            }
+
+            IRange dasColumn = matchingColumns.Single();
+
+            for (int index = 1; index < dasColumn.Cells.Length; index++)
+            {
+                IRange cell = dasColumn.Cells[index];
+                string zip = cell.Text.Trim();
+
+                if (!Regex.IsMatch(zip, "^[0-9]{5}$"))
+                {
+                    string errorMessage = string.Format(InvalidZipCode, dasTypeEntry.ApiValue, cell.Row);
+                    throw new UpsLocalRatingException(errorMessage);
+                }
+                readSurcharges.Add(new UpsLocalRatingDeliveryAreaSurchargeEntity()
+                {
+                    DestinationZip = int.Parse(zip),
+                    DeliveryAreaType = (int) dasTypeEntry.Value
+                });
+            }
+        }
+    }
+}
