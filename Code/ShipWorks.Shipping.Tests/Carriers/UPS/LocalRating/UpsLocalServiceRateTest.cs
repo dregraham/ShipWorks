d5cdb5d95bcae666ce2dd70db6283bf32d5e2681
Kevin Croke <k.croke@shipworks.com>
2017-05-01 13:19:28 -0500

    TP#16425: Add UpsLocalServiceRate

---
+++ Code/ShipWorks.Shipping.Tests/Carriers/UPS/LocalRating/UpsLocalServiceRateTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Autofac.Extras.Moq;
+using log4net;
+using Moq;
+using ShipWorks.Shipping.Carriers.Ups.LocalRating;
+using ShipWorks.Shipping.Carriers.UPS.Enums;
+using ShipWorks.Tests.Shared;
+using Xunit;
+
+namespace ShipWorks.Shipping.Tests.Carriers.UPS.LocalRating
+{
+    public class UpsLocalServiceRateTest : IDisposable
+    {
+        private readonly AutoMock mock;
+        private readonly Mock<ILog> log;
+
+        public UpsLocalServiceRateTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+            log = mock.CreateMock<ILog>();
+        }
+
+        [Fact]
+        public void AddAmount_AmountIsAddedToBaseAmount()
+        {
+            UpsLocalServiceRate testObject = new UpsLocalServiceRate(UpsServiceType.Ups3DaySelect, 10.10M, false, 3);
+            testObject.AddAmount(.42M, "blah");
+
+            Assert.Equal(10.52M, testObject.Amount);
+        }
+
+        [Fact]
+        public void Log_LogsBaseAmount()
+        {
+            UpsLocalServiceRate testObject = 
+                new UpsLocalServiceRate(UpsServiceType.Ups3DaySelect, 10.10M, false, 3);
+            testObject.Log(log.Object);
+            log.Verify(l => l.Info("Initial Value : $10.10"));
+        }
+
+        [Fact]
+        public void Log_LogsService()
+        {
+            UpsLocalServiceRate testObject =
+                new UpsLocalServiceRate(UpsServiceType.Ups3DaySelect, 10.10M, false, 3);
+            testObject.Log(log.Object);
+            log.Verify(l => l.Info("Rate Calculation for Ups3DaySelect"));
+        }
+
+        [Fact]
+        public void Log_LogsAddedSurcharge()
+        {
+            UpsLocalServiceRate testObject =
+                new UpsLocalServiceRate(UpsServiceType.Ups3DaySelect, 10.10M, false, 3);
+
+            testObject.AddAmount(.42M, "blah");
+            testObject.Log(log.Object);
+
+            log.Verify(l => l.Info("\tAdded for 'blah' : $0.42"));
+        }
+
+        [Fact]
+        public void Logs_Total()
+        {
+            UpsLocalServiceRate testObject =
+                new UpsLocalServiceRate(UpsServiceType.Ups3DaySelect, 10.10M, false, 3);
+
+            testObject.AddAmount(.42M, "blah");
+            testObject.Log(log.Object);
+
+            log.Verify(l => l.Info("Total : $10.52"));
+        }
+
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+
+    }
+}
---
+++ Code/ShipWorks.Shipping/Carriers/UPS/LocalRating/UpsLocalServiceRate.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using ShipWorks.Shipping.Carriers.UPS.Enums;
+using ShipWorks.Shipping.Carriers.UPS.OnLineTools.Api;
+using log4net;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating
+{
+    /// <summary>
+    /// Data structure for storing rating information for a UPS service with methods used when rating locally
+    /// </summary>
+    public class UpsLocalServiceRate : UpsServiceRate
+    {
+        private readonly List<KeyValuePair<string, decimal>> addedSurcharges = new List<KeyValuePair<string, decimal>>();
+        private readonly decimal initialAmount;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public UpsLocalServiceRate(UpsServiceType service,
+            decimal amount,
+            bool negotiated,
+            int? guaranteedDaysToDelivery) :
+            base(service, amount, negotiated, guaranteedDaysToDelivery)
+        {
+            initialAmount = amount;
+        }
+
+        /// <summary>
+        /// Adds the amount.
+        /// </summary>
+        public void AddAmount(decimal amount, string surchargeName)
+        {
+            addedSurcharges.Add(new KeyValuePair<string, decimal>(surchargeName,amount));
+            Amount += amount;
+        }
+
+        /// <summary>
+        /// Logs the specified log.
+        /// </summary>
+        public void Log(ILog log)
+        {
+            log.Info($"Rate Calculation for {Service}");
+            log.Info($"Initial Value : {initialAmount:C}");
+            addedSurcharges.ForEach(s=>log.Info($"\tAdded for '{s.Key}' : {s.Value:C}"));
+            log.Info($"Total : {Amount:C}");
+        }
+    }
+}
