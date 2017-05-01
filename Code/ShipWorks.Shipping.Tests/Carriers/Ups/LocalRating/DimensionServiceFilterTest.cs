fa65be5ea0310738d7d59d2e381ad41bbea7a05d
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-05-01 14:53:17 -0500

    TP#16425 create DimensionServiceFilter and tests

---
+++ Code/ShipWorks.Shipping.Tests/Carriers/Ups/LocalRating/DimensionServiceFilterTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac.Extras.Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.Ups.LocalRating;
+using ShipWorks.Shipping.Carriers.UPS.Enums;
+using ShipWorks.Tests.Shared;
+using Syncfusion.XlsIO;
+using Xunit;
+
+namespace ShipWorks.Shipping.Tests.Carriers.UPS.LocalRating
+{
+    public class DimensionServiceFilterTest : IDisposable
+    {
+        private readonly AutoMock mock;
+        private readonly DimensionServiceFilter testObject;
+
+        public DimensionServiceFilterTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+            testObject = new DimensionServiceFilter();
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3, false)]
+        [InlineData(5, 5, 5, false)]
+        [InlineData(10, 10, 10, false)]
+        [InlineData(1, 2, 108, true)]
+        [InlineData(10, 10, 100, false)]
+        [InlineData(30, 10, 100, true)]
+        public void GetEligibleServices_ReturnsEmptyList_WhenPackageDimensionsAreNotValidForUPS(double length, double width, double height, bool emptyResult)
+        {
+            IEnumerable<UpsServiceType> services = new List<UpsServiceType>()
+            {
+                UpsServiceType.Ups2DayAir,
+                UpsServiceType.UpsMailInnovationsExpedited,
+                UpsServiceType.Ups3DaySelect
+            };
+
+            UpsShipmentEntity shipment = new UpsShipmentEntity
+            {
+                Packages = {new UpsPackageEntity {DimsLength = length, DimsWidth = width, DimsHeight = height}}
+            };
+
+            IEnumerable<UpsServiceType> result = testObject.GetEligibleServices(shipment, services);
+
+
+            Assert.Equal(!emptyResult, result.Any());
+        }
+
+        [Fact]
+        public void GetEligibleServices_ReturnsEmptyList_WhenPackageDimensionsAreNotValidForUPSWithMultiplePackages()
+        {
+            IEnumerable<UpsServiceType> services = new List<UpsServiceType>()
+            {
+                UpsServiceType.Ups2DayAir,
+                UpsServiceType.UpsMailInnovationsExpedited,
+                UpsServiceType.Ups3DaySelect
+            };
+
+            UpsShipmentEntity shipment = new UpsShipmentEntity
+            {
+                Packages =
+                {
+                    new UpsPackageEntity {DimsLength = 12, DimsWidth = 19, DimsHeight = 7},
+                    new UpsPackageEntity {DimsLength = 22, DimsWidth = 98, DimsHeight = 100},
+                    new UpsPackageEntity {DimsLength = 22, DimsWidth = 98, DimsHeight = 106}
+                }
+            };
+
+            IEnumerable<UpsServiceType> result = testObject.GetEligibleServices(shipment, services);
+
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void GetEligibleServices_ReturnsServiceList_WhenPackageDimensionsAreValidForUPSWithMultiplePackages()
+        {
+            IEnumerable<UpsServiceType> services = new List<UpsServiceType>()
+            {
+                UpsServiceType.Ups2DayAir,
+                UpsServiceType.UpsMailInnovationsExpedited,
+                UpsServiceType.Ups3DaySelect
+            };
+
+            UpsShipmentEntity shipment = new UpsShipmentEntity
+            {
+                Packages =
+                {
+                    new UpsPackageEntity {DimsLength = 12, DimsWidth = 19, DimsHeight = 7},
+                    new UpsPackageEntity {DimsLength = 22, DimsWidth = 20, DimsHeight = 2},
+                    new UpsPackageEntity {DimsLength = 22, DimsWidth = 20, DimsHeight = 2}
+                }
+            };
+
+            IEnumerable<UpsServiceType> result = testObject.GetEligibleServices(shipment, services);
+
+
+            Assert.Equal(services, result);
+        }
+
+        public void Dispose()
+        {
+            mock?.Dispose();
+        }
+    }
+}
---
+++ Code/ShipWorks.Shipping/Carriers/Ups/LocalRating/DimensionServiceFilter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Interapptive.Shared.Collections;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.UPS.Enums;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating
+{
+    /// <summary>
+    /// Filtker services based on package dimensions
+    /// </summary>
+    public class DimensionServiceFilter : IServiceFilter
+    {
+        /// <summary>
+        /// Get eligible services based on the dimensions
+        /// </summary>
+        public IEnumerable<UpsServiceType> GetEligibleServices(UpsShipmentEntity shipment, IEnumerable<UpsServiceType> services)
+        {
+            if (shipment.Packages.Any(package => GetPackageLength(package) >= 108 ||
+                                                 GetPackageLength(package) + GetPackageGirth(package) >= 165))
+            {
+                return Enumerable.Empty<UpsServiceType>();
+            }
+
+            return services;
+        }
+
+        /// <summary>
+        /// Get the longest side of a package
+        /// </summary>
+        /// <param name="package"></param>
+        /// <returns></returns>
+        private static double GetPackageLength(UpsPackageEntity package)
+        {
+            return Math.Max(Math.Max(package.DimsLength, package.DimsHeight), package.DimsWidth);
+        }
+
+        /// <summary>
+        /// Get the package girth 
+        /// </summary>
+        private static double GetPackageGirth(UpsPackageEntity package)
+        {
+            return new List<double> {package.DimsLength, package.DimsHeight, package.DimsWidth}.OrderBy(d => d)
+                .Take(2)
+                .Aggregate<double, double>(0, (c, d) => c + d * 2);
+        }
+    }
+}
