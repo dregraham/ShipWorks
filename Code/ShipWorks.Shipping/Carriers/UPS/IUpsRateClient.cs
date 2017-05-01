fa2c8bb7e2c703cd77329b68f13b1b5484c69a49
Chris Hicks <c.hicks@shipworks.com>
2017-05-01 12:50:03 -0500

    TP#16425: Create IUpsRateClient, IServiceFilter, IUpsServiceRequirements

---
+++ Code/ShipWorks.Shipping/Carriers/UPS/IUpsRateClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.UPS.OnLineTools.Api;
+
+namespace ShipWorks.Shipping.Carriers.UPS
+{
+    public interface IUpsRateClient
+    {
+        /// <summary>
+        /// Gets rates for the given shipment
+        /// </summary>
+        IEnumerable<UpsServiceRate> GetRates(UpsShipmentEntity shipment);
+    }
+}
---
+++ Code/ShipWorks.Shipping/Carriers/UPS/LocalRating/IServiceFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.UPS.Enums;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating
+{
+    /// <summary>
+    /// Filter for determining eligible services
+    /// </summary>
+    public interface IServiceFilter
+    {
+        /// <summary>
+        /// Gets the list of eligible service types for the given shipment, from the given list of service types
+        /// </summary>
+        IEnumerable<UpsServiceType> GetEligibleServices(UpsShipmentEntity shipment, IEnumerable<UpsServiceType> services);
+    }
+}
