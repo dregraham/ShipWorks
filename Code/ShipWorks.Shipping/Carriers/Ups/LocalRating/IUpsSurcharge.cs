53dbec4175b7f665b185492f4df2bd776c4eb423
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-05-01 14:59:23 -0500

    TP#16425 Create IUpsSurcharge

---
+++ Code/ShipWorks.Shipping/Carriers/Ups/LocalRating/IUpsSurcharge.cs
@@ -0,0 +1,15 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating
+{
+    /// <summary>
+    /// Interface that represents a surcharge
+    /// </summary>
+    public interface IUpsSurcharge
+    {
+        /// <summary>
+        /// Apply the surcharge to the service rate based on the shipment
+        /// </summary>
+        void Apply(UpsShipmentEntity shipment, UpsLocalServiceRate serviceRate);
+    }
+}
