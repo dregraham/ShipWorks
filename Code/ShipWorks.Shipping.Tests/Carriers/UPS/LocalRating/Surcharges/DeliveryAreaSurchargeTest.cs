39624f339b6a16f83958606b7a289e2fe2370121
Chris Hicks <c.hicks@shipworks.com>
2017-05-02 17:02:15 -0500

    TP#16425: Create DeliveryAreaSurcharge

---
+++ Code/ShipWorks.Shipping.Tests/Carriers/UPS/LocalRating/Surcharges/DeliveryAreaSurchargeTest.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using Autofac.Extras.Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.Ups.LocalRating;
+using ShipWorks.Shipping.Carriers.Ups.LocalRating.Surcharges;
+using ShipWorks.Shipping.Carriers.UPS.Enums;
+using ShipWorks.Shipping.Carriers.UPS.LocalRating;
+using ShipWorks.Tests.Shared;
+using Xunit;
+
+namespace ShipWorks.Shipping.Tests.Carriers.UPS.LocalRating.Surcharges
+{
+    public class DeliveryAreaSurchargeTest : IDisposable
+    {
+        private const int StandardZip = 12345;
+        private const int DeliveryAreaSurchargeZip = 01007;
+        private const int DeliveryAreaExtendedSurchargeZip = 58001;
+        private const int RemoteAlaskaZip = 96703;
+        private const int RemoteHawaiiZip = 99950;
+        private readonly UpsLocalServiceRate serviceRate;
+        private readonly DeliveryAreaSurcharge testObject;
+        private readonly AutoMock mock;
+        private readonly Dictionary<UpsSurchargeType, decimal> surcharges = new Dictionary<UpsSurchargeType, decimal>
+        {
+            {UpsSurchargeType.LargePackage, 67.50m},
+            {UpsSurchargeType.DeliveryAreaCommercialAir, 2.45m},
+            {UpsSurchargeType.DeliveryAreaResidentialAir, 3.70m},
+            {UpsSurchargeType.DeliveryAreaCommercialExtendedAir, 2.45m},
+            {UpsSurchargeType.DeliveryAreaResidentialExtendedAir, 4.00m},
+            {UpsSurchargeType.DeliveryAreaCommercialGround, 2.30m},
+            {UpsSurchargeType.DeliveryAreaResidentialGround, 3.15m},
+            {UpsSurchargeType.DeliveryAreaCommercialExtendedGround, 2.35m},
+            {UpsSurchargeType.DeliveryAreaResidentialExtendedGround, 4.00m},
+            {UpsSurchargeType.RemoteAreaAlaska, 22.50m},
+            {UpsSurchargeType.RemoteAreaHawaii, 6.75m},
+            {UpsSurchargeType.ResidentialAir, 3.65m},
+            {UpsSurchargeType.ResidentialGround, 3.25m}
+        };
+
+        public DeliveryAreaSurchargeTest()
+        {
+            var zoneFile = new UpsLocalRatingZoneFileEntity
+            {
+                UpsLocalRatingDeliveryAreaSurcharge =
+                {
+                    new UpsLocalRatingDeliveryAreaSurchargeEntity
+                    {
+                        DeliveryAreaType = (int) UpsDeliveryAreaSurchargeType.Us48Das,
+                        DestinationZip = DeliveryAreaSurchargeZip
+                    },
+                    new UpsLocalRatingDeliveryAreaSurchargeEntity
+                    {
+                        DeliveryAreaType = (int) UpsDeliveryAreaSurchargeType.Us48DasExtended,
+                        DestinationZip = DeliveryAreaExtendedSurchargeZip
+                    },
+                    new UpsLocalRatingDeliveryAreaSurchargeEntity
+                    {
+                        DeliveryAreaType = (int) UpsDeliveryAreaSurchargeType.UsRemoteAk,
+                        DestinationZip = RemoteAlaskaZip
+                    },
+                    new UpsLocalRatingDeliveryAreaSurchargeEntity
+                    {
+                        DeliveryAreaType = (int) UpsDeliveryAreaSurchargeType.UsRemoteHi,
+                        DestinationZip = RemoteHawaiiZip
+                    }
+                }
+            };
+
+            serviceRate = new UpsLocalServiceRate(UpsServiceType.UpsGround, 0, false, 0);
+
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+
+            mock.Mock<IUpsLocalRateTableRepository>()
+                .Setup(repo => repo.GetLatestZoneFile())
+                .Returns(zoneFile);
+
+            testObject = mock.Create<DeliveryAreaSurcharge>(new TypedParameter(typeof(Dictionary<UpsSurchargeType, decimal>), surcharges));
+        }
+
+        [Fact]
+        public void Apply_AppliesNoSurcharge_WhenShipToZipIsNotDASZipAndNotResidential()
+        {
+            UpsShipmentEntity shipment = new UpsShipmentEntity
+            {
+                Service = (int) UpsServiceType.UpsGround,
+                Shipment = new ShipmentEntity
+                {
+                    ShipPostalCode = StandardZip.ToString(),
+                    ResidentialResult = false
+                }
+            };
+
+            testObject.Apply(shipment, serviceRate);
+
+            Assert.Equal(0, serviceRate.Amount);
+        }
+
+        [Theory]
+        [InlineData(UpsSurchargeType.ResidentialGround, UpsServiceType.UpsGround, StandardZip, true)]
+        [InlineData(UpsSurchargeType.ResidentialAir, UpsServiceType.Ups2DayAir, StandardZip, true)]
+        [InlineData(UpsSurchargeType.DeliveryAreaResidentialGround, UpsServiceType.UpsGround, DeliveryAreaSurchargeZip, true)]
+        [InlineData(UpsSurchargeType.DeliveryAreaResidentialAir, UpsServiceType.Ups2DayAir, DeliveryAreaSurchargeZip, true)]
+        [InlineData(UpsSurchargeType.DeliveryAreaCommercialGround, UpsServiceType.UpsGround, DeliveryAreaSurchargeZip, false)]
+        [InlineData(UpsSurchargeType.DeliveryAreaCommercialAir, UpsServiceType.Ups2DayAir, DeliveryAreaSurchargeZip, false)]
+        [InlineData(UpsSurchargeType.DeliveryAreaResidentialExtendedGround, UpsServiceType.UpsGround, DeliveryAreaExtendedSurchargeZip, true)]
+        [InlineData(UpsSurchargeType.DeliveryAreaResidentialExtendedAir, UpsServiceType.Ups2DayAir, DeliveryAreaExtendedSurchargeZip, true)]
+        [InlineData(UpsSurchargeType.DeliveryAreaCommercialExtendedGround, UpsServiceType.UpsGround, DeliveryAreaExtendedSurchargeZip, false)]
+        [InlineData(UpsSurchargeType.DeliveryAreaCommercialExtendedAir, UpsServiceType.Ups2DayAir, DeliveryAreaExtendedSurchargeZip, false)]
+        [InlineData(UpsSurchargeType.RemoteAreaAlaska, UpsServiceType.UpsGround, RemoteAlaskaZip, true)]
+        [InlineData(UpsSurchargeType.RemoteAreaHawaii, UpsServiceType.UpsGround, RemoteHawaiiZip, true)]
+        public void Apply_AppliesSurchargesCorrectly(UpsSurchargeType surchargeType, UpsServiceType serviceType, string destZip, bool isResidential)
+        {
+            UpsShipmentEntity shipment = new UpsShipmentEntity
+            {
+                Service = (int) serviceType,
+                Shipment = new ShipmentEntity
+                {
+                    ShipPostalCode = destZip,
+                    ResidentialResult = isResidential
+                }
+            };
+
+            testObject.Apply(shipment, serviceRate);
+
+            Assert.Equal(surcharges[surchargeType], serviceRate.Amount);
+        }
+
+        public void Dispose()
+        {
+            mock?.Dispose();
+        }
+    }
+}
---
+++ Code/ShipWorks.Shipping/Carriers/UPS/LocalRating/Surcharges/DeliveryAreaSurcharge.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Interapptive.Shared.Utility;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.UPS.Enums;
+using ShipWorks.Shipping.Carriers.UPS.LocalRating;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating.Surcharges
+{
+    /// <summary>
+    /// Surcharge for Delivery Area, Delivery Area Extended, Remote AK and Remote HI
+    /// </summary>
+    /// <seealso cref="ShipWorks.Shipping.Carriers.Ups.LocalRating.Surcharges.IUpsSurcharge" />
+    public class DeliveryAreaSurcharge : IUpsSurcharge
+    {
+        private readonly Dictionary<UpsSurchargeType, decimal> surcharges;
+        private readonly IUpsLocalRateTableRepository rateTableRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeliveryAreaSurcharge"/> class.
+        /// </summary>
+        public DeliveryAreaSurcharge(Dictionary<UpsSurchargeType, decimal> surcharges, IUpsLocalRateTableRepository rateTableRepository)
+        {
+            this.surcharges = surcharges;
+            this.rateTableRepository = rateTableRepository;
+        }
+
+        /// <summary>
+        /// Apply the surcharge to the service rate based on the shipment
+        /// </summary>
+        /// <param name="shipment"></param>
+        /// <param name="serviceRate"></param>
+        public void Apply(UpsShipmentEntity shipment, UpsLocalServiceRate serviceRate)
+        {
+            UpsLocalRatingZoneFileEntity zoneFile = rateTableRepository.GetLatestZoneFile();
+
+            int destinationZip = int.Parse(shipment.Shipment.ShipPostalCode);
+
+            UpsLocalRatingDeliveryAreaSurchargeEntity deliveryAreaSurcharge =
+                zoneFile.UpsLocalRatingDeliveryAreaSurcharge.FirstOrDefault(
+                    das => das.DestinationZip == destinationZip);
+
+            bool isResidential = shipment.Shipment.ResidentialResult;
+            bool isGround = shipment.Service == (int) UpsServiceType.UpsGround;
+
+            if (deliveryAreaSurcharge == null)
+            {
+                if (isResidential)
+                {
+                    AddSurcharge(serviceRate,
+                        isGround ? UpsSurchargeType.ResidentialGround : UpsSurchargeType.ResidentialAir);
+                }
+            }
+            else
+            {
+                UpsDeliveryAreaSurchargeType deliveryAreaType =
+                    (UpsDeliveryAreaSurchargeType) deliveryAreaSurcharge.DeliveryAreaType;
+                switch (deliveryAreaType)
+                {
+                    case UpsDeliveryAreaSurchargeType.Us48Das:
+                    case UpsDeliveryAreaSurchargeType.Us48DasExtended:
+                        AddDeliveryAreaSurcharge(serviceRate, deliveryAreaType, isResidential, isGround);
+                        break;
+                    case UpsDeliveryAreaSurchargeType.UsRemoteHi:
+                        AddSurcharge(serviceRate, UpsSurchargeType.RemoteAreaHawaii);
+                        break;
+                    case UpsDeliveryAreaSurchargeType.UsRemoteAk:
+                        AddSurcharge(serviceRate, UpsSurchargeType.RemoteAreaAlaska);
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds the delivery area surcharge.
+        /// </summary>
+        private void AddDeliveryAreaSurcharge(UpsLocalServiceRate serviceRate, UpsDeliveryAreaSurchargeType deliveryAreaSurchargeType, bool isResidential, bool isGround)
+        {
+            if (deliveryAreaSurchargeType == UpsDeliveryAreaSurchargeType.Us48Das)
+            {
+                if (isResidential)
+                {
+                    AddSurcharge(serviceRate, isGround ?
+                        UpsSurchargeType.DeliveryAreaResidentialGround :
+                        UpsSurchargeType.DeliveryAreaResidentialAir);
+                }
+                else
+                {
+                    AddSurcharge(serviceRate, isGround ?
+                        UpsSurchargeType.DeliveryAreaCommercialGround :
+                        UpsSurchargeType.DeliveryAreaCommercialAir);
+                }
+            }
+            else
+            {
+                if (isResidential)
+                {
+                    AddSurcharge(serviceRate, isGround ?
+                        UpsSurchargeType.DeliveryAreaResidentialExtendedGround :
+                        UpsSurchargeType.DeliveryAreaResidentialExtendedAir);
+                }
+                else
+                {
+                    AddSurcharge(serviceRate, isGround ?
+                        UpsSurchargeType.DeliveryAreaCommercialExtendedGround :
+                        UpsSurchargeType.DeliveryAreaCommercialExtendedAir);
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Adds the surcharge to the service rate
+        /// </summary>
+        private void AddSurcharge(UpsLocalServiceRate serviceRate, UpsSurchargeType surchargeType)
+        {
+            serviceRate.AddAmount(surcharges[surchargeType], EnumHelper.GetDescription(surchargeType));
+        }
+    }
+}
