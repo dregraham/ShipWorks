f9c1aa1306d8af8245e011f8162fc9d2b790a97e
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-05-04 14:49:40 -0500

    TP#16425 Create ShipperReleaseSurcharge and units

---
+++ Code/ShipWorks.Shipping.Tests/Carriers/Ups/LocalRating/Surcharges/ShipperReleaseSurchargeTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac.Extras.Moq;
+using Interapptive.Shared.Utility;
+using Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.Ups.LocalRating;
+using ShipWorks.Shipping.Carriers.Ups.LocalRating.Surcharges;
+using ShipWorks.Shipping.Carriers.UPS.LocalRating;
+using ShipWorks.Tests.Shared;
+using Xunit;
+
+namespace ShipWorks.Shipping.Tests.Carriers.UPS.LocalRating.Surcharges
+{
+    public class ShipperReleaseSurchargeTest : IDisposable
+    {
+        private readonly AutoMock mock;
+        private readonly ShipperReleaseSurcharge testObject;
+
+        public ShipperReleaseSurchargeTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+            testObject = new ShipperReleaseSurcharge(
+                new Dictionary<UpsSurchargeType, double> {{UpsSurchargeType.ShipperRelease, 123}});
+        }
+
+        [Fact]
+        public void Apply_AddsShipperReleaseAmountToServiceRate_WhenShipmentHasShipperRelease()
+        {
+            UpsShipmentEntity shipment = new UpsShipmentEntity(){ShipperRelease = true};
+            Mock<IUpsLocalServiceRate> serviceRate = mock.Mock<IUpsLocalServiceRate>();
+
+            testObject.Apply(shipment, serviceRate.Object);
+
+            serviceRate.Verify(r => r.AddAmount(123, EnumHelper.GetDescription(UpsSurchargeType.ShipperRelease)));
+        }
+
+        [Fact]
+        public void Apply_DoesNotAddShipperReleaseAmountToServiceRate_WhenShipmentDoesNotHaveShipperRelease()
+        {
+            UpsShipmentEntity shipment = new UpsShipmentEntity() { ShipperRelease = false };
+            Mock<IUpsLocalServiceRate> serviceRate = mock.Mock<IUpsLocalServiceRate>();
+
+            testObject.Apply(shipment, serviceRate.Object);
+
+            serviceRate.Verify(r => r.AddAmount(It.IsAny<decimal>(), It.IsAny<string>()), Times.Never);
+        }
+
+        public void Dispose()
+        {
+            mock?.Dispose();
+        }
+    }
+}
---
+++ Code/ShipWorks.Shipping/Carriers/Ups/LocalRating/Surcharges/ShipperReleaseSurcharge.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Interapptive.Shared.Utility;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.UPS.LocalRating;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating.Surcharges
+{
+    /// <summary>
+    /// Surcharge for Shipper Release
+    /// </summary>
+    public class ShipperReleaseSurcharge : IUpsSurcharge
+    {
+        private readonly IDictionary<UpsSurchargeType, double> surcharges;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="surcharges"></param>
+        public ShipperReleaseSurcharge(IDictionary<UpsSurchargeType, double> surcharges)
+        {
+            this.surcharges = surcharges;
+        }
+
+        /// <summary>
+        /// Apply the shipper release surcharge to the rate if applicable 
+        /// </summary>
+        public void Apply(UpsShipmentEntity shipment, IUpsLocalServiceRate serviceRate)
+        {
+            if (shipment.ShipperRelease)
+            {
+                serviceRate.AddAmount((decimal) surcharges[UpsSurchargeType.ShipperRelease],
+                    EnumHelper.GetDescription(UpsSurchargeType.ShipperRelease));
+            }
+        }
+    }
+}
