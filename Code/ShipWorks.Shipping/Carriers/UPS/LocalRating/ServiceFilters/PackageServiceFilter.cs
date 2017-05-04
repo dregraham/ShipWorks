22b3130e54833fb5de574e33a9932c9e45ded846
Kevin Croke <k.croke@shipworks.com>
2017-05-04 10:45:14 -0500

    TP#16425: Reformat PackageServiceFilter to conform to coding standards (mostly spacing)

--- Code/ShipWorks.Shipping/Carriers/UPS/LocalRating/ServiceFilters/PackageServiceFilter.cs
+++ Code/ShipWorks.Shipping/Carriers/UPS/LocalRating/ServiceFilters/PackageServiceFilter.cs
@@ -14,27 +14,28 @@ namespace ShipWorks.Shipping.Carriers.Ups.LocalRating.ServiceFilters
         /// <summary>
         /// Gets the list of eligible service types for the given shipment, from the given list of service types, based on packaging type
         /// </summary>
-        public IEnumerable<UpsServiceType> GetEligibleServices(UpsShipmentEntity shipment, IEnumerable<UpsServiceType> services)
+        public IEnumerable<UpsServiceType> GetEligibleServices(UpsShipmentEntity shipment,
+            IEnumerable<UpsServiceType> services)
         {
             List<UpsServiceType> eligibleServices = services as List<UpsServiceType> ?? services.ToList();
 
             foreach (UpsPackageEntity package in shipment.Packages)
             {
-                if (package.PackagingType != (int)UpsPackagingType.Custom)
+                if (package.PackagingType != (int) UpsPackagingType.Custom)
                 {
                     eligibleServices.Remove(UpsServiceType.UpsGround);
                     eligibleServices.Remove(UpsServiceType.Ups3DaySelect);
                 }
 
-                if (package.PackagingType != (int)UpsPackagingType.Custom &&
-                    package.PackagingType != (int)UpsPackagingType.Letter &&
-                    package.PackagingType != (int)UpsPackagingType.Tube &&
-                    package.PackagingType != (int)UpsPackagingType.BoxExpress &&
-                    package.PackagingType != (int)UpsPackagingType.BoxExpressSmall &&
-                    package.PackagingType != (int)UpsPackagingType.BoxExpressMedium &&
-                    package.PackagingType != (int)UpsPackagingType.BoxExpressLarge &&
-                    package.PackagingType != (int)UpsPackagingType.ExpressEnvelope &&
-                    package.PackagingType != (int)UpsPackagingType.Pak)
+                if (package.PackagingType != (int) UpsPackagingType.Custom &&
+                    package.PackagingType != (int) UpsPackagingType.Letter &&
+                    package.PackagingType != (int) UpsPackagingType.Tube &&
+                    package.PackagingType != (int) UpsPackagingType.BoxExpress &&
+                    package.PackagingType != (int) UpsPackagingType.BoxExpressSmall &&
+                    package.PackagingType != (int) UpsPackagingType.BoxExpressMedium &&
+                    package.PackagingType != (int) UpsPackagingType.BoxExpressLarge &&
+                    package.PackagingType != (int) UpsPackagingType.ExpressEnvelope &&
+                    package.PackagingType != (int) UpsPackagingType.Pak)
                 {
                     eligibleServices.Remove(UpsServiceType.Ups2DayAirAM);
                     eligibleServices.Remove(UpsServiceType.Ups2DayAir);
