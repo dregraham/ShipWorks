9515fe08a7844c581c2b522ebccd74650899c8af
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-05-04 15:14:29 -0500

    TP#16425 Create CarbonNeutralSurcharge and units

---
+++ Code/ShipWorks.Shipping.Tests/Carriers/Ups/LocalRating/Surcharges/CarbonNeutralSurchargeTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac.Extras.Moq;
+using Interapptive.Shared.Utility;
+using Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.Ups.LocalRating;
+using ShipWorks.Shipping.Carriers.Ups.LocalRating.Surcharges;
+using ShipWorks.Shipping.Carriers.UPS.Enums;
+using ShipWorks.Shipping.Carriers.UPS.LocalRating;
+using ShipWorks.Tests.Shared;
+using Xunit;
+
+namespace ShipWorks.Shipping.Tests.Carriers.UPS.LocalRating.Surcharges
+{
+    public class CarbonNeutralSurchargeTest : IDisposable
+    {
+        private readonly AutoMock mock;
+        private readonly CarbonNeutralSurcharge testObject;
+
+        public CarbonNeutralSurchargeTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+            testObject = new CarbonNeutralSurcharge(
+                new Dictionary<UpsSurchargeType, double>
+                {
+                    {UpsSurchargeType.CarbonNeutralAir, 123},
+                    {UpsSurchargeType.CarbonNeutralGround, 999}
+                });
+        }
+
+        [Fact]
+        public void Apply_AddsCarbonNeutralAirAmountToServiceRate_WhenShipmentHasCarbonNeutralAndServiceIsAir()
+        {
+            UpsShipmentEntity shipment =
+                new UpsShipmentEntity() {CarbonNeutral = true, Service = (int) UpsServiceType.UpsNextDayAir};
+            Mock<IUpsLocalServiceRate> serviceRate = mock.Mock<IUpsLocalServiceRate>();
+
+            testObject.Apply(shipment, serviceRate.Object);
+
+            serviceRate.Verify(r => r.AddAmount(123, EnumHelper.GetDescription(UpsSurchargeType.CarbonNeutralAir)));
+        }
+
+        [Fact]
+        public void Apply_AddsCarbonNeutralGroundAmountToServiceRate_WhenShipmentHasCarbonNeutralAndServiceIsGround()
+        {
+            UpsShipmentEntity shipment =
+                new UpsShipmentEntity() { CarbonNeutral = true, Service = (int)UpsServiceType.UpsGround };
+            Mock<IUpsLocalServiceRate> serviceRate = mock.Mock<IUpsLocalServiceRate>();
+
+            testObject.Apply(shipment, serviceRate.Object);
+
+            serviceRate.Verify(r => r.AddAmount(999, EnumHelper.GetDescription(UpsSurchargeType.CarbonNeutralGround)));
+        }
+
+        public void Dispose()
+        {
+            mock?.Dispose();
+        }
+    }
+}
---
+++ Code/ShipWorks.Shipping/Carriers/Ups/LocalRating/Surcharges/CarbonNeutralSurcharge.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Interapptive.Shared.Utility;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.UPS.Enums;
+using ShipWorks.Shipping.Carriers.UPS.LocalRating;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating.Surcharges
+{
+    /// <summary>
+    /// Surcharge for Carbon Neutral
+    /// </summary>
+    public class CarbonNeutralSurcharge : IUpsSurcharge
+    {
+        private readonly IDictionary<UpsSurchargeType, double> surcharges;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="surcharges"></param>
+        public CarbonNeutralSurcharge(IDictionary<UpsSurchargeType, double> surcharges)
+        {
+            this.surcharges = surcharges;
+        }
+
+        /// <summary>
+        /// Apply the carbon neutral surcharge to the rate if applicable 
+        /// </summary>
+        public void Apply(UpsShipmentEntity shipment, IUpsLocalServiceRate serviceRate)
+        {
+            if (shipment.CarbonNeutral)
+            {
+                if (shipment.Service == (int) UpsServiceType.UpsGround)
+                {
+                    serviceRate.AddAmount((decimal)surcharges[UpsSurchargeType.CarbonNeutralGround],
+                        EnumHelper.GetDescription(UpsSurchargeType.CarbonNeutralGround));
+                }
+                else
+                {
+                    serviceRate.AddAmount((decimal)surcharges[UpsSurchargeType.CarbonNeutralAir],
+                        EnumHelper.GetDescription(UpsSurchargeType.CarbonNeutralAir));
+                }
+            }
+        }
+    }
+}
