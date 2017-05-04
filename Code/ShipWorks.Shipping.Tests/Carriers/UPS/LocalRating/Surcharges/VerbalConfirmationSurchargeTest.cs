0172648b37fae9c618a46e27f816105fbe12404c
Kevin Croke <k.croke@shipworks.com>
2017-05-04 16:47:55 -0500

    TP#16425: Add VerbalConfirmationSurcharge

---
+++ Code/ShipWorks.Shipping.Tests/Carriers/UPS/LocalRating/Surcharges/VerbalConfirmationSurchargeTest.cs
@@ -0,0 +1,77 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac.Extras.Moq;
+using Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.Ups.LocalRating;
+using ShipWorks.Shipping.Carriers.Ups.LocalRating.Surcharges;
+using ShipWorks.Shipping.Carriers.UPS.LocalRating;
+using ShipWorks.Tests.Shared;
+using Xunit;
+
+namespace ShipWorks.Shipping.Tests.Carriers.UPS.LocalRating.Surcharges
+{
+    public class VerbalConfirmationSurchargeTest : IDisposable
+    {
+        readonly AutoMock mock;
+
+        public VerbalConfirmationSurchargeTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+        }
+
+        [Theory]
+        [InlineData(1, 10)]
+        [InlineData(10, 100)]
+        public void Apply_CorrectSurchargeApplied(int applicablePackages, decimal amount)
+        {
+            var packageWithConfirmation = new UpsPackageEntity()
+            {
+                VerbalConfirmationEnabled = true
+            };
+
+            var packageWithoutConfirmation = new UpsPackageEntity();
+
+            var shipment = new UpsShipmentEntity();
+            shipment.Packages.AddRange(Enumerable.Repeat(packageWithoutConfirmation, 5));
+            shipment.Packages.AddRange(Enumerable.Repeat(packageWithConfirmation, applicablePackages));
+            shipment.Packages.AddRange(Enumerable.Repeat(packageWithoutConfirmation, 5));
+
+            var surcharges = new Dictionary<UpsSurchargeType, double>() {{UpsSurchargeType.VerbalConfirmation, 10}};
+
+            var localServiceRate = mock.CreateMock<IUpsLocalServiceRate>();
+            var testObject = new VerbalConfirmationSurcharge(surcharges);
+
+            testObject.Apply(shipment, localServiceRate.Object);
+
+            localServiceRate.Verify(r => r.AddAmount(amount, $"{applicablePackages} Package(s) with Verbal Confirmation"));
+        }
+
+        [Fact]
+        public void Apply_SurchargeNotApplied()
+        {
+            var packageWithoutConfirmation = new UpsPackageEntity();
+
+            var shipment = new UpsShipmentEntity();
+            shipment.Packages.AddRange(Enumerable.Repeat(packageWithoutConfirmation, 5));
+
+            var surcharges = new Dictionary<UpsSurchargeType, double>() { { UpsSurchargeType.VerbalConfirmation, 10 } };
+
+            var localServiceRate = mock.CreateMock<IUpsLocalServiceRate>();
+            var testObject = new VerbalConfirmationSurcharge(surcharges);
+
+            testObject.Apply(shipment, localServiceRate.Object);
+
+            localServiceRate.Verify(r => r.AddAmount(It.IsAny<decimal>(), It.IsAny<string>()), Times.Never);
+        }
+
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+
+    }
+}
---
+++ Code/ShipWorks.Shipping/Carriers/UPS/LocalRating/Surcharges/VerbalConfirmationSurcharge.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.UPS.Enums;
+using ShipWorks.Shipping.Carriers.UPS.LocalRating;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating.Surcharges
+{
+    /// <summary>
+    /// Applies a Signature Surcharge to the service rate based on the shipment
+    /// </summary>
+    /// <seealso cref="ShipWorks.Shipping.Carriers.Ups.LocalRating.Surcharges.IUpsSurcharge" />
+    public class VerbalConfirmationSurcharge : IUpsSurcharge
+    {
+        private readonly IDictionary<UpsSurchargeType, double> surcharges;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SignatureSurcharge"/> class.
+        /// </summary>
+        public VerbalConfirmationSurcharge(IDictionary<UpsSurchargeType, double> surcharges)
+        {
+            this.surcharges = surcharges;
+        }
+
+        /// <summary>
+        /// Apply the surcharge to the service rate based on the shipment
+        /// </summary>
+        public void Apply(UpsShipmentEntity shipment, IUpsLocalServiceRate serviceRate)
+        {
+            int applicablePackages = shipment.Packages.Count(ChargeApplies);
+
+            if (applicablePackages > 0)
+            {
+                decimal rate = (decimal) surcharges[UpsSurchargeType.VerbalConfirmation];
+                decimal surcharge = rate * applicablePackages;
+
+                serviceRate.AddAmount(surcharge, $"{applicablePackages} Package(s) with Verbal Confirmation");
+            }
+        }
+
+        /// <summary>
+        /// Returns true if charge applies
+        /// </summary>
+        private static bool ChargeApplies(UpsPackageEntity package)
+            => package.VerbalConfirmationEnabled;
+    }
+}
