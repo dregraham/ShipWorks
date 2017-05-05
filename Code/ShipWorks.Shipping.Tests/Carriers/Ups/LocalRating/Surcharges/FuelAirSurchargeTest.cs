da1940bc20919b9ebb5f1b0b15d5f7cbd3232c59
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-05-05 11:30:25 -0500

    TP#16425 create FuelAirSurcharge and units

---
+++ Code/ShipWorks.Shipping.Tests/Carriers/Ups/LocalRating/Surcharges/FuelAirSurchargeTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Extras.Moq;
+using ShipWorks.Shipping.Carriers.Ups.LocalRating;
+using ShipWorks.Shipping.Carriers.Ups.LocalRating.Surcharges;
+using ShipWorks.Shipping.Carriers.UPS.Enums;
+using ShipWorks.Shipping.Carriers.UPS.LocalRating;
+using ShipWorks.Tests.Shared;
+using Xunit;
+
+namespace ShipWorks.Shipping.Tests.Carriers.UPS.LocalRating.Surcharges
+{
+    public class FuelAirSurchargeTest : IDisposable
+    {
+        readonly AutoMock mock;
+        private readonly IDictionary<UpsSurchargeType, double> surcharges;
+        private readonly FuelAirSurcharge testObject;
+        
+        public FuelAirSurchargeTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+            surcharges = new Dictionary<UpsSurchargeType, double> {[UpsSurchargeType.FuelAir] = .0525};
+            testObject = new FuelAirSurcharge(surcharges);
+        }
+
+        [Fact]
+        public void Apply_FuelAirSurchargeIsAppliedToServiceRate_WhenRateIsAir()
+        {
+            UpsLocalServiceRate serviceRate = new UpsLocalServiceRate(UpsServiceType.UpsNextDayAir, 100, true, null);
+            testObject.Apply(null, serviceRate);
+
+            Assert.Equal(105.25M, serviceRate.Amount);
+        }
+
+        [Fact]
+        public void Apply_FuelAirSurchargeIsNotAppliedToServiceRate_WhenRateIsNotAir()
+        {
+            UpsLocalServiceRate serviceRate = new UpsLocalServiceRate(UpsServiceType.UpsGround, 100, true, null);
+            testObject.Apply(null, serviceRate);
+
+            Assert.Equal(100M, serviceRate.Amount);
+        }
+
+        [Fact]
+        public void Apply_CallsAddAmountWithAirFuelSurchargeText()
+        {
+            UpsLocalServiceRate serviceRate = new UpsLocalServiceRate(UpsServiceType.UpsNextDayAir, 100, true, null);
+            testObject.Apply(null, serviceRate);
+
+            var stringBuilder = new StringBuilder();
+            serviceRate.Log(stringBuilder);
+            Assert.Contains("Air Fuel Surcharge", stringBuilder.ToString());
+        }
+
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+
+    }
+}
---
+++ Code/ShipWorks.Shipping/Carriers/Ups/LocalRating/Surcharges/FuelAirSurcharge.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.UPS.Enums;
+using ShipWorks.Shipping.Carriers.UPS.LocalRating;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating.Surcharges
+{
+    /// <summary>
+    /// Applies fuel surcharge to air service rate
+    /// </summary>
+    public class FuelAirSurcharge : IUpsSurcharge
+    {
+        private readonly IDictionary<UpsSurchargeType, double> surcharges;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public FuelAirSurcharge(IDictionary<UpsSurchargeType, double> surcharges)
+        {
+            this.surcharges = surcharges;
+        }
+
+        /// <summary>
+        /// Apply the surcharge to the service rate based on the shipment
+        /// </summary>
+        public void Apply(UpsShipmentEntity shipment, IUpsLocalServiceRate serviceRate)
+        {
+            if (serviceRate.Service != UpsServiceType.UpsGround)
+            {
+                decimal fuelSurchargeRate = (decimal) surcharges[UpsSurchargeType.FuelAir];
+                decimal fuelSurcharge = Math.Round(serviceRate.Amount * fuelSurchargeRate, 2);
+
+                serviceRate.AddAmount(fuelSurcharge, "Air Fuel Surcharge");
+            }
+        }
+    }
+}
