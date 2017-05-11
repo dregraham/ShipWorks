ecca00eb1d72f9b019b0e5c57a955dc6dbc7fbd2
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-05-11 10:22:34 -0500

    TP#16425 refactor readers into factory

---
+++ Code/ShipWorks.Shipping/Carriers/Ups/LocalRating/UpsExcelReaderFactory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using ShipWorks.ApplicationCore.ComponentRegistration;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating
+{
+    /// <summary>
+    /// Factory for creating Ups Local Rate Excel Readers
+    /// </summary>
+    [Component]
+    public class UpsLocalRateExcelReaderFactory : IUpsLocalRateExcelReaderFactory
+    {
+        /// <summary>
+        /// Create the rate excel readers
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<IUpsRateExcelReader> CreateRateExcelReaders()
+        {
+            return new IUpsRateExcelReader[]
+            {
+                new ServiceUpsRateExcelReader(),
+                new ServiceUpsRateExcelReader()
+            };
+        }
+
+        /// <summary>
+        /// Create the zone excel readers
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<IUpsZoneExcelReader> CreateZoneExcelReaders()
+        {
+            return new IUpsZoneExcelReader[]
+            {
+                new UpsZoneExcelReader(new AlaskaHawaiiZoneExcelReader()),
+                new DeliveryAreaSurchargeUpsZoneExcelReader()
+            };
+        }
+    }
+}
