871f9e2a83a014d4efdccba0255493c9c7cfe09d
Tim Hughes <t.hughes@shipworks.com>
2017-05-19 10:36:21 -0500

    TP#17888: Added upgrade script for UpsProfile CustomsDescription

---
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.13/5.13.0.0.sql
@@ -0,0 +1,18 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Adding [CustomsDescription] to [dbo].[UpsProfile]'
+GO
+IF NOT EXISTS(SELECT * FROM sys.columns WHERE Name= N'CustomsDescription' AND Object_ID = Object_ID(N'UpsProfile'))
+BEGIN
+   	ALTER TABLE [dbo].[UpsProfile] ADD [CustomsDescription] [nvarchar] (150) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+END
+GO
+PRINT N'Updating primary UPS Profile'
+GO
+UPDATE [dbo].UpsProfile
+SET 
+	[CustomsDescription] = ''
+WHERE ShippingProfileID in (SELECT ShippingProfileID FROM ShippingProfile WHERE ShipmentType IN (0,1) AND ShipmentTypePrimary = 1)
+GO
