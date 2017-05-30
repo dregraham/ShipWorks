3827dbdf1fab43095220f23c4a35f68b8555f7b3
Kevin Croke <k.croke@shipworks.com>
2017-05-30 08:40:59 -0500

    TP#16428: Add ILocalRateValidationResult

---
+++ Code/ShipWorks.Shipping/Carriers/UPS/LocalRating/Validation/ILocalRateValidationResult.cs
@@ -0,0 +1,18 @@
+﻿namespace ShipWorks.Shipping.Carriers.Ups.LocalRating.Validation
+{
+    /// <summary>
+    /// The result of a validation
+    /// </summary>
+    public interface ILocalRateValidationResult
+    {
+        /// <summary>
+        /// Gets the validation message.
+        /// </summary>
+        string Message { get; }
+
+        /// <summary>
+        /// Shows the validation message.
+        /// </summary>
+        void ShowMessage();
+    }
+}
