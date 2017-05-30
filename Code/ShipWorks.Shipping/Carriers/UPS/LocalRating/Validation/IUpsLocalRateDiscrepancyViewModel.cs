e65e5a6e63657451ffef6d2b7b53ba084c041fc9
Kevin Croke <k.croke@shipworks.com>
2017-05-30 09:01:03 -0500

    TP#16428: Add IUpsLocalRateDiscrepancyViewModel

---
+++ Code/ShipWorks.Shipping/Carriers/UPS/LocalRating/Validation/IUpsLocalRateDiscrepancyViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ShipWorks.Shipping.Carriers.Ups.LocalRating.Validation
+{
+    /// <summary>
+    /// ViewModel for the UpsLocalRateDiscrepancyDialog
+    /// </summary>
+    interface IUpsLocalRateDiscrepancyViewModel
+    {
+        /// <summary>
+        /// The validation message
+        /// </summary>
+        string Message { get; }
+
+        /// <summary>
+        /// Url to a help article
+        /// </summary>
+        Uri HelpArticleUrl { get; }
+
+        /// <summary>
+        /// Gets or sets the method to close the window.
+        /// If user cancels, we will pass in false, else pass in true
+        /// </summary>
+        Action<bool> Close { get; set; }
+        
+        /// <summary>
+        /// Snoozes validation so the user is not warned at every validation error
+        /// </summary>
+        ICommand SnoozeClickCommand();
+
+        /// <summary>
+        /// Closes the dialog
+        /// </summary>
+        ICommand CloseClickCommand();
+    }
+}
