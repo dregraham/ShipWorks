037e3d1205d6e34c9040d03961154759e285053c
Kevin Croke <k.croke@shipworks.com>
2017-06-06 15:26:46 -0500

    TP#17501: Updating usings after merge

--- Code/ShipWorks.Shipping/Carriers/UPS/LocalRating/Validation/UpsLocalRateValidator.cs
+++ Code/ShipWorks.Shipping/Carriers/UPS/LocalRating/Validation/UpsLocalRateValidator.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 using Autofac.Features.Indexed;
 using Interapptive.Shared.Net;
 using Interapptive.Shared.Utility;
-using ShipWorks.ApplicationCore.ComponentRegistration;
+using Interapptive.Shared.ComponentRegistration;
 using ShipWorks.ApplicationCore.Logging;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Data.Model.EntityInterfaces;
