8a1a94f2981c7bbe1dc00b867ea97085f4de690b
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-06-15 10:05:46 -0500

    TP#17553 fix jenkins build, remove properties that hide inherited properties

--- Code/Interapptive.Shared/Metrics/ITrackedDurationEvent.cs
+++ Code/Interapptive.Shared/Metrics/ITrackedDurationEvent.cs
@@ -1,20 +1,9 @@
-﻿using System;
-
-namespace Interapptive.Shared.Metrics
+﻿namespace Interapptive.Shared.Metrics
 {
     /// <summary>
     /// Track the duration of an event, along with other metric info
     /// </summary>
     public interface ITrackedDurationEvent : ITrackedEvent
     {
-        /// <summary>
-        /// Add a metric value to the event
-        /// </summary>
-        void AddMetric(string metricName, double metricValue);
-
-        /// <summary>
-        /// Adds a property value to the event.
-        /// </summary>
-        void AddProperty(string propertyName, string propertyValue);
     }
 }
