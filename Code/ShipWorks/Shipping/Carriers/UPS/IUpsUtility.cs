27549b8adc38f2b82ec1858c30684265ccfd4da1
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-06-21 15:39:24 -0500

    TP#18437 add missing comments

--- Code/ShipWorks/Shipping/Carriers/UPS/IUpsUtility.cs
+++ Code/ShipWorks/Shipping/Carriers/UPS/IUpsUtility.cs
@@ -2,9 +2,19 @@
 
 namespace ShipWorks.Shipping.Carriers.UPS
 {
+    /// <summary>
+    /// Interface that represents the static UpsUtility
+    /// </summary>
     public interface IUpsUtility
     {
+        /// <summary>
+        /// Get the global instanced UPS access key
+        /// </summary>
         string FetchAndSaveUpsAccessKey(UpsAccountEntity upsAccount, string upsLicense);
+
+        /// <summary>
+        /// Get the Ups AccessLicense text
+        /// </summary>
         string GetAccessLicenseText();
     }
 }
--- Code/ShipWorks/Shipping/Carriers/UPS/UpsUtilityWrapper.cs
+++ Code/ShipWorks/Shipping/Carriers/UPS/UpsUtilityWrapper.cs
@@ -3,14 +3,23 @@ using ShipWorks.Data.Model.EntityClasses;
 
 namespace ShipWorks.Shipping.Carriers.UPS
 {
+    /// <summary>
+    /// wrapper for the static UpsUtility
+    /// </summary>
     [Component]
     public class UpsUtilityWrapper : IUpsUtility
     {
+        /// <summary>
+        /// Get the global instanced UPS access key
+        /// </summary>
         public string FetchAndSaveUpsAccessKey(UpsAccountEntity upsAccount, string upsLicense)
         {
             return UpsUtility.FetchAndSaveUpsAccessKey(upsAccount, upsLicense);
         }
 
+        /// <summary>
+        /// Get the Ups AccessLicense text
+        /// </summary>
         public string GetAccessLicenseText()
         {
             return UpsUtility.GetAccessLicenseText();
