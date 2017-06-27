41885bec82f402470bcaf3f7e1e918f09e03046b
Josh Winchester <j.winchester@shipworks.com>
2017-06-27 12:02:13 -0500

    TP#18214: Adding interface for combine order validator

---
+++ Code/ShipWorks/Stores/Content/IOrderCombineValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Interapptive.Shared.Utility;
+
+namespace ShipWorks.Stores.Content
+{
+    public interface IOrderCombineValidator
+    {
+        /// <summary>
+        /// Validate the orders selected for combining
+        /// </summary>
+        Result Validate(IEnumerable<long> validate);
+    }
+}
