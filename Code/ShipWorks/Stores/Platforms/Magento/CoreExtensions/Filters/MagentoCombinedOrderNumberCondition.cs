ff2f9fc58dad8b58d24eb6e3a536c975a9e5fe87
Andrew Benz <a.benz@shipworks.com>
2017-06-30 12:36:47 -0500

    TP#18215: Add Magento order search conditions

---
+++ Code/ShipWorks/Stores/Platforms/Magento/CoreExtensions/Filters/MagentoCombinedOrderNumberCondition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Interapptive.Shared.ComponentRegistration;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+using ShipWorks.Filters.Content.SqlGeneration;
+
+namespace ShipWorks.Stores.Platforms.Magento.CoreExtensions.Filters.Orders
+{
+    /// <summary>
+    /// Condition for the Magento order numbers that were combined into a single order
+    /// </summary>
+    [ConditionElement("Magento Order #", "Magento.CombinedOrderNumbers")]
+    [ConditionStoreType(StoreTypeCode.Magento)]
+    [Component]
+    public class MagentoCombinedOrderNumberCondition : NumericStringCondition<long>, ICombinedOrderCondition
+    {
+        /// <summary>
+        /// Generate the filter SQL
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            string orderSearchSql = String.Empty;
+
+            EntityField2 searchField = IsNumeric ? MagentoOrderSearchFields.OrderNumber : MagentoOrderSearchFields.OrderNumberComplete;
+
+            // Add any combined order number/number complete entries.
+            using (SqlGenerationScope scope = context.PushScope(OrderFields.OrderID, MagentoOrderSearchFields.OrderID, SqlGenerationScopeType.AnyChild))
+            {
+                orderSearchSql = scope.Adorn(GenerateSql(context.GetColumnReference(searchField), context));
+            }
+
+            return orderSearchSql;
+        }
+    }
+}
