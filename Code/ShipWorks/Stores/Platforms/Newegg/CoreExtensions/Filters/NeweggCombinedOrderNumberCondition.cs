7a0d3edca1426c457357d41d8d1c37547f967a7e
Andrew Benz <a.benz@shipworks.com>
2017-06-30 12:44:02 -0500

    TP#18215: Add Newegg order search conditions

---
+++ Code/ShipWorks/Stores/Platforms/Newegg/CoreExtensions/Filters/NeweggCombinedOrderNumberCondition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Interapptive.Shared.ComponentRegistration;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+using ShipWorks.Filters.Content.SqlGeneration;
+
+namespace ShipWorks.Stores.Platforms.Newegg.CoreExtensions.Filters.Orders
+{
+    /// <summary>
+    /// Condition for the Newegg order numbers that were combined into a single order
+    /// </summary>
+    [ConditionElement("Newegg Order #", "Newegg.CombinedOrderNumbers")]
+    [ConditionStoreType(StoreTypeCode.NeweggMarketplace)]
+    [Component]
+    public class NeweggCombinedOrderNumberCondition : NumericStringCondition<long>, ICombinedOrderCondition
+    {
+        /// <summary>
+        /// Generate the filter SQL
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            string orderSearchSql = String.Empty;
+
+            EntityField2 searchField = IsNumeric ? NeweggOrderSearchFields.OrderNumber : NeweggOrderSearchFields.OrderNumberComplete;
+
+            // Add any combined order number/number complete entries.
+            using (SqlGenerationScope scope = context.PushScope(OrderFields.OrderID, NeweggOrderSearchFields.OrderID, SqlGenerationScopeType.AnyChild))
+            {
+                orderSearchSql = scope.Adorn(GenerateSql(context.GetColumnReference(searchField), context));
+            }
+
+            return orderSearchSql;
+        }
+    }
+}
