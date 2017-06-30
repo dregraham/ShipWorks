d008cc8b6a89dda4fea623f50c98462f8cc11eaa
Andrew Benz <a.benz@shipworks.com>
2017-06-30 12:32:07 -0500

    TP#18215: Add Lemon Stand order search conditions

---
+++ Code/ShipWorks.Stores/Platforms/LemonStand/CoreExtensions/Filters/LemonStandCombinedOrderNumberCondition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Interapptive.Shared.ComponentRegistration;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+using ShipWorks.Filters.Content.SqlGeneration;
+
+namespace ShipWorks.Stores.Platforms.LemonStand.CoreExtensions.Filters.Orders
+{
+    /// <summary>
+    /// Condition for the LemonStand order numbers that were combined into a single order
+    /// </summary>
+    [ConditionElement("Lemon Stand Order #", "LemonStand.CombinedOrderNumbers")]
+    [ConditionStoreType(StoreTypeCode.LemonStand)]
+    [Component]
+    public class LemonStandCombinedOrderNumberCondition : NumericStringCondition<long>, ICombinedOrderCondition
+    {
+        /// <summary>
+        /// Generate the filter SQL
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            string orderSearchSql = String.Empty;
+
+            EntityField2 searchField = IsNumeric ? LemonStandOrderSearchFields.OrderNumber : LemonStandOrderSearchFields.OrderNumberComplete;
+
+            // Add any combined order number/number complete entries.
+            using (SqlGenerationScope scope = context.PushScope(OrderFields.OrderID, LemonStandOrderSearchFields.OrderID, SqlGenerationScopeType.AnyChild))
+            {
+                orderSearchSql = scope.Adorn(GenerateSql(context.GetColumnReference(searchField), context));
+            }
+
+            return orderSearchSql;
+        }
+    }
+}
