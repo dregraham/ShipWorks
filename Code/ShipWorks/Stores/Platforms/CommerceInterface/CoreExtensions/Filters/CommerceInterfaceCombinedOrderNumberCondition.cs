efaa20fd27bea611fd5e6bbbcb4630aa91968c2d
Andrew Benz <a.benz@shipworks.com>
2017-06-30 12:16:56 -0500

    TP#18215: Add CommerceInterface order search conditions

---
+++ Code/ShipWorks/Stores/Platforms/CommerceInterface/CoreExtensions/Filters/CommerceInterfaceCombinedOrderNumberCondition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Interapptive.Shared.ComponentRegistration;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+using ShipWorks.Filters.Content.SqlGeneration;
+
+namespace ShipWorks.Stores.Platforms.CommerceInterface.CoreExtensions.Filters.Orders
+{
+    /// <summary>
+    /// Condition for the CommerceInterface order numbers that were combined into a single order
+    /// </summary>
+    [ConditionElement("CommerceInterface Order #", "CommerceInterface.CombinedOrderNumbers")]
+    [ConditionStoreType(StoreTypeCode.CommerceInterface)]
+    [Component]
+    public class CommerceInterfaceCombinedOrderNumberCondition : NumericStringCondition<long>, ICombinedOrderCondition
+    {
+        /// <summary>
+        /// Generate the filter SQL
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            string orderSearchSql = String.Empty;
+
+            EntityField2 searchField = IsNumeric ? CommerceInterfaceOrderSearchFields.OrderNumber : CommerceInterfaceOrderSearchFields.OrderNumberComplete;
+
+            // Add any combined order number/number complete entries.
+            using (SqlGenerationScope scope = context.PushScope(OrderFields.OrderID, CommerceInterfaceOrderSearchFields.OrderID, SqlGenerationScopeType.AnyChild))
+            {
+                orderSearchSql = scope.Adorn(GenerateSql(context.GetColumnReference(searchField), context));
+            }
+
+            return orderSearchSql;
+        }
+    }
+}
--- Code/ShipWorks/Stores/Platforms/CommerceInterface/CoreExtensions/Filters/CommerceInterfaceNumberCondition.cs
+++ Code/ShipWorks/Stores/Platforms/CommerceInterface/CoreExtensions/Filters/CommerceInterfaceNumberCondition.cs
@@ -1,25 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using ShipWorks.Data.Model.HelperClasses;
 using ShipWorks.Filters.Content;
 using ShipWorks.Filters.Content.Conditions;
 using ShipWorks.Filters.Content.SqlGeneration;
-using ShipWorks.Data.Model.HelperClasses;
 
 namespace ShipWorks.Stores.Platforms.CommerceInterface.CoreExtensions.Filters
 {
+    /// <summary>
+    /// Condition for the CommerceInterface OrderNumber field
+    /// </summary>
     [ConditionElement("CommerceInterface #", "CommerceInterfaceOrder.CommerceInterfaceOrderNumber")]
     [ConditionStoreType(StoreTypeCode.CommerceInterface)]
     public class CommerceInterfaceNumberCondition : StringCondition
     {
+        /// <summary>
+        /// Generate the filter SQL
+        /// </summary>
         public override string GenerateSql(SqlGenerationContext context)
         {
+            string orderSql = String.Empty;
+            string orderSearchSql = String.Empty;
+
             // first get from Order -> CommerceInterfaceOrder
             using (SqlGenerationScope scope = context.PushScope(OrderFields.OrderID, CommerceInterfaceOrderFields.OrderID, SqlGenerationScopeType.AnyChild))
             {
-                return scope.Adorn(GenerateSql(context.GetColumnReference(CommerceInterfaceOrderFields.CommerceInterfaceOrderNumber), context));
+                orderSql = scope.Adorn(GenerateSql(context.GetColumnReference(CommerceInterfaceOrderFields.CommerceInterfaceOrderNumber), context));
             }
+
+            // Combined order search
+            using (SqlGenerationScope scope = context.PushScope(OrderSearchFields.OrderID, CommerceInterfaceOrderSearchFields.OrderID, SqlGenerationScopeType.AnyChild))
+            {
+                orderSearchSql = scope.Adorn(GenerateSql(context.GetColumnReference(CommerceInterfaceOrderSearchFields.CommerceInterfaceOrderNumber), context));
+            }
+
+            return $"{orderSql} OR {orderSearchSql}";
         }
     }
 }
