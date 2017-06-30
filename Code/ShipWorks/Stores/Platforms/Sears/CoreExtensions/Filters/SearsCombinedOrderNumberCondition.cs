12ad7478a2dbe1f4d1039c840fbdf7a130ce0b98
Andrew Benz <a.benz@shipworks.com>
2017-06-30 13:03:58 -0500

    TP#18215: Add Sears order search conditions

---
+++ Code/ShipWorks/Stores/Platforms/Sears/CoreExtensions/Filters/SearsCombinedOrderNumberCondition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Interapptive.Shared.ComponentRegistration;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+using ShipWorks.Filters.Content.SqlGeneration;
+
+namespace ShipWorks.Stores.Platforms.Sears.CoreExtensions.Filters.Orders
+{
+    /// <summary>
+    /// Condition for the Sears order numbers that were combined into a single order
+    /// </summary>
+    [ConditionElement("Sears Order #", "Sears.CombinedOrderNumbers")]
+    [ConditionStoreType(StoreTypeCode.Sears)]
+    [Component]
+    public class SearsCombinedOrderNumberCondition : NumericStringCondition<long>, ICombinedOrderCondition
+    {
+        /// <summary>
+        /// Generate the filter SQL
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            string orderSearchSql = String.Empty;
+
+            EntityField2 searchField = IsNumeric ? SearsOrderSearchFields.OrderNumber : SearsOrderSearchFields.OrderNumberComplete;
+
+            // Add any combined order number/number complete entries.
+            using (SqlGenerationScope scope = context.PushScope(OrderFields.OrderID, SearsOrderSearchFields.OrderID, SqlGenerationScopeType.AnyChild))
+            {
+                orderSearchSql = scope.Adorn(GenerateSql(context.GetColumnReference(searchField), context));
+            }
+
+            return orderSearchSql;
+        }
+    }
+}
--- Code/ShipWorks/Stores/Platforms/Sears/CoreExtensions/Filters/SearsPoNumberCondition.cs
+++ Code/ShipWorks/Stores/Platforms/Sears/CoreExtensions/Filters/SearsPoNumberCondition.cs
@@ -1,25 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using ShipWorks.Data.Model.HelperClasses;
 using ShipWorks.Filters.Content;
 using ShipWorks.Filters.Content.Conditions;
 using ShipWorks.Filters.Content.SqlGeneration;
-using ShipWorks.Data.Model.HelperClasses;
 
 namespace ShipWorks.Stores.Platforms.Sears.CoreExtensions.Filters
 {
+    /// <summary>
+    /// Condition for the Sears PO Number field
+    /// </summary>
     [ConditionElement("Sears PO Number", "SearsOrder.PoNumber")]
     [ConditionStoreType(StoreTypeCode.Sears)]
     public class SearsPoNumberCondition : StringCondition
     {
+        /// <summary>
+        /// Generate the filter SQL
+        /// </summary>
         public override string GenerateSql(SqlGenerationContext context)
         {
+            string orderSql = String.Empty;
+            string orderSearchSql = String.Empty;
+
             // first get from Order -> SearsOrder
             using (SqlGenerationScope scope = context.PushScope(OrderFields.OrderID, SearsOrderFields.OrderID, SqlGenerationScopeType.AnyChild))
             {
-                return scope.Adorn(GenerateSql(context.GetColumnReference(SearsOrderFields.PoNumber), context));
+                orderSql = scope.Adorn(GenerateSql(context.GetColumnReference(SearsOrderFields.PoNumber), context));
             }
+
+            using (SqlGenerationScope scope = context.PushScope(OrderSearchFields.OrderID, SearsOrderSearchFields.OrderID, SqlGenerationScopeType.AnyChild))
+            {
+                orderSearchSql = scope.Adorn(GenerateSql(context.GetColumnReference(SearsOrderSearchFields.PoNumber), context));
+            }
+
+            return $"{orderSql} OR {orderSearchSql}";
         }
     }
 }
