cbffe2835ae50ffae12dd224f7b48a82a55d1bbd
Tim Hughes <t.hughes@shipworks.com>
2017-06-30 06:48:32 -0500

    TP#18215: Added combined order number condition to search the OrderSearch table and Amazon combined number condition to search AmazonOrderSearch.

--- Code/ShipWorks/Filters/Content/Conditions/Orders/OrderNumberCondition.cs
+++ Code/ShipWorks/Filters/Content/Conditions/Orders/OrderNumberCondition.cs
@@ -1,5 +1,8 @@
-﻿using ShipWorks.Filters.Content.SqlGeneration;
+﻿using System.Text;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Filters.Content.SqlGeneration;
 using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Stores.Platforms.Amazon.CoreExtensions.Filters;
 
 namespace ShipWorks.Filters.Content.Conditions.Orders
 {
@@ -9,19 +12,70 @@ namespace ShipWorks.Filters.Content.Conditions.Orders
     [ConditionElement("Order Number", "Order.Number")]
     public class OrderNumberCondition : NumericStringCondition<long>
     {
+        private EntityField2 orderNumberField = null;
+        private EntityField2 orderNumberCompleteField = null;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public OrderNumberCondition()
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public OrderNumberCondition(EntityField2 orderNumberField, EntityField2 orderNumberCompleteField)
+        {
+            this.orderNumberField = orderNumberField;
+            this.orderNumberCompleteField = orderNumberCompleteField;
+        }
+
         /// <summary>
         /// Generate the SQL that evaluates the condition
         /// </summary>
         public override string GenerateSql(SqlGenerationContext context)
         {
+            string orderNumberSql = string.Empty;
+            string orderSearchSql = string.Empty;
+
             if (IsNumeric)
             {
-                return GenerateSql(context.GetColumnReference(OrderFields.OrderNumber), context);
+                orderNumberField = orderNumberField ?? OrderFields.OrderNumber;
+                orderNumberSql = GenerateSql(context.GetColumnReference(orderNumberField), context);
+
+                CombinedOrderNumberCondition combinedOrderNumberCondition = new CombinedOrderNumberCondition(OrderSearchFields.OrderNumber);
+                combinedOrderNumberCondition.Operator = Operator;
+                combinedOrderNumberCondition.Value1 = Value1;
+                combinedOrderNumberCondition.Value2 = Value2;
+                orderSearchSql = $" OR {combinedOrderNumberCondition.GenerateSql(context)}";
+
+                AmazonCombinedOrderNumberCondition amazonCombinedOrderNumberCondition = new AmazonCombinedOrderNumberCondition(AmazonOrderSearchFields.OrderNumber);
+                amazonCombinedOrderNumberCondition.Operator = Operator;
+                amazonCombinedOrderNumberCondition.Value1 = Value1;
+                amazonCombinedOrderNumberCondition.Value2 = Value2;
+                orderSearchSql = $" {orderSearchSql} OR {amazonCombinedOrderNumberCondition.GenerateSql(context)}";
             }
             else
             {
-                return StringCondition.GenerateSql(StringValue, StringOperator, context.GetColumnReference(OrderFields.OrderNumberComplete), context);
+                orderNumberCompleteField = orderNumberCompleteField ?? OrderFields.OrderNumberComplete;
+                orderNumberSql = StringCondition.GenerateSql(StringValue, StringOperator, context.GetColumnReference(orderNumberCompleteField), context);
+                
+                CombinedOrderNumberCondition combinedOrderNumberCondition = new CombinedOrderNumberCondition(OrderSearchFields.OrderNumberComplete);
+                combinedOrderNumberCondition.IsNumeric = IsNumeric;
+                combinedOrderNumberCondition.StringOperator = StringOperator;
+                combinedOrderNumberCondition.StringValue = StringValue;
+                orderSearchSql = $" OR {combinedOrderNumberCondition.GenerateSql(context)}";
+
+                AmazonCombinedOrderNumberCondition amazonCombinedOrderNumberCondition = new AmazonCombinedOrderNumberCondition(AmazonOrderSearchFields.OrderNumberComplete);
+                amazonCombinedOrderNumberCondition.IsNumeric = IsNumeric;
+                amazonCombinedOrderNumberCondition.StringOperator = StringOperator;
+                amazonCombinedOrderNumberCondition.StringValue = StringValue;
+
+                orderSearchSql = $" {orderSearchSql} OR {amazonCombinedOrderNumberCondition.GenerateSql(context)}";
             }
+
+            return $"{orderNumberSql} {orderSearchSql}";
         }
     }
 }
---
+++ Code/ShipWorks/Stores/Platforms/Amazon/CoreExtensions/Filters/AmazonCombinedOrderNumberCondition.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+using ShipWorks.Filters.Content.SqlGeneration;
+using ShipWorks.Data.Model.HelperClasses;
+
+namespace ShipWorks.Stores.Platforms.Amazon.CoreExtensions.Filters
+{
+    /// <summary>
+    /// Condition for the Amazon order numbers that were combined into a single order
+    /// </summary>
+    [ConditionElement("Amazon Order #", "Amazon.CombinedOrderNumbers")]
+    [ConditionStoreType(StoreTypeCode.Amazon)]
+    public class AmazonCombinedOrderNumberCondition : NumericStringCondition<long>
+    {
+        private readonly EntityField2 searchField;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="searchField">This is the field to search, either OrderNumber or OrderNumberComplete</param>
+        public AmazonCombinedOrderNumberCondition(EntityField2 searchField)
+        {
+            this.searchField = searchField;
+        }
+
+        /// <summary>
+        /// Generate the filter SQL
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            string amazonOrderSearchSql = String.Empty;
+
+            // Add any combined order AmazonOrderID entries.
+            using (SqlGenerationScope scope = context.PushScope(OrderFields.OrderID, AmazonOrderSearchFields.OrderID, SqlGenerationScopeType.AnyChild))
+            {
+                amazonOrderSearchSql = scope.Adorn(GenerateSql(context.GetColumnReference(searchField), context));
+            }
+
+            return amazonOrderSearchSql;
+        }
+    }
+}
