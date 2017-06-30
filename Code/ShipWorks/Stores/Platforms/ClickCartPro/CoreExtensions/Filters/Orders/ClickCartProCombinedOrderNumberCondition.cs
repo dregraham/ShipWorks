118f5a3ff33936a7942404a2caa961f92e1de821
Andrew Benz <a.benz@shipworks.com>
2017-06-30 12:12:12 -0500

    TP#18215: Add ClickCartPro conditions

---
+++ Code/ShipWorks/Stores/Platforms/ClickCartPro/CoreExtensions/Filters/Orders/ClickCartProCombinedOrderNumberCondition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Interapptive.Shared.ComponentRegistration;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+using ShipWorks.Filters.Content.SqlGeneration;
+
+namespace ShipWorks.Stores.Platforms.ClickCartPro.CoreExtensions.Filters.Orders
+{
+    /// <summary>
+    /// Condition for the ClickCartPro order numbers that were combined into a single order
+    /// </summary>
+    [ConditionElement("Click Cart Pro Order #", "ClickCartPro.CombinedOrderNumbers")]
+    [ConditionStoreType(StoreTypeCode.ClickCartPro)]
+    [Component]
+    public class ClickCartProCombinedOrderNumberCondition : NumericStringCondition<long>, ICombinedOrderCondition
+    {
+        /// <summary>
+        /// Generate the filter SQL
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            string orderSearchSql = String.Empty;
+
+            EntityField2 searchField = IsNumeric ? ClickCartProOrderSearchFields.OrderNumber : ClickCartProOrderSearchFields.OrderNumberComplete;
+
+            // Add any combined order number/number complete entries.
+            using (SqlGenerationScope scope = context.PushScope(OrderFields.OrderID, ClickCartProOrderSearchFields.OrderID, SqlGenerationScopeType.AnyChild))
+            {
+                orderSearchSql = scope.Adorn(GenerateSql(context.GetColumnReference(searchField), context));
+            }
+
+            return orderSearchSql;
+        }
+    }
+}
