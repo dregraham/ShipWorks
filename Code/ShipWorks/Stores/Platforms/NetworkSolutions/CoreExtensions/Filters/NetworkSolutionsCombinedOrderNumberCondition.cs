0e8e95fc2890c0f9697f808f4b14740f5c156f48
Andrew Benz <a.benz@shipworks.com>
2017-06-30 12:42:20 -0500

    TP#18215: Add Network Solutions order search conditions

---
+++ Code/ShipWorks/Stores/Platforms/NetworkSolutions/CoreExtensions/Filters/NetworkSolutionsCombinedOrderNumberCondition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Interapptive.Shared.ComponentRegistration;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+using ShipWorks.Filters.Content.SqlGeneration;
+
+namespace ShipWorks.Stores.Platforms.NetworkSolutions.CoreExtensions.Filters.Orders
+{
+    /// <summary>
+    /// Condition for the NetworkSolutions order numbers that were combined into a single order
+    /// </summary>
+    [ConditionElement("Network Solutions Order #", "NetworkSolutions.CombinedOrderNumbers")]
+    [ConditionStoreType(StoreTypeCode.NetworkSolutions)]
+    [Component]
+    public class NetworkSolutionsCombinedOrderNumberCondition : NumericStringCondition<long>, ICombinedOrderCondition
+    {
+        /// <summary>
+        /// Generate the filter SQL
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            string orderSearchSql = String.Empty;
+
+            EntityField2 searchField = IsNumeric ? NetworkSolutionsOrderSearchFields.OrderNumber : NetworkSolutionsOrderSearchFields.OrderNumberComplete;
+
+            // Add any combined order number/number complete entries.
+            using (SqlGenerationScope scope = context.PushScope(OrderFields.OrderID, NetworkSolutionsOrderSearchFields.OrderID, SqlGenerationScopeType.AnyChild))
+            {
+                orderSearchSql = scope.Adorn(GenerateSql(context.GetColumnReference(searchField), context));
+            }
+
+            return orderSearchSql;
+        }
+    }
+}
