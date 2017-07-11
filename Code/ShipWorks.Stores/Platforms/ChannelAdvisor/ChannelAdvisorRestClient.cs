be668fa996e540d9ce5cdd9e37681a7e77bbcd41
Chris Hicks <c.hicks@shipworks.com>
2017-07-11 15:23:06 -0500

    TP#18418: Create ChannelAdvisorRestClient with method to get refresh token

---
+++ Code/ShipWorks.Stores/Platforms/ChannelAdvisor/ChannelAdvisorRestClient.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.Net;
+using Newtonsoft.Json.Linq;
+using ShipWorks.ApplicationCore.Logging;
+using ShipWorks.Stores.Communication;
+
+namespace ShipWorks.Stores.Platforms.ChannelAdvisor
+{
+    [Component]
+    public class ChannelAdvisorRestClient : IChannelAdvisorRestClient
+    {
+        private const string TokenEndpoint = "https://api.channeladvisor.com/oauth2/token";
+        private const string ApplicationID = "wx76dgzjcwlfy1ck3nb8oke7ql2ukv05";
+        private const string SharedSecret = "Preb8E42ckWZZpFHh6OV2w";
+        private const string RedirectUri = "https://www.interapptive.com/channeladvisor/subscribe.php";
+
+        /// <summary>
+        /// Gets the refresh token.
+        /// </summary>
+        public string GetRefreshToken(string code)
+        {
+            HttpVariableRequestSubmitter submitter = new HttpVariableRequestSubmitter();
+            submitter.Uri = new Uri(TokenEndpoint);
+            submitter.Verb = HttpVerb.Post;
+            submitter.ContentType = "application/x-www-form-urlencoded";
+            submitter.Headers.Add("Authorization", AuthorizationHeaderValue);
+            submitter.Variables.Add("grant_type", "authorization_code");
+            submitter.Variables.Add("code", code);
+            submitter.Variables.Add("redirect_uri", RedirectUri);
+
+            return JObject.Parse(ProcessRequest(submitter, "GetRefreshToken"))["refresh_token"].ToString();
+        }
+
+        /// <summary>
+        /// Processes the request.
+        /// </summary>
+        private string ProcessRequest(HttpVariableRequestSubmitter request, string action)
+        {
+            ApiLogEntry apiLogEntry = new ApiLogEntry(ApiLogSource.ChannelAdvisor, action);
+            apiLogEntry.LogRequest(request);
+
+            try
+            {
+                IHttpResponseReader httpResponseReader = request.GetResponse();
+                string result = httpResponseReader.ReadResult();
+                apiLogEntry.LogResponse(result, "json");
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                apiLogEntry.LogResponse(ex);
+                throw new ChannelAdvisorException("Error communicating with ChannelAdvisor REST API", ex);
+            }
+        }
+
+        /// <summary>
+        /// Gets the authorization header.
+        /// </summary>
+        private string AuthorizationHeaderValue => Convert.ToBase64String(Encoding.ASCII.GetBytes($"{ApplicationID}:{SharedSecret}"));
+    }
+}
