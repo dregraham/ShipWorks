fd846f07be1243da205a8fbf16849179086b7388
Kevin Croke <k.croke@shipworks.com>
2017-07-11 12:36:29 -0500

    TP#18418: Add IChannelAdvisorRestClient

---
+++ Code/ShipWorks.Stores/Communication/IChannelAdvisorRestClient.cs
@@ -0,0 +1,13 @@
+﻿namespace ShipWorks.Stores.Communication
+{
+    /// <summary>
+    /// ChannelAdvisor Rest Web Client
+    /// </summary>
+    public interface IChannelAdvisorRestClient
+    {
+        /// <summary>
+        /// Given a Channel Advisor Authorization Code, request and return the refresh token
+        /// </summary>
+        string GetRefreshToken(string code);
+    }
+}
