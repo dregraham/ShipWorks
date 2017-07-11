689e0e4a03c1404405301ff58d332c55aab1a209
Josh Winchester <j.winchester@shipworks.com>
2017-07-11 14:02:29 -0500

    TP#18215: Remove version unit tests

--- Code/ShipWorks.Tests/Data/Administration/VersionSpecificUpdates/V_05_13_00_01_Test.cs
+++ Code/ShipWorks.Tests/Data/Administration/VersionSpecificUpdates/V_05_13_00_01_Test.cs
@@ -39,54 +39,6 @@ namespace ShipWorks.Tests.Data.Administration.VersionSpecificUpdates
             Assert.False(result);
         }
 
-        [Fact]
-        public void Update_DelegatesToSqlAdapter_ToFetchStores()
-        {
-            var sqlAdapter = mock.FromFactory<ISqlAdapterFactory>()
-                .Mock(x => x.Create());
-            sqlAdapter.Setup(x => x.FetchQueryAsync(It.IsAny<EntityQuery<BigCommerceStoreEntity>>()))
-                .ReturnsAsync(mock.CreateMock<IEntityCollection2>().Object);
-            var testObject = mock.Create<V_05_13_00_01>();
-
-            testObject.Update();
-
-            sqlAdapter.Verify(x => x.FetchQueryAsync(It.IsAny<EntityQuery<BigCommerceStoreEntity>>()));
-        }
-
-        [Fact]
-        public void Update_DelegatesToIdentifier_ForEachStore()
-        {
-            var store1 = new BigCommerceStoreEntity { ApiUrl = "Foo" };
-            var store2 = new BigCommerceStoreEntity { ApiUrl = "Bar" };
-            var collection = new BigCommerceStoreCollection { store1, store2 };
-
-            mock.FromFactory<ISqlAdapterFactory>()
-                .Mock(x => x.Create())
-                .Setup(x => x.FetchQueryAsync(It.IsAny<EntityQuery<BigCommerceStoreEntity>>()))
-                .ReturnsAsync(collection);
-            var testObject = mock.Create<V_05_13_00_01>();
-
-            testObject.Update();
-
-            mock.Mock<IBigCommerceIdentifier>().Verify(x => x.Set(store1));
-            mock.Mock<IBigCommerceIdentifier>().Verify(x => x.Set(store2));
-        }
-
-        [Fact]
-        public void Update_DelegatesToSqlAdapter_ToSaveCollection()
-        {
-            var collection = mock.CreateMock<IEntityCollection2>().Object;
-            var sqlAdapter = mock.FromFactory<ISqlAdapterFactory>()
-                .Mock(x => x.Create());
-            sqlAdapter.Setup(x => x.FetchQueryAsync(It.IsAny<EntityQuery<BigCommerceStoreEntity>>()))
-                .ReturnsAsync(collection);
-            var testObject = mock.Create<V_05_13_00_01>();
-
-            testObject.Update();
-
-            sqlAdapter.Verify(x => x.SaveEntityCollection(collection, true, false));
-        }
-
         public void Dispose()
         {
             mock.Dispose();
--- Code/ShipWorks.Tests/Data/Administration/VersionSpecificUpdates/V_05_13_00_02_Test.cs
+++ Code/ShipWorks.Tests/Data/Administration/VersionSpecificUpdates/V_05_13_00_02_Test.cs
@@ -39,54 +39,6 @@ namespace ShipWorks.Tests.Data.Administration.VersionSpecificUpdates
             Assert.False(result);
         }
 
-        [Fact]
-        public void Update_DelegatesToSqlAdapter_ToFetchStores()
-        {
-            var sqlAdapter = mock.FromFactory<ISqlAdapterFactory>()
-                .Mock(x => x.Create());
-            sqlAdapter.Setup(x => x.FetchQueryAsync(It.IsAny<EntityQuery<ShopSiteStoreEntity>>()))
-                .ReturnsAsync(mock.CreateMock<IEntityCollection2>().Object);
-            var testObject = mock.Create<V_05_13_00_02>();
-
-            testObject.Update();
-
-            sqlAdapter.Verify(x => x.FetchQueryAsync(It.IsAny<EntityQuery<ShopSiteStoreEntity>>()));
-        }
-
-        [Fact]
-        public void Update_DelegatesToIdentifier_ForEachStore()
-        {
-            var store1 = new ShopSiteStoreEntity { ApiUrl = "Foo", Password = "pwd", Username = "pwd" };
-            var store2 = new ShopSiteStoreEntity { ApiUrl = "Bar", Password = "pwd", Username = "pwd" };
-            var collection = new ShopSiteStoreCollection { store1, store2 };
-
-            mock.FromFactory<ISqlAdapterFactory>()
-                .Mock(x => x.Create())
-                .Setup(x => x.FetchQueryAsync(It.IsAny<EntityQuery<ShopSiteStoreEntity>>()))
-                .ReturnsAsync(collection);
-            var testObject = mock.Create<V_05_13_00_02>();
-
-            testObject.Update();
-
-            mock.Mock<IShopSiteIdentifier>().Verify(x => x.Set(store1, "Foo"));
-            mock.Mock<IShopSiteIdentifier>().Verify(x => x.Set(store2, "Bar"));
-        }
-
-        [Fact]
-        public void Update_DelegatesToSqlAdapter_ToSaveCollection()
-        {
-            var collection = mock.CreateMock<IEntityCollection2>().Object;
-            var sqlAdapter = mock.FromFactory<ISqlAdapterFactory>()
-                .Mock(x => x.Create());
-            sqlAdapter.Setup(x => x.FetchQueryAsync(It.IsAny<EntityQuery<ShopSiteStoreEntity>>()))
-                .ReturnsAsync(collection);
-            var testObject = mock.Create<V_05_13_00_02>();
-
-            testObject.Update();
-
-            sqlAdapter.Verify(x => x.SaveEntityCollection(collection, true, false));
-        }
-
         public void Dispose()
         {
             mock.Dispose();
