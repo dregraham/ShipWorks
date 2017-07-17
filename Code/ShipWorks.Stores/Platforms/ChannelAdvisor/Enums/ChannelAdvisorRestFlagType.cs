ac9dab3979f36d49cb7ed795caabed8f2708e7b9
Chris Hicks <c.hicks@shipworks.com>
2017-07-17 09:37:42 -0500

    TP#18420: Add description to ChannelAdvisorRestFlagType

--- Code/ShipWorks.Stores/Platforms/ChannelAdvisor/Enums/ChannelAdvisorRestFlagType.cs
+++ Code/ShipWorks.Stores/Platforms/ChannelAdvisor/Enums/ChannelAdvisorRestFlagType.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using System.ComponentModel;
+using Interapptive.Shared.Utility;
 
 namespace ShipWorks.Stores.Platforms.ChannelAdvisor.Enums
 {
@@ -8,28 +10,46 @@ namespace ShipWorks.Stores.Platforms.ChannelAdvisor.Enums
     [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
     public enum ChannelAdvisorRestFlagType
     {
+        [Description("Not Specified")]
         NotSpecified = -9999,
 
+        [Description("Item Copied")]
+        [ImageResource("copy")]
         ItemCopied = -2,
 
+        [Description("Exclamation Point")]
+        [ImageResource("cancel16")]
         ExclamationPoint = -1,
 
+        [Description("None")]
         NoFlag = 0,
 
+        [Description("Red")]
+        [ImageResource("flag_red")]
         RedFlag = 1,
 
+        [Description("Question Mark")]
+        [ImageResource("help2_16")]
         QuestionMark = 2,
 
+        [Description("Not Available")]
+        [ImageResource("not_available")]
         NotAvailable = 3,
 
+        [Description("Price")]
+        [ImageResource("currency_dollar_green16")]
         Price = 4,
 
+        [Description("Yellow")]
+        [ImageResource("flag_yellow")]
         YellowFlag = 5,
 
+        [Description("Green")]
+        [ImageResource("flag_green")]
         GreenFlag = 6,
 
+        [Description("Blue")]
+        [ImageResource("flag_blue")]
         BlueFlag = 7,
-
-        Unknown = 99
     }
 }
