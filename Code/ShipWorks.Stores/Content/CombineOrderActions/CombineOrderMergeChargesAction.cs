20e90789405ea5fe0e9ca83c32dc07c818d6573d
Tim Hughes <t.hughes@shipworks.com>
2017-07-19 12:56:36 -0500

    TP#18215: Removed unused code, merged setting the OrderId duing grouping, set ConfigureAwait(false)

--- Code/ShipWorks.Stores/Content/CombineOrderActions/CombineOrderMergeChargesAction.cs
+++ Code/ShipWorks.Stores/Content/CombineOrderActions/CombineOrderMergeChargesAction.cs
@@ -22,11 +22,10 @@ namespace ShipWorks.Stores.Content.CombineOrderActions
         /// </summary>
         public async Task Perform(OrderEntity combinedOrder, IEnumerable<IOrderEntity> orders, ISqlAdapter sqlAdapter)
         {
-            OrderChargeComparer orderChargeComparer = new OrderChargeComparer();
             EntityQuery<OrderChargeEntity> query = new QueryFactory().OrderCharge
                 .Where(OrderChargeFields.OrderID.In(orders.Select(x => x.OrderID)));
 
-            IEnumerable<OrderChargeEntity> orderCharges = (await sqlAdapter.FetchQueryAsync(query)).Cast<OrderChargeEntity>();
+            IEnumerable<OrderChargeEntity> orderCharges = (await sqlAdapter.FetchQueryAsync(query).ConfigureAwait(false)).Cast<OrderChargeEntity>();
 
             if (orderCharges.None())
             {
@@ -38,40 +37,15 @@ namespace ShipWorks.Stores.Content.CombineOrderActions
                                                                 .GroupBy(c => new { c.Type, c.Description },
                                                                   (key, g) => new OrderChargeEntity(0)
                                                                       {
-                                                                          Type = key.Type, Description = key.Description, Amount = g.Sum(g2 => g2.Amount)
+                                                                          OrderID = combinedOrder.OrderID,
+                                                                          Type = key.Type,
+                                                                          Description = key.Description,
+                                                                          Amount = g.Sum(g2 => g2.Amount)
                                                                       });
             
-            // Set the OrderID to the newly combined order id
-            foreach (OrderChargeEntity newOrderCharge in groupedCharges)
-            {
-                newOrderCharge.OrderID = combinedOrder.OrderID;
-            }
-
             combinedOrder.OrderCharges.AddRange(groupedCharges);
 
             await sqlAdapter.SaveEntityCollectionAsync(combinedOrder.OrderCharges);
-        }
-
-        /// <summary>
-        /// Order Charge comparer for combining orders
-        /// </summary>
-        private class OrderChargeComparer : IEqualityComparer<OrderChargeEntity>
-        {
-            /// <summary>
-            /// Compare 2 OrderChargeEntities
-            /// </summary>
-            public bool Equals(OrderChargeEntity x, OrderChargeEntity y)
-            {
-                return x.Type == y.Type && x.Description == y.Description;
-            }
-
-            /// <summary>
-            /// Get the hash code of an OrderChargeEntity
-            /// </summary>
-            public int GetHashCode(OrderChargeEntity obj)
-            {
-                return $"{obj.Type}-{obj.Description}".GetHashCode();
-            }
         }
     }
 }
