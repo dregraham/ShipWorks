81e8447365289fac2a259c6fe83039c9ee49e880
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-07-20 09:59:39 -0500

    TP#18419 create IChannelAdvisorRestAccountSettingsControl

---
+++ Code/ShipWorks/Stores/Platforms/ChannelAdvisor/IChannelAdvisorRestAccountSettingsControl.cs
@@ -0,0 +1,20 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Stores.Platforms.ChannelAdvisor
+{
+    /// <summary>
+    /// Represents the ChannelAdvisor Account settings control from Rest store
+    /// </summary>
+    public interface IChannelAdvisorRestAccountSettingsControl
+    {
+        /// <summary>
+        /// Load the store into the control
+        /// </summary>
+        void LoadStore(StoreEntity store);
+
+        /// <summary>
+        /// Save to the store entity
+        /// </summary>
+        bool SaveToEntity(StoreEntity store);
+    }
+}
