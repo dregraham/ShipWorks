8b794407499128344ed763e21fb4a16f5cfa960c
Andrew Benz <a.benz@shipworks.com>
2017-07-26 14:08:07 -0500

    TP#17406: Use read-only entities when dispatching actions to avoid cloning

---
+++ Code/ShipWorks.Data.Model/Custom/EntityClasses/ActionEntity.Interface.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Interapptive.Shared.Business;
+
+namespace ShipWorks.Data.Model.EntityInterfaces
+{
+    /// <summary>
+    /// Custom action code
+    /// </summary>
+    public partial interface IActionEntity
+    {
+        /// <summary>
+        /// List of StoreID's that the action is limited to acting on
+        /// </summary>
+        IEnumerable<long> StoreLimitedList { get; }
+
+        /// <summary>
+        /// List of ComputerID's that the action is limited to acting on.
+        /// </summary>
+        IEnumerable<long> ComputerLimitedList { get; }
+
+        /// <summary>
+        /// If the action is store limited - and limited only to a single store - that single ID is returned. If it is not limited,
+        /// or its limited to more than one store, this returns null.
+        /// </summary>
+        long? StoreLimitedSingleStoreID { get; }
+    }
+}
