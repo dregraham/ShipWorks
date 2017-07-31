f81d545d69ce77f90e8d259d47eb53e32ac6e1a9
Kevin Croke <k.croke@shipworks.com>
2017-07-31 14:19:21 -0500

    TP#18772: Add tests for JetStoreType

---
+++ Code/ShipWorks.Stores.Tests/Platforms/Jet/JetStoreTypeTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using ShipWorks.Tests.Shared;
+using Autofac.Extras.Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.Jet;
+using Xunit;
+
+namespace ShipWorks.Stores.Tests.Platforms.Jet
+{
+    public class JetStoreTypeTest : IDisposable
+    {
+        private readonly AutoMock mock;
+
+        public JetStoreTypeTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+        }
+
+        [Fact]
+        public void TypeCode_IsJet()
+        {
+            JetStoreType testObject = new JetStoreType(new JetStoreEntity());
+
+            var typeCode = testObject.TypeCode;
+
+            Assert.Equal(StoreTypeCode.Jet, typeCode);
+        }
+
+        [Fact]
+        public void CreateStoreInstance_ReturnsJetStoreEntity()
+        {
+            JetStoreType testObject = new JetStoreType(new JetStoreEntity());
+
+            StoreEntity store = testObject.CreateStoreInstance();
+
+            Assert.IsType<JetStoreEntity>(store);
+        }
+
+        [Fact]
+        public void CreateStoreInstance_InitializesStoreValues()
+        {
+            JetStoreType testObject = new JetStoreType(new JetStoreEntity());
+
+            JetStoreEntity store = testObject.CreateStoreInstance() as JetStoreEntity;
+
+            Assert.Empty(store.ApiUser);
+            Assert.Empty(store.Secret);
+            Assert.Equal("My Jet Store", store.StoreName);
+        }
+
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+
+    }
+}
