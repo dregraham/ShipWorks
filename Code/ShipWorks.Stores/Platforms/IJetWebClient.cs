ff5410c76de5b7e3978ffae11fed2d5f76b47a25
Kevin Croke <k.croke@shipworks.com>
2017-07-31 11:26:00 -0500

    TP#18772: Add IJetWebClient

---
+++ Code/ShipWorks.Stores/Platforms/IJetWebClient.cs
@@ -0,0 +1,13 @@
+﻿using Interapptive.Shared.Utility;
+
+namespace ShipWorks.Stores.Platforms
+{
+    /// <summary>
+    /// Interface for JetWebClient
+    /// </summary>
+    public interface IJetWebClient
+    {
+        // Given a username and password, get a token.
+        GenericResult<string> GetToken(string username, string password);
+    }
+}
