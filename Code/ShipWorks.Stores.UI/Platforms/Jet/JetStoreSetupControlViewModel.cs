e9f18e2ca45c7d379c6471d93e998d40fcaa1014
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-07-31 13:18:41 -0500

    TP#18772 implement load and save method in view model

--- Code/ShipWorks.Stores.UI/Platforms/Jet/JetStoreSetupControlViewModel.cs
+++ Code/ShipWorks.Stores.UI/Platforms/Jet/JetStoreSetupControlViewModel.cs
@@ -1,20 +1,35 @@
 ﻿using System.ComponentModel;
 using System.Reflection;
-using System.Runtime.CompilerServices;
+using Interapptive.Shared.Security;
+using Interapptive.Shared.UI;
+using Interapptive.Shared.Utility;
 using ShipWorks.Core.UI;
 using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.Jet;
 
-namespace ShipWorks.Stores.Platforms.Jet
+namespace ShipWorks.Stores.UI.Platforms.Jet
 {
+    /// <summary>
+    /// ViewModel for setting up a Jet store
+    /// </summary>
     public class JetStoreSetupControlViewModel : IJetStoreSetupControlViewModel, INotifyPropertyChanged
     {
+        private readonly IJetWebClient webClient;
+        private readonly IEncryptionProviderFactory encryptionProviderFactory;
+        private readonly IMessageHelper messageHelper;
         private string apiUser;
         private string secret;
-
         public event PropertyChangedEventHandler PropertyChanged;
         private readonly PropertyChangedHandler handler;
-        public JetStoreSetupControlViewModel()
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public JetStoreSetupControlViewModel(IJetWebClient webClient, IEncryptionProviderFactory encryptionProviderFactory, IMessageHelper messageHelper)
         {
+            this.webClient = webClient;
+            this.encryptionProviderFactory = encryptionProviderFactory;
+            this.messageHelper = messageHelper;
             handler = new PropertyChangedHandler(this, () => PropertyChanged);
         }
 
@@ -38,14 +53,32 @@ namespace ShipWorks.Stores.Platforms.Jet
             set { handler.Set(nameof(Secret), ref secret, value); }
         }
 
+        /// <summary>
+        /// Load the store into the view model
+        /// </summary>
         public void Load(JetStoreEntity store)
         {
-            throw new System.NotImplementedException();
+            apiUser = store.ApiUser;
+            Secret = encryptionProviderFactory.CreateSecureTextEncryptionProvider(store.ApiUser).Decrypt(store.Secret);
         }
 
+        /// <summary>
+        /// Save the Api user and Secret to the store
+        /// </summary>
         public bool Save(JetStoreEntity store)
         {
-            throw new System.NotImplementedException();
+            GenericResult<string> result = webClient.GetToken(ApiUser, Secret);
+
+            if (result.Failure)
+            {
+                messageHelper.ShowError(result.Message);
+                return false;
+            }
+            
+            store.ApiUser = ApiUser;
+            store.Secret = encryptionProviderFactory.CreateSecureTextEncryptionProvider(store.ApiUser).Encrypt(Secret);
+
+            return true;
         }
     }
 }
