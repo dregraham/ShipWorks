418a446acd06c6cbaf3754a107cf1607811ce9a4
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-08-01 15:02:01 -0500

    TP#18773 create JetProduct and IProductRepo

---
+++ Code/ShipWorks.Stores/Platforms/Jet/DTO/JetProduct.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace ShipWorks.Stores.Platforms.Jet.DTO
+{
+    public class JetProduct
+    {
+        [JsonProperty("swatch_image_url")]
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Code/ShipWorks.Stores/Platforms/Jet/IProductRepo.cs
@@ -0,0 +1,15 @@
+﻿using ShipWorks.Stores.Platforms.Jet.DTO;
+
+namespace ShipWorks.Stores.Platforms.Jet
+{
+    /// <summary>
+    /// Represents a Jet product repository
+    /// </summary>
+    public interface IProductRepo
+    {
+        /// <summary>
+        /// Get the JetProduct matching he JetOrderItem
+        /// </summary>
+        JetProduct GetProduct(JetOrderItem item);
+    }
+}
