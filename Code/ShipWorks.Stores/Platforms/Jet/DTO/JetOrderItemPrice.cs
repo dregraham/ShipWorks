1334b6f810b6fae060f4056d0c5a6d2cbd786435
Kevin Croke <k.croke@shipworks.com>
2017-08-01 13:56:59 -0500

    TP#18773: Added Jet order DTOs

---
+++ Code/ShipWorks.Stores/Platforms/Jet/DTO/JetOrderItemPrice.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace ShipWorks.Stores.Platforms.Jet.DTO
+{
+    public class JetOrderItemPrice
+    {
+        [JsonProperty("base_price")]
+        public int BasePrice { get; set; }
+
+        [JsonProperty("item_tax")]
+        public double ItemTax { get; set; }
+
+        [JsonProperty("item_shipping_cost")]
+        public int ItemShippingCost { get; set; }
+
+        [JsonProperty("item_shipping_tax")]
+        public double ItemShippingTax { get; set; }
+    }
+}
--- Code/ShipWorks.Stores/Platforms/Jet/DTO/TokenResponse.cs
+++ Code/ShipWorks.Stores/Platforms/Jet/DTO/TokenResponse.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace ShipWorks.Stores.Platforms.Jet.DTO
 {
-    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = true, StripAfterObfuscation = false)]
     public class TokenResponse
     {
         [JsonProperty("id_token")]
