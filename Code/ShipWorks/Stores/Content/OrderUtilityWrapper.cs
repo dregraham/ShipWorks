6bcc434c5d7301060863751a22981ecb97ed7444
Tim Hughes <t.hughes@shipworks.com>
2017-08-01 11:19:58 -0500

    TP#19028:             // Due to obfuscation, we need to use the object array parameter list.  Otherwise the anonymous type properties             // get obfuscated and the SQL is no longer valid.             // So, we must use the @p0, @p1 variables that get generated.             // DO NOT change the order of the object array without changing the @p numbers appropriately.

--- Code/ShipWorks/Stores/Content/OrderUtilityWrapper.cs
+++ Code/ShipWorks/Stores/Content/OrderUtilityWrapper.cs
@@ -29,15 +29,20 @@ namespace ShipWorks.Stores.Content
         public async Task<long> GetNextOrderNumberAsync(long storeID)
         {
             ParameterValue maxOrderNumberParam = new ParameterValue(ParameterDirection.InputOutput, dbType: DbType.Int64);
+
+            // Due to obfuscation, we need to use the object array parameter list.  Otherwise the anonymous type properties
+            // get obfuscated and the SQL is no longer valid.
+            // So, we must use the @p0, @p1 variables that get generated.
+            // DO NOT change the order of the object array without changing the @p numbers appropriately.
             await sqlAdapterFactory.Create().ExecuteSQLAsync(@"
                     ;with MaxOrderNumber as
                     (
-	                    SELECT coalesce(max(OrderNumber), 0) as 'OrderNumber' FROM [Order] WHERE [StoreID] = @StoreID and IsManual = 0
+	                    SELECT coalesce(max(OrderNumber), 0) as 'OrderNumber' FROM [Order] WHERE [StoreID] = @p1 and IsManual = 0
 	                    UNION ALL
-	                    SELECT coalesce(max(OrderNumber), 0) as 'OrderNumber' FROM [OrderSearch] WHERE [StoreID] = @StoreID and IsManual = 0
+	                    SELECT coalesce(max(OrderNumber), 0) as 'OrderNumber' FROM [OrderSearch] WHERE [StoreID] = @p1 and IsManual = 0
                     )
-                    SELECT @MaxOrderNumber = max(OrderNumber) from MaxOrderNumber;", 
-                    new { MaxOrderNumber = maxOrderNumberParam, StoreID = storeID })
+                    SELECT @p0 = max(OrderNumber) from MaxOrderNumber;", 
+                    new object[] { maxOrderNumberParam, storeID })
                 .ConfigureAwait(false);
 
             return Convert.ToInt64(maxOrderNumberParam.Value) + 1;
