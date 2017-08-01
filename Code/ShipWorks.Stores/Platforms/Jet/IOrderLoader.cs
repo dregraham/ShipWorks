41384593e20cb5314340661a19650375eafe12cc
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-08-01 15:04:42 -0500

    TP#18773 create IORderLoader

---
+++ Code/ShipWorks.Stores/Platforms/Jet/IOrderLoader.cs
@@ -0,0 +1,16 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.Jet.DTO;
+
+namespace ShipWorks.Stores.Platforms.Jet
+{
+    /// <summary>
+    /// Interface to load order data from jet
+    /// </summary>
+    public interface IOrderLoader
+    {
+        /// <summary>
+        /// Load the OrderEntity with the JetOrderDetails
+        /// </summary>
+        void LoadOrder(OrderEntity order, JetOrderDetail jetOrder);
+    }
+}
