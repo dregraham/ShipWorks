135c96f2a4a49038bc19bce8ff881d1e7bb6366f
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-08-07 15:36:02 -0500

    TP#18774 add files to proj

---
+++ Code/ShipWorks.Stores/Platforms/Jet/JetShipmentRequestFactory.cs
@@ -0,0 +1,18 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.Jet.DTO.Requests;
+
+namespace ShipWorks.Stores.Platforms.Jet
+{
+    public class JetShipmentRequestFactory : IJetShipmentRequestFactory
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="shipment"></param>
+        /// <returns></returns>
+        public JetShipmentRequest Create(ShipmentEntity shipment)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
