f87ebb2d31dde5d3f14a583870731d292232c76b
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-08-07 15:31:35 -0500

    TP#18774 create JetShipmentRequest

---
+++ Code/ShipWorks.Stores/Platforms/Jet/DTO/Requests/JetShipmentRequest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ShipWorks.Stores.Platforms.Jet.DTO.Requests
+{
+    public class JetShipmentRequest
+    {
+        [JsonProperty("alt_order_id")]
+        public string AltOrderId{ get; set; }
+
+        [JsonProperty("shipments")]
+        public List<Shipment> Shipments { get; set; }
+    }
+
+    public class ShipmentItem
+    {
+        [JsonProperty("alt_shipment_item_id")]
+        public string AltShipmentItemId { get; set; }
+
+        [JsonProperty("merchant_sku")]
+        public string MerchantSku { get; set; }
+
+        [JsonProperty("response_shipment_sku_quantity")]
+        public int ResponseShipmentSkuQuantity { get; set; }
+    }
+
+    public class Shipment
+    {
+        [JsonProperty("alt_shipment_id")]
+        public string AltShipmentId { get; set; }
+
+        [JsonProperty("shipment_tracking_number")]
+        public string ShipmentTrackingNumber { get; set; }
+
+        [JsonProperty("response_shipment_date")]
+        public DateTime ResponseShipmentDate { get; set; }
+
+        [JsonProperty("response_shipment_method")]
+        public string ResponseShipmentMethod { get; set; }
+
+        [JsonProperty("expected_delivery_date")]
+        public DateTime ExpectedDeliveryDate { get; set; }
+
+        [JsonProperty("ship_from_zip_code")]
+        public string ShipFromZipCode { get; set; }
+
+        [JsonProperty("carrier_pick_up_date")]
+        public DateTime CarrierPickUpDate { get; set; }
+
+        [JsonProperty("carrier")]
+        public string Carrier { get; set; }
+
+        [JsonProperty("shipment_items")]
+        public List<ShipmentItem> ShipmentItems { get; set; }
+    }
+}
