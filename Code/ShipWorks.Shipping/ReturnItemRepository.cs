58c9b21e075ce9ac8fa3948ec1d7d3ba87367515
Kevin Croke <k.croke@shipworks.com>
2017-08-22 17:02:24 -0500

    TP#19296: Add ReturnItemRepository

---
+++ Code/ShipWorks.Shipping/ReturnItemRepository.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Interapptive.Shared.Collections;
+using SD.LLBLGen.Pro.QuerySpec;
+using ShipWorks.Data.Connection;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Data.Model.FactoryClasses;
+using ShipWorks.Data.Model.HelperClasses;
+
+namespace ShipWorks.Shipping
+{
+    public class ReturnItemRepository : IReturnItemRepository
+    {
+        private readonly ISqlAdapterFactory sqlAdapterFactory;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ReturnItemRepository(ISqlAdapterFactory sqlAdapterFactory)
+        {
+            this.sqlAdapterFactory = sqlAdapterFactory;
+        }
+
+        /// <summary>
+        /// Loads shipment with ShipmentReturnItems.
+        /// </summary>
+        /// <param name="shipment"></param>
+        /// <param name="createIfNone">if set to <c>true</c> [create the return item if it does not exist].</param>
+        public void LoadReturnData(ShipmentEntity shipment, bool createIfNone)
+        {
+            if (shipment.ShipmentReturnItem.None())
+            {
+                using (ISqlAdapter sqlAdapter = sqlAdapterFactory.Create())
+                {
+                    EntityQuery<ShipmentReturnItemEntity> query = new QueryFactory().ShipmentReturnItem
+                        .Where(ShipmentReturnItemFields.ShipmentID == shipment.ShipmentID);
+
+                    IEnumerable<ShipmentReturnItemEntity> returnItems =
+                        sqlAdapter.FetchQueryAsync(query).Result.Cast<ShipmentReturnItemEntity>();
+
+                    shipment.ShipmentReturnItem.AddRange(returnItems);
+
+                    if (shipment.ShipmentReturnItem.None() && createIfNone)
+                    {
+                        InitializeReturnData(shipment, sqlAdapter);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Populates shipments with ShipmentReturnItems based on the order.
+        /// </summary>
+        private void InitializeReturnData(ShipmentEntity shipment, ISqlAdapter sqlAdapter)
+        {
+            DynamicQuery<ShipmentReturnItemEntity> query = new QueryFactory().OrderItem
+                .Select(() => new ShipmentReturnItemEntity
+                {
+                    Name = OrderItemFields.Name.ToValue<string>(),
+                    Quantity = OrderItemFields.Quantity.ToValue<double>(),
+                    Weight = OrderItemFields.Weight.ToValue<double>(),
+                    SKU = OrderItemFields.SKU.ToValue<string>(),
+                    Code = OrderItemFields.Code.ToValue<string>()
+                }).Where(OrderItemFields.OrderID == shipment.OrderID);
+
+            shipment.ShipmentReturnItem.AddRange(sqlAdapter.FetchQuery(query));
+        }
+    }
+}
--- Code/ShipWorks/Shipping/IReturnItemRepository.cs
+++ Code/ShipWorks/Shipping/IReturnItemRepository.cs
@@ -8,9 +8,8 @@ namespace ShipWorks.Shipping
     public interface IReturnItemRepository
     {
         /// <summary>
-        /// Loads the return data.
+        /// Loads shipment with ShipmentReturnItems.
         /// </summary>
-        /// <param name="shipment">The shipment.</param>
         /// <param name="createIfNone">if set to <c>true</c> [create the return item if it does not exist].</param>
         void LoadReturnData(ShipmentEntity shipment, bool createIfNone);
     }
