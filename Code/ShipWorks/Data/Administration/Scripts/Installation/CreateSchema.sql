1449931bd1ecf03c617901fb4b8dd91bf3ed9e02
Kevin Croke <k.croke@shipworks.com>
2017-08-22 14:03:08 -0500

    TP#19296: Schema scripts for adding ShipmentReturnItem table

--- Code/ShipWorks/Data/Administration/Scripts/Installation/CreateSchema.sql
+++ Code/ShipWorks/Data/Administration/Scripts/Installation/CreateSchema.sql
@@ -5078,6 +5078,12 @@ CREATE TABLE [dbo].[WorldShipProcessed]
 [ShipmentIdCalculated] AS (case when isnumeric([ShipmentID]+'.e0')=(1) then CONVERT([bigint],[ShipmentID],(0))  end) PERSISTED
 )
 GO
+PRINT N'Creating primary key [PK_WorldShipProcessed] on [dbo].[WorldShipProcessed]'
+GO
+ALTER TABLE [dbo].[WorldShipProcessed] ADD CONSTRAINT [PK_WorldShipProcessed] PRIMARY KEY CLUSTERED  ([WorldShipProcessedID])
+GO
+
+GO
 PRINT N'Creating [dbo].[ValidatedAddress]'
 GO
 CREATE TABLE [dbo].[ValidatedAddress](
@@ -5109,12 +5115,29 @@ CREATE NONCLUSTERED INDEX [IX_ValidatedAddress_ConsumerIDAddressPrefix]
     ON [dbo].[ValidatedAddress]([ConsumerID] ASC, [AddressPrefix] ASC);
 GO
 
+PRINT N'Creating [dbo].[ShipmentReturnItem]'
+GO
+CREATE TABLE [dbo].[ShipmentReturnItem]
+(
+[ShipmentReturnItemID] [bigint] NOT NULL IDENTITY(1101, 1000),
+[RowVersion] [timestamp] NOT NULL,
+[ShipmentID] [bigint] NOT NULL,
+[Name] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Quantity] [float] NOT NULL,
+[Weight] [float] NOT NULL,
+[Notes] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[SKU] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+)
+GO
+PRINT N'Creating primary key [PK_ShipmentReturnItem] on [dbo].[ShipmentReturnItem]'
+GO
+ALTER TABLE [dbo].[ShipmentReturnItem] ADD CONSTRAINT [PK_ShipmentReturnItem] PRIMARY KEY CLUSTERED  ([ShipmentReturnItemID])
+GO
+PRINT N'Adding foreign keys to [dbo].[ShipmentReturnItem]'
+GO
+ALTER TABLE [dbo].[ShipmentReturnItem] ADD CONSTRAINT [FK_ShipmentReturnItem_Shipment] FOREIGN KEY ([ShipmentID]) REFERENCES [dbo].[Shipment] ([ShipmentID]) ON DELETE CASCADE
+GO
 
-GO
-PRINT N'Creating primary key [PK_WorldShipProcessed] on [dbo].[WorldShipProcessed]'
-GO
-ALTER TABLE [dbo].[WorldShipProcessed] ADD CONSTRAINT [PK_WorldShipProcessed] PRIMARY KEY CLUSTERED  ([WorldShipProcessedID])
-GO
 PRINT N'Altering [dbo].[DimensionsProfile]'
 GO
 PRINT N'Altering [dbo].[EmailAccount]'
