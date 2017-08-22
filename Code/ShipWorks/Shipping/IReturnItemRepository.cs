c404f7188eead1ff768e7287500dbbb8ea2b2c8e
Chris Hicks <c.hicks@shipworks.com>
2017-08-22 16:05:32 -0500

    TP#19296: Create IReturnItemRepository

---
+++ Code/ShipWorks/Shipping/IReturnItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping
+{
+    /// <summary>
+    /// Repository for return items
+    /// </summary>
+    public interface IReturnItemRepository
+    {
+        /// <summary>
+        /// Loads the return data.
+        /// </summary>
+        /// <param name="shipment">The shipment.</param>
+        /// <param name="createIfNone">if set to <c>true</c> [create the return item if it does not exist].</param>
+        void LoadReturnData(ShipmentEntity shipment, bool createIfNone);
+    }
+}
