251fababaf6678a3b3ba531370dc16a95c98e977
Andrew Benz <a.benz@shipworks.com>
2017-08-25 09:23:20 -0500

    TP#18536: Fix crash when combining two manual NetworkSolutions orders

--- Code/ShipWorks.Stores/Platforms/NetworkSolutions/Content/NetworkSolutionsCombineOrderAction.cs
+++ Code/ShipWorks.Stores/Platforms/NetworkSolutions/Content/NetworkSolutionsCombineOrderAction.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Interapptive.Shared.ComponentRegistration;
 using ShipWorks.Data.Connection;
-using ShipWorks.Data.Model.Custom;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Data.Model.EntityInterfaces;
+using ShipWorks.Data.Model.HelperClasses;
 using ShipWorks.Stores.Content.CombineOrderActions;
 
 namespace ShipWorks.Stores.Platforms.NetworkSolutionss.Content
@@ -21,15 +20,15 @@ namespace ShipWorks.Stores.Platforms.NetworkSolutionss.Content
         /// </summary>
         public Task Perform(OrderEntity combinedOrder, IEnumerable<IOrderEntity> orders, ISqlAdapter sqlAdapter)
         {
-            IEnumerable<NetworkSolutionsOrderSearchEntity> orderSearches = orders.Cast<INetworkSolutionsOrderEntity>()
-                .Select(x => new NetworkSolutionsOrderSearchEntity
+            var recordCreator = new SearchRecordMerger<INetworkSolutionsOrderEntity>(combinedOrder, orders, sqlAdapter);
+
+            return recordCreator.Perform(NetworkSolutionsOrderSearchFields.OrderID,
+                x => new NetworkSolutionsOrderSearchEntity
                 {
                     OrderID = combinedOrder.OrderID,
                     NetworkSolutionsOrderID = x.NetworkSolutionsOrderID,
                     OriginalOrderID = x.OrderID
                 });
-
-            return sqlAdapter.SaveEntityCollectionAsync(orderSearches.ToEntityCollection());
         }
     }
 }
