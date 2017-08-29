f1cb43d3b86be07331f3ff15ab9790daf21166fb
Tim Hughes <t.hughes@shipworks.com>
2017-08-29 08:21:14 -0500

    TP#18536: IsCombinedOrder wasn't filtering on StoreID, so any combined order with the same order number across stores would be shown as already combined.  Now filtering on StoreID so this doesn't happen. Excluding StackTraceHelper from obfuscation.

--- Code/ShipWorks/Stores/Communication/StoreDownloader.cs
+++ Code/ShipWorks/Stores/Communication/StoreDownloader.cs
@@ -309,6 +309,7 @@ namespace ShipWorks.Stores.Communication
             {
                 QueryFactory factory = new QueryFactory();
                 QuerySpec combinedSearchQuery = orderIdentifier.CreateCombinedSearchQuery(factory);
+                combinedSearchQuery.AndWhere(OrderSearchFields.StoreID == this.Store.StoreID);
                 DynamicQuery query = factory.Create().Select(combinedSearchQuery.Any());
 
                 return (await sqlAdapter.FetchScalarAsync<bool?>(query).ConfigureAwait(false)) ?? false;
