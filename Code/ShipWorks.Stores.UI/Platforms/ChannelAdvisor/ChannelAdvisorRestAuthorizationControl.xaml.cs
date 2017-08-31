f30e33a99046c5cf14a74723416191ae615831ed
Chris Hicks <c.hicks@shipworks.com>
2017-08-31 10:26:45 -0500

    TP#19301: Add attached property to link to open in browser. Add padding to line up control text with wizard text.

--- Code/ShipWorks.Stores.UI/Platforms/ChannelAdvisor/ChannelAdvisorRestAuthorizationControl.xaml.cs
+++ Code/ShipWorks.Stores.UI/Platforms/ChannelAdvisor/ChannelAdvisorRestAuthorizationControl.xaml.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace ShipWorks.Stores.UI.Platforms.ChannelAdvisor
+﻿namespace ShipWorks.Stores.UI.Platforms.ChannelAdvisor
 {
     /// <summary>
     /// Interaction logic for ChannelAdvisorAccountSettingsControl.xaml
     /// </summary>
-    public partial class ChannelAdvisorRestAuthorizationControl : UserControl
+    public partial class ChannelAdvisorRestAuthorizationControl
     {
         /// <summary>
         /// Constructor
