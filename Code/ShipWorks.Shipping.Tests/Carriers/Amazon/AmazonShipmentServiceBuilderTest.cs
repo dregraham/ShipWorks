06e0c6e73b41cfd1ad0aae815db5a80432847ebb
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-09-13 09:35:37 -0500

    TP#19637 update AmazonShipmentServiceBuilder to use IAmazonServiceTypeRepository when building service list

---
+++ Code/ShipWorks.Shipping.Tests/Carriers/Amazon/AmazonShipmentServiceBuilderTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Autofac.Extras.Moq;
+
+namespace ShipWorks.Shipping.Tests.Carriers.Amazon
+{
+    public class AmazonShipmentServiceBuilderTest : IDisposable
+    {
+        private readonly AutoMock mock;
+        public AmazonShipmentServiceBuilderTest()
+        {
+            mock = AutoMock.GetLoose();
+        }
+        
+        public void Dispose()
+        {
+            mock?.Dispose();
+        }
+    }
+}
--- Code/ShipWorks.Shipping/Carriers/Amazon/AmazonShipmentServicesBuilder.cs
+++ Code/ShipWorks.Shipping/Carriers/Amazon/AmazonShipmentServicesBuilder.cs
@@ -1,10 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Interapptive.Shared.Utility;
 using ShipWorks.Data.Model.EntityClasses;
-using ShipWorks.Shipping.Carriers.Amazon.Api;
-using ShipWorks.Shipping.Carriers.Amazon.Enums;
-using ShipWorks.Shipping.Editing.Rating;
 using ShipWorks.Shipping.Settings;
 
 namespace ShipWorks.Shipping.Carriers.Amazon
@@ -16,14 +12,18 @@ namespace ShipWorks.Shipping.Carriers.Amazon
     {
         private readonly AmazonShipmentType shipmentType;
         private readonly IExcludedServiceTypeRepository excludedServiceTypeRepository;
+        private readonly IAmazonServiceTypeRepository amazonServiceTypeRepository;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AmazonShipmentServicesBuilder"/> class.
         /// </summary>
-        public AmazonShipmentServicesBuilder(AmazonShipmentType shipmentType, IExcludedServiceTypeRepository excludedServiceTypeRepository)
+        public AmazonShipmentServicesBuilder(AmazonShipmentType shipmentType,
+            IExcludedServiceTypeRepository excludedServiceTypeRepository,
+            IAmazonServiceTypeRepository amazonServiceTypeRepository)
         {
             this.shipmentType = shipmentType;
             this.excludedServiceTypeRepository = excludedServiceTypeRepository;
+            this.amazonServiceTypeRepository = amazonServiceTypeRepository;
         }
 
         /// <summary>
@@ -31,16 +31,33 @@ namespace ShipWorks.Shipping.Carriers.Amazon
         /// </summary>
         public Dictionary<int, string> BuildServiceTypeDictionary(IEnumerable<ShipmentEntity> shipments)
         {
-            // Always include the service that the shipments are currently configured with
-            IEnumerable<AmazonServiceType> servicesUsedByShipments =
-                shipments?.Select(s => EnumHelper.GetEnumByApiValue<AmazonServiceType>(s.Amazon.ShippingServiceID))
-                    .Distinct() ??
-                Enumerable.Empty<AmazonServiceType>();
+            // Get all of the amazon services
+            List<AmazonServiceTypeEntity> allServiceTypes = amazonServiceTypeRepository.Get();
 
-            return shipmentType.GetAvailableServiceTypes(excludedServiceTypeRepository)
-                .Cast<AmazonServiceType>()
-                .Union(servicesUsedByShipments)
-                .ToDictionary(service => (int)service, service => EnumHelper.GetDescription(service));
+            // Get all of the services from the shipments
+            IEnumerable<string> shipmentsServices = shipments.Select(s => s.Amazon.ShippingServiceID).Distinct();
+
+            // Build a dictionary of all of AmazonServiceTypeID and Description values using the given shipments ShippingServiceID
+            Dictionary<int, string> shipmentsServiceTypes = allServiceTypes
+                .Where(s => shipmentsServices.Contains(s.ApiValue))
+                .ToDictionary(s => s.AmazonServiceTypeID, s => s.Description);
+
+            // combine that with a a dictionary of available services
+            return shipmentsServiceTypes.Union(GetAvailableServiceTypes(allServiceTypes))
+                .ToDictionary(service => service.Key, service => service.Value);
+        }
+
+        /// <summary>
+        /// Get all of the services which are available from the given list of services
+        /// </summary>
+        private Dictionary<int, string> GetAvailableServiceTypes(List<AmazonServiceTypeEntity> allServiceTypes)
+        {
+            // Get all of the avaialble services from the shipment type (this excludes the ones that have been excluded by the user)
+            IEnumerable<int> availableServices = shipmentType.GetAvailableServiceTypes(excludedServiceTypeRepository);
+
+            // Build a dictionary of AmazonServiceTypeID and Description from the available service types
+            return allServiceTypes.Where(s => availableServices.Contains(s.AmazonServiceTypeID))
+                .ToDictionary(s => s.AmazonServiceTypeID, s => s.Description);
         }
     }
 }
