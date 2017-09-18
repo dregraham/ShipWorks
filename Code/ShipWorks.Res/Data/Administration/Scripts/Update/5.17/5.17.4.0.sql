38b7f0f204127539732a68ec17ba14902ff21b10
Tim Hughes <t.hughes@shipworks.com>
2017-09-18 15:50:18 -0500

    TP#19861:  The new OrderItem.HarmonizedCode was changed from length of nvarchar(20) to varchar(14), so made it correctly be nvarchar(20) so copying the data succeeds.

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.17/5.17.4.0.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.17/5.17.4.0.sql
@@ -1,7 +1,7 @@
 ﻿PRINT N'Altering [dbo].[OrderItem]'
 GO
 ALTER TABLE [dbo].[OrderItem] ADD
-[HarmonizedCode] [varchar] (14) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_OrderItem_HarmonizedCode] DEFAULT ('')
+[HarmonizedCode] [nvarchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_OrderItem_HarmonizedCode] DEFAULT ('')
 GO
 PRINT N'Dropping constraints from [dbo].[OrderItem]'
 GO
