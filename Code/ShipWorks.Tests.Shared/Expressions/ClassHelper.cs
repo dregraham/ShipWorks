56a1c2c08dd3aff77e465b872fe74a2e35ba6693
Andrew Benz <a.benz@shipworks.com>
2017-09-21 11:24:32 -0500

    TP#19829: Handle downloading Paypal orders have no payer address without crashing

---
+++ Code/ShipWorks.Tests.Shared/Expressions/ClassHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ShipWorks.Tests.Shared.Expressions
+{
+    /// <summary>
+    /// Class helper for testing various things about a class
+    /// </summary>
+    public class ClassHelper<TClass>
+    {
+        /// <summary>
+        /// Get the return type of a property or method
+        /// </summary>
+        public Type GetReturnTypeOf<T>(Expression<Func<TClass, T>> expr)
+        {
+            var memberExpression = expr.Body as MemberExpression;
+            return memberExpression.Member is MethodInfo ?
+                ((MethodInfo) memberExpression.Member).ReturnType :
+                ((PropertyInfo) memberExpression.Member).PropertyType;
+        }
+    }
+}
