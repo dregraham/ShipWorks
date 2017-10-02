b2c0b30bc34f8e3f30ba0340f644ab3f57e38a85
Chris Hicks <c.hicks@shipworks.com>
2017-10-02 16:29:52 -0500

    TP#19925: Create IDhlExpressAccountRepository

---
+++ Code/ShipWorks.Shipping/Carriers/Dhl/IDhlExpressAccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping.Carriers.Dhl
+{
+    /// <summary>
+    /// Repository for DHL Express accounts
+    /// </summary>
+    public interface IDhlExpressAccountRepository
+    {
+        /// <summary>
+        /// Saves the given DHL Express account entity to the underlying data source.
+        /// </summary>
+        void Save(DhlExpressAccountEntity account);
+    }
+}
