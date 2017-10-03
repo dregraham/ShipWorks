fa446fcc00fa985170f4b217535616d067f241a1
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-10-03 09:46:08 -0500

    TP#19925 fix broken shipengine nuget package

---
+++ Code/ShipWorks.Shipping/Carriers/ShipEngine/ShipEngineClient.cs
@@ -0,0 +1,17 @@
+﻿using ShipEngine.ApiClient.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Shipping.Carriers.ShipEngine
+{
+    public class ShipEngineClient
+    {
+        public ShipEngineClient()
+        {
+            var apiInstance = new AddressValidationApi();
+        }
+    }
+}
