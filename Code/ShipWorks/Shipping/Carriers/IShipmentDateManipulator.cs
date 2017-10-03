95c683b0898efbd5f07336e8799648511ed93a9b
Andrew Benz <a.benz@shipworks.com>
2017-10-03 09:59:13 -0500

    TP#19995: Add shipment date manipulator interface

---
+++ Code/ShipWorks/Shipping/Carriers/IShipmentDateManipulator.cs
@@ -0,0 +1,15 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping.Carriers
+{
+    /// <summary>
+    /// Manipulate the date of a shipment
+    /// </summary>
+    public interface IShipmentDateManipulator
+    {
+        /// <summary>
+        /// Manipulate the date of the given shipment
+        /// </summary>
+        bool Manipulate(ShipmentEntity shipment);
+    }
+}
