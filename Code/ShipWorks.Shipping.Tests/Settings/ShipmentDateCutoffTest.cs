baeb0c58fd26999f0f8a9a3a32b536563c4c3a7d
Tim Hughes <t.hughes@shipworks.com>
2017-10-05 12:25:41 -0500

    TP#20005: Added ShipmentDateCutoff dto for ShippingSettings

---
+++ Code/ShipWorks.Shipping.Tests/Settings/ShipmentDateCutoffTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+using ShipWorks.Shipping.Settings;
+using Xunit;
+
+namespace ShipWorks.Shipping.Tests.Settings
+{
+    public class ShipmentDateCutoffTest
+    {
+        [Fact]
+        public void ShippingDateCutoff_SerializesAndDeserializes()
+        {
+            TimeSpan cutoffTime = TimeSpan.FromHours(2);
+
+            ShipmentDateCutoff shipmentDateCutoff = new ShipmentDateCutoff(true, cutoffTime);
+
+            string json = JsonConvert.SerializeObject(shipmentDateCutoff);
+
+            ShipmentDateCutoff testObject = JsonConvert.DeserializeObject<ShipmentDateCutoff>(json);
+
+            Assert.Equal(true, testObject.Enabled);
+            Assert.Equal(cutoffTime, testObject.CutoffTime);
+        }
+    }
+}
---
+++ Code/ShipWorks.Shipping/Settings/ShipmentDateCutoff.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace ShipWorks.Shipping.Settings
+{
+    /// <summary>
+    /// Shipment date cutoff DTO
+    /// </summary>
+    [Obfuscation(Exclude = true, ApplyToMembers = true, StripAfterObfuscation = false)]
+    public class ShipmentDateCutoff
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        [JsonConstructor]
+        public ShipmentDateCutoff(bool enabled, TimeSpan cutoffTime)
+        {
+            Enabled = enabled;
+            CutoffTime = cutoffTime;
+        }
+
+        /// <summary>
+        /// Is this cutoff enabled
+        /// </summary>
+        public bool Enabled { get; }
+
+        /// <summary>
+        /// Timespan for this cutoff
+        /// </summary>
+        public TimeSpan CutoffTime { get; }
+    }
+}
