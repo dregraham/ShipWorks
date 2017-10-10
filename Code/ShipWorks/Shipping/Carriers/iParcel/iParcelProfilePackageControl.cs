7dacefe2341f01bfcedcfc957fa79ded8ef23d94
Chris Hicks <c.hicks@shipworks.com>
2017-10-10 10:20:17 -0500

    TP#19969: Create DhlExpressProfilePackageControl

--- Code/ShipWorks/Shipping/Carriers/iParcel/iParcelProfilePackageControl.cs
+++ Code/ShipWorks/Shipping/Carriers/iParcel/iParcelProfilePackageControl.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using ShipWorks.Data.Model.EntityClasses;
+﻿using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Shipping.Profiles;
 using ShipWorks.Data.Model.HelperClasses;
 
