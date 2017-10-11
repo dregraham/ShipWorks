9a8665a3f87f07a6b93d606690711adaf9b7c4e1
Chris Hicks <c.hicks@shipworks.com>
2017-10-11 13:14:03 -0500

    TP#19971: Add SupportsGetRates to DhlExpressShipmentType. Implement DhlExpressRateHashingService.

---
+++ Code/ShipWorks.Shipping/Carriers/Dhl/DhlExpressRateHashingService.cs
@@ -0,0 +1,51 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Data.Model.HelperClasses;
+
+namespace ShipWorks.Shipping.Carriers.Dhl
+{
+    /// <summary>
+    /// Rate hashing service implementation for DHL Express
+    /// </summary>
+    [KeyedComponent(typeof(IRateHashingService), ShipmentTypeCode.DhlExpress)]
+    public class DhlExpressRateHashingService : RateHashingService
+    {
+        /// <summary>
+        /// Gets the fields used for rating a shipment.
+        /// </summary>
+        public override RatingFields RatingFields
+        {
+            get
+            {
+                if (ratingField != null)
+                {
+                    return ratingField;
+                }
+
+                ratingField = base.RatingFields;
+                ratingField.AddShipmentField(DhlExpressShipmentFields.DhlExpressAccountID, genericAccountIdFieldName);
+                ratingField.AddShipmentField(DhlExpressShipmentFields.SaturdayDelivery);
+                ratingField.AddShipmentField(DhlExpressShipmentFields.DeliveredDutyPaid);
+                ratingField.AddShipmentField(DhlExpressShipmentFields.NonMachinable);
+                ratingField.AddShipmentField(DhlExpressShipmentFields.Service);
+
+                ratingField.PackageFields.Add(DhlExpressPackageFields.DeclaredValue);
+                ratingField.PackageFields.Add(DhlExpressPackageFields.DimsAddWeight);
+                ratingField.PackageFields.Add(DhlExpressPackageFields.DimsWeight);
+                ratingField.PackageFields.Add(DhlExpressPackageFields.DimsLength);
+                ratingField.PackageFields.Add(DhlExpressPackageFields.DimsHeight);
+                ratingField.PackageFields.Add(DhlExpressPackageFields.DimsWidth);
+
+                return ratingField;
+            }
+        }
+
+        /// <summary>
+        /// Gets the rating hash based on the shipment's configuration.
+        /// </summary>
+        public override string GetRatingHash(ShipmentEntity shipment)
+        {
+            return RatingFields.GetRatingHash(shipment, shipment.DhlExpress.Packages);
+        }
+    }
+}
--- Code/ShipWorks.Shipping/Carriers/Dhl/DhlExpressShipmentType.cs
+++ Code/ShipWorks.Shipping/Carriers/Dhl/DhlExpressShipmentType.cs
@@ -6,7 +6,6 @@ using ShipWorks.Shipping.Services;
 using ShipWorks.Data.Model.Custom;
 using System.Linq;
 using System;
-using ShipWorks.Shipping.Settings;
 using ShipWorks.Shipping.Insurance;
 using ShipWorks.Data.Connection;
 using SD.LLBLGen.Pro.ORMSupportClasses;
@@ -14,13 +13,10 @@ using ShipWorks.Data.Model.HelperClasses;
 using ShipWorks.Data.Model;
 using System.ComponentModel;
 using Interapptive.Shared.Utility;
-using ShipWorks.Shipping.Carriers.Dhl;
 using ShipWorks.Shipping.Editing;
 using ShipWorks.Data.Model.EntityInterfaces;
 using ShipWorks.Shipping.Settings.Origin;
-using Interapptive.Shared;
 using ShipWorks.Shipping.Profiles;
-using ShipWorks.Shipping.Carriers.iParcel;
 using ShipWorks.Common.IO.Hardware.Printers;
 
 namespace ShipWorks.Shipping.Carriers.Dhl
@@ -56,6 +52,11 @@ namespace ShipWorks.Shipping.Carriers.Dhl
         /// <c>true</c> if [supports multiple packages]; otherwise, <c>false</c>.
         /// </value>
         public override bool SupportsMultiplePackages => true;
+
+        /// <summary>
+        /// Indicates if the shipment service type supports getting rates
+        /// </summary>
+        public override bool SupportsGetRates => true;
 
         /// <summary>
         /// Create and Initialize a new shipment
