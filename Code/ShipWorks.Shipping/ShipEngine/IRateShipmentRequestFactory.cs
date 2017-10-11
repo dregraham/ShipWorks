ededd5a239477eaf4a117ea52f0030d961105092
Kevin Croke <k.croke@shipworks.com>
2017-10-11 13:28:28 -0500

    TP#19971: Add IRateShipmentRequestFactory

---
+++ Code/ShipWorks.Shipping/ShipEngine/IRateShipmentRequestFactory.cs
@@ -0,0 +1,21 @@
+﻿using ShipEngine.ApiClient.Model;
+using ShipWorks.Data.Model.EntityClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Shipping.ShipEngine
+{
+    /// <summary>
+    /// Factory to create a RateShipmentRequest from a ShipmentEntity
+    /// </summary>
+    interface IRateShipmentRequestFactory
+    {
+        /// <summary>
+        ///  Create a RateShipmentRequest from a ShipmentEntity
+        /// </summary>
+        RateShipmentRequest Create(ShipmentEntity shipment);
+    }
+}
