815cdcff5b47750bcbcdfa58664647f07082f065
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-10-12 09:49:48 -0500

    TP#19971 create shipengine contents and non delivery types

---
+++ Code/ShipWorks.Shipping/ShipEngine/ShipEngineContentsType.cs
@@ -0,0 +1,35 @@
+﻿using Interapptive.Shared.Utility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Shipping.ShipEngine
+{
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum ShipEngineContentsType
+    {
+        [Description("Gift")]
+        [ApiValue("gift")]
+        Gift = 0,
+
+        [Description("Merchandise")]
+        [ApiValue("Merchandise")]
+        Merchandise = 1,
+
+        [Description("Returned Goods")]
+        [ApiValue("returned_goods")]
+        ReturnedGoods = 2,
+
+        [Description("Documents")]
+        [ApiValue("documents")]
+        Documents = 3,
+
+        [Description("Sample")]
+        [ApiValue("sample")]
+        Sample = 4
+    }
+}
---
+++ Code/ShipWorks.Shipping/ShipEngine/ShipEngineNonDeliveryType.cs
@@ -0,0 +1,23 @@
+﻿using Interapptive.Shared.Utility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Shipping.ShipEngine
+{
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum ShipEngineNonDeliveryType
+    {
+        [Description("Treat As Abandoned")]
+        [ApiValue("treat_as_abandoned")]
+        TreatAsAbandoned = 0,
+
+        [Description("Return To Sender")]
+        [ApiValue("return_to_sender")]
+        ReturnToSender = 1
+    }
+}
