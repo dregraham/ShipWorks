e274af1e206321107a0f8afe7a9b9651a562640a
Kevin Croke <k.croke@shipworks.com>
2017-10-17 09:17:56 -0500

    TP#20068: Add IShipEngineResourecDownloader

---
+++ Code/ShipWorks.Shipping/ShipEngine/IShipEngineResourceDownloader.cs
@@ -0,0 +1,17 @@
+﻿using ShipWorks.ApplicationCore.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Shipping.ShipEngine
+{
+    public interface IShipEngineResourceDownloader
+    {
+        /// <summary>
+        /// Download a resource from shipengine and return its contents.
+        /// </summary>        
+        byte[] Download(Uri uri, ApiLogSource logSource, string actionName);
+    }
+}
