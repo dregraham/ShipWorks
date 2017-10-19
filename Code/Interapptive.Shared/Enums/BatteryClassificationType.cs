a17aae6544b6703230e53e800d04d9348b1ec73d
Josh Winchester <j.winchester@shipworks.com>
2017-10-19 15:28:03 -0500

    TP#20471: Add FedEx PackageType enum and BatteryClassificationType enum

---
+++ Code/Interapptive.Shared/Enums/BatteryClassificationType.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.ComponentModel;
+
+namespace Interapptive.Shared.Enums
+{
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum BatteryClassificationType
+    {
+        [Description("Lithium Ion")]
+        LithiumIon = 0,
+
+        [Description("Lithium Metal")]
+        LithiumMetal = 1,
+    }
+}
---
+++ Code/Interapptive.Shared/Enums/PackageType.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.ComponentModel;
+
+namespace Interapptive.Shared.Enums
+{
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum PackageType
+    {
+        [Description("Contained in equipment")]
+        ContainsInEquipement = 0,
+
+        [Description("Packed with equipment")]
+        PackedWithEquipment = 1,
+    }
+}
