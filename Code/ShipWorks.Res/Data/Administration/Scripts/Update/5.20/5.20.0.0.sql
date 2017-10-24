b8b9afa1d33eef3773ac254a3aca8898a5058174
Tim Hughes <t.hughes@shipworks.com>
2017-10-24 15:18:37 -0500

    TP#20444: Added FedEx shipping settings  username/pwd update for new api

---
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.20/5.20.0.0.sql
@@ -0,0 +1,7 @@
+﻿PRINT N'Updating [dbo].[ShippingSettings]'
+GO
+UPDATE ShippingSettings
+SET	FedExUsername = 'MFG2EvMKBLcxcCsk',
+	FedExPassword = 'nF4kG4o3/NwRrGa+QhLZtw95OnmtqNMr6mhhziyFEYE='
+	WHERE ISNULL(FedExUsername,'') != ''
+GO
