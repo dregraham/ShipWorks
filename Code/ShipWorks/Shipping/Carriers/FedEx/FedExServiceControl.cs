f2c1b2c1f927cc6838be887f091a279d816d4a1f
Tim Hughes <t.hughes@shipworks.com>
2017-10-25 06:53:15 -0500

    TP#20444: Removed the registered mark from SmartPost for the lower case parcel services, as per branding certification.

--- Code/ShipWorks/Shipping/Carriers/FedEx/FedExServiceControl.cs
+++ Code/ShipWorks/Shipping/Carriers/FedEx/FedExServiceControl.cs
@@ -1087,10 +1087,10 @@ namespace ShipWorks.Shipping.Carriers.FedEx
                 "FedEx Evening Home Delivery®\n" +
                 "FedEx Appointment Home Delivery®\n" +
                 "FedEx SmartPost®\n" +
-                "FedEx SmartPost® parcel select lightweight\n" +
+                "FedEx SmartPost parcel select lightweight\n" +
                 "FedEx SmartPost® Bound Printed Matter\n" +
                 "FedEx SmartPost® Media\n" +
-                "FedEx SmartPost® parcel select\n" +
+                "FedEx SmartPost parcel select\n" +
                 "FedEx ShipAlert®\n" +
                 "FedEx Priority Alert Plus™\n" +
                 "FedEx International Ground® Distribution\n" +
