ee12e220df100877f27b87240b5edf9b230db97b
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-10-25 08:49:15 -0500

    add comments to dto

--- Code/ShipWorks/Stores/Platforms/Magento/DTO/MagentoUploadAction.cs
+++ Code/ShipWorks/Stores/Platforms/Magento/DTO/MagentoUploadAction.cs
@@ -11,11 +11,34 @@ namespace ShipWorks.Stores.Platforms.Magento
     /// </summary>
     public struct MagentoUploadAction
     {
+        /// <summary>
+        /// Order number
+        /// </summary>
         public long OrderNumber { get; set; }
+
+        /// <summary>
+        /// Action (complete, cancel, hold)
+        /// </summary>
         public string Action { get; set; }
+
+        /// <summary>
+        /// Comments to add to the order
+        /// </summary>
         public string Comments { get; set; }
+
+        /// <summary>
+        /// Carrier used for shipping
+        /// </summary>
         public string Carrier { get; set; }
+
+        /// <summary>
+        /// Tracking number of the shipment
+        /// </summary>
         public string TrackingNumber { get; set; }
+
+        /// <summary>
+        /// Should we send an email to the user
+        /// </summary>
         public bool SendEmail { get; set; }
     }
 }
