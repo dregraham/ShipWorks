0e6706973a1812de2aa1733385f84e7729198a35
Andrew Benz <a.benz@shipworks.com>
2017-10-30 10:54:34 -0500

    TP#21773: Add new interface for request manipulators

---
+++ Code/ShipWorks/Shipping/Carriers/FedEx/Api/Rate/Request/Manipulators/IFedExRateRequestManipulator.cs
@@ -0,0 +1,23 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Data.Model.EntityInterfaces;
+using ShipWorks.Shipping.Carriers.FedEx.WebServices.Rate;
+
+namespace ShipWorks.Shipping.Carriers.FedEx.Api.Rate.Request.Manipulators
+{
+    /// <summary>
+    /// Manipulators for a FedEx rate request
+    /// </summary>
+    [Service]
+    public interface IFedExRateRequestManipulator
+    {
+        /// <summary>
+        /// Manipulate the given request
+        /// </summary>
+        RateRequest Manipulate(IShipmentEntity shipment, RateRequest request);
+
+        /// <summary>
+        /// Should the manipulator be applied
+        /// </summary>
+        bool ShouldApply(IShipmentEntity shipment, FedExRateRequestOptions options);
+    }
+}
