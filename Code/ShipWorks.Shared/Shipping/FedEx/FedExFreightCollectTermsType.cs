6f2e497f22c6e4b16c51673d9172694cd0dc6fbb
Tim Hughes <t.hughes@shipworks.com>
2017-10-30 11:40:10 -0500

    TP#21773: Added FedEx freight collect terms

---
+++ Code/ShipWorks.Shared/Shipping/FedEx/FedExFreightCollectTermsType.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using Interapptive.Shared.Utility;
+
+namespace ShipWorks.Shipping.FedEx
+{
+    /// <summary>
+    /// Enum representing FedExFreightCollectTerms
+    /// </summary>
+    [Obfuscation(Exclude = true, ApplyToMembers = true, StripAfterObfuscation = false)]
+    public enum FedExFreightCollectTermsType
+    {
+        /// <summary>
+        /// None selected
+        /// </summary>
+        [Description("None")]
+        [ApiValue("")]
+        None = 1,
+
+        /// <summary>
+        /// Standard
+        /// </summary>
+        [Description("Standard")]
+        [ApiValue("STANDARD")]
+        Standard = 2,
+
+        /// <summary>
+        /// Non-recourse shipper signed
+        /// </summary>
+        [Description("Non-recourse shipper signed")]
+        [ApiValue("NON_RECOURSE_SHIPPER_SIGNED")]
+        NonRecourseShipperSigned = 2,
+    }
+}
