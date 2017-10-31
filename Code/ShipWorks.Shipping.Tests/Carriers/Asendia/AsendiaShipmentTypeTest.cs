1c8c9f33754c1850f0356a80737a82c6926e41e4
Chris Hicks <c.hicks@shipworks.com>
2017-10-31 15:11:51 -0500

    TP#21795: Create AsendiaShipmentType

---
+++ Code/ShipWorks.Shipping.Tests/Carriers/Asendia/AsendiaShipmentTypeTest.cs
@@ -0,0 +1,31 @@
+﻿using Autofac.Extras.Moq;
+using ShipWorks.Shipping.Carriers.Asendia;
+using ShipWorks.Tests.Shared;
+using System;
+using Xunit;
+
+namespace ShipWorks.Shipping.Tests.Carriers.Asendia
+{
+    public class AsendiaShipmentTypeTest : IDisposable
+    {
+        AutoMock mock;
+
+        public AsendiaShipmentTypeTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+        }
+        
+        [Fact]
+        public void ShipmentTypeCode_IsAsendia()
+        {
+            var testObject = mock.Create<AsendiaShipmentType>();
+
+            Assert.Equal(ShipmentTypeCode.Asendia, testObject.ShipmentTypeCode);
+        }
+        
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+    }
+}
---
+++ Code/ShipWorks/Shipping/Carriers/Asendia/AsendiaShipmentType.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.BestRate;
+using ShipWorks.Shipping.Services;
+using Interapptive.Shared.ComponentRegistration;
+
+namespace ShipWorks.Shipping.Carriers.Asendia
+{
+    /// <summary>
+    /// Asendia implementation of shipment type
+    /// </summary>
+    [KeyedComponent(typeof(ShipmentType), ShipmentTypeCode.Asendia, SingleInstance = true)]
+    public class AsendiaShipmentType : ShipmentType
+    {
+        /// <summary>
+        /// The ShipmentTypeCode represented by this ShipmentType
+        /// </summary>
+        public override ShipmentTypeCode ShipmentTypeCode => ShipmentTypeCode.Asendia;
+
+        /// <summary>
+        /// Gets the package adapter for the shipment.
+        /// </summary>
+        public override IEnumerable<IPackageAdapter> GetPackageAdapters(ShipmentEntity shipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Get the parcel data that describes details about a particular parcel
+        /// </summary>
+        public override ShipmentParcel GetParcelDetail(ShipmentEntity shipment, int parcelIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Get the carrier specific description of the shipping service used. The carrier specific data must already exist
+        /// when this method is called.
+        /// </summary>
+        public override string GetServiceDescription(ShipmentEntity shipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the best rate shipping broker for Asendia
+        /// </summary>
+        public override IBestRateShippingBroker GetShippingBroker(ShipmentEntity shipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Ensures that the carrier specific data for the shipment, such as the DhlExpress data, are loaded for the shipment.  If the data
+        /// already exists, nothing is done: it is not refreshed.  This method can throw SqlForeignKeyException if the root shipment
+        /// or order has been deleted, ORMConcurrencyException if the shipment had been edited elsewhere, and ObjectDeletedException if the shipment
+        /// had been deleted.
+        /// </summary>
+        protected override void LoadShipmentDataInternal(ShipmentEntity shipment, bool refreshIfPresent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
