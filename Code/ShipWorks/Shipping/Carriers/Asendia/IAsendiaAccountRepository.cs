359468bf2c1fc65077f851615e886e626fdd6e46
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-11-01 09:20:33 -0500

    TP#21795 create AsendiaAccountRepository and interface

---
+++ Code/ShipWorks/Shipping/Carriers/Asendia/IAsendiaAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Data.Model.EntityInterfaces;
+
+namespace ShipWorks.Shipping.Carriers.Asendia
+{
+    /// <summary>
+    /// Repository for Asendia accounts
+    /// </summary>
+    public interface IAsendiaAccountRepository : ICarrierAccountRepository<ShipEngineAccountEntity, IShipEngineAccountEntity>
+    {
+    }
+}
