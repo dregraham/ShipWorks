30ee36e0dad0e99898fa4fd95d4e68ab5a880ba8
Tim Hughes <t.hughes@shipworks.com>
2017-11-01 15:25:42 -0500

    TP#21773: Updated FedExWebAuthenticationManipulator

--- Code/ShipWorks.Shipping.Tests/Carriers/FedEx/Api/Rate/Manipulators/Request/FedExRateWebAuthenticationManipulatorTest.cs
+++ Code/ShipWorks.Shipping.Tests/Carriers/FedEx/Api/Rate/Manipulators/Request/FedExRateWebAuthenticationManipulatorTest.cs
@@ -5,6 +5,7 @@ using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Shipping.Api;
 using ShipWorks.Shipping.Carriers.Api;
 using ShipWorks.Shipping.Carriers.FedEx.Api.Environment;
+using ShipWorks.Shipping.Carriers.FedEx.Api.Rate;
 using ShipWorks.Shipping.Carriers.FedEx.Api.Rate.Manipulators.Request;
 using ShipWorks.Shipping.Carriers.FedEx.WebServices.Rate;
 using Xunit;
@@ -20,7 +21,7 @@ namespace ShipWorks.Shipping.Tests.Carriers.FedEx.Api.Rate.Manipulators.Request
         private FedExSettings settings;
 
         private Mock<CarrierRequest> carrierRequest;
-        private RateRequest nativeRequest;
+        private RateRequest rateRequest;
 
         public FedExRateWebAuthenticationManipulatorTest()
         {
@@ -31,53 +32,43 @@ namespace ShipWorks.Shipping.Tests.Carriers.FedEx.Api.Rate.Manipulators.Request
 
             settings = new FedExSettings(settingsRepository.Object);
 
-            nativeRequest = new RateRequest { WebAuthenticationDetail = new WebAuthenticationDetail() };
-            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), new ShipmentEntity(), nativeRequest);
+            rateRequest = new RateRequest { WebAuthenticationDetail = new WebAuthenticationDetail() };
+            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), new ShipmentEntity(), rateRequest);
 
             testObject = new FedExRateWebAuthenticationManipulator(settings);
         }
 
         [Fact]
-        public void Manipulate_ThrowsArgumentNullException_WhenCarrierRequestIsNull()
+        public void Shouldapply_ReturnsTrue()
         {
-            Assert.Throws<ArgumentNullException>(() => testObject.Manipulate(null));
+            Assert.True(testObject.ShouldApply(null, FedExRateRequestOptions.None));
         }
 
         [Fact]
-        public void Manipulate_ThrowsCarrierException_WhenNativeRequestIsNull()
+        public void Manipulate_ThrowsArgumentNullException_WhenRateRequestIsNull()
         {
-            // Setup the native request to be null
-            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), new ShipmentEntity(), null);
-
-            Assert.Throws<CarrierException>(() => testObject.Manipulate(carrierRequest.Object));
-        }
-
-        [Fact]
-        public void Manipulate_ThrowsCarrierException_WhenNativeRequestIsNotRateRequest()
-        {
-            // Setup the native request to be an unexpected type
-            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), new ShipmentEntity(), new RateReply());
-
-            Assert.Throws<CarrierException>(() => testObject.Manipulate(carrierRequest.Object));
+            Assert.Throws<ArgumentNullException>(() => testObject.Manipulate(new ShipmentEntity(), null));
         }
 
         [Fact]
         public void Manipulate_SetsWebAuthenticationDetail_WhenWebAuthenticationDetailsIsNull()
         {
             // Only setup is  to set the detail to null value
-            nativeRequest.WebAuthenticationDetail = null;
+            rateRequest.WebAuthenticationDetail = null;
 
-            testObject.Manipulate(carrierRequest.Object);
+            testObject.Manipulate(null, rateRequest);
 
             WebAuthenticationDetail detail = ((RateRequest) carrierRequest.Object.NativeRequest).WebAuthenticationDetail;
             Assert.NotNull(detail);
+            Assert.Equal(settings.UserCredentialsPassword, detail.UserCredential.Password);
+            Assert.Equal(settings.UserCredentialsKey, detail.UserCredential.Key);
         }
 
         [Fact]
         public void Manipulate_SetsWebAuthenticationDetail_WhenWebAuthenticationDetailsIsNotNull()
         {
             // No additional setup since everything is in the Initialize method
-            testObject.Manipulate(carrierRequest.Object);
+            testObject.Manipulate(null, rateRequest);
 
             WebAuthenticationDetail detail = ((RateRequest) carrierRequest.Object.NativeRequest).WebAuthenticationDetail;
             Assert.NotNull(detail);
--- Code/ShipWorks.Shipping/Carriers/FedEx/Api/Rate/Manipulators/Request/FedExRateWebAuthenticationManipulator.cs
+++ Code/ShipWorks.Shipping/Carriers/FedEx/Api/Rate/Manipulators/Request/FedExRateWebAuthenticationManipulator.cs
@@ -1,65 +1,59 @@
 ﻿using System;
+using ShipWorks.Data.Model.EntityInterfaces;
 using ShipWorks.Shipping.Carriers.Api;
 using ShipWorks.Shipping.Carriers.FedEx.Api.Environment;
 using ShipWorks.Shipping.Carriers.FedEx.WebServices.Rate;
 
 namespace ShipWorks.Shipping.Carriers.FedEx.Api.Rate.Manipulators.Request
 {
-    public class FedExRateWebAuthenticationManipulator: ICarrierRequestManipulator
+    public class FedExRateWebAuthenticationManipulator: IFedExRateRequestManipulator
     {
         private readonly FedExSettings fedExSettings;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="FedExShippingWebAuthenticationDetailManipulator" /> class using 
-        /// a FedExSettings backed by the FedExSettingsRepository.
+        /// Constructor
         /// </summary>
         public FedExRateWebAuthenticationManipulator()
             : this(new FedExSettings(new FedExSettingsRepository()))
         { }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="FedExShippingWebAuthenticationDetailManipulator" /> class.
+        /// Constructor
         /// </summary>
-        /// <param name="fedExSettings">The FedEx settings.</param>
         public FedExRateWebAuthenticationManipulator(FedExSettings fedExSettings)
         {
             this.fedExSettings = fedExSettings;
         }
 
         /// <summary>
+        /// Does this manipulator apply to the shipment
+        /// </summary>
+        public bool ShouldApply(IShipmentEntity shipment, FedExRateRequestOptions options)
+        {
+            return true;
+        }
+
+        /// <summary>
         /// Manipulates the specified request by setting the WebAuthenticationDetail property of a RateRequest object.
         /// </summary>
-        /// <param name="request">The request being manipulated.</param>
-        public void Manipulate(CarrierRequest request)
+        public RateRequest Manipulate(IShipmentEntity shipment, RateRequest request)
         {
             // Make sure all of the properties we'll be accessing have been created
             ValidateRequest(request);
 
-            // We can safely cast this since we've passed validation
-            RateRequest nativeRequest = request.NativeRequest as RateRequest;
+            request.WebAuthenticationDetail = FedExRequestManipulatorUtilities.CreateRateWebAuthenticationDetail(fedExSettings);
 
-            nativeRequest.WebAuthenticationDetail = FedExRequestManipulatorUtilities.CreateRateWebAuthenticationDetail(fedExSettings);
+            return request;
         }
 
         /// <summary>
         /// Validates the request making sure it is not null and of the correct type.
         /// </summary>
-        /// <param name="request">The request.</param>
-        /// <exception cref="System.ArgumentNullException">request</exception>
-        /// <exception cref="CarrierException">An unexpected request type was provided.</exception>
-        private void ValidateRequest(CarrierRequest request)
+        private void ValidateRequest(RateRequest request)
         {
             if (request == null)
             {
                 throw new ArgumentNullException("request");
-            }
-
-            // The native FedEx request type should be a RateRequest
-            RateRequest nativeRequest = request.NativeRequest as RateRequest;
-            if (nativeRequest == null)
-            {
-                // Abort - we have an unexpected native request
-                throw new CarrierException("An unexpected request type was provided.");
             }
         }
     }
