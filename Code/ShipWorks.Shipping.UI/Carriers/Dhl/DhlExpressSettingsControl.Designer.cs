9ede77f39cee4f312c15a40375230a33c4993466
Chris Hicks <c.hicks@shipworks.com>
2017-11-03 11:03:19 -0500

    Make DhlExpressSettingsControl look a bit nicer

--- Code/ShipWorks.Shipping.UI/Carriers/Dhl/DhlExpressSettingsControl.Designer.cs
+++ Code/ShipWorks.Shipping.UI/Carriers/Dhl/DhlExpressSettingsControl.Designer.cs
@@ -39,7 +39,7 @@
             // carrierAccountManagerControl
             // 
             this.carrierAccountManagerControl.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.carrierAccountManagerControl.Location = new System.Drawing.Point(30, 111);
+            this.carrierAccountManagerControl.Location = new System.Drawing.Point(25, 100);
             this.carrierAccountManagerControl.Name = "carrierAccountManagerControl";
             this.carrierAccountManagerControl.Size = new System.Drawing.Size(462, 168);
             this.carrierAccountManagerControl.TabIndex = 0;
@@ -48,7 +48,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 90);
+            this.label1.Location = new System.Drawing.Point(6, 79);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(131, 13);
             this.label1.TabIndex = 1;
@@ -57,7 +57,7 @@
             // shippingCutoff
             // 
             this.shippingCutoff.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.shippingCutoff.Location = new System.Drawing.Point(27, 62);
+            this.shippingCutoff.Location = new System.Drawing.Point(22, 50);
             this.shippingCutoff.Name = "shippingCutoff";
             this.shippingCutoff.Size = new System.Drawing.Size(467, 22);
             this.shippingCutoff.TabIndex = 16;
@@ -66,7 +66,7 @@
             // 
             this.labelsLabel.AutoSize = true;
             this.labelsLabel.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelsLabel.Location = new System.Drawing.Point(6, 10);
+            this.labelsLabel.Location = new System.Drawing.Point(6, 8);
             this.labelsLabel.Name = "labelsLabel";
             this.labelsLabel.Size = new System.Drawing.Size(43, 13);
             this.labelsLabel.TabIndex = 17;
@@ -75,7 +75,7 @@
             // requestedLabelFormatOptionControl
             // 
             this.requestedLabelFormatOptionControl.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.requestedLabelFormatOptionControl.Location = new System.Drawing.Point(27, 31);
+            this.requestedLabelFormatOptionControl.Location = new System.Drawing.Point(22, 24);
             this.requestedLabelFormatOptionControl.Name = "requestedLabelFormatOptionControl";
             this.requestedLabelFormatOptionControl.Size = new System.Drawing.Size(344, 23);
             this.requestedLabelFormatOptionControl.TabIndex = 18;
@@ -83,9 +83,9 @@
             // excludedServiceControl
             // 
             this.excludedServiceControl.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.excludedServiceControl.Location = new System.Drawing.Point(7, 305);
+            this.excludedServiceControl.Location = new System.Drawing.Point(7, 281);
             this.excludedServiceControl.Name = "excludedServiceControl";
-            this.excludedServiceControl.Size = new System.Drawing.Size(379, 130);
+            this.excludedServiceControl.Size = new System.Drawing.Size(384, 130);
             this.excludedServiceControl.TabIndex = 19;
             // 
             // DhlExpressSettingsControl
@@ -99,7 +99,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.carrierAccountManagerControl);
             this.Name = "DhlExpressSettingsControl";
-            this.Size = new System.Drawing.Size(500, 641);
+            this.Size = new System.Drawing.Size(500, 470);
             this.ResumeLayout(false);
             this.PerformLayout();
 
