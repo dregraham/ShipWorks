5d28cd6d9c0dc9aba5fba061e446ee2fc3a75919
Andrew Benz <a.benz@shipworks.com>
2017-11-06 08:07:27 -0600

    TP#21774: Update TotalWeight manipulator to use new interface

--- Code/ShipWorks.Shipping.Tests/Carriers/FedEx/Api/Ship/Manipulators/Request/FedExTotalWeightManipulatorTest.cs
+++ Code/ShipWorks.Shipping.Tests/Carriers/FedEx/Api/Ship/Manipulators/Request/FedExTotalWeightManipulatorTest.cs
@@ -1,111 +1,70 @@
-using System;
-using System.Collections.Generic;
+using Autofac.Extras.Moq;
 using Interapptive.Shared.Enums;
-using Xunit;
-using Moq;
 using ShipWorks.Data.Model.EntityClasses;
-using ShipWorks.Shipping.Carriers.Api;
 using ShipWorks.Shipping.Carriers.FedEx.Api.Shipping.Request.Manipulators;
 using ShipWorks.Shipping.Carriers.FedEx.WebServices.Ship;
+using ShipWorks.Tests.Shared;
+using Xunit;
 
 namespace ShipWorks.Tests.Shipping.Carriers.FedEx.Api.Shipping.Request.Manipulators
 {
     public class FedExTotalWeightManipulatorTest
     {
-        FedExTotalWeightManipulator testObject;
-
-        private Mock<CarrierRequest> carrierRequest;
-        private ProcessShipmentRequest nativeRequest;
-        private ShipmentEntity shipmentEntity;
+        private readonly FedExTotalWeightManipulator testObject;
+        private readonly AutoMock mock;
+        private readonly ShipmentEntity shipment;
 
         public FedExTotalWeightManipulatorTest()
         {
-            // Create a ProcessShipmentRequest type and set the properties the manipulator is interested in
-            nativeRequest = new ProcessShipmentRequest()
-            {
-                RequestedShipment = new RequestedShipment()
-            };
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
 
             // Create our default shipment entity and initialize the properties our test object will be accessing
-            shipmentEntity = new ShipmentEntity()
+            shipment = new ShipmentEntity()
             {
                 TotalWeight = 3.245f,
-                FedEx = new FedExShipmentEntity { WeightUnitType = (int)WeightUnitOfMeasure.Pounds }
+                FedEx = new FedExShipmentEntity { WeightUnitType = (int) WeightUnitOfMeasure.Pounds }
             };
-
-            // Setup the carrier request's NativeRequest property to return the ProcessShipmentRequest object
-            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), shipmentEntity, nativeRequest);
 
             testObject = new FedExTotalWeightManipulator();
         }
 
         [Fact]
-        public void Manipulate_ThrowsArgumentNullException_WhenCarrierRequestIsNull()
-        {
-            Assert.Throws<ArgumentNullException>(() => testObject.Manipulate(null));
-        }
-
-        [Fact]
-        public void Manipulate_ThrowsCarrierException_WhenNativeRequestIsNull()
-        {
-            // Setup the native request to be null
-            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), shipmentEntity, null);
-
-            Assert.Throws<CarrierException>(() => testObject.Manipulate(carrierRequest.Object));
-        }
-
-        [Fact]
-        public void Manipulate_ThrowsCarrierException_WhenNativeRequestIsNotProcessShipmentRequest()
-        {
-            // Setup the native request to be an unexpected type
-            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), shipmentEntity, new object());
-
-            Assert.Throws<CarrierException>(() => testObject.Manipulate(carrierRequest.Object));
-        }
-
-        [Fact]
         public void Manipulate_AccountsForNullRequestedShipment()
         {
-            // Setup the test by configuring the native request to have a null requested shipment property and re-initialize
-            // the carrier request with the updated native request
-            nativeRequest.RequestedShipment = null;
-            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), shipmentEntity, nativeRequest);
+            var result = testObject.Manipulate(shipment, new ProcessShipmentRequest(), 0);
 
-            testObject.Manipulate(carrierRequest.Object);
-
-            // The requested shipment property should be created now
-            Assert.NotNull(nativeRequest.RequestedShipment);
+            Assert.NotNull(result.Value.RequestedShipment);
         }
 
         [Fact]
         public void Manipulate_SetsUnitsToPounds()
         {
-            shipmentEntity.FedEx.WeightUnitType = (int)WeightUnitOfMeasure.Pounds;
+            shipment.FedEx.WeightUnitType = (int) WeightUnitOfMeasure.Pounds;
 
-            testObject.Manipulate(carrierRequest.Object);
+            var result = testObject.Manipulate(shipment, new ProcessShipmentRequest(), 0);
 
-            Weight weight = ((ProcessShipmentRequest)carrierRequest.Object.NativeRequest).RequestedShipment.TotalWeight;
+            Weight weight = result.Value.RequestedShipment.TotalWeight;
             Assert.Equal(WeightUnits.LB, weight.Units);
         }
 
         [Fact]
         public void Manipulate_SetsUnitsToKilograms()
         {
-            shipmentEntity.FedEx.WeightUnitType = (int)WeightUnitOfMeasure.Kilograms;
+            shipment.FedEx.WeightUnitType = (int) WeightUnitOfMeasure.Kilograms;
 
-            testObject.Manipulate(carrierRequest.Object);
+            var result = testObject.Manipulate(shipment, new ProcessShipmentRequest(), 0);
 
-            Weight weight = ((ProcessShipmentRequest)carrierRequest.Object.NativeRequest).RequestedShipment.TotalWeight;
+            Weight weight = result.Value.RequestedShipment.TotalWeight;
             Assert.Equal(WeightUnits.KG, weight.Units);
         }
 
         [Fact]
         public void Manipulate_SetsWeightValue()
         {
-            testObject.Manipulate(carrierRequest.Object);
+            var result = testObject.Manipulate(shipment, new ProcessShipmentRequest(), 0);
 
-            Weight weight = ((ProcessShipmentRequest)carrierRequest.Object.NativeRequest).RequestedShipment.TotalWeight;
-            Assert.Equal((decimal)shipmentEntity.TotalWeight, weight.Value);
+            Weight weight = result.Value.RequestedShipment.TotalWeight;
+            Assert.Equal((decimal) shipment.TotalWeight, weight.Value);
         }
     }
 }
--- Code/ShipWorks.Shipping/Carriers/FedEx/Api/Ship/Manipulators/Request/FedExTotalWeightManipulator.cs
+++ Code/ShipWorks.Shipping/Carriers/FedEx/Api/Ship/Manipulators/Request/FedExTotalWeightManipulator.cs
@@ -1,8 +1,7 @@
-using System;
 using Interapptive.Shared.Enums;
-using ShipWorks.Data.Model.EntityClasses;
-using ShipWorks.Shipping.Carriers.Api;
-using ShipWorks.Shipping.Carriers.FedEx.Api.Environment;
+using Interapptive.Shared.Utility;
+using ShipWorks.Data.Model.EntityInterfaces;
+using ShipWorks.Shipping.Carriers.FedEx.Api.Rate.Manipulators.Request.International;
 using ShipWorks.Shipping.Carriers.FedEx.WebServices.Ship;
 
 namespace ShipWorks.Shipping.Carriers.FedEx.Api.Shipping.Request.Manipulators
@@ -11,38 +10,24 @@ namespace ShipWorks.Shipping.Carriers.FedEx.Api.Shipping.Request.Manipulators
     /// An implementation of the ICarrierRequestManipulator interface that will manipulate the total weight
     /// of the IFedExNativeShipmentRequest based on the shipment entity's total weight.
     /// </summary>
-    public class FedExTotalWeightManipulator : FedExShippingRequestManipulatorBase
+    public class FedExTotalWeightManipulator : IFedExShipRequestManipulator
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="FedExTotalWeightManipulator" /> class.
+        /// Should the manipulator be applied
         /// </summary>
-        public FedExTotalWeightManipulator()
-            : this(new FedExSettings(new FedExSettingsRepository()))
-        {}
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="FedExTotalWeightManipulator" /> class.
-        /// </summary>
-        /// <param name="fedExSettings">The fed ex settings.</param>
-        public FedExTotalWeightManipulator(FedExSettings fedExSettings)
-            : base(fedExSettings)
-        {
-        }
+        public bool ShouldApply(IShipmentEntity shipment) => true;
 
         /// <summary>
         /// Manipulates the specified request.
         /// </summary>
         /// <param name="request">The request being manipulated.</param>
-        public override void Manipulate(CarrierRequest request)
+        public GenericResult<ProcessShipmentRequest> Manipulate(IShipmentEntity shipment, ProcessShipmentRequest request, int sequenceNumber)
         {
-            // Make sure all of the properties we'll be accessing have been created
             InitializeRequest(request);
 
-            // We can safely cast this since we've passed initialization 
-            IFedExNativeShipmentRequest nativeRequest = request.NativeRequest as IFedExNativeShipmentRequest;
+            request.RequestedShipment.TotalWeight = new Weight { Units = GetApiWeightUnit(shipment), Value = (decimal) shipment.TotalWeight };
 
-            // Just need to assign the weight value in pounds
-            nativeRequest.RequestedShipment.TotalWeight = new Weight { Units =GetApiWeightUnit(request.ShipmentEntity), Value = (decimal)request.ShipmentEntity.TotalWeight };
+            return GenericResult.FromSuccess(request);
         }
 
         /// <summary>
@@ -51,38 +36,19 @@ namespace ShipWorks.Shipping.Carriers.FedEx.Api.Shipping.Request.Manipulators
         /// <param name="request">The request.</param>
         /// <exception cref="System.ArgumentNullException">request</exception>
         /// <exception cref="CarrierException">An unexpected request type was provided.</exception>
-        private void InitializeRequest(CarrierRequest request)
-        {
-            if (request == null)
-            {
-                throw new ArgumentNullException("request");
-            }
-
-            // The native FedEx request type should be a IFedExNativeShipmentRequest
-            IFedExNativeShipmentRequest nativeRequest = request.NativeRequest as IFedExNativeShipmentRequest;
-            if (nativeRequest == null)
-            {
-                // Abort - we have an unexpected native request
-                throw new CarrierException("An unexpected request type was provided.");
-            }
-
-            if (nativeRequest.RequestedShipment == null)
-            {
-                // We'll be manipulating properties of the requested shipment, so make sure it's been created
-                nativeRequest.RequestedShipment = new RequestedShipment();
-            }
-        }
+        private void InitializeRequest(ProcessShipmentRequest request) =>
+            request.Ensure(x => x.RequestedShipment);
 
         /// <summary>
         /// Gets the FedEx API weight unit.
         /// </summary>
         /// <param name="shipment">The shipment.</param>
         /// <returns>The FedEx WeightUnits value.</returns>
-        private WeightUnits GetApiWeightUnit(ShipmentEntity shipment)
+        private WeightUnits GetApiWeightUnit(IShipmentEntity shipment)
         {
             WeightUnits weightUnits = WeightUnits.LB;
 
-            if (shipment.FedEx.WeightUnitType == (int)WeightUnitOfMeasure.Kilograms)
+            if (shipment.FedEx.WeightUnitType == (int) WeightUnitOfMeasure.Kilograms)
             {
                 weightUnits = WeightUnits.KG;
             }
