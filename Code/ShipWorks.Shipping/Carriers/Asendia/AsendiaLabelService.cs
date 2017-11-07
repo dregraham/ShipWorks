863c7bf4b85f765e96de5af78db8e3c9ad76f6b3
Chris Hicks <c.hicks@shipworks.com>
2017-11-07 11:13:45 -0600

    TP#21803: Create AsendiaDownloadedLabelData and AsendiaLabelService

--- Code/ShipWorks.Shipping/Carriers/Asendia/AsendiaLabelService.cs
+++ Code/ShipWorks.Shipping/Carriers/Asendia/AsendiaLabelService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Autofac.Features.Indexed;
+using ShipWorks.ApplicationCore.Logging;
+using System;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Shipping.ShipEngine;
 using ShipEngine.ApiClient.Model;
@@ -7,40 +9,39 @@ using Autofac.Features.Indexed;
 using Interapptive.Shared.ComponentRegistration;
 using log4net;
 
-namespace ShipWorks.Shipping.Carriers.Dhl
+namespace ShipWorks.Shipping.Carriers.Asendia
 {
     /// <summary>
-    /// Dhl Express Implmentation
+    /// Label service for Asendia
     /// </summary>
-    [KeyedComponent(typeof(ILabelService), ShipmentTypeCode.DhlExpress)]
-    public class DhlExpressLabelService : ShipEngineLabelService
+    public class AsendiaLabelService : ShipEngineLabelService
     {
-        private readonly IDhlExpressAccountRepository accountRepository;
+        private readonly IAsendiaAccountRepository accountRepository;
 
         /// <summary>
         /// Constructor
         /// </summary>
-        public DhlExpressLabelService(
+        public AsendiaLabelService(
             IShipEngineWebClient shipEngineWebClient,
-            IDhlExpressAccountRepository accountRepository,
+            IAsendiaAccountRepository accountRepository,
             IIndex<ShipmentTypeCode, ICarrierShipmentRequestFactory> shipmentRequestFactory,
-            Func<ShipmentEntity, Label, DhlExpressDownloadedLabelData> createDownloadedLabelData,
+            Func<ShipmentEntity, Label, AsendiaDownloadedLabelData> createDownloadedLabelData,
             Func<Type, ILog> logFactory) 
             : base(shipEngineWebClient, shipmentRequestFactory, createDownloadedLabelData)
         {
-            log = logFactory(typeof(DhlExpressLabelService));
+            log = logFactory(typeof(AsendiaLabelService));
             this.accountRepository = accountRepository;
         }
 
         /// <summary>
         /// The api log source for this label service
         /// </summary>
-        public override ApiLogSource ApiLogSource => ApiLogSource.DHLExpress;
+        public override ApiLogSource ApiLogSource => ApiLogSource.Asendia;
 
         /// <summary>
         /// The shipment type code for this label service
         /// </summary>
-        public override ShipmentTypeCode ShipmentTypeCode => ShipmentTypeCode.DhlExpress;
+        public override ShipmentTypeCode ShipmentTypeCode => ShipmentTypeCode.Asendia;
 
         /// <summary>
         /// Get the ShipEngine carrier ID from the shipment
@@ -50,6 +51,6 @@ namespace ShipWorks.Shipping.Carriers.Dhl
         /// <summary>
         /// Get the ShipEngine label ID from the shipment
         /// </summary>
-        protected override string GetShipEngineLabelID(ShipmentEntity shipment) => shipment.DhlExpress.ShipEngineLabelID;        
+        protected override string GetShipEngineLabelID(ShipmentEntity shipment) => shipment.Asendia.ShipEngineLabelID;
     }
 }
