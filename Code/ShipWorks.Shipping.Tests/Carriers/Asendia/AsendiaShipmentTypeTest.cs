964b2b49e10e519aeadec28d3c99c1341a91e21e
Chris Hicks <c.hicks@shipworks.com>
2017-11-08 09:46:00 -0600

    TP#21805: Implement TrackShipment in AsendiaShipmentType

--- Code/ShipWorks.Shipping.Tests/Carriers/Asendia/AsendiaShipmentTypeTest.cs
+++ Code/ShipWorks.Shipping.Tests/Carriers/Asendia/AsendiaShipmentTypeTest.cs
@@ -1,12 +1,18 @@
 ﻿using Autofac.Extras.Moq;
 using Interapptive.Shared.Enums;
+using Moq;
+using ShipEngine.ApiClient.Model;
+using ShipWorks.ApplicationCore.Logging;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Data.Model.EntityInterfaces;
 using ShipWorks.Shipping.Carriers;
 using ShipWorks.Shipping.Carriers.Asendia;
 using ShipWorks.Shipping.ShipEngine;
+using ShipWorks.Shipping.Tracking;
 using ShipWorks.Tests.Shared;
+using ShipWorks.Tests.Shared.ExtensionMethods;
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace ShipWorks.Shipping.Tests.Carriers.Asendia
@@ -91,6 +97,96 @@ namespace ShipWorks.Shipping.Tests.Carriers.Asendia
             testObject.ConfigurePrimaryProfile(profile);
 
             Assert.Equal(123456789, profile.Asendia.AsendiaAccountID);
+        }
+
+        [Fact]
+        public void TrackShipment_DelegatesTrackingToWebClient()
+        {
+            var testObject = mock.Create<AsendiaShipmentType>();
+
+            var shipment = new ShipmentEntity()
+            {
+                Asendia = new AsendiaShipmentEntity()
+                {
+                    ShipEngineLabelID = "test"
+                }
+            };
+
+            testObject.TrackShipment(shipment);
+
+            mock.Mock<IShipEngineWebClient>().Verify(c => c.Track("test", ApiLogSource.Asendia), Times.Once);
+        }
+
+        [Fact]
+        public void TrackShipment_DelegatesTrackingInformationFromWebClient_ToTrackingResultFactory()
+        {
+            var testObject = mock.Create<AsendiaShipmentType>();
+
+            var shipment = new ShipmentEntity()
+            {
+                Asendia = new AsendiaShipmentEntity()
+                {
+                    ShipEngineLabelID = "test"
+                }
+            };
+
+            var trackingInformation = new TrackingInformation();
+
+            mock.Mock<IShipEngineWebClient>()
+                .Setup(c => c.Track(ParameterShorteners.AnyString, ApiLogSource.Asendia))
+                .Returns(Task.FromResult(trackingInformation));
+
+            testObject.TrackShipment(shipment);
+
+            mock.Mock<IShipEngineTrackingResultFactory>().Verify(f => f.Create(trackingInformation));
+        }
+
+        [Fact]
+        public void TrackShipment_ReturnsTrackingResultCreatedByFactory()
+        {
+            var testObject = mock.Create<AsendiaShipmentType>();
+
+            var shipment = new ShipmentEntity()
+            {
+                Asendia = new AsendiaShipmentEntity()
+                {
+                    ShipEngineLabelID = "test"
+                }
+            };
+
+            TrackingResult trackingResult = new TrackingResult();
+
+            mock.Mock<IShipEngineTrackingResultFactory>()
+                .Setup(c => c.Create(It.IsAny<TrackingInformation>()))
+                .Returns(trackingResult);
+
+            var testResult = testObject.TrackShipment(shipment);
+
+            Assert.Equal(trackingResult, testResult);
+        }
+
+        [Fact]
+        public void TrackShipment_ReturnsCannedResult_WhenExceptionIsThrown()
+        {
+            var testObject = mock.Create<AsendiaShipmentType>();
+
+            var shipment = new ShipmentEntity()
+            {
+                TrackingNumber = "test",
+                Asendia = new AsendiaShipmentEntity()
+                {
+                    ShipEngineLabelID = "foo"
+                }
+            };
+
+            mock.Mock<IShipEngineTrackingResultFactory>()
+                .Setup(c => c.Create(It.IsAny<TrackingInformation>()))
+                .Throws(new Exception());
+
+            var testResult = testObject.TrackShipment(shipment);
+            string expectedSummary = $"<a href='http://tracking.asendiausa.com/t.aspx?p={shipment.TrackingNumber}' style='color:blue; background-color:white'>Click here to view tracking information online</a>";
+
+            Assert.Equal(expectedSummary, testResult.Summary);
         }
 
         public void Dispose()
