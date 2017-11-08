ad053335d12723ffeb2c11205b9a77f600af2148
Andrew Benz <a.benz@shipworks.com>
2017-11-08 09:50:06 -0600

    TP#21774: Add shell for the LTL Freight label repository

---
+++ Code/ShipWorks.Shipping/Carriers/FedEx/Api/Ship/FedExLTLFreightLabelRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ShipWorks.Data.Model.EntityInterfaces;
+using ShipWorks.Shipping.Carriers.FedEx.Api.Shipping;
+using ShipWorks.Shipping.Carriers.FedEx.WebServices.Ship;
+
+namespace ShipWorks.Shipping.Carriers.FedEx.Api.Ship
+{
+    public class FedExLTLFreightLabelRepository : IFedExLabelRepository
+    {
+        public void ClearReferences(IShipmentEntity shipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveLabels(IShipmentEntity shipment, ProcessShipmentReply reply)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Code/ShipWorks/Shipping/Carriers/FedEx/Api/Shipping/IFedExLabelRepositoryFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Shipping.Carriers.FedEx.Api.Shipping
+{
+    public interface IFedExLabelRepositoryFactory
+    {
+    }
+}
