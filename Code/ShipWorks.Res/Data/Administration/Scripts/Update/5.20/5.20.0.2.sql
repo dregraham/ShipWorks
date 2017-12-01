70fdf4ce15c466ddf615b8b084932d51d9ae1701
Andrew Benz <a.benz@shipworks.com>
2017-12-01 11:47:54 -0600

    TP#22462: Add indexes to reduce likelihood of deadlock while processing ebay feedback

---
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.20/5.20.0.2.sql
@@ -0,0 +1,8 @@
+﻿CREATE NONCLUSTERED INDEX [SW_EbayOrderItem_EbayItemID_EbayTransactionID]
+       ON [dbo].[EbayOrderItem] ([EbayItemID],[EbayTransactionID])
+
+CREATE NONCLUSTERED INDEX [SW_EbayOrderItem_EffectiveCheckoutStatus_EbayOrderItemID]
+       ON [dbo].[EbayOrderItem] ([EffectiveCheckoutStatus], [OrderItemID])
+
+CREATE INDEX [SW_FilterNodeContentDirty_FilterNodeContentDirtyID] on dbo.FilterNodeContentDirty
+       (FilterNodeContentDirtyID)
