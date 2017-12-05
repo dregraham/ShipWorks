f1f28ffca958153b05a9b953abac650b553d6e2c
Tim Hughes <t.hughes@shipworks.com>
2017-12-05 08:22:02 -0600

    TP#21873: Converted LemonStand to use new quick search pattern.

---
+++ Code/ShipWorks.Stores.Tests/Platforms/LemonStand/Content/LemonStandQuickSearchSqlTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Autofac.Extras.Moq;
+using Moq;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content.SqlGeneration;
+using ShipWorks.Stores.Platforms.LemonStand.Content;
+using ShipWorks.Tests.Shared;
+using ShipWorks.Tests.Shared.ExtensionMethods;
+using Xunit;
+using static ShipWorks.Tests.Shared.ExtensionMethods.ParameterShorteners;
+
+namespace ShipWorks.Stores.Tests.Platforms.LemonStand.Content
+{
+    public class LemonStandQuickSearchSqlTest : IDisposable
+    {
+        readonly AutoMock mock;
+        private readonly Mock<ISqlGenerationBuilder> context;
+        private readonly LemonStandQuickSearchSql testObject;
+
+        public LemonStandQuickSearchSqlTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+
+            context = mock.Mock<ISqlGenerationBuilder>();
+            testObject = mock.Create<LemonStandQuickSearchSql>();
+        }
+
+        [Fact]
+        public void GenerateSql_AddsTwoParameters()
+        {
+            testObject.GenerateSql(context.Object, "Foo");
+
+            context.Verify(x => x.RegisterParameter(ItIs.Field(LemonStandOrderFields.LemonStandOrderID), "Foo"));
+        }
+
+        [Fact]
+        public void GenerateSql_ReturnsSearchSql()
+        {
+            context.SetupSequence(x => x.RegisterParameter(It.IsAny<EntityField2>(), AnyObject))
+                .Returns("p1")
+                .Returns("p2");
+
+            var result = testObject.GenerateSql(context.Object, "Foo");
+
+            Assert.Contains("SELECT OrderId FROM [LemonStandOrder] WHERE LemonStandOrderID LIKE p1", result);
+            Assert.Contains("SELECT OrderId FROM [LemonStandOrderSearch] WHERE LemonStandOrderID LIKE p1", result);
+        }
+
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+    }
+}
