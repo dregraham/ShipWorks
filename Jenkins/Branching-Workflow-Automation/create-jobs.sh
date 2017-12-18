c70a2988ab680a50eca0d14f79652d7262588fe0
Andrew Benz <a.benz@shipworks.com>
2017-12-18 16:36:25 -0600

    Get Jenkins crumb for CSRF protection

--- Jenkins/Branching-Workflow-Automation/create-jobs.sh
+++ Jenkins/Branching-Workflow-Automation/create-jobs.sh
@@ -7,7 +7,11 @@ FEATURE_NAME="fedex-recert"
 echo Feature name: $FEATURE_NAME
 
 echo Submitting request to create view $FEATURE_NAME"..."
-curl -d 'name='$FEATURE_NAME'&mode=hudson.model.ListView&json={"name": "'$FEATURE_NAME'", "mode": "hudson.model.ListView"}' http://intdev1201:8080/createView > /dev/null
+CRUMB_RESULT=$(curl -q 'http://intdev1201:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+CRUMB_NAME=$(echo $CRUMB_RESULT | cut -d':' -f1)
+CRUMB_VALUE=$(echo $CRUMB_RESULT | cut -d':' -f2)
+
+curl -d '$CRUMB_NAME=$CRUMB_VALUE&name='$FEATURE_NAME'&mode=hudson.model.ListView&json={"name": "'$FEATURE_NAME'", "mode": "hudson.model.ListView"}' http://intdev1201:8080/createView > /dev/null
 echo $FEATURE_NAME view created
 
 for CFG in $(ls ./Jenkins/Config/*_config.xml); do
@@ -15,14 +19,17 @@ for CFG in $(ls ./Jenkins/Config/*_config.xml); do
 	JOB=${FEATURE_NAME}-$JOB
 	sed -i 's/@@BRANCH_NAME@@/'$BRANCH_NAME'/g' $CFG
 	sed -i 's/@@FEATURE_NAME@@/'$FEATURE_NAME'/g' $CFG
-	
+
 	echo Uploading job configuration for $JOB"..."
 	curl -X POST -H "Content-Type: text/xml" --upload-file $CFG http://intdev1201:8080/createItem?name=$JOB > /dev/null
 	echo $JOB configuration uploaded
-	
+
     # Add Job to View
 	echo Adding $JOB to $FEATURE_NAME view...
-    curl -d '' http://intdev1201:8080/view/$FEATURE_NAME/addJobToView?name=$JOB
+	CRUMB_RESULT=$(curl -q 'http://intdev1201:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+	CRUMB_NAME=$(echo $CRUMB_RESULT | cut -d':' -f1)
+	CRUMB_VALUE=$(echo $CRUMB_RESULT | cut -d':' -f2)
+    curl -d '$CRUMB_NAME=$CRUMB_VALUE' http://intdev1201:8080/view/$FEATURE_NAME/addJobToView?name=$JOB
 	echo $JOB added to $FEATURE_NAME view
 done
 
