71720d36885d2205c0eae39c2e92106ac0523dad
Andrew Benz <a.benz@shipworks.com>
2017-12-19 09:42:11 -0600

    Update scripts to reflect new versions of Jenkins plugins

--- Jenkins/Branching-Workflow-Automation/create-jobs.sh
+++ Jenkins/Branching-Workflow-Automation/create-jobs.sh
@@ -6,11 +6,9 @@
 echo Feature name: $FEATURE_NAME
 
 echo "Submitting request to create view $FEATURE_NAME..."
-CRUMB_RESULT=$(curl -q 'http://intdev1201:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
-CRUMB_NAME=$(echo $CRUMB_RESULT | cut -d':' -f1)
-CRUMB_VALUE=$(echo $CRUMB_RESULT | cut -d':' -f2)
+CRUMB=$(curl -q 'http://intdev1201:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,"=",//crumb)')
 
-curl -d $CRUMB_NAME'='$CRUMB_VALUE'&name='$FEATURE_NAME'&mode=hudson.model.ListView&json={"name": "'$FEATURE_NAME'", "mode": "hudson.model.ListView"}' http://intdev1201:8080/createView > /dev/null
+curl -d $CRUMB'&name='$FEATURE_NAME'&mode=hudson.model.ListView&json={"name": "'$FEATURE_NAME'", "mode": "hudson.model.ListView"}' http://intdev1201:8080/createView > /dev/null
 echo $FEATURE_NAME view created
 
 for CFG in $(ls ./Jenkins/Config/*_config.xml); do
@@ -20,16 +18,14 @@ for CFG in $(ls ./Jenkins/Config/*_config.xml); do
 	sed -i 's/@@FEATURE_NAME@@/'$FEATURE_NAME'/g' $CFG
 
 	echo Uploading job configuration for $JOB"..."
-	CRUMB_RESULT=$(curl -q 'http://intdev1201:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
-	curl -X POST -H "Content-Type: text/xml" -H $CRUMB_RESULT --upload-file $CFG http://intdev1201:8080/createItem?name=$JOB > /dev/null
+	CRUMB=$(curl -q 'http://intdev1201:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+	curl -X POST -H "Content-Type: text/xml" -H $CRUMB --upload-file $CFG http://intdev1201:8080/createItem?name=$JOB > /dev/null
 	echo $JOB configuration uploaded
 
     # Add Job to View
 	echo Adding $JOB to $FEATURE_NAME view...
-	CRUMB_RESULT=$(curl -q 'http://intdev1201:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
-	CRUMB_NAME=$(echo $CRUMB_RESULT | cut -d':' -f1)
-	CRUMB_VALUE=$(echo $CRUMB_RESULT | cut -d':' -f2)
-    curl -d "$CRUMB_NAME=$CRUMB_VALUE" http://intdev1201:8080/view/$FEATURE_NAME/addJobToView?name=$JOB
+	CRUMB=$(curl -q 'http://intdev1201:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,"=",//crumb)')
+    curl -d $CRUMB http://intdev1201:8080/view/$FEATURE_NAME/addJobToView?name=$JOB
 	echo $JOB added to $FEATURE_NAME view
 done
 
