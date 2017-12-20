05672ab515309ab46500fcaa1c877a918c73f189
Tim Hughes <t.hughes@shipworks.com>
2017-12-20 07:34:14 -0600

    TP#18542: Implemented SplitOrderValidator.cs

---
+++ Code/ShipWorks.Tests/Stores/Content/SplitOrder/SplitOrderValidatorTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Autofac.Extras.Moq;
+using Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Content;
+using ShipWorks.Stores.Content.SplitOrder;
+using ShipWorks.Tests.Shared;
+using ShipWorks.Users.Security;
+using Xunit;
+
+namespace ShipWorks.Tests.Stores.Content.SplitOrder
+{
+    public class SplitOrderValidatorTest : IDisposable
+    {
+        private readonly AutoMock mock;
+
+        public SplitOrderValidatorTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+
+            mock.Mock<ISecurityContext>()
+                .Setup(x => x.HasPermission(It.IsAny<PermissionType>(), It.IsAny<long?>()))
+                .Returns(true);
+
+            mock.Mock<IOrderManager>()
+                .Setup(x => x.GetLatestActiveShipment(It.IsAny<long>()))
+                .Returns((ShipmentEntity) null);
+        }
+
+        [Fact]
+        public void Validate_ReturnSuccess_WhenOrderCountIsOne()
+        {
+            var testObject = mock.Create<SplitOrderValidator>();
+            var result = testObject.Validate(new long[] { 1006 });
+            Assert.True(result.Success);
+        }
+
+        [Fact]
+        public void Validate_ReturnFailure_WhenOrderCountNone()
+        {
+            var testObject = mock.Create<SplitOrderValidator>();
+            var result = testObject.Validate(new long[] { });
+            Assert.False(result.Success);
+        }
+
+        [Fact]
+        public void Validate_ReturnFailure_WhenOrderCountIsGreaterThanOne()
+        {
+            var testObject = mock.Create<SplitOrderValidator>();
+            var result = testObject.Validate(new long[] { 1006, 206 });
+            Assert.False(result.Success);
+        }
+
+        [Fact]
+        public void Validate_ReturnFailure_WhenUserDoesNotHavePermission()
+        {
+            mock.Mock<ISecurityContext>()
+                .Setup(x => x.HasPermission(It.IsAny<PermissionType>(), It.IsAny<long?>()))
+                .Returns(false);
+
+            var testObject = mock.Create<SplitOrderValidator>();
+            var result = testObject.Validate(new long[] { 1006 });
+
+            Assert.True(result.Failure);
+        }
+
+        [Fact]
+        public void Validate_ReturnFalse_WhenOrderCanNotBeSplit()
+        {
+            mock.Mock<IOrderManager>()
+                .Setup(x => x.GetLatestActiveShipment(It.IsAny<long>()))
+                .Returns(new ShipmentEntity());
+
+            var testObject = mock.Create<SplitOrderValidator>();
+            var result = testObject.Validate(new long[] { 1006 });
+
+            Assert.False(result.Success);
+        }
+
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+    }
+}
---
+++ Code/ShipWorks/Stores/Content/SplitOrder/SplitOrderValidator.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Interapptive.Shared.Collections;
+using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.Utility;
+using ShipWorks.Data;
+using ShipWorks.Data.Model;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Users.Security;
+
+namespace ShipWorks.Stores.Content.SplitOrder
+{
+    /// <summary>
+    /// Validate the splitting of an order
+    /// </summary>
+    /// <remarks>
+    /// Validation requirements:
+    /// Number of orders is = 1
+    /// User has permissions to modify the store orders
+    /// No processed shipments
+    /// </remarks>
+    [Component]
+    public class SplitOrderValidator : ISplitOrderValidator
+    {
+        private readonly ICombineOrderGateway gateway;
+        private readonly ISecurityContext securityContext;
+        private readonly IOrderManager orderManager;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public SplitOrderValidator(ICombineOrderGateway gateway, ISecurityContext securityContext, IOrderManager orderManager)
+        {
+            this.gateway = gateway;
+            this.securityContext = securityContext;
+            this.orderManager = orderManager;
+        }
+
+        /// <summary>
+        /// Validate the list of orders
+        /// </summary>
+        public Result Validate(IEnumerable<long> orderIDs)
+        {
+            if (!orderIDs.IsCountEqualTo(1))
+            {
+                return Result.FromError("A single order must be selected to combine orders");
+            }
+
+            // All ids must be order ids, not customer
+            if (!orderIDs.All(orderID => EntityUtility.GetEntityType(orderID) == EntityType.OrderEntity))
+            {
+                return Result.FromError("The selected row must be an order");
+            }
+
+            long firstId = orderIDs.First();
+            bool hasPermission = securityContext.HasPermission(PermissionType.OrdersModify, firstId);
+
+            if (!hasPermission)
+            {
+                return Result.FromError("The current user does not have permission to modify orders");
+            }
+
+            ShipmentEntity processedShipment = orderManager.GetLatestActiveShipment(firstId);
+
+            bool canCombine = processedShipment == null;
+
+            return canCombine == false ? 
+                Result.FromError("Selected order cannot be split as it has a processed shipment") : 
+                Result.FromSuccess();
+        }
+    }
+}
