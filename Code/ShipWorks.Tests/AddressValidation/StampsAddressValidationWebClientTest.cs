d3046be571c8b867e39e9736c077b001d4c4f908
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-12-21 14:28:40 -0600

    Fix broken units

--- Code/ShipWorks.Tests/AddressValidation/StampsAddressValidationWebClientTest.cs
+++ Code/ShipWorks.Tests/AddressValidation/StampsAddressValidationWebClientTest.cs
@@ -37,7 +37,8 @@ namespace ShipWorks.Tests.AddressValidation
                 IsCityStateZipOk = true,
                 ResidentialIndicator = ResidentialDeliveryIndicatorType.Yes,
                 MatchedAddress = matchedAddress,
-                Candidates = new[] { candidateOne, candidateTwo }
+                Candidates = new[] { candidateOne, candidateTwo },
+                VerificationLevel = AddressVerificationLevel.Maximum
             };
 
             mock.Mock<IUspsWebClient>()
@@ -47,7 +48,7 @@ namespace ShipWorks.Tests.AddressValidation
             var resultFactory = mock.Mock<IAddressValidationResultFactory>();
 
             var testObject = mock.Create<StampsAddressValidationWebClient>();
-            await testObject.ValidateAddressAsync(new AddressAdapter());
+            await testObject.ValidateAddressAsync(new AddressAdapter() { CountryCode = "US" });
 
             resultFactory.Verify(r => r.CreateAddressValidationResult(matchedAddress, true, validationResult, It.IsAny<int>()));
             resultFactory.Verify(r => r.CreateAddressValidationResult(candidateOne, false, validationResult, It.IsAny<int>()));
@@ -99,7 +100,7 @@ namespace ShipWorks.Tests.AddressValidation
                 residentialIndicator);
 
             var testObject = mock.Create<StampsAddressValidationWebClient>();
-            var result = await testObject.ValidateAddressAsync(new AddressAdapter());
+            var result = await testObject.ValidateAddressAsync(new AddressAdapter() { CountryCode = "US"});
 
             Assert.Equal(expectedAddressType, result.AddressType);
         }
