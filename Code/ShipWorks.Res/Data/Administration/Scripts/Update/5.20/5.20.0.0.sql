cc2f3d83db047bcdd308a66ea49042d9b0b5fd44
Kevin Croke <k.croke@shipworks.com>
2017-12-21 09:18:13 -0600

    Restoring script that didn't make it into the merge.

---
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.20/5.20.0.0.sql
@@ -0,0 +1,10 @@
+﻿PRINT N'Altering [dbo].[ThreeDCartStore]'
+GO
+ALTER TABLE [dbo].[ThreeDCartStore] ADD
+[OrderIDUpgradeFixDate] [datetime] NULL
+GO
+PRINT N'Setting date of OrderIDUpgradeFixDate'
+GO
+UPDATE ThreeDCartStore 
+SET OrderIDUpgradeFixDate = GetUtcDate()
+GO
