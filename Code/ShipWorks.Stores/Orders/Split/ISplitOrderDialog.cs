da884f1d143e318fa9d71a669af599f90f6d601d
Josh Winchester <j.winchester@shipworks.com>
2017-12-22 09:03:59 -0600

    Initial shell of the split order dialog

---
+++ Code/ShipWorks.Stores/Orders/Split/ISplitOrderDialog.cs
@@ -0,0 +1,11 @@
+﻿using Interapptive.Shared.UI;
+
+namespace ShipWorks.Stores.Orders.Split
+{
+    /// <summary>
+    /// Get order split details from user
+    /// </summary>
+    public interface ISplitOrderDialog : IDialog
+    {
+    }
+}
