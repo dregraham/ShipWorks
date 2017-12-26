51ffd92b426e90a89da471d9b564a6d7c7707c3a
Andrew Benz <a.benz@shipworks.com>
2017-12-26 09:42:46 -0600

    TP#18539: Add OrderSplitSuccessViewModel

---
+++ Code/ShipWorks.Stores.Tests/Orders/Split/OrderSplitSuccessViewModelTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using Autofac.Extras.Moq;
+using Interapptive.Shared.UI;
+using Moq;
+using ShipWorks.Stores.Orders.Split;
+using ShipWorks.Tests.Shared;
+using ShipWorks.Users;
+using Xunit;
+
+namespace ShipWorks.Stores.Tests.Orders.Split
+{
+    public class OrderSplitSuccessViewModelTest : IDisposable
+    {
+        private readonly AutoMock mock;
+        private readonly OrderSplitSuccessViewModel testObject;
+
+        public OrderSplitSuccessViewModelTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+            testObject = mock.Create<OrderSplitSuccessViewModel>();
+
+            mock.Mock<ICurrentUserSettings>()
+                .Setup(x => x.ShouldShowNotification(It.IsAny<UserConditionalNotificationType>()))
+                .Returns(true);
+        }
+
+        [Theory]
+        [InlineData(true, 1)]
+        [InlineData(false, 0)]
+        public void ShowSuccessConfirmation_OnlyShowsDialog_WhenRequested(bool showDialog, int expectedInvocations)
+        {
+            var dialog = mock.Mock<IOrderSplitSuccessDialog>().Object;
+
+            mock.Mock<ICurrentUserSettings>()
+                .Setup(x => x.ShouldShowNotification(UserConditionalNotificationType.SplitOrders))
+                .Returns(showDialog);
+
+            testObject.ShowSuccessConfirmation(Enumerable.Empty<string>());
+
+            mock.Mock<IMessageHelper>()
+                .Verify(x => x.ShowDialog(dialog), Times.Exactly(expectedInvocations));
+        }
+
+        [Fact]
+        public void ShowSuccessConfirmation_SetsViewModelProperties_WhenDialogShouldBeShown()
+        {
+            testObject.ShowSuccessConfirmation(new[] { "foo", "bar" });
+
+            Assert.Contains("foo", testObject.SplitOrders);
+            Assert.Contains("bar", testObject.SplitOrders);
+        }
+
+        [Fact]
+        public void ShowSuccessConfirmation_SetsDataContext_OnDialog()
+        {
+            testObject.ShowSuccessConfirmation(Enumerable.Empty<string>());
+
+            mock.Mock<IOrderSplitSuccessDialog>()
+                .VerifySet(x => x.DataContext = testObject);
+        }
+
+        [Theory]
+        [InlineData(true, 1)]
+        [InlineData(false, 0)]
+        public void Dismiss_CallsStopShowingNotification_BasedOnDoNotShowAgainProperty(bool doNotShowAgain, int expectedInvocations)
+        {
+            testObject.DoNotShowAgain = doNotShowAgain;
+
+            testObject.Dismiss.Execute(null);
+
+            mock.Mock<ICurrentUserSettings>()
+                .Verify(x => x.StopShowingNotification(UserConditionalNotificationType.SplitOrders), Times.Exactly(expectedInvocations));
+        }
+
+        [Fact]
+        public void Dismiss_CallsClose_OnDialog()
+        {
+            testObject.Dismiss.Execute(null);
+
+            mock.Mock<IOrderSplitSuccessDialog>().Verify(x => x.Close());
+        }
+
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+    }
+}
---
+++ Code/ShipWorks.Stores/Orders/Split/OrderSplitSuccessViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.CommandWpf;
+using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.UI;
+using ShipWorks.Core.UI;
+using ShipWorks.Users;
+
+namespace ShipWorks.Stores.Orders.Split
+{
+    /// <summary>
+    /// View model for the OrderSplitSuccessDialog
+    /// </summary>
+    [Component]
+    public class OrderSplitSuccessViewModel : IOrderSplitSuccessViewModel, INotifyPropertyChanged
+    {
+        private const UserConditionalNotificationType notificationType = UserConditionalNotificationType.SplitOrders;
+        private readonly IOrderSplitSuccessDialog dialog;
+        private readonly ICurrentUserSettings currentUserSettings;
+        private readonly IMessageHelper messageHelper;
+        private readonly PropertyChangedHandler handler;
+
+        private IEnumerable<string> combinedOrders;
+        private bool doNotShowAgain;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public OrderSplitSuccessViewModel(IOrderSplitSuccessDialog dialog, ICurrentUserSettings currentUserSettings, IMessageHelper messageHelper)
+        {
+            this.messageHelper = messageHelper;
+            this.dialog = dialog;
+            this.currentUserSettings = currentUserSettings;
+
+            handler = new PropertyChangedHandler(this, () => PropertyChanged);
+
+            Dismiss = new RelayCommand(() => DismissAction());
+        }
+
+        /// <summary>
+        /// Notify of changing properties
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Dismiss the dialog
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public ICommand Dismiss { get; }
+
+        /// <summary>
+        /// SplitOrders to display
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public IEnumerable<string> SplitOrders
+        {
+            get { return combinedOrders; }
+            set { handler.Set(nameof(SplitOrders), ref combinedOrders, value); }
+        }
+
+        /// <summary>
+        /// Should the dialog be shown again
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public bool DoNotShowAgain
+        {
+            get { return doNotShowAgain; }
+            set { handler.Set(nameof(DoNotShowAgain), ref doNotShowAgain, value); }
+        }
+
+        /// <summary>
+        /// Show a success dialog after an order has been split
+        /// </summary>
+        public void ShowSuccessConfirmation(IEnumerable<string> orderNumbers)
+        {
+            if (!currentUserSettings.ShouldShowNotification(notificationType))
+            {
+                return;
+            }
+
+            SplitOrders = orderNumbers.ToImmutableList();
+
+            dialog.DataContext = this;
+            messageHelper.ShowDialog(dialog);
+        }
+
+        /// <summary>
+        /// Dismiss the dialog
+        /// </summary>
+        private void DismissAction()
+        {
+            if (DoNotShowAgain)
+            {
+                currentUserSettings.StopShowingNotification(notificationType);
+            }
+
+            dialog.Close();
+        }
+    }
+}
