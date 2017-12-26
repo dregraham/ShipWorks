c42dc35bbe24e85bacaf7700c8d7f7a65a072979
Kevin Croke <k.croke@shipworks.com>
2017-12-26 14:11:40 -0600

    TP#22706: UspsTermsAndConditionsException Test

---
+++ Code/ShipWorks.Tests/Shipping/Carriers/Postal/Usps/UspsTermsAndConditionsExceptionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ShipWorks.Tests.Shared;
+using Autofac.Extras.Moq;
+using Moq;
+using ShipWorks.Shipping.Carriers.Postal.Usps;
+using Xunit;
+
+namespace ShipWorks.Tests.Shipping.Carriers.Postal.Usps
+{
+    public class UspsTermsAndConditionsExceptionTest : IDisposable
+    {
+        private readonly AutoMock mock;
+
+        public UspsTermsAndConditionsExceptionTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+        }
+
+        [Fact]
+        public void Constructor_TermsAndCondionsFieldSet()
+        {
+            var termsAndConditionsMock = mock.CreateMock<IUspsTermsAndConditions>();
+            var testObject = new UspsTermsAndConditionsException("message", termsAndConditionsMock.Object);
+
+            Assert.Equal(termsAndConditionsMock.Object, testObject.TermsAndConditions);
+        }
+
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+    }
+}
