976f92d1c12bf7669b309dda8262c0660251dc22
Tim Hughes <t.hughes@shipworks.com>
2017-12-27 09:24:24 -0600

    TP#18539: Implemented OrderChargeSplitter

---
+++ Code/ShipWorks.Stores.Tests/Orders/Split/OrderChargeSplitterTest.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Orders.Split;
+using Xunit;
+
+namespace ShipWorks.Stores.Tests.Orders.Split
+{
+    public class OrderChargeSplitterTest
+    {
+        [Fact]
+        public void Split_SplitOrder_HasNoCharges_WhenNoChargesRequested()
+        {
+            OrderEntity originalOrder = new OrderEntity()
+            {
+                OrderNumber = 1
+            };
+
+            originalOrder.OrderCharges.Add(new OrderChargeEntity(1) { Amount = 1 });
+            originalOrder.OrderCharges.Add(new OrderChargeEntity(2) { Amount = 3 });
+
+            OrderEntity splitOrder = new OrderEntity()
+            {
+                OrderNumber = 1
+            };
+
+            splitOrder.OrderCharges.Add(new OrderChargeEntity(1) { Amount = 1 });
+            splitOrder.OrderCharges.Add(new OrderChargeEntity(2) { Amount = 3 });
+
+            Dictionary<long, decimal> newOrderChargeAmounts = new Dictionary<long, decimal>();
+
+            OrderChargeSplitter testObject = new OrderChargeSplitter();
+            testObject.Split(newOrderChargeAmounts, originalOrder, splitOrder);
+
+            Assert.Equal(0, splitOrder.OrderCharges.Count);
+            Assert.Equal(2, originalOrder.OrderCharges.Count);
+
+            Assert.Equal(1M, originalOrder.OrderCharges.First(oc => oc.OrderChargeID == 1).Amount);
+            Assert.Equal(3M, originalOrder.OrderCharges.First(oc => oc.OrderChargeID == 2).Amount);
+        }
+
+        [Fact]
+        public void Split_SplitOrder_HasAllCharges_WhenAllChargesRequested()
+        {
+            OrderEntity originalOrder = new OrderEntity()
+            {
+                OrderNumber = 1
+            };
+
+            originalOrder.OrderCharges.Add(new OrderChargeEntity(1) { Amount = 1 });
+            originalOrder.OrderCharges.Add(new OrderChargeEntity(2) { Amount = 3 });
+
+            OrderEntity splitOrder = new OrderEntity()
+            {
+                OrderNumber = 1
+            };
+
+            splitOrder.OrderCharges.Add(new OrderChargeEntity(1) { Amount = 1 });
+            splitOrder.OrderCharges.Add(new OrderChargeEntity(2) { Amount = 3 });
+
+            Dictionary<long, decimal> newOrderChargeAmounts = new Dictionary<long, decimal>();
+            newOrderChargeAmounts.Add(1, 1);
+            newOrderChargeAmounts.Add(2, 3);
+
+            OrderChargeSplitter testObject = new OrderChargeSplitter();
+            testObject.Split(newOrderChargeAmounts, originalOrder, splitOrder);
+
+            Assert.Equal(2, splitOrder.OrderCharges.Count);
+            Assert.Equal(0, originalOrder.OrderCharges.Count);
+
+            Assert.Equal(1M, splitOrder.OrderCharges.First(oc => oc.OrderChargeID == 1).Amount);
+            Assert.Equal(3M, splitOrder.OrderCharges.First(oc => oc.OrderChargeID == 2).Amount);
+        }
+
+        [Fact]
+        public void Split_SplitOrder_HasCorrectChargeAmounts_ForChangedAmounts()
+        {
+            OrderEntity originalOrder = new OrderEntity()
+            {
+                OrderNumber = 1
+            };
+
+            originalOrder.OrderCharges.Add(new OrderChargeEntity(1) { Amount = 1 });
+            originalOrder.OrderCharges.Add(new OrderChargeEntity(2) { Amount = 3 });
+
+            OrderEntity splitOrder = new OrderEntity()
+            {
+                OrderNumber = 1
+            };
+
+            splitOrder.OrderCharges.Add(new OrderChargeEntity(1) { Amount = 1 });
+            splitOrder.OrderCharges.Add(new OrderChargeEntity(2) { Amount = 3 });
+
+            Dictionary<long, decimal> newOrderChargeAmounts = new Dictionary<long, decimal>();
+            newOrderChargeAmounts.Add(1, 0.5M);
+            newOrderChargeAmounts.Add(2, 1.5M);
+
+            OrderChargeSplitter testObject = new OrderChargeSplitter();
+            testObject.Split(newOrderChargeAmounts, originalOrder, splitOrder);
+
+            Assert.Equal(2, splitOrder.OrderCharges.Count);
+            Assert.Equal(2, originalOrder.OrderCharges.Count);
+
+            Assert.Equal(0.5M, splitOrder.OrderCharges.First(oc => oc.OrderChargeID == 1).Amount);
+            Assert.Equal(1.5M, splitOrder.OrderCharges.First(oc => oc.OrderChargeID == 2).Amount);
+
+            Assert.Equal(0.5M, originalOrder.OrderCharges.First(oc => oc.OrderChargeID == 1).Amount);
+            Assert.Equal(1.5M, originalOrder.OrderCharges.First(oc => oc.OrderChargeID == 2).Amount);
+        }
+
+        [Fact]
+        public void Split_SplitOrder_HasCorrectChargeCounts_ForZeroAmounts()
+        {
+            OrderEntity originalOrder = new OrderEntity()
+            {
+                OrderNumber = 1
+            };
+
+            originalOrder.OrderCharges.Add(new OrderChargeEntity(1) { Amount = 1 });
+            originalOrder.OrderCharges.Add(new OrderChargeEntity(2) { Amount = 3 });
+
+            OrderEntity splitOrder = new OrderEntity()
+            {
+                OrderNumber = 1
+            };
+
+            splitOrder.OrderCharges.Add(new OrderChargeEntity(1) { Amount = 1 });
+            splitOrder.OrderCharges.Add(new OrderChargeEntity(2) { Amount = 3 });
+
+            Dictionary<long, decimal> newOrderChargeAmounts = new Dictionary<long, decimal>();
+            newOrderChargeAmounts.Add(1, 0M);
+            newOrderChargeAmounts.Add(2, 0M);
+
+            OrderChargeSplitter testObject = new OrderChargeSplitter();
+            testObject.Split(newOrderChargeAmounts, originalOrder, splitOrder);
+
+            Assert.Equal(0, splitOrder.OrderCharges.Count);
+            Assert.Equal(2, originalOrder.OrderCharges.Count);
+            
+            Assert.Equal(1M, originalOrder.OrderCharges.First(oc => oc.OrderChargeID == 1).Amount);
+            Assert.Equal(3M, originalOrder.OrderCharges.First(oc => oc.OrderChargeID == 2).Amount);
+        }
+    }
+}
---
+++ Code/ShipWorks.Stores/Orders/Split/OrderChargeSplitter.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Stores.Orders.Split
+{
+    /// <summary>
+    /// Class for splitting order charges between two orders
+    /// </summary>
+    [Component]
+    public class OrderChargeSplitter : IOrderChargeSplitter
+    {
+        /// <summary>
+        /// Split the charges
+        /// </summary>
+        /// <param name="newOrderChargeAmounts">Dictionary<OrderChargeID, Amount></OrderChargeID></param>
+        /// <param name="originalOrder">The source order that is being split in two</param>
+        /// <param name="splitOrder">The new order created from originalOrder</param>
+        public void Split(IDictionary<long, decimal> newOrderChargeAmounts, OrderEntity originalOrder, OrderEntity splitOrder)
+        {
+            // Go through each new order charges 
+            foreach (KeyValuePair<long, decimal> orderChargesDefinition in newOrderChargeAmounts)
+            {
+                // Find the new order charge based on OrderChargeID
+                OrderChargeEntity newOrderChargeEntity = splitOrder.OrderCharges.First(oi => oi.OrderChargeID == orderChargesDefinition.Key);
+
+                // Find the original charge based on OrderChargeID
+                OrderChargeEntity originalOrderChargeEntity = originalOrder.OrderCharges.First(oi => oi.OrderChargeID == orderChargesDefinition.Key);
+
+                // Update the new Amount to be the split order defined Amount
+                newOrderChargeEntity.Amount = orderChargesDefinition.Value;
+
+                // Update the original charge Amount to be the difference of the two
+                originalOrderChargeEntity.Amount = originalOrderChargeEntity.Amount - newOrderChargeEntity.Amount;
+            }
+
+            // Remove any charges from the split order that were not in the newOrderChargeAmounts
+            // or had 0 Amounts
+            IList<long> notPresentOrderChargeIDs = splitOrder.OrderCharges
+                .Select(oi => oi.OrderChargeID)
+                .Except(newOrderChargeAmounts.Keys)
+                .Concat(splitOrder.OrderCharges.Where(oc => oc.Amount == 0).Select(oc => oc.OrderChargeID))
+                .Distinct()
+                .ToList();
+
+            foreach (long orderChargeID in notPresentOrderChargeIDs)
+            {
+                splitOrder.OrderCharges.Remove(splitOrder.OrderCharges.First(oi => oi.OrderChargeID == orderChargeID));
+            }
+
+            // Remove any charges from the original order that have an Amount of 0
+            IList<long> originalChargesToRemove = originalOrder.OrderCharges
+                .Where(oc => oc.Amount == 0)
+                .Select(oc => oc.OrderChargeID)
+                .ToList();
+
+            foreach (long orderChargeID in originalChargesToRemove)
+            {
+                originalOrder.OrderCharges.Remove(originalOrder.OrderCharges.First(oi => oi.OrderChargeID == orderChargeID));
+            }
+        }
+    }
+}
