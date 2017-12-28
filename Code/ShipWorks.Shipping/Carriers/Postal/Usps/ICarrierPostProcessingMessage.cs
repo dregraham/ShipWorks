80358c07d34c5a74dca3715ae7ed104a7a834e9a
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2017-12-28 10:53:15 -0600

    TP#22710 refactor ShipmentProcessor to use ICarrierPostProcessingMessage to display messages

---
+++ Code/ShipWorks.Shipping/Carriers/Postal/Usps/ICarrierPostProcessingMessage.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ShipWorks.Data.Model.EntityInterfaces;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Usps
+{
+    public interface ICarrierPostProcessingMessage
+    {
+        void Show(IEnumerable<IShipmentEntity> processedShipments);
+    }
+}
---
+++ Code/ShipWorks.Shipping/Carriers/Postal/Usps/UspsPostProcessingMessage.cs
@@ -0,0 +1,59 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Data.Model.EntityInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Usps
+{
+    /// <summary>
+    /// Show message for Usps Shipments after processing
+    /// </summary>
+    [KeyedComponent(typeof(ICarrierPostProcessingMessage), ShipmentTypeCode.Usps)]
+    public class UspsPostProcessingMessage : ICarrierPostProcessingMessage
+    {
+        private readonly IGlobalPostLabelNotification globalPostNotification;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public UspsPostProcessingMessage(IGlobalPostLabelNotification globalPostNotification)
+        {
+            this.globalPostNotification = globalPostNotification;
+        }
+
+        /// <summary>
+        /// Show messages that apply to the given shipment
+        /// </summary>
+        public void Show(IEnumerable<IShipmentEntity> processedShipments)
+        {
+            bool hasGlobalPost = processedShipments.Any(IsProcessedGlobalPost);
+
+            if (hasGlobalPost)
+            {
+                if (globalPostNotification.AppliesToCurrentUser())
+                {
+                    globalPostNotification.Show();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the shipment is a Processed GlobalPost shipment.
+        /// </summary>
+        private static bool IsProcessedGlobalPost(IShipmentEntity shipment)
+        {
+            if (shipment.Processed &&
+                shipment.ShipmentType == (int)ShipmentTypeCode.Usps &&
+                shipment.Postal != null)
+            {
+                // We have a processed USPS shipment. Now check for the GlobalPost service type
+                return PostalUtility.IsGlobalPost((PostalServiceType)shipment.Postal.Service);
+            }
+
+            return false;
+        }
+    }
+}
