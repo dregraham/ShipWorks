12fd76e6b2d0e9d3f98fcd6218b4539793048719
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-01-02 08:41:24 -0600

    TP#22710 add comments

--- Code/ShipWorks.Shipping/Services/ICarrierPostProcessingMessage.cs
+++ Code/ShipWorks.Shipping/Services/ICarrierPostProcessingMessage.cs
@@ -3,8 +3,14 @@ using ShipWorks.Data.Model.EntityInterfaces;
 
 namespace ShipWorks.Shipping.Services
 {
+    /// <summary>
+    /// Interface that represents a post processing message
+    /// </summary>
     public interface ICarrierPostProcessingMessage
     {
+        /// <summary>
+        /// Show a message for the given shipments
+        /// </summary>
         void Show(IEnumerable<IShipmentEntity> processedShipments);
     }
 }
