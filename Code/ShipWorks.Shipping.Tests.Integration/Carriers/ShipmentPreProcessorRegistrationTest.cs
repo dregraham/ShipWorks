ac8abfb957fc6c27636868ee2ead9b493c0d01cb
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-01-03 13:37:16 -0600

    TP#22732 fix integration test

--- Code/ShipWorks.Shipping.Tests.Integration/Carriers/ShipmentPreProcessorRegistrationTest.cs
+++ Code/ShipWorks.Shipping.Tests.Integration/Carriers/ShipmentPreProcessorRegistrationTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Autofac;
 using ShipWorks.Shipping.Carriers;
@@ -22,26 +23,34 @@ namespace ShipWorks.Shipping.Tests.Integration.Carriers
         }
 
         [Fact]
-        public void EnsureBestRateUsesBestRateShipmentPreProcessor()
+        public void EnsureCarriersThatDoNotUseDefaultPreProcessorAreRegistered()
         {
-            IShipmentPreProcessor service = container.ResolveKeyed<IShipmentPreProcessor>(ShipmentTypeCode.BestRate);
-            Assert.IsType<BestRateShipmentPreProcessor>(service);
-        }
-
-        [Fact]
-        public void EnsureUspsUsesUspsShipmentPreProcessor()
-        {
-            IShipmentPreProcessor service = container.ResolveKeyed<IShipmentPreProcessor>(ShipmentTypeCode.Usps);
-            Assert.IsType<PostalShipmentPreProcessor>(service);
+            foreach (KeyValuePair<ShipmentTypeCode, Type> carrier in CarriersThatDoNotUseDefaultPreProcessor)
+            {
+                IShipmentPreProcessor service = container.ResolveKeyed<IShipmentPreProcessor>(carrier.Key);
+                Assert.Equal(carrier.Value, service.GetType());
+            }
         }
 
         [Fact]
         public void EnsureAllCarriersExceptBestRateUseGenericShipmentPreProcessor()
         {
-            foreach (var value in Enum.GetValues(typeof(ShipmentTypeCode)).OfType<ShipmentTypeCode>().Where(x => x != ShipmentTypeCode.BestRate && x != ShipmentTypeCode.Usps))
+            foreach (var value in Enum.GetValues(typeof(ShipmentTypeCode)).OfType<ShipmentTypeCode>().Where(x => !CarriersThatDoNotUseDefaultPreProcessor.ContainsKey(x)))
             {
                 IShipmentPreProcessor service = container.ResolveKeyed<IShipmentPreProcessor>(value);
                 Assert.IsType<DefaultShipmentPreProcessor>(service);
+            }
+        }
+
+        private static IDictionary<ShipmentTypeCode, Type> CarriersThatDoNotUseDefaultPreProcessor
+        {
+            get
+            {
+                var dict = new Dictionary<ShipmentTypeCode, Type>();
+                dict.Add(ShipmentTypeCode.Endicia, typeof(PostalShipmentPreProcessor));
+                dict.Add(ShipmentTypeCode.Usps, typeof(PostalShipmentPreProcessor));
+                dict.Add(ShipmentTypeCode.BestRate, typeof(BestRateShipmentPreProcessor));
+                return dict;
             }
         }
 
