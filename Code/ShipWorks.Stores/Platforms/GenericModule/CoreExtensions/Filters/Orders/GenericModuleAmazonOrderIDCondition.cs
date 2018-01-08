1da09914aff43ce96e745c4821f33734483c16ae
Chris Hicks <c.hicks@shipworks.com>
2018-01-08 15:53:20 -0600

    TP#20564: Create AmazonOrderID filter

---
+++ Code/ShipWorks.Stores/Platforms/GenericModule/CoreExtensions/Filters/Orders/GenericModuleAmazonOrderIDCondition.cs
@@ -0,0 +1,31 @@
+﻿using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+using ShipWorks.Filters.Content.SqlGeneration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Stores.Platforms.GenericModule.CoreExtensions.Filters.Orders
+{
+    /// <summary>
+    /// Filter condition for Is Prime
+    /// </summary>
+    [ConditionElement("Generic Module Amazon Order ID", "GenericModule.AmazonOrderID")]
+    [ConditionStoreType(StoreTypeCode.GenericModule)]
+    public class GenericModuleAmazonOrderIDCondition : StringCondition
+    {
+        /// <summary>
+        /// Generate the SQL for the condition
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            using (SqlGenerationScope scope = context.PushScope(OrderFields.OrderID, GenericModuleOrderFields.OrderID, SqlGenerationScopeType.AnyChild))
+            {
+                return scope.Adorn(base.GenerateSql(context.GetColumnReference(GenericModuleOrderFields.AmazonOrderID), context));
+            }
+        }
+    }
+}
