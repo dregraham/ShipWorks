294c6031855f376d883ec70d0654f859f3bc565f
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-01-08 15:35:19 -0600

    TP#20564 create GenericModuleIsPrimeCondition

---
+++ Code/ShipWorks.Stores/Platforms/GenericModule/CoreExtensions/Filters/Orders/GenericModuleIsPrimeCondition.cs
@@ -0,0 +1,35 @@
+﻿using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content;
+using ShipWorks.Filters.Content.Conditions;
+using ShipWorks.Filters.Content.SqlGeneration;
+using ShipWorks.Stores.Platforms.GenericModule.Enums;
+
+namespace ShipWorks.Stores.Platforms.GenericModule.CoreExtensions.Filters
+{
+    /// <summary>
+    /// Filter condition for Is Prime
+    /// </summary>
+    [ConditionElement("Generic Module Is Amazon Prime", "GenericModule.IsPrime")]
+    [ConditionStoreType(StoreTypeCode.GenericModule)]
+    public class GenericModuleIsPrimeCondition : EnumCondition<GenericModuleIsAmazonPrime>
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public GenericModuleIsPrimeCondition()
+        {
+            Value = GenericModuleIsAmazonPrime.Yes;
+        }
+
+        /// <summary>
+        /// Generate the SQL for the condition
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            using (SqlGenerationScope scope = context.PushScope(OrderFields.OrderID, GenericModuleOrderFields.OrderID, SqlGenerationScopeType.AnyChild))
+            {
+                return scope.Adorn(base.GenerateSql(context.GetColumnReference(GenericModuleOrderFields.IsPrime), context));
+            }
+        }
+    }
+}
