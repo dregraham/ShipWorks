2b32bea6515baab862de3da1dd9a63bc7bb3d807
Tim Hughes <t.hughes@shipworks.com>
2018-01-12 13:36:00 -0600

    TP#22742: Added CommerceInterface combine/split tests.

---
+++ Code/ShipWorks.Stores/Platforms/CommerceInterface/Content/CommerceInterfaceSplitOrderAction.cs
@@ -0,0 +1,25 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Orders.Split.Actions;
+
+namespace ShipWorks.Stores.Platforms.CommerceInterface.Content
+{
+    /// <summary>
+    /// Split action that is specific to CommerceInterface
+    /// </summary>
+    [KeyedComponent(typeof(IStoreSpecificSplitOrderAction), StoreTypeCode.CommerceInterface)]
+    public class CommerceInterfaceSplitOrderAction : IStoreSpecificSplitOrderAction
+    {
+        /// <summary>
+        /// Perform the platform specific action
+        /// </summary>
+        public void Perform(long originalOrderID, OrderEntity splitOrder)
+        {
+            CommerceInterfaceOrderEntity order = (CommerceInterfaceOrderEntity) splitOrder;
+            CommerceInterfaceOrderSearchEntity orderSearchEntity = order.CommerceInterfaceOrderSearch.AddNew();
+
+            orderSearchEntity.CommerceInterfaceOrderNumber = order.CommerceInterfaceOrderNumber;
+            orderSearchEntity.OriginalOrderID = originalOrderID;
+        }
+    }
+}
