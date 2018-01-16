30b376e31ac65523c7a9dcfe32387279f30aba6b
Andrew Benz <a.benz@shipworks.com>
2018-01-16 10:35:18 -0600

    TP#22742: Add Jet combine/split tests

---
+++ Code/ShipWorks.Stores/Platforms/Jet/Content/JetSplitOrderAction.cs
@@ -0,0 +1,25 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Orders.Split.Actions;
+
+namespace ShipWorks.Stores.Platforms.Jet.Content
+{
+    /// <summary>
+    /// Split action that is specific to Jet
+    /// </summary>
+    [KeyedComponent(typeof(IStoreSpecificSplitOrderAction), StoreTypeCode.Jet)]
+    public class JetSplitOrderAction : IStoreSpecificSplitOrderAction
+    {
+        /// <summary>
+        /// Perform the platform specific action
+        /// </summary>
+        public void Perform(long originalOrderID, OrderEntity splitOrder)
+        {
+            JetOrderEntity order = (JetOrderEntity) splitOrder;
+            JetOrderSearchEntity orderSearchEntity = order.JetOrderSearch.AddNew();
+
+            orderSearchEntity.MerchantOrderID = order.MerchantOrderId;
+            orderSearchEntity.OriginalOrderID = originalOrderID;
+        }
+    }
+}
