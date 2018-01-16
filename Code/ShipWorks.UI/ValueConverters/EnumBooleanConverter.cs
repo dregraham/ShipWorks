32960c015151d3a22209d61c0138d3925efcb5fd
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-01-16 13:26:14 -0600

    TP#23037 add on demand to OdbcImportMapSettingsControl

---
+++ Code/ShipWorks.UI/ValueConverters/EnumBooleanConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ShipWorks.UI.ValueConverters
+{
+    /// <summary>
+    /// Convert from the string parameter to the enum
+    /// </summary>
+    public class EnumBooleanConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert
+        /// </summary>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string parameterString = parameter as string;
+            if (parameterString == null ||
+                Enum.IsDefined(value.GetType(), value) == false)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+                
+            object parameterValue = Enum.Parse(value.GetType(), parameterString);
+
+            return parameterValue.Equals(value);
+        }
+
+        /// <summary>
+        /// Convert back
+        /// </summary>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string parameterString = parameter as string;
+            if (parameterString == null)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+                
+            return Enum.Parse(targetType, parameterString);
+        }
+    }
+}
