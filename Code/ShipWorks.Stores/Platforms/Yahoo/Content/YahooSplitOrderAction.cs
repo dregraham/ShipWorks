666d590cedb1d4c1b867bba3504a52763e63e3ee
Andrew Benz <a.benz@shipworks.com>
2018-01-16 13:01:21 -0600

    TP#22742: Add Yahoo combine/split tests

---
+++ Code/ShipWorks.Stores/Platforms/Yahoo/Content/YahooSplitOrderAction.cs
@@ -0,0 +1,25 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Orders.Split.Actions;
+
+namespace ShipWorks.Stores.Platforms.Yahoo.Content
+{
+    /// <summary>
+    /// Split action that is specific to Yahoo
+    /// </summary>
+    [KeyedComponent(typeof(IStoreSpecificSplitOrderAction), StoreTypeCode.Yahoo)]
+    public class YahooSplitOrderAction : IStoreSpecificSplitOrderAction
+    {
+        /// <summary>
+        /// Perform the platform specific action
+        /// </summary>
+        public void Perform(long originalOrderID, OrderEntity splitOrder)
+        {
+            YahooOrderEntity order = (YahooOrderEntity) splitOrder;
+            YahooOrderSearchEntity orderSearchEntity = order.YahooOrderSearch.AddNew();
+
+            orderSearchEntity.YahooOrderID = order.YahooOrderID;
+            orderSearchEntity.OriginalOrderID = originalOrderID;
+        }
+    }
+}
