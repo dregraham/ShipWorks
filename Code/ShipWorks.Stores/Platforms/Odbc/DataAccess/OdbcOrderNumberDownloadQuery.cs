aa846d9cc9e062946c14685b81d799f4c7cbb6af
Kevin Croke <k.croke@shipworks.com>
2018-01-18 09:08:40 -0600

    TP#23038: Add OdbcOrderNumberDownloadQuery

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/DataAccess/OdbcOrderNumberDownloadQuery.cs
@@ -0,0 +1,84 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Linq;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Stores.Platforms.Odbc.DataSource;
+using ShipWorks.Stores.Platforms.Odbc.Mapping;
+
+namespace ShipWorks.Stores.Platforms.Odbc.DataAccess
+{
+    /// <summary>
+    /// OdbcOrderNumberDownloadQuery
+    /// </summary>
+    public class OdbcOrderNumberDownloadQuery : IOdbcQuery
+    {
+        private readonly IOdbcQuery downloadQuery;
+        private readonly string orderNumber;
+        private readonly IShipWorksDbProviderFactory dbProviderFactory;
+        private readonly IOdbcDataSource dataSource;
+        private readonly string orderNumberColumnName;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public OdbcOrderNumberDownloadQuery(IOdbcQuery downloadQuery,
+            string orderNumber,
+            IOdbcFieldMap fieldMap,
+            IShipWorksDbProviderFactory dbProviderFactory,
+            IOdbcDataSource dataSource)
+        {
+            this.downloadQuery = downloadQuery;
+            this.orderNumber = orderNumber;
+            this.dbProviderFactory = dbProviderFactory;
+            this.dataSource = dataSource;
+            orderNumberColumnName = fieldMap.FindEntriesBy(OrderFields.OrderNumberComplete).FirstOrDefault()?.ExternalField.Column.Name;
+        }
+
+        /// <summary>
+        /// Generates the Sql for the query
+        /// </summary>
+        /// <exception cref="ShipWorksOdbcException">The Connection string is not valid</exception>
+        public string GenerateSql()
+        {
+            // If the orderNumber column is not mapped we cannot generate the query
+            if (string.IsNullOrWhiteSpace(orderNumberColumnName))
+            {
+                throw new ShipWorksOdbcException("The OrderNumber column must be mapped to download orders On Demand.");
+            }
+
+            string columnNameInQuotes = WrapColumnInQuoteIdentifier(orderNumberColumnName);
+
+            // Generate the query
+            return $@"SELECT sub.* FROM({downloadQuery.GenerateSql()}) sub WHERE {columnNameInQuotes} = ?";
+        }
+
+        /// <summary>
+        /// Adds Command Text to the given sql command
+        /// </summary>
+        /// <exception cref="ShipWorksOdbcException">The Connection string is not valid</exception>
+        public void ConfigureCommand(IShipWorksOdbcCommand command)
+        {
+            command.ChangeCommandText(GenerateSql());
+            string columnNameInQuotes = WrapColumnInQuoteIdentifier(orderNumberColumnName);
+
+            OdbcParameter param = new OdbcParameter(columnNameInQuotes, OdbcType.NVarChar, 50, ParameterDirection.Input, false, 1, 3, columnNameInQuotes, DataRowVersion.Current, orderNumber);
+            command.AddParameter(param);
+        }
+
+        /// <summary>
+        /// Wraps the given column string in the data sources quoted identifier
+        /// </summary>
+        /// <exception cref="ShipWorksOdbcException">The Connection string is not valid</exception>
+        private string WrapColumnInQuoteIdentifier(string column)
+        {
+            using (DbConnection connection = dataSource.CreateConnection())
+            using (IShipWorksOdbcDataAdapter adapter = dbProviderFactory.CreateShipWorksOdbcDataAdapter(string.Empty, connection))
+            using (IShipWorksOdbcCommandBuilder cmdBuilder = dbProviderFactory.CreateShipWorksOdbcCommandBuilder(adapter))
+            {
+                connection.Open();
+                return cmdBuilder.QuoteIdentifier(column);
+            }
+        }
+    }
+}
