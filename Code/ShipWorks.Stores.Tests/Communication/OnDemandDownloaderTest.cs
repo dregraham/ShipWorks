ff5e7987bfca7a98b60ce44b7f3697639fd99bad
Chris Hicks <c.hicks@shipworks.com>
2018-01-18 12:53:02 -0600

    TP#23038: Implement OnDemandDownloader

---
+++ Code/ShipWorks.Stores.Tests/Communication/OnDemandDownloaderTest.cs
@@ -0,0 +1,47 @@
+﻿using Autofac.Extras.Moq;
+using Interapptive.Shared.UI;
+using Interapptive.Shared.Utility;
+using ShipWorks.Stores.Communication;
+using ShipWorks.Tests.Shared;
+using System;
+using Xunit;
+
+namespace ShipWorks.Stores.Tests.Communication
+{
+    public class OnDemandDownloaderTest : IDisposable
+    {
+        private AutoMock mock;
+        private OnDemandDownloader testObject;
+
+        public OnDemandDownloaderTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+            testObject = mock.Create<OnDemandDownloader>();
+        }
+
+        [Fact]
+        public void Download_DelegatesToDownloadManager()
+        {
+            var downloadManager = mock.Mock<IDownloadManager>();
+
+            testObject.Download("1");
+
+            downloadManager.Verify(m => m.Download("1"));
+        }
+
+        [Fact]
+        public void Download_DelegatesToMessageHelper_WhenDownloadFails()
+        {
+            mock.Mock<IDownloadManager>().Setup(m => m.Download("1")).Returns(Result.FromError("Error"));
+            var messageHelper = mock.Mock<IMessageHelper>();
+            testObject.Download("1");
+
+            messageHelper.Verify(m => m.ShowError("Error"));
+        }
+        
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+    }
+}
---
+++ Code/ShipWorks.Stores/Communication/OnDemandDownloader.cs
@@ -0,0 +1,38 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.UI;
+using Interapptive.Shared.Utility;
+
+namespace ShipWorks.Stores.Communication
+{
+    /// <summary>
+    /// Downloader for downloading a specific order
+    /// </summary>
+    [Component]
+    public class OnDemandDownloader : IOnDemandDownloader
+    {
+        private readonly IMessageHelper messageHelper;
+        private readonly IDownloadManager downloadManager;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public OnDemandDownloader(IMessageHelper messageHelper, IDownloadManager downloadManager)
+        {
+            this.messageHelper = messageHelper;
+            this.downloadManager = downloadManager;
+        }
+
+        /// <summary>
+        /// Initiate download using given orderNumber
+        /// </summary>
+        public void Download(string orderNumber)
+        {
+            IResult result = downloadManager.Download(orderNumber);
+
+            if (result.Failure)
+            {
+                messageHelper.ShowError(result.Message);
+            }
+        }
+    }
+}
