081c3d5a2efcfd87c0af235f7a7b4ca7957cf339
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-01-18 08:44:06 -0600

    TP#23038 create IDownloadManager

---
+++ Code/ShipWorks/Stores/Communication/IDownloadManager.cs
@@ -0,0 +1,15 @@
+﻿using Interapptive.Shared.Utility;
+
+namespace ShipWorks.Stores.Communication
+{
+    /// <summary>
+    /// interface that represents the DownloadManager
+    /// </summary>
+    public  interface IDownloadManager
+    {
+        /// <summary>
+        /// Download the order number from all stores
+        /// </summary>
+        IResult Download(string orderNumber);
+    }
+}
