cd9e5ece5370a222d93112a373d23bbbde9db144
Chris Hicks <c.hicks@shipworks.com>
2018-01-18 09:15:11 -0600

    TP#23038: Create IOnDemandDownloader

---
+++ Code/ShipWorks/Stores/Communication/IOnDemandDownloader.cs
@@ -0,0 +1,13 @@
+﻿namespace ShipWorks.Stores.Communication
+{
+    /// <summary>
+    /// Download
+    /// </summary>
+    public interface IOnDemandDownloader
+    {
+        /// <summary>
+        /// Download using an order number
+        /// </summary>
+        void Download(string orderNumber);
+    }
+}
