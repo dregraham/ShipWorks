18eb755c0e6613e915104479d02730addbcb29b6
Kevin Croke <k.croke@shipworks.com>
2018-01-19 09:57:50 -0600

    TP#23038: Unit tests to validate the download factories return Task.CompletedTask

--- Code/ShipWorks.Stores.Tests/Platforms/Magento/MagentoDownloaderFactoryTest.cs
+++ Code/ShipWorks.Stores.Tests/Platforms/Magento/MagentoDownloaderFactoryTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data.Common;
 using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
 using Autofac;
 using Autofac.Extras.Moq;
 using Interapptive.Shared.Threading;
@@ -59,6 +60,19 @@ namespace ShipWorks.Stores.Tests.Platforms.Magento
             testObject.Download(progress, 1, dbConnection);
             mock.Mock<IMagentoTwoRestDownloader>()
                 .Verify(x => x.Download(progress, 1, dbConnection));
+        }
+
+        [Fact]
+        public void DownloadWithOrderNumber_ReturnsCompletedTask()
+        {
+            StoreEntity store = new MagentoStoreEntity();
+            var testObject = mock.Create<MagentoDownloaderFactory>(TypedParameter.From(store));
+
+            var dbConnection = mock.Mock<DbConnection>().Object;
+
+            var task = testObject.Download("blah", 1, dbConnection);
+
+            Assert.Equal(Task.CompletedTask, task);
         }
 
         [SuppressMessage("SonarLint", "S1481: Unused local variables should be removed",
