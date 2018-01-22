f7faf4a48046c5e167a21be41ceb067937f4d010
Kevin Croke <k.croke@shipworks.com>
2018-01-22 08:30:59 -0600

    TP#23038: add [component] attribute to factory

--- Code/ShipWorks.Stores/Communication/OnDemandDownloaderFactory.cs
+++ Code/ShipWorks.Stores/Communication/OnDemandDownloaderFactory.cs
@@ -1,4 +1,5 @@
-﻿using Interapptive.Shared.UI;
+﻿using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.UI;
 using ShipWorks.Filters.Search;
 
 namespace ShipWorks.Stores.Communication
@@ -6,6 +7,7 @@ namespace ShipWorks.Stores.Communication
     /// <summary>
     /// Factory for creating OnDemandDownloaders
     /// </summary>
+    [Component]
     public class OnDemandDownloaderFactory : IOnDemandDownloaderFactory
     {
         private readonly IMessageHelper messageHelper;
