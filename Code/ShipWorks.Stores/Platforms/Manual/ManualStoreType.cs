40a13114b67df1999ef5927a60168931b7198293
Josh Winchester <j.winchester@shipworks.com>
2018-02-06 10:10:44 -0600

    Add ManualStoreType

---
+++ Code/ShipWorks.Stores/Platforms/Manual/ManualStoreType.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac.Features.Indexed;
+using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Data.Model.EntityInterfaces;
+using ShipWorks.Stores.Communication;
+using ShipWorks.Stores.Content;
+using ShipWorks.Stores.Management;
+
+namespace ShipWorks.Stores.Platforms.Manual
+{
+    /// <summary>
+    /// Store type for Manual
+    /// </summary>
+    [KeyedComponent(typeof(StoreType), StoreTypeCode.Manual, ExternallyOwned = false)]
+    public class ManualStoreType : StoreType
+    {
+        private readonly StoreEntity manualStore;
+
+        /// <summary>
+        /// Initializes a new instance of the ManualStoreType class. 
+        /// </summary>
+        public ManualStoreType(StoreEntity store) : base(store)
+        {
+            manualStore = store;
+        }
+
+        /// <summary>
+        /// The type code of the store
+        /// </summary>
+        public override StoreTypeCode TypeCode => StoreTypeCode.Manual;
+
+        /// <summary>
+        /// Creates an instance of StoreEntity 
+        /// </summary>
+        public override StoreEntity CreateStoreInstance()
+        {
+            StoreEntity store = new StoreEntity();
+
+            InitializeStoreDefaults(store);
+
+            return store;
+        }
+
+        /// <summary>
+        /// Get the store-specific OrderIdentifier that can be used to identify the specified order.
+        /// </summary>
+        public override OrderIdentifier CreateOrderIdentifier(IOrderEntity order)
+        {
+            return null;
+        }
+
+        protected override string InternalLicenseIdentifier => manualStore.License;
+    }
+}
