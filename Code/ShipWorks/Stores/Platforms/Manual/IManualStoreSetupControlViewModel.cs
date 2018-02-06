17e9564250d2b11f87afba377a970abfde8adfad
Josh Winchester <j.winchester@shipworks.com>
2018-02-06 12:40:24 -0600

    Add IManualStoreSetupControlViewModel

---
+++ Code/ShipWorks/Stores/Platforms/Manual/IManualStoreSetupControlViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Stores.Platforms.Manual
+{
+    /// <summary>
+    /// Interface for ManualStoreControlViewModel
+    /// </summary>
+    public interface IManualStoreSetupControlViewModel
+    {
+        /// <summary>
+        /// Load the store into the view model 
+        /// </summary>
+        void Load(StoreEntity store);
+
+        /// <summary>
+        /// Save the store to the view model 
+        /// </summary>
+        bool Save(StoreEntity store);
+    }
+}
