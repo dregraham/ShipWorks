6dc875426e965fe09b999de9c907c5ed0de18ee4
Josh Winchester <j.winchester@shipworks.com>
2018-02-06 11:42:12 -0600

    Add unit tests for ManualStoreType

---
+++ Code/ShipWorks.Stores.Tests/Platforms/Manual/ManualStoreTypeTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Autofac;
+using Autofac.Extras.Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.Manual;
+using ShipWorks.Stores.Platforms.SparkPay.DTO;
+using ShipWorks.Tests.Shared;
+using Xunit;
+
+namespace ShipWorks.Stores.Tests.Platforms.Manual
+{
+    public class ManualStoreTypeTest : IDisposable
+    {
+        private readonly AutoMock mock;
+        private readonly ManualStoreType testObject;
+
+        public ManualStoreTypeTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+
+            testObject = mock.Create<ManualStoreType>(new TypedParameter(typeof(StoreEntity),
+                new StoreEntity() { StoreTypeCode = StoreTypeCode.Manual }));
+        }
+
+        [Fact]
+        public void TypeCode_IsManual()
+        {
+           var typeCode = testObject.TypeCode;
+ 
+           Assert.Equal(StoreTypeCode.Manual, typeCode);
+        }
+
+        [Fact]
+        public void CreateStoreInstance_ReturnsStoreEntity()
+        {
+            StoreEntity store = testObject.CreateStoreInstance();
+
+            Assert.IsType<StoreEntity>(store);
+        }
+
+        [Fact]
+        public void CreateStoreInstance_InitializesStoreValues()
+        {
+            StoreEntity store = testObject.CreateStoreInstance();
+
+            Assert.Equal("My Manual Store", store.StoreName);
+        }
+
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+    }
+}
