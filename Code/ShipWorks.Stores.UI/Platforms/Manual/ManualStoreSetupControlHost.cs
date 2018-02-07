07a3ecf38b87a6299f927282595d0b9dfdc74a71
Josh Winchester <j.winchester@shipworks.com>
2018-02-07 13:46:46 -0600

    Add ManualStoreSetupControlHost

---
+++ Code/ShipWorks.Stores.UI/Platforms/Manual/ManualStoreSetupControlHost.cs
@@ -0,0 +1,10 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Stores.Management;
+using ShipWorks.UI.Wizard;
+
+namespace ShipWorks.Stores.UI.Platforms.Manual
+{
+    [KeyedComponent(typeof(WizardPage), StoreTypeCode.Manual, ExternallyOwned = false)]
+    public class ManualStoreSetupControlHost : AddStoreWizardPage {}
+
+}
