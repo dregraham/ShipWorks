66b0d276c1df2f9911cd67e47ca3c60d92a94ea8
Andrew Benz <a.benz@shipworks.com>
2018-02-07 15:58:51 -0600

    TP#23302: Add IsInList value editor to address string editor

--- Code/ShipWorks/Filters/Content/Conditions/StateProvinceCondition.cs
+++ Code/ShipWorks/Filters/Content/Conditions/StateProvinceCondition.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using ShipWorks.Filters.Content.Editors.ValueEditors;
+﻿using ShipWorks.Filters.Content.Editors.ValueEditors;
 
 namespace ShipWorks.Filters.Content.Conditions
 {
     /// <summary>
-    /// Base class for conditions that compare againast a single state\province
+    /// Base class for conditions that compare against a single state\province
     /// </summary>
     public abstract class StateProvinceCondition : StringCondition
     {
--- Code/ShipWorks/Filters/Content/Editors/ValueEditors/BillShipAddressStringValueEditor.Designer.cs
+++ Code/ShipWorks/Filters/Content/Editors/ValueEditors/BillShipAddressStringValueEditor.Designer.cs
@@ -31,6 +31,7 @@ namespace ShipWorks.Filters.Content.Editors.ValueEditors
             this.targetValue = new System.Windows.Forms.TextBox();
             this.addressOperator = new ShipWorks.Filters.Content.Editors.ChoiceLabel();
             this.stringOperator = new ShipWorks.Filters.Content.Editors.ChoiceLabel();
+            this.editButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // targetValue
@@ -46,7 +47,7 @@ namespace ShipWorks.Filters.Content.Editors.ValueEditors
             // 
             this.addressOperator.AutoSize = true;
             this.addressOperator.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.addressOperator.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.addressOperator.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.addressOperator.ForeColor = System.Drawing.Color.Green;
             this.addressOperator.Location = new System.Drawing.Point(3, 6);
             this.addressOperator.Name = "addressOperator";
@@ -58,7 +59,7 @@ namespace ShipWorks.Filters.Content.Editors.ValueEditors
             // 
             this.stringOperator.AutoSize = true;
             this.stringOperator.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.stringOperator.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.stringOperator.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.stringOperator.ForeColor = System.Drawing.Color.Green;
             this.stringOperator.Location = new System.Drawing.Point(102, 6);
             this.stringOperator.Name = "stringOperator";
@@ -66,15 +67,26 @@ namespace ShipWorks.Filters.Content.Editors.ValueEditors
             this.stringOperator.TabIndex = 1;
             this.stringOperator.Text = "String Operator";
             // 
-            // AddressValueEditor
+            // editButton
+            // 
+            this.editButton.Image = global::ShipWorks.Properties.Resources.edit16;
+            this.editButton.Location = new System.Drawing.Point(429, 2);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(24, 23);
+            this.editButton.TabIndex = 4;
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.OnEditButtonClick);
+            // 
+            // BillShipAddressStringValueEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.editButton);
             this.Controls.Add(this.stringOperator);
             this.Controls.Add(this.addressOperator);
             this.Controls.Add(this.targetValue);
-            this.Name = "AddressValueEditor";
-            this.Size = new System.Drawing.Size(436, 26);
+            this.Name = "BillShipAddressStringValueEditor";
+            this.Size = new System.Drawing.Size(493, 26);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,5 +97,6 @@ namespace ShipWorks.Filters.Content.Editors.ValueEditors
         private System.Windows.Forms.TextBox targetValue;
         private ChoiceLabel addressOperator;
         private ChoiceLabel stringOperator;
+        private System.Windows.Forms.Button editButton;
     }
 }
