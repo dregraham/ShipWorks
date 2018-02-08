7a5f4e83bbacdbf72147bdb0b9decfa7c6180e1b
Josh Winchester <j.winchester@shipworks.com>
2018-02-08 13:38:53 -0600

    TP#23264: Remove ManualStoreSetupControlHost due to redundancy

--- Code/ShipWorks.Stores.UI/Platforms/Manual/ManualStoreSetupControlHost.cs
+++
@@ -1,9 +0,0 @@
-﻿using Interapptive.Shared.ComponentRegistration;
-using ShipWorks.Stores.Management;
-using ShipWorks.UI.Wizard;
-
-namespace ShipWorks.Stores.UI.Platforms.Manual
-{
-    [KeyedComponent(typeof(WizardPage), StoreTypeCode.Manual, ExternallyOwned = false)]
-    public class ManualStoreSetupControlHost : AddStoreWizardPage {}
-}
