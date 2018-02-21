bce6ed34c35b81bf41ff39bff44a078e4b4015e6
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-02-21 11:02:49 -0600

    TP#23174 create IShippingProfileManagerDialogFactory

---
+++ Code/ShipWorks.Shipping/Services/Dialogs/IShippingProfileManagerDialogFactory.cs
@@ -0,0 +1,15 @@
+﻿using Interapptive.Shared.UI;
+
+namespace ShipWorks.Shipping.Services.Dialogs
+{
+    /// <summary>
+    /// Interface that represents a factory for creating the ShippingProfileManagerDialog
+    /// </summary>
+    interface IShippingProfileManagerDialogFactory
+    {
+        /// <summary>
+        /// Create the ShippingProfileManagerDialog
+        /// </summary>
+        IDialog Create();
+    }
+}
