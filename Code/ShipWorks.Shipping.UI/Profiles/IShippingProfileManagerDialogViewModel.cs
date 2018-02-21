54cd761e5421b6987640f0cc28aebba6118a98a5
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-02-21 10:28:06 -0600

    TP#23174 create IShippingProfileManagerDialogViewModel

---
+++ Code/ShipWorks.Shipping.UI/Profiles/IShippingProfileManagerDialogViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping.UI.Profiles
+{
+    /// <summary>
+    /// Interface that represents the Shipping Profile Manager Dialog view model
+    /// </summary>
+    interface IShippingProfileManagerDialogViewModel
+    {
+        /// <summary>
+        /// Add a profile
+        /// </summary>
+        ICommand AddCommand { get; }
+
+        /// <summary>
+        /// Edit the selected profile
+        /// </summary>
+        ICommand EditCommand { get; }
+
+        /// <summary>
+        /// Delete the selected profile
+        /// </summary>
+        ICommand DeleteCommand { get; }
+
+        /// <summary>
+        /// Shipping Profiles loaded in the dialog
+        /// </summary>
+        IEnumerable<ShippingProfileEntity> ShippingProfiles { get; }
+
+        /// <summary>
+        /// Selected Shipping Profile
+        /// </summary>
+        ShippingProfileEntity SelectedShippingProfile { get; }
+    }
+}
