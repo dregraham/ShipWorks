1c0712225e7065b513951d224eaf792d69b1c017
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-02-27 12:45:39 -0600

    TP#23174 add obfuscation attribute to binding dto

--- Code/ShipWorks.Shipping.UI/Profiles/ShippingProfileAndShortcut.cs
+++ Code/ShipWorks.Shipping.UI/Profiles/ShippingProfileAndShortcut.cs
@@ -1,4 +1,5 @@
-﻿using Interapptive.Shared.Utility;
+﻿using System.Reflection;
+using Interapptive.Shared.Utility;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Data.Model.EntityInterfaces;
 
@@ -43,11 +44,13 @@ namespace ShipWorks.Shipping.UI.Profiles
         /// <remarks>
         /// This is the description of the ShortcutKey. Blank if no associated keyboard shortcut
         /// </remarks>
+        [Obfuscation(Exclude = true)]
         public string ShortcutKey { get; }
 
         /// <summary>
         /// The associated ShipmentType description. Blank if global
         /// </summary>
+        [Obfuscation(Exclude = true)]
         public string ShipmentTypeDescription { get; }
     }
 }
