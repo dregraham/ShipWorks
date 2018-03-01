5824d547f829f644c97af44dac96e6ff6cd843da
Kevin Croke <k.croke@shipworks.com>
2018-03-01 09:54:25 -0600

    TP#23524: Added unit tests for ShippingProfileTest

--- Code/ShipWorks.Shipping.UI.Tests/Profiles/ShippingProfileTest.cs
+++ Code/ShipWorks.Shipping.UI.Tests/Profiles/ShippingProfileTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Autofac.Extras.Moq;
 using Interapptive.Shared.Utility;
+using Moq;
 using ShipWorks.Common.IO.KeyboardShortcuts;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Shipping.Profiles;
@@ -83,6 +84,89 @@ namespace ShipWorks.Shipping.UI.Tests.Profiles
 
             ShippingProfile testObject = CreateShippingProfile(profile, shortcut);
             Assert.Equal(testObject.ShipmentTypeDescription, string.Empty);
+        }
+
+        [Fact]
+        public void Validate_ReturnsFailure_WhenProfileNameIsEmpty()
+        {
+            var testObject = CreateShippingProfile(new ShippingProfileEntity(), new ShortcutEntity());
+            var result = testObject.Validate();
+            
+            Assert.True(result.Failure);
+            Assert.Equal("Enter a name for the profile.", result.Message);
+        }
+
+        [Fact]
+        public void Validate_ReturnsFailure_WhenAProfileWithSameNameExists()
+        {
+            mock.Mock<IShippingProfileManager>()
+                .SetupGet(m => m.Profiles)
+                .Returns(new[] { new ShippingProfileEntity() { Name = "same" } });
+            
+            var testObject = CreateShippingProfile(new ShippingProfileEntity() { ShippingProfileID = 5, Name = "same" },
+                new ShortcutEntity());
+            
+            var result = testObject.Validate();
+            
+            Assert.True(result.Failure);
+            Assert.Equal("A profile with the chosen name already exists.", result.Message);
+        }
+        
+        [Fact]
+        public void Validate_ReturnsFailure_WhenAShortcutWithSameBarcodeExists()
+        {
+            mock.Mock<IShortcutManager>()
+                .SetupGet(m => m.Shortcuts)
+                .Returns(new[] { new ShortcutEntity() { Barcode = "same" } });
+
+            var testObject = CreateShippingProfile(new ShippingProfileEntity() { Name = "blah" }, new ShortcutEntity()
+            {
+                ShortcutID = 42,
+                Barcode = "same"
+            });
+            
+            var result = testObject.Validate();
+            
+            Assert.True(result.Failure);
+            Assert.Equal("The barcode \"same\" is already in use.", result.Message);
+        }
+
+        [Theory]
+        [InlineData(null, ShipmentTypeCode.Usps)]
+        [InlineData(ShipmentTypeCode.Usps, null)]
+        [InlineData(ShipmentTypeCode.FedEx, ShipmentTypeCode.UpsOnLineTools)]
+        public void ChangeProvider_ChangesShipmentType(ShipmentTypeCode? initialShipmentType, ShipmentTypeCode? newShipmentType)
+        {
+            var testObject = CreateShippingProfile(new ShippingProfileEntity() { ShipmentType = initialShipmentType },
+                new ShortcutEntity());
+            
+            testObject.ChangeProvider(newShipmentType);
+            
+            Assert.Equal(newShipmentType, testObject.ShippingProfileEntity.ShipmentType);
+        }
+
+        [Fact]
+        public void ChangeProvider_ClearsPackages()
+        {
+            var profile = new ShippingProfileEntity();
+            profile.Packages.Add(new UpsProfilePackageEntity());
+                
+            var testObject = CreateShippingProfile(profile, new ShortcutEntity());
+            
+            testObject.ChangeProvider(ShipmentTypeCode.Amazon);
+            Assert.Empty(profile.Packages);
+        }
+
+        [Fact]
+        public void ChangeProvider_DelegatesToProfileLoader()
+        {
+            ShippingProfileEntity profile = new ShippingProfileEntity();
+            ShortcutEntity shortcut = new ShortcutEntity();
+            ShippingProfile testObject = CreateShippingProfile(profile, shortcut);
+            
+            testObject.ChangeProvider(ShipmentTypeCode.Endicia);
+            
+            mock.Mock<IShippingProfileLoader>().Verify(l=>l.LoadProfileData(profile, true), Times.Once);
         }
 
         private ShippingProfile CreateShippingProfile(ShippingProfileEntity profile, ShortcutEntity shortcut)
