f2e35e0ee2b8b585c85da46d0d0128acb842b638
Kevin Croke <k.croke@shipworks.com>
2018-03-06 15:38:46 -0600

    TP#23526: Add PostalShippingProfileApplicationStrategy

---
+++ Code/ShipWorks.Shipping.Tests/Carriers/Postal/PostalShippingProfileApplicationStrategyTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using ShipWorks.Tests.Shared;
+using Autofac.Extras.Moq;
+using Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.Postal;
+using ShipWorks.Shipping.Profiles;
+using Xunit;
+using static ShipWorks.Tests.Shared.ExtensionMethods.ParameterShorteners;
+
+namespace ShipWorks.Shipping.Tests.Carriers.Postal
+{
+    public class PostalShippingProfileApplicationStrategyTest : IDisposable
+    {
+        private readonly AutoMock mock;
+        private readonly ShippingProfileEntity profile;
+        private readonly ShipmentEntity shipment;
+        private PackageProfileEntity packageProfile;
+
+        public PostalShippingProfileApplicationStrategyTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+            profile = new ShippingProfileEntity()
+            {
+                Packages = { new PackageProfileEntity() },
+                Postal = new PostalProfileEntity()
+            };
+            shipment = new ShipmentEntity() { Postal = new PostalShipmentEntity() };
+            packageProfile = profile.Packages.Single();
+        }
+
+        [Fact]
+        public void ApplyProfile_DelegatesToBaseStrategy()
+        {
+            var testObject = mock.Create<PostalShippingProfileApplicationStrategy>();
+            testObject.ApplyProfile(profile, shipment);
+
+            mock.Mock<IShippingProfileApplicationStrategy>().Verify(s => s.ApplyProfile(profile, shipment), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(null, 42)]
+        [InlineData(0, 42)]
+        [InlineData(5, 5)]
+        public void ApplyProfile_WeightIsSetProperly(double? profileWeight, double expectedWeight)
+        {
+            shipment.ContentWeight = 42;
+            packageProfile.Weight = profileWeight;
+
+            var testObject = mock.Create<PostalShippingProfileApplicationStrategy>();
+            testObject.ApplyProfile(profile, shipment);
+
+            Assert.Equal(expectedWeight, shipment.ContentWeight);
+        }
+
+        [Fact]
+        public void ApplyProfile_DimsProfileIDIsSet()
+        {
+            packageProfile.DimsProfileID = 44;
+            
+            var testObject = mock.Create<PostalShippingProfileApplicationStrategy>();
+            testObject.ApplyProfile(profile, shipment);
+
+            Assert.Equal(42, shipment.Postal.DimsProfileID);
+        }
+        
+        [Fact]
+        public void ApplyProfile_DimsensionsAreSet()
+        {
+            packageProfile.DimsProfileID = 44;
+            packageProfile.DimsLength = 1;
+            packageProfile.DimsWidth = 2;
+            packageProfile.DimsHeight = 3;
+            packageProfile.DimsWeight = 4;
+            packageProfile.DimsAddWeight = true;
+            
+            var testObject = mock.Create<PostalShippingProfileApplicationStrategy>();
+            testObject.ApplyProfile(profile, shipment);
+
+            Assert.Equal(42, shipment.Postal.DimsProfileID);
+            Assert.Equal(1, shipment.Postal.DimsLength);
+            Assert.Equal(1, shipment.Postal.DimsWidth);
+            Assert.Equal(1, shipment.Postal.DimsHeight);
+            Assert.Equal(1, shipment.Postal.DimsWeight);
+            Assert.True(shipment.Postal.DimsAddWeight);
+        }
+
+        [Fact]
+        public void ApplyProfile_PackagingInformationIsSet()
+        {
+            profile.Postal.PackagingType = 7;
+            profile.Postal.NonRectangular = true;
+            profile.Postal.NonMachinable = true;
+            
+            var testObject = mock.Create<PostalShippingProfileApplicationStrategy>();
+            testObject.ApplyProfile(profile, shipment);
+            
+            Assert.Equal(7, shipment.Postal.PackagingType);
+            Assert.True(shipment.Postal.NonRectangular);
+            Assert.True(shipment.Postal.NonMachinable);
+        }
+        
+        //TODO: Test all the things!
+        
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+    }
+}
---
+++ Code/ShipWorks.Shipping/Carriers/Postal/PostalShippingProfileApplicationStrategy.cs
@@ -0,0 +1,80 @@
+﻿using System.Linq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Data.Model.EntityInterfaces;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Shipping.Profiles;
+
+namespace ShipWorks.Shipping.Carriers.Postal
+{
+    /// <summary>
+    /// Applies a postal shipping profile
+    /// </summary>
+    public class PostalShippingProfileApplicationStrategy : IShippingProfileApplicationStrategy
+    {
+        private readonly IShippingProfileApplicationStrategy baseStrategy;
+        private readonly ShipmentType shipmentType;
+
+        /// <summary>
+        /// Applies Constructor
+        /// </summary>
+        public PostalShippingProfileApplicationStrategy(IShippingProfileApplicationStrategy baseStrategy,
+            ShipmentType shipmentType)
+        {
+            this.baseStrategy = baseStrategy;
+            this.shipmentType = shipmentType;
+        }
+        
+        /// <summary>
+        /// Applies a profile 
+        /// </summary>
+        public void ApplyProfile(IShippingProfileEntity profile, ShipmentEntity shipment)
+        {
+            baseStrategy.ApplyProfile(profile, shipment);
+
+            PostalShipmentEntity postalShipment = shipment.Postal;
+            IPostalProfileEntity postalProfile = profile.Postal;
+            IPackageProfileEntity packageProfile = profile.Packages.Single();
+            
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.Service, postalShipment, PostalShipmentFields.Service);
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.Confirmation, postalShipment, PostalShipmentFields.Confirmation);
+
+            // Special case - only apply if the weight is not zero.  This prevents the weight entry from the default profile from overwriting the prefilled weight from products.
+            if (packageProfile.Weight != null && packageProfile.Weight.Value != 0)
+            {
+                ShippingProfileUtility.ApplyProfileValue(packageProfile.Weight, shipment, ShipmentFields.ContentWeight);
+            }
+
+            ShippingProfileUtility.ApplyProfileValue(packageProfile.DimsProfileID, postalShipment, PostalShipmentFields.DimsProfileID);
+            if (packageProfile.DimsProfileID != null)
+            {
+                ShippingProfileUtility.ApplyProfileValue(packageProfile.DimsLength, postalShipment, PostalShipmentFields.DimsLength);
+                ShippingProfileUtility.ApplyProfileValue(packageProfile.DimsWidth, postalShipment, PostalShipmentFields.DimsWidth);
+                ShippingProfileUtility.ApplyProfileValue(packageProfile.DimsHeight, postalShipment, PostalShipmentFields.DimsHeight);
+                ShippingProfileUtility.ApplyProfileValue(packageProfile.DimsWeight, postalShipment, PostalShipmentFields.DimsWeight);
+                ShippingProfileUtility.ApplyProfileValue(packageProfile.DimsAddWeight, postalShipment, PostalShipmentFields.DimsAddWeight);
+            }
+
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.PackagingType, postalShipment, PostalShipmentFields.PackagingType);
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.NonRectangular, postalShipment, PostalShipmentFields.NonRectangular);
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.NonMachinable, postalShipment, PostalShipmentFields.NonMachinable);
+
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.CustomsContentType, postalShipment, PostalShipmentFields.CustomsContentType);
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.CustomsContentDescription, postalShipment, PostalShipmentFields.CustomsContentDescription);
+
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.ExpressSignatureWaiver, postalShipment, PostalShipmentFields.ExpressSignatureWaiver);
+
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.SortType, postalShipment, PostalShipmentFields.SortType);
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.EntryFacility, postalShipment, PostalShipmentFields.EntryFacility);
+
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.Memo1, postalShipment, PostalShipmentFields.Memo1);
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.Memo2, postalShipment, PostalShipmentFields.Memo2);
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.Memo3, postalShipment, PostalShipmentFields.Memo3);
+
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.NoPostage, postalShipment, PostalShipmentFields.NoPostage);
+            ShippingProfileUtility.ApplyProfileValue(postalProfile.Profile.Insurance, postalShipment, PostalShipmentFields.Insurance);
+
+            shipmentType.UpdateDynamicShipmentData(shipment);
+            shipmentType.UpdateTotalWeight(shipment);
+        }
+    }
+}
