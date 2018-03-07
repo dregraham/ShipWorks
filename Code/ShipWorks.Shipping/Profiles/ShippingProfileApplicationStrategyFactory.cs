24e51c1ebace1dde8ad20ecd760b9df99b649248
Kevin Croke <k.croke@shipworks.com>
2018-03-07 14:01:01 -0600

    TP#23526: Create ShippingProfileApplicationStrategyFactory

---
+++ Code/ShipWorks.Shipping/Profiles/ShippingProfileApplicationStrategyFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Autofac.Features.Indexed;
+using Interapptive.Shared.ComponentRegistration;
+
+namespace ShipWorks.Shipping.Profiles
+{
+    /// <summary>
+    /// Factory to create a ShippingProfileApplicationStrategy
+    /// </summary>
+    [Component]
+    public class ShippingProfileApplicationStrategyFactory : IShippingProfileApplicationStrategyFactory
+    {
+        private readonly IIndex<ShipmentTypeCode, IShippingProfileApplicationStrategy> strategyChooser;
+        private readonly Func<IShippingProfileApplicationStrategy> createGlobalStrategy;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ShippingProfileApplicationStrategyFactory(IIndex<ShipmentTypeCode, IShippingProfileApplicationStrategy> strategyChooser,
+            Func<IShippingProfileApplicationStrategy> createGlobalStrategy)
+        {
+            this.strategyChooser = strategyChooser;
+            this.createGlobalStrategy = createGlobalStrategy;
+        }
+
+        /// <summary>
+        /// Create an Profile Application Strategy
+        /// </summary>
+        public IShippingProfileApplicationStrategy Create(ShipmentTypeCode? shipmentType) => 
+            shipmentType == null ? createGlobalStrategy() : strategyChooser[shipmentType.Value];
+    }
+}
