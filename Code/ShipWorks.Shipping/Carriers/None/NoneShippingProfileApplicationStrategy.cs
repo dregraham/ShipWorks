4a29353f74cdc48e6d81e1a124c7475f4eb323c4
Kevin Croke <k.croke@shipworks.com>
2018-03-08 14:51:42 -0600

    TP#23526: Add NoneShippingProfileApplicationStrategy

---
+++ Code/ShipWorks.Shipping/Carriers/None/NoneShippingProfileApplicationStrategy.cs
@@ -0,0 +1,16 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Data.Model.EntityInterfaces;
+using ShipWorks.Shipping.Profiles;
+
+namespace ShipWorks.Shipping.Carriers.None
+{
+    [KeyedComponent(typeof(IShippingProfileApplicationStrategy), ShipmentTypeCode.None)]
+    public class NoneShippingProfileApplicationStrategy : BaseShippingProfileApplicationStrategy
+    {
+        public NoneShippingProfileApplicationStrategy(IShipmentTypeManager shipmentTypeManager) 
+            : base(shipmentTypeManager)
+        {
+        }
+    }
+}
