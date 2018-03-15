5edf6b1e0c2b5a50d3296b4d5d33dc7200b9d8bf
Kevin Croke <k.croke@shipworks.com>
2018-03-15 09:06:01 -0500

    TP#24058: Fix for the rest of the unit tests

--- Code/ShipWorks.Shipping.Tests/Services/ShippingProfileRepositoryTest.cs
+++ Code/ShipWorks.Shipping.Tests/Services/ShippingProfileRepositoryTest.cs
@@ -265,7 +265,10 @@ namespace ShipWorks.Shipping.Tests.Services
 
         private ShippingProfile CreateShippingProfile(ShippingProfileEntity profile, ShortcutEntity shortcut)
         {
-            return mock.Create<ShippingProfile>(TypedParameter.From(profile), TypedParameter.From(shortcut));
+            var shippingProfile = mock.Create<ShippingProfile>();
+            shippingProfile.Shortcut = shortcut;
+            shippingProfile.ShippingProfileEntity = profile;
+            return shippingProfile;
         }
     }
 }
