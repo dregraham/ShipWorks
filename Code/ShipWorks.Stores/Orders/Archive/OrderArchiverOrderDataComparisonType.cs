4bc51309cc0d878ee2ae2e8db3bbb405e84fce96
Tim Hughes <t.hughes@shipworks.com>
2018-03-21 09:54:43 -0500

    TP#24061: Added enum for order archive date comparison

---
+++ Code/ShipWorks.Stores/Orders/Archive/OrderArchiverOrderDataComparisonType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ShipWorks.Stores.Platforms.BigCommerce.Enums;
+
+namespace ShipWorks.Stores.Orders.Archive
+{
+    /// <summary>
+    /// Enum for order archiving by order date comparison
+    /// </summary>
+    [Obfuscation(Exclude = true, StripAfterObfuscation = false, ApplyToMembers = true)]
+    public enum OrderArchiverOrderDataComparisonType
+    {
+        [Description("Order dates less than given date.")]
+        LessThan = 0,
+
+        [Description("Order dates greater than or equal to given date.")]
+        GreaterThanOrEqual = 1
+    }
+}
