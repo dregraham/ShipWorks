40a9073406272db3001c3e4e78183465a1d1ce5c
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-03-28 14:40:33 -0500

    TP#23914 reset profile values back when canceling out of dlg

--- Code/ShipWorks.Data.Model/Custom/EntityExtensions.cs
+++ Code/ShipWorks.Data.Model/Custom/EntityExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Interapptive.Shared.Collections;
 using Interapptive.Shared.Utility;
 using SD.LLBLGen.Pro.ORMSupportClasses;
 using ShipWorks.Data.Model.HelperClasses;
@@ -20,6 +22,18 @@ namespace ShipWorks.Data.Model.Custom
             EntityCollection<T> collection = new EntityCollection<T>();
             collection.AddRange(source);
             return collection;
+        }
+
+        /// <summary>
+        /// Reset the entities dirty fields values to database values
+        /// </summary>
+        public static void ResetDirtyFieldsToDbValues(this EntityCore<IEntityFields2> entity)
+        {
+            if (entity.IsDirty)
+            {
+                entity.Fields.Where(f => f.IsChanged).ForEach(f => f.CurrentValue = f.DbValue);
+                entity.IsDirty = false;
+            }
         }
     }
 }
