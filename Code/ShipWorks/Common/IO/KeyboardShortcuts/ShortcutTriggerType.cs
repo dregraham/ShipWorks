2bb34c7ad785a663970b81fdf3898a0bb92e5910
Chris Hicks <c.hicks@shipworks.com>
2018-04-04 10:20:47 -0500

    TP#23529: Create ShortcutTriggerType

---
+++ Code/ShipWorks/Common/IO/KeyboardShortcuts/ShortcutTriggerType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Common.IO.KeyboardShortcuts
+{
+    [Obfuscation(Exclude = true, StripAfterObfuscation = false, ApplyToMembers = true)]
+    public enum ShortcutTriggerType
+    {
+        Hotkey = 0,
+
+        Barcode = 1
+    }
+}
