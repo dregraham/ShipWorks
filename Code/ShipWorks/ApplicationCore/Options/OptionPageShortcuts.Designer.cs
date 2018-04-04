1744be2f588d53db47aa7094d7a29ed0715d2882
Chris Hicks <c.hicks@shipworks.com>
2018-04-04 10:09:38 -0500

    TP#23528: Make menu item height dynamic in ShipWorksOptions

--- Code/ShipWorks/ApplicationCore/Options/OptionPageShortcuts.Designer.cs
+++ Code/ShipWorks/ApplicationCore/Options/OptionPageShortcuts.Designer.cs
@@ -162,7 +162,7 @@
             // 
             this.label1.Location = new System.Drawing.Point(17, 180);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(518, 43);
+            this.label1.Size = new System.Drawing.Size(511, 43);
             this.label1.TabIndex = 44;
             this.label1.Text = resources.GetString("label1.Text");
             // 
