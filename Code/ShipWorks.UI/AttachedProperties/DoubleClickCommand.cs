d098bb9984c4c5a1ec58ef4e92f2617f23d130f4
Kevin Croke <k.croke@shipworks.com>
2018-04-11 10:07:14 -0500

    TP#24678: Made Grid double click command a general control double click command

---
+++ Code/ShipWorks.UI/AttachedProperties/DoubleClickCommand.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ShipWorks.UI.AttachedProperties
+{
+    /// <summary>
+    /// Attached property to issue a command when the control is double clicked
+    /// </summary>
+    [Obfuscation(Exclude = true)]
+    public static class DoubleClickCommand
+    {
+        /// <summary>
+        /// Associate a command with a double click
+        /// </summary>
+        public static readonly DependencyProperty DoubleClickProperty =
+            DependencyProperty.RegisterAttached("DoubleClickCommand", typeof(ICommand), typeof(DoubleClickCommand),
+                new PropertyMetadata(AttachOrRemoveDoubleClickEvent));
+
+        /// <summary>
+        /// Get the double click command
+        /// </summary>
+        public static ICommand GetDoubleClickCommand(DependencyObject obj)
+        {
+            return (ICommand) obj.GetValue(DoubleClickProperty);
+        }
+
+        /// <summary>
+        /// Set the double click command
+        /// </summary>
+        public static void SetDoubleClickCommand(DependencyObject obj, ICommand value)
+        {
+            obj.SetValue(DoubleClickProperty, value);
+        }
+
+        /// <summary>
+        /// Attach or Remove double click event
+        /// </summary>
+        public static void AttachOrRemoveDoubleClickEvent(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            if (obj is Control control)
+            {
+                if (args.OldValue == null && args.NewValue != null)
+                {
+                    control.MouseDoubleClick += ExecuteDoubleClick;
+                }
+                else if (args.OldValue != null && args.NewValue == null)
+                {
+                    control.MouseDoubleClick -= ExecuteDoubleClick;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Run double click command
+        /// </summary>
+        private static void ExecuteDoubleClick(object sender, MouseButtonEventArgs args)
+        {
+            DependencyObject obj = sender as DependencyObject;
+            ICommand cmd = (ICommand) obj?.GetValue(DoubleClickProperty);
+            cmd?.Execute(obj);
+        }
+    }
+}
