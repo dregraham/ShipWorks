b1464f71d4b6f92ee15cd04f3ee1d47e5686cab9
Josh Winchester <j.winchester@shipworks.com>
2018-04-11 14:08:49 -0500

    TP#24577: Adding Interfaces for SingleDatabaseSelectorDialog and SingleDatabaseSelector

---
+++ Code/ShipWorks/Stores/Orders/Archive/ISingleDatabaseSelectorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ShipWorks.Data.Administration;
+
+namespace ShipWorks.Stores.Orders.Archive
+{
+    public interface ISingleDatabaseSelectorViewModel
+    {
+        /// <summary>
+        /// Gets whether a single database instance will be returned or not.
+        /// </summary>
+        ISqlDatabaseDetail SelectSingleDatabase(IEnumerable<ISqlDatabaseDetail> databaseDetail);
+
+        /// <summary>
+        /// Gets the selected database chosen the user.
+        /// </summary>
+        ISqlDatabaseDetail SelectedDatabase { get; set; }
+    }
+}
