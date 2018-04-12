a027472be1ab068964f12e22210fee946ff18678
Andrew Benz <a.benz@shipworks.com>
2018-04-12 09:55:13 -0500

    TP#24577: Add commands to the single database selector view model

--- Code/ShipWorks.Stores/Orders/Archive/SingleDatabaseSelectorViewModel.cs
+++ Code/ShipWorks.Stores/Orders/Archive/SingleDatabaseSelectorViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.CommandWpf;
 using Interapptive.Shared.Collections;
 using Interapptive.Shared.ComponentRegistration;
 using Interapptive.Shared.UI;
@@ -22,6 +24,7 @@ namespace ShipWorks.Stores.Orders.Archive
         private readonly PropertyChangedHandler handler;
 
         private ISqlDatabaseDetail selectedDatabase;
+        private ISingleDatabaseSelectorDialog dialog;
 
         /// <summary>
         /// Constructor
@@ -32,12 +35,27 @@ namespace ShipWorks.Stores.Orders.Archive
             this.createDialog = createDialog;
 
             handler = new PropertyChangedHandler(this, () => PropertyChanged);
+
+            Cancel = new RelayCommand(() => CloseDialog(dialog, false));
+            Accept = new RelayCommand(() => CloseDialog(dialog, true), () => SelectedDatabase != null);
         }
 
         /// <summary>
         /// A property has changed
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Cancels the dialog
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public ICommand Cancel { get; }
+
+        /// <summary>
+        /// Accepts the dialog
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public ICommand Accept { get; }
 
         /// <summary>
         /// Gets the selected database chosen the user.
@@ -67,10 +85,22 @@ namespace ShipWorks.Stores.Orders.Archive
 
             Databases = databaseDetails;
 
-            var dialog = createDialog();
+            dialog = createDialog();
             dialog.DataContext = this;
 
             return messageHelper.ShowDialog(dialog) == true ? SelectedDatabase : null;
+        }
+
+        /// <summary>
+        /// Close the dialog
+        /// </summary>
+        private void CloseDialog(ISingleDatabaseSelectorDialog localDialog, bool result)
+        {
+            if (localDialog != null)
+            {
+                localDialog.DialogResult = result;
+                localDialog.Close();
+            }
         }
     }
 }
