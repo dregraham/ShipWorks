9bcd2f0e7c117a3407f2585f0abada51d770f4fc
Chris Hicks <c.hicks@shipworks.com>
2018-04-24 12:18:11 -0500

    TP#23811: Default ShopifyNotifyCustomer to true for existing stores.

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.24/5.24.0.2.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.24/5.24.0.2.sql
@@ -1,7 +1,7 @@
 PRINT N'Altering [dbo].[ShopifyStore]'
 GO
 ALTER TABLE [dbo].[ShopifyStore] ADD
-[ShopifyNotifyCustomer] [bit] NOT NULL CONSTRAINT [DF_ShopifyStore_ShopifyNotifyCustomer] DEFAULT (0)
+[ShopifyNotifyCustomer] [bit] NOT NULL CONSTRAINT [DF_ShopifyStore_ShopifyNotifyCustomer] DEFAULT (1)
 GO
 
 PRINT N'Dropping constraints from [dbo].[ShopifyStore]'
