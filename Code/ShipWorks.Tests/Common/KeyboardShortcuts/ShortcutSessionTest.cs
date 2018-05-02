27568e53a74ce47957ad547cd9bb5b7516497cc5
Kevin Croke <k.croke@shipworks.com>
2018-05-02 13:50:02 -0500

    TP#25092: Better formatting unit tests

--- Code/ShipWorks.Tests/Common/KeyboardShortcuts/ShortcutSessionTest.cs
+++ Code/ShipWorks.Tests/Common/KeyboardShortcuts/ShortcutSessionTest.cs
@@ -1,22 +1,38 @@
 ﻿using System;
-using ShipWorks.Tests.Shared;
 using Autofac.Extras.Moq;
 using Moq;
 using ShipWorks.ApplicationCore;
 using ShipWorks.Common;
 using ShipWorks.Common.IO.KeyboardShortcuts;
+using ShipWorks.Tests.Shared;
 using Xunit;
-using static ShipWorks.Tests.Shared.ExtensionMethods.ParameterShorteners;
 
 namespace ShipWorks.Tests.Common.KeyboardShortcuts
 {
     public class ShortcutSessionTest : IDisposable
     {
         private readonly AutoMock mock;
-
+        private readonly KeyboardShortcutKeyFilter keyboardShortcutKeyFilter;
+        
         public ShortcutSessionTest()
         {
             mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+            
+            keyboardShortcutKeyFilter = mock.Create<KeyboardShortcutKeyFilter>();
+            
+            Mock<Func<KeyboardShortcutKeyFilter>> mockFuncKeyboardShortcutKeyFilter =
+                mock.MockRepository.Create<Func<KeyboardShortcutKeyFilter>>();
+            mockFuncKeyboardShortcutKeyFilter.Setup(func => func()).Returns(keyboardShortcutKeyFilter);
+            mock.Provide(mockFuncKeyboardShortcutKeyFilter.Object);
+        }
+
+        [Fact]
+        public void EndSession_AddsMessageFilter()
+        {
+            ShortcutSession shortcutSession = mock.Create<ShortcutSession>();
+            shortcutSession.InitializeForCurrentSession();
+
+            mock.Mock<IWindowsMessageFilterRegistrar>().Verify(r => r.AddMessageFilter(keyboardShortcutKeyFilter), Times.Once);
         }
 
         [Fact]
@@ -24,33 +40,12 @@ namespace ShipWorks.Tests.Common.KeyboardShortcuts
         {
             mock.Mock<IMainForm>().SetupGet(m => m.InvokeRequired).Returns(false);
 
-            KeyboardShortcutKeyFilter keyboardShortcutKeyFilter = mock.Create<KeyboardShortcutKeyFilter>();
-            
-            var mockFuncKeyboardShortcutKeyFilter = mock.MockRepository.Create<Func<KeyboardShortcutKeyFilter>>();
-            mockFuncKeyboardShortcutKeyFilter.Setup(func => func()).Returns(keyboardShortcutKeyFilter);
-            mock.Provide(mockFuncKeyboardShortcutKeyFilter.Object);
-            
             ShortcutSession shortcutSession = mock.Create<ShortcutSession>();
             shortcutSession.EndSession();
-            
-            mock.Mock<IWindowsMessageFilterRegistrar>().Verify(r=>r.RemoveMessageFilter(keyboardShortcutKeyFilter), Times.Once);
+
+            mock.Mock<IWindowsMessageFilterRegistrar>().Verify(r => r.RemoveMessageFilter(keyboardShortcutKeyFilter), Times.Once);
         }
         
-        [Fact]
-        public void EndSession_AddsMessageFilter()
-        {
-            KeyboardShortcutKeyFilter keyboardShortcutKeyFilter = mock.Create<KeyboardShortcutKeyFilter>();
-            
-            var mockFuncKeyboardShortcutKeyFilter = mock.MockRepository.Create<Func<KeyboardShortcutKeyFilter>>();
-            mockFuncKeyboardShortcutKeyFilter.Setup(func => func()).Returns(keyboardShortcutKeyFilter);
-            mock.Provide(mockFuncKeyboardShortcutKeyFilter.Object);
-            
-            ShortcutSession shortcutSession = mock.Create<ShortcutSession>();
-            shortcutSession.InitializeForCurrentSession();
-            
-            mock.Mock<IWindowsMessageFilterRegistrar>().Verify(r=>r.AddMessageFilter(keyboardShortcutKeyFilter), Times.Once);
-        }
-
         public void Dispose()
         {
             mock.Dispose();
