753a413d27fa5e81883835317f85e169405862ab
Kevin Croke <k.croke@shipworks.com>
2018-05-15 10:18:48 -0500

    TP#24312: Add MainGridControlShortcutPipeline

---
+++ Code/ShipWorks.SingleScan.Tests/MainGridControlShortcutPipelineTest.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using ShipWorks.Tests.Shared;
+using Autofac.Extras.Moq;
+using Interapptive.Shared.Threading;
+using Interapptive.Shared.UI;
+using Microsoft.Reactive.Testing;
+using Moq;
+using ShipWorks.ApplicationCore;
+using ShipWorks.Common.IO.KeyboardShortcuts;
+using ShipWorks.Common.IO.KeyboardShortcuts.Messages;
+using ShipWorks.Core.Messaging;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.IO.KeyboardShortcuts;
+using ShipWorks.Users;
+using Xunit;
+using static ShipWorks.Tests.Shared.ExtensionMethods.ParameterShorteners;
+
+namespace ShipWorks.SingleScan.Tests
+{
+    public class MainGridControlShortcutPipelineTest : IDisposable
+    {
+        private readonly AutoMock mock;
+        private readonly TestMessenger testMessenger;
+        private readonly Mock<IMainGridControl> mainGridControl;
+        private readonly MainGridControlShortcutPipeline testObject;
+        private readonly TestScheduler scheduler;
+
+        public MainGridControlShortcutPipelineTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+
+            testMessenger = new TestMessenger();
+            mock.Provide<IMessenger>(testMessenger);
+            mainGridControl = mock.CreateMock<IMainGridControl>();
+            testObject = mock.Create<MainGridControlShortcutPipeline>();
+
+            Mock<ISchedulerProvider> scheduleProvider = mock.WithMockImmediateScheduler();
+            scheduler = new TestScheduler();
+            scheduleProvider.Setup(s => s.WindowsFormsEventLoop).Returns(scheduler);
+            scheduleProvider.Setup(s => s.Default).Returns(scheduler);
+
+            testObject.Register(mainGridControl.Object);
+        }
+
+        [Fact]
+        public void ClearQuickSearch_ClearsQuickSearch()
+        {
+            var shortcut = new ShortcutEntity() { Action = KeyboardShortcutCommand.ClearQuickSearch };
+            var message = new ShortcutMessage(this, shortcut, ShortcutTriggerType.Barcode, string.Empty);
+            testMessenger.Send(message);
+
+            scheduler.Start();
+
+            mainGridControl.VerifySet(m => m.SearchBoxText = string.Empty, Times.Once());
+        }
+
+        [Fact]
+        public void SetQuickSearchFocus_SetsQuickSearchFocus()
+        {
+            var shortcut = new ShortcutEntity() { Action = KeyboardShortcutCommand.FocusQuickSearch };
+            var message = new ShortcutMessage(this, shortcut, ShortcutTriggerType.Barcode, string.Empty);
+            testMessenger.Send(message);
+
+            scheduler.Start();
+
+            mainGridControl.Verify(m => m.FocusSearch(), Times.Once());
+        }
+
+        [Theory]
+        [InlineData(KeyboardShortcutCommand.ClearQuickSearch, "Barcode: Clear Quick Search")]
+        [InlineData(KeyboardShortcutCommand.FocusQuickSearch, "Barcode: Focus Quick Search")]
+        public void BarcodeMessage_ShowsMessageIndicator_WhenShouldShowNotificationIsTrue(KeyboardShortcutCommand action, string expectedIndicatorText)
+        {
+            mock.Mock<ICurrentUserSettings>()
+                .Setup(s => s.ShouldShowNotification(It.IsAny<UserConditionalNotificationType>()))
+                .Returns(true);
+
+            var shortcut = new ShortcutEntity() { Action = action };
+            var message = new ShortcutMessage(this, shortcut, ShortcutTriggerType.Barcode, string.Empty);
+            testMessenger.Send(message);
+
+            scheduler.Start();
+
+            mock.Mock<IMessageHelper>().Verify(m => m.ShowBarcodePopup(expectedIndicatorText), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(KeyboardShortcutCommand.ClearQuickSearch, "The Hotkey: Clear Quick Search")]
+        [InlineData(KeyboardShortcutCommand.FocusQuickSearch, "The Hotkey: Focus Quick Search")]
+        public void KeyboardShortcutMessage_ShowsMessageIndicator_WhenShouldShowNotificationIsTrue(KeyboardShortcutCommand action, string expectedIndicatorText)
+        {
+            mock.Mock<ICurrentUserSettings>()
+                .Setup(s => s.ShouldShowNotification(It.IsAny<UserConditionalNotificationType>()))
+                .Returns(true);
+
+            var shortcut = new ShortcutEntity() { Action = action };
+            var message = new ShortcutMessage(this, shortcut, ShortcutTriggerType.Hotkey, "The Hotkey");
+            testMessenger.Send(message);
+
+            scheduler.Start();
+
+            mock.Mock<IMessageHelper>().Verify(m => m.ShowKeyboardPopup(expectedIndicatorText), Times.Once);
+        }
+
+        [Fact]
+        public void ShortcutMessage_ShowsNoMessageIndicator_WhenShouldShowNotificationIsFalse()
+        {
+            mock.Mock<ICurrentUserSettings>()
+                .Setup(s => s.ShouldShowNotification(It.IsAny<UserConditionalNotificationType>()))
+                .Returns(false);
+
+            var shortcut = new ShortcutEntity() { Action = KeyboardShortcutCommand.FocusQuickSearch };
+            var message = new ShortcutMessage(this, shortcut, ShortcutTriggerType.Hotkey, "The Hotkey");
+            testMessenger.Send(message);
+
+            scheduler.Start();
+
+            mock.Mock<IMessageHelper>().Verify(m => m.ShowKeyboardPopup(AnyString), Times.Never);
+        }
+
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+    }
+}
---
+++ Code/ShipWorks.SingleScan/MainGridControlShortcutPipeline.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Reactive.Linq;
+using Interapptive.Shared.Threading;
+using Interapptive.Shared.UI;
+using Interapptive.Shared.Utility;
+using ShipWorks.ApplicationCore;
+using ShipWorks.Common.IO.KeyboardShortcuts;
+using ShipWorks.Common.IO.KeyboardShortcuts.Messages;
+using ShipWorks.Core.Messaging;
+using ShipWorks.IO.KeyboardShortcuts;
+using ShipWorks.Users;
+
+namespace ShipWorks.SingleScan
+{
+    /// <summary>
+    /// Pipeline to clear or focus quick search text box based on shortcut messages
+    /// </summary>
+    public class MainGridControlShortcutPipeline : IMainGridControlPipeline
+    {
+        private IDisposable subscription;
+        private readonly IMessenger messenger;
+        private readonly ICurrentUserSettings currentUserSettings;
+        private readonly IMessageHelper messageHelper;
+        private readonly ISchedulerProvider schedulerProvider;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainGridControlShortcutPipeline(IMessenger messenger,
+            ICurrentUserSettings currentUserSettings,
+            IMessageHelper messageHelper,
+            ISchedulerProvider schedulerProvider)
+        {
+            this.messageHelper = messageHelper;
+            this.schedulerProvider = schedulerProvider;
+            this.currentUserSettings = currentUserSettings;
+            this.messenger = messenger;
+        }
+
+        /// <summary>
+        /// Subscribe to QuickSearch shortcut messages 
+        /// </summary>
+        public IDisposable Register(IMainGridControl mainGridControl)
+        {
+            subscription = messenger.OfType<ShortcutMessage>()
+                .Where(m => m.AppliesTo(KeyboardShortcutCommand.ClearQuickSearch) ||
+                            m.AppliesTo(KeyboardShortcutCommand.FocusQuickSearch))
+                .ObserveOn(schedulerProvider.WindowsFormsEventLoop)
+                .Subscribe(message => HandleQuickSearchMessage(message, mainGridControl));
+
+            return subscription;
+        }
+
+        /// <summary>
+        /// Handle QuickSearch Message
+        /// </summary>
+        private void HandleQuickSearchMessage(ShortcutMessage shortcutMessage, IMainGridControl mainGridControl)
+        {
+            UpdateQuickSearchTextbox(shortcutMessage, mainGridControl);
+            ShowShortcutIndicator(shortcutMessage);
+        }
+
+        /// <summary>
+        /// Show shortcut indicator
+        /// </summary>
+        private void ShowShortcutIndicator(ShortcutMessage shortcutMessage)
+        {
+            if (currentUserSettings.ShouldShowNotification(UserConditionalNotificationType.ShortcutIndicator))
+            {
+                string actionName = EnumHelper.GetDescription(shortcutMessage.Shortcut.Action);
+
+                if (shortcutMessage.Trigger == ShortcutTriggerType.Hotkey)
+                {
+                    messageHelper.ShowKeyboardPopup($"{shortcutMessage.Value}: {actionName}");
+                }
+                else
+                {
+                    messageHelper.ShowBarcodePopup($"Barcode: {actionName}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clears or focuses quick search textbox
+        /// </summary>
+        private static void UpdateQuickSearchTextbox(ShortcutMessage shortcutMessage, IMainGridControl mainGridControl)
+        {
+            if (shortcutMessage.AppliesTo(KeyboardShortcutCommand.ClearQuickSearch))
+            {
+                mainGridControl.SearchBoxText = string.Empty;
+            }
+            else
+            {
+                mainGridControl.FocusSearch();
+            }
+        }
+    }
+}
