534dc2a0a1fb0695269250026d522fd29c9c41e0
Andrew Benz <a.benz@shipworks.com>
2018-05-16 14:34:58 -0500

    TP#24716: Add OnlineCommandCreator for Overstock

---
+++ Code/ShipWorks.Stores/Platforms/Overstock/OnlineUpdating/OverstockShipmentDetailsUpdater.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Stores.Platforms.Overstock
+{
+    /// <summary>
+    /// Handles uploading data to Overstock
+    /// </summary>
+    [Component]
+    public class OverstockShipmentDetailsUpdater : IOverstockShipmentDetailsUpdater
+    {
+        /// <summary>
+        /// Uploads shipment details for the given shipment Id
+        /// </summary>
+        public Task UploadShipmentDetails(long shipmentID)
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Uploads shipment details for the given shipment entity
+        /// </summary>
+        public Task UploadShipmentDetails(ShipmentEntity shipment)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
