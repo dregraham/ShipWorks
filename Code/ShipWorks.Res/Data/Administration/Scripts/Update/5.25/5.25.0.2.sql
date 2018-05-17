ea8975b92fdde5e00230ad8173efe37ed0ebed26
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-05-17 15:43:26 -0500

    consolidate upgrade scripts

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.25/5.25.0.2.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.25/5.25.0.2.sql
@@ -1,7 +1,45 @@
+IF NOT EXISTS (SELECT 1 FROM [dbo].[Shortcut] WHERE Barcode = '-PL-')
+	INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
+	VALUES (0, 121, '-PL-', 3)
+ELSE
+	INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
+	VALUES (0, 121, '', 3)
+GO
+IF NOT EXISTS (SELECT 1 FROM [dbo].[Shortcut] WHERE Barcode = '-TB-')
+	INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
+	VALUES (null, null, '-TB-', 4)
+GO
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[Shortcut] WHERE Barcode = '-ES-')
+	INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
+	VALUES (null, null, '-ES-', 5)
+GO
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[Shortcut] WHERE Barcode = '-CR-')
+	INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
+	VALUES (null, null, '-CR-', 6)
+GO
+
 IF NOT EXISTS (SELECT 1 FROM [dbo].[Shortcut] WHERE Barcode = '-AP-')
 	INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
 	VALUES (3, 65, '-AP-', 7)
 ELSE
 	INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
 	VALUES (3, 65, '' , 7)
+GO
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[Shortcut] WHERE Barcode = '-FO-')
+	INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
+	VALUES (3, 70, '-FO-', 1)
+ELSE
+	INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
+	VALUES (3, 70, '' , 1)
+GO
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[Shortcut] WHERE Barcode = '-CL-')
+	INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
+	VALUES (3, 67, '-CL-', 8)
+ELSE
+	INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
+	VALUES (3, 67, '' , 8)
 GO
