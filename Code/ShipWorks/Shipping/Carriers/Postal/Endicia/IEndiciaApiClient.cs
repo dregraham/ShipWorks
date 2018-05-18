a871f04b816ad0a7e1f0faf3b2da5b3657300b9c
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-05-18 10:58:26 -0500

    TP#25097 create IEndiciaApiClient

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Endicia/IEndiciaApiClient.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using ShipWorks.Data.Model.EntityInterfaces;
+using ShipWorks.Shipping.Carriers.Postal.Endicia.WebServices.LabelService;
+
+namespace ShipWorks.Shipping.Carriers.Postal.Endicia
+{
+    /// <summary>
+    /// Represents the Endicia Api Client
+    /// </summary>
+    interface IEndiciaApiClient
+    {
+        /// <summary>
+        /// Generate a scan form for the given shipments
+        /// </summary>
+        SCANResponse GetScanForm(IEndiciaAccountEntity account, IEnumerable<IShipmentEntity> shipments);
+    }
+}
