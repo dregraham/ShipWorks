03d302ab8aeb777b43d08a5cfb536408059de7d2
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-05-21 09:58:18 -0500

    TP#25527 create EndiciaNewAccountCredentials

---
+++ Code/ShipWorks/Shipping/Carriers/Postal/Endicia/Account/EndiciaNewAccountCredentials.cs
@@ -0,0 +1,28 @@
+﻿namespace ShipWorks.Shipping.Carriers.Postal.Endicia.Account
+{
+    /// <summary>
+    /// Data structure for new endicia accounts credentials
+    /// </summary>
+    public struct EndiciaNewAccountCredentials
+    {
+        /// <summary>
+        /// Internet Password
+        /// </summary>
+        public string InternetPassword { get; set; }
+
+        /// <summary>
+        /// Softare Password
+        /// </summary>
+        public string SoftwarePassword { get; set; }
+
+        /// <summary>
+        /// Challenge Question
+        /// </summary>
+        public string ChallengeQuestion { get; set; }
+
+        /// <summary>
+        /// Challenge Answer
+        /// </summary>
+        public string ChallengeAnswer { get; set; }
+    }
+}
