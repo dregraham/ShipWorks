4a894f2c74debdd1271ca97578d4b5eff6d95f17
Josh Winchester <j.winchester@shipworks.com>
2018-06-11 11:08:30 -0500

    TP#25665: Add CommerceV3StoreType

---
+++ Code/ShipWorks.Stores/Platforms/CommerceV3/CommerceV3StoreType.cs
@@ -0,0 +1,38 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.UI;
+using ShipWorks.ApplicationCore.Logging;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Content;
+using ShipWorks.Stores.Platforms.GenericModule;
+
+namespace ShipWorks.Stores.Platforms.CommerceV3
+{
+    [KeyedComponent(typeof(StoreType), StoreTypeCode.CommerceV3)]
+    [Component(RegistrationType.Self)]
+    public class CommerceV3StoreType : GenericModuleStoreType
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public CommerceV3StoreType(StoreEntity store, IMessageHelper messageHelper, IOrderManager orderManager) : 
+            base(store, messageHelper, orderManager)
+        {
+
+        }
+
+        /// <summary>
+        /// StoreTypeCode enum value
+        /// </summary>
+        public override StoreTypeCode TypeCode => StoreTypeCode.CommerceV3;
+
+        /// <summary>
+        /// Log request/responses as CommerceV3
+        /// </summary>
+        public override ApiLogSource LogSource => ApiLogSource.CommerceV3;
+
+        /// <summary>
+        /// Gets the account settings help URL.
+        /// </summary>
+        public override string AccountSettingsHelpUrl => "http://support.shipworks.com/support/solutions/articles/4000024104";
+    }
+}
