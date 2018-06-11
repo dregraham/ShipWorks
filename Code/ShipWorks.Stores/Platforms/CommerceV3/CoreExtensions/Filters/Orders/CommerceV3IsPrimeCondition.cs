a6b8a8297ea6caabe312b141af3ef42bbab8c44f
Josh Winchester <j.winchester@shipworks.com>
2018-06-11 11:08:51 -0500

    TP#25665: Add Amazon filters for CommerceV3

---
+++ Code/ShipWorks.Stores/Platforms/CommerceV3/CoreExtensions/Filters/Orders/CommerceV3IsPrimeCondition.cs
@@ -0,0 +1,12 @@
+﻿using ShipWorks.Filters.Content;
+using ShipWorks.Stores.Platforms.GenericModule.CoreExtensions.Filters.Orders;
+
+namespace ShipWorks.Stores.Platforms.CommerceV3.CoreExtensions.Filters.Orders
+{
+    [ConditionElement("CommerceV3 is Amazon Prime", "CommerceV3.IsPrime")]
+    [ConditionStoreType(StoreTypeCode.CommerceV3)]
+    public class CommerceV3IsPrimeCondition : GenericModuleIsPrimeCondition
+    {
+        
+    }
+}
