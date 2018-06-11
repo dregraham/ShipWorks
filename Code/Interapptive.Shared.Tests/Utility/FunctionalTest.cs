1b80aa04d6415377eef29a139c48030fd53420d2
Andrew Benz <a.benz@shipworks.com>
2018-06-11 13:42:57 -0500

    TP#22541: Add retry method

---
+++ Code/Interapptive.Shared.Tests/Utility/FunctionalTest.cs
@@ -0,0 +1,158 @@
+﻿using Autofac.Extras.Moq;
+using Interapptive.Shared.Utility;
+using log4net.Core;
+using Moq;
+using ShipWorks.Tests.Shared;
+using System;
+using System.Reactive;
+using Xunit;
+using static ShipWorks.Tests.Shared.ExtensionMethods.ParameterShorteners;
+
+namespace Interapptive.Shared.Tests.Utility
+{
+    public class FunctionalTest
+    {
+        private readonly AutoMock mock;
+
+        public FunctionalTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+        }
+
+        [Fact]
+        public void Retry_ReturnsValueOfMethod_WhenMethodSucceeds()
+        {
+            var result = Functional.Retry(() => 99, 2, ex => true);
+
+            Assert.Equal(99, result);
+        }
+
+        [Fact]
+        public void Retry_CallsMethodOnce_WhenMethodSucceeds()
+        {
+            int count = 0;
+
+            Functional.Retry(() => count += 1, 2, ex => true);
+
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public void Retry_CallsMethodTwice_WhenMethodFailsFirstButSucceedsSecondTime()
+        {
+            int count = 0;
+
+            Functional.Retry(() =>
+            {
+                count += 1;
+
+                if (count == 1)
+                {
+                    throw new InvalidOperationException("Failed");
+                }
+
+                return 0;
+            }, 2, ex => true);
+
+            Assert.Equal(2, count);
+        }
+
+        [Fact]
+        public void Retry_DoesNotCallMethodTwice_WhenMethodFailsAndShouldRetryIsFalse()
+        {
+            var count = 0;
+
+            var result = Functional
+                .Retry<Unit>(() =>
+                {
+                    count += 1;
+                    throw new InvalidOperationException("Failed");
+                }
+                , 2, ex => false);
+
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public void Retry_ReturnsFailure_WhenMethodFailsAndShouldRetryIsFalse()
+        {
+            var result = Functional
+                .Retry<Unit>(() => throw new InvalidOperationException("Failed"), 1, ex => false);
+
+            Assert.True(result.Failure);
+            Assert.Equal("Failed", result.Message);
+        }
+
+        [Fact]
+        public void Retry_ReturnsFailureWithError_WhenMethodFails()
+        {
+            int count = 0;
+
+            Functional.Retry<Unit>(() =>
+            {
+                count += 1;
+                throw new InvalidOperationException("Failed");
+            }, 2, ex => false);
+
+            Assert.Equal(1, count);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(20)]
+        public void Retry_CallsMethodMaxNumberOfTimes_WhenMethodFails(int times)
+        {
+            int count = 0;
+
+            Functional.Retry<Unit>(() =>
+            {
+                count += 1;
+                throw new InvalidOperationException("Failed");
+            }, times, ex => true);
+
+            Assert.Equal(times + 1, count);
+        }
+
+        [Fact]
+        public void Retry_DoesNotCallLogger_WhenMethodSucceeds()
+        {
+            var logMock = new Mock<IInternalLogger>();
+
+            Functional.Retry(() => 1, 1, ex => true, logMock.Object.Log);
+
+            logMock.Verify(x => x.Log(It.IsAny<Level>(), AnyString, It.IsAny<object[]>()), Times.Never);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(20)]
+        public void Retry_CallsLoggerWithWarning_ForEachFailure(int times)
+        {
+            var logMock = new Mock<IInternalLogger>();
+
+            Functional.Retry<Unit>(() => throw new InvalidOperationException("Failed"), times, ex => true, logMock.Object.Log);
+
+            for (int i = times; i >= 0; i--)
+            {
+                logMock.Verify(x => x.Log(Level.Warn, "{0} detected while trying to execute.  Retrying {1} more times.", new object[] { "InvalidOperationException", i }));
+            }
+        }
+
+        [Fact]
+        public void Retry_CallsLoggerWithError_AfterLastFailure()
+        {
+            var logMock = new Mock<IInternalLogger>();
+
+            Functional.Retry<Unit>(() => throw new InvalidOperationException("Failed"), 1, ex => true, logMock.Object.Log);
+
+            logMock.Verify(x => x.Log(Level.Error, "Could not execute due to maximum retry failures reached.", new object[] { }));
+        }
+
+        public interface IInternalLogger
+        {
+            Unit Log(Level t1, string t2, object[] t3);
+        }
+    }
+}
--- Code/Interapptive.Shared/Utility/Functional.cs
+++ Code/Interapptive.Shared/Utility/Functional.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using log4net.Core;
+using System;
+using System.Linq;
+using System.Reactive;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Interapptive.Shared.Utility
@@ -8,6 +12,11 @@ namespace Interapptive.Shared.Utility
     /// </summary>
     public static class Functional
     {
+        public static Func<TResult> ToFunc<TResult>(Func<TResult> func) => func;
+        public static Func<T, TResult> ToFunc<T, TResult>(Func<T, TResult> func) => func;
+        public static Func<T1, T2, TResult> ToFunc<T1, T2, TResult>(Func<T1, T2, TResult> func) => func;
+        public static Func<T1, T2, T3, TResult> ToFunc<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> func) => func;
+
         /// <summary>
         /// Call a method with a disposable object
         /// </summary>
@@ -56,14 +65,132 @@ namespace Interapptive.Shared.Utility
         /// </summary>
         /// <param name="arg">String to try and parse</param>
         /// <returns></returns>
-        public static GenericResult<bool> ParseBool(string arg)
+        public static GenericResult<bool> ParseBool(string arg) =>
+            bool.TryParse(arg, out bool result) ?
+                result :
+                GenericResult.FromError<bool>($"Could not parse {arg} as bool");
+
+        /// <summary>
+        /// Retry a function a specified number of times
+        /// </summary>
+        /// <typeparam name="TReturn">Type of return value</typeparam>
+        /// <param name="func">Function to retry</param>
+        /// <param name="retries">Max number of retries</param>
+        /// <param name="shouldRetry">Can the function be retried?</param>
+        public static GenericResult<TReturn> Retry<TReturn>(this Func<TReturn> func, int retries, Func<Exception, bool> shouldRetry) =>
+            Retry(func, retries, shouldRetry, (l, t, p) => Unit.Default);
+
+        /// <summary>
+        /// Retry a function a specified number of times
+        /// </summary>
+        /// <typeparam name="TReturn">Type of return value</typeparam>
+        /// <param name="func">Function to retry</param>
+        /// <param name="retries">Max number of retries</param>
+        /// <param name="shouldRetry">Can the function be retried?</param>
+        /// <param name="logger">Logger to use to on failure</param>
+        public static GenericResult<TReturn> Retry<TReturn>(this Func<TReturn> func, int retries, Func<Exception, bool> shouldRetry, Func<Level, string, object[], Unit> logger)
         {
-            if (bool.TryParse(arg, out bool result))
+            GenericResult<int> retryCounter = retries;
+
+            while (retryCounter.Success)
             {
-                return GenericResult.FromSuccess(result);
+                try
+                {
+                    return func();
+                }
+                catch (Exception ex)
+                {
+                    retryCounter = retryCounter.Bind(counter => HandleException(TimeSpan.FromSeconds(1), ex, counter, shouldRetry, logger));
+                }
             }
 
-            return GenericResult.FromError<bool>($"Could not parse {arg} as bool");
+            return retryCounter.Exception;
         }
+
+
+        ///// <summary>
+        ///// Retry a function a specified number of times
+        ///// </summary>
+        ///// <typeparam name="TReturn">Type of return value</typeparam>
+        ///// <param name="func">Function to retry</param>
+        ///// <param name="retries">Max number of retries</param>
+        ///// <param name="shouldRetry">Can the function be retried?</param>
+        //public static Task<TReturn> RetryAsync<TReturn>(this Func<Task<TReturn>> func, int retries, Func<Exception, bool> shouldRetry) =>
+        //    RetryAsync(func, retries, shouldRetry, (l, t, p) => Unit.Default);
+
+        ///// <summary>
+        ///// Retry a function a specified number of times
+        ///// </summary>
+        ///// <typeparam name="TReturn">Type of return value</typeparam>
+        ///// <param name="func">Function to retry</param>
+        ///// <param name="retries">Max number of retries</param>
+        ///// <param name="shouldRetry">Can the function be retried?</param>
+        ///// <param name="logger">Logger to use to on failure</param>
+        //public static Task<TReturn> RetryAsync<TReturn>(this Func<Task<TReturn>> func, int retries, Func<Exception, bool> shouldRetry, Func<Level, string, object[], Unit> logger)
+        //{
+        //    GenericResult<int> retryCounter = retries;
+
+        //    while (retryCounter.Success)
+        //    {
+        //        try
+        //        {
+        //            return func();
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            retryCounter = retryCounter.Bind(counter => HandleException(TimeSpan.FromSeconds(1), ex, counter, shouldRetry, logger));
+        //        }
+        //    }
+
+        //    return retryCounter.Exception;
+        //}
+
+        /// <summary>
+        /// Handle the exception, if possible
+        /// </summary>
+        private static GenericResult<int> HandleException(TimeSpan retryDelay, Exception ex, int retryCounter, Func<Exception, bool> shouldRetry, Func<Level, string, object[], Unit> logger)
+        {
+            if (shouldRetry(ex))
+            {
+                logger(Level.Warn, "{0} detected while trying to execute.  Retrying {1} more times.", new object[] { ex.GetType().Name, retryCounter });
+
+                if (retryCounter == 0)
+                {
+                    logger(Level.Error, "Could not execute due to maximum retry failures reached.", Enumerable.Empty<object>().ToArray());
+                    return ex;
+                }
+
+                // Wait before trying again, give the other guy some time to resolve itself
+                Thread.Sleep(retryDelay);
+
+                return retryCounter - 1;
+            }
+
+            return ex;
+        }
+
+        ///// <summary>
+        ///// Handle the exception, if possible
+        ///// </summary>
+        //private static async Task<GenericResult<int>> HandleExceptionAsync(TimeSpan retryDelay, Exception ex, int retryCounter, Func<Exception, bool> shouldRetry, Func<Level, string, object[], Unit> logger)
+        //{
+        //    if (shouldRetry(ex))
+        //    {
+        //        logger(Level.Warn, "{0} detected while trying to execute.  Retrying {1} more times.", new object[] { ex.GetType().Name, retryCounter });
+
+        //        if (retryCounter == 0)
+        //        {
+        //            logger(Level.Error, "Could not execute due to maximum retry failures reached.", Enumerable.Empty<object>().ToArray());
+        //            return ex;
+        //        }
+
+        //        // Wait before trying again, give the other guy some time to resolve itself
+        //        await Task.Delay(retryDelay).ConfigureAwait(false);
+
+        //        return retryCounter - 1;
+        //    }
+
+        //    return ex;
+        //}
     }
 }
