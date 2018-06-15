fb224ebf1f8766d02bf3ceeab59342db0a950794
Tim Hughes <t.hughes@shipworks.com>
2018-06-15 14:50:40 -0500

    Fixed schema conflict between master and staging

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.26/5.26.0.0.sql
+++
@@ -1,9 +0,0 @@
-﻿PRINT N'Altering [dbo].[GrouponOrderItem]'
-GO
-ALTER TABLE [dbo].[GrouponOrderItem] ADD
-[PONumber] [nvarchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_GrouponOrderItem_PONumber] DEFAULT ('')
-GO
-PRINT N'Dropping constraints from [dbo].[GrouponOrderItem]'
-GO
-ALTER TABLE [dbo].[GrouponOrderItem] DROP CONSTRAINT [DF_GrouponOrderItem_PONumber]
-GO
---
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.26/5.26.0.1.sql
@@ -0,0 +1,10 @@
+﻿PRINT N'Altering [dbo].[GrouponOrderItem]'
+GO
+IF NOT EXISTS(SELECT 1 FROM sys.columns WHERE Name = N'PONumber' AND Object_ID = Object_ID(N'GrouponOrderItem'))
+BEGIN
+	ALTER TABLE [dbo].[GrouponOrderItem] ADD
+	[PONumber] [nvarchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_GrouponOrderItem_PONumber] DEFAULT ('')
+	
+	ALTER TABLE [dbo].[GrouponOrderItem] DROP CONSTRAINT [DF_GrouponOrderItem_PONumber]
+END
+GO
