c6e546648dc8cafdc0fcfcd118397b06615d55f3
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-06-25 13:27:08 -0500

    TP#26143 do not enable change tracking for asendiashipment and dhlexpressshipment in create schema

--- Code/ShipWorks/Data/Administration/Scripts/Installation/CreateSchema.sql
+++ Code/ShipWorks/Data/Administration/Scripts/Installation/CreateSchema.sql
@@ -2854,9 +2854,6 @@ GO
 CREATE NONCLUSTERED INDEX [IX_DhlExpressShipment_Service] ON [dbo].[DhlExpressShipment] ([Service])
 GO
 
-ALTER TABLE [dbo].[DhlExpressShipment] ENABLE CHANGE_TRACKING
-GO
-
 PRINT N'Creating [dbo].[DhlExpressPackage]'
 GO
 CREATE TABLE [dbo].[DhlExpressPackage](
@@ -2954,8 +2951,6 @@ GO
 PRINT N'Creating index [IX_AsendiaShipment_Service] on [dbo].[AsendiaShipment]'
 GO
 CREATE NONCLUSTERED INDEX [IX_AsendiaShipment_Service] ON [dbo].[AsendiaShipment] ([Service])
-GO
-ALTER TABLE [dbo].[AsendiaShipment] ENABLE CHANGE_TRACKING
 GO
 PRINT N'Creating [dbo].[Customer]'
 GO
