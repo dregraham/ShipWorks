c5d47956b0fc17de8bf496777e65363c14e9b8ca
Chris Hicks <c.hicks@shipworks.com>
2018-06-27 15:11:54 -0500

    TP#26119: Create ShipmentProcessorModule to contain decorator registration

---
+++ Code/ShipWorks.Shipping/Services/ShipmentProcessorSteps/ShipmentProcessorModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+
+namespace ShipWorks.Shipping.Services.ShipmentProcessorSteps
+{
+    public class ShipmentProcessorModule : Module
+    {
+        /// <summary>
+        /// Override to add registrations to the container.
+        /// </summary>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterDecorator<ILabelRetrievalStep>(
+                (c, inner) => new TelemetricLabelRetrievalStep(inner, c.Resolve<ICarrierShipmentAdapterFactory>(), c.Resolve<IShipmentServicesBuilderFactory>()),
+                "LabelRetrievalStep");
+        }
+    }
+}
