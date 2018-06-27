3af409b9b14fd484c38bd16a5b0f97f34ea0d95f
Chris Hicks <c.hicks@shipworks.com>
2018-06-27 13:57:33 -0500

    TP#26119: Create TelemetricResult

---
+++ Code/Interapptive.Shared/Utility/TelemetricResult.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Interapptive.Shared.Utility
+{
+    /// <summary>
+    /// Generic class that can be used to return telemetry data along with a result
+    /// </summary>
+    public struct TelemetricResult<T>
+    {
+        /// <summary>
+        /// Contsructor
+        /// </summary>
+        public TelemetricResult(T value, Dictionary<string, string> telemetry)
+        {
+            Value = value;
+            Telemetry = telemetry;
+        }
+        
+        /// <summary>
+        /// The actual result
+        /// </summary>
+        public T Value { get; }
+        
+        /// <summary>
+        /// The telemetry data associated with the result
+        /// </summary>
+        public Dictionary<string, string> Telemetry { get; }
+    }
+}
