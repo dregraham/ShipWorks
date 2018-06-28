a3d7bc3e62e738764aa19645fe8274b095b4a2f2
Jon Eman <jeman@interapptive.local>
2018-06-28 16:44:55 -0500

    First commit for all Ranorex Test Suites

---
+++ Code/ShipWorks.Ranorex.Tests/LoadTesting/LoadTesting/MouseMoveClass.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by Ranorex
+ * User: jeman
+ * Date: 6/9/2017
+ * Time: 10:29 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace LoadTesting
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public class MoveMouseClass
+	{
+        public void MoveToName(RepoItemInfo rawtextInfoJon, RepoItemInfo rawtextInfoBrett, RepoItemInfo rawtextInfoKatie, RepoItemInfo rawtestInfoMirza)
+        {
+        	
+        	switch(Environment.MachineName)
+        	{
+        	
+        		case "QATesting-PC":
+        		
+            		Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Move item 'rawtextInfo' at Center.", rawtextInfo);
+            		rawtextInfoJon.FindAdapter<RawText>().MoveTo();
+                    break;
+            
+                case "QATesting-PC2":
+                    
+            		Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Move item 'rawtextInfo' at Center.", rawtextInfo);
+            		rawtextInfoBrett.FindAdapter<RawText>().MoveTo();
+            		break;
+            
+            	case "QATesting-PC3":
+            		
+           			Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Move item 'rawtextInfo' at Center.", rawtextInfo);
+           			rawtextInfoKatie.FindAdapter<RawText>().MoveTo();
+           			break;
+           			
+           		default:
+           			
+           			Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Move item 'rawtextInfo' at Center.", rawtextInfo);
+           			rawtextInfoMirza.FindAdapter<RawText>().MoveTo();
+           			break;           
+            
+            
+        	}
+        }
+	}
+}
---
+++ Code/ShipWorks.Ranorex.Tests/SmokeTest/SmokeTest/UserCodeModule2.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Created by Ranorex
+ * User: KGicona
+ * Date: 6/4/2018
+ * Time: 3:35 PM
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+namespace SmokeTest
+{
+    /// <summary>
+    /// Description of UserCodeModule2.
+    /// </summary>
+    [TestModule("AB3F09D0-DAC7-45F4-8AE2-5A43B53122D3", ModuleType.UserCode, 1)]
+    public class UserCodeModule2 : ITestModule
+    {
+        /// <summary>
+        /// Constructs a new instance.
+        /// </summary>
+        public UserCodeModule2()
+        {
+            // Do not delete - a parameterless constructor is required!
+        }
+
+        /// <summary>
+        /// Performs the playback of actions in this module.
+        /// </summary>
+        /// <remarks>You should not call this method directly, instead pass the module
+        /// instance to the <see cref="TestModuleRunner.Run(ITestModule)"/> method
+        /// that will in turn invoke this method.</remarks>
+        void ITestModule.Run()
+        {
+            Mouse.DefaultMoveTime = 300;
+            Keyboard.DefaultKeyPressTime = 100;
+            Delay.SpeedFactor = 1.0;
+        }
+    }
+}
