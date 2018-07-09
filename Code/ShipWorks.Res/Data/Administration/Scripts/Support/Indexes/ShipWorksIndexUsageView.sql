f49e7b18bebc0deea824cf7e5d22906b9ee6e854
Tim Hughes <t.hughes@shipworks.com>
2018-07-09 14:33:28 -0500

    TP#26136: Added Manage index state task to disable unused indexes.

---
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Support/Indexes/ShipWorksIndexUsageView.sql
@@ -0,0 +1,52 @@
+IF EXISTS(SELECT 1 FROM sys.views WHERE name='ShipWorksIndexUsage' and type='v')
+	DROP VIEW IF EXISTS [dbo].[ShipWorksIndexUsage]
+GO
+
+CREATE VIEW ShipWorksIndexUsage  as
+
+	WITH IndexUsage AS
+	(
+		SELECT t.name AS TableName, i.name AS IndexName, i.is_primary_key as IsPrimaryKey, i.is_unique as IsUnique, i.index_id, i.object_id,
+			CASE i.is_disabled
+				WHEN 1 THEN 0
+				ELSE 1
+			END as 'IsEnabled',
+			CASE LEN(COALESCE(constraints.CONSTRAINT_NAME, ''))
+				WHEN 0 THEN 0
+				ELSE 1
+			END as 'HasConstraintColumn',
+			sum(ius.user_seeks) + sum(ius.user_scans) + sum(ius.user_lookups) as 'UserQueries',
+			max(lastQuery.QueryDate) as 'MaxLastQueryDate'
+		FROM sys.dm_db_index_usage_stats ius
+			JOIN sys.tables t ON t.object_id = ius.object_id
+			JOIN sys.indexes i ON   i.object_id = ius.object_id AND i.index_id = ius.index_id 
+			JOIN Sys.Index_Columns ic On i.Object_Id = ic.Object_Id And i.Index_Id = ic.Index_Id
+			JOIN Sys.Columns c on c.Object_Id = ic.Object_Id And c.Column_Id = ic.Column_Id
+			JOIN Sys.Schemas s On t.ScheMa_Id = s.Schema_Id
+			CROSS APPLY
+			(
+				select ius.last_user_seek as 'QueryDate'
+				union  
+				select ius.last_user_lookup
+				union 
+				select ius.last_user_scan
+			) as lastQuery
+			outer APPLY
+			(
+				select A.CONSTRAINT_NAME
+				from INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS b 
+				JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE a ON a.CONSTRAINT_CATALOG = b.CONSTRAINT_CATALOG AND a.CONSTRAINT_NAME = b.CONSTRAINT_NAME 
+				WHERE A.TABLE_NAME = t.name and A.COLUMN_NAME = c.name and ic.is_included_column = 0
+			) as constraints
+		WHERE ius.database_id = db_id() 
+		group by t.name, i.name, I.is_disabled, i.is_primary_key, i.is_unique, constraints.CONSTRAINT_NAME, i.index_id, i.object_id
+	)
+	select *, 
+		CASE IsPrimaryKey
+			WHEN 0 THEN 
+				'IF EXISTS(SELECT * FROM ' + QUOTENAME(TableName) + ')  ALTER INDEX ' + QUOTENAME(IndexName) + ' ON ' + QUOTENAME(TableName) + ' DISABLE'
+			ELSE '' 
+		END as 'DisableIndex',
+		'IF EXISTS(SELECT * FROM ' + QUOTENAME(TableName) + ')  ALTER INDEX ' + QUOTENAME(IndexName) + ' ON ' + QUOTENAME(TableName) + ' REBUILD' AS 'EnableIndex'
+	from IndexUsage
+go
---
+++ Code/ShipWorks/Actions/Tasks/Common/ManageIndexStateTask.cs
@@ -0,0 +1,173 @@
+﻿using Interapptive.Shared.Utility;
+using log4net;
+using ShipWorks.Actions.Triggers;
+using ShipWorks.ApplicationCore.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using ShipWorks.Data.Connection;
+using ShipWorks.Data.Model;
+using System.Reactive.Disposables;
+using System.Xml.Linq;
+using Interapptive.Shared.Extensions;
+
+namespace ShipWorks.Actions.Tasks.Common
+{
+    /// <summary>
+    /// ActionTask that manages database table indexes state (enabled/disabled)..
+    /// </summary>
+    [ActionTask("Manage Index State", "ManageIndexState", ActionTaskCategory.Administration, true)]
+    public class ManageIndexStateTask : ActionTask
+    {
+        private readonly ILog log;
+        private readonly IDateTimeProvider dateTimeProvider;
+        private const int timeoutHours = 3;
+        private readonly int timeoutSeconds = (int) TimeSpan.FromHours(timeoutHours).TotalSeconds;
+        private string sqlConnectionString;
+        private int daysBack = 14;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ManageIndexStateTask" /> class.
+        /// </summary>
+        public ManageIndexStateTask(IDateTimeProvider dateTimeProvider, Func<Type, ILog> logFactory)
+        {
+            this.dateTimeProvider = dateTimeProvider;
+            log = logFactory(typeof(ManageIndexStateTask));
+
+            TimeoutInMinutes = 180;
+        }
+
+        /// <summary>
+        /// Indicates if the task requires input to function.  Such as the contents of a filter, or the item that caused the action.
+        /// </summary>
+        public override ActionTaskInputRequirement InputRequirement
+        {
+            get { return ActionTaskInputRequirement.None; }
+        }
+
+        /// <summary>
+        /// Defines the maximum number of minutes the task can be running.
+        /// </summary>
+        public int TimeoutInMinutes { get; set; }
+
+        /// <summary>
+        /// Gets a type from the specified type string
+        /// </summary>
+        /// <param name="value">Name and namespace of the type to get.</param>
+        /// <returns>The actual type for the string, or null if it can't be found.</returns>
+        /// <remarks>This is overridden so that the ShipWorks assembly is searched.</remarks>
+        protected override Type GetTypeByFullName(string value)
+        {
+            return Type.GetType(value);
+        }
+
+        /// <summary>
+        /// Is the task allowed to be run using the specified trigger type?
+        /// </summary>
+        /// <param name="triggerType">Type of trigger that should be tested</param>
+        /// <returns><c>true</c> when the task can be run with the given trigger; otherwise <c>false</c>.</returns>
+        public override bool IsAllowedForTrigger(ActionTriggerType triggerType)
+        {
+            return triggerType == ActionTriggerType.Scheduled;
+        }
+
+        /// <summary>
+        /// Creates the editor that is used to edit the task.
+        /// </summary>
+        /// <exception cref="System.InvalidOperationException">There is not an editor associated with the task.</exception>
+        public override ActionTaskEditor CreateEditor()
+        {
+            // This task should not appear in the UI
+            throw new InvalidOperationException("There is not an editor associated with the task for managing database index state.");
+        }
+
+        /// <summary>
+        /// Run the index state management script.
+        /// </summary>
+        public override void Run(List<long> inputKeys, ActionStepContext context)
+        {
+            try
+            {
+                ScheduledTrigger scheduledTrigger = new ScheduledTrigger(context.Step.TaskSettings);
+                TimeSpan startTimeOfDay = scheduledTrigger.Schedule.StartDateTimeInUtc.TimeOfDay;
+                DateTime scheduledStart = dateTimeProvider.UtcNow.Date + startTimeOfDay;
+                DateTime scheduledEndTimeInUtc = scheduledStart + TimeSpan.FromMinutes(TimeoutInMinutes);
+
+                XDocument doc = XDocument.Parse(context.Step.TaskSettings);
+                XElement daysBackElement = doc.Element("DaysBack");
+                if (daysBackElement != null)
+                {
+                    int.TryParse(daysBackElement.Value, out daysBack);
+                }
+
+                if (dateTimeProvider.UtcNow < scheduledEndTimeInUtc)
+                {
+                    using (new LoggedStopwatch(log, $"Manage Index State Total Time. Days back: {daysBack}"))
+                    {
+                        DisableUnusedIndexes();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string message = $"An error occurred while running the Manage Index State task. {ex.Message}";
+                log.Error(message, ex);
+
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Disable unused indexes.
+        /// </summary>
+        private void DisableUnusedIndexes()
+        {
+            using (new LoggedStopwatch(log, "Finished disabling unused indexes."))
+            {
+                using (DbConnection sqlConnection = DataAccessAdapter.CreateConnection(ConnectionString))
+                {
+                    sqlConnection.Open();
+
+                    if (sqlConnection is SqlConnection connection)
+                    {
+                        void infoHandler(object sender, SqlInfoMessageEventArgs e) => log.Info(e.Message);
+
+                        connection.FireInfoMessageEventOnUserErrors = true;
+                        connection.InfoMessage += infoHandler;
+
+                        Disposable.Create(() => connection.InfoMessage -= infoHandler);
+                    }
+
+                    using (DbCommand sqlCommand = sqlConnection.CreateCommand())
+                    {
+                        sqlCommand.CommandTimeout = timeoutSeconds;
+                        sqlCommand.CommandText = $"EXECUTE DisableUnusedIndexes @daysBack = {daysBack}";
+                        sqlCommand.CommandType = CommandType.Text;
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a connection string, based on SqlAdapter.Default.ConnectionString, and modifies it to have a new
+        /// number of minutes for the timeout.
+        /// </summary>
+        private string ConnectionString
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(sqlConnectionString))
+                {
+                    SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder(SqlAdapter.Default.ConnectionString);
+                    sqlConnectionStringBuilder.ConnectTimeout = timeoutSeconds;
+                    sqlConnectionString = sqlConnectionStringBuilder.ConnectionString;
+                }
+
+                return sqlConnectionString;
+            }
+        }
+    }
+}
