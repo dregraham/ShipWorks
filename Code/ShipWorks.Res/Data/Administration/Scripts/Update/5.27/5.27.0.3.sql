7406eb68bd5d9eec1e9b0b84b4b00b43f9a1104c
Kevin Croke <k.croke@shipworks.com>
2018-07-09 11:25:35 -0500

    TP#26357: Add update script to add Length Width and Height columns to database

---
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/5.27/5.27.0.3.sql
@@ -0,0 +1,9 @@
+﻿ALTER TABLE dbo.OrderItem ADD [Length] [float] NOT NULL CONSTRAINT DF_OrderItem_Length DEFAULT 0
+ALTER TABLE dbo.OrderItem ADD [Width] [float] NOT NULL CONSTRAINT DF_OrderItem_Width DEFAULT 0
+ALTER TABLE dbo.OrderItem ADD [Height] [float] NOT NULL CONSTRAINT DF_OrderItem_Height DEFAULT 0
+GO 
+
+ALTER TABLE dbo.OrderItem DROP CONSTRAINT DF_OrderItem_Length
+ALTER TABLE dbo.OrderItem DROP CONSTRAINT DF_OrderItem_Width
+ALTER TABLE dbo.OrderItem DROP CONSTRAINT DF_OrderItem_Height  
+GO
