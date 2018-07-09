0a4bae0c7950fffe706a043058e8ec3657714c27
Chris Hicks <c.hicks@shipworks.com>
2018-07-09 10:59:27 -0500

    TP#26357: Create OrderItemHeightCondition, OrderItemLengthCondition, and OrderItemWidthCondition

---
+++ Code/ShipWorks/Filters/Content/Conditions/OrderItems/OrderItemLengthCondition.cs
@@ -0,0 +1,20 @@
+﻿using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content.SqlGeneration;
+
+namespace ShipWorks.Filters.Content.Conditions.OrderItems
+{
+    /// <summary>
+    /// Condition that compares against the Length of an OrderItem
+    /// </summary>
+    [ConditionElement("Item Length", "OrderItem.Length")]
+    public class OrderItemLengthCondition : StringCondition
+    {
+        /// <summary>
+        /// Generate the sql
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            return GenerateSql(context.GetColumnReference(OrderItemFields.Length), context);
+        }
+    }
+}
---
+++ Code/ShipWorks/Filters/Content/Conditions/OrderItems/OrderItemWidthCondition.cs
@@ -0,0 +1,20 @@
+﻿using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Filters.Content.SqlGeneration;
+
+namespace ShipWorks.Filters.Content.Conditions.OrderItems
+{
+    /// <summary>
+    /// Condition that compares against the Width of an OrderItem
+    /// </summary>
+    [ConditionElement("Item Width", "OrderItem.Width")]
+    public class OrderItemWidthCondition : StringCondition
+    {
+        /// <summary>
+        /// Generate the sql
+        /// </summary>
+        public override string GenerateSql(SqlGenerationContext context)
+        {
+            return GenerateSql(context.GetColumnReference(OrderItemFields.Width), context);
+        }
+    }
+}
