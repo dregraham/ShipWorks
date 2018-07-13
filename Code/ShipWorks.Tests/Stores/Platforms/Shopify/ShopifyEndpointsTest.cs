967a459cc73e94a7b9dd64ddae4840930874c987
Josh Winchester <j.winchester@shipworks.com>
2018-07-13 10:54:13 -0500

    TP#26434: Add code review changes to ShopifyEndpointTest

--- Code/ShipWorks.Tests/Stores/Platforms/Shopify/ShopifyEndpointsTest.cs
+++ Code/ShipWorks.Tests/Stores/Platforms/Shopify/ShopifyEndpointsTest.cs
@@ -1,29 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using Autofac.Extras.Moq;
+﻿using System.Collections.Generic;
 using ShipWorks.Stores.Platforms.Shopify;
-using ShipWorks.Tests.Shared;
 using Xunit;
 
 namespace ShipWorks.Tests.Stores.Shopify
 {
     public class ShopifyEndpointsTest
     {
-        private ShopifyEndpoints endpoints = new ShopifyEndpoints("test");
-        private IEnumerable<long> InventoryItemIDs;
-
         [Fact]
         public void Inventoryurl_ReturnsCorrect()
         {
-            List<long> list = new List<long>();
-            list.Add(123);
-            list.Add(456);
-            list.Add(789);
-
-            InventoryItemIDs = list;
-
-            string url = endpoints.InventoryLevelUrl(InventoryItemIDs);
+            ShopifyEndpoints endpoints = new ShopifyEndpoints("test");
+            List<long> list = new List<long> {123, 456, 789};
+            string url = endpoints.InventoryLevelUrl(list);
 
             Assert.Equal("https://test.myshopify.com/admin/inventory_levels.json?inventory_item_ids=123,456,789", url);
         }
