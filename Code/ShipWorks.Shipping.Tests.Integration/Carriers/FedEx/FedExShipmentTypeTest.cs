10badf0a531495d57c8545c5e1bc1656359372b4
Josh Winchester <j.winchester@shipworks.com>
2018-07-24 16:12:13 -0500

    TP#26453: Update FedExShipmentTypeTest with code review changes

--- Code/ShipWorks.Shipping.Tests.Integration/Carriers/FedEx/FedExShipmentTypeTest.cs
+++ Code/ShipWorks.Shipping.Tests.Integration/Carriers/FedEx/FedExShipmentTypeTest.cs
@@ -152,7 +152,7 @@ namespace ShipWorks.Shipping.Tests.Integration.Carriers.FedEx
 
             string url = testObject.GetCarrierTrackingUrl(shipment);
 
-            Assert.Equal("http://mailviewrecipient.fedex.com/recip_package_summary.aspx?PostalID=" + shipment.TrackingNumber, url);
+            Assert.Equal("http://mailviewrecipient.fedex.com/recip_package_summary.aspx?PostalID=123456", url);
         }
 
         public void Dispose() => context.Dispose();
