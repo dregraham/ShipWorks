eac6acecd93d5d9ddc77a927add455112acc7d51
Tim Hughes <t.hughes@shipworks.com>
2018-07-26 15:24:43 -0500

    TP#26118: Update purging print results to support hard deletes.

--- Code/ShipWorks.SqlServer/Purge/PurgePrintResultProcedure.cs
+++ Code/ShipWorks.SqlServer/Purge/PurgePrintResultProcedure.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Data;
-using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using Microsoft.SqlServer.Server;
-
-using ShipWorks.SqlServer.Common.Data;
-using ShipWorks.SqlServer;
 using ShipWorks.SqlServer.Purge;
 
 public partial class StoredProcedures
@@ -47,27 +41,30 @@ public partial class StoredProcedures
 
                 -- Create a dummy resource record for non-thermal print jobs 
                 IF NOT EXISTS (SELECT 1 FROM [Resource] WHERE Filename = '__purged_print_html.swr') 
-                  BEGIN 
-                      INSERT INTO [Resource] ([Data], [Checksum], [Compressed], [Filename]) VALUES (0x1C, 0x1C, 0, '__purged_print_html.swr'); 
+                    BEGIN 
+                        INSERT INTO [Resource] ([Data], [Checksum], [Compressed], [Filename]) VALUES (0x1C, 0x1C, 0, '__purged_print_html.swr'); 
 
-                      SET @htmlResourceID = @@IDENTITY 
-                  END 
+                        SET @htmlResourceID = @@IDENTITY 
+                    END 
                 ELSE 
-                  BEGIN 
-                      SELECT @htmlResourceID = ResourceID FROM [Resource] WHERE [Filename] = '__purged_print_html.swr'
-                  END 
+                    BEGIN 
+                        SELECT @htmlResourceID = ResourceID FROM [Resource] WHERE [Filename] = '__purged_print_html.swr'
+                    END 
 
                 -- Create dummy resource record for Thermal 
                 IF NOT EXISTS (SELECT 1 FROM [Resource] WHERE [Filename] = '__purged_print_thermal.swr') 
-                  BEGIN 
-                      INSERT INTO [Resource] ([Data], [Checksum], [Compressed], [Filename]) VALUES (0x1D, 0x1D, 0, '__purged_print_thermal.swr'); 
+                    BEGIN 
+                        INSERT INTO [Resource] ([Data], [Checksum], [Compressed], [Filename]) VALUES (0x1D, 0x1D, 0, '__purged_print_thermal.swr'); 
 
-                      SET @thermalResourceID = @@IDENTITY 
-                  END 
+                        SET @thermalResourceID = @@IDENTITY 
+                    END 
                 ELSE 
-                  BEGIN 
-                      SELECT @thermalResourceID = ResourceID FROM [Resource] WHERE [Filename] = '__purged_print_thermal.swr' 
-                  END 
+                    BEGIN 
+                        SELECT @thermalResourceID = ResourceID FROM [Resource] WHERE [Filename] = '__purged_print_thermal.swr' 
+                    END 
+
+	            IF OBJECT_ID('tempdb.dbo.#PrintJobWorking', 'U') IS NOT NULL
+		            DROP TABLE #PrintJobWorking; 
 
                 -- Create temp tables 
                 CREATE TABLE #PrintJobWorking 
@@ -82,37 +79,26 @@ public partial class StoredProcedures
 	                SELECT 
 		                PrintResult.PrintResultID, 
 		                PrintResult.ContentResourceID, 
-		                CASE 
-			                PrintResult.TemplateType 
-			                WHEN 3 
-				                THEN 1 
-			                ELSE 
-				                0 
+		                CASE PrintResult.TemplateType 
+			                 WHEN 3 THEN 1 
+			                 ELSE 0 
 		                END 
-	                FROM   
-		                PrintResult 
-	                INNER JOIN 
-		                ObjectReference 
-	                ON 
-		                ObjectReference.ObjectReferenceID = PrintResult.ContentResourceID 
-	                WHERE  
-		                PrintResult.PrintDate < @olderThan 
-		                   AND ObjectReference.ObjectID NOT IN ( @htmlResourceID, @thermalResourceID ) 
-
-	                CREATE INDEX IX_PrintJobWorking ON #PrintJobWorking (PrintResultID) 
-
+	                FROM PrintResult INNER JOIN ObjectReference ON ObjectReference.ObjectReferenceID = PrintResult.ContentResourceID
+	                WHERE PrintResult.PrintDate < @olderThan 
+		              AND ObjectReference.ObjectID NOT IN ( @htmlResourceID, @thermalResourceID ) 
+		              
                 -- See if there's any actual work to do 
                 IF EXISTS (SELECT 1 FROM #PrintJobWorking) 
                 BEGIN 
-                      DECLARE @CurrentBatch TABLE 
-                      ( 
+                        DECLARE @CurrentBatch TABLE 
+                        ( 
                         PrintResultID     BIGINT, 
                         ContentResourceID BIGINT, 
                         IsThermal         BIT 
-                      ) 
+                        ) 
 
-                      WHILE (@runUntil IS NULL OR @runUntil > GETUTCDATE())
-                      BEGIN 
+                        WHILE (@runUntil IS NULL OR @runUntil > GETUTCDATE())
+                        BEGIN 
                             INSERT INTO @CurrentBatch 
 				                SELECT TOP (@batchSize) 
 					                PrintResultID, 
@@ -123,54 +109,51 @@ public partial class StoredProcedures
                             SET @batchSize = @@ROWCOUNT; 
 
                             IF @batchSize = 0 
-                              BREAK; 
+                                BREAK; 
 
                             DECLARE @TotalSeconds INT = DATEDIFF(second, @startTime, GETUTCDATE()) + 1; 
 
                             -- stop if the batch isn't expected to complete in time 
                             IF ( @runUntil IS NOT NULL AND @batchTotal > 0 AND DATEADD(second, @TotalSeconds * @batchSize / @batchTotal, GETUTCDATE()) > @runUntil )
-                              BREAK; 
+                                BREAK; 
 
                             -- Wrap edits in transaction 
                             BEGIN TRANSACTION 
+							
+								IF (@softDelete IS NULL OR @softDelete = 0)
+								BEGIN  
+									-- Update ObjectReference refrerenced by PrintResult.ContentResourceID so they point to the generic, purged resource ID 
+									UPDATE objRef 
+									SET    objRef.ObjectID = 
+												CASE batch.IsThermal 
+													WHEN 1 THEN @thermalResourceID 
+													ELSE @htmlResourceID 
+												END, 
+											objRef.ReferenceKey = 
+												CASE batch.IsThermal 
+													WHEN 1 THEN '#' + convert(VARCHAR, @thermalResourceID)
+													ELSE '#' + convert(VARCHAR, @htmlResourceID)
+												END 
+									FROM @CurrentBatch batch 
+										INNER JOIN ObjectReference objRef ON objRef.objectreferenceid = batch.ContentResourceID 
+								END
+								ELSE
+								BEGIN
+									DELETE ObjectReference
+									FROM ObjectReference
+										INNER JOIN @CurrentBatch batch ON batch.ContentResourceID = ObjectReference.ObjectReferenceID
+								END
 
-                            -- Update ObjectReference refrerenced by PrintResult.ContentResourceID so they point to the generic, purged resource ID 
-                            UPDATE objRef 
-                            SET    objRef.ObjectID = 
-                                        CASE batch.IsThermal 
-                                            WHEN 1 THEN @thermalResourceID 
-                                            ELSE @htmlResourceID 
-                                        END, 
-                                   objRef.ReferenceKey = 
-                                        CASE batch.IsThermal 
-                                            WHEN 1 THEN '#' + convert(VARCHAR, @thermalResourceID)
-                                            ELSE '#' + convert(VARCHAR, @htmlResourceID)
-                                        END 
-                            FROM   
-                                @CurrentBatch batch 
-                            INNER JOIN 
-                                ObjectReference objRef 
-                            ON 
-                                objRef.objectreferenceid = batch.ContentResourceID 
+								-- Delete ObjectReference not explicitly pointed at by PrintResult (and therefore updated to point to @htmlResourceID
+								DELETE ObjectReference 
+								FROM ObjectReference 
+									INNER JOIN @CurrentBatch batch ON batch.PrintResultID = ObjectReference.ConsumerID
+										AND batch.ContentResourceID != ObjectReference.ObjectReferenceID
 
-                            -- Delete ObjectReference not explicitly pointed at by PrintResult (and therefore updated to point to @htmlResourceID
-                            DELETE ObjectReference 
-                            FROM   
-                                ObjectReference 
-                            INNER JOIN 
-                                @CurrentBatch batch 
-                            ON 
-                                batch.PrintResultID = ObjectReference.ConsumerID
-                                AND batch.ContentResourceID != ObjectReference.ObjectReferenceID
-
-                            --Delete items in the batch from the #PrintJobWorking table 
-                            DELETE printJobWorking 
-                            FROM   
-                                #PrintJobWorking printJobWorking 
-                            INNER JOIN 
-                                @CurrentBatch AS batch 
-                            ON 
-                                batch.PrintResultID = PrintJobWorking.PrintResultID 
+								--Delete items in the batch from the #PrintJobWorking table 
+								DELETE printJobWorking 
+								FROM #PrintJobWorking printJobWorking 
+									INNER JOIN @CurrentBatch AS batch ON batch.PrintResultID = PrintJobWorking.PrintResultID 
 
                             COMMIT TRANSACTION 
 
@@ -180,8 +163,10 @@ public partial class StoredProcedures
                             -- update batch total and adjust batch size to an amount expected to complete in 10 seconds 
                             SET @batchTotal = @batchTotal + @batchSize; 
                             SET @batchSize = @batchTotal / @TotalSeconds * 10; 
-                      END 
+                        END 
                 END 
+
+	            DROP TABLE #PrintJobWorking; 
             ";
         }
     }
