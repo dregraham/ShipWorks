bdc1311dff385a73e0f8014dc881eb3ec260465d
Tim Hughes <t.hughes@shipworks.com>
2018-07-27 06:56:22 -0500

    TP#26118: Optimized purging audits.

--- Code/ShipWorks.SqlServer/Purge/PurgeAuditProcedure.cs
+++ Code/ShipWorks.SqlServer/Purge/PurgeAuditProcedure.cs
@@ -34,71 +34,70 @@ public partial class StoredProcedures
         get
         {
             return @"
-                SET NOCOUNT ON;
+    SET NOCOUNT ON;
+		    
+    IF OBJECT_ID('tempdb.dbo.#AuditPurgeBatch', 'U') IS NOT NULL
+        DROP TABLE #AuditPurgeBatch; 
 
-                -- create batch purge ID table
-                CREATE TABLE #AuditPurgeBatch (
-                    AuditID BIGINT PRIMARY KEY
-                );
+    -- create batch purge ID table
+    CREATE TABLE #AuditPurgeBatch (
+        AuditID BIGINT
+    );
 
-                DECLARE
-                    @startTime DATETIME = GETUTCDATE(),
-                    @batchSize INT = 1000,
-                    @batchTotal BIGINT = 0;
+    DECLARE
+        @startTime DATETIME = GETUTCDATE(),
+        @batchSize INT = 1000,
+        @batchTotal BIGINT = 0;
 
-                -- purge in batches while time allows
-                WHILE @runUntil IS NULL OR GETUTCDATE() < @runUntil
-                BEGIN
-                    INSERT #AuditPurgeBatch
-                    SELECT TOP (@batchSize) AuditID
-                    FROM Audit
-                    WHERE [Date] < @olderThan;
+    -- purge in batches while time allows
+    WHILE @runUntil IS NULL OR GETUTCDATE() < @runUntil
+    BEGIN
+        INSERT #AuditPurgeBatch
+        SELECT TOP (@batchSize) AuditID
+        FROM [Audit]
+        WHERE [Date] < @olderThan;
 
-                    SET @batchSize = @@ROWCOUNT;
-                    IF @batchSize = 0
-                        BREAK;
+        SET @batchSize = @@ROWCOUNT;
+        IF @batchSize = 0
+            BREAK;
 
-                    DECLARE @totalSeconds INT = DATEDIFF(SECOND, @startTime, GETUTCDATE()) + 1;
+        DECLARE @totalSeconds INT = DATEDIFF(SECOND, @startTime, GETUTCDATE()) + 1;
 
-                    -- stop if the batch isn't expected to complete in time
-                    IF (
-                        @runUntil IS NOT NULL AND
-                        @batchTotal > 0 AND
-                        DATEADD(SECOND, @totalSeconds * @batchSize / @batchTotal, GETUTCDATE()) > @runUntil
-                    )
-                        BREAK;
+        -- stop if the batch isn't expected to complete in time
+        IF (
+            @runUntil IS NOT NULL AND
+            @batchTotal > 0 AND
+            DATEADD(SECOND, @totalSeconds * @batchSize / @batchTotal, GETUTCDATE()) > @runUntil
+        )
+            BREAK;
 
-                    BEGIN TRANSACTION;
+        BEGIN TRANSACTION;
 
-                    DELETE AuditChangeDetail
-                    FROM AuditChangeDetail acd
-                    INNER JOIN AuditChange ac ON
-                        ac.AuditChangeID = acd.AuditChangeID
-                    INNER JOIN #AuditPurgeBatch b ON
-                        b.AuditID = ac.AuditID;
+		    DELETE AuditChangeDetail
+		    FROM AuditChangeDetail acd
+			    INNER JOIN #AuditPurgeBatch b ON acd.AuditID = b.AuditID;
 
-                    DELETE ObjectLabel
-                    FROM AuditChange ac INNER JOIN #AuditPurgeBatch b ON b.AuditID = ac.AuditID
-					INNER JOIN ObjectLabel ol ON ac.ObjectID = ol.ObjectID AND ol.IsDeleted = 1
+		    DELETE ObjectLabel
+		    FROM AuditChange ac 
+			    INNER JOIN #AuditPurgeBatch b ON b.AuditID = ac.AuditID
+			    INNER JOIN ObjectLabel ol ON ac.ObjectID = ol.ObjectID AND ol.IsDeleted = 1
 
-                    DELETE AuditChange
-                    FROM AuditChange ac
-                    INNER JOIN #AuditPurgeBatch b ON
-                        b.AuditID = ac.AuditID;
+		    DELETE AuditChange
+		    FROM AuditChange ac
+			    INNER JOIN #AuditPurgeBatch b ON b.AuditID = ac.AuditID;
 
-                    DELETE Audit
-                    FROM Audit a
-                    INNER JOIN #AuditPurgeBatch b ON
-                        b.AuditID = a.AuditID;
+		    DELETE [Audit]
+		    FROM [Audit] a
+			    INNER JOIN #AuditPurgeBatch b ON b.AuditID = a.AuditID;
+			    
+        TRUNCATE TABLE #AuditPurgeBatch;
 
-                    COMMIT TRANSACTION;
+        COMMIT TRANSACTION;
 
-                    TRUNCATE TABLE #AuditPurgeBatch;
-
-                    -- update batch total and adjust batch size to an amount expected to complete in 10 seconds
-                    SET @batchTotal = @batchTotal + @batchSize;
-                    SET @batchSize = @batchTotal / @totalSeconds * 10;
-                END;
+        -- update batch total and adjust batch size to an amount expected to complete in 10 seconds
+        SET @batchTotal = @batchTotal + @batchSize;
+        SET @batchSize = @batchTotal / @totalSeconds * 10;
+    END;
             ";
         }
     }
