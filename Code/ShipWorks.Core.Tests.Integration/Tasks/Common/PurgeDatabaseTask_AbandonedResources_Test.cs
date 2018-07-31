eaf13f9abf4dc633ff2f5a4968238e8466180789
Andrew Benz <a.benz@shipworks.com>
2018-07-31 12:58:15 -0500

    TP#18306: Add abanded resources purge test

---
+++ Code/ShipWorks.Core.Tests.Integration/Tasks/Common/PurgeDatabaseTask_AbandonedResources_Test.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Interapptive.Shared.UI;
+using SD.LLBLGen.Pro.QuerySpec;
+using ShipWorks.Actions.Tasks.Common;
+using ShipWorks.Data.Connection;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Data.Model.FactoryClasses;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Startup;
+using ShipWorks.Tests.Shared;
+using ShipWorks.Tests.Shared.Database;
+using ShipWorks.Tests.Shared.EntityBuilders;
+using Xunit;
+
+namespace ShipWorks.Core.Tests.Integration.Tasks.Common
+{
+    [Collection("Database collection")]
+    [Trait("Category", "ContinuousIntegration")]
+    public class PurgeDatabaseTask_AbandonedResources_Test
+    {
+        private readonly DataContext context;
+        private readonly PurgeDatabaseTask testObject;
+        private readonly Random random = new Random();
+        private long nextConsumerID = 0;
+
+        public PurgeDatabaseTask_AbandonedResources_Test(DatabaseFixture db)
+        {
+            context = db.CreateDataContext(x => ContainerInitializer.Initialize(x), mock =>
+            {
+                mock.Override<IMessageHelper>();
+            });
+
+            testObject = context.Mock.Create<PurgeDatabaseTask>();
+            testObject.Purges.Add(PurgeDatabaseType.Downloads); // This will auto-include abandoned resources since we can't select it directly
+        }
+
+        [Fact]
+        public async Task PurgeDatabaseTask_PurgesTwoResources_WhenHalfOfResourcesAreAbandoned()
+        {
+            var resource1 = CreateValidResource();
+            var resource2 = CreateValidResource();
+            var resource3 = CreateAbandonedResource();
+            var resource4 = CreateAbandonedResource();
+
+            testObject.Run(new List<long>(), null);
+
+            AssertResourceExists(resource1);
+            AssertResourceExists(resource2);
+            AssertResourceDoesNotExists(resource3);
+            AssertResourceDoesNotExists(resource4);
+        }
+
+        [Fact]
+        public async Task PurgeDatabaseTask_PurgesAllResources_WhenAllResourcesAreAbandoned()
+        {
+            var resource1 = CreateAbandonedResource();
+            var resource2 = CreateAbandonedResource();
+            var resource3 = CreateAbandonedResource();
+            var resource4 = CreateAbandonedResource();
+
+            testObject.Run(new List<long>(), null);
+
+            AssertResourceDoesNotExists(resource1);
+            AssertResourceDoesNotExists(resource2);
+            AssertResourceDoesNotExists(resource3);
+            AssertResourceDoesNotExists(resource4);
+        }
+
+        [Fact]
+        public async Task PurgeDatabaseTask_PurgesNoAbandonedResources_WhenNoAbandonedResourcesAreOlderThan180Days()
+        {
+            var resource1 = CreateValidResource();
+            var resource2 = CreateValidResource();
+            var resource3 = CreateValidResource();
+            var resource4 = CreateValidResource();
+
+            testObject.Run(new List<long>(), null);
+
+            AssertResourceExists(resource1);
+            AssertResourceExists(resource2);
+            AssertResourceExists(resource3);
+            AssertResourceExists(resource4);
+        }
+
+        private static void AssertResourceExists(ResourceEntity resource)
+        {
+            using (var sqlAdapter = SqlAdapter.Create(false))
+            {
+                var factory = new QueryFactory();
+
+                var resourceQuery = factory.Resource.Where(ResourceFields.ResourceID == resource.ResourceID);
+                var entity = sqlAdapter.FetchFirst(resourceQuery);
+                Assert.NotNull(entity);
+            }
+        }
+
+        private static void AssertResourceDoesNotExists(ResourceEntity resource)
+        {
+            using (var sqlAdapter = SqlAdapter.Create(false))
+            {
+                var factory = new QueryFactory();
+
+                var resourceQuery = factory.Resource.Where(ResourceFields.ResourceID == resource.ResourceID);
+                var entity = sqlAdapter.FetchFirst(resourceQuery);
+                Assert.Null(entity);
+            }
+        }
+
+        private ResourceEntity CreateAbandonedResource() =>
+            Create.Entity<ResourceEntity>()
+                .Set(x => x.Checksum = CreateChecksum())
+                .Set(x => x.Filename, Path.GetRandomFileName())
+                .Save();
+
+        private ResourceEntity CreateValidResource()
+        {
+            nextConsumerID += 1;
+            var resource = CreateAbandonedResource();
+
+            Create.Entity<ObjectReferenceEntity>()
+                .Set(x => x.EntityID, resource.ResourceID)
+                .Set(x => x.ConsumerID, nextConsumerID)
+                .Save();
+
+            return resource;
+        }
+
+        private byte[] CreateChecksum() =>
+            Enumerable.Range(1, 30).Select(_ => (byte) random.Next(0, 255)).ToArray();
+    }
+}
---
+++ Code/ShipWorks.Core.Tests.Integration/Tasks/Common/PurgeDatabaseTask_Downloads_Test.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Interapptive.Shared.UI;
+using SD.LLBLGen.Pro.QuerySpec;
+using ShipWorks.Actions.Tasks.Common;
+using ShipWorks.Data.Connection;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Data.Model.FactoryClasses;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Startup;
+using ShipWorks.Tests.Shared;
+using ShipWorks.Tests.Shared.Database;
+using ShipWorks.Tests.Shared.EntityBuilders;
+using Xunit;
+
+namespace ShipWorks.Core.Tests.Integration.Tasks.Common
+{
+    [Collection("Database collection")]
+    [Trait("Category", "ContinuousIntegration")]
+    public class PurgeDatabaseTask_Downloads_Test
+    {
+        private const int RetentionPeriodInDays = 180;
+        private readonly DataContext context;
+        private readonly DateTime cutoffDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(RetentionPeriodInDays));
+        private readonly PurgeDatabaseTask testObject;
+
+        public PurgeDatabaseTask_Downloads_Test(DatabaseFixture db)
+        {
+            context = db.CreateDataContext(x => ContainerInitializer.Initialize(x), mock =>
+            {
+                mock.Override<IMessageHelper>();
+            });
+
+            testObject = context.Mock.Create<PurgeDatabaseTask>();
+
+            testObject.Purges.Add(PurgeDatabaseType.Downloads);
+            testObject.RetentionPeriodInDays = RetentionPeriodInDays;
+        }
+
+        [Fact]
+        public async Task PurgeDatabaseTask_PurgesTwoDownloads_WhenHalfOfDownloadsAreOlderThan180Days()
+        {
+            var download1 = CreateDownload(DateTime.UtcNow);
+            var download2 = CreateDownload(cutoffDate.AddDays(1));
+            var download3 = CreateDownload(cutoffDate.AddDays(-1));
+            var download4 = CreateDownload(cutoffDate.AddDays(-1000));
+
+            testObject.Run(new List<long>(), null);
+
+            AssertDownloadExists(download1);
+            AssertDownloadExists(download2);
+            AssertDownloadDoesNotExists(download3);
+            AssertDownloadDoesNotExists(download4);
+        }
+
+        [Fact]
+        public async Task PurgeDatabaseTask_PurgesAllDownloads_WhenAllDownloadsAreOlderThan180Days()
+        {
+            var download1 = CreateDownload(cutoffDate.AddDays(-1));
+            var download2 = CreateDownload(cutoffDate.AddDays(-2));
+            var download3 = CreateDownload(cutoffDate.AddDays(-3));
+            var download4 = CreateDownload(cutoffDate.AddDays(-1000));
+
+            testObject.Run(new List<long>(), null);
+
+            AssertDownloadDoesNotExists(download1);
+            AssertDownloadDoesNotExists(download2);
+            AssertDownloadDoesNotExists(download3);
+            AssertDownloadDoesNotExists(download4);
+        }
+
+        [Fact]
+        public async Task PurgeDatabaseTask_PurgesNoDownloads_WhenNoDownloadsAreOlderThan180Days()
+        {
+            var download1 = CreateDownload(cutoffDate.AddDays(1));
+            var download2 = CreateDownload(cutoffDate.AddDays(2));
+            var download3 = CreateDownload(cutoffDate.AddDays(3));
+            var download4 = CreateDownload(cutoffDate.AddDays(1000));
+
+            testObject.Run(new List<long>(), null);
+
+            AssertDownloadExists(download1);
+            AssertDownloadExists(download2);
+            AssertDownloadExists(download3);
+            AssertDownloadExists(download4);
+        }
+
+        private static void AssertDownloadExists(DownloadEntity download)
+        {
+            using (var sqlAdapter = SqlAdapter.Create(false))
+            {
+                var factory = new QueryFactory();
+
+                var downloadQuery = factory.Download.Where(DownloadFields.DownloadID == download.DownloadID);
+                var entity = sqlAdapter.FetchFirst(downloadQuery);
+                Assert.NotNull(entity);
+
+                var detailQuery = factory.DownloadDetail.Where(DownloadDetailFields.DownloadID == download.DownloadID);
+                var detailEntity = sqlAdapter.FetchFirst(detailQuery);
+                Assert.NotNull(detailEntity);
+            }
+        }
+
+        private static void AssertDownloadDoesNotExists(DownloadEntity download)
+        {
+            using (var sqlAdapter = SqlAdapter.Create(false))
+            {
+                var factory = new QueryFactory();
+
+                var downloadQuery = factory.Download.Where(DownloadFields.DownloadID == download.DownloadID);
+                var entity = sqlAdapter.FetchFirst(downloadQuery);
+                Assert.Null(entity);
+
+                var detailQuery = factory.DownloadDetail.Where(DownloadDetailFields.DownloadID == download.DownloadID);
+                var detailEntity = sqlAdapter.FetchFirst(detailQuery);
+                Assert.Null(detailEntity);
+            }
+        }
+
+        private DownloadEntity CreateDownload(DateTime dateTime)
+        {
+            var download = Create.Entity<DownloadEntity>()
+                .Set(x => x.ComputerID, context.Computer.ComputerID)
+                .Set(x => x.UserID, context.User.UserID)
+                .Set(x => x.StoreID, context.Store.StoreID)
+                .Set(x => x.Started, dateTime)
+                .Save();
+
+            Create.Entity<DownloadDetailEntity>()
+                .Set(x => x.DownloadID, download.DownloadID)
+                .Save();
+
+            return download;
+        }
+    }
+}
