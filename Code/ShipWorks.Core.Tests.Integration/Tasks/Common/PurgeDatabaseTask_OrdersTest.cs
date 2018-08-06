3eb892450bcf94275a97c36b3047d69490900770
Tim Hughes <t.hughes@shipworks.com>
2018-08-06 10:37:50 -0500

    TP#18306: Added purge orders integration test.

--- Code/ShipWorks.Core.Tests.Integration/Tasks/Common/PurgeDatabaseTask_OrdersTest.cs
+++ Code/ShipWorks.Core.Tests.Integration/Tasks/Common/PurgeDatabaseTask_OrdersTest.cs
@@ -20,19 +20,21 @@ using ShipWorks.Startup;
 using ShipWorks.Stores;
 using ShipWorks.Tests.Shared;
 using ShipWorks.Tests.Shared.Database;
+using ShipWorks.Tests.Shared.EntityBuilders;
 using Xunit;
 
 namespace ShipWorks.Core.Tests.Integration.Tasks.Common
 {
     [Collection("Database collection")]
     [Trait("Category", "ContinuousIntegration")]
-    public class PurgeDatabaseTaskTest
+    public class PurgeDatabaseTask_OrdersTest
     {
         private readonly DataContext context;
         private const int RetentionPeriodInDays = 30;
-        private readonly List<TableData> tableDataList = new List<TableData>();
+        private readonly List<PurgeTableData> tableDataList = new List<PurgeTableData>();
+        private ConfigurationEntity config = new ConfigurationEntity(true);
 
-        public PurgeDatabaseTaskTest(DatabaseFixture db)
+        public PurgeDatabaseTask_OrdersTest(DatabaseFixture db)
         {
             context = db.CreateDataContext(x => ContainerInitializer.Initialize(x), mock =>
             {
@@ -42,122 +44,63 @@ namespace ShipWorks.Core.Tests.Integration.Tasks.Common
             // Delete the default order and customer
             using (ISqlAdapter sqlAdapter = context.Mock.Container.Resolve<ISqlAdapterFactory>().Create())
             {
+                sqlAdapter.FetchEntity(config);
                 sqlAdapter.DeleteEntity(context.Order);
                 sqlAdapter.DeleteEntity(context.Order.Customer);
             }
+        }
 
+        private void CreateOrders()
+        {
             BulkOrderCreator bulkOrderCreator = new BulkOrderCreator(context);
 
             bulkOrderCreator.StoreTypeCodes = EnumHelper.GetEnumList<StoreTypeCode>()
                 .Select(e => e.Value)
-                .Where(stc => stc != StoreTypeCode.Invalid)
-                .Take(2); // TODO: Remove this Take later...just here for speediness of testing during dev.
+                .Where(stc => stc != StoreTypeCode.Invalid);
 
             bulkOrderCreator.ShipmentTypeCodes = EnumHelper.GetEnumList<ShipmentTypeCode>()
                 .Select(e => e.Value)
-                .Where(stc => stc != ShipmentTypeCode.None)
-                .Take(2); // TODO: Remove this Take later...just here for speediness of testing during dev.
+                .Where(stc => stc != ShipmentTypeCode.None);
 
             bulkOrderCreator.OrderDates = new List<DateTime>
-                {
-                    DateTime.Now.AddDays(-(RetentionPeriodInDays + 1)),
-                    DateTime.Now.AddDays(-(RetentionPeriodInDays + 2)),
-                    DateTime.Now.AddDays(-1)
-                };
-
+            {
+                DateTime.Now.AddDays(-(RetentionPeriodInDays + 1)),
+                DateTime.Now.AddDays(-(RetentionPeriodInDays + 2)),
+                DateTime.Now.AddDays(-1)
+            };
+            
             bulkOrderCreator.CreateOrderForAllStores();
 
             LoadInitialTableData();
         }
 
-        [Fact]
-        public async Task PurgeDatabaseTask_PurgesAllEmailContent_WhenPurgeHistoryTrue()
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task PurgeDatabaseTask_PurgesOrdersAllOrders(bool auditingEnabled)
         {
-            PurgeDatabaseTask testObject = new PurgeDatabaseTask(new SqlPurgeScriptRunner(), new DateTimeProvider());
+            Modify.Entity(config).Set(c => c.AuditEnabled = auditingEnabled).Save();
 
-            testObject.Purges.Add(PurgeDatabaseType.Email);
+            CreateOrders();
 
-            testObject.PurgeEmailHistory = true;
-            testObject.PurgePrintJobHistory = false;
-
-            testObject.RetentionPeriodInDays = -1;
-            testObject.Run(new List<long>(), null);
-
-            UpdateTableDataAfterPurge();
-
-            ValidateEmails(testObject);
-
-            // Now validate the others didn't change.
-            ValidatePrintResults(testObject);
-            ValidateDownloads(testObject);
-            ValidateAudits(testObject);
-        }
-
-        [Fact]
-        public async Task PurgeDatabaseTask_PurgesAllDownloads()
-        {
-            PurgeDatabaseTask testObject = new PurgeDatabaseTask(new SqlPurgeScriptRunner(), new DateTimeProvider());
-
-            testObject.Purges.Add(PurgeDatabaseType.Downloads);
-
-            testObject.PurgeEmailHistory = false;
-            testObject.PurgePrintJobHistory = false;
-
-            testObject.RetentionPeriodInDays = -1;
-            testObject.Run(new List<long>(), null);
-
-            UpdateTableDataAfterPurge();
-
-            ValidateDownloads(testObject);
-
-            // Now validate the others didn't change.
-            ValidatePrintResults(testObject);
-            ValidateEmails(testObject);
-            ValidateAudits(testObject);
-        }
-
-        [Fact]
-        public async Task PurgeDatabaseTask_PurgesAllAudits()
-        {
-            PurgeDatabaseTask testObject = new PurgeDatabaseTask(new SqlPurgeScriptRunner(), new DateTimeProvider());
-
-            testObject.Purges.Add(PurgeDatabaseType.Audit);
-
-            testObject.PurgeEmailHistory = false;
-            testObject.PurgePrintJobHistory = false;
-
-            testObject.RetentionPeriodInDays = -1;
-            testObject.Run(new List<long>(), null);
-
-            UpdateTableDataAfterPurge();
-
-            ValidateAudits(testObject);
-
-            // Now validate the others didn't change.
-            ValidatePrintResults(testObject);
-            ValidateEmails(testObject);
-            ValidateDownloads(testObject);
-        }
-
-        [Fact]
-        public async Task PurgeDatabaseTask_PurgesOrdersAllOrders()
-        {
             // Get the total number of orders
             long startingNumberOfOrders = 0;
             var queryFactory = new QueryFactory();
             var countOrderQuery = queryFactory.Order.Select(OrderFields.OrderID.CountBig());
+
             using (ISqlAdapter sqlAdapter = context.Mock.Container.Resolve<ISqlAdapterFactory>().Create())
             {
                 startingNumberOfOrders = sqlAdapter.FetchScalar<long>(countOrderQuery);
             }
+
             Assert.NotEqual(0, startingNumberOfOrders);
 
             PurgeDatabaseTask testObject = new PurgeDatabaseTask(new SqlPurgeScriptRunner(), new DateTimeProvider());
 
             testObject.Purges.Add(PurgeDatabaseType.Orders);
 
-            testObject.PurgeEmailHistory = true;
-            testObject.PurgePrintJobHistory = true;
+            testObject.PurgeEmailHistory = false;
+            testObject.PurgePrintJobHistory = false;
 
             testObject.RetentionPeriodInDays = -1;
             testObject.Run(new List<long>(), null);
@@ -167,9 +110,7 @@ namespace ShipWorks.Core.Tests.Integration.Tasks.Common
             ValidateEmails(testObject);
             ValidatePrintResults(testObject);
             ValidateDownloads(testObject);
-            
-            // TODO: When auditing is enabled, the deletion of orders (and other entities) will be audited, making counts difficult to check.  Do we care?
-            //ValidateAudits(testObject);
+            ValidateAudits(testObject);
 
             long endingNumberOfOrders = 0;
             using (ISqlAdapter sqlAdapter = context.Mock.Container.Resolve<ISqlAdapterFactory>().Create())
@@ -251,69 +192,56 @@ namespace ShipWorks.Core.Tests.Integration.Tasks.Common
             }
             else
             {
-                if (testObject.PurgePrintJobHistory)
-                {
-                    Assert.Equal(0, printResultTables.Sum(t => t.AfterPurgeRowCount));
-                }
-                else
-                {
-                    Assert.True(printResultTables.Sum(t => t.InitialRowCount - t.AfterPurgeRowCount) == 0);
-                }
+                Assert.Equal(0, printResultTables.Sum(t => t.AfterPurgeRowCount));
             }
         }
 
         private void LoadInitialTableData()
         {
             var queryFactory = new QueryFactory();
+            tableDataList.Add(new PurgeTableData(nameof(ResourceEntity), FetchDataTable(queryFactory.Resource, ResourceFields.ResourceID > 0), null));
+            tableDataList.Add(new PurgeTableData(nameof(DownloadEntity), FetchDataTable(queryFactory.Download, DownloadFields.DownloadID > 1), null));
+            tableDataList.Add(new PurgeTableData(nameof(DownloadDetailEntity), FetchDataTable(queryFactory.DownloadDetail, DownloadDetailFields.DownloadedDetailID > 0), null));
+            tableDataList.Add(new PurgeTableData(nameof(EmailOutboundEntity), FetchDataTable(queryFactory.EmailOutbound, EmailOutboundFields.EmailOutboundID > 0), null));
+            tableDataList.Add(new PurgeTableData(nameof(EmailOutboundRelationEntity), FetchDataTable(queryFactory.EmailOutboundRelation, EmailOutboundRelationFields.EmailOutboundRelationID > 0), null));
+            tableDataList.Add(new PurgeTableData(nameof(ObjectReferenceEntity), FetchDataTable(queryFactory.ObjectReference, ObjectReferenceFields.ObjectReferenceID > 0), null));
+            tableDataList.Add(new PurgeTableData(nameof(ObjectLabelEntity), FetchDataTable(queryFactory.ObjectLabel, ObjectLabelFields.EntityID > 0 & ObjectLabelFields.ObjectType != 5), null));
+            tableDataList.Add(new PurgeTableData(nameof(PrintResultEntity), FetchDataTable(queryFactory.PrintResult, PrintResultFields.PrintResultID > 1), null));
+            tableDataList.Add(new PurgeTableData(nameof(OrderEntity), FetchDataTable(queryFactory.Order, OrderFields.OrderID > 1), null));
+            tableDataList.Add(new PurgeTableData(nameof(ShipmentEntity), FetchDataTable(queryFactory.Shipment, ShipmentFields.ShipmentID > 1), null));
 
-            tableDataList.Add(new TableData(nameof(ResourceEntity), FetchDataTable(queryFactory.Resource, ResourceFields.ResourceID > 0), null));
-
-            tableDataList.Add(new TableData(nameof(DownloadEntity), FetchDataTable(queryFactory.Download, DownloadFields.DownloadID > 1), null));
-
-            tableDataList.Add(new TableData(nameof(DownloadDetailEntity), FetchDataTable(queryFactory.DownloadDetail, DownloadDetailFields.DownloadedDetailID > 0), null));
-
-            tableDataList.Add(new TableData(nameof(EmailOutboundEntity), FetchDataTable(queryFactory.EmailOutbound, EmailOutboundFields.EmailOutboundID > 0), null));
-
-            tableDataList.Add(new TableData(nameof(EmailOutboundRelationEntity), FetchDataTable(queryFactory.EmailOutboundRelation, EmailOutboundRelationFields.EmailOutboundRelationID > 0), null));
-
-            tableDataList.Add(new TableData(nameof(ObjectReferenceEntity), FetchDataTable(queryFactory.ObjectReference, ObjectReferenceFields.ObjectReferenceID > 0), null));
-
-            tableDataList.Add(new TableData(nameof(ObjectLabelEntity), FetchDataTable(queryFactory.ObjectLabel, ObjectLabelFields.EntityID > 0 & ObjectLabelFields.ObjectType != 5), null));
-
-            tableDataList.Add(new TableData(nameof(PrintResultEntity), FetchDataTable(queryFactory.PrintResult, PrintResultFields.PrintResultID > 1), null));
-
-            tableDataList.Add(new TableData(nameof(AuditChangeEntity), FetchDataTable(queryFactory.AuditChange, AuditChangeFields.AuditID > 1), null));
-
-            tableDataList.Add(new TableData(nameof(OrderEntity), FetchDataTable(queryFactory.Order, OrderFields.OrderID > 1), null));
-
-            tableDataList.Add(new TableData(nameof(ShipmentEntity), FetchDataTable(queryFactory.Shipment, ShipmentFields.ShipmentID > 1), null));
+            if (config.AuditEnabled)
+            {
+                tableDataList.Add(new PurgeTableData(nameof(AuditChangeEntity), FetchDataTable(queryFactory.AuditChange, AuditChangeFields.AuditID > 1 & AuditChangeFields.ChangeType != 3), null));
+            }
+            else
+            {
+                tableDataList.Add(new PurgeTableData(nameof(AuditChangeEntity), FetchDataTable(queryFactory.AuditChange, AuditChangeFields.AuditID > 1), null));
+            }
         }
 
         private void UpdateTableDataAfterPurge()
         {
             var queryFactory = new QueryFactory();
-
             tableDataList.First(t => t.TableName == nameof(ResourceEntity)).SetAfterPurgeTable(FetchDataTable(queryFactory.Resource, ResourceFields.ResourceID > 0));
-
             tableDataList.First(t => t.TableName == nameof(DownloadDetailEntity)).SetAfterPurgeTable(FetchDataTable(queryFactory.DownloadDetail, DownloadDetailFields.DownloadedDetailID > 0));
-
             tableDataList.First(t => t.TableName == nameof(EmailOutboundEntity)).SetAfterPurgeTable(FetchDataTable(queryFactory.EmailOutbound, EmailOutboundFields.EmailOutboundID > 0));
-
             tableDataList.First(t => t.TableName == nameof(ObjectReferenceEntity)).SetAfterPurgeTable(FetchDataTable(queryFactory.ObjectReference, ObjectReferenceFields.ObjectReferenceID > 0));
-
             tableDataList.First(t => t.TableName == nameof(EmailOutboundRelationEntity)).SetAfterPurgeTable(FetchDataTable(queryFactory.EmailOutboundRelation, EmailOutboundRelationFields.EmailOutboundRelationID > 0));
-
             tableDataList.First(t => t.TableName == nameof(ObjectLabelEntity)).SetAfterPurgeTable(FetchDataTable(queryFactory.ObjectLabel, ObjectLabelFields.EntityID > 0 & ObjectLabelFields.ObjectType != 5));
-
             tableDataList.First(t => t.TableName == nameof(DownloadEntity)).SetAfterPurgeTable(FetchDataTable(queryFactory.Download, DownloadFields.DownloadID > 1));
-
             tableDataList.First(t => t.TableName == nameof(PrintResultEntity)).SetAfterPurgeTable(FetchDataTable(queryFactory.PrintResult, PrintResultFields.PrintResultID > 1));
-
-            tableDataList.First(t => t.TableName == nameof(AuditChangeEntity)).SetAfterPurgeTable(FetchDataTable(queryFactory.AuditChange, AuditChangeFields.AuditID > 1));
-
             tableDataList.First(t => t.TableName == nameof(OrderEntity)).SetAfterPurgeTable(FetchDataTable(queryFactory.Order, OrderFields.OrderID > 1));
-
             tableDataList.First(t => t.TableName == nameof(ShipmentEntity)).SetAfterPurgeTable(FetchDataTable(queryFactory.Shipment, ShipmentFields.ShipmentID > 1));
+
+            if (config.AuditEnabled)
+            {
+                tableDataList.First(t => t.TableName == nameof(AuditChangeEntity)).SetAfterPurgeTable(FetchDataTable(queryFactory.AuditChange, AuditChangeFields.AuditID > 1 & AuditChangeFields.ChangeType != 3));
+            }
+            else
+            {
+                tableDataList.First(t => t.TableName == nameof(AuditChangeEntity)).SetAfterPurgeTable(FetchDataTable(queryFactory.AuditChange, AuditChangeFields.AuditID > 1));
+            }
         }
 
         private long GetCounts<T>(EntityQuery<T> entityQuery, IPredicate predicate, IEntityFieldCore countBigField) where T : IEntityCore
@@ -345,37 +273,37 @@ namespace ShipWorks.Core.Tests.Integration.Tasks.Common
             }
         }
 
-        /// <summary>
-        /// Helper class for loading/updating/holding table info
-        /// </summary>
-        private class TableData
-        {
-            public string TableName;
-            public long InitialRowCount = 0;
-            public long AfterPurgeRowCount = 0;
-            public DataTable InitialTable = null;
-            public DataTable AfterPurgeTable = null;
+        ///// <summary>
+        ///// Helper class for loading/updating/holding table info
+        ///// </summary>
+        //private class TableData
+        //{
+        //    public string TableName;
+        //    public long InitialRowCount = 0;
+        //    public long AfterPurgeRowCount = 0;
+        //    public DataTable InitialTable = null;
+        //    public DataTable AfterPurgeTable = null;
 
-            public TableData(string tableName, DataTable initialTable, DataTable afterPurgeTable)
-            {
-                TableName = tableName;
-                InitialTable = initialTable;
-                AfterPurgeTable = afterPurgeTable;
-                InitialRowCount = InitialTable?.Rows.Count ?? 0;
-                AfterPurgeRowCount = AfterPurgeTable?.Rows.Count ?? 0;
-            }
+        //    public TableData(string tableName, DataTable initialTable, DataTable afterPurgeTable)
+        //    {
+        //        TableName = tableName;
+        //        InitialTable = initialTable;
+        //        AfterPurgeTable = afterPurgeTable;
+        //        InitialRowCount = InitialTable?.Rows.Count ?? 0;
+        //        AfterPurgeRowCount = AfterPurgeTable?.Rows.Count ?? 0;
+        //    }
 
-            public void SetAfterPurgeTable(DataTable after)
-            {
-                if (after != null)
-                {
-                    AfterPurgeTable = after;
-                    AfterPurgeRowCount = AfterPurgeTable.Rows.Count;
-                }
-            }
+        //    public void SetAfterPurgeTable(DataTable after)
+        //    {
+        //        if (after != null)
+        //        {
+        //            AfterPurgeTable = after;
+        //            AfterPurgeRowCount = AfterPurgeTable.Rows.Count;
+        //        }
+        //    }
 
-            public bool AreTablesEqual() => InitialTable.Rows.Cast<DataRow>()
-                                                .Intersect(AfterPurgeTable.Rows.Cast<DataRow>(), DataRowComparer.Default).Count() == AfterPurgeRowCount;
-        }
+        //    public bool AreTablesEqual() => InitialTable.Rows.Cast<DataRow>()
+        //                                        .Intersect(AfterPurgeTable.Rows.Cast<DataRow>(), DataRowComparer.Default).Count() == AfterPurgeRowCount;
+        //}
     }
 }
