e260527c7b3cb7b4a4cc69d891affcafb9fdc8b2
Jon Eman <j.eman@shipworks.com>
2018-08-08 16:08:37 -0500

    Added the pdf labels, pdf compare report, and diffpdfc output file, along with a message explaining everything, into the completion email

--- Code/ShipWorks.Ranorex.Tests/SmokeTest/SmokeTest/SendInfoEmail.UserCode.cs
+++ Code/ShipWorks.Ranorex.Tests/SmokeTest/SmokeTest/SendInfoEmail.UserCode.cs
@@ -15,6 +15,7 @@ using System.Drawing;
 using System.Threading;
 using WinForms = System.Windows.Forms;
 using System.Net.Mail;
+using System.IO;
 
 using Ranorex;
 using Ranorex.Core;
@@ -44,7 +45,20 @@ namespace SmokeTest
             
             
             if (Environment.MachineName != "V-QA-AVG-CUSTOM" && Environment.MachineName != "V-QA-BEEFCAKE1" && Environment.MachineName != "KGICONA-4WX3JH2" && Environment.MachineName != "BERGER-PC" && Environment.MachineName != "MADKE-PC" && Environment.MachineName != "JEMAN-PC")
-                {
+                
+
+            
+            	{
+            	string labelDirectory = "C:\\printpdflabels\\" + FileName.pdfFolder + "\\";
+            	string reportDirectory = "C:\\printpdflabels\\" + FileName.pdfFolder + "\\ComparisonReport\\";
+            	using(Ionic.Zip.ZipFile sourcelabelszip = new Ionic.Zip.ZipFile("labels.zip"))
+        			{
+            		sourcelabelszip.AddDirectory(labelDirectory);
+        			}
+            	using(Ionic.Zip.ZipFile sourcelabelszip = new Ionic.Zip.ZipFile("reports.zip"))
+        			{
+            		sourcelabelszip.AddDirectory(labelDirectory);
+        			}
             try
             {
                 MailMessage mail = new MailMessage();
@@ -56,12 +70,66 @@ namespace SmokeTest
                 mail.To.Add("b.berger@shipworks.com");
                 mail.To.Add("s.madke@shipworks.com");
                 mail.Subject = "Test Mail";
-                mail.Body = "The smoketest was ran on a PC other than the average customer VM or the beefcake VM. It was ran on " + gdt.localDate 
+                mail.Body = "The smoketest was ran on: " + Environment.MachineName 
+                			+ ". + It was ran on: " + gdt.localDate
                 			+ ". The version of ShipWorks it was ran on was: " + SWVersion.SWVersionNumber 
                 			+ ". The PDF label comparison reports are located: " + gcrd.ComparsionDirectory
                 			+ ". The pass/fail report is on " + Environment.MachineName
                 			+ ", and the file is located here: \"" + Ranorex.Core.Reporting.TestReport.ReportEnvironment.ReportViewFilePath + "\"";
-   
+                
+                if(Directory.Exists(labelDirectory))
+                {
+                	if(!Directory.EnumerateFileSystemEntries(labelDirectory).IsEmpty())
+                   	{
+                		if(File.Exists(@"C:\Program Files (x86)\diffpdfc\diffpdfc.exe"))
+                		{
+                		   	if(Directory.Exists(reportDirectory))
+                		   	{
+                		   		if(!Directory.EnumerateFileSystemEntries(reportDirectory).IsEmpty())
+                		   		{
+                		   			if(File.Exists("diffpdfcoutput.txt"))
+                		   			   {
+                		   			   	mail.Attachments.Add(new Attachment("labels.zip"));
+										mail.Attachments.Add(new Attachment("reports.zip"));
+										mail.Attachments.Add(new Attachment("diffpdfcoutput.txt"));
+										mail.Body = mail.Body + "Attached are the labels that the smoketest produced, the comparison reports that show the difference between those labels and the known good labels. If there are any issues, please review the attached diffpdfcoutput.txt for errors that were produced during the automated comparison process.";
+                		   			   }
+                		   			else
+                		   			{
+                		   				mail.Attachments.Add(new Attachment("labels.zip"));
+										mail.Attachments.Add(new Attachment("reports.zip"));
+										mail.Body = mail.Body + "Attached are the labels that the smoketest produced, the comparison reports that show the difference between those labels and the known good labels. There was no output from the automated comparison process.";
+
+                		   			}
+                		   		}
+                		   	}
+                		   	if((!Directory.Exists(reportDirectory)) | (Directory.EnumerateFileSystemEntries(reportDirectory).IsEmpty()))
+                		   	{
+                		 	  	if(File.Exists("diffpdfcoutput.txt"))
+                		 	  	{
+                		 	  		mail.Attachments.Add(new Attachment("labels.zip"));
+									mail.Attachments.Add(new Attachment("diffpdfcoutput.txt"));
+									mail.Body = mail.Body + "Attached are the labels that the smoketest produced. The comparison reports (pdfs that show the difference between those labels and the known good labels) were not produced. Please review the attached diffpdfcoutput.txt for errors that were produced during the automated comparison process.";
+                		 	  	}
+                		 	  	else
+                		 	  	{
+                		 	  		mail.Attachments.Add(new Attachment("labels.zip"));
+									mail.Body = mail.Body + "Attached are the labels that the smoketest produced. The comparison reports (pdfs that show the difference between those labels and the known good labels) were not produced. Diffpdfc (which was installed on the computer the smoketest was ran on) did not produce any output. Please review the ranorex logs to see what happened.";
+
+                		 	  	}
+                			}
+                		}
+                		else
+                		{
+							mail.Attachments.Add(new Attachment("labels.zip"));
+							mail.Body = mail.Body + "Attached are the labels that the smoketest produced. Diffpdfc was not installed on the computer the smoketest was ran on, please manually verify that the labels printed correctly.";
+	               		}
+                   	}
+                 }
+                if((!Directory.Exists(labelDirectory)) | (Directory.EnumerateFileSystemEntries(labelDirectory).IsEmpty()))
+                {
+              	 	 mail.Body = mail.Body + "No shipping labels were created during this run of the smoketest. If this was not expected, please check all ranorex and shipworks logs to understand why this occurred.";
+	            }
                 SmtpServer.Port = 587;
                 SmtpServer.Credentials = new System.Net.NetworkCredential("ShipWorksQA@gmail.com", "katieisanerd");
                 SmtpServer.EnableSsl = true;
