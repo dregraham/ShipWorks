129e548b7cc08b7cb70b5ddf223bd4697da58690
Chris Hicks <c.hicks@shipworks.com>
2018-08-15 15:22:26 -0500

    TP#26870: Try again after any web exception

--- Code/ShipWorks.Stores/Platforms/ChannelAdvisor/ChannelAdvisorRestClient.cs
+++ Code/ShipWorks.Stores/Platforms/ChannelAdvisor/ChannelAdvisorRestClient.cs
@@ -233,14 +233,7 @@ namespace ShipWorks.Stores.Platforms.ChannelAdvisor
         /// </summary>
         private static bool ShouldRetryRequest(Exception ex)
         {
-            WebException webException = ((ex as ChannelAdvisorException)?.InnerException as WebException);
-            HttpStatusCode? statusCode = (webException?.Response as HttpWebResponse)?.StatusCode;
-
-            return webException?.Status == WebExceptionStatus.Timeout ||
-                   statusCode == HttpStatusCode.RequestTimeout ||
-                   statusCode == HttpStatusCode.GatewayTimeout ||
-                   statusCode == HttpStatusCode.InternalServerError ||
-                   statusCode == HttpStatusCode.ServiceUnavailable;
+            return (ex as ChannelAdvisorException)?.InnerException is WebException;
         }
 
         /// <summary>
