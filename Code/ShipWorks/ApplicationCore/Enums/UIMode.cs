7b2d13d1af35a72bcb76b5b52e2b21b2e8a82a6d
Kevin Croke <k.croke@shipworks.com>
2018-09-04 14:31:23 -0500

    TP#26926: Add UIMode

---
+++ Code/ShipWorks/ApplicationCore/Enums/UIMode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.ApplicationCore.Enums
+{
+    /// <summary>
+    /// Shipworks UI Mode
+    /// </summary>
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum UIMode
+    {
+        [Description("Batch Mode")]
+        Batch = 0,
+        
+        [Description("Order Lookup Mode")]
+        OrderLookup = 1
+    }
+}
