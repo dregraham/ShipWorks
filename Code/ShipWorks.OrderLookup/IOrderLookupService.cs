94ce788e0d64cbe5c6e61f7faae788e5a9af68c8
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-09-11 10:25:27 -0500

    TP#26930 add comments to interface

--- Code/ShipWorks.OrderLookup/IOrderLookupService.cs
+++ Code/ShipWorks.OrderLookup/IOrderLookupService.cs
@@ -3,8 +3,14 @@ using ShipWorks.Data.Model.EntityClasses;
 
 namespace ShipWorks.OrderLookup
 {
+    /// <summary>
+    /// Represents a service for finding orders based on scan message text
+    /// </summary>
     public interface IOrderLookupService
     {
+        /// <summary>
+        /// Find an order based on scanned message text
+        /// </summary>
         Task<OrderEntity> FindOrder(string scanMsgScannedText);
     }
 }
