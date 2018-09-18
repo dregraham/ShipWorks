5075788b6d9abe4236662187bd95cbac84852efc
Kevin Croke <k.croke@shipworks.com>
2018-09-18 16:14:32 -0500

    TP#26961 Stub out OrderLookup from and to controls

---
+++ Code/ShipWorks.OrderLookup/Controls/OrderLookupFromViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interapptive.Shared.ComponentRegistration;
+
+namespace ShipWorks.OrderLookup.Controls
+{
+    [Component(RegistrationType.Self)]
+    public class OrderLookupFromViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Code/ShipWorks.OrderLookup/Controls/OrderLookupToViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interapptive.Shared.ComponentRegistration;
+
+namespace ShipWorks.OrderLookup.Controls
+{
+    [Component(RegistrationType.Self)]
+    public class OrderLookupToViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
