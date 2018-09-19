860811314f19f5fee6b946c1978e7501c67b0497
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-09-19 10:47:25 -0500

    TP#26961 WIP continue implementing OrderLookup To and From controls

--- Code/ShipWorks.OrderLookup/Controls/OrderLookupToViewModel.cs
+++ Code/ShipWorks.OrderLookup/Controls/OrderLookupToViewModel.cs
@@ -5,12 +5,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Core.UI;
 
 namespace ShipWorks.OrderLookup.Controls
 {
     [Component(RegistrationType.Self)]
     public class OrderLookupToViewModel : INotifyPropertyChanged
     {
+        private readonly IOrderLookupMessageBus messageBus;
+        private readonly PropertyChangedHandler handler;
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public OrderLookupToViewModel(IOrderLookupMessageBus messageBus)
+        {
+            this.messageBus = messageBus;
+            handler = new PropertyChangedHandler(this, () => PropertyChanged);
+        }
     }
 }
