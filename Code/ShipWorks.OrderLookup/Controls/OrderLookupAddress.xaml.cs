1edbf18f862bb28e8284c702234c533367c8f476
Kevin Croke <k.croke@shipworks.com>
2018-09-27 08:16:18 -0500

    TP#26961: Style From and To

---
+++ Code/ShipWorks.OrderLookup/Controls/OrderLookupAddress.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ShipWorks.OrderLookup.Controls
+{
+    /// <summary>
+    /// Interaction logic for AddressControl.xaml
+    /// </summary>
+    public partial class OrderLookupAddress : UserControl
+    {
+        public OrderLookupAddress()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
