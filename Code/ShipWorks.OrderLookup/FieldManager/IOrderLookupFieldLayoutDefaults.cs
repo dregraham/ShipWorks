16ff0d269e38d243bdb7332f20998ec6beffa27a
Josh Winchester <j.winchester@shipworks.com>
2018-10-02 14:41:05 -0500

    TP#26937: Add Comments to IOrderLookupFieldLayoutDefaults

--- Code/ShipWorks.OrderLookup/FieldManager/IOrderLookupFieldLayoutDefaults.cs
+++ Code/ShipWorks.OrderLookup/FieldManager/IOrderLookupFieldLayoutDefaults.cs
@@ -2,8 +2,14 @@
 
 namespace ShipWorks.OrderLookup.FieldManager
 {
+    /// <summary>
+    /// Interface for retrieving default lookup field layouts
+    /// </summary>
     public interface IOrderLookupFieldLayoutDefaults
     {
+        /// <summary>
+        /// Return the default layouts.
+        /// </summary>
         IEnumerable<SectionLayout> GetDefaults();
     }
 }
