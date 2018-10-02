4a45573accf219b6abc9a9947262a8a6dfbd7657
Josh Winchester <j.winchester@shipworks.com>
2018-10-02 14:43:51 -0500

    TP#26937: Update SectionFieldLayout to remove row

--- Code/ShipWorks.OrderLookup/FieldManager/SectionFieldLayout.cs
+++ Code/ShipWorks.OrderLookup/FieldManager/SectionFieldLayout.cs
@@ -28,34 +28,3 @@ namespace ShipWorks.OrderLookup.FieldManager
         /// </summary>
         [Description("Is this field selected")]
         public bool Selected { get; set; } = true;
-
-        /// <summary>
-        /// Row that this field should be in
-        /// </summary>
-        [Description("Row that this field should be in")]
-        public int Row { get; set; } = 0;
-
-        /// <summary>
-        /// Copy the given SectionFieldLayout to this instance.
-        /// </summary>
-        public void Copy(SectionFieldLayout toCopy)
-        {
-            if (!toCopy.Id.Equals(Id, StringComparison.CurrentCultureIgnoreCase))
-            {
-                throw new InvalidOperationException("Copying SectionFieldLayouts with different Ids is not supported.");
-            }
-
-            Name = toCopy.Name;
-            Row = toCopy.Row;
-            Selected = toCopy.Selected;
-        }
-
-        /// <summary>
-        /// Create a clone of this SectionFieldLayout
-        /// </summary>
-        public SectionFieldLayout Clone()
-        {
-            return (SectionFieldLayout) MemberwiseClone();
-        }
-    }
-}
