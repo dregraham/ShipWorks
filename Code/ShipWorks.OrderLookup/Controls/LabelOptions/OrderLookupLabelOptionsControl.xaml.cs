8a56882fb52bb4cded5371c0c54691557f6113f3
Chris Hicks <c.hicks@shipworks.com>
2018-10-04 15:11:26 -0600

    TP#26961: Create unit tests for IntToEnumConverter. Remove garbage file that was accidentally committed. Add comments.

--- Code/ShipWorks.OrderLookup/Controls/LabelOptions/OrderLookupLabelOptionsControl.xaml.cs
+++ Code/ShipWorks.OrderLookup/Controls/LabelOptions/OrderLookupLabelOptionsControl.xaml.cs
@@ -1,25 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace ShipWorks.OrderLookup.Controls.LabelOptions
+﻿namespace ShipWorks.OrderLookup.Controls.LabelOptions
 {
     /// <summary>
     /// Interaction logic for OrderLookupLabelOptionsControl.xaml
     /// </summary>
-    public partial class OrderLookupLabelOptionsControl : UserControl
+    public partial class OrderLookupLabelOptionsControl
     {
+        /// <summary>
+        /// Ctor
+        /// </summary>
         public OrderLookupLabelOptionsControl()
         {
             InitializeComponent();
