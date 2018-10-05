1af16d0201f4ff79d9f356bb005b38b20e9e3669
Andrew Benz <a.benz@shipworks.com>
2018-10-05 12:16:08 -0500

    TP#27560: Wire up the SCAN form button on the order lookup mode

--- Code/ShipWorks/ApplicationCore/MainForm.Designer.cs
+++ Code/ShipWorks/ApplicationCore/MainForm.Designer.cs
@@ -226,10 +226,15 @@ namespace ShipWorks
             this.buttonEndiciaSCAN = new Divelements.SandRibbon.Button();
             this.popupPostalScanForm = new Divelements.SandRibbon.Popup();
             this.menuEndiciaScanForm = new Divelements.SandRibbon.Menu();
+            this.menuOrderLookupViewEndiciaScanForm = new Divelements.SandRibbon.Menu();
             this.menuCreateEndiciaScanForm = new Divelements.SandRibbon.MenuItem();
+            this.menuOrderLookupViewCreateEndiciaScanForm = new Divelements.SandRibbon.MenuItem();
             this.menuPrintScanForm = new Divelements.SandRibbon.MenuItem();
+            this.menuOrderLookupViewPrintScanForm = new Divelements.SandRibbon.MenuItem();
             this.menuPrintEndiciaScanForm = new Divelements.SandRibbon.Menu();
+            this.menuOrderLookupViewPrintEndiciaScanForm = new Divelements.SandRibbon.Menu();
             this.menuEndiciaScanFormNone = new Divelements.SandRibbon.MenuItem();
+            this.menuOrderLookupViewEndiciaScanFormNone = new Divelements.SandRibbon.MenuItem();
             this.ribbonChunkManageEmail = new Divelements.SandRibbon.RibbonChunk();
             this.buttonEmailMessages = new Divelements.SandRibbon.Button();
             this.ribbonChunkDownload = new Divelements.SandRibbon.RibbonChunk();
@@ -253,7 +258,6 @@ namespace ShipWorks
             this.ribbonChunkOrderLookupViewActions = new Divelements.SandRibbon.RibbonChunk();
             this.buttonOrderLookupViewVoid = new Divelements.SandRibbon.Button();
             this.buttonOrderLookupViewShipAgain = new Divelements.SandRibbon.Button();
-            this.buttonOrderLookupViewReturn = new Divelements.SandRibbon.Button();
             this.buttonOrderLookupViewSCANForm = new Divelements.SandRibbon.Button();
             this.popupOrderLookupViewSCANForm = new Divelements.SandRibbon.Popup();
             this.ribbonTabAdmin = new Divelements.SandRibbon.RibbonTab();
@@ -2575,7 +2579,6 @@ namespace ShipWorks
             this.ribbonChunkOrderLookupViewActions.Items.AddRange(new Divelements.SandRibbon.WidgetBase[] {
             this.buttonOrderLookupViewVoid,
             this.buttonOrderLookupViewShipAgain,
-            this.buttonOrderLookupViewReturn,
             this.buttonOrderLookupViewSCANForm});
             this.ribbonChunkOrderLookupViewActions.Text = "Actions";
             // 
@@ -2599,16 +2602,6 @@ namespace ShipWorks
             this.buttonOrderLookupViewShipAgain.Text = "Ship Again";
             this.buttonOrderLookupViewShipAgain.TextContentRelation = Divelements.SandRibbon.TextContentRelation.Underneath;
             // 
-            // buttonOrderLookupViewReturn
-            // 
-            this.buttonOrderLookupViewReturn.Guid = new System.Guid("287f6db0-b93d-4db6-bbb1-9d0905704b59");
-            this.buttonOrderLookupViewReturn.Image = global::ShipWorks.Properties.Resources.box_previous_32_32;
-            this.buttonOrderLookupViewReturn.Padding = new Divelements.SandRibbon.WidgetEdges(3, 2, 4, 14);
-            this.ribbonSecurityProvider.SetPermission(this.buttonOrderLookupViewReturn, ShipWorks.Users.Security.PermissionType.ShipmentsCreateEditProcess);
-            this.buttonOrderLookupViewReturn.QuickAccessKey = "OE";
-            this.buttonOrderLookupViewReturn.Text = "Return";
-            this.buttonOrderLookupViewReturn.TextContentRelation = Divelements.SandRibbon.TextContentRelation.Underneath;
-            // 
             // buttonOrderLookupViewSCANForm
             // 
             this.buttonOrderLookupViewSCANForm.DropDownStyle = Divelements.SandRibbon.DropDownStyle.Integral;
@@ -2620,6 +2613,42 @@ namespace ShipWorks
             this.buttonOrderLookupViewSCANForm.QuickAccessKey = "L";
             this.buttonOrderLookupViewSCANForm.Text = "SCAN Form";
             this.buttonOrderLookupViewSCANForm.TextContentRelation = Divelements.SandRibbon.TextContentRelation.Underneath;
+            // 
+            // popupPostalScanForm
+            // 
+            this.popupOrderLookupViewSCANForm.Items.AddRange(new Divelements.SandRibbon.WidgetBase[] {
+            this.menuOrderLookupViewEndiciaScanForm});
+            this.popupOrderLookupViewSCANForm.BeforePopup += new Divelements.SandRibbon.BeforePopupEventHandler(this.OnOrderLookupViewPostalScanFormOpening);
+            // 
+            // menuOrderLookupViewEndiciaScanForm
+            // 
+            this.menuOrderLookupViewEndiciaScanForm.Items.AddRange(new Divelements.SandRibbon.WidgetBase[] {
+            this.menuOrderLookupViewCreateEndiciaScanForm,
+            this.menuOrderLookupViewPrintScanForm});
+            // 
+            // menuOrderLookupViewCreateEndiciaScanForm
+            // 
+            this.menuOrderLookupViewCreateEndiciaScanForm.Guid = new System.Guid("3494C176-AF18-4D60-8853-77027C1DB9B2");
+            this.menuOrderLookupViewCreateEndiciaScanForm.Text = "Create SCAN Form...";
+            // 
+            // menuOrderLookupViewPrintScanForm
+            // 
+            this.menuOrderLookupViewPrintScanForm.GroupName = "Reprint";
+            this.menuOrderLookupViewPrintScanForm.Guid = new System.Guid("0D98E502-C129-4802-834E-87BEE24192B5");
+            this.menuOrderLookupViewPrintScanForm.Items.AddRange(new Divelements.SandRibbon.WidgetBase[] {
+            this.menuOrderLookupViewPrintEndiciaScanForm});
+            this.menuOrderLookupViewPrintScanForm.Text = "Print";
+            // 
+            // menuOrderLookupViewPrintEndiciaScanForm
+            // 
+            this.menuOrderLookupViewPrintEndiciaScanForm.Items.AddRange(new Divelements.SandRibbon.WidgetBase[] {
+            this.menuOrderLookupViewEndiciaScanFormNone});
+            // 
+            // menuOrderLookupViewEndiciaScanFormNone
+            // 
+            this.menuOrderLookupViewEndiciaScanFormNone.Enabled = false;
+            this.menuOrderLookupViewEndiciaScanFormNone.Guid = new System.Guid("961F1C13-E3FE-49BF-A2A8-BB7BEAD18B14");
+            this.menuOrderLookupViewEndiciaScanFormNone.Text = "(none)";
             // 
             // ribbonTabAdmin
             // 
@@ -3314,10 +3343,15 @@ namespace ShipWorks
         private Divelements.SandRibbon.Button buttonEndiciaSCAN;
         private Divelements.SandRibbon.Popup popupPostalScanForm;
         private Divelements.SandRibbon.MenuItem menuCreateEndiciaScanForm;
+        private Divelements.SandRibbon.MenuItem menuOrderLookupViewCreateEndiciaScanForm;
         private Divelements.SandRibbon.MenuItem menuPrintScanForm;
+        private Divelements.SandRibbon.MenuItem menuOrderLookupViewPrintScanForm;
         private Divelements.SandRibbon.MenuItem menuEndiciaScanFormNone;
+        private Divelements.SandRibbon.MenuItem menuOrderLookupViewEndiciaScanFormNone;
         private Divelements.SandRibbon.Menu menuEndiciaScanForm;
+        private Divelements.SandRibbon.Menu menuOrderLookupViewEndiciaScanForm;
         private Divelements.SandRibbon.Menu menuPrintEndiciaScanForm;
+        private Divelements.SandRibbon.Menu menuOrderLookupViewPrintEndiciaScanForm;
         private Divelements.SandRibbon.MenuItem menuFedExSmartPostClose;
         private Divelements.SandRibbon.MainMenuItem mainMenuItemSupport;
         private Divelements.SandRibbon.MenuItem menuItemSupportForum;
@@ -3364,7 +3398,6 @@ namespace ShipWorks
         private Divelements.SandRibbon.Popup popup2;
         private Divelements.SandRibbon.Button buttonOrderLookupViewVoid;
         private Divelements.SandRibbon.Button buttonOrderLookupViewShipAgain;
-        private Divelements.SandRibbon.Button buttonOrderLookupViewReturn;
         private Divelements.SandRibbon.Button buttonOrderLookupViewSCANForm;
         private Divelements.SandRibbon.Popup popupOrderLookupViewSCANForm;
         private Divelements.SandRibbon.Button button2;
--- Code/ShipWorks/ApplicationCore/MainForm.cs
+++ Code/ShipWorks/ApplicationCore/MainForm.cs
@@ -78,11 +78,6 @@ using ShipWorks.Settings;
 using ShipWorks.Shipping;
 using ShipWorks.Shipping.Carriers.FedEx;
 using ShipWorks.Shipping.Carriers.FedEx.Api;
-using ShipWorks.Shipping.Carriers.Postal.Endicia;
-using ShipWorks.Shipping.Carriers.Postal.Endicia.Express1;
-using ShipWorks.Shipping.Carriers.Postal.Usps;
-using ShipWorks.Shipping.Carriers.Postal.Usps.Express1.ScanForm;
-using ShipWorks.Shipping.Carriers.Postal.Usps.ScanForm;
 using ShipWorks.Shipping.Carriers.UPS.WorldShip;
 using ShipWorks.Shipping.Profiles;
 using ShipWorks.Shipping.ScanForms;
@@ -298,7 +293,8 @@ namespace ShipWorks
             // Initialize ribbon security
             ribbonSecurityProvider.AddAdditionalCondition(buttonUpdateOnline, () => OnlineUpdateCommandProvider.HasOnlineUpdateCommands());
             ribbonSecurityProvider.AddAdditionalCondition(buttonFedExClose, () => FedExAccountManager.Accounts.Count > 0);
-            ribbonSecurityProvider.AddAdditionalCondition(buttonEndiciaSCAN, () => (EndiciaAccountManager.EndiciaAccounts.Count + EndiciaAccountManager.Express1Accounts.Count + UspsAccountManager.Express1Accounts.Count + UspsAccountManager.UspsAccounts.Count) > 0);
+            ribbonSecurityProvider.AddAdditionalCondition(buttonEndiciaSCAN, AreThereAnyPostalAccounts);
+            ribbonSecurityProvider.AddAdditionalCondition(buttonOrderLookupViewSCANForm, AreThereAnyPostalAccounts);
             ribbonSecurityProvider.AddAdditionalCondition(buttonFirewall, () => SqlSession.IsConfigured && !SqlSession.Current.Configuration.IsLocalDb());
             ribbonSecurityProvider.AddAdditionalCondition(buttonChangeConnection, () => SqlSession.IsConfigured && !SqlSession.Current.Configuration.IsLocalDb());
             ribbonSecurityProvider.AddAdditionalCondition(buttonOrderLookupViewFields, () => UIMode == UIMode.OrderLookup);
@@ -316,6 +312,15 @@ namespace ShipWorks
 
             ApplyEditingContext();
         }
+
+        /// <summary>
+        /// Checks whether we have any postal accounts
+        /// </summary>
+        /// <returns></returns>
+        private static bool AreThereAnyPostalAccounts() =>
+            Using(
+                IoC.BeginLifetimeScope(),
+                scope => scope.Resolve<IEnumerable<IScanFormAccountRepository>>().Any(x => x.HasAccounts));
 
         /// <summary>
         /// Main form has been made visible
@@ -1055,8 +1060,7 @@ namespace ShipWorks
         /// </summary>
         private void ExecuteLogonActions()
         {
-            Action action = null;
-            while (logonActions.TryDequeue(out action))
+            while (logonActions.TryDequeue(out Action action))
             {
                 action();
             }
@@ -1074,9 +1078,9 @@ namespace ShipWorks
         {
             using (ILifetimeScope lifetimeScope = IoC.BeginLifetimeScope())
             {
-                ISetupGuide SetupGuide = lifetimeScope.Resolve<ISetupGuide>();
-                SetupGuide.LoadOwner(this);
-                SetupGuide.ShowDialog();
+                ISetupGuide setupGuide = lifetimeScope.Resolve<ISetupGuide>();
+                setupGuide.LoadOwner(this);
+                setupGuide.ShowDialog();
             }
         }
 
@@ -1707,6 +1711,7 @@ namespace ShipWorks
             editionGuiHelper.RegisterElement(buttonNewOrder, EditionFeature.AddOrderCustomer);
             editionGuiHelper.RegisterElement(buttonNewCustomer, EditionFeature.AddOrderCustomer);
             editionGuiHelper.RegisterElement(buttonEndiciaSCAN, EditionFeature.EndiciaScanForm);
+            editionGuiHelper.RegisterElement(buttonOrderLookupViewSCANForm, EditionFeature.EndiciaScanForm);
         }
 
         /// <summary>
@@ -4147,16 +4152,27 @@ namespace ShipWorks
         /// <summary>
         /// The postal scan form popup is opening, we need to dynamically repopulate the print menu
         /// </summary>
-        private void OnPostalScanFormOpening(object sender, BeforePopupEventArgs e)
-        {
-            List<IScanFormAccountRepository> repositories = new List<IScanFormAccountRepository>();
-            repositories.Add(new EndiciaScanFormAccountRepository());
-            repositories.Add(new Express1EndiciaScanFormAccountRepository());
-            repositories.Add(new Express1UspsScanFormAccountRepository());
-            repositories.Add(new UspsScanFormAccountRepository());
+        private void OnPostalScanFormOpening(object sender, BeforePopupEventArgs e) =>
+            PopulatePostalSCANFormMenu(menuCreateEndiciaScanForm, menuPrintEndiciaScanForm);
 
-            ScanFormUtility.PopulateCreateScanFormMenu(menuCreateEndiciaScanForm, repositories);
-            ScanFormUtility.PopulatePrintScanFormMenu(menuPrintEndiciaScanForm, repositories);
+        /// <summary>
+        /// The postal scan form popup is opening, we need to dynamically repopulate the print menu
+        /// </summary>
+        private void OnOrderLookupViewPostalScanFormOpening(object sender, BeforePopupEventArgs e) =>
+            PopulatePostalSCANFormMenu(menuOrderLookupViewCreateEndiciaScanForm, menuOrderLookupViewPrintEndiciaScanForm);
+
+        /// <summary>
+        /// Populate the contents of the postal scan form menu
+        /// </summary>
+        private void PopulatePostalSCANFormMenu(SandMenuItem createMenu, Divelements.SandRibbon.Menu printMenu)
+        {
+            using (ILifetimeScope lifetimeScope = IoC.BeginLifetimeScope())
+            {
+                var repositories = lifetimeScope.Resolve<IEnumerable<IScanFormAccountRepository>>();
+
+                ScanFormUtility.PopulateCreateScanFormMenu(createMenu, repositories);
+                ScanFormUtility.PopulatePrintScanFormMenu(printMenu, repositories);
+            }
         }
 
         #endregion
