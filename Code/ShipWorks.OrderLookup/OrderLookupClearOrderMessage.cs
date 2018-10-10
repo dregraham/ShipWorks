cb115e049bc9e96a084663d0c870b231f03777fd
Tim Hughes <t.hughes@shipworks.com>
2018-10-10 14:01:08 -0500

    TP#27735: Added OrderLookupClearOrderMessage

---
+++ Code/ShipWorks.OrderLookup/OrderLookupClearOrderMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Interapptive.Shared.Messaging;
+
+namespace ShipWorks.OrderLookup
+{
+    /// <summary>
+    /// Clear the order lookup order
+    /// </summary>
+    public struct OrderLookupClearOrderMessage : IShipWorksMessage
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public OrderLookupClearOrderMessage(object sender)
+        {
+            Sender = sender;
+            MessageId = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Originator of the message
+        /// </summary>
+        public object Sender { get; }
+
+        /// <summary>
+        /// Id of the message used for tracking purposes
+        /// </summary>
+        public Guid MessageId { get; }
+    }
+}
