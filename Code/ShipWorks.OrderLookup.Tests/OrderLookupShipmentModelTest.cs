dba82c91210b0776984663e4c9e731f46ccbf8c8
Josh Winchester <j.winchester@shipworks.com>
2018-10-17 14:49:54 -0500

    TP#26966: Add additional create label test for when order is processed

--- Code/ShipWorks.OrderLookup.Tests/OrderLookupShipmentModelTest.cs
+++ Code/ShipWorks.OrderLookup.Tests/OrderLookupShipmentModelTest.cs
@@ -31,20 +31,37 @@ namespace ShipWorks.OrderLookup.Tests
         }
 
         [Fact]
-        public void CreateLabel_DoesNotReturnMessage_OrderIsNotProcessed()
+        public void CreateLabel_DoesNotReturnMessage_WhenOrderIsNotProcessed()
         {
             OrderLookupShipmentModel testObject = mock.Create<OrderLookupShipmentModel>();
 
             var shipmentAdapter = mock.Mock<ICarrierShipmentAdapter>();
-            shipmentAdapter.Setup(sa => sa.Shipment).Returns(new ShipmentEntity() { ShipmentID = 1, Processed = false });
+            shipmentAdapter.Setup(sa => sa.Shipment).Returns(new ShipmentEntity { ShipmentID = 1, Processed = false });
 
             viewModel = mock.CreateMock<IOrderLookupShipmentModel>();
-            viewModel.Setup(d => d.SelectedOrder).Returns(new OrderEntity() { OrderNumber = 123 });
+            viewModel.Setup(d => d.SelectedOrder).Returns(new OrderEntity { OrderNumber = 123 });
             viewModel.Setup(d => d.ShipmentAdapter).Returns(shipmentAdapter.Object);
             
             testObject.CreateLabel();
             
             Assert.Empty(testMessenger.SentMessages.OfType<ProcessShipmentsMessage>());
+        }
+
+        [Fact]
+        public void CreateLabel_DoesReturnMessage_WhenOrderIsProcessed()
+        {
+            OrderLookupShipmentModel testObject = mock.Create<OrderLookupShipmentModel>();
+
+            var shipmentAdapter = mock.Mock<ICarrierShipmentAdapter>();
+            shipmentAdapter.Setup(sa => sa.Shipment).Returns(new ShipmentEntity { ShipmentID = 1, Processed = true });
+
+            viewModel = mock.CreateMock<IOrderLookupShipmentModel>();
+            viewModel.Setup(d => d.SelectedOrder).Returns(new OrderEntity { OrderNumber = 123 });
+            viewModel.Setup(d => d.ShipmentAdapter).Returns(shipmentAdapter.Object);
+
+            testObject.CreateLabel();
+
+            Assert.Equal(1, testMessenger.SentMessages.OfType<ProcessShipmentsMessage>().Count());
         }
     }
 }
