f58f76e3f81ac3b02a0d962e2ac823c856a2007d
Kevin Croke <k.croke@shipworks.com>
2018-10-17 16:12:59 -0500

    TP#26969: Add AmazonReferenceControl.xaml

---
+++ Code/ShipWorks.OrderLookup/Controls/Reference/AmazonReferenceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Shipping;
+using ShipWorks.UI;
+
+namespace ShipWorks.OrderLookup.Controls.Reference
+{
+    /// <summary>
+    /// View model for order lookup reference control
+    /// </summary>
+    [KeyedComponent(typeof(IReferenceViewModel), ShipmentTypeCode.Amazon)]
+    [WpfView(typeof(AmazonReferenceControl))]
+    public class AmazonReferenceViewModel : GenericReferenceViewModel
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AmazonReferenceViewModel(IOrderLookupShipmentModel shipmentModel) : base(shipmentModel)
+        {
+        }
+    }
+}
