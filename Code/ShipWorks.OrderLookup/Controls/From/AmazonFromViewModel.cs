7ac7f47d30dc572ffafdd326d29d3bdfbc835362
Kevin Croke <k.croke@shipworks.com>
2018-10-17 16:48:50 -0500

    TP#26969: Add Amazon From Control

---
+++ Code/ShipWorks.OrderLookup/Controls/From/AmazonFromViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Shipping;
+using ShipWorks.Shipping.Carriers;
+using ShipWorks.UI;
+using ShipWorks.UI.Controls.AddressControl;
+
+namespace ShipWorks.OrderLookup.Controls.From
+{
+    [KeyedComponent(typeof(IFromViewModel), ShipmentTypeCode.Amazon)]
+    [WpfView(typeof(AmazonFromControl))]
+    public class AmazonFromViewModel : GenericFromViewModel
+    {
+        public AmazonFromViewModel(IOrderLookupShipmentModel shipmentModel, IShipmentTypeManager shipmentTypeManager, ICarrierAccountRetrieverFactory carrierAccountRetrieverFactory, AddressViewModel addressViewModel) : base(shipmentModel, shipmentTypeManager, carrierAccountRetrieverFactory, addressViewModel)
+        {
+        }
+    }
+}
