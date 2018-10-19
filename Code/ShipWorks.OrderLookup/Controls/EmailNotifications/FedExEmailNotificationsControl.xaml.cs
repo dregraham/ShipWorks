99acbee66d4dae14be9a9e568964b44ec34b2bc0
Chris Hicks <c.hicks@shipworks.com>
2018-10-19 11:14:35 -0600

    TP#26964: Create FedExEmailNofitications Control and ViewModel.

---
+++ Code/ShipWorks.OrderLookup/Controls/EmailNotifications/FedExEmailNotificationsControl.xaml.cs
@@ -0,0 +1,16 @@
+namespace ShipWorks.OrderLookup.Controls.EmailNotifications
+{
+    /// <summary>
+    /// FedEx email notification control
+    /// </summary>
+    public partial class FedExEmailNotificationsControl
+    {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public FedExEmailNotificationsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
