9c51d8b928fc22aedc631bd5895905f8ba3393ca
Chris Hicks <c.hicks@shipworks.com>
2018-10-23 15:11:35 -0600

    TP#26954: Fix logic and add comments to FirstNotNullOrEmptyStringMultiConverter

--- Code/ShipWorks.UI/ValueConverters/FirstNotNullOrEmptyStringMultiConverter.cs
+++ Code/ShipWorks.UI/ValueConverters/FirstNotNullOrEmptyStringMultiConverter.cs
@@ -4,21 +4,30 @@ using System.Windows.Data;
 
 namespace ShipWorks.UI.ValueConverters
 {
-    public class FirstNotNullItemMultiConverter : IMultiValueConverter
+    /// <summary>
+    /// Converter for returning the first not null or empty string in a list
+    /// </summary>
+    public class FirstNotNullOrEmptyStringMultiConverter : IMultiValueConverter
     {
+        /// <summary>
+        /// Return the first not null or empty string
+        /// </summary>
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             foreach (var value in values)
             {
-                if (value != null)
+                if (value is string stringValue && !string.IsNullOrWhiteSpace(stringValue))
                 {
-                    return value;
+                    return stringValue;
                 }
             }
 
             return null;
         }
 
+        /// <summary>
+        /// Not implemented
+        /// </summary>
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
