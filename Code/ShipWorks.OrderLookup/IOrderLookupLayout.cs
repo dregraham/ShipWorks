0a28f8db4ac0ef6e9f48e9678b3e34dc796569fc
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-10-24 13:17:18 -0500

    TP#27172 create IOrderLookupLayout

---
+++ Code/ShipWorks.OrderLookup/IOrderLookupLayout.cs
@@ -0,0 +1,18 @@
+﻿namespace ShipWorks.OrderLookup
+{
+    /// <summary>
+    /// Represents the OrderLookup Layout
+    /// </summary>
+    public interface IOrderLookupLayout
+    {
+        /// <summary>
+        /// Apply the layout to the view model
+        /// </summary>
+        void Apply(IOrderLookupViewModel orderLookupViewModel);
+
+        /// <summary>
+        /// Save the view model
+        /// </summary>
+        void Save();
+    }
+}
