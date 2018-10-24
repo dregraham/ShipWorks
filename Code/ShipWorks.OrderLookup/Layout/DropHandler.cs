ea5ecac9e3efbfd4a469c90925bd9111cf33c0c0
Chris Hicks <c.hicks@shipworks.com>
2018-10-24 12:36:20 -0600

    TP#27172: Create DropHandler

---
+++ Code/ShipWorks.OrderLookup/Layout/DropHandler.cs
@@ -0,0 +1,13 @@
+using GongSolutions.Wpf.DragDrop;
+using Interapptive.Shared.ComponentRegistration;
+
+namespace ShipWorks.OrderLookup.Layout
+{
+    /// <summary>
+    /// Drop Handler for Order Lookup Mode
+    /// </summary>
+    [Component]
+    public class DropHandler : DefaultDropHandler
+    {
+    }
+}
