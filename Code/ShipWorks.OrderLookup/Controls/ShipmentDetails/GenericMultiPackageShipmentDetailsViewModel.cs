0e9bdd9ad22b27e0e1cee7452248768a21431392
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-10-24 08:24:48 -0500

    TP#26964 reload insurance when package selection changes

--- Code/ShipWorks.OrderLookup/Controls/ShipmentDetails/GenericMultiPackageShipmentDetailsViewModel.cs
+++ Code/ShipWorks.OrderLookup/Controls/ShipmentDetails/GenericMultiPackageShipmentDetailsViewModel.cs
@@ -254,6 +254,9 @@ namespace ShipWorks.OrderLookup.Controls.ShipmentDetails
                 }
 
                 handler.Set(nameof(SelectedPackage), ref selectedPackage, value);
+
+                RefreshInsurance();
+
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedPackageWeight)));
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedPackageDimsProfileID)));
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsProfileSelected)));
