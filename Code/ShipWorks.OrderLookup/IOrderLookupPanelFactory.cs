bdcd3ba90c788756efac26ffec4a1542af859c53
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-10-24 13:15:08 -0500

    TP#27172 create IOrderLookupPanelFactory

---
+++ Code/ShipWorks.OrderLookup/IOrderLookupPanelFactory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ShipWorks.OrderLookup
+{
+    /// <summary>
+    /// Represents a factory for creating OrderLookupPanels
+    /// </summary>
+    public interface IOrderLookupPanelFactory
+    {
+        /// <summary>
+        /// Get a collection of IOrderLookupPanelViewModels
+        /// </summary>
+        IEnumerable<IOrderLookupPanelViewModel<T>> GetPanels<T>() where T : IOrderLookupViewModel;
+    }
+}
