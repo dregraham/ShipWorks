d69db68f69efed2af0fa42f8025118cb19e64c93
Chris Hicks <c.hicks@shipworks.com>
2018-10-29 13:27:45 -0600

    TP#27172: Refactor how column resizing is implemented

--- Code/ShipWorks.OrderLookup/Controls/OrderLookup/MainOrderLookupControl.xaml.cs
+++ Code/ShipWorks.OrderLookup/Controls/OrderLookup/MainOrderLookupControl.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Controls;
-using GongSolutions.Wpf.DragDrop; 
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using DragDrop = GongSolutions.Wpf.DragDrop.DragDrop;
 
 namespace ShipWorks.OrderLookup.Controls.OrderLookup
 {
@@ -19,5 +21,41 @@ namespace ShipWorks.OrderLookup.Controls.OrderLookup
             // included in the project.
             System.Windows.DataFormat _ = DragDrop.DataFormat;
         } 
+        
+        /// <summary>
+        /// Update column widths when control size changes
+        /// </summary>
+        private void OnControlSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateColumnWidths();
+        }
+
+        /// <summary>
+        /// Update column widths when column grid size changes
+        /// </summary>
+        private void OnGridControlSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateColumnWidths();
+        }
+
+        /// <summary>
+        /// Update column widths when the grid splitter drag is completed
+        /// </summary>
+        private void OnGridSplitterDragComplete(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            UpdateColumnWidths();
+        }
+
+        /// <summary>
+        /// Update the column widths so that a single column doesn't extend past the screen
+        /// </summary>
+        private void UpdateColumnWidths()
+        {
+            double columnMaxWidth = Math.Min(ColumnGrid.ActualWidth, ColumnGrid.MaxWidth) - (2 * (LeftColumn.MinWidth + 10));
+
+            LeftColumn.MaxWidth = columnMaxWidth;
+            MiddleColumn.MaxWidth = columnMaxWidth;
+            RightColumn.MaxWidth = columnMaxWidth;
+        }
     }
 }
