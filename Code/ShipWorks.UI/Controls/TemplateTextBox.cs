a7d99a7fc720ffdac75d3df5a1e86890ca9a6f2f
Andrew Benz <a.benz@shipworks.com>
2018-10-31 08:23:21 -0500

    TP#27334: Add initial UI for token text box

---
+++ Code/ShipWorks.UI/Controls/TemplateTextBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ShipWorks.UI.Controls
+{
+    /// <summary>
+    /// Text box that allows template editing
+    /// </summary>
+    public class TemplateTextBox : TextBox
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        static TemplateTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TemplateTextBox), new FrameworkPropertyMetadata(typeof(TemplateTextBox)));
+        }
+    }
+}
