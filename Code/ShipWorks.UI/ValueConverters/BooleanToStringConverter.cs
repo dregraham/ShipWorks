f3880cffa44b546cef1a06bb1ae5f5c523d0b6f3
Andrew Benz <a.benz@shipworks.com>
2018-11-26 16:16:14 -0600

    TP#26755: Add sort controls to column header template

---
+++ Code/ShipWorks.UI/ValueConverters/BooleanToStringConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace ShipWorks.UI.ValueConverters
+{
+    /// <summary>
+    /// Convert a boolean to a string
+    /// </summary>
+    public class BooleanToStringConverter : BooleanConverter<string>
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="trueValue">String that represents true</param>
+        /// <param name="falseValue">String that represents false</param>
+        public BooleanToStringConverter() : base("Yes", "No")
+        {
+        }
+    }
+}
