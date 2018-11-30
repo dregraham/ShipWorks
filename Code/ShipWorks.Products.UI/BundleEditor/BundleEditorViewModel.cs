a247d919beff53780468653b84a70c7d1780ef74
Chris Hicks <c.hicks@shipworks.com>
2018-11-30 14:11:51 -0700

    TP#26747: Create BundleEditorControl and create skeleton for BundleEditorControlViewModel

---
+++ Code/ShipWorks.Products.UI/BundleEditor/BundleEditorViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace ShipWorks.Products.UI.BundleEditor
+{
+    public class BundleEditorViewModel
+    {
+        public string Sku { get; set; }
+        
+        public int Quantity { get; set; }
+        
+        public IEnumerable<string> BundledSkus { get; set; }
+        
+        public string SelectedSku { get; set; }
+        
+        public ICommand AddCommand { get; }
+        
+        public ICommand RemoveCommand { get; }
+    }
+}
