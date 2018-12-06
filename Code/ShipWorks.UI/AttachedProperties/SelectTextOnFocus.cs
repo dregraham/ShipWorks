ac5e33f32da02e4d833fb76c407b0212c642af0c
Chris Hicks <c.hicks@shipworks.com>
2018-12-06 09:02:42 -0700

    TP#26747: Create attached property to highlight all text when a textbox recieves focus. Apply attached property to BlueSteel textboxes.

---
+++ Code/ShipWorks.UI/AttachedProperties/SelectTextOnFocus.cs
@@ -0,0 +1,104 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ShipWorks.UI.AttachedProperties
+{
+    /// <summary>
+    /// Attached property for highlighting all of a textboxes text on focus
+    /// </summary>
+    public class SelectTextOnFocus : DependencyObject
+    {
+        /// <summary>
+        /// Whether or not this attached property is enabled
+        /// </summary>
+        public static readonly DependencyProperty EnabledProperty = DependencyProperty.RegisterAttached(
+            "Enabled",
+            typeof(bool),
+            typeof(SelectTextOnFocus),
+            new PropertyMetadata(false, EnabledPropertyChanged));
+
+        
+        /// <summary>
+        /// Get the enabled property
+        /// </summary>
+        public static bool GetEnabled(DependencyObject d)
+        {
+            return (bool) d.GetValue(EnabledProperty);
+        }
+
+        /// <summary>
+        /// Set the enabled property
+        /// </summary>
+        public static void SetEnabled(DependencyObject d, bool value)
+        {
+            d.SetValue(EnabledProperty, value);
+        }
+
+        /// <summary>
+        /// Hook up or unhook events when the enabled property changes
+        /// </summary>
+        private static void EnabledPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBox textBox)
+            {
+                if ((e.NewValue as bool?).GetValueOrDefault(false))
+                {
+                    textBox.GotKeyboardFocus += OnKeyboardFocusSelectText;
+                    textBox.PreviewMouseLeftButtonDown += OnMouseLeftButtonDown;
+                }
+                else
+                {
+                    textBox.GotKeyboardFocus -= OnKeyboardFocusSelectText;
+                    textBox.PreviewMouseLeftButtonDown -= OnMouseLeftButtonDown;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Focus textbox when left clicked
+        /// </summary>
+        private static void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DependencyObject dependencyObject = GetParentFromVisualTree(e.OriginalSource);
+
+            if (dependencyObject == null)
+            {
+                return;
+            }
+
+            TextBox textBox = (TextBox)dependencyObject;
+            if (!textBox.IsKeyboardFocusWithin)
+            {
+                textBox.Focus();
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Highlight all text when textbox receives focus 
+        /// </summary> 
+        private static void OnKeyboardFocusSelectText(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (e.OriginalSource is TextBox textBox)
+            {
+                textBox.SelectAll();
+            }
+        }
+
+        /// <summary>
+        /// Get the given dependency objects parent from the visual tree
+        /// </summary>
+        private static DependencyObject GetParentFromVisualTree(object source)
+        {
+            DependencyObject parent = source as UIElement;
+            while (parent != null && !(parent is TextBox))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return parent;
+        }
+    }
+}
