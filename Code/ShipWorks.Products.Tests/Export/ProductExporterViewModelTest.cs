59ee3dc525174989fc17db7f6e7cca7b5639ce3e
Andrew Benz <a.benz@shipworks.com>
2018-12-10 12:20:47 -0600

    TP#28929: Finish the product export view model and tests

--- Code/ShipWorks.Products.Tests/Export/ProductExporterViewModelTest.cs
+++ Code/ShipWorks.Products.Tests/Export/ProductExporterViewModelTest.cs
@@ -2,6 +2,7 @@
 using Autofac.Extras.Moq;
 using Interapptive.Shared.IO;
 using Interapptive.Shared.Threading;
+using Interapptive.Shared.UI;
 using Interapptive.Shared.Utility;
 using Moq;
 using ShipWorks.Products.Export;
@@ -20,8 +21,16 @@ namespace ShipWorks.Products.Tests.Export
             mock = AutoMockExtensions.GetLooseThatReturnsMocks();
         }
 
-        //[Fact]
-        //public void ExportProducts_DelegatesToProduct
+        [Fact]
+        public void ExportProducts_DelegatesToFileSelector()
+        {
+            mock.Mock<IDateTimeProvider>().SetupGet(x => x.Now).Returns(new DateTime(2018, 12, 10, 11, 7, 13));
+            var testObject = mock.Create<ProductExporterViewModel>();
+
+            testObject.ExportProducts();
+
+            mock.Mock<IFileSelector>().Verify(x => x.GetFilePathToSave("Comma Separated|*.csv|All Files|*.*", "ProductExport_2018-12-10_110713.csv"));
+        }
 
         [Fact]
         public void ExportProducts_DoesNotDelegateToProductExporter_WhenFileSelectorIsCanceled()
@@ -32,6 +41,41 @@ namespace ShipWorks.Products.Tests.Export
             testObject.ExportProducts();
 
             mock.Mock<IProductExporter>().Verify(x => x.Export(AnyString, It.IsAny<IProgressReporter>()), Times.Never);
+        }
+
+        [Fact]
+        public void ExportProducts_DelegatesToProductExporter_WhenFileNameIsSelected()
+        {
+            mock.Mock<IFileSelector>().Setup(x => x.GetFilePathToSave(AnyString, AnyString)).Returns("Bar");
+            var testObject = mock.Create<ProductExporterViewModel>();
+
+            testObject.ExportProducts();
+
+            mock.Mock<IProductExporter>().Verify(x => x.Export("Bar", It.IsAny<IProgressReporter>()));
+        }
+
+        [Fact]
+        public void ExportProducts_DoesNotShowError_WhenExportSucceed()
+        {
+            mock.Mock<IFileSelector>().Setup(x => x.GetFilePathToSave(AnyString, AnyString)).Returns("Bar");
+            var testObject = mock.Create<ProductExporterViewModel>();
+
+            testObject.ExportProducts();
+
+            mock.Mock<IMessageHelper>().Verify(x => x.ShowError(AnyString, It.IsAny<Exception>()), Times.Never);
+        }
+
+        [Fact]
+        public void ExportProducts_ShowEsrror_WhenExportFails()
+        {
+            var exception = new Exception("Foo");
+            mock.Mock<IProductExporter>().Setup(x => x.Export(AnyString, It.IsAny<IProgressReporter>())).ThrowsAsync(exception);
+            mock.Mock<IFileSelector>().Setup(x => x.GetFilePathToSave(AnyString, AnyString)).Returns("Bar");
+            var testObject = mock.Create<ProductExporterViewModel>();
+
+            testObject.ExportProducts();
+
+            mock.Mock<IMessageHelper>().Verify(x => x.ShowError("Foo", exception));
         }
 
         public void Dispose()
--- Code/ShipWorks.Products/Export/ProductExporterViewModel.cs
+++ Code/ShipWorks.Products/Export/ProductExporterViewModel.cs
@@ -1,5 +1,14 @@
-﻿using Interapptive.Shared.ComponentRegistration;
+﻿using System;
+using System.Linq;
+using System.Reactive;
+using System.Threading.Tasks;
+using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.Extensions;
 using Interapptive.Shared.IO;
+using Interapptive.Shared.UI;
+using Interapptive.Shared.Utility;
+using ShipWorks.Core.Common.Threading;
+using static Interapptive.Shared.Utility.Functional;
 
 namespace ShipWorks.Products.Export
 {
@@ -11,12 +20,16 @@ namespace ShipWorks.Products.Export
     {
         private readonly IFileSelector fileSelector;
         private readonly IProductExporter productExporter;
+        private readonly IDateTimeProvider dateTimeProvider;
+        private readonly IMessageHelper messageHelper;
 
         /// <summary>
         /// Constructor
         /// </summary>
-        public ProductExporterViewModel(IFileSelector fileSelector, IProductExporter productExporter)
+        public ProductExporterViewModel(IFileSelector fileSelector, IProductExporter productExporter, IDateTimeProvider dateTimeProvider, IMessageHelper messageHelper)
         {
+            this.messageHelper = messageHelper;
+            this.dateTimeProvider = dateTimeProvider;
             this.productExporter = productExporter;
             this.fileSelector = fileSelector;
         }
@@ -26,7 +39,30 @@ namespace ShipWorks.Products.Export
         /// </summary>
         public void ExportProducts()
         {
+            var timestamp = dateTimeProvider.Now.ToString("yyyy-MM-dd_HHmmss");
+            fileSelector.GetFilePathToSave("Comma Separated|*.csv|All Files|*.*", $"ProductExport_{timestamp}.csv")
+                .Do(path => ExportProducts(path).Forget());
+        }
 
+        /// <summary>
+        /// Export products to the given path
+        /// </summary>
+        private Task<Unit> ExportProducts(string path) =>
+            UsingAsync(
+                    messageHelper.ShowProgressDialog("Exporting products", "Exporting products"),
+                    dialog => productExporter.Export(path, dialog.ProgressItem).ToTyped<Unit>())
+                .Recover(HandleExportError);
+
+        /// <summary>
+        /// Handle export errors
+        /// </summary>
+        private Unit HandleExportError(Exception ex)
+        {
+            var actualException = ex is AggregateException aggregateException ?
+                aggregateException.InnerExceptions.FirstOrDefault() :
+                ex;
+            messageHelper.ShowError(actualException.Message, actualException);
+            return Unit.Default;
         }
     }
 }
