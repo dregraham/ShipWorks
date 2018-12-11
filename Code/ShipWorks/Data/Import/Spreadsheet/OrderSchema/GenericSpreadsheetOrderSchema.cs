4e18e2eaf954fa74e4fc0a700eb5becf6cae48bb
Josh Winchester <j.winchester@shipworks.com>
2018-12-11 14:05:10 -0600

    TP#28832: Update GenericSpreadsheetOrderSchema to set ShipByDate from string to datetime

--- Code/ShipWorks/Data/Import/Spreadsheet/OrderSchema/GenericSpreadsheetOrderSchema.cs
+++ Code/ShipWorks/Data/Import/Spreadsheet/OrderSchema/GenericSpreadsheetOrderSchema.cs
@@ -95,7 +95,7 @@ namespace ShipWorks.Data.Import.Spreadsheet.OrderSchema
                     new GenericSpreadsheetTargetField("Order.Date",                 "Order Date",             typeof(DateTime)),
                     new GenericSpreadsheetTargetField("Order.Time",                 "Order Time",             typeof(TimeSpan)),
                     new GenericSpreadsheetTargetField("Order.LocalStatus",          "Local Status",           typeof(string)),
-                    new GenericSpreadsheetTargetField("Order.OnlineStatus",         "Store Status",          typeof(string)),
+                    new GenericSpreadsheetTargetField("Order.OnlineStatus",         "Store Status",           typeof(string)),
                     new GenericSpreadsheetTargetField("Order.RequestedShipping",    "Requested Shipping",     typeof(string)),
                     new GenericSpreadsheetTargetField("Order.CustomerNumber",       "Customer Number",        typeof(string)),
                     new GenericSpreadsheetTargetField("Order.NoteInternal",         "Note (Internal)",        typeof(string)),
@@ -112,7 +112,7 @@ namespace ShipWorks.Data.Import.Spreadsheet.OrderSchema
                     new GenericSpreadsheetTargetField("Order.CreditCardNumber",     "Credit Card Number",     typeof(string)),
                     new GenericSpreadsheetTargetField("Order.CreditCardExpiration", "Credit Card Expiration", typeof(string)),
                     new GenericSpreadsheetTargetField("Order.CreditCardName",       "Credit Card Name",       typeof(string)),
-                    new GenericSpreadsheetTargetField("Order.ShipByDate",           "Ship By Date",           typeof(string)),
+                    new GenericSpreadsheetTargetField("Order.ShipByDate",           "Ship By Date",           typeof(DateTime))
                 });
         }
 
