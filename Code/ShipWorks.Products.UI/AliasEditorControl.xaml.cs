362fe199ed3434c734c35d8817a7208a7b74730c
Chris Hicks <c.hicks@shipworks.com>
2018-12-13 08:50:01 -0700

    TP#26751: Create AliasEditorControl

---
+++ Code/ShipWorks.Products.UI/AliasEditorControl.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace ShipWorks.Products.UI
+{
+    public partial class AliasEditorControl : UserControl
+    {
+        public AliasEditorControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
