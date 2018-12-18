a2bf9a5a8aa4593ca4626df1a4bf124c3fb32b2e
Andrew Benz <a.benz@shipworks.com>
2018-12-18 11:12:05 -0600

    Use entity-friendly IDs for the product tables to avoid conflicts with the entity cache

--- Code/ShipWorks/Data/Administration/Scripts/Installation/CreateSchema.sql
+++ Code/ShipWorks/Data/Administration/Scripts/Installation/CreateSchema.sql
@@ -7823,7 +7823,7 @@ PRINT N'Creating [dbo].[Product]'
 GO
 CREATE TABLE [dbo].[Product]
 (
-[ProductID] [bigint] NOT NULL IDENTITY(1, 1),
+[ProductID] [bigint] NOT NULL IDENTITY(1101, 1000),
 [CreatedDate] [datetime] NOT NULL,
 [IsActive] [bit] NOT NULL,
 [IsBundle] [bit] NOT NULL
@@ -7850,7 +7850,7 @@ PRINT N'Creating [dbo].[ProductVariant]'
 GO
 CREATE TABLE [dbo].[ProductVariant]
 (
-[ProductVariantID] [bigint] NOT NULL IDENTITY(1, 1),
+[ProductVariantID] [bigint] NOT NULL IDENTITY(1102, 1000),
 [ProductID] [bigint] NOT NULL,
 [CreatedDate] [datetime] NOT NULL,
 [Name] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
@@ -7877,7 +7877,7 @@ PRINT N'Creating [dbo].[ProductVariantAlias]'
 GO
 CREATE TABLE [dbo].[ProductVariantAlias]
 (
-[ProductVariantAliasID] [bigint] NOT NULL IDENTITY(1, 1),
+[ProductVariantAliasID] [bigint] NOT NULL IDENTITY(1103, 1000),
 [ProductVariantID] [bigint] NOT NULL,
 [AliasName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [Sku] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
@@ -7906,7 +7906,7 @@ GO
 IF OBJECT_ID(N'[dbo].[ProductAttribute]', 'U') IS NULL
 CREATE TABLE [dbo].[ProductAttribute]
 (
-[ProductAttributeID] [bigint] IDENTITY(1,1) NOT NULL,
+[ProductAttributeID] [bigint] IDENTITY(1104, 1000) NOT NULL,
 [ProductID] [bigint] NOT NULL,
 [AttributeName] [nvarchar](50) NOT NULL
 )
@@ -7920,7 +7920,7 @@ PRINT N'Creating [dbo].[ProductVariantAttributeValue]'
 GO
 CREATE TABLE [dbo].[ProductVariantAttributeValue]
 (
-[ProductVariantAttributeValueID] [bigint] NOT NULL IDENTITY(1, 1),
+[ProductVariantAttributeValueID] [bigint] NOT NULL IDENTITY(1105, 1000),
 [ProductVariantID] [bigint] NOT NULL,
 [ProductAttributeID] [bigint] NOT NULL,
 [AttributeValue] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
