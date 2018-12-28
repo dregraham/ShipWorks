e66740d5917a13ee4b6794bf7ca8952c8bfd8ec9
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2018-12-28 11:58:04 -0600

    TP#29571 add row for printing picking slip

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/6.0/6.0.0.7.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/6.0/6.0.0.7.sql
@@ -4,3 +4,8 @@ IF COL_LENGTH(N'[dbo].[Configuration]', N'DefaultPickListTemplateID') IS NULL
 ALTER TABLE [dbo].[Configuration] ADD [DefaultPickListTemplateID] [bigint] NULL
 GO
 
+PRINT N'Adding Shortcut'
+GO
+INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
+VALUES (3, 80, '' , 9)
+GO
