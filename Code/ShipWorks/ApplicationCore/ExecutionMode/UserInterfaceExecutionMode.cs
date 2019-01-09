07378d65a39536697c76cf550e521ac2f1b71f56
Andrew Benz <a.benz@shipworks.com>
2019-01-09 08:26:41 -0600

    TP#29239: Fix crash when trying to display a crash before MainForm has been created

--- Code/ShipWorks/ApplicationCore/ExecutionMode/UserInterfaceExecutionMode.cs
+++ Code/ShipWorks/ApplicationCore/ExecutionMode/UserInterfaceExecutionMode.cs
@@ -241,7 +241,7 @@ namespace ShipWorks.ApplicationCore.ExecutionMode
 
                 try
                 {
-                    new WindowInteropHelper(crashDialog).Owner = Program.MainForm.Handle;
+                    new WindowInteropHelper(crashDialog).Owner = Program.MainForm?.Handle ?? IntPtr.Zero;
                 }
                 catch (ObjectDisposedException)
                 {
