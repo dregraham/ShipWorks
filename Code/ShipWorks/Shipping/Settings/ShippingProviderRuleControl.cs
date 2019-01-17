0be981ccb61f9ffcd368aee6bd44e768182f5b6b
Tim Hughes <t.hughes@shipworks.com>
2019-01-17 15:15:11 -0600

    TP#29735:  Added the validation code

--- Code/ShipWorks/Shipping/Settings/ShippingProviderRuleControl.cs
+++ Code/ShipWorks/Shipping/Settings/ShippingProviderRuleControl.cs
@@ -20,10 +20,11 @@ namespace ShipWorks.Shipping.Settings
     public partial class ShippingProviderRuleControl : UserControl
     {
         static readonly ILog log = LogManager.GetLogger(typeof(ShippingProviderRuleControl));
-
-        ShippingProviderRuleEntity rule;
+        private const long NoFilterSelectedID = long.MinValue;
+        private ShippingProviderRuleEntity rule;
         private long originalFilterNodeID;
         private readonly IShippingProviderRuleManager shippingProviderRuleManager;
+        private long originalFilterID;
 
         /// <summary>
         /// User has clicked the delete button on the rule line
@@ -37,7 +38,11 @@ namespace ShipWorks.Shipping.Settings
         {
             InitializeComponent();
 
+            filterCombo.AllowMyFilters = false;
+
             toolStipDelete.Renderer = new NoBorderToolStripRenderer();
+            originalFilterID = NoFilterSelectedID;
+            originalFilterNodeID = 0;
             shippingProviderRuleManager = IoC.UnsafeGlobalLifetimeScope.Resolve<IShippingProviderRuleManager>();
         }
 
@@ -50,6 +55,16 @@ namespace ShipWorks.Shipping.Settings
         }
 
         /// <summary>
+        /// Gets a value indicating whether this instance is a my filter.
+        /// </summary>
+        public bool IsMyFilter => filterCombo.IsSelectedFilterMyFilter;
+
+        /// <summary>
+        /// Gets the selected filter name
+        /// </summary>
+        public string SelectedFilterName => filterCombo.SelectedFilterNode?.Filter?.Name;
+		
+        /// <summary>
         /// Initialize the control to display settings for the given rule
         /// </summary>
         public void Initialize(ShippingProviderRuleEntity rule, List<ShipmentType> shipmentTypes)
@@ -58,8 +73,8 @@ namespace ShipWorks.Shipping.Settings
 
             shipmentTypeCombo.DisplayMember = "Key";
             shipmentTypeCombo.ValueMember = "Value";
+            filterCombo.AllowMyFilters = Rule.FilterNodeID != 0 && FilterHelper.IsMyFilter(Rule.FilterNodeID);
 
-            filterCombo.AllowMyFilters = false;
             filterCombo.LoadLayouts(FilterTarget.Orders);
 
             originalFilterNodeID = rule.FilterNodeID;
@@ -67,6 +82,10 @@ namespace ShipWorks.Shipping.Settings
             if (filterCombo.SelectedFilterNode == null)
             {
                 filterCombo.SelectFirstNode();
+            }
+            else
+            {
+                originalFilterID = filterCombo.SelectedFilterNode.FilterID;
             }
 
             UpdateActiveProviders(shipmentTypes);
@@ -142,12 +161,63 @@ namespace ShipWorks.Shipping.Settings
         }
 
         /// <summary>
+        /// Return a list of validation errors
+        /// </summary>
+        public string ValidationErrors
+        {
+            get
+            {
+                if (IsMyFilter)
+                {
+                    return $"A Rule is using a My Filter, '{filterCombo.SelectedFilterNode.Filter.Name}'. Shipping Provider Rules can no longer use a My Filter. Any changes to this rule will not be saved.";
+                }
+
+                if (FilterHelper.IsMyFilter(originalFilterNodeID))
+                {
+                    return $"A rule was using a My Filter, '{filterCombo.SelectedFilterNode.Filter.Name}', that is not available to this user account. " +
+                           $"Shipping Provider Rules can no longer use a My filter.  " +
+                           $"This rule has been updated to use a filter that is not a My Filter and the provider will be changed to \"none\".";
+                }
+
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
         /// Save the settings in the control
         /// </summary>
         public void SaveSettings()
         {
-            rule.FilterNodeID = filterCombo.SelectedFilterNodeID;
-            rule.ShipmentType = (int) shipmentTypeCombo.SelectedValue;
+            bool isOriginalFilterNodeAMyFilter = originalFilterNodeID != 0 && FilterHelper.IsMyFilter(originalFilterNodeID);
+
+            // If the selected filter node is not a my filter and the original was not a my filter, just carry on as usual.
+            if (!FilterHelper.IsMyFilter(filterCombo.SelectedFilterNode) && !isOriginalFilterNodeAMyFilter)
+            {
+                SaveRule(filterCombo.SelectedFilterNodeID, (int) shipmentTypeCombo.SelectedValue);
+            }
+
+            // The selected filter is a my filter, so it must by this user's filter (we don't show other user's my filters).
+            // In this case we want to tell the user we will not be saving this shipping rule because a my filter
+            // is still selected.
+            if (IsMyFilter)
+            {
+                return;
+            }
+
+            // If the original filter node id was a my filter, set the profile to the "none" profile and save
+            if (isOriginalFilterNodeAMyFilter)
+            {
+                SaveRule(originalFilterNodeID, (int) ShipmentTypeCode.None);
+            }
+        }
+
+        /// <summary>
+        /// Save the shipping rule with the given shipping profile id
+        /// </summary>
+        private void SaveRule(long filterNodeID, int shipmentType)
+        {
+            rule.FilterNodeID = filterNodeID;
+            rule.ShipmentType = shipmentType;
 
             using (SqlAdapter adapter = new SqlAdapter(true))
             {
