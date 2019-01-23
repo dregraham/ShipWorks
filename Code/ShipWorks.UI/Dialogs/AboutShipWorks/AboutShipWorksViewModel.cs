255d4a6e883893110afdad72f7dc54cfcefb3abf
Chris Hicks <c.hicks@shipworks.com>
2019-01-23 13:07:20 -0700

    TP#29799: Remove extra space.

--- Code/ShipWorks.UI/Dialogs/AboutShipWorks/AboutShipWorksViewModel.cs
+++ Code/ShipWorks.UI/Dialogs/AboutShipWorks/AboutShipWorksViewModel.cs
@@ -23,7 +23,7 @@ namespace ShipWorks.UI.Dialogs.AboutShipWorks
             SupportEmail = "support@shipworks.com";
             SupportEmailUri = new Uri($"mailto:{SupportEmail}");
             SupportPhone = "1-800-952-7784";
-            PatentInfo = "Covered by and/or for use with U.S. Patents 6,244,763; 6,939,063; 7,216,110; 7,236,956; 7,236,970; 7,458,612; 7,490,065; 7,743,043; 7,818,267; 7,831,524; 7,844,553; 7,882,094; 8,027,926; 8,041,644; 8,103,647;  8,240,579; 8,255,337; 8,374,970; 8,392,391; 8,489,519; 8,626,673; 8,626,674; 8,751,409; 8,762,290; 8,768,857; 8,843,464; and 8,954,355.";
+            PatentInfo = "Covered by and/or for use with U.S. Patents 6,244,763; 6,939,063; 7,216,110; 7,236,956; 7,236,970; 7,458,612; 7,490,065; 7,743,043; 7,818,267; 7,831,524; 7,844,553; 7,882,094; 8,027,926; 8,041,644; 8,103,647; 8,240,579; 8,255,337; 8,374,970; 8,392,391; 8,489,519; 8,626,673; 8,626,674; 8,751,409; 8,762,290; 8,768,857; 8,843,464; and 8,954,355.";
             CopyrightInfo = $"Copyright © 2003 – {DateTime.Now.Year} Interapptive, Inc. All rights reserved. Interapptive, ShipWorks, and the ShipWorks logo are trademarks or registered trademarks of Interapptive, Inc.";
         }
         
