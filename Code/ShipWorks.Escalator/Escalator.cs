376c446a9ff3a637cd6b86303d2cafc6985ec3f0
Kevin Croke <k.croke@shipworks.com>
2019-01-30 11:22:44 -0600

    TP#29931: Created service, made sure it was in application directory after build.

---
+++ Code/ShipWorks.Escalator/Escalator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Escalator
+{
+    public partial class Escalator : ServiceBase
+    {
+        public Escalator()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"\hereIam.txt"))
+            {
+                file.WriteLine(DateTime.Now.ToString());
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
