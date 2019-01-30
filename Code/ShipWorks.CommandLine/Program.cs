106882c42dbdaffdeacd8f0ec57e8de92a23dd7a
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2019-01-30 15:28:14 -0600

    TP#29906 create ShipWorks.CommandLine project

---
+++ Code/ShipWorks.CommandLine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Interapptive.Shared.Metrics;
+using ShipWorks.Startup;
+
+namespace ShipWorks.CommandLine
+{
+    class Program
+    {
+        /// <summary>
+        /// Command line executable entry point
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide command line parameters.");
+                return;
+            }
+
+            ContainerInitializer.Initialize();
+
+            ShipWorks.Program.Main().GetAwaiter().GetResult();
+
+            Telemetry.Flush();
+        }
+    }
+}
