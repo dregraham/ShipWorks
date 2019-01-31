89bc982a5e40e876ff99632b86bce510d0a0da43
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2019-01-31 11:41:06 -0600

    TP#29906 fix csproj so output is in the correct folder, add comments

--- Code/ShipWorks.CommandLine/Program.cs
+++ Code/ShipWorks.CommandLine/Program.cs
@@ -4,6 +4,9 @@ using ShipWorks.ApplicationCore;
 
 namespace ShipWorks.CommandLine
 {
+    /// <summary>
+    /// Command line version of ShipWorks
+    /// </summary>
     class Program
     {
         /// <summary>
@@ -12,6 +15,9 @@ namespace ShipWorks.CommandLine
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            // Require the user to give us some command line args
+            // we don't pass the args to the ShipWorks Main method
+            // they are pulled in downstream via Environment.GetCommandLineArgs()
             if (args.Length == 0)
             {
                 Console.WriteLine("Please provide command line parameters.");
