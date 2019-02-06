8a3497193b5cc89c12e057d717fdca7ffb378743
Kevin Croke <k.croke@shipworks.com>
2019-02-06 13:10:01 -0600

    TP#29952: Add ShipWorksInstaller

---
+++ Code/ShipWorks.Escalator/ShipWorksInstaller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Escalator
+{
+    public class ShipWorksInstaller
+    {
+
+        public void Install(InstallFile file)
+        {
+            if (!file.IsValid)
+            {
+                throw new InvalidOperationException("Install file is invalid");
+            }
+
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = file.Path;
+            start.Arguments = "/VERYSILENT";
+            start.WindowStyle = ProcessWindowStyle.Hidden;
+            start.CreateNoWindow = true;
+            int exitCode;
+
+            using (Process proc = Process.Start(start))
+            {
+                proc.WaitForExit();
+                exitCode = proc.ExitCode;
+            }
+
+            if (exitCode > 0)
+            {
+                throw new InvalidOperationException($"Update failed with exit code {exitCode}");
+            }
+        }
+    }
+}
