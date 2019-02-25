a3428c20c6eab0b4d42f5a36618c9043e2f89f18
Tim Hughes <t.hughes@shipworks.com>
2019-02-25 14:58:26 -0600

    TP#30160: actionTask.CommitAsync was causing the hang...not really sure why, but changing it to just be synchronous fixed it.

--- Code/ShipWorks/Actions/Tasks/Common/ValidateAddressTask.cs
+++ Code/ShipWorks/Actions/Tasks/Common/ValidateAddressTask.cs
@@ -92,11 +92,11 @@ namespace ShipWorks.Actions.Tasks.Common
         /// <summary>
         /// Commit the database changes
         /// </summary>
-        public override async Task Commit(List<long> inputKeys, ActionStepContext context, IDataAccessAdapter adapter)
+        public override async Task CommitAsync(List<long> inputKeys, ActionStepContext context, IDataAccessAdapter adapter)
         {
             try
             {
-                await base.Commit(inputKeys, context, adapter).ConfigureAwait(false);
+                await base.CommitAsync(inputKeys, context, adapter).ConfigureAwait(false);
             }
             catch (ORMConcurrencyException ex)
             {
