40486e812cd649aac2a846189cb000ce07cec9c2
Tim Hughes <t.hughes@shipworks.com>
2019-02-26 13:13:57 -0600

    TP#29948: Added HasCustomTriggers integration test

---
+++ Code/ShipWorks.Core.Tests.Integration/Data/Connection/SqlServerInfoTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using ShipWorks.Data.Connection;
+using ShipWorks.Startup;
+using ShipWorks.Tests.Shared.Database;
+using Xunit;
+
+namespace ShipWorks.Core.Tests.Integration.Data
+{
+    [Collection("Database collection")]
+    [Trait("Category", "ContinuousIntegration")]
+    public class SqlServerInfoTest : IDisposable
+    {
+        private readonly DataContext context;
+
+        public SqlServerInfoTest(DatabaseFixture db)
+        {
+            context = db.CreateDataContext(x => ContainerInitializer.Initialize(x));
+        }
+
+        [Fact]
+        public void HasCustomTriggers_ReturnsCorrectValue()
+        {
+            IDbConnection connection = SqlSession.Current.OpenConnection();
+            DropStoredProc(connection);
+
+            Assert.False(SqlServerInfo.HasCustomTriggers());
+
+            AddStoredProc(connection);
+            Assert.True(SqlServerInfo.HasCustomTriggers());
+        }
+
+        private void AddStoredProc(IDbConnection connection)
+        {
+            string sql = $@"
+                IF EXISTS (SELECT * FROM sys.objects WHERE [name] = N'ExtTrig' AND [type] = 'TR')
+                BEGIN
+                      DROP TRIGGER [dbo].[ExtTrig];
+                END;";
+            RunSqlNonQuery(connection, sql);
+
+            sql = @"
+                CREATE TRIGGER dbo.ExtTrig 
+                   ON  dbo.[Product]
+                   AFTER INSERT,DELETE,UPDATE
+                AS 
+                BEGIN
+                    SET NOCOUNT ON;
+                END";
+
+            RunSqlNonQuery(connection, sql);
+        }
+
+        private void DropStoredProc(IDbConnection connection)
+        {
+            string sql = $@"
+                IF EXISTS (SELECT * FROM sys.objects WHERE [name] = N'ExtTrig' AND [type] = 'TR')
+                BEGIN
+                      DROP TRIGGER [dbo].[ExtTrig];
+                END;";
+
+            RunSqlNonQuery(connection, sql);
+        }
+
+        private void RunSqlNonQuery(IDbConnection connection, string sql)
+        {
+            using (IDbCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = sql;
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
