9c2f09a7e129717ef5cda88df6f2a4ee150555ae
Tim Hughes <t.hughes@shipworks.com>
2019-03-05 12:42:21 -0600

    TP#30166: Updated FilterInfo view to return IsSavedSearch

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/6.2/6.2.0.2.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/6.2/6.2.0.2.sql
@@ -1,8 +1,20 @@
 ﻿PRINT N'Altering Filter'
 GO
 
-IF COL_LENGTH('Filter', 'IsOnDemand') IS NULL
+IF COL_LENGTH('Filter', 'IsSavedSearch') IS NULL
 BEGIN
 	ALTER TABLE [dbo].[Filter] ADD [IsSavedSearch] [bit] NOT NULL CONSTRAINT [DF_Filter_IsSavedSearch] DEFAULT ((0))
 END
+GO
+
+PRINT 'Altering FilterInfo view.'
+GO
+IF EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[dbo].[FilterInfo]'))
+	DROP VIEW [dbo].[FilterInfo]
+GO
+CREATE VIEW FilterInfo WITH ENCRYPTION AS
+	SELECT f.Name, f.FilterID, f.IsFolder, f.State, f.IsSavedSearch, f.[Definition], n.FilterNodeID, c.*
+		FROM FilterNode n INNER JOIN FilterSequence s ON n.FilterSequenceID = s.FilterSequenceID 
+						  INNER JOIN Filter f ON s.FilterID = f.FilterID 
+						  INNER JOIN FilterNodeContent c ON n.FilterNodeContentID = c.FilterNodeContentID
 GO
