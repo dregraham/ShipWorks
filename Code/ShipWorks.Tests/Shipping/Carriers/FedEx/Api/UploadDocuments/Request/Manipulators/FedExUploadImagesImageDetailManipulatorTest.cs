c8c705fc1533b9dd03438c9bf772f0b10f2bd0e5
Josh Winchester <j.winchester@shipworks.com>
2019-03-19 11:46:03 -0500

    TP#23813: Add unit tests for FedExUploadImage request and response

---
+++ Code/ShipWorks.Tests/Shipping/Carriers/FedEx/Api/UploadDocuments/Request/Manipulators/FedExUploadImagesImageDetailManipulatorTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.Api;
+using ShipWorks.Shipping.Carriers.FedEx.Api.UploadDocuments.Request.Manipulators;
+using ShipWorks.Shipping.Carriers.FedEx.WebServices.Track;
+using ShipWorks.Shipping.Carriers.FedEx.WebServices.UploadDocument;
+
+namespace ShipWorks.Tests.Shipping.Carriers.FedEx.Api.UploadDocuments.Request.Manipulators
+{
+    public class FedExUploadImagesImageDetailManipulatorTest
+    {
+        private FedExUploadImagesImageDetailManipulator testObject;
+
+        private Mock<CarrierRequest> carrierRequest;
+        private UploadImagesRequest nativeRequest;
+
+        private FedExAccountEntity account;
+        private List<UploadImageDetail> images;
+
+        public FedExUploadImagesImageDetailManipulatorTest()
+        {
+            account = new FedExAccountEntity { AccountNumber = "12345", MeterNumber = "12345", Letterhead = "Letterhead", Signature = "Signature" };
+            
+            nativeRequest = new UploadImagesRequest
+            {
+                Images = new UploadImageDetail[0]
+            };
+            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), null, nativeRequest);
+
+            testObject = new FedExUploadImagesImageDetailManipulator();
+        }
+
+        [Fact]
+        public void Manipulate_ThrowsArgumentNullException_WhenCarrierRequestIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => testObject.Manipulate(null));
+        }
+
+        [Fact]
+        public void Manipulate_ThrowsCarrierException_WhenNativeRequestIsNull()
+        {
+            // Setup the native request to be null
+            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), null, null);
+
+            Assert.Throws<CarrierException>(() => testObject.Manipulate(carrierRequest.Object));
+        }
+
+        [Fact]
+        public void Manipulate_ThrowsCarrierException_WhenNativeRequestIsNotUploadImagesRequest()
+        {
+            // Setup the native request to be an unexpected type
+            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), null, new TrackRequest());
+
+            Assert.Throws<CarrierException>(() => testObject.Manipulate(carrierRequest.Object));
+        }
+    }
+}
---
+++ Code/ShipWorks.Tests/Shipping/Carriers/FedEx/Api/UploadDocuments/Request/Manipulators/FedExUploadImagesVersionManipulatorTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using Moq;
+using ShipWorks.Shipping.Carriers.Api;
+using ShipWorks.Shipping.Carriers.FedEx.Api.UploadDocuments.Request.Manipulators;
+using ShipWorks.Shipping.Carriers.FedEx.WebServices.Track;
+using ShipWorks.Shipping.Carriers.FedEx.WebServices.UploadDocument;
+using VersionId = ShipWorks.Shipping.Carriers.FedEx.WebServices.UploadDocument.VersionId;
+
+namespace ShipWorks.Tests.Shipping.Carriers.FedEx.Api.UploadDocuments.Request.Manipulators
+{
+    public class FedExUploadImagesVersionManipulatorTest
+    {
+        private FedExUploadImagesVersionManipulator testObject;
+        private Mock<CarrierRequest> carrierRequest;
+        private UploadImagesRequest nativeRequest;
+
+        public FedExUploadImagesVersionManipulatorTest()
+        {
+            nativeRequest = new UploadImagesRequest { Version = new VersionId() };
+            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), null, nativeRequest);
+
+            testObject = new FedExUploadImagesVersionManipulator();
+        }
+
+        [Fact]
+        public void Manipulate_ThrowsArgumentNullException_WhenCarrierRequestIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => testObject.Manipulate(null));
+        }
+
+        [Fact]
+        public void Manipulate_ThrowsCarrierException_WhenNativeRequestIsNull()
+        {
+            // Setup the native request to be null
+            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), null, null);
+
+            Assert.Throws<CarrierException>(() => testObject.Manipulate(carrierRequest.Object));
+        }
+
+        [Fact]
+        public void Manipulate_ThrowsCarrierException_WhenNativeRequestIsNotUploadImagesRequest()
+        {
+            // Set up the native request
+            carrierRequest = new Mock<CarrierRequest>(new List<ICarrierRequestManipulator>(), null, new TrackRequest());
+
+            Assert.Throws<CarrierException>(() => testObject.Manipulate(carrierRequest.Object));
+        }
+
+        [Fact]
+        public void Manipulate_SetsServiceID_ForUploadImages()
+        {
+            testObject.Manipulate(carrierRequest.Object);
+            VersionId version = ((UploadImagesRequest) carrierRequest.Object.NativeRequest).Version;
+
+            // This assertion is based on the FedEx development guide.
+            // Please advice this before making changes.
+            Assert.Equal("cdus", version.ServiceId);
+        }
+
+        [Fact]
+        public void Manipulate_SetsMajor_ForUploadImages()
+        {
+            testObject.Manipulate(carrierRequest.Object);
+            VersionId version = ((UploadImagesRequest) carrierRequest.Object.NativeRequest).Version;
+
+            // This assertion is based on the FedEx development guide.
+            // Please advice this before making changes.
+            Assert.Equal(5, version.Major);
+        }
+
+        [Fact]
+        public void Manipulate_SetsIntermediate_ForUploadImages()
+        {
+            testObject.Manipulate(carrierRequest.Object);
+            VersionId version = ((UploadImagesRequest) carrierRequest.Object.NativeRequest).Version;
+
+            // This assertion is based on the FedEx development guide.
+            // Please advice this before making changes.
+            Assert.Equal(0, version.Intermediate);
+        }
+
+        [Fact]
+        public void Manipulate_SetsMinor_ForUploadImages()
+        {
+            testObject.Manipulate(carrierRequest.Object);
+            VersionId version = ((UploadImagesRequest) carrierRequest.Object.NativeRequest).Version;
+
+            // This assertion is based on the FedEx development guide.
+            // Please advice this before making changes.
+            Assert.Equal(0, version.Minor);
+        }
+    }
+}
