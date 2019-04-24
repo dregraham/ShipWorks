5e06902f64cf2941e6cbbda5d8818f803a4e837b
Tim Hughes <t.hughes@shipworks.com>
2019-04-24 08:00:43 -0500

    TP#31333: Saving a product, if its dirty, will also set it to be uploaded to the warehouse.

--- Code/ShipWorks.Products/ProductCatalog.cs
+++ Code/ShipWorks.Products/ProductCatalog.cs
@@ -286,6 +286,12 @@ namespace ShipWorks.Products
                 productVariant.CreatedDate = now;
             }
 
+            if (product.IsDirty || productVariant.IsDirty)
+            {
+                // Set the product to be uploaded to the warehouse
+                product.UploadToWarehouseNeeded = true;
+            }
+
             using (ISqlAdapter sqlAdapter = sqlAdapterFactory.CreateTransacted())
             {
                 try
