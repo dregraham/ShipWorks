fa3342f8a307b6cc436fc1da63a8fb8d279c8dc1
Garrett DeBlois <g.deblois@shipworks.com>
2019-04-29 15:24:42 -0500

    TP#31599: Commiting first part of Warehouse automation scripts

---
+++ Code/ShipWorks.Ranorex.Tests/WebAutomation/WarehouseAutomation/WarehouseAutomation/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+[assembly: TestCaseOrderer("Xunit.Extensions.Ordering.TestCaseOrderer", "Xunit.Extensions.Ordering")]
+[assembly: TestCollectionOrderer("Xunit.Extensions.Ordering.CollectionOrderer", "Xunit.Extensions.Ordering")]
+
+//this code is required by the Xunit Ordering extension
+//reference on how to use the extension: https://github.com/tomaszeman/Xunit.Extensions.Ordering#test-cases-ordering
