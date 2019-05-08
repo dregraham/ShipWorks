ccca0f9d7854a87b52f53c6ae734f86de2eb7400
Josh Winchester <j.winchester@shipworks.com>
2019-05-08 14:32:46 -0500

    TP#31348: Add AutoReturnShipmentService

---
+++ Code/ShipWorks.Shipping/Services/AutoReturnShipmentService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping.Services
+{
+    public class AutoReturnShipmentService
+    {
+        private IEnumerable<ShipmentEntity> shipments;
+        private List<Tuple<ShipmentEntity, ShipmentEntity>> shipmentsWithReturns;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AutoReturnShipmentService(IEnumerable<ShipmentEntity> shipments)
+        {
+            this.shipments = shipments;
+        }
+
+        /// <summary>
+        /// Creates an auto return shipment
+        /// </summary>
+        private ShipmentEntity CreateReturnShipment(ShipmentEntity shipment)
+        {
+            // Copy auto return shipment logic here
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Applies the return profile
+        /// </summary>
+        private void ApplyReturnProfile(ref ShipmentEntity shipment)
+        {
+            // Copy auto return profile logic here
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the new auto return shipments
+        /// </summary>
+        /// <returns>
+        /// A tuple that is of the original shipment and the
+        /// new return shipment.
+        /// </returns>
+        public List<Tuple<ShipmentEntity, ShipmentEntity>> GetShipments()
+        {
+            foreach (ShipmentEntity shipment in shipments)
+            {
+                if (shipment.IncludeReturn)
+                {
+                    // Create new auto return shipment
+                    var newReturnShipment = CreateReturnShipment(shipment);
+
+                    if (shipment.ApplyReturnProfile)
+                    {
+                        // Apply return profile
+                        ApplyReturnProfile(ref newReturnShipment);
+                    }
+                    shipmentsWithReturns.Add(new Tuple<ShipmentEntity, ShipmentEntity>(shipment, newReturnShipment));
+                }
+                else
+                {
+                    shipmentsWithReturns.Add(new Tuple<ShipmentEntity, ShipmentEntity>(shipment, null));
+                }
+            }
+
+            return shipmentsWithReturns;
+        }
+    }
+}
---
+++ Code/ShipWorks.Shipping/Services/IAutoReturnShipmentService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping.Services
+{
+    public interface IAutoReturnShipmentService
+    {
+        /// <summary>
+        /// Creates an auto return shipment
+        /// </summary>
+        ShipmentEntity CreateReturnShipment(ShipmentEntity shipment);
+
+        /// <summary>
+        /// Applies the return profile
+        /// </summary>
+        void ApplyReturnProfile(ref ShipmentEntity shipment);
+
+        /// <summary>
+        /// Gets the new auto return shipments
+        /// </summary>
+        /// <returns>
+        /// A tuple that is of the original shipment and the
+        /// new return shipment.
+        /// </returns>
+        List<Tuple<ShipmentEntity, ShipmentEntity>> GetShipments();
+    }
+}
