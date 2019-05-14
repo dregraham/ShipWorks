3cbf32c25f4ca3cc72683604c4efe085894137c8
Josh Winchester <j.winchester@shipworks.com>
2019-05-14 09:52:48 -0500

    TP#31348: Remove redundancy

--- Code/ShipWorks/Shipping/Carriers/Postal/PostalProfileControlBase.cs
+++ Code/ShipWorks/Shipping/Carriers/Postal/PostalProfileControlBase.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Linq;
 using System.Windows.Forms;
 using Autofac;
-using Interapptive.Shared;
 using Interapptive.Shared.ComponentRegistration;
 using Interapptive.Shared.Utility;
 using ShipWorks.ApplicationCore;
