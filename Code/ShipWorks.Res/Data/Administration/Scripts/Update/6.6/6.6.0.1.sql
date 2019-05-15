5ce5a51b072f72cc513d2a206a0fefd0bb15d98a
Kevin Croke <k.croke@shipworks.com>
2019-05-15 08:48:24 -0500

    TP#30689: Split DB script per code review

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/6.6/6.6.0.1.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/6.6/6.6.0.1.sql
@@ -2,9 +2,4 @@
 GO
 IF COL_LENGTH(N'[dbo].[Product]', N'UploadToWarehouseNeeded') IS NULL
 	ALTER TABLE [dbo].[Product] ADD [UploadToWarehouseNeeded] [bit] NOT NULL CONSTRAINT [DF_Product_UploadToWarehouseNeeded] DEFAULT (1)
-
-PRINT N'Altering [dbo].[Store]'
-GO
-IF COL_LENGTH(N'[dbo].[Store]', N'WarehouseStoreID') IS NULL
-	ALTER TABLE [dbo].Store ADD [WarehouseStoreID] [uniqueidentifier] NULL
 GO
