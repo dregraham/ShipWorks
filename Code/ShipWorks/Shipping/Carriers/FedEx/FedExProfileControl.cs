2d557f675a03e398a6748f742ca3333c6a117ac3
John Ackerman <j.ackerman@shipworks.com>
2019-05-16 13:17:08 -0500

    TP#31552: Only remove the OnStateCheckChanged event handler once

--- Code/ShipWorks/Shipping/Carriers/FedEx/FedExProfileControl.cs
+++ Code/ShipWorks/Shipping/Carriers/FedEx/FedExProfileControl.cs
@@ -103,9 +103,6 @@ namespace ShipWorks.Shipping.Carriers.FedEx
             // Remove state checkbox event handler since we'll be enabling/disabling manually
             returnShipmentState.CheckedChanged -= OnStateCheckChanged;
             includeReturnState.CheckedChanged -= OnStateCheckChanged;
-
-            // Remove this event handler twice since it's added twice by AddValueMapping above
-            applyReturnProfileState.CheckedChanged -= OnStateCheckChanged;
             applyReturnProfileState.CheckedChanged -= OnStateCheckChanged;
 
             // Manually enable/disable for mutually exclusive return controls
