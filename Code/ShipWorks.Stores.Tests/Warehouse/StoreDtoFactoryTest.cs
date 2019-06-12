43c4ad64675ed33dd3c79bf6377cf8f11c3a5828
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2019-06-12 13:49:31 -0500

    fix broken unit

--- Code/ShipWorks.Stores.Tests/Warehouse/StoreDtoFactoryTest.cs
+++ Code/ShipWorks.Stores.Tests/Warehouse/StoreDtoFactoryTest.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Threading.Tasks;
+using Autofac;
 using Autofac.Extras.Moq;
 using Interapptive.Shared.Security;
 using Moq;
 using Newtonsoft.Json;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Stores.Communication;
+using ShipWorks.Stores.Platforms.Amazon;
 using ShipWorks.Stores.Warehouse;
 using ShipWorks.Stores.Warehouse.Encryption;
 using ShipWorks.Stores.Warehouse.StoreData;
@@ -34,14 +36,17 @@ namespace ShipWorks.Stores.Tests.Warehouse
             amazonStoreEntity.ExcludeFBA = true;
             amazonStoreEntity.AmazonVATS = false;
             amazonStoreEntity.StoreTypeCode = StoreTypeCode.Amazon;
+            amazonStoreEntity.MerchantToken = "blah";
 
             var downloadStartingPoint = mock.Mock<IDownloadStartingPoint>();
             downloadStartingPoint.Setup(x => x.OnlineLastModified(amazonStoreEntity)).ReturnsAsync(DateTime.Now);
 
-
             var encryptionProviderFactory = mock.Mock<IEncryptionProviderFactory>();
 
             var storeTypeManager = mock.Mock<IStoreTypeManager>();
+            storeTypeManager.Setup(s => s.GetType(amazonStoreEntity))
+                .Returns(mock.Create<AmazonStoreType>(new TypedParameter(typeof(StoreEntity), amazonStoreEntity)));
+
             var encryptionService = mock.Mock<IWarehouseEncryptionService>();
 
             StoreDtoFactory testObject = new StoreDtoFactory(downloadStartingPoint.Object, storeTypeManager.Object, encryptionService.Object,encryptionProviderFactory.Object);
