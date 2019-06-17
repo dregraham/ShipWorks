7fe1d716010cb4269a5018a684db7876657422e1
Josh Winchester <j.winchester@shipworks.com>
2019-06-17 10:53:25 -0500

    TP#30907: Update phone fields from 25 characters to 35 characters

--- Code/ShipWorks.Data.Model/EntityClasses/OrderEntity.cs
+++ Code/ShipWorks.Data.Model/EntityClasses/OrderEntity.cs
@@ -1509,7 +1509,7 @@ namespace ShipWorks.Data.Model.EntityClasses
 
 		/// <summary> The ShipPhone property of the Entity Order<br/><br/></summary>
 		/// <remarks>Mapped on  table field: "Order"."ShipPhone"<br/>
-		/// Table field type characteristics (type, precision, scale, length): NVarChar, 0, 0, 25<br/>
+		/// Table field type characteristics (type, precision, scale, length): NVarChar, 0, 0, 35<br/>
 		/// Table field behavior characteristics (is nullable, is PK, is identity): false, false, false</remarks>
 		public virtual System.String ShipPhone
 		{
