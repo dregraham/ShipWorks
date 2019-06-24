79a2ce4b85090af475cac79464b5f7871c233aa0
Andrew Benz <a.benz@shipworks.com>
2019-06-24 09:49:31 -0500

    Rename update scripts before merging staging

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/6.6/6.6.0.1.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/6.6/6.6.0.1.sql
@@ -1,5 +1,10 @@
-﻿PRINT N'Altering [dbo].[Product]'
+﻿PRINT N'Altering [dbo].[Configuration]'
 GO
-IF COL_LENGTH(N'[dbo].[Product]', N'UploadToWarehouseNeeded') IS NULL
-	ALTER TABLE [dbo].[Product] ADD [UploadToWarehouseNeeded] [bit] NOT NULL CONSTRAINT [DF_Product_UploadToWarehouseNeeded] DEFAULT (1)
+IF COL_LENGTH(N'[dbo].[Configuration]', N'WarehouseID') IS NULL
+	ALTER TABLE [dbo].[Configuration] ADD [WarehouseID] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Configuration_WarehouseID] DEFAULT ('')
+
+IF COL_LENGTH(N'[dbo].[Configuration]', N'WarehouseName') IS NULL
+	ALTER TABLE [dbo].[Configuration] ADD [WarehouseName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Configuration_WarehouseName] DEFAULT ('')
 GO
+
+
--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/6.6/6.6.0.4.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/6.6/6.6.0.4.sql
@@ -1,11 +1,15 @@
-﻿PRINT N'Altering [dbo].[Download]'
+﻿PRINT N'Altering [dbo].[Order]'
+GO
+IF COL_LENGTH(N'[dbo].[Order]', N'HubSequence') IS NULL
+    ALTER TABLE [dbo].[Order]
+        ADD [HubOrderID] [uniqueidentifier] NULL,
+            [HubSequence] [bigint] NULL
+
 GO
 
-IF COL_LENGTH(N'[dbo].[Download]', N'BatchID') IS NULL
-ALTER TABLE [dbo].[Download] ADD [BatchID] [uniqueidentifier] NOT NULL CONSTRAINT [DF_Download_BatchID] DEFAULT NEWID()
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[Order]')
+                                 AND name = N'[IX_SWDefault_Order_HubSequence]')
+BEGIN
+    CREATE NONCLUSTERED INDEX [IX_SWDefault_Order_HubSequence] ON [dbo].[Order] ([HubSequence])
+END
 GO
-
-IF OBJECT_ID('[dbo].[DF_Download_BatchID]', 'D') IS NOT NULL
-ALTER TABLE [dbo].[Download] DROP CONSTRAINT [DF_Download_BatchID]
-GO
-
