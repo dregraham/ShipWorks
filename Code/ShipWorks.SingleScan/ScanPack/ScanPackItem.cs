e5e14b3a81bf1fcd7618fd0b6f4df955694fd261
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2019-07-16 15:32:20 -0500

    TP#32874 create ScanPackItem

---
+++ Code/ShipWorks.SingleScan/ScanPack/ScanPackItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+
+namespace ShipWorks.SingleScan.ScanPack
+{
+    /// <summary>
+    /// Represents an item to be scan and packed
+    /// </summary>
+    public class ScanPackItem
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ScanPackItem(string name, string imageUrl, double quantity)
+        {
+            Name = name;
+            ImageUrl = imageUrl;
+            Quantity = quantity;
+        }
+
+        /// <summary>
+        /// The Items Name
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The Items Image Url
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public string ImageUrl { get; set; }
+
+        /// <summary>
+        /// The Items Quantity
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public double Quantity { get; set; }
+    }
+}
