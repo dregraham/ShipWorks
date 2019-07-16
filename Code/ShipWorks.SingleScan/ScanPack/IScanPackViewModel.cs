2feb76607d00577237c1e1666725ae437b5e466d
Kevin Croke <k.croke@shipworks.com>
2019-07-16 15:44:22 -0500

    TP#32874: Add IScanPackViewModel

---
+++ Code/ShipWorks.SingleScan/ScanPack/IScanPackViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.SingleScan.ScanPack
+{
+    /// <summary>
+    /// Viewmodel for ScanPackView
+    /// </summary>
+    public interface IScanPackViewModel
+    {
+        /// <summary>
+        /// Load an order
+        /// </summary>
+        void Load(string orderNumber);
+    }
+}
