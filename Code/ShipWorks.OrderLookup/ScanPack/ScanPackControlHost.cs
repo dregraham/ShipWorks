c5e784c744f2baeb6fa96c94ba84dbc811843199
Chris Hicks <c.hicks@shipworks.com>
2019-07-17 14:02:50 -0600

    TP#32874: Create ScanPackControlHost. Move ScanPackViewModel to SingleScan assembly.

---
+++ Code/ShipWorks.OrderLookup/ScanPack/ScanPackControlHost.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.OrderLookup.ScanPack.ScanPack;
+using ShipWorks.SingleScan.ScanPack;
+using ShipWorks.UI.Controls;
+
+namespace ShipWorks.OrderLookup.ScanPack
+{
+    [Component(RegisterAs = RegistrationType.SpecificService, Service = typeof(IScanPack))]
+    public partial class ScanPackControlHost : UserControl, IScanPack
+    {
+        private readonly IScanPackViewModel scanPackViewModel;
+        private ScanPackControl scanPackControl;
+
+        public ScanPackControlHost(IScanPackViewModel scanPackViewModel)
+        {
+            InitializeComponent();
+            this.scanPackViewModel = scanPackViewModel;
+        }
+
+        /// <summary>
+        /// Expose the Control
+        /// </summary>
+        public UserControl Control => this;
+
+        /// <summary>
+        /// Set the element host on load
+        /// </summary>
+        protected override void OnLoad(EventArgs e)
+        {
+            Dock = DockStyle.Fill;
+
+            base.OnLoad(e);
+
+            scanPackControl = new ScanPackControl
+            {
+                DataContext = scanPackViewModel
+            };
+
+            ElementHost host = new ElementHost
+            {
+                Dock = DockStyle.Fill,
+                Child = scanPackControl
+            };
+
+            Controls.Add(host);
+        }
+    }
+}
--- Code/ShipWorks.SingleScan/ScanPack/ScanPackViewModel.cs
+++ Code/ShipWorks.SingleScan/ScanPack/ScanPackViewModel.cs
@@ -1,18 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
 using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Shipping;
 
 namespace ShipWorks.SingleScan.ScanPack
 {
     [Component]
-    public class ScanPackViewModel : IScanPackViewModel
+    public class ScanPackViewModel : ViewModelBase, IScanPackViewModel
     {
+        private readonly IOrderLoader orderLoader;
+        private ObservableCollection<ScanPackItem> itemsToScan;
+        private ObservableCollection<ScanPackItem> scannedItems;
+        private string scanHeader;
+        private string scanImageUrl;
+        private string scanFooter;
+
+        public ScanPackViewModel(IOrderLoader orderLoader)
+        {
+            this.orderLoader = orderLoader;
+        }
+
+        public ObservableCollection<ScanPackItem> ItemsToScan
+        {
+            get => itemsToScan;
+            set => Set(ref itemsToScan, value);
+        }
+
+        public ObservableCollection<ScanPackItem> ScannedItems
+        {
+            get => scannedItems;
+            set => Set(ref scannedItems, value);
+        }
+
+        public int TotalItemCount => ItemsToScan.Count + ScannedItems.Count;
+
+        public string ScanHeader
+        {
+            get => scanHeader;
+            set => Set(ref scanHeader, value);
+        }
+
+        public string ScanImageUrl
+        {
+            get => scanImageUrl;
+            set => Set(ref scanImageUrl, value);
+        }
+
+        public string ScanFooter
+        {
+            get => scanFooter;
+            set => Set(ref scanFooter, value);
+        }
+
         public void Load(string orderNumber)
         {
-            throw new NotImplementedException();
+
+        }
+
+        public void Update()
+        {
+            // No order scanned yet
+            if (TotalItemCount == 0)
+            {
+                ScanHeader = "Ready to Scan and Pack";
+                ScanFooter = "Scan an Order to Begin";
+            }
+            else
+            {
+                // Order has been scanned, still items left to scan
+                if (ItemsToScan.Any())
+                {
+                    ScanHeader = "Scan an Item to Pack";
+                    ScanFooter = $"{ScannedItems.Count} of {TotalItemCount} items have been scanned";
+                }
+                else
+                {
+                    // Order has been scanned, all items have been scanned
+                    ScanHeader = "This order has been verified!";
+                    ScanFooter = "Scan a new order to continue";
+                }
+            }
         }
     }
 }
