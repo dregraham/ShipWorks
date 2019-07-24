e92cdab73ba76b5054bef0d52f60cca7d31b091e
Andrew Benz <a.benz@shipworks.com>
2019-07-24 14:38:55 -0500

    TP#32803: Upload BigCommerce store details to the Hub

---
+++ Code/ShipWorks.Stores/Platforms/BigCommerce/Warehouse/BigCommerceStore.cs
@@ -0,0 +1,16 @@
+﻿using ShipWorks.ApplicationCore.Licensing.Warehouse.DTO;
+
+namespace ShipWorks.Stores.Platforms.BigCommerce.Warehouse
+{
+    /// <summary>
+    /// Big Commerce warehouse store DTO
+    /// </summary>
+    public class BigCommerceStore : Store
+    {
+        public string ClientId { get; internal set; }
+        public string AccessToken { get; internal set; }
+        public string Url { get; internal set; }
+        public int DownloadNumberOfDaysBack { get; internal set; }
+        public int? InitialDownloadDays { get; internal set; }
+    }
+}
