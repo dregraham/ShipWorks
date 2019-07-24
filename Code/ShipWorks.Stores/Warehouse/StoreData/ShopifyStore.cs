955861ba32fb02e81bea6f13a54f109e2941c074
Josh Winchester <j.winchester@shipworks.com>
2019-07-24 18:24:58 -0500

    TP#30352: Add Shopify to warehouse

---
+++ Code/ShipWorks.Stores/Warehouse/StoreData/ShopifyStore.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using ShipWorks.ApplicationCore.Licensing.Warehouse.DTO;
+
+namespace ShipWorks.Stores.Warehouse.StoreData
+{
+    /// <summary>
+    /// Shopify store credentials needed for downloading
+    /// </summary>
+    [Obfuscation(Exclude = true, ApplyToMembers = true, StripAfterObfuscation = false)]
+    public class ShopifyStore : Store
+    {
+        /// <summary>
+        /// The date to start downloading orders from
+        /// </summary>
+        public ulong DownloadStartDate { get; set; }
+
+        /// <summary>
+        /// The auth token this store
+        /// </summary>
+        public string ShopifyToken { get; set; }
+    }
+}
