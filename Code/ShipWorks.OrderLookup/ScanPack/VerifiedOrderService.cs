f57430c2d3eea875e1e4de7b5cbf8319dcd4f7ec
Chris Hicks <c.hicks@shipworks.com>
2019-07-25 16:03:36 -0600

    TP#32879: Fix crash when loading Order Lookup mode. Add comments.

--- Code/ShipWorks.OrderLookup/ScanPack/VerifiedOrderService.cs
+++ Code/ShipWorks.OrderLookup/ScanPack/VerifiedOrderService.cs
@@ -1,25 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Interapptive.Shared.ComponentRegistration;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Stores.Orders;
 
 namespace ShipWorks.OrderLookup.ScanPack
 {
+    /// <summary>
+    /// Service for interacting with verified orders
+    /// </summary>
+    [Component]
     public class VerifiedOrderService : IVerifiedOrderService
     {
         private readonly IOrderRepository orderRepository;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public VerifiedOrderService(IOrderRepository orderRepository)
         {
             this.orderRepository = orderRepository;
         }
+
+        /// <summary>
+        /// Save verified order data
+        /// </summary>
         public void Save(OrderEntity order)
         {
             orderRepository.Save(order);
-
         }
     }
 }
