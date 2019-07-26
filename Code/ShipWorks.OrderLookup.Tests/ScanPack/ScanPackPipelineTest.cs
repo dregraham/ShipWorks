fd636d84559af6c021cbc0a239317370b297c7b1
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2019-07-26 10:33:15 -0500

    TP#32882 write units for pipeline, add boarder to message

---
+++ Code/ShipWorks.OrderLookup.Tests/ScanPack/ScanPackPipelineTest.cs
@@ -0,0 +1,64 @@
+﻿using Autofac.Extras.Moq;
+using Interapptive.Shared.Threading;
+using Microsoft.Reactive.Testing;
+using Moq;
+using ShipWorks.ApplicationCore.Licensing;
+using ShipWorks.Core.Messaging;
+using ShipWorks.Editions;
+using ShipWorks.OrderLookup.ScanPack;
+using ShipWorks.Tests.Shared;
+using Xunit;
+
+namespace ShipWorks.OrderLookup.Tests.ScanPack
+{
+    public class ScanPackPipelineTest
+    {
+        private readonly AutoMock mock;
+        private readonly TestScheduler scheduler;
+        private readonly TestMessenger testMessenger;
+        private readonly Mock<ILicenseService> licenseService;
+        private readonly Mock<IScanPackViewModel> scanPackViewModel;
+        private readonly ScanPackPipeline testObject;
+
+        public ScanPackPipelineTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+
+            Mock<ISchedulerProvider> scheduleProvider = mock.WithMockImmediateScheduler();
+            scheduler = new TestScheduler();
+            scheduleProvider.Setup(s => s.WindowsFormsEventLoop).Returns(scheduler);
+
+            licenseService = mock.Mock<ILicenseService>();
+            scanPackViewModel = mock.Mock<IScanPackViewModel>();
+
+            testMessenger = new TestMessenger();
+            mock.Provide<IMessenger>(testMessenger);
+
+            testObject = mock.Create<ScanPackPipeline>();
+        }
+
+        [Fact]
+        public void InitializeForCurrentScope_DisablesScanPack_WhenFeatureRestricted()
+        {
+            licenseService
+                .Setup(l => l.CheckRestriction(EditionFeature.Warehouse, null))
+                .Returns(EditionRestrictionLevel.Forbidden);
+
+            testObject.InitializeForCurrentScope();
+
+            scanPackViewModel.Verify(s => s.Enabled == false);
+        }
+
+        [Fact]
+        public void InitializeForCurrentScope_EnablesScanPack_WhenFeatureRestricted()
+        {
+            licenseService
+                .Setup(l => l.CheckRestriction(EditionFeature.Warehouse, null))
+                .Returns(EditionRestrictionLevel.None);
+
+            testObject.InitializeForCurrentScope();
+
+            scanPackViewModel.Verify(s => s.Enabled == true);
+        }
+    }
+}
