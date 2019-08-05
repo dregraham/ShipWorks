f722c42d1b7049c81e180e283fad85e1a334494f
Tim Hughes <t.hughes@shipworks.com>
2019-08-05 11:16:10 -0500

    Forcing the strange sql character fix again

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.0/7.0.0.2.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.0/7.0.0.2.sql
@@ -1,6 +1,7 @@
-﻿PRINT N'Altering [dbo].[ChannelAdvisorStore]'
-GO
-
-IF COL_LENGTH(N'[dbo].[ChannelAdvisorStore]', N'DownloadModifiedNumberOfDaysBack') IS NOT NULL
-ALTER TABLE [dbo].[ChannelAdvisorStore] DROP COLUMN [DownloadModifiedNumberOfDaysBack]
-GO
+﻿PRINT N'Altering [dbo].[ChannelAdvisorStore]' 
+GO 
+ 
+IF COL_LENGTH(N'[dbo].[ChannelAdvisorStore]', N'DownloadModifiedNumberOfDaysBack') IS NOT NULL 
+ALTER TABLE [dbo].[ChannelAdvisorStore] DROP COLUMN [DownloadModifiedNumberOfDaysBack] 
+GO 
+ 
