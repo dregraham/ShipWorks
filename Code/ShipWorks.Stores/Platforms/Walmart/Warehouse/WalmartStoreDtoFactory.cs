fd31f1c9b590ea0bf706be88a7ff3fbf3893a90d
Tim Hughes <t.hughes@shipworks.com>
2019-08-08 08:19:15 -0500

    Fixed encryption/decryption before uploading walmart store to hub
    (cherry picked from commit 4a4d3db83be63853bdfacb3bbc43b3aeba4cefd1)

--- Code/ShipWorks.Stores/Platforms/Walmart/Warehouse/WalmartStoreDtoFactory.cs
+++ Code/ShipWorks.Stores/Platforms/Walmart/Warehouse/WalmartStoreDtoFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.Security;
 using ShipWorks.ApplicationCore.Licensing.Warehouse.DTO;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Stores.Warehouse;
@@ -13,13 +14,16 @@ namespace ShipWorks.Stores.Platforms.Walmart.Warehouse
     public class WalmartStoreDtoFactory : IStoreDtoFactory
     {
         private readonly IStoreDtoHelpers helpers;
+        private readonly IEncryptionProviderFactory encryptionProviderFactory;
 
         /// <summary>
         /// Constructor
         /// </summary>
-        public WalmartStoreDtoFactory(IStoreDtoHelpers helpers)
+        public WalmartStoreDtoFactory(IStoreDtoHelpers helpers,
+            IEncryptionProviderFactory encryptionProviderFactory)
         {
             this.helpers = helpers;
+            this.encryptionProviderFactory = encryptionProviderFactory;
         }
 
         /// <summary>
@@ -30,8 +34,11 @@ namespace ShipWorks.Stores.Platforms.Walmart.Warehouse
             var storeEntity = baseStoreEntity as WalmartStoreEntity;
             var store = helpers.PopulateCommonData(storeEntity, new WalmartStore());
 
+            IEncryptionProvider encryptionProvider = encryptionProviderFactory.CreateWalmartEncryptionProvider();
+            string decryptedClientSecret = encryptionProvider.Decrypt(storeEntity.ClientSecret);
+
             store.ClientId = storeEntity.ClientID;
-            store.ClientSecret = await helpers.EncryptSecret(storeEntity.ClientSecret).ConfigureAwait(false);
+            store.ClientSecret = await helpers.EncryptSecret(decryptedClientSecret).ConfigureAwait(false);
             store.DownloadNumberOfDaysBack = storeEntity.DownloadModifiedNumberOfDaysBack;
 
             return store;
