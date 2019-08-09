5df5878748d04e440f68b6cd64cefb8650202247
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2019-08-09 09:56:46 -0500

    TP#33479 stub out OdbcStoreClient

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/Warehouse/IOdbcStoreClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using ShipWorks.ApplicationCore.Licensing.Warehouse.DTO;
+using ShipWorks.Stores.Platforms.Odbc.Mapping;
+
+namespace ShipWorks.Stores.Platforms.Odbc.Warehouse
+{
+    /// <summary>
+    /// Client for interacting with odbc stores and warehouse
+    /// </summary>
+    public interface IOdbcStoreClient
+    {
+        /// <summary>
+        /// Get the warehouse customers odbc stores where key is the warehouse store id
+        /// </summary>
+        IDictionary<Guid, Store> GetStores();
+
+        /// <summary>
+        /// Get the given warehouseStoreIds import map
+        /// </summary>
+        IOdbcFieldMap GetImportMap(Guid warehouseStoreId);
+
+        /// <summary>
+        /// Get the given warehouseStoreIds upload map
+        /// </summary>
+        IOdbcFieldMap GetUploadMap(Guid warehouseStoreId);
+    }
+}
---
+++ Code/ShipWorks.Stores/Platforms/Odbc/Warehouse/OdbcStoreClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShipWorks.ApplicationCore.Licensing.Warehouse.DTO;
+using ShipWorks.Stores.Platforms.Odbc.Mapping;
+
+namespace ShipWorks.Stores.Platforms.Odbc.Warehouse
+{
+    /// <summary>
+    /// Client for interacting with Odbc Stores and the hub
+    /// </summary>
+    public class OdbcStoreClient : IOdbcStoreClient
+    {
+        /// <summary>
+        /// Get the warehouse customers odbc stores where key is the warehouse store id
+        /// </summary>
+        public IDictionary<Guid, Store> GetStores()
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        /// <summary>
+        /// Get the given warehouseStoreIds import map
+        /// </summary>
+        public IOdbcFieldMap GetImportMap(Guid warehouseStoreId)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        /// <summary>
+        /// Get the given warehouseStoreIds upload map
+        /// </summary>
+        public IOdbcFieldMap GetUploadMap(Guid warehouseStoreId)
+        {
+            throw new NotImplementedException("TODO");
+        }
+    }
+
+}
