9195ea9d6a6a2bce652a3b870e570c84761cbbf5
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2019-08-09 14:41:44 -0500

    TP#32894 stub out Odbc OrderFactory

---
+++ Code/ShipWorks.Stores/Platforms/Odbc/OdbcWarehouseOrderFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.Utility;
+using ShipWorks.Data.Import;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Data.Model.EntityInterfaces;
+using ShipWorks.Warehouse;
+using ShipWorks.Warehouse.DTO.Orders;
+
+namespace ShipWorks.Stores.Platforms.Odbc
+{
+    [KeyedComponent(typeof(IWarehouseOrderFactory), StoreTypeCode.Odbc)]
+    public class OdbcWarehouseOrderFactory : WarehouseOrderFactory
+    {
+        private readonly ILog log;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public OdbcWarehouseOrderFactory(IOrderElementFactory orderElementFactory, Func<Type, ILog> logFactory) :
+            base(orderElementFactory)
+        {
+            log = logFactory(typeof(OdbcWarehouseOrderFactory));
+        }
+
+        protected override Task<GenericResult<OrderEntity>> CreateStoreOrderEntity(IStoreEntity store, StoreType storeType, WarehouseOrder warehouseOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void LoadStoreItemDetails(IStoreEntity store, OrderItemEntity itemEntity, WarehouseOrderItem warehouseItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void LoadStoreOrderDetails(OrderEntity orderEntity, WarehouseOrder warehouseOrder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
