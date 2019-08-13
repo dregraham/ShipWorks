0184cbe9a19b4ba1629856e0d5634622f14a654c
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2019-08-13 09:42:10 -0500

    TP#32894 code review comments

--- Code/ShipWorks.Stores/Platforms/Odbc/OdbcWarehouseOrderFactory.cs
+++ Code/ShipWorks.Stores/Platforms/Odbc/OdbcWarehouseOrderFactory.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Interapptive.Shared.ComponentRegistration;
 using Interapptive.Shared.Utility;
@@ -14,6 +11,9 @@ using ShipWorks.Warehouse.DTO.Orders;
 
 namespace ShipWorks.Stores.Platforms.Odbc
 {
+    /// <summary>
+    /// Factory for creating Odbc orders from warehouse orders
+    /// </summary>
     [KeyedComponent(typeof(IWarehouseOrderFactory), StoreTypeCode.Odbc)]
     public class OdbcWarehouseOrderFactory : WarehouseOrderFactory
     {
@@ -28,16 +28,25 @@ namespace ShipWorks.Stores.Platforms.Odbc
             log = logFactory(typeof(OdbcWarehouseOrderFactory));
         }
 
+        /// <summary>
+        /// Create an order entity with the store specific identifier
+        /// </summary>
         protected override Task<GenericResult<OrderEntity>> CreateStoreOrderEntity(IStoreEntity store, StoreType storeType, WarehouseOrder warehouseOrder)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Load store specific order details
+        /// </summary>
         protected override void LoadStoreItemDetails(IStoreEntity store, OrderItemEntity itemEntity, WarehouseOrderItem warehouseItem)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Load store specific item details
+        /// </summary>
         protected override void LoadStoreOrderDetails(OrderEntity orderEntity, WarehouseOrder warehouseOrder)
         {
             throw new NotImplementedException();
