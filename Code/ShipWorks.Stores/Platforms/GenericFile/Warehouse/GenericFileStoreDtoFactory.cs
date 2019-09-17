733885b4af829ff02b459922063fb0b44827b94e
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2019-09-17 14:56:54 -0500

    TP#33622 create GenericFileStoreDtoFactory

---
+++ Code/ShipWorks.Stores/Platforms/GenericFile/Warehouse/GenericFileStoreDtoFactory.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading.Tasks;
+using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.ApplicationCore.Licensing.Warehouse.DTO;
+using ShipWorks.Data;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Warehouse;
+
+namespace ShipWorks.Stores.Platforms.GenericFile.Warehouse
+{
+    /// <summary>
+    /// Factory for creating StoreDtos from StoreEntities
+    /// </summary>
+    [KeyedComponent(typeof(IStoreDtoFactory), StoreTypeCode.GenericFile)]
+    public class GenericFileStoreDtoFactory : IStoreDtoFactory
+    {
+        private readonly IStoreDtoHelpers helpers;
+        readonly Lazy<string> warehouseID;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="helpers"></param>
+        public GenericFileStoreDtoFactory(IStoreDtoHelpers helpers, IConfigurationData configurationData)
+        {
+            this.helpers = helpers;
+            warehouseID = new Lazy<string>(() => configurationData.FetchReadOnly().WarehouseID);
+        }
+
+        /// <summary>
+        /// Create a StoreDto from the given store entity
+        /// </summary>
+        public Task<Store> Create(StoreEntity baseStoreEntity)
+        {
+            GenericFileStore store = helpers.PopulateCommonData(baseStoreEntity, new GenericFileStore());
+            store.UniqueIdentifier = Guid.NewGuid().ToString();
+
+            GenericFileStoreEntity storeEntity = baseStoreEntity as GenericFileStoreEntity;
+
+            if (storeEntity.FileFormat == (int) GenericFileFormat.Xml)
+            {
+                store.ImportMap = storeEntity.XmlXsltContent;
+            }
+            else
+            {
+                store.ImportMap = storeEntity.FlatImportMap;
+            }
+
+            store.FileFormat = storeEntity.FileFormat;
+
+            return Task.FromResult<Store>(store);
+        }
+    }
+}
