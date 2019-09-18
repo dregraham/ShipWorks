5976b7b09cd641055c5e88f65beb678cd290a480
Cody Hart <CHart@interapptive.local>
2019-09-18 09:46:57 -0500

    TP #33627: Update EbayItemID column in SQL Server

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.3/7.3.1.1.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.3/7.3.1.1.sql
@@ -1,16 +1,16 @@
-PRINT N'Altering [dbo].[EndiciaShipment]'
+PRINT N'Altering [dbo].[EbayOrder]'
 GO
-ALTER TABLE [dbo].[EndiciaShipment] ADD
-[ReferenceID2] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_EndiciaShipment_ReferenceID2] DEFAULT (''),
-[ReferenceID3] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_EndiciaShipment_ReferenceID3] DEFAULT (''),
-[ReferenceID4] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_EndiciaShipment_ReferenceID4] DEFAULT (''),
-[GroupCode] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_EndiciaShipment_GroupCode] DEFAULT ('')
-GO
-PRINT N'Altering [dbo].[EndiciaProfile]'
-GO
-ALTER TABLE [dbo].[EndiciaProfile] ADD
-[ReferenceID2] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[ReferenceID3] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[ReferenceID4] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[GroupCode] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+;with ItemsToUpdate (OrderID, EbayOrderID, EbayItemID) AS (
+SELECT eo.OrderID, eo.EbayOrderID, eoi.EbayItemID
+FROM EbayOrder AS eo
+CROSS APPLY
+    (SELECT TOP 1 EbayItemID
+     FROM EbayOrderItem
+     WHERE OrderID = eo.OrderID
+     ORDER BY OrderID DESC) AS eoi
+WHERE eo.EbayOrderID = 0
+)
+update eo SET eo.EbayOrderID = eoi.EbayItemID FROM
+[dbo].[EbayOrder] eo inner join
+ItemsToUpdate eoi ON eo.OrderID = eoi.OrderID
 GO
