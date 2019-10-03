2f7903a019a7b8162627eb4dd4548e0010742802
Chris Hicks <c.hicks@shipworks.com>
2019-10-03 10:30:00 -0600

    TP#34543: Remove expanders and carrier checkboxes from BestRateAccountSettingsControl. Remove test data from BestRateAccountSettingsViewModel

--- Code/ShipWorks.Shipping.UI/Settings/BestRateAccountSettingsViewModel.cs
+++ Code/ShipWorks.Shipping.UI/Settings/BestRateAccountSettingsViewModel.cs
@@ -1,67 +1,31 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.Utility;
 
 namespace ShipWorks.Shipping.UI.Settings
 {
     [Component(RegistrationType.Self)]
     public class BestRateAccountSettingsViewModel
     {
+        private static readonly List<ShipmentTypeCode> excludedShipmentTypes = new List<ShipmentTypeCode>
+        {
+            ShipmentTypeCode.None,
+            ShipmentTypeCode.BestRate,
+            ShipmentTypeCode.Other,
+            ShipmentTypeCode.PostalWebTools,
+            ShipmentTypeCode.Express1Endicia,
+            ShipmentTypeCode.Express1Usps,
+            ShipmentTypeCode.UpsWorldShip,
+            ShipmentTypeCode.AmazonSFP,
+            ShipmentTypeCode.iParcel
+        };
+
         public BestRateAccountSettingsViewModel()
         {
             Carriers = new ObservableCollection<BestRateCarrier>();
-            Carriers.Add(new BestRateCarrier()
-            {
-                Name = "USPS",
-                ShipmentType = ShipmentTypeCode.Usps,
-                Accounts = new ObservableCollection<BestRateAccount>()
-                {
-                    new BestRateAccount()
-                    {
-                        AccountID = "12312",
-                        IsActive = true
-                    },
-                    new BestRateAccount()
-                    {
-                        AccountID = "7890192",
-                        IsActive = true
-                    },                    new BestRateAccount()
-                    {
-                        AccountID = "6789732189",
-                        IsActive = true
-                    },
-                }
-
-            });
-            Carriers.Add(new BestRateCarrier()
-            {
-                Name = "UPS",
-                ShipmentType = ShipmentTypeCode.UpsOnLineTools,
-                Accounts = new ObservableCollection<BestRateAccount>()
-                {
-                    new BestRateAccount()
-                    {
-                        AccountID = "12312",
-                        IsActive = true
-                    },
-                    new BestRateAccount()
-                    {
-                        AccountID = "1254",
-                        IsActive = true
-                    },                    new BestRateAccount()
-                    {
-                        AccountID = "24313125",
-                        IsActive = true
-                    },                    new BestRateAccount()
-                    {
-                        AccountID = "213",
-                        IsActive = true
-                    },                    new BestRateAccount()
-                    {
-                        AccountID = "789",
-                        IsActive = true
-                    }                    
-                }
-            });
         }
 
         public ObservableCollection<BestRateCarrier> Carriers { get; set; }
