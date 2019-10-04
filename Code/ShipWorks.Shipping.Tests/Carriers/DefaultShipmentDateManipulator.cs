c0f5ff9388181d304667815a535ec2fc7d498ebe
Cody Hart <c.hart@shipworks.com>
2019-10-04 09:49:24 -0500

    TP #32031: Update manipulator tests to align with new time output

--- Code/ShipWorks.Shipping.Tests/Carriers/DefaultShipmentDateManipulator.cs
+++ Code/ShipWorks.Shipping.Tests/Carriers/DefaultShipmentDateManipulator.cs
@@ -19,7 +19,7 @@ namespace ShipWorks.Shipping.Tests.Carriers
 
         [Theory]
         [InlineData(true, "2017-10-02 10:00:00", "2017-10-03 12:00:00", "2017-10-02 10:00:00")]
-        [InlineData(false, "2017-10-02 10:00:00", "2017-10-03 10:00:00", "2017-10-02 10:00:00")]
+        [InlineData(false, "2017-10-02 10:00:00", "2017-10-03 10:00:00", "2017-10-03 5:00:00")]
         [InlineData(false, "2017-10-03 08:00:00", "2017-10-03 10:00:00", "2017-10-03 8:00:00")]
         [InlineData(false, "2017-10-04 08:00:00", "2017-10-03 10:00:00", "2017-10-04 8:00:00")]
         public void Manipulate_SetsDate_Properly(bool processed, string shipDateText, string nowText, string expectedText)
