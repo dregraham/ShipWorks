ac6b239e919fca1206edaf1b8e7a766f8be012b6
John Ackerman <j.ackerman@shipworks.com>
2019-10-22 10:42:28 -0500

    TP #35063: Add default value to DownloadDaysBack

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.4/7.4.0.3.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.4/7.4.0.3.sql
@@ -1,5 +1,5 @@
 PRINT N'Altering [dbo].[ChannelAdvisorStore]'
 GO
 IF COL_LENGTH(N'[dbo].[ChannelAdvisorStore]', N'DownloadDaysBack') IS NULL
-ALTER TABLE [dbo].[ChannelAdvisorStore] ADD [DownloadDaysBack] [tinyint] NOT NULL
+ALTER TABLE [dbo].[ChannelAdvisorStore] ADD [DownloadDaysBack] [tinyint] NOT NULL DEFAULT ((0))
 GO
