a7434f9eaba13a56357a8db790122863ab11895b
John Ackerman <j.ackerman@shipworks.com>
2019-11-07 11:41:02 -0600

    TP #34801: WIP: Add RakutenDownloader and RakutenWebClient

---
+++ Code/ShipWorks.Stores/Platforms/Rakuten/DTO/RakutenErrors.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ShipWorks.Stores.Platforms.Rakuten.DTO
+{
+    public class RakutenErrors
+    {
+        [JsonProperty("failedResources")]
+        IList<string> FailedResources { get; set; }
+
+        [JsonProperty("common")]
+        IList<RakutenError> Common { get; set; }
+
+        [JsonProperty("specific")]
+        Dictionary<string, IList<RakutenError>> Specific { get; set; }
+    }
+
+    public class RakutenError
+    {
+        [JsonProperty("errorCode")]
+        int ErrorCode { get; set; }
+
+        [JsonProperty("shortMessage")]
+        string ShortMessage { get; set; }
+
+        [JsonProperty("longMessage")]
+        string LongMessage { get; set; }
+    }
+}
---
+++ Code/ShipWorks.Stores/Platforms/Rakuten/DTO/RakutenOrdersResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ShipWorks.Stores.Platforms.Rakuten.DTO
+{
+    /// <summary>
+    /// Response returned by Rakuten when searching for orders
+    /// </summary>
+    public class RakutenOrdersResponse
+    {
+        [JsonProperty("orders")]
+        IList<RakutenOrder> Orders { get; set; }
+
+        [JsonProperty("errors")]
+        RakutenErrors Errors { get; set; }
+
+        [JsonProperty("totalCount")]
+        int TotalCount { get; set; }
+    }
+}
