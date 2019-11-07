2b42b2797a21d88efc5b6f8161b4d1f4553c398b
Cody Hart <c.hart@shipworks.com>
2019-11-07 11:21:12 -0600

    TP #34801: Add store setup control host

---
+++ Code/ShipWorks.Stores.UI/Platforms/Rakuten/RakutenStoreSetupControlHost.cs
@@ -0,0 +1,49 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.ComponentRegistration.Ordering;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Management;
+using ShipWorks.Stores.Platforms.Rakuten;
+using ShipWorks.UI.Wizard;
+
+namespace ShipWorks.Stores.UI.Platforms.Rakuten
+{
+    /// <summary>
+    /// Store setup control host for Rakuten
+    /// </summary>
+    [KeyedComponent(typeof(WizardPage), StoreTypeCode.Rakuten, ExternallyOwned = true)]
+    [Order(typeof(WizardPage), Order.Unordered)]
+    public partial class RakutenStoreSetupControlHost : AddStoreWizardPage
+    {
+        private readonly IRakutenStoreSetupControlViewModel viewModel;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public RakutenStoreSetupControlHost(IRakutenStoreSetupControlViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+            InitializeComponent();
+
+            storeSetupControl.DataContext = viewModel;
+        }
+
+        /// <summary>
+        /// Called when stepping into this wizard page
+        /// </summary>
+        private void OnSteppingInto(object sender, WizardSteppingIntoEventArgs e)
+        {
+            viewModel.Load(GetStore<RakutenStoreEntity>());
+        }
+
+        /// <summary>
+        /// Called when [step next].
+        /// </summary>
+        private void OnStepNext(object sender, WizardStepEventArgs e)
+        {
+            if (!viewModel.Save(GetStore<RakutenStoreEntity>()))
+            {
+                e.NextPage = this;
+            }
+        }
+    }
+}
