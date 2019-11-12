4921fe43307a01a204a16c76473da058e6b8e6be
Josh Winchester <j.winchester@shipworks.com>
2019-11-12 10:59:46 -0600

    TP#34801: Add scripts for Rakuten tables

---
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.6/7.6.0.0.sql
@@ -0,0 +1,66 @@
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[RakutenOrder]'
+GO
+IF OBJECT_ID(N'[dbo].[RakutenOrder]', 'U') IS NULL
+CREATE TABLE [dbo].[RakutenOrder]
+(
+[OrderID] [bigint] NOT NULL,
+[RakutenOrderID] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+)
+GO
+PRINT N'Creating primary key [PK_RakutenOrder] on [dbo].[RakutenOrder]'
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'PK_RakutenOrder' AND object_id = OBJECT_ID(N'[dbo].[RakutenOrder]'))
+ALTER TABLE [dbo].[RakutenOrder] ADD CONSTRAINT [PK_RakutenOrder] PRIMARY KEY CLUSTERED  ([OrderID])
+GO
+PRINT N'Creating [dbo].[RakutenOrderItem]'
+GO
+IF OBJECT_ID(N'[dbo].[RakutenOrderItem]', 'U') IS NULL
+CREATE TABLE [dbo].[RakutenOrderItem]
+(
+[OrderItemID] [bigint] NOT NULL,
+[RakutenOrderItemID] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Discount] [money] NOT NULL,
+[ItemTotal] [money] NOT NULL
+)
+GO
+PRINT N'Creating primary key [PK_RakutenOrderItem] on [dbo].[RakutenOrderItem]'
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'PK_RakutenOrderItem' AND object_id = OBJECT_ID(N'[dbo].[RakutenOrderItem]'))
+ALTER TABLE [dbo].[RakutenOrderItem] ADD CONSTRAINT [PK_RakutenOrderItem] PRIMARY KEY CLUSTERED  ([OrderItemID])
+GO
+PRINT N'Creating [dbo].[RakutenStore]'
+GO
+IF OBJECT_ID(N'[dbo].[RakutenStore]', 'U') IS NULL
+CREATE TABLE [dbo].[RakutenStore]
+(
+[StoreID] [bigint] NOT NULL,
+[AuthKey] [nvarchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[MarketplaceID] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[ShopURL] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[DownloadStartDate] [datetime] NULL,
+)
+GO
+PRINT N'Creating primary key [PK_RakutenStore] on [dbo].[RakutenStore]'
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'PK_RakutenStore' AND object_id = OBJECT_ID(N'[dbo].[RakutenStore]'))
+ALTER TABLE [dbo].[RakutenStore] ADD CONSTRAINT [PK_RakutenStore] PRIMARY KEY CLUSTERED  ([StoreID])
+GO
+PRINT N'Adding foreign keys to [dbo].[RakutenOrderItem]'
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_RakutenOrderItem_OrderItem]', 'F') AND parent_object_id = OBJECT_ID(N'[dbo].[RakutenOrderItem]', 'U'))
+ALTER TABLE [dbo].[RakutenOrderItem] ADD CONSTRAINT [FK_RakutenOrderItem_OrderItem] FOREIGN KEY ([OrderItemID]) REFERENCES [dbo].[OrderItem] ([OrderItemID])
+GO
+PRINT N'Adding foreign keys to [dbo].[RakutenOrder]'
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_RakutenOrder_Order]', 'F') AND parent_object_id = OBJECT_ID(N'[dbo].[RakutenOrder]', 'U'))
+ALTER TABLE [dbo].[RakutenOrder] ADD CONSTRAINT [FK_RakutenOrder_Order] FOREIGN KEY ([OrderID]) REFERENCES [dbo].[Order] ([OrderID])
+GO
+PRINT N'Adding foreign keys to [dbo].[RakutenStore]'
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_RakutenStore_Store]', 'F') AND parent_object_id = OBJECT_ID(N'[dbo].[RakutenStore]', 'U'))
+ALTER TABLE [dbo].[RakutenStore] ADD CONSTRAINT [FK_RakutenStore_Store] FOREIGN KEY ([StoreID]) REFERENCES [dbo].[Store] ([StoreID])
+GO
--- Code/ShipWorks/Data/Administration/Scripts/Installation/CreateSchema.sql
+++ Code/ShipWorks/Data/Administration/Scripts/Installation/CreateSchema.sql
@@ -7220,6 +7220,60 @@ PRINT N'Adding foreign keys to [dbo].[OverstockStore]'
 GO
 ALTER TABLE [dbo].[OverstockStore] ADD CONSTRAINT [FK_OverstockStore_Store] FOREIGN KEY ([StoreID]) REFERENCES [dbo].[Store] ([StoreID])
 GO
+PRINT N'Creating [dbo].[RakutenOrder]'
+GO
+CREATE TABLE [dbo].[RakutenOrder]
+(
+[OrderID] [bigint] NOT NULL,
+[RakutenOrderID] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+)
+GO
+PRINT N'Creating primary key [PK_RakutenOrder] on [dbo].[RakutenOrder]'
+GO
+ALTER TABLE [dbo].[RakutenOrder] ADD CONSTRAINT [PK_RakutenOrder] PRIMARY KEY CLUSTERED  ([OrderID])
+GO
+
+PRINT N'Creating [dbo].[RakutenOrderItem]'
+GO
+CREATE TABLE [dbo].[RakutenOrderItem]
+(
+[OrderItemID] [bigint] NOT NULL,
+[RakutenOrderItemID] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Discount] [money] NOT NULL,
+[ItemTotal] [money] NOT NULL
+)
+GO
+PRINT N'Creating primary key [PK_RakutenOrderItem] on [dbo].[RakutenOrderItem]'
+GO
+ALTER TABLE [dbo].[RakutenOrderItem] ADD CONSTRAINT [PK_RakutenOrderItem] PRIMARY KEY CLUSTERED  ([OrderItemID])
+GO
+PRINT N'Creating [dbo].[RakutenStore]'
+GO
+CREATE TABLE [dbo].[RakutenStore]
+(
+[StoreID] [bigint] NOT NULL,
+[AuthKey] [nvarchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[MarketplaceID] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[ShopURL] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[DownloadStartDate] [datetime] NULL,
+)
+GO
+PRINT N'Creating primary key [PK_RakutenStore] on [dbo].[RakutenStore]'
+GO
+ALTER TABLE [dbo].[RakutenStore] ADD CONSTRAINT [PK_RakutenStore] PRIMARY KEY CLUSTERED  ([StoreID])
+GO
+PRINT N'Adding foreign keys to [dbo].[RakutenOrderItem]'
+GO
+ALTER TABLE [dbo].[RakutenOrderItem] ADD CONSTRAINT [FK_RakutenOrderItem_OrderItem] FOREIGN KEY ([OrderItemID]) REFERENCES [dbo].[OrderItem] ([OrderItemID])
+GO
+PRINT N'Adding foreign keys to [dbo].[RakutenOrder]'
+GO
+ALTER TABLE [dbo].[RakutenOrder] ADD CONSTRAINT [FK_RakutenOrder_Order] FOREIGN KEY ([OrderID]) REFERENCES [dbo].[Order] ([OrderID])
+GO
+PRINT N'Adding foreign keys to [dbo].[RakutenStore]'
+GO
+ALTER TABLE [dbo].[RakutenStore] ADD CONSTRAINT [FK_RakutenStore_Store] FOREIGN KEY ([StoreID]) REFERENCES [dbo].[Store] ([StoreID])
+GO
 PRINT N'Creating custom types'
 GO
 CREATE TYPE dbo.LongList AS TABLE ( item BIGINT );
