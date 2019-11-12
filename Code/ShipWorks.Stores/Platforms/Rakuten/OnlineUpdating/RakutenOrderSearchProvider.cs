60ccf9f8b877e32bb2dae993e189dadda687dd8a
Josh Winchester <j.winchester@shipworks.com>
2019-11-12 13:48:54 -0600

    TP#34801: WIP - RakutenOrderSearchProvider

---
+++ Code/ShipWorks.Stores/Platforms/Rakuten/OnlineUpdating/RakutenOrderSearchProvider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Interapptive.Shared.ComponentRegistration;
+using SD.LLBLGen.Pro.QuerySpec;
+using ShipWorks.Data.Connection;
+using ShipWorks.Data.Model.EntityInterfaces;
+using ShipWorks.Data.Model.FactoryClasses;
+using ShipWorks.Data.Model.HelperClasses;
+using ShipWorks.Stores.Orders.Combine.SearchProviders;
+
+namespace ShipWorks.Stores.Platforms.Rakuten.OnlineUpdating
+{
+    /// <summary>
+    /// Get order search identifiers for uploading shipment data
+    /// </summary>
+    [Component]
+    public class RakutenOrderSearchProvider : CombineOrderSearchBaseProvider<string>, IRakutenOrderSearchProvider
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="sqlAdapterFactory"></param>
+        public RakutenOrderSearchProvider(ISqlAdapterFactory sqlAdapterFactory) : base(sqlAdapterFactory)
+        {
+
+        }
+
+        protected override Task<IEnumerable<string>> GetCombinedOnlineOrderIdentifiers(IOrderEntity order)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override string GetOnlineOrderIdentifier(IOrderEntity order)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
