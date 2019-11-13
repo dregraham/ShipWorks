49458fae577db9967cef4eca4bda9492ec3f6444
Josh Winchester <j.winchester@shipworks.com>
2019-11-13 15:20:12 -0600

    TP#34801: WIP:  Add RakutenStoreTypeTest

---
+++ Code/ShipWorks.Stores.Tests/Platforms/Rakuten/RakutenStoreTypeTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Autofac;
+using Autofac.Extras.Moq;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Platforms.Rakuten;
+using ShipWorks.Tests.Shared;
+using Xunit;
+
+namespace ShipWorks.Stores.Tests.Platforms.Rakuten
+{
+    public class RakutenStoreTypeTest : IDisposable
+    {
+        private readonly AutoMock mock;
+
+        public RakutenStoreTypeTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+        }
+
+        [Fact]
+        public void TypeCode_IsRakuten()
+        {
+            RakutenStoreType testObject = mock.Create<RakutenStoreType>(new TypedParameter(typeof(StoreEntity), 
+                new RakutenStoreEntity() { StoreTypeCode = StoreTypeCode.Rakuten}));
+
+            var typeCode = testObject.TypeCode;
+
+            Assert.Equal(StoreTypeCode.Rakuten, typeCode);
+        }
+
+        public void Dispose()
+        {
+            mock.Dispose();
+        }
+    }
+}
