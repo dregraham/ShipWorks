25ab07308227e099b0640f5a8709ef9d7db72064
Cody Hart <c.hart@shipworks.com>
2019-11-20 09:24:00 -0600

    TP #34801: Add client factory for testing

---
+++ Code/ShipWorks.Stores/Platforms/Rakuten/RakutenRestClientFactory.cs
@@ -0,0 +1,28 @@
+﻿using RestSharp;
+using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Data.Model.EntityInterfaces;
+
+namespace ShipWorks.Stores.Platforms.Rakuten
+{
+    /// <summary>
+    /// Class for creating Rakuten IRestClients
+    /// </summary>
+    [Component]
+    public class RakutenRestClientFactory : IRakutenRestClientFactory
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public RakutenRestClientFactory()
+        {
+        }
+
+        /// <summary>
+        /// Create an IRestClient for the given endpoint
+        /// </summary>
+        public IRestClient Create(string endpointBase)
+        {
+            return new RestClient(endpointBase);
+        }
+    }
+}
