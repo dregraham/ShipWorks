4db460ceacbfb1d7f6c86da7332ab2bf15a3ae33
Kevin Croke <k.croke@shipworks.com>
2019-12-06 13:15:26 -0600

    TP#35957: Add settings controls to update excludeFBA

---
+++ Code/ShipWorks/Stores/Platforms/ChannelAdvisor/ChannelAdvisorAddStoreWizardPage.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace ShipWorks.Stores.Platforms.ChannelAdvisor
+{
+    partial class ChannelAdvisorAddStoreWizardPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.excludeFba = new ShipWorks.Stores.Platforms.ChannelAdvisor.ChannelAdvisorExcludeFbaControl();
+            this.SuspendLayout();
+            // 
+            // excludeFba
+            // 
+            this.excludeFba.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.excludeFba.Location = new System.Drawing.Point(0, 0);
+            this.excludeFba.Name = "excludeFba";
+            this.excludeFba.Size = new System.Drawing.Size(580, 60);
+            this.excludeFba.TabIndex = 0;
+            // 
+            // ChannelAdvisorAddStoreWizardPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.excludeFba);
+            this.Name = "ChannelAdvisorAddStoreWizardPage";
+            this.Size = new System.Drawing.Size(585, 150);
+            this.StepNext += new System.EventHandler<ShipWorks.UI.Wizard.WizardStepEventArgs>(this.OnStepNext);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ChannelAdvisorExcludeFbaControl excludeFba;
+    }
+}
---
+++ Code/ShipWorks/Stores/Platforms/ChannelAdvisor/ChannelAdvisorAddStoreWizardPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.ComponentRegistration.Ordering;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Stores.Management;
+using ShipWorks.UI.Wizard;
+
+namespace ShipWorks.Stores.Platforms.ChannelAdvisor
+{
+    /// <summary>
+    /// Wizard page for ChannelAdvisor
+    /// </summary>
+    [KeyedComponent(typeof(WizardPage), StoreTypeCode.ChannelAdvisor)]
+    [Order(typeof(WizardPage), Order.Unordered)]
+    public partial class ChannelAdvisorAddStoreWizardPage : AddStoreWizardPage
+    {
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public ChannelAdvisorAddStoreWizardPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Save excludeFba when leaving page
+        /// </summary>
+        private void OnStepNext(object sender, UI.Wizard.WizardStepEventArgs e)
+        {
+            excludeFba.SaveToEntity(GetStore<ChannelAdvisorStoreEntity>());
+        }
+    }
+}
