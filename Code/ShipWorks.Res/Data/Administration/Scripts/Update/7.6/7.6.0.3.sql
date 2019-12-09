e590afd23a38db09c7dc5eb84835e7b25bcc0127
Chris Hicks <c.hicks@shipworks.com>
2019-12-09 15:07:34 -0700

    TP#35510: Fix spacing. Rename script.

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.6/7.6.0.3.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.6/7.6.0.3.sql
@@ -2,6 +2,6 @@ PRINT N'Adding Shortcut'
 GO
 INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
 VALUES 
-(null, null, '-NP-' ,10),
-(null, null, '-NS-' , 11);
+(null, null, '-NP-',10),
+(null, null, '-NS-', 11);
 GO
