d65dd15fbe2de6adf9591fd4d0b9f5ff867ec798
Cody Hart <c.hart@shipworks.com>
2019-12-11 10:28:28 -0600

    TP #34854: WIP: Update warehouse DTOs

--- Code/ShipWorks.Stores/Platforms/Rakuten/Warehouse/RakutenWarehouseOrder.cs
+++ Code/ShipWorks.Stores/Platforms/Rakuten/Warehouse/RakutenWarehouseOrder.cs
@@ -13,87 +13,9 @@ namespace ShipWorks.Stores.Platforms.Rakuten.Warehouse
     public class RakutenWarehouseOrder
     {
         /// <summary>
-        /// The Rakuten order number
+        /// Order number postfix
         /// </summary>
-        [JsonProperty("orderNumber")]
-        public string OrderNumber { get; set; }
-
-        /// <summary>
-        /// The Rakuten order status
-        /// </summary>
-        [JsonProperty("orderStatus")]
-        public string OrderStatus { get; set; }
-
-        /// <summary>
-        /// The order total
-        /// </summary>
-        [JsonProperty("orderTotal")]
-        public decimal OrderTotal { get; set; }
-
-        /// <summary>
-        /// The last modified date
-        /// </summary>
-        [JsonProperty("lastModifiedDate")]
-        public DateTime LastModifiedDate { get; set; }
-
-        /// <summary>
-        /// The date the order was created
-        /// </summary>
-        [JsonProperty("orderDate")]
-        public DateTime OrderDate { get; set; }
-
-        /// <summary>
-        /// Shopper comment or memo entered during checkout before the order was placed
-        /// </summary>
-        [JsonProperty("shopperComment")]
-        public string ShopperComment { get; set; }
-
-        /// <summary>
-        /// Items contained in the order
-        /// </summary>
-        [JsonProperty("orderItems")]
-        public IList<RakutenOrderItem> OrderItems { get; set; }
-
-        /// <summary>
-        /// The shipping information of the order
-        /// </summary>
-        [JsonProperty("shipping")]
-        public RakutenShipment Shipping { get; set; }
-
-        /// <summary>
-        /// The payment information of the order
-        /// </summary>
-        [JsonProperty("payment")]
-        public RakutenPayment Payment { get; set; }
-
-        /// <summary>
-        /// The buyers name
-        /// </summary>
-        [JsonProperty("buyerName")]
-        public string BuyerName { get; set; }
-
-        /// <summary>
-        /// The buyers phone number
-        /// </summary>
-        [JsonProperty("buyerPhoneNumber")]
-        public string BuyerPhoneNumber { get; set; }
-
-        /// <summary>
-        /// The anonymized email address of the shopper
-        /// </summary>
-        [JsonProperty("shopperAnonymizedAddress")]
-        public string AnonymizedEmailAddress { get; set; }
-
-        /// <summary>
-        /// The merchant memo attached to the order
-        /// </summary>
-        [JsonProperty("merchantMemo")]
-        public string MerchantMemo { get; set; }
-
-        /// <summary>
-        /// The optional information entered by the customer
-        /// </summary>
-        [JsonProperty("checkoutOptionalInfo")]
-        public RakutenOptionalCheckoutInfo checkoutOptionalInfo { get; set; }
+        /// <value></value>
+        public string PackageID { get; set; }
     }
 }
