54cf0e001e2e03fb3b3f1904808f3a352e917f3a
Kevin Croke <k.croke@shipworks.com>
2020-01-02 10:16:16 -0600

    TP#33698: Allow ShipWorks to work with fiddler

--- Code/Interapptive.Shared/Net/HttpRequestSubmitter.cs
+++ Code/Interapptive.Shared/Net/HttpRequestSubmitter.cs
@@ -278,8 +278,7 @@ namespace Interapptive.Shared.Net
             webRequest.ServicePoint.Expect100Continue = expect100Continue;
             webRequest.KeepAlive = keepAlive;
             webRequest.UserAgent = DefaultUserAgent;
-            webRequest.Proxy = null;
-
+            
             SetAllowAutoRedirect(webRequest);
 
             // If it's not get set the content type and redirect option.  Set them before applying the headers, so any headers will override
