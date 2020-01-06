560a36eb19808d78626a83ce987b21d94b92f557
Kevin Croke <k.croke@shipworks.com>
2020-01-06 14:36:09 -0600

    TP#33698: Don't countdown in autoupdate is turned off

--- Code/ShipWorks/ApplicationCore/MessageBoxes/NeedUpgradeShipWorks.cs
+++ Code/ShipWorks/ApplicationCore/MessageBoxes/NeedUpgradeShipWorks.cs
@@ -31,7 +31,7 @@ namespace ShipWorks.ApplicationCore.MessageBoxes
                 Resolve<IShipWorksCommunicationBridge>(new TypedParameter(typeof(string), ShipWorksSession.InstanceID.ToString()));
             updateService = IoC.UnsafeGlobalLifetimeScope.Resolve<IUpdateService>();
 
-            if (!AutoUpdateSettings.LastAutoUpdateSucceeded)
+            if (!AutoUpdateSettings.LastAutoUpdateSucceeded || AutoUpdateSettings.IsAutoUpdateDisabled)
             {
                 close.Text = "Close";
                 update.Visible = false;
