4ce72af514454c48d8b209475a647f85280406b2
Cody Hart <c.hart@shipworks.com>
2020-01-07 11:01:38 -0600

    Change update script to be rerunable

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.6/7.6.0.3.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.6/7.6.0.3.sql
@@ -1,7 +1,10 @@
 PRINT N'Adding Shortcut'
 GO
-INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
-VALUES 
-(null, null, '-NP-',10),
-(null, null, '-NS-', 11);
+IF NOT EXISTS(SELECT * FROM Shortcut WHERE Barcode = '-NP-')
+BEGIN
+	INSERT INTO Shortcut (ModifierKeys, VirtualKey, Barcode, [Action])
+	VALUES 
+	(null, null, '-NP-',10),
+	(null, null, '-NS-', 11) 
+END;
 GO
