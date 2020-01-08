174e73e6edb6f8e1124b44f0367a7a42cd6022fa
Kevin Croke <k.croke@shipworks.com>
2020-01-08 08:50:21 -0600

    TP#36053: Add OrderLookupVerifyMessage

---
+++ Code/ShipWorks.OrderLookup/Messages/OrderLookupVerifyMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Interapptive.Shared.Messaging;
+
+namespace ShipWorks.OrderLookup.Messages
+{
+    /// <summary>
+    /// Request that a order be unverified
+    /// </summary>
+    public struct OrderLookupUnverifyMessage : IShipWorksMessage
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public OrderLookupUnverifyMessage(object sender, long orderID)
+        {
+            Sender = sender;
+            OrderID= orderID;
+            MessageId = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Sender of the message
+        /// </summary>
+        public object Sender { get; }
+
+        /// <summary>
+        /// The orderID to ship again
+        /// </summary>
+        public long OrderID { get; }
+
+        /// <summary>
+        /// Id of the message used for tracking purposes
+        /// </summary>
+        public Guid MessageId { get; }
+    }
+}
