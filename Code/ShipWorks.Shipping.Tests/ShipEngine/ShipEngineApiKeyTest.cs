c163919cc6d87e154d5050b3a3a2554499c231c6
Cody Hart <c.hart@shipworks.com>
2020-01-21 10:43:25 -0600

    TP #34870: Add tests for new sync methods

--- Code/ShipWorks.Shipping.Tests/ShipEngine/ShipEngineApiKeyTest.cs
+++ Code/ShipWorks.Shipping.Tests/ShipEngine/ShipEngineApiKeyTest.cs
@@ -7,6 +7,7 @@ using ShipWorks.ShipEngine;
 using ShipWorks.Tests.Shared;
 using System;
 using Xunit;
+using System.Threading.Tasks;
 
 namespace ShipWorks.Shipping.Tests.ShipEngine
 {
@@ -35,8 +36,8 @@ namespace ShipWorks.Shipping.Tests.ShipEngine
                .Returns(new ShippingSettingsEntity());
 
             mock.Mock<IShipEnginePartnerWebClient>()
-                .Setup(c => c.GetApiKeyAsync(It.IsAny<string>(), It.IsAny<string>()))
-                .ReturnsAsync("newkey");
+                .Setup(c => c.GetApiKey(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns("newkey");
 
             var testObject = mock.Create<ShipEngineApiKey>();
             testObject.Configure();
@@ -52,8 +53,8 @@ namespace ShipWorks.Shipping.Tests.ShipEngine
                .Returns(new ShippingSettingsEntity());
 
             mock.Mock<IShipEnginePartnerWebClient>()
-                .Setup(c => c.CreateNewAccountAsync(It.IsAny<string>()))
-                .ReturnsAsync("accountId");
+                .Setup(c => c.CreateNewAccount(It.IsAny<string>()))
+                .Returns("accountId");
 
             var testObject = mock.Create<ShipEngineApiKey>();
             testObject.Configure();
@@ -150,6 +151,146 @@ namespace ShipWorks.Shipping.Tests.ShipEngine
 
             var testObject = mock.Create<ShipEngineApiKey>();
             testObject.Configure();
+
+            encryptionProvider.Verify(p => p.Decrypt("Auapk4J9PBSgT+Luq91kHHGNhTddMY2y0Ih7x0/7V5bjZ1FQE2yF7WyR7oR0e0DA"), Times.Once());
+        }
+
+        [Fact]
+        public async Task ConfigureAsync_SetsValueToShipEngineApiKey_FromSettings()
+        {
+            mock.Mock<IShippingSettings>()
+                .Setup(s => s.Fetch())
+                .Returns(new ShippingSettingsEntity() { ShipEngineApiKey = "apikey" });
+
+            var testObject = mock.Create<ShipEngineApiKey>();
+            await testObject.ConfigureAsync();
+
+            Assert.Equal("apikey", testObject.Value);
+        }
+
+        [Fact]
+        public async Task ConfigureAsync_SetsValueToShipEngineApiKey_WhenShippingSettingsKeyBlank_FromPartnerClient()
+        {
+            mock.Mock<IShippingSettings>()
+               .Setup(s => s.Fetch())
+               .Returns(new ShippingSettingsEntity());
+
+            mock.Mock<IShipEnginePartnerWebClient>()
+                .Setup(c => c.GetApiKeyAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync("newkey");
+
+            var testObject = mock.Create<ShipEngineApiKey>();
+            await testObject.ConfigureAsync();
+
+            Assert.Equal("newkey", testObject.Value);
+        }
+
+        [Fact]
+        public async Task ConfigureAsync_UsesAccountNumberWhenGettingApiKey()
+        {
+            mock.Mock<IShippingSettings>()
+               .Setup(s => s.Fetch())
+               .Returns(new ShippingSettingsEntity());
+
+            mock.Mock<IShipEnginePartnerWebClient>()
+                .Setup(c => c.CreateNewAccountAsync(It.IsAny<string>()))
+                .ReturnsAsync("accountId");
+
+            var testObject = mock.Create<ShipEngineApiKey>();
+            await testObject.ConfigureAsync();
+
+            mock.Mock<IShipEnginePartnerWebClient>()
+                .Verify(c => c.GetApiKeyAsync(It.IsAny<string>(), "accountId"), Times.Once());
+        }
+
+        [Fact]
+        public async Task ConfigureAsync_UsePartnerApiKeyWhenGettingApiKey()
+        {
+            mock.Mock<IShippingSettings>()
+               .Setup(s => s.Fetch())
+               .Returns(new ShippingSettingsEntity());
+
+            var encryptionProvider = mock.FromFactory<IEncryptionProviderFactory>()
+                .Mock(f => f.CreateSecureTextEncryptionProvider(It.IsAny<string>()));
+
+            encryptionProvider
+                .Setup(p => p.Decrypt(It.IsAny<string>()))
+                .Returns("decrypted");
+
+            var testObject = mock.Create<ShipEngineApiKey>();
+            await testObject.ConfigureAsync();
+
+            mock.Mock<IShipEnginePartnerWebClient>()
+                .Verify(c => c.GetApiKeyAsync("decrypted", It.IsAny<string>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task ConfigureAsync_UsesPartnerIdWhenCreatingNewAccount()
+        {
+            mock.Mock<IShippingSettings>()
+              .Setup(s => s.Fetch())
+              .Returns(new ShippingSettingsEntity());
+
+            var encryptionProvider = mock.FromFactory<IEncryptionProviderFactory>()
+                .Mock(f => f.CreateSecureTextEncryptionProvider(It.IsAny<string>()));
+
+            encryptionProvider
+                .Setup(p => p.Decrypt(It.IsAny<string>()))
+                .Returns("decrypted");
+
+            var testObject = mock.Create<ShipEngineApiKey>();
+            await testObject.ConfigureAsync();
+
+            mock.Mock<IShipEnginePartnerWebClient>()
+                .Verify(c => c.CreateNewAccountAsync("decrypted"), Times.Once());
+        }
+
+        [Fact]
+        public async Task ConfigureAsync_ValueIsBlank_WhenCreateNewAccountThrowsShipEngineException()
+        {
+            mock.Mock<IShippingSettings>()
+               .Setup(s => s.Fetch())
+               .Returns(new ShippingSettingsEntity());
+
+            mock.Mock<IShipEnginePartnerWebClient>()
+                .Setup(c => c.CreateNewAccount(It.IsAny<string>()))
+                .Throws(new ShipEngineException("error"));
+
+            var testObject = mock.Create<ShipEngineApiKey>();
+            await testObject.ConfigureAsync();
+
+            Assert.True(string.IsNullOrEmpty(testObject.Value));
+        }
+
+        [Fact]
+        public async Task ConfigureAsync_ValueIsBlank_WhenGetApiKeyThrowsShipEngineException()
+        {
+            mock.Mock<IShippingSettings>()
+               .Setup(s => s.Fetch())
+               .Returns(new ShippingSettingsEntity());
+
+            mock.Mock<IShipEnginePartnerWebClient>()
+                .Setup(c => c.GetApiKey(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws(new ShipEngineException("error"));
+
+            var testObject = mock.Create<ShipEngineApiKey>();
+            await testObject.ConfigureAsync();
+
+            Assert.True(string.IsNullOrEmpty(testObject.Value));
+        }
+
+        [Fact]
+        public async Task ConfigureAsync_DecryptsPartnerApiKey()
+        {
+            mock.Mock<IShippingSettings>()
+              .Setup(s => s.Fetch())
+              .Returns(new ShippingSettingsEntity());
+
+            var encryptionProvider = mock.FromFactory<IEncryptionProviderFactory>()
+                .Mock(f => f.CreateSecureTextEncryptionProvider(It.IsAny<string>()));
+
+            var testObject = mock.Create<ShipEngineApiKey>();
+            await testObject.ConfigureAsync();
 
             encryptionProvider.Verify(p => p.Decrypt("Auapk4J9PBSgT+Luq91kHHGNhTddMY2y0Ih7x0/7V5bjZ1FQE2yF7WyR7oR0e0DA"), Times.Once());
         }
--- Code/ShipWorks.Shipping.Tests/ShipEngine/ShipEnginePartnerWebClientTest.cs
+++ Code/ShipWorks.Shipping.Tests/ShipEngine/ShipEnginePartnerWebClientTest.cs
@@ -20,7 +20,7 @@ namespace ShipWorks.Shipping.Tests.ShipEngine
         }
 
         [Fact]
-        public async Task CreateNewAccount_ReturnsAccountID_FromResponse()
+        public async Task CreateNewAccountAsync_ReturnsAccountID_FromResponse()
         {
             var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
                 .Mock(f => f.GetHttpTextPostRequestSubmitter(string.Empty, "application/json"));
@@ -40,7 +40,27 @@ namespace ShipWorks.Shipping.Tests.ShipEngine
         }
 
         [Fact]
-        public void CreateNewAccount_PartnerKeyIsAddedToTheRequest()
+        public void CreateNewAccount_ReturnsAccountID_FromResponse()
+        {
+            var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
+                .Mock(f => f.GetHttpTextPostRequestSubmitter(string.Empty, "application/json"));
+
+            var responseReader = mock.Mock<IHttpResponseReader>();
+            responseReader.Setup(r => r.ReadResult())
+                .Returns("{\"account_id\":\"1234\"}");
+
+            request.Setup(r => r.GetResponse())
+                .Returns(responseReader.Object);
+
+            var testObject = mock.Create<ShipEnginePartnerWebClient>();
+
+            var accountId = testObject.CreateNewAccount("partnerKey");
+
+            Assert.Equal("1234", accountId);
+        }
+
+        [Fact]
+        public async Task CreateNewAccountAsync_PartnerKeyIsAddedToTheRequest()
         {
             var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
                 .Mock(f => f.GetHttpTextPostRequestSubmitter(string.Empty, "application/json"));
@@ -54,13 +74,33 @@ namespace ShipWorks.Shipping.Tests.ShipEngine
 
             var testObject = mock.Create<ShipEnginePartnerWebClient>();
 
+            await testObject.CreateNewAccountAsync("partnerKey");
+
+            request.Verify(r => r.Headers.Add("api-key", "partnerKey"), Times.Once);
+        }
+
+        [Fact]
+        public void CreateNewAccount_PartnerKeyIsAddedToTheRequest()
+        {
+            var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
+                .Mock(f => f.GetHttpTextPostRequestSubmitter(string.Empty, "application/json"));
+
+            var responseReader = mock.Mock<IHttpResponseReader>();
+            responseReader.Setup(r => r.ReadResult())
+                .Returns("{\"account_id\":\"1234\"}");
+
+            request.Setup(r => r.GetResponse())
+                .Returns(responseReader.Object);
+
+            var testObject = mock.Create<ShipEnginePartnerWebClient>();
+
             testObject.CreateNewAccount("partnerKey");
 
             request.Verify(r => r.Headers.Add("api-key", "partnerKey"), Times.Once);
         }
 
         [Fact]
-        public async Task CreateNewAccount_ThrowsShipEngineException_WhenResponseDoesNotReturnJson()
+        public async Task CreateNewAccountAsync_ThrowsShipEngineException_WhenResponseDoesNotReturnJson()
         {
             var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
                 .Mock(f => f.GetHttpTextPostRequestSubmitter(string.Empty, "application/json"));
@@ -78,7 +118,25 @@ namespace ShipWorks.Shipping.Tests.ShipEngine
         }
 
         [Fact]
-        public async Task CreateNewAccount_ThrowsShipEngineException_WhenWebExceptionEncountered()
+        public void CreateNewAccount_ThrowsShipEngineException_WhenResponseDoesNotReturnJson()
+        {
+            var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
+                .Mock(f => f.GetHttpTextPostRequestSubmitter(string.Empty, "application/json"));
+
+            var responseReader = mock.Mock<IHttpResponseReader>();
+            responseReader.Setup(r => r.ReadResult())
+                .Returns("not json");
+
+            request.Setup(r => r.GetResponse())
+                .Returns(responseReader.Object);
+
+            var testObject = mock.Create<ShipEnginePartnerWebClient>();
+
+            Assert.Throws<ShipEngineException>(() => testObject.CreateNewAccount("partnerKey"));
+        }
+
+        [Fact]
+        public async Task CreateNewAccountAsync_ThrowsShipEngineException_WhenWebExceptionEncountered()
         {
             var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
                 .Mock(f => f.GetHttpTextPostRequestSubmitter(string.Empty, "application/json"));
@@ -95,9 +153,26 @@ namespace ShipWorks.Shipping.Tests.ShipEngine
             await Assert.ThrowsAsync<ShipEngineException>(() => testObject.CreateNewAccountAsync("partnerKey"));
         }
 
+        [Fact]
+        public void CreateNewAccount_ThrowsShipEngineException_WhenWebExceptionEncountered()
+        {
+            var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
+                .Mock(f => f.GetHttpTextPostRequestSubmitter(string.Empty, "application/json"));
+
+            var responseReader = mock.Mock<IHttpResponseReader>();
+            responseReader.Setup(r => r.ReadResult())
+                .Throws(new WebException());
+
+            request.Setup(r => r.GetResponse())
+                .Returns(responseReader.Object);
+
+            var testObject = mock.Create<ShipEnginePartnerWebClient>();
+
+            Assert.Throws<ShipEngineException>(() => testObject.CreateNewAccount("partnerKey"));
+        }
 
         [Fact]
-        public async Task GetApiKey_ReturnsApiKey_FromResponse()
+        public async Task GetApiKeyAsync_ReturnsApiKey_FromResponse()
         {
             var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
                 .Mock(f => f.GetHttpTextPostRequestSubmitter(It.IsAny<string>(), "application/json"));
@@ -116,8 +191,29 @@ namespace ShipWorks.Shipping.Tests.ShipEngine
             Assert.Equal("1234", apiKey);
         }
 
+
         [Fact]
-        public void GetApiKey_PartnerKeyIsAddedToTheRequest()
+        public void GetApiKey_ReturnsApiKey_FromResponse()
+        {
+            var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
+                .Mock(f => f.GetHttpTextPostRequestSubmitter(It.IsAny<string>(), "application/json"));
+
+            var responseReader = mock.Mock<IHttpResponseReader>();
+            responseReader.Setup(r => r.ReadResult())
+                .Returns("{\"encrypted_api_key\":\"1234\"}");
+
+            request.Setup(r => r.GetResponse())
+                .Returns(responseReader.Object);
+
+            var testObject = mock.Create<ShipEnginePartnerWebClient>();
+
+            var apiKey = testObject.GetApiKey("partnerKey", "accountId");
+
+            Assert.Equal("1234", apiKey);
+        }
+
+        [Fact]
+        public async Task GetApiKeyAsync_PartnerKeyIsAddedToTheRequest()
         {
             var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
                 .Mock(f => f.GetHttpTextPostRequestSubmitter(It.IsAny<string>(), "application/json"));
@@ -131,13 +227,33 @@ namespace ShipWorks.Shipping.Tests.ShipEngine
 
             var testObject = mock.Create<ShipEnginePartnerWebClient>();
 
+            await testObject.GetApiKeyAsync("partnerKey", "accountId");
+
+            request.Verify(r => r.Headers.Add("api-key", "partnerKey"), Times.Once);
+        }
+
+        [Fact]
+        public void GetApiKey_PartnerKeyIsAddedToTheRequest()
+        {
+            var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
+                .Mock(f => f.GetHttpTextPostRequestSubmitter(It.IsAny<string>(), "application/json"));
+
+            var responseReader = mock.Mock<IHttpResponseReader>();
+            responseReader.Setup(r => r.ReadResult())
+                .Returns("{\"encrypted_api_key\":\"1234\"}");
+
+            request.Setup(r => r.GetResponse())
+                .Returns(responseReader.Object);
+
+            var testObject = mock.Create<ShipEnginePartnerWebClient>();
+
             testObject.GetApiKey("partnerKey", "accountId");
 
             request.Verify(r => r.Headers.Add("api-key", "partnerKey"), Times.Once);
         }
 
         [Fact]
-        public async Task GetApiKey_ThrowsShipEngineException_WhenResponseDoesNotReturnJson()
+        public async Task GetApiKeyAsync_ThrowsShipEngineException_WhenResponseDoesNotReturnJson()
         {
             var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
                 .Mock(f => f.GetHttpTextPostRequestSubmitter(It.IsAny<string>(), "application/json"));
@@ -155,7 +271,25 @@ namespace ShipWorks.Shipping.Tests.ShipEngine
         }
 
         [Fact]
-        public async Task GetApiKey_ThrowsShipEngineException_WhenWebExceptionEncountered()
+        public void GetApiKey_ThrowsShipEngineException_WhenResponseDoesNotReturnJson()
+        {
+            var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
+                .Mock(f => f.GetHttpTextPostRequestSubmitter(It.IsAny<string>(), "application/json"));
+
+            var responseReader = mock.Mock<IHttpResponseReader>();
+            responseReader.Setup(r => r.ReadResult())
+                .Returns("not json");
+
+            request.Setup(r => r.GetResponse())
+                .Returns(responseReader.Object);
+
+            var testObject = mock.Create<ShipEnginePartnerWebClient>();
+
+            Assert.Throws<ShipEngineException>(() => testObject.GetApiKey("partnerKey", "accountId"));
+        }
+
+        [Fact]
+        public async Task GetApiKeyAsync_ThrowsShipEngineException_WhenWebExceptionEncountered()
         {
             var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
                 .Mock(f => f.GetHttpTextPostRequestSubmitter(It.IsAny<string>(), "application/json"));
@@ -172,6 +306,24 @@ namespace ShipWorks.Shipping.Tests.ShipEngine
             await Assert.ThrowsAsync<ShipEngineException>(() => testObject.GetApiKeyAsync("partnerKey", "accountId"));
         }
 
+
+        [Fact]
+        public void GetApiKey_ThrowsShipEngineException_WhenWebExceptionEncountered()
+        {
+            var request = mock.FromFactory<IHttpRequestSubmitterFactory>()
+                .Mock(f => f.GetHttpTextPostRequestSubmitter(It.IsAny<string>(), "application/json"));
+
+            var responseReader = mock.Mock<IHttpResponseReader>();
+            responseReader.Setup(r => r.ReadResult())
+                .Throws(new WebException());
+
+            request.Setup(r => r.GetResponse())
+                .Returns(responseReader.Object);
+
+            var testObject = mock.Create<ShipEnginePartnerWebClient>();
+
+            Assert.Throws<ShipEngineException>(() => testObject.GetApiKey("partnerKey", "accountId"));
+        }
 
         public void Dispose()
         {
