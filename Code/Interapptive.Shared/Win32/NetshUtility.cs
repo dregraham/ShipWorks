4b394565e6ecea54fc9a3595c6018ab7bd7b9c51
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2020-02-10 14:46:18 -0600

    TP#36992 create NetshUtility and ApiPortRegistrationService

---
+++ Code/Interapptive.Shared/Win32/NetshUtility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace Interapptive.Shared.Win32
+{
+    /// <summary>
+    /// Utility for executing netsh commands
+    /// </summary>
+    public static class NetshUtility
+    {
+        static readonly ILog log = LogManager.GetLogger(typeof(WindowsFirewallUtility));
+
+        /// <summary>
+        /// Execute 'netsh' with the specified arguments
+        /// </summary>
+        public static int ExecuteNetsh(string args)
+        {
+            log.Info("Running 'netsh " + args + "'");
+
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo("netsh", args);
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.Start();
+            process.WaitForExit();
+
+            log.Info($"netsh command returned {process.ExitCode}");
+
+            return process.ExitCode;
+        }
+    }
+}
---
+++ Code/ShipWorks.Api/ApiPortRegistrationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interapptive.Shared.Win32;
+
+namespace ShipWorks.Api
+{
+    /// <summary>
+    /// service for registering a port for the api
+    /// </summary>
+    public class ApiPortRegistrationService : IApiPortRegistrationService
+    {
+        /// <summary>
+        /// Register the given port
+        /// </summary>
+        public bool Register(long portNumber)
+        {
+            string command = $"http add urlacl url=http://+:{portNumber}/ShipWorks/ user='Everyone'";
+
+            return NetshUtility.ExecuteNetsh(command) == 0;
+        }
+    }
+}
