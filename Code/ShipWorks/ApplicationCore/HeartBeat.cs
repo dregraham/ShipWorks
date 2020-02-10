b89a2da3e5fc6189c91ae8e89e2052bc5a161eec
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2020-02-10 14:18:45 -0600

    TP#36992 call ApiService.Start from HeartBeat

--- Code/ShipWorks/ApplicationCore/HeartBeat.cs
+++ Code/ShipWorks/ApplicationCore/HeartBeat.cs
@@ -5,6 +5,7 @@ using Autofac;
 using Interapptive.Shared;
 using log4net;
 using ShipWorks.Actions;
+using ShipWorks.Api;
 using ShipWorks.ApplicationCore.Crashes;
 using ShipWorks.ApplicationCore.Dashboard;
 using ShipWorks.ApplicationCore.Enums;
@@ -158,6 +159,8 @@ namespace ShipWorks.ApplicationCore
             try
             {
                 InternalDoHeartbeat(options);
+
+                IoC.UnsafeGlobalLifetimeScope.Resolve<IApiService>().Start();
             }
             finally
             {
