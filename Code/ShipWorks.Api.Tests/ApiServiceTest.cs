72f8a4e78f02f1ec51411f6cac7178743ae20dc1
Kevin Croke <k.croke@shipworks.com>
2020-02-11 17:26:29 -0600

    TP#36992: Add unit test to ApiServiceTest

---
+++ Code/ShipWorks.Api.Tests/ApiServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Autofac.Extras.Moq;
+using Interapptive.Shared.Utility;
+using Moq;
+using ShipWorks.Api.HealthCheck;
+using ShipWorks.ApplicationCore.ExecutionMode;
+using ShipWorks.Tests.Shared;
+using Xunit;
+
+namespace ShipWorks.Api.Tests
+{
+    public class ApiServiceTest
+    {
+        private readonly AutoMock mock;
+        private readonly Mock<ITimer> timer;
+
+        public ApiServiceTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+
+            timer = mock.Mock<ITimer>();
+        }
+
+        [Fact]
+        public void InitializeForCurrentDatabase_StopsAndStartsTheTimer_WhenTimeEllapses()
+        {
+            mock.Mock<IHealthCheckClient>()
+                .Setup(h => h.IsRunning())
+                .Returns(false);
+
+            var testObject = mock.Create<ApiService>();
+            testObject.InitializeForCurrentDatabase(null);
+            timer.Raise(r => r.Elapsed += null, EventArgs.Empty as ElapsedEventArgs);
+
+            timer.Verify(t => t.Stop(), Times.Once);
+            timer.Verify(t => t.Start(), Times.Once);
+        }
+    }
+}
