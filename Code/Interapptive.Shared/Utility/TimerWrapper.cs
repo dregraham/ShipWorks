016d4b46587f1bf52f33b7e1fde81f2e567ae357
Kevin Croke <k.croke@shipworks.com>
2020-02-11 16:42:32 -0600

    TP#36992: Create test project for the API

---
+++ Code/Interapptive.Shared/Utility/TimerWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Interapptive.Shared.ComponentRegistration;
+
+namespace Interapptive.Shared.Utility
+{
+    /// <summary>
+    /// Wraps a timer for unit tests
+    /// </summary>
+    [Component]
+    public class TimerWrapper : ITimer
+    {
+        private Timer timer = new Timer();
+
+        /// <summary>
+        /// Time between calling Elapsed event
+        /// </summary>
+        public double Interval { get; set; }
+
+        /// <summary>
+        /// Starts the timer
+        /// </summary>
+        public void Start() => timer.Start();
+
+        /// <summary>
+        /// Stops the timer
+        /// </summary>
+        public void Stop() => timer.Stop();
+
+        /// <summary>
+        /// Event to call when timer is ellapsed
+        /// </summary>
+        public event ElapsedEventHandler Elapsed
+        {
+            add { timer.Elapsed += value; }
+            remove { timer.Elapsed -= value; }
+        }
+    }
+}
