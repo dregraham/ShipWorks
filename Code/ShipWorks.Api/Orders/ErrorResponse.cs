b879d9c3ddb86d0e8d2e43cd4353cefe7e01bfe9
Kevin Croke <k.croke@shipworks.com>
2020-02-21 09:45:33 -0600

    TP#36995: Exclude error response from obfuscation

--- Code/ShipWorks.Api/Orders/ErrorResponse.cs
+++ Code/ShipWorks.Api/Orders/ErrorResponse.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 
 namespace ShipWorks.Api.Orders
 {
     /// <summary>
     /// ErrorResponse to return when there is an error
     /// </summary>
+    [Obfuscation(Exclude = true)]
     public class ErrorResponse
     {
         /// <summary>
