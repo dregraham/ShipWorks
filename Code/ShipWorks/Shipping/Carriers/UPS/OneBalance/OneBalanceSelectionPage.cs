6e13495e7af0b721f4f71aa4cb37b2983701d962
Chris Hicks <c.hicks@shipworks.com>
2020-02-25 11:30:42 -0700

    TP#37167: Create IOneBalanceUpsAccountRegistrationActivity. Add ConnectStampsAccount and RegisterUpsAccount to IShipEngineWebClient. Create OneBalanceSelectionPage and OneBalanceTermsAndConditionsPage.

---
+++ Code/ShipWorks/Shipping/Carriers/UPS/OneBalance/OneBalanceSelectionPage.cs
@@ -0,0 +1,12 @@
+﻿using ShipWorks.UI.Wizard;
+
+namespace ShipWorks.Shipping.Carriers.UPS.OneBalance
+{
+    public partial class OneBalanceSelectionPage : WizardPage
+    {
+        public OneBalanceSelectionPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
