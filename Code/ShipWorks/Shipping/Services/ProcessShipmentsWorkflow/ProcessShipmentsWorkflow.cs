242d9566265fee89be1a4b5aa3ee3b73c42b5c19
Kevin Croke <k.croke@shipworks.com>
2020-02-25 08:29:16 -0600

    TP#36995: Fix unit test

--- Code/ShipWorks/Shipping/Services/ProcessShipmentsWorkflow/ProcessShipmentsWorkflow.cs
+++ Code/ShipWorks/Shipping/Services/ProcessShipmentsWorkflow/ProcessShipmentsWorkflow.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace ShipWorks.Shipping.Services.ProcessShipmentsWorkflow
     /// <summary>
     /// Method by which shipments are processed
     /// </summary>
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
     public enum ProcessShipmentsWorkflow
     {
         Serial,
