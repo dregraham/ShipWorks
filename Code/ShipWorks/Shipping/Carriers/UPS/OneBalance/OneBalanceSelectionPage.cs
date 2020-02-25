ab222e52d117cf6ecbcfb76d1364d52220efeb73
Chris Hicks <c.hicks@shipworks.com>
2020-02-25 11:52:07 -0700

    TP#37167: Expose whether or not the One Balance option was selected.

--- Code/ShipWorks/Shipping/Carriers/UPS/OneBalance/OneBalanceSelectionPage.cs
+++ Code/ShipWorks/Shipping/Carriers/UPS/OneBalance/OneBalanceSelectionPage.cs
@@ -2,11 +2,23 @@
 
 namespace ShipWorks.Shipping.Carriers.UPS.OneBalance
 {
+    /// <summary>
+    /// Wizard page for a user to select whether they would like to create a One Balance UPS account or a standard
+    /// UPS account.
+    /// </summary>
     public partial class OneBalanceSelectionPage : WizardPage
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public OneBalanceSelectionPage()
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Whether or not the user has chosen to setup One Balance
+        /// </summary>
+        public bool SetupOneBalance => createOneBalanceOption.Checked;
     }
 }
