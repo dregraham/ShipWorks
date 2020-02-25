c4d2fe084cdef687207929b1d596214788959a5d
Cody Hart <c.hart@shipworks.com>
2020-02-25 12:29:37 -0600

    TP #37169: Add OnBalanaceSettingsControlHost

---
+++ Code/ShipWorks.Shipping.UI/Settings/OneBalance/OneBalanceSettingsControlHost.cs
@@ -0,0 +1,42 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Settings;
+using ShipWorks.Shipping;
+using System.Windows.Forms;
+using ShipWorks.Shipping.Carriers.Postal.Usps;
+using ShipWorks.Shipping.Carriers.Postal.Usps.Api.Net;
+using System.Linq;
+
+namespace ShipWorks.Shipping.UI.Settings.OneBalance
+{
+    /// <summary>
+    /// WinForms host for One Balance Settings Control
+    /// </summary>
+
+    [KeyedComponent(typeof(IOneBalanceSettingsControlHost), "OneBalanceSettings")]
+    [Component(RegistrationType.Self)]
+    public partial class OneBalanceSettingsControlHost : UserControl, IOneBalanceSettingsControlHost
+    {
+        private OneBalanceSettingsControlViewModel settingsViewModel;
+        private IUspsAccountManager accountManager;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        
+        public OneBalanceSettingsControlHost(IUspsAccountManager accountManager)
+        {
+            InitializeComponent();
+            this.accountManager = accountManager;
+        }
+
+        public void Initialize()
+        {
+            var account = accountManager.GetAccounts(UspsResellerType.None).FirstOrDefault();
+
+            var webClient = new UspsPostageWebClient(account);
+            settingsViewModel = new OneBalanceSettingsControlViewModel(accountManager, webClient);
+
+            settingsControl.DataContext = settingsViewModel;
+        }
+    }
+}
---
+++ Code/ShipWorks/Shipping/IOneBalanceSettingsControlHost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping
+{
+    public interface IOneBalanceSettingsControlHost
+    {
+        void Initialize();
+    }
+}
