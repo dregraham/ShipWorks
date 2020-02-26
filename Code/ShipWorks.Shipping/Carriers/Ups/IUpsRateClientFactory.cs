196f1ee50059d4933be1b96001b845f3e211901e
John Ackerman <j.ackerman@shipworks.com>
2020-02-26 15:51:47 -0600

    TP #37287: Add IUpsRateClientFactory

---
+++ Code/ShipWorks.Shipping/Carriers/Ups/IUpsRateClientFactory.cs
@@ -0,0 +1,17 @@
+﻿using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers.UPS;
+
+namespace ShipWorks.Shipping.Carriers.Ups
+{
+    /// <summary>
+    /// The factory for creating UPS rate clients
+    /// </summary>
+    public interface IUpsRateClientFactory
+    {
+        /// <summary>
+        /// Get a UPS rate client
+        /// </summary>
+        IUpsRateClient GetClient(UpsAccountEntity account);
+    }
+
+}
