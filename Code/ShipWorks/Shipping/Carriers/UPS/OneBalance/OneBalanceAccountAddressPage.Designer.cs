02d872042f2d7772f8fbf43ddfd366eae0332d3a
Chris Hicks <c.hicks@shipworks.com>
2020-02-26 08:16:38 -0700

    TP#37167: Create OneBalanceAccountAddressPage

---
+++ Code/ShipWorks/Shipping/Carriers/UPS/OneBalance/OneBalanceAccountAddressPage.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ShipWorks.Shipping.Carriers.UPS.OneBalance
+{
+    partial class OneBalanceAccountAddressPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.personControl = new ShipWorks.Data.Controls.AutofillPersonControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // autofillPersonControl1
+            // 
+            this.personControl.AvailableFields = ((ShipWorks.Data.Controls.PersonFields)(((((((((ShipWorks.Data.Controls.PersonFields.Name | ShipWorks.Data.Controls.PersonFields.Company)
+            | ShipWorks.Data.Controls.PersonFields.Street) 
+            | ShipWorks.Data.Controls.PersonFields.City) 
+            | ShipWorks.Data.Controls.PersonFields.State) 
+            | ShipWorks.Data.Controls.PersonFields.Postal) 
+            | ShipWorks.Data.Controls.PersonFields.Country) 
+            | ShipWorks.Data.Controls.PersonFields.Residential) 
+            | ShipWorks.Data.Controls.PersonFields.Phone)));
+            this.personControl.Location = new System.Drawing.Point(24, 28);
+            this.personControl.Name = "personControl";
+            this.personControl.RequiredFields = ((ShipWorks.Data.Controls.PersonFields)(((((((ShipWorks.Data.Controls.PersonFields.Name | ShipWorks.Data.Controls.PersonFields.Street)
+            | ShipWorks.Data.Controls.PersonFields.City) 
+            | ShipWorks.Data.Controls.PersonFields.State) 
+            | ShipWorks.Data.Controls.PersonFields.Postal) 
+            | ShipWorks.Data.Controls.PersonFields.Country) 
+            | ShipWorks.Data.Controls.PersonFields.Phone)));
+            this.personControl.Size = new System.Drawing.Size(346, 339);
+            this.personControl.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Account Address";
+            // 
+            // OneBalanceAccountAddressPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.personControl);
+            this.Name = "OneBalanceAccountAddressPage";
+            this.Size = new System.Drawing.Size(579, 474);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Data.Controls.AutofillPersonControl personControl;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Code/ShipWorks/Shipping/Carriers/UPS/OneBalance/OneBalanceAccountAddressPage.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Interapptive.Shared.Business;
+using Interapptive.Shared.Collections;
+using Interapptive.Shared.UI;
+using Interapptive.Shared.Utility;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.UI.Wizard;
+
+namespace ShipWorks.Shipping.Carriers.UPS.OneBalance
+{
+    /// <summary>
+    /// Wizard page for entering an address to use with One Balance
+    /// </summary>
+    public partial class OneBalanceAccountAddressPage : WizardPage
+    {
+        private readonly IOneBalanceUpsAccountRegistrationActivity accountRegistrationActivity;
+        private readonly IMessageHelper messageHelper;
+        private readonly UpsAccountEntity upsAccount;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public OneBalanceAccountAddressPage(IOneBalanceUpsAccountRegistrationActivity accountRegistrationActivity, IMessageHelper messageHelper, UpsAccountEntity upsAccount)
+        {
+            this.accountRegistrationActivity = accountRegistrationActivity;
+            this.messageHelper = messageHelper;
+            this.upsAccount = upsAccount;
+            InitializeComponent();
+
+            StepNext += OnStepNext;
+        }
+
+        /// <summary>
+        /// When trying to proceed past this wizard page, check that all required fields are filled in and that all
+        /// fields are less than their maximum length. If validation passes, register the UPS account with One Balance.
+        /// </summary>
+        private void OnStepNext(object sender, WizardStepEventArgs e)
+        {
+            // validate required fields
+            if (!personControl.ValidateRequiredFields())
+            {
+                e.NextPage = this;
+                return;
+            }
+
+            // create PersonAdapter from control
+            PersonAdapter personAdapter = new PersonAdapter();
+            personControl.SaveToEntity(personAdapter);
+
+            // Validate field lengths
+            Result validationResult = ValidateFieldLengths(personAdapter);
+            if (validationResult.Failure)
+            {
+                messageHelper.ShowError(validationResult.Message);
+                e.NextPage = this;
+                return;
+            }
+
+            // populate the account with the given address info
+            PersonAdapter.Copy(personAdapter, new PersonAdapter(upsAccount, ""));
+
+            // execute account registration activity
+            Result result = accountRegistrationActivity.Execute(upsAccount);
+            if (result.Failure)
+            {
+                messageHelper.ShowError(this, result.Message);
+                e.NextPage = this;
+            }
+        }
+
+        /// <summary>
+        /// Ensure that all fields are less than their maximum length
+        /// </summary>
+        private Result ValidateFieldLengths(PersonAdapter personAdapter)
+        {
+            List<Result> validationResults = new List<Result>();
+
+            validationResults.Add(ValidateFieldLength("Name", personAdapter.ParsedName.FullName.Length, 20));
+            validationResults.Add(ValidateFieldLength("Company", personAdapter.Company.Length, 30));
+            validationResults.Add(ValidateFieldLength("Street Line 1", personAdapter.Street1.Length, 30));
+            validationResults.Add(ValidateFieldLength("Street Line 2", personAdapter.Street2.Length, 30));
+            validationResults.Add(ValidateFieldLength("Street Line 3", personAdapter.Street3.Length, 30));
+            validationResults.Add(ValidateFieldLength("City", personAdapter.City.Length, 30));
+            validationResults.Add(ValidateFieldLength("State", personAdapter.StateProvCode.Length, 2));
+            validationResults.Add(ValidateFieldLength("Postal Code", personAdapter.PostalCode.Length, 5));
+            validationResults.Add(ValidateFieldLength("Country", personAdapter.CountryCode.Length, 2));
+
+            var errors = validationResults.Where(r => r.Failure);
+
+            ComparisonResult comparisonResult = errors.CompareCountTo(1);
+
+            switch (comparisonResult)
+            {
+                case ComparisonResult.Equal:
+                    return Result.FromError(errors.FirstOrDefault().Message);
+                case ComparisonResult.More:
+                {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.AppendLine("The following errors occured");
+                    foreach (Result error in errors)
+                    {
+                        stringBuilder.AppendLine($"\t- {error.Message}");
+                    }
+
+                    return Result.FromError(stringBuilder.ToString());
+                }
+                default:
+                    return Result.FromSuccess();
+            }
+        }
+
+        /// <summary>
+        /// Check if a field is less than it's maximum length
+        /// </summary>
+        private Result ValidateFieldLength(string fieldName, int fieldLength, int maxFieldLength)
+        {
+            return fieldLength <= maxFieldLength ?
+                Result.FromSuccess() :
+                Result.FromError($"\"{fieldName}\" must be less than {maxFieldLength} characters");
+        }
+    }
+}
