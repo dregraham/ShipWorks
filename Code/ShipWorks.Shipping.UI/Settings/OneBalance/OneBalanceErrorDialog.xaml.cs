f883bbfdff2024fc1ac72eae66e9e30391755ca0
Cody Hart <c.hart@shipworks.com>
2020-02-27 11:39:07 -0600

    TP #37170: Add error dialog to display errors when adding money

---
+++ Code/ShipWorks.Shipping.UI/Settings/OneBalance/OneBalanceErrorDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShipWorks.Shipping.UI.Settings.OneBalance
+{
+    /// <summary>
+    /// Interaction logic for OneBalanceErrorDialog.xaml
+    /// </summary>
+    public partial class OneBalanceErrorDialog : Window
+    {
+        public OneBalanceErrorDialog(string message)
+        {
+            var viewModel = new OneBalanceErrorViewModel()
+            {
+                Message = message
+            };
+            this.DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
