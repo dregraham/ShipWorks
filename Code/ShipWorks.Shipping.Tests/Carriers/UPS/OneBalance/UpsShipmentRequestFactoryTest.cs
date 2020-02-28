44fc10f65a75237eab393b9496c7441d7d8ae2c4
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2020-02-28 15:35:40 -0600

    TP#37173 save account number

---
+++ Code/ShipWorks.Shipping.Tests/Carriers/UPS/OneBalance/UpsShipmentRequestFactoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
+using ShipWorks.Shipping.Carriers.Ups.OneBalance;
+using ShipWorks.Tests.Shared;
+using Xunit;
+
+namespace ShipWorks.Shipping.Tests.Carriers.UPS.OneBalance
+{
+    public class UpsShipmentRequestFactoryTest
+    {
+        private readonly AutoMock mock;
+        private readonly UpsShipmentRequestFactory testObject;
+
+        public UpsShipmentRequestFactoryTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+            testObject = mock.Create<UpsShipmentRequestFactory>();
+        }
+
+        [Fact]
+        public void EnsureCarrierShipmentIsNotNull_ThrowsWhenUpsShipmentIsNulls()
+        {
+        }
+
+    }
+}
