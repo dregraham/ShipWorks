5b01b8c5958ec6bc68352f7f874b597c9f1136df
Kevin Croke <k.croke@shipworks.com>
2020-03-03 11:02:03 -0600

    TP#37175: Add IUpsShipmentValidatorFactory

---
+++ Code/ShipWorks/Shipping/Carriers/UPS/OnLineTools/IUpsShipmentValidatorFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping.Carriers.UPS.OnLineTools
+{
+    /// <summary>
+    /// Create a IUpsShipmentValidator
+    /// </summary>
+    public interface IUpsShipmentValidatorFactory
+    {
+        /// <summary>
+        /// Create a IUpsShipmentValidator
+        /// </summary>
+        IUpsShipmentValidator Create(ShipmentEntity shipment);
+    }
+}
