2c6d6efb59514171d51b25b10abe440da6214371
Chris Hicks <c.hicks@shipworks.com>
2020-03-05 13:17:39 -0700

    TP#37677: Add description to UpsLogoType values

--- Code/ShipWorks/Shipping/Carriers/UPS/Enums/UpsLogoType.cs
+++ Code/ShipWorks/Shipping/Carriers/UPS/Enums/UpsLogoType.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Reflection;
 
 namespace ShipWorks.Shipping.Carriers.UPS.Enums
@@ -5,8 +6,10 @@ namespace ShipWorks.Shipping.Carriers.UPS.Enums
     [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
     public enum UpsLogoType
     {
+        [Description("UPS")]
         Ups = 1,
 
+        [Description("UPS from ShipWorks")]
         UpsFromShipWorks = 2
     }
 }
