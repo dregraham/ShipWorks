1b8fbcce4929c5a0ea090cd4dade20019eeab868
Chris Hicks <c.hicks@shipworks.com>
2020-03-05 11:55:34 -0700

    TP#37677: Update the logo in the UPS setup wizard based on whether or not its a one balance account

---
+++ Code/ShipWorks/Shipping/Carriers/UPS/Enums/UpsLogoType.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+namespace ShipWorks.Shipping.Carriers.UPS.Enums
+{
+    [Obfuscation(Feature = "PreserveLiteralValues", Exclude = false, StripAfterObfuscation = false)]
+    public enum UpsLogoType
+    {
+        Ups = 1,
+
+        UpsFromShipWorks = 2
+    }
+}
