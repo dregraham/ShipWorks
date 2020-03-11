445a4f94636cb0e890734efada0d5b62db3a8592
Cody Hart <c.hart@shipworks.com>
2020-03-11 10:13:47 -0500

    TP #37171: Finish breaking out the auto fund control

---
+++ Code/ShipWorks.Shipping.UI/Settings/OneBalance/IOneBalanceAutoFundControlViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace ShipWorks.Shipping.UI.Settings.OneBalance
+{
+    /// <summary>
+    /// Interface for the OneBalanceAutoFundControlViewModel
+    /// </summary>
+    public interface IOneBalanceAutoFundControlViewModel
+    {
+        /// <summary>
+        /// The current balance of the one balance account
+        /// </summary>
+        decimal Balance { get; set; }
+
+        /// <summary>
+        /// The amount to add to the account balance when auto funding
+        /// </summary>
+        decimal AutoFundAmount { get; set; }
+
+        /// <summary>
+        /// The message to be displayed if we couldn't get auto fund settings
+        /// </summary>
+        string AutoFundError { get; set; }
+
+        /// <summary>
+        /// A value indicating if auto funding is turned on
+        /// </summary>
+        bool IsAutoFund { get; set; }
+
+        /// <summary>
+        /// The account balance that triggers the auto fund
+        /// </summary>
+        decimal MinimumBalance { get; set; }
+
+        /// <summary>
+        /// Make an API call to stamps tp save the auto fund settings
+        /// </summary>
+        void SaveSettings();
+    }
+}
--- Code/ShipWorks.Shipping.UI/Settings/OneBalance/OneBalanceAutoFundControlViewModel.cs
+++ Code/ShipWorks.Shipping.UI/Settings/OneBalance/OneBalanceAutoFundControlViewModel.cs
@@ -1,18 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
+using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Shipping.Carriers.Postal.Usps;
+using ShipWorks.Shipping.Carriers.Postal.Usps.Api.Net;
+using ShipWorks.Shipping.Carriers.Postal.Usps.WebServices;
 
 namespace ShipWorks.Shipping.UI.Settings.OneBalance
 {
-    public class OneBalanceAutoFundControlViewModel : ViewModelBase
+    /// <summary>
+    /// View model for the OneBalanceAutoFundControl
+    /// </summary>
+    [Component]
+    public class OneBalanceAutoFundControlViewModel : ViewModelBase, IOneBalanceAutoFundControlViewModel
     {
+        private readonly IUspsAccountManager accountManager;
+        private IUspsWebClient webClient;
+        private AutoBuySettings autoBuySettings;
+
+        private decimal balance;
         private decimal minimumBalance;
         private decimal autoFundAmount;
         private bool isAutoFund;
+        private string autoFundError;
+        private bool loading = true;
+
+        /// <summary>
+        /// Constuctor
+        /// </summary>
+        public OneBalanceAutoFundControlViewModel(IUspsAccountManager accountManager)
+        {
+            this.accountManager = accountManager;
+            SetupWebClient();
+            InitiateGetAutoFundSettings();
+        }
+
+        /// <summary>
+        /// The current balance of the one balance account
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public decimal Balance
+        {
+            get => balance;
+            set
+            {
+                Set(ref balance, value);
+                Loading = false;
+            }
+        }
 
         /// <summary>
         /// The account balance that triggers the auto fund
@@ -42,6 +79,107 @@ namespace ShipWorks.Shipping.UI.Settings.OneBalance
         {
             get => isAutoFund;
             set => Set(ref isAutoFund, value);
+        }
+
+        /// <summary>
+        /// The message to be displayed if we couldn't get auto fund settings
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public string AutoFundError
+        {
+            get => autoFundError;
+            set => Set(ref autoFundError, value);
+        }
+
+        /// <summary>
+        /// A flag to indicate if we are still trying to load the balance
+        /// </summary>
+        [Obfuscation(Exclude = true)]
+        public bool Loading
+        {
+            get => loading;
+            set => Set(ref loading, value);
+        }
+
+        /// <summary>
+        /// Make an API call to stamps tp save the auto fund settings
+        /// </summary>
+        public void SaveSettings()
+        {
+            // Only set if something changed
+            if (autoBuySettings == null ||
+                autoBuySettings.AutoBuyEnabled != IsAutoFund ||
+                autoBuySettings.PurchaseAmount != AutoFundAmount ||
+                autoBuySettings.TriggerAmount != MinimumBalance)
+            {
+                var account = accountManager.UspsAccounts.FirstOrDefault(a => a.ShipEngineCarrierId != null);
+
+                if (account == null)
+                {
+                    return;
+                }
+
+                var newAutoBuySettings = new AutoBuySettings()
+                {
+                    AutoBuyEnabled = IsAutoFund,
+                    PurchaseAmount = AutoFundAmount,
+                    TriggerAmount = MinimumBalance
+                };
+
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        webClient.SetAutoBuy(account, newAutoBuySettings);
+                    }
+                    catch (Exception ex)
+                    {
+                        // If there's an exception it's already been logged, so we don't need to do anything here
+                        // because the user has already left the One Balance settings screen
+                    }
+                });
+            }
+        }
+
+        /// <summary>
+        /// Sets up the web client
+        /// </summary>
+        private void SetupWebClient()
+        {
+            UspsShipmentType shipmentType = ShipmentTypeManager.GetType(ShipmentTypeCode.Usps) as UspsShipmentType;
+            webClient = shipmentType.CreateWebClient();
+        }
+
+        /// <summary>
+        /// Fire and forget the retrieval of auto fund settings
+        /// </summary>
+        private void InitiateGetAutoFundSettings()
+        {
+            Task.Run(() => GetAutoFundSettings());
+        }
+
+        /// <summary>
+        /// Retrieve the accounts balance if we have a One Balance account
+        /// </summary>
+        private void GetAutoFundSettings()
+        {
+            var account = accountManager.UspsAccounts.FirstOrDefault(a => a.ShipEngineCarrierId != null);
+
+            try
+            {
+                var accountInfo = (AccountInfoV41) webClient.GetAccountInfo(account);
+
+                IsAutoFund = accountInfo.AutoBuySettings.AutoBuyEnabled;
+                MinimumBalance = accountInfo.AutoBuySettings.TriggerAmount;
+                AutoFundAmount = accountInfo.AutoBuySettings.PurchaseAmount;
+
+                autoBuySettings = accountInfo.AutoBuySettings;
+            }
+            catch (Exception)
+            {
+                // We don't need to log the exception because the webclient takes care of that
+                AutoFundError = "There was a problem retrieving your automatic funding settings.";
+            }
         }
     }
 }
