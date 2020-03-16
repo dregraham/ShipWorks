2aafd0bde958eef3d01ede633de6bc2bf2362aae
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2020-03-16 11:05:51 -0500

    TP#37671 add get rates to IDhlExpressStampsWebClient

--- Code/ShipWorks.Shipping/Carriers/Dhl/API/Stamps/IDhlExpressStampsWebClient.cs
+++ Code/ShipWorks.Shipping/Carriers/Dhl/API/Stamps/IDhlExpressStampsWebClient.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Interapptive.Shared.Utility;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Shipping.Carriers.Postal.Usps;
+using ShipWorks.Shipping.Editing.Rating;
 
 namespace ShipWorks.Shipping.Carriers.Dhl.API.Stamps
 {
@@ -19,5 +22,10 @@ namespace ShipWorks.Shipping.Carriers.Dhl.API.Stamps
         /// Void the given shipment
         /// </summary>
         void VoidShipment(ShipmentEntity shipment);
+
+        /// <summary>
+        /// Get rates for the given shipment
+        /// </summary>
+        (IEnumerable<RateResult> rates, IEnumerable<Exception> errors) GetRates(ShipmentEntity shipment);
     }
 }
