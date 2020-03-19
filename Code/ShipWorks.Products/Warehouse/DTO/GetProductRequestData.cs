488ff52e022033ca26fbef8a7a3ba7d51c513a77
thughesSW <t.hughes@shipworks.com>
2020-03-19 12:26:14 -0500

    TP#37911: Added GetProductRequestData

---
+++ Code/ShipWorks.Products/Warehouse/DTO/GetProductRequestData.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace ShipWorks.Products.Warehouse.DTO
+{
+    /// <summary>
+    /// Request for getting a product by productId 
+    /// </summary>
+    [Obfuscation]
+    public class GetProductRequestData
+    {
+        /// <summary>
+        /// ProductId of product to be retrieved
+        /// </summary>
+        public string ProductId { get; set; }
+    }
+}
