bcbec15bfdc6bd36ca3c7c01c288ee1576288da4
thughesSW <t.hughes@shipworks.com>
2020-03-19 12:28:57 -0500

    TP#37911: Added GetProductResponseData

---
+++ Code/ShipWorks.Products/Warehouse/DTO/GetProductResponseData.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace ShipWorks.Products.Warehouse.DTO
+{
+    /// <summary>
+    /// Response from call to get a product
+    /// </summary>
+    [Obfuscation]
+    public class GetProductResponseData
+    {
+        /// <summary>
+        /// WarehouseProduct returned
+        /// </summary>
+        public WarehouseProduct Product { get; set; }
+    }
+}
