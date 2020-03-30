57a2b51db4b7e226a50d018986b2b318806397e2
Josh Winchester <j.winchester@shipworks.com>
2020-03-30 16:26:52 -0500

    TP#37122: Add single scan confirmation enums

---
+++ Code/ShipWorks.SingleScan/Enums/SingleScanConfirmationMode.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace ShipWorks.SingleScan.Enums
+{
+    /// <summary>
+    /// Enums representing each single scan confirmation mode.
+    /// </summary>
+    public enum SingleScanConfirmationMode
+    {
+        [Description("Select")]
+        Select = 0,
+
+        [Description("Create New")]
+        CreateNew = 1,
+
+        [Description("Print Existing")]
+        PrintExisting = 2
+    }
+}
