5a8098aaa5f04d5d2ee603a3169fc775085a7681
Josh Winchester <j.winchester@shipworks.com>
2020-04-01 17:02:59 -0500

    TP#37122: Update upgrade script

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.9/7.9.0.0.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.9/7.9.0.0.sql
@@ -1,4 +1,5 @@
 PRINT N'Altering [dbo].[UserSettings]'
 GO
 IF COL_LENGTH(N'[dbo].[UserSettings]', N'SingleScanConfirmationMode') IS NULL
-ALTER TABLE [dbo].[UspsAccount] ADD [SingleScanConfirmationMode] [int] NOT NULL CONSTRAINT [DF_UserSettings_SingleScanConfirmationMode] DEFAULT ((0))
+ALTER TABLE [dbo].[UserSettings] ADD [SingleScanConfirmationMode] [int] NOT NULL CONSTRAINT [DF_UserSettings_SingleScanConfirmationMode] DEFAULT ((0))
+GO
