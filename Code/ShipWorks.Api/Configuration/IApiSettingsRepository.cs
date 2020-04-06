aa9cb08d62869d561207185bb8f4fa8e213fff44
Kevin Croke <k.croke@shipworks.com>
2020-04-06 14:11:47 -0500

    TP#36993 Add IApiSettingsRepository

---
+++ Code/ShipWorks.Api/Configuration/IApiSettingsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipWorks.Api.Configuration
+{
+    /// <summary>
+    /// Interface for an ApiSettingsRepository
+    /// </summary>
+    public interface IApiSettingsRepository
+    {
+        /// <summary>
+        /// Save settings
+        /// </summary>
+        void Save(ApiSettings settings);
+
+        /// <summary>
+        /// Load Settings
+        /// </summary
+        ApiSettings Load();
+    }
+}
