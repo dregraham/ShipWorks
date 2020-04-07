451a76031f1ec887518e1ce1c6ba58917512be5c
Kevin Croke <k.croke@shipworks.com>
2020-04-07 10:22:30 -0500

    TP#36993: Confirm the healthcheck is from ShipWorks

--- Code/ShipWorks.Api/HealthCheck/HealthCheckClient.cs
+++ Code/ShipWorks.Api/HealthCheck/HealthCheckClient.cs
@@ -4,6 +4,7 @@ using Interapptive.Shared.ComponentRegistration;
 using Interapptive.Shared.Net.RestSharp;
 using log4net;
 using RestSharp;
+using ShipWorks.ApplicationCore;
 
 namespace ShipWorks.Api.HealthCheck
 {
@@ -15,16 +16,18 @@ namespace ShipWorks.Api.HealthCheck
     {
         private readonly IRestClientFactory clientFactory;
         private readonly IRestRequestFactory requestFactory;
+        private readonly IShipWorksSession session;
         private readonly ILog log;
         
         /// <summary>
         /// Constructor
         /// </summary>
         public HealthCheckClient(IRestClientFactory clientFactory, IRestRequestFactory requestFactory,
-                                 Func<Type, ILog> loggerFactory)
+                                 IShipWorksSession session, Func<Type, ILog> loggerFactory)
         {
             this.clientFactory = clientFactory;
             this.requestFactory = requestFactory;
+            this.session = session;
             log = loggerFactory(typeof(HealthCheckClient));
         }
 
@@ -40,11 +43,23 @@ namespace ShipWorks.Api.HealthCheck
 
             try
             {
-                HttpStatusCode statusCode = client.Execute(request).StatusCode;
+                var response = client.Execute<HealthCheckResponse>(request);
+                HttpStatusCode statusCode = response.StatusCode;
 
                 log.Debug($"Api healthcheck response status {statusCode} on port ${portNumber}");
 
-                return statusCode == HttpStatusCode.OK;
+                if (response.StatusCode != HttpStatusCode.OK)
+                {                    
+                    return false;
+                }
+
+                if (response.Data.InstanceId != session.InstanceID)
+                {
+                    log.Debug($"Api healthcheck responded with wrong instance code: {response.Data.InstanceId}");
+                    return false;
+                }
+
+                return true;
             }
             catch (Exception ex)
             {
