23843d5906aa0720ff051115f1d3405b05509be0
Cody Hart <c.hart@shipworks.com>
2020-04-08 14:38:29 -0500

    TP #38040: Update banner button text

--- Code/ShipWorks/Shipping/Carriers/UPS/OneBalanceUpsBannerControl.Designer.cs
+++ Code/ShipWorks/Shipping/Carriers/UPS/OneBalanceUpsBannerControl.Designer.cs
@@ -40,7 +40,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(90, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Enable UPS";
+            this.button1.Text = "Add UPS";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.OnEnableUps);
             // 
