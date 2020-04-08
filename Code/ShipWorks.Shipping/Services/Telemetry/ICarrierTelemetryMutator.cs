c1741501a1aca745479be6991e4b2aa084fc5e32
Cody Hart <c.hart@shipworks.com>
2020-04-08 10:32:46 -0500

    TP #37533: Add mutator interface

---
+++ Code/ShipWorks.Shipping/Services/Telemetry/ICarrierTelemetryMutator.cs
@@ -0,0 +1,18 @@
+﻿using Interapptive.Shared.Metrics;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.Shipping.Services.Telemetry
+{
+    public interface ICarrierTelemetryMutator
+    {
+        /// <summary>
+        /// Sets the carrier specific shipment telemetry properties
+        /// </summary>
+        TrackedDurationEvent SetShipmentTelemetry(TrackedDurationEvent telemetryEvent, ShipmentEntity shipment);
+
+        /// <summary>
+        /// Sets the carrier specific package telemetry properties
+        /// </summary>
+        TrackedDurationEvent SetPackageTelemetry(TrackedDurationEvent telemetryEvent, IPackageAdapter[] packages);
+    }
+}
