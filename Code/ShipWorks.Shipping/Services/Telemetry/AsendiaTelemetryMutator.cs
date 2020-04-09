3bde404f1ea5e336107d2737ff4739760731d351
John Ackerman <j.ackerman@shipworks.com>
2020-04-09 10:57:36 -0500

    TP #37533: Add AsendiaTelemetryMutator

---
+++ Code/ShipWorks.Shipping/Services/Telemetry/AsendiaTelemetryMutator.cs
@@ -0,0 +1,48 @@
+﻿using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.Metrics;
+using Interapptive.Shared.Utility;
+using ShipWorks.Common.IO.Hardware.Printers;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.ShipEngine;
+
+namespace ShipWorks.Shipping.Services.Telemetry
+{
+    /// <summary>
+    /// Sets Asendia-specific telemetry
+    /// </summary>
+    [KeyedComponent(typeof(ICarrierTelemetryMutator), ShipmentTypeCode.Asendia)]
+    public class AsendiaTelemetryMutator : ICarrierTelemetryMutator
+    {
+        /// <summary>
+        /// Sets the carrier specific telemetry properties
+        /// </summary>
+        public void MutateTelemetry(TrackedDurationEvent telemetryEvent, ShipmentEntity shipment)
+        {
+            SetShipmentTelemetry(telemetryEvent, shipment);
+        }
+
+        /// <summary>
+        /// Sets Asendia-specific shipment telemetry
+        /// </summary>
+        /// <param name="telemetryEvent"></param>
+        /// <param name="shipment"></param>
+        private void SetShipmentTelemetry(TrackedDurationEvent telemetryEvent, ShipmentEntity shipment)
+        {
+            var asendiaShipment = shipment.Asendia;
+
+            telemetryEvent.AddProperty("Label.Asendia.AsendiaAccountID", asendiaShipment.AsendiaAccountID.ToString());
+            telemetryEvent.AddProperty("Label.Asendia.", EnumHelper.GetDescription((ShipEngineContentsType) asendiaShipment.Contents));
+            telemetryEvent.AddProperty("Label.Asendia.DimsAddWeight", asendiaShipment.DimsAddWeight.ToString());
+            telemetryEvent.AddProperty("Label.Asendia.DimsHeight", asendiaShipment.DimsHeight.ToString());
+            telemetryEvent.AddProperty("Label.Asendia.DimsLength", asendiaShipment.DimsLength.ToString());
+            telemetryEvent.AddProperty("Label.Asendia.DimsWeight", asendiaShipment.DimsWeight.ToString());
+            telemetryEvent.AddProperty("Label.Asendia.DimsWidth", asendiaShipment.DimsWidth.ToString());
+            telemetryEvent.AddProperty("Label.Asendia.Insurance", asendiaShipment.Insurance.ToString());
+            telemetryEvent.AddProperty("Label.Asendia.InsuranceValue", asendiaShipment.InsuranceValue.ToString());
+            telemetryEvent.AddProperty("Label.Asendia.NonDelivery", EnumHelper.GetDescription((ShipEngineNonDeliveryType) asendiaShipment.NonDelivery));
+            telemetryEvent.AddProperty("Label.Asendia.NonMachinable", asendiaShipment.NonMachinable.ToString());
+            telemetryEvent.AddProperty("Label.Asendia.RequestedLabelFormat", EnumHelper.GetDescription((ThermalLanguage) asendiaShipment.RequestedLabelFormat));
+            telemetryEvent.AddProperty("Label.Asendia.Service", EnumHelper.GetDescription(asendiaShipment.Service));
+        }
+    }
+}
