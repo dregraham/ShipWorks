459d861d8159756fc5ff1a0ffe544943e959db75
Josh Winchester <j.winchester@shipworks.com>
2020-05-13 13:28:43 -0500

    TP#36991: Add 7.10.0.0 upgrade script

---
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/7.10/7.10.0.0.sql
@@ -0,0 +1,5 @@
+﻿PRINT N'Altering [dbo].[ShippingSettings]'
+GO
+IF COL_LENGTH(N'[dbo].[ShippingSettings]', N'UpsAllowNoDims') IS NULL
+ALTER TABLE [dbo].[ShippingSettings] ADD [UpsAllowNoDims] [bit] NOT NULL CONSTRAINT [DF_ShippingSettings_UpsAllowNoDims] DEFAULT ((0))
+GO
