f8c29a54c596ea0e64f33a35797c9d30d79dc5f1
Kevin Croke <k.croke@shipworks.com>
2020-06-02 09:37:58 -0400

    TP#39601: Add IDefaultWarehoueCreator

---
+++ Code/ShipWorks/ApplicationCore/Licensing/Warehouse/IDefaultWarehouseCreator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interapptive.Shared.Utility;
+using ShipWorks.Data.Model.EntityClasses;
+
+namespace ShipWorks.ApplicationCore.Licensing.Warehouse
+{
+    /// <summary>
+    /// Creates a default warehouse
+    /// </summary>
+    public interface IDefaultWarehouseCreator
+    {
+        /// <summary>
+        /// Creates a default warehouse if needed
+        /// </summary>
+        Task Create(StoreEntity store);
+
+        /// <summary>
+        /// Returns true if no linked warehouse and one needs to be created
+        /// </summary>
+        Task<bool> NeedsDefaultWarehouse();
+    }
+}
