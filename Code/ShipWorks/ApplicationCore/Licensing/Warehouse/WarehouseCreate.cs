a220b7cd7e0820fbd1ff52fd17c769ed9f87a1ad
Kevin Croke <k.croke@shipworks.com>
2020-06-02 10:12:09 -0400

    TP#39601: Implement WarehouseCreate

---
+++ Code/ShipWorks/ApplicationCore/Licensing/Warehouse/WarehouseCreate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Interapptive.Shared.Utility;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using ShipEngine.ApiClient.Model;
+using ShipWorks.ApplicationCore.Licensing.Warehouse.DTO;
+using ShipWorks.Common.Net;
+
+namespace ShipWorks.ApplicationCore.Licensing.Warehouse
+{
+    /// <summary>
+    /// Create warehouse
+    /// </summary>
+    public class WarehouseCreate
+    {
+        private readonly IWarehouseRequestClient warehouseRequestClient;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public WarehouseCreate(IWarehouseRequestClient warehouseRequestClient)
+        {
+            this.warehouseRequestClient = warehouseRequestClient;
+        }
+
+        /// <summary>
+        /// Creates a warehouse on the hub and returns the id of the new warehouse
+        /// </summary>
+        public async Task<GenericResult<string>> Create(Details warehouse)
+        {
+            try
+            {
+                IRestRequest request = new RestRequest(WarehouseEndpoints.Warehouses, Method.POST);
+                request.JsonSerializer = new RestSharpJsonNetSerializer();
+                request.RequestFormat = DataFormat.Json;
+                request.AddJsonBody(warehouse);
+
+                var response = await warehouseRequestClient.MakeRequest(request, "CreateDefaultWarehouse").ConfigureAwait(false);
+
+                if (response.Success)
+                {
+                    return JObject.Parse(response.Value.Content).GetValue("id").Value<string>();
+                }
+   
+                return GenericResult.FromError<string>(response.Message);
+
+            }
+            catch (Exception ex)
+            {
+                return GenericResult.FromError<string>(ex);
+            }
+        }
+    }
+}
