ad273488f61f663b54da3c66d353bb4b612ac81f
Josh Winchester <j.winchester@shipworks.com>
2020-07-31 08:35:46 -0500

    TP#40420: Add validate credentials to remove shopify throttle for checking credentials

--- Code/ShipWorks/Stores/Platforms/Shopify/ShopifyWebClient.cs
+++ Code/ShipWorks/Stores/Platforms/Shopify/ShopifyWebClient.cs
@@ -72,48 +72,29 @@ namespace ShipWorks.Stores.Platforms.Shopify
         }
 
         /// <summary>
-        /// Determines if the given shop name is a valid shopify shop
+        /// Validate Authorization for getting orders in Shopify.
         /// </summary>
-        public static bool IsRealShopifyShopUrlName(string shopUrlName)
+        public void ValidateCredentials()
         {
             HttpVariableRequestSubmitter requestSubmitter = new HttpVariableRequestSubmitter();
             requestSubmitter.Verb = HttpVerb.Get;
+            requestSubmitter.Uri = new Uri(Endpoints.ApiGetOrderCountUrl);
 
             try
             {
-                requestSubmitter.Uri = new ShopifyEndpoints(shopUrlName).GetApiAuthorizeUrl();
-            }
-            catch (UriFormatException ex)
-            {
-                log.Warn("The specified shop name created an invalid URI", ex);
-
-                return false;
-            }
-
-            ApiLogEntry logEntry = new ApiLogEntry(ApiLogSource.Shopify, "LoginPage");
-            logEntry.LogRequest(requestSubmitter);
-
-            try
-            {
-                RequestThrottleParameters requestThrottleArgs = new RequestThrottleParameters(ShopifyWebClientApiCall.IsRealShopifyShopUrlName, requestSubmitter, null);
-
-                using (IHttpResponseReader respReader = throttler.ExecuteRequest<HttpRequestSubmitter, IHttpResponseReader>(requestThrottleArgs, MakeRequest))
-                {
-                    string pageText = respReader.ReadResult();
-                    logEntry.LogResponse(pageText, "html");
-
-                    // Check the content of the page for the form action that goes to the auth login page.
-                    // If the content contains the html, it is the login page.
-                    bool isLoginPage = pageText.IndexOf("login", StringComparison.OrdinalIgnoreCase) > -1;
-
-                    return isLoginPage;
-                }
+                //Get the response without throttle
+                AddAuthHeaderToRequest(requestSubmitter);
+                requestSubmitter.GetResponse();
             }
             catch (WebException ex)
             {
-                log.Error("Could not open shopify login page", ex);
+                HttpWebResponse webResponse = ex.Response as HttpWebResponse;
 
-                return false;
+                if (webResponse?.StatusCode == HttpStatusCode.Forbidden || webResponse?.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    throw new ShopifyAuthorizationException("You do not have the correct permissions to perform this operation. \n" +
+                                                            " Try updating your Shopify token in the store manager", ex);
+                }
             }
         }
 
@@ -139,8 +120,8 @@ namespace ShipWorks.Stores.Platforms.Shopify
                     string response = reader.ReadResult();
 
                     // Parse out the access token
-                    JObject accessTokenRensponse = JObject.Parse(response);
-                    return (string) accessTokenRensponse[ShopifyConstants.AccessTokenParamName];
+                    JObject accessTokenResponse = JObject.Parse(response);
+                    return (string) accessTokenResponse[ShopifyConstants.AccessTokenParamName];
                 }
             }
             catch (JsonException ex)
