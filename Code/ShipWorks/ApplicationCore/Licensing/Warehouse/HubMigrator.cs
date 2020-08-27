d3e493091dde917445c0b17eff2afee974bd2a43
Chris Hicks <c.hicks@shipworks.com>
2020-08-27 12:49:07 -0600

    TP#39826: Only prompt user to migrate if they are an admin and they have linked a warehouse.

--- Code/ShipWorks/ApplicationCore/Licensing/Warehouse/HubMigrator.cs
+++ Code/ShipWorks/ApplicationCore/Licensing/Warehouse/HubMigrator.cs
@@ -6,9 +6,11 @@ using System.Windows.Forms;
 using Interapptive.Shared.ComponentRegistration;
 using Interapptive.Shared.UI;
 using Interapptive.Shared.Utility;
+using ShipWorks.Data;
 using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Stores;
 using ShipWorks.Stores.Management;
+using ShipWorks.Users;
 
 namespace ShipWorks.ApplicationCore.Licensing.Warehouse
 {
@@ -22,16 +24,22 @@ namespace ShipWorks.ApplicationCore.Licensing.Warehouse
         private readonly IStoreTypeManager storeTypeManager;
         private readonly IMessageHelper messageHelper;
         private readonly IWarehouseStoreClient warehouseStoreClient;
+        private readonly IConfigurationData configurationData;
+        private readonly IUserSession userSession;
 
         /// <summary>
         /// Constructor
         /// </summary>
-        public HubMigrator(IStoreManager storeManager, IStoreTypeManager storeTypeManager, IMessageHelper messageHelper, IWarehouseStoreClient warehouseStoreClient)
+        public HubMigrator(IStoreManager storeManager, IStoreTypeManager storeTypeManager, IMessageHelper messageHelper,
+                           IWarehouseStoreClient warehouseStoreClient, IConfigurationData configurationData,
+                           IUserSession userSession)
         {
             this.storeManager = storeManager;
             this.storeTypeManager = storeTypeManager;
             this.messageHelper = messageHelper;
             this.warehouseStoreClient = warehouseStoreClient;
+            this.configurationData = configurationData;
+            this.userSession = userSession;
         }
 
         /// <summary>
@@ -40,51 +48,58 @@ namespace ShipWorks.ApplicationCore.Licensing.Warehouse
         /// </summary>
         public async Task MigrateStores()
         {
-            IEnumerable<StoreEntity> storesToMigrate =
-                storeManager.GetAllStores()
-                    .Where(s => s.WarehouseStoreID == null && storeTypeManager.GetType(s).ShouldUseHub(s));
-
-            List<string> storesThatFailedMigration = new List<string>();
-
-            if (storesToMigrate.Any())
+            // Only allow admin users to perform migration. Also require the database to be linked to warehouse.
+            if (!string.IsNullOrWhiteSpace(configurationData.FetchReadOnly().WarehouseID) && userSession.User.IsAdmin)
             {
-                DialogResult dialogResult = messageHelper.ShowQuestion(
-                    "ShipWorks has detected stores that need to be migrated to the Hub. Would you like to perform the migration now?");
+                IEnumerable<StoreEntity> storesToMigrate =
+                    storeManager.GetAllStores()
+                        .Where(s => s.WarehouseStoreID == null && storeTypeManager.GetType(s).ShouldUseHub(s));
 
-                if (dialogResult == DialogResult.OK)
+                List<string> storesThatFailedMigration = new List<string>();
+
+                // Only prompt user to migrate if they have stores that need to be migrated
+                if (storesToMigrate.Any())
                 {
-                    using (var progressDialog =
-                        messageHelper.ShowProgressDialog("Migrating stores to Hub", "Migrating stores to Hub"))
-                    {
-                        int index = 1;
-                        int totalStoresToMigrate = storesToMigrate.Count();
-                        progressDialog.ProgressItem.PercentComplete = 0;
+                    DialogResult dialogResult = messageHelper.ShowQuestion(
+                        "ShipWorks has detected stores that need to be migrated to the Hub. Would you like to perform the migration now?");
 
-                        foreach (StoreEntity store in storesToMigrate)
+                    if (dialogResult == DialogResult.OK)
+                    {
+                        using (var progressDialog =
+                            messageHelper.ShowProgressDialog("Migrating stores to Hub", "Migrating stores to Hub"))
                         {
-                            progressDialog.ProgressItem.Detail = $"Store {index} of {totalStoresToMigrate}";
-                            Result result = await warehouseStoreClient.UploadStoreToWarehouse(store);
+                            int index = 1;
+                            int totalStoresToMigrate = storesToMigrate.Count();
+                            progressDialog.ProgressItem.PercentComplete = 0;
 
-                            if (result.Success)
+                            foreach (StoreEntity store in storesToMigrate)
                             {
-                                storeManager.SaveStore(store);
+                                progressDialog.ProgressItem.Detail = $"Store {index} of {totalStoresToMigrate}";
+                                Result result = await warehouseStoreClient.UploadStoreToWarehouse(store);
+
+                                if (result.Success)
+                                {
+                                    storeManager.SaveStore(store);
+                                }
+                                else
+                                {
+                                    storesThatFailedMigration.Add(store.StoreName);
+                                }
+
+                                progressDialog.ProgressItem.PercentComplete = index / totalStoresToMigrate * 100;
                             }
-                            else
-                            {
-                                storesThatFailedMigration.Add(store.StoreName);
-                            }
-                            progressDialog.ProgressItem.PercentComplete = index / totalStoresToMigrate * 100;
                         }
-                    }
 
-                    if (storesThatFailedMigration.Any())
-                    {
-                        string errorMessage = CreateStoreMigrationErrorMessage(storesThatFailedMigration);
-                        messageHelper.ShowError(errorMessage);
-                    }
-                    else
-                    {
-                        messageHelper.ShowInformation("ShipWorks successfully migrated all of your supported stores to the Hub!");
+                        if (storesThatFailedMigration.Any())
+                        {
+                            string errorMessage = CreateStoreMigrationErrorMessage(storesThatFailedMigration);
+                            messageHelper.ShowError(errorMessage);
+                        }
+                        else
+                        {
+                            messageHelper.ShowInformation(
+                                "ShipWorks successfully migrated all of your supported stores to the Hub!");
+                        }
                     }
                 }
             }
