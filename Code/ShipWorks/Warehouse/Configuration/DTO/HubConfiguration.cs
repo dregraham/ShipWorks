692f6be96c9c8f3f020504979cec5c788ed42337
Cody Hart <c.hart@shipworks.com>
2020-09-08 09:01:04 -0500

    Add comments for DTOs

--- Code/ShipWorks/Warehouse/Configuration/DTO/HubConfiguration.cs
+++ Code/ShipWorks/Warehouse/Configuration/DTO/HubConfiguration.cs
@@ -4,9 +4,15 @@ using ShipWorks.Warehouse.Configuration.DTO.ShippingSettings;
 
 namespace ShipWorks.Warehouse.Configuration.DTO
 {
+    /// <summary>
+    /// DTO for importing hub configuration data
+    /// </summary>
     [Obfuscation]
     public class HubConfiguration
     {
+        /// <summary>
+        /// A list of the carriers configured in hub
+        /// </summary>
         public List<CarrierConfiguration> CarrierConfigurations { get; set; }
     }
 }
--- Code/ShipWorks/Warehouse/Configuration/DTO/ShippingSettings/CarrierConfigurationPayload.cs
+++ Code/ShipWorks/Warehouse/Configuration/DTO/ShippingSettings/CarrierConfigurationPayload.cs
@@ -6,15 +6,30 @@ using Newtonsoft.Json.Linq;
 
 namespace ShipWorks.Warehouse.Configuration.DTO.ShippingSettings
 {
+    /// <summary>
+    /// DTO for importing a carrier confiugration from hub
+    /// </summary>
     [Obfuscation]
     public class CarrierConfigurationPayload
     {
+        /// <summary>
+        /// The address for the carrier
+        /// </summary>
         public ConfigurationAddress Address { get; set; }
 
+        /// <summary>
+        /// The version of the configuration that is in hub
+        /// </summary>
         public int HubVersion { get; set; }
 
+        ///<summary>
+        /// The hub carrier id
+        ///</summary>
         public Guid HubCarrierId { get; set; }
 
+        /// <summary>
+        /// Additional carrier specific configuration data
+        /// </summary>
         [JsonExtensionData]
         public IDictionary<string, JToken> AdditionalData { get; set; }
     }
