3a3819a39c8b3b82c86df2daaa6b3a23c5768fc4
Josh Winchester <j.winchester@shipworks.com>
2020-09-21 17:09:48 -0500

    TP#40992: Add UpsAccountConfiguration

---
+++ Code/ShipWorks/Warehouse/Configuration/DTO/ShippingSettings/UpsAccountConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace ShipWorks.Warehouse.Configuration.DTO.ShippingSettings
+{
+    /// <summary>
+    /// DTO for importing ups account data from the hub
+    /// </summary>
+    [Obfuscation]
+    public class UpsAccountConfiguration
+    {
+        /// <summary>
+        /// The UPS access token
+        /// </summary>
+        public string AccessToken { get; set; }
+    }
+}
