f07f1af3fc99192d0b4ccf4f042b52574f4d446f
Josh Winchester <j.winchester@shipworks.com>
2020-09-22 16:35:21 -0500

    TP#40978: Add new scripts for FedExAccount

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/8.3/8.3.0.0.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/8.3/8.3.0.0.sql
@@ -10,3 +10,9 @@ ALTER TABLE [dbo].[UpsAccount] ADD
 [HubVersion] [int] NULL,
 [HubCarrierId] [uniqueidentifier] NULL
 GO
+PRINT N'Altering [dbo].[FedExAccount]'
+GO
+ALTER TABLE [dbo].[FedExAccount] ADD
+[HubVersion] [int] NULL,
+[HubCarrierId] [uniqueidentifier] NULL
+GO
