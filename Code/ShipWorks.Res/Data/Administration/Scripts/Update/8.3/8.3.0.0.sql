71d193842e26fa5b526d93511d3d07efbecad1a6
Josh Winchester <j.winchester@shipworks.com>
2020-09-22 09:43:14 -0500

    TP#40992: Update scripts for adding new columns to ups account

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/8.3/8.3.0.0.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/8.3/8.3.0.0.sql
@@ -4,3 +4,9 @@ ALTER TABLE [dbo].[UspsAccount] ADD
 [HubVersion] [int] NULL,
 [HubCarrierId] [uniqueidentifier] NULL
 GO
+PRINT N'Altering [dbo].[UpsAccount]'
+GO
+ALTER TABLE [dbo].[UpsAccount] ADD
+[HubVersion] [int] NULL,
+[HubCarrierId] [uniqueidentifier] NULL
+GO
