711986dadda4d4a8e32abf951a46effa0c65d6a7
Josh Winchester <j.winchester@shipworks.com>
2020-09-27 16:59:56 -0500

    TP#40846: Add EndiciaAccountConfiguration

---
+++ Code/ShipWorks/Warehouse/Configuration/DTO/ShippingSettings/EndiciaAccountConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace ShipWorks.Warehouse.Configuration.DTO.ShippingSettings
+{
+    /// <summary>
+    /// DTO for importing Endicia account data from the hub
+    /// </summary>
+    [Obfuscation]
+    public class EndiciaAccountConfiguration
+    {
+        /// <summary>
+        /// The Endicia account number
+        /// </summary>
+        public string accountNumber { get; set; }
+
+        /// <summary>
+        /// The Endicia passphrase
+        /// </summary>
+        public string passphrase { get; set; }
+    }
+}
