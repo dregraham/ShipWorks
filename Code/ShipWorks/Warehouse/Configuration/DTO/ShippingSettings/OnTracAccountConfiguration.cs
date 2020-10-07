032aaf7c1c63b597f275500eff0cc4a700396c00
Josh Winchester <j.winchester@shipworks.com>
2020-10-07 10:42:24 -0500

    TP#41002: Add OnTracAccountConfiguration

---
+++ Code/ShipWorks/Warehouse/Configuration/DTO/ShippingSettings/OnTracAccountConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace ShipWorks.Warehouse.Configuration.DTO.ShippingSettings
+{
+    /// <summary>
+    /// DTO for importing OnTrac account data from the hub
+    /// </summary>
+    [Obfuscation]
+    public class OnTracAccountConfiguration
+    {
+        /// <summary>
+        /// The OnTrac username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The OnTrac account number
+        /// </summary>
+        public int AccountNumber { get; set; }
+    }
+}
