b0ff699889d47f3dc8d99119d291742eb0adc95f
John Ackerman <j.ackerman@shipworks.com>
2020-10-27 16:09:05 -0500

    JIRA #5: Add download error handling to InnoSetupService

--- Code/ShipWorks.Installer/Services/InnoSetupService.cs
+++ Code/ShipWorks.Installer/Services/InnoSetupService.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.IO;
 using System.Net;
+using log4net;
 using ShipWorks.Installer.Models;
 
 namespace ShipWorks.Installer.Services
@@ -12,7 +13,13 @@ namespace ShipWorks.Installer.Services
     public class InnoSetupService : IInnoSetupService
     {
         private const string InnoSetupInstallerURL = "https://prod-sw-downloads-app.s3.amazonaws.com/ShipWorksInstaller.exe";
+        private readonly ILog log;
         private InstallSettings installSettings;
+
+        public InnoSetupService(Func<Type, ILog> logFactory)
+        {
+            log = logFactory(typeof(InnoSetupService));
+        }
 
         /// <summary>
         /// Download the INNO Setup installer
@@ -27,10 +34,9 @@ namespace ShipWorks.Installer.Services
             {
                 client.DownloadFileAsync(new Uri(InnoSetupInstallerURL), $"{Path.GetTempPath()}\\ShipWorksInstaller.exe");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                // TODO: Log exception
-                installSettings.Error = Enums.InstallError.ShipWorks;
+                HandleErrors(ex);
             }
         }
 
@@ -39,7 +45,22 @@ namespace ShipWorks.Installer.Services
         /// </summary>
         private void OnDownloadComplete(object sender, AsyncCompletedEventArgs e)
         {
+            if (e.Error != null)
+            {
+                HandleErrors(e.Error);
+                return;
+            }
+
             installSettings.InnoSetupDownloaded = true;
+        }
+
+        /// <summary>
+        /// Handle errors from downloading the installer
+        /// </summary>
+        private void HandleErrors(Exception ex)
+        {
+            log.Error("An error occurred downloading the background installer.", ex);
+            installSettings.Error = Enums.InstallError.ShipWorks;
         }
     }
 }
