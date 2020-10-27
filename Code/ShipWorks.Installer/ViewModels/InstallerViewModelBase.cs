45f060dae83355df885299a09212aaa5c8615cc2
Cody Hart <c.hart@shipworks.com>
2020-10-27 16:41:07 -0500

    TP #5: Wire up back buttons

--- Code/ShipWorks.Installer/ViewModels/InstallerViewModelBase.cs
+++ Code/ShipWorks.Installer/ViewModels/InstallerViewModelBase.cs
@@ -6,6 +6,9 @@ using ShipWorks.Installer.Services;
 
 namespace ShipWorks.Installer.ViewModels
 {
+    /// <summary>
+    /// Base class for installer view models
+    /// </summary>
     [Obfuscation]
     public abstract class InstallerViewModelBase : ViewModelBase
     {
@@ -21,15 +24,32 @@ namespace ShipWorks.Installer.ViewModels
             NextPage = nextPage;
         }
 
+        /// <summary>
+        /// The page to go to when NextCommand is called
+        /// </summary>
         public NavigationPageType NextPage;
 
+        /// <summary>
+        /// Command for going to the next page
+        /// </summary>
         public RelayCommand NextCommand => new RelayCommand(NextExecute, NextCanExecute);
 
+        /// <summary>
+        /// Command for going to the previous page
+        /// </summary>
+        public RelayCommand BackCommand => new RelayCommand(navigationService.GoBack);
+
+        /// <summary>
+        /// Command handler for the NextCommand
+        /// </summary>
         protected virtual void NextExecute()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Determines if the the NextCommand can execute
+        /// </summary>
         protected virtual bool NextCanExecute()
         {
             throw new NotImplementedException();
