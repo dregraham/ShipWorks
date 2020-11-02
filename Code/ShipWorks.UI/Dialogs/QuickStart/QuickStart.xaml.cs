f2bebaa3766a9352963bdd0365fdfd708a4d79c9
Chris Hicks <c.hicks@shipworks.com>
2020-11-02 13:53:42 -0700

    WORKS-152: Create QuickStart dialog. Add new svg based icons.

---
+++ Code/ShipWorks.UI/Dialogs/QuickStart/QuickStart.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace ShipWorks.UI.Dialogs.QuickStart
+{
+    public partial class QuickStart : Window
+    {
+        public QuickStart()
+        {
+            InitializeComponent();
+        }
+    }
+}
