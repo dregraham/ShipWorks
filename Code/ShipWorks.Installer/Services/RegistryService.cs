cec4007f6651fd7c860ac865b4d136a79035bea0
John Ackerman <j.ackerman@shipworks.com>
2020-11-03 09:40:58 -0600

    WORKS-257: Return default path if an error occurs while fetching existing installation path

--- Code/ShipWorks.Installer/Services/RegistryService.cs
+++ Code/ShipWorks.Installer/Services/RegistryService.cs
@@ -26,23 +26,30 @@ namespace ShipWorks.Installer.Services
         public string GetInstallPath()
         {
             log.Info("Getting install path");
-            var shipWorksKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Interapptive\\ShipWorks");
-            var lastInstanceId = (string) shipWorksKey.GetValue("LastInstalledInstanceID", null);
-
-            if (lastInstanceId != null)
+            try
             {
-                log.Info($"Found previous instance ID: {lastInstanceId}");
+                var shipWorksKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Interapptive\\ShipWorks");
+                var lastInstanceId = (string) shipWorksKey.GetValue("LastInstalledInstanceID", null);
+
+                if (lastInstanceId != null)
+                {
+                    log.Info($"Found previous instance ID: {lastInstanceId}");
+                }
+
+                var instancesKey = shipWorksKey.OpenSubKey("Instances");
+                var previousInstallPaths = instancesKey.GetValueNames();
+
+                var previousPath = previousInstallPaths.FirstOrDefault(x => ((string) instancesKey.GetValue(x, string.Empty)) == lastInstanceId);
+
+                if (!string.IsNullOrWhiteSpace(previousPath))
+                {
+                    log.Info($"Got previous installation path: {previousPath}");
+                    return previousPath;
+                }
             }
-
-            var instancesKey = shipWorksKey.OpenSubKey("Instances");
-            var previousInstallPaths = instancesKey.GetValueNames();
-
-            var previousPath = previousInstallPaths.FirstOrDefault(x => ((string) instancesKey.GetValue(x, string.Empty)) == lastInstanceId);
-
-            if (previousPath != null)
+            catch (Exception)
             {
-                log.Info($"Got previous installation path: {previousPath}");
-                return previousPath;
+                // Do nothing - we'll return the default path
             }
 
             log.Info($"Could not find previous installation path, using default");
