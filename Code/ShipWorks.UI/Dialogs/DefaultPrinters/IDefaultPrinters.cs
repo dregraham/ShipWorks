4a91d0993f93e83b2b4a8074a8fa39c1712d14b4
Chris Hicks <c.hicks@shipworks.com>
2020-11-10 08:52:39 -0700

    WORKS-153: Add comment

--- Code/ShipWorks.UI/Dialogs/DefaultPrinters/IDefaultPrinters.cs
+++ Code/ShipWorks.UI/Dialogs/DefaultPrinters/IDefaultPrinters.cs
@@ -3,6 +3,9 @@ using Interapptive.Shared.UI;
 
 namespace ShipWorks.UI.Dialogs.DefaultPrinters
 {
+    /// <summary>
+    /// Interface used to resolve the DefaultPrinters dialog
+    /// </summary>
     [Service]
     public interface IDefaultPrinters : IDialog
     {
