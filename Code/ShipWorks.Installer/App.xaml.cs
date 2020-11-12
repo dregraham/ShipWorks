fab3a27908c4e0ac9ea035d634fe778ffae90832
Cody Hart <c.hart@shipworks.com>
2020-11-12 14:12:14 -0600

    WORKS-5: Add handling for uncaught exceptions

--- Code/ShipWorks.Installer/App.xaml.cs
+++ Code/ShipWorks.Installer/App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Data.Common;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 using System.Windows;
 using GalaSoft.MvvmLight;
 using log4net;
@@ -22,9 +24,11 @@ namespace ShipWorks.Installer
     public partial class App : Application
     {
         private readonly IHost host;
-
         public static IServiceProvider ServiceProvider { get; private set; }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public App()
         {
             host = Host.CreateDefaultBuilder()  // Use default settings
@@ -46,6 +50,9 @@ namespace ShipWorks.Installer
             ServiceProvider = host.Services;
         }
 
+        /// <summary>
+        /// Configure all of our services
+        /// </summary>
         private void ConfigureServices(IConfiguration configuration, IServiceCollection services)
         {
             services.AddScoped<ISqlSession, SqlSession>();
@@ -85,6 +92,9 @@ namespace ShipWorks.Installer
             services.AddSingleton<MainWindow>();
         }
 
+        /// <summary>
+        /// Override for the base App.OnStartup event handler
+        /// </summary>
         protected override async void OnStartup(StartupEventArgs e)
         {
             Logger.Setup();
@@ -92,10 +102,13 @@ namespace ShipWorks.Installer
 
             var window = ServiceProvider.GetRequiredService<MainWindow>();
             window.Show();
-
+            SetupUnhandledExceptionHandling();
             base.OnStartup(e);
         }
 
+        /// <summary>
+        /// Override for the base App.OnExit event handler
+        /// </summary>
         protected override async void OnExit(ExitEventArgs e)
         {
             using (host)
@@ -104,6 +117,55 @@ namespace ShipWorks.Installer
             }
 
             base.OnExit(e);
+        }
+
+        /// <summary>
+        /// Setup handlers for any exceptions that are not caught
+        /// </summary>
+        private void SetupUnhandledExceptionHandling()
+        {
+            // Catch exceptions from all threads in the AppDomain.
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+                ShowUnhandledException(args.ExceptionObject as Exception);
+
+
+            // Catch exceptions from each AppDomain that uses a task scheduler for async operations.
+            TaskScheduler.UnobservedTaskException += (sender, args) =>
+            {
+                // If we are debugging, let Visual Studio handle the exception and take us to the code that threw it.
+                if (!Debugger.IsAttached)
+                {
+                    args.SetObserved();
+                    ShowUnhandledException(args.Exception);
+                }
+            };
+
+
+            // Catch exceptions from a single specific UI dispatcher thread.
+            Dispatcher.UnhandledException += (sender, args) =>
+            {
+                // If we are debugging, let Visual Studio handle the exception and take us to the code that threw it.
+                if (!Debugger.IsAttached)
+                {
+                    args.Handled = true;
+                    ShowUnhandledException(args.Exception);
+                }
+            };
+        }
+
+        /// <summary>
+        /// In the event of an unhandled exception we take users to the warnings page
+        /// and log out the exception for support
+        /// </summary>
+        void ShowUnhandledException(Exception e)
+        {
+            var mainViewModel = ServiceProvider.GetRequiredService<MainViewModel>();
+            mainViewModel.InstallSettings.Error = InstallError.Unknown;
+            var navService = ServiceProvider.GetRequiredService<INavigationService<NavigationPageType>>();
+            var logFactory = ServiceProvider.GetRequiredService<Func<Type, ILog>>();
+
+            logFactory(typeof(App)).Error("An uncaught exception occured:", e);
+            navService.NavigateTo(NavigationPageType.Warning);
         }
     }
 }
