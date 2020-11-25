1fb8f53f9130ae44f3c40e9b274945e82623458a
Chris Hicks <c.hicks@shipworks.com>
2020-11-25 15:39:30 -0700

    WORKS-422: Implement HubCustomerConfigurator. Update HubConfigurationImporter to use HubConfigurator.

---
+++ Code/ShipWorks/Warehouse/Configuration/Customer/HubCustomerConfigurator.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Threading.Tasks;
+using Interapptive.Shared.ComponentRegistration;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Settings;
+using ShipWorks.Warehouse.Configuration.DTO;
+
+namespace ShipWorks.Warehouse.Configuration.Customer
+{
+    /// <summary>
+    /// Configures customer information downloaded from the Hub
+    /// </summary>
+    [Component]
+    public class HubCustomerConfigurator : IHubCustomerConfigurator
+    {
+        private readonly IShippingSettings shippingSettings;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public HubCustomerConfigurator(IShippingSettings shippingSettings)
+        {
+            this.shippingSettings = shippingSettings;
+        }
+
+        /// <summary>
+        /// Configure customer
+        /// </summary>
+        public async Task Configure(HubConfiguration hubConfiguration)
+        {
+            ShippingSettingsEntity shippingSettingsEntity = shippingSettings.Fetch();
+            bool hasShipEngineApiKey = !string.IsNullOrWhiteSpace(shippingSettingsEntity.ShipEngineApiKey);
+            bool hasShipEngineAccountID = !string.IsNullOrWhiteSpace(shippingSettingsEntity.ShipEngineAccountID);
+
+            if (!hasShipEngineAccountID)
+            {
+                if (!hasShipEngineApiKey)
+                {
+                    // If the user doesn't have either ShipEngine field, that means a SE account has not been created for this
+                    // database, so import the account from the hub.
+                    shippingSettingsEntity.ShipEngineAccountID = hubConfiguration.ShipEngineAccountID;
+                    shippingSettingsEntity.ShipEngineApiKey = hubConfiguration.ShipEngineApiKey;
+                }
+                else
+                {
+                    // If the user has a ShipEngine API key, but not an account ID, that means they have a ShipEngine account that
+                    // was created for this specific database. We don't want to overwrite this with the account from the hub,
+                    // so just return.
+                    return;
+                }
+            }
+            else
+            {
+                // If the ShipEngine Account ID from the hub matches the one in the database, but the keys do not match
+                // import the new key from the hub. This allows us to change a customers SE API key on the hub
+                // and have it propagate down to each database
+                if (string.Equals(shippingSettingsEntity.ShipEngineAccountID,
+                                  hubConfiguration.ShipEngineAccountID,
+                                  StringComparison.InvariantCultureIgnoreCase) &&
+                    !string.Equals(shippingSettingsEntity.ShipEngineApiKey,
+                                  hubConfiguration.ShipEngineApiKey,
+                                  StringComparison.InvariantCultureIgnoreCase))
+                {
+                    shippingSettingsEntity.ShipEngineApiKey = hubConfiguration.ShipEngineApiKey;
+                }
+            }
+
+            shippingSettings.Save(shippingSettingsEntity);
+        }
+    }
+}
--- Code/ShipWorks/Warehouse/Configuration/HubConfigurationImporter.cs
+++ Code/ShipWorks/Warehouse/Configuration/HubConfigurationImporter.cs
@@ -19,7 +19,7 @@ namespace ShipWorks.Warehouse.Configuration
     public class HubConfigurationImporter : IInitializeForCurrentSession
     {
         private readonly ILicenseService licenseService;
-        private readonly IHubCarrierConfigurator carrierConfigurator;
+        private readonly IHubConfigurator configurator;
         private readonly IHubConfigurationWebClient webClient;
         private readonly IConfigurationData configurationData;
         private readonly ILog log;
@@ -28,13 +28,13 @@ namespace ShipWorks.Warehouse.Configuration
         /// Constructor
         /// </summary>
         public HubConfigurationImporter(ILicenseService licenseService,
-            IHubCarrierConfigurator carrierConfigurator,
+            IHubConfigurator configurator,
             IHubConfigurationWebClient webClient,
             IConfigurationData configurationData,
              Func<Type, ILog> logFactory)
         {
             this.licenseService = licenseService;
-            this.carrierConfigurator = carrierConfigurator;
+            this.configurator = configurator;
             this.webClient = webClient;
             this.configurationData = configurationData;
             log = logFactory(typeof(HubConfigurationImporter));
@@ -55,7 +55,7 @@ namespace ShipWorks.Warehouse.Configuration
                         try
                         {
                             var hubConfig = await webClient.GetConfig(configuration.WarehouseID).ConfigureAwait(false);
-                            await carrierConfigurator.Configure(hubConfig.CarrierConfigurations);
+                            await configurator.Configure(hubConfig).ConfigureAwait(false);
                         }
                         catch (AggregateException ex) when (ex.InnerExceptions.FirstOrDefault() is WebException)
                         {
