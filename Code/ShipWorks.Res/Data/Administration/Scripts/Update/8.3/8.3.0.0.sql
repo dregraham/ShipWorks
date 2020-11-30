96b54773fd6759c85fab239fbea77265b361f3cf
Chris Hicks <c.hicks@shipworks.com>
2020-11-30 09:34:49 -0700

    WORKS-422: Make script re-runnable

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/8.3/8.3.0.0.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/8.3/8.3.0.0.sql
@@ -1,9 +1,11 @@
 PRINT N'Altering [dbo].[ShippingSettings]'
 GO
+IF COL_LENGTH(N'[dbo].[ShippingSettings]', N'ShipEngineAccountID') IS NULL
 ALTER TABLE [dbo].[ShippingSettings] ADD
     [ShipEngineAccountID] [nvarchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_ShippingSettings_ShipEngineAccountID] DEFAULT ('')
 GO
 PRINT N'Dropping constraints from [dbo].[ShippingSettings]'
 GO
+IF EXISTS (SELECT 1 FROM sys.columns WHERE name = N'ShipEngineAccountID' AND object_id = OBJECT_ID(N'[dbo].[ShippingSettings]', 'U') AND default_object_id = OBJECT_ID(N'[dbo].[DF_ShippingSettings_ShipEngineAccountID]', 'D'))
 ALTER TABLE [dbo].[ShippingSettings] DROP CONSTRAINT [DF_ShippingSettings_ShipEngineAccountID]
 GO
