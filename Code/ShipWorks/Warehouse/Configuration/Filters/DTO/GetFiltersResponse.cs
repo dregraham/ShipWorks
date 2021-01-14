ae350648ba0ebcae760d1aaa3273dc625848471a
John Ackerman <j.ackerman@shipworks.com>
2021-01-14 15:32:36 -0600

    WORKS-396: WIP: Import filters

--- Code/ShipWorks/Warehouse/Configuration/Filters/DTO/GetFiltersResponse.cs
+++ Code/ShipWorks/Warehouse/Configuration/Filters/DTO/GetFiltersResponse.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.IO;
+using System.Xml;
 using Newtonsoft.Json;
-using ShipWorks.Filters.Content;
+using Newtonsoft.Json.Linq;
 
 namespace ShipWorks.Warehouse.Configuration.Filters.DTO
 {
@@ -13,7 +15,34 @@ namespace ShipWorks.Warehouse.Configuration.Filters.DTO
         /// The ID for this filter in the Hub
         /// </summary>
         [JsonProperty("SK")]
-        public string HubFilterID { get; set; }
+        public string SK { get; set; }
+
+        /// <summary>
+        /// The Hub ID as a GUID
+        /// </summary>
+        public Guid HubFilterID
+        {
+            get => Guid.Parse(SK);
+        }
+
+        /// <summary>
+        /// The HubFilterID of this filter's parent
+        /// </summary>
+        [JsonProperty("ParentFilterId")]
+        public string ParentFilterString { get; set; }
+
+        /// <summary>
+        /// The parent ID as a GUID
+        /// </summary>
+        public Guid ParentFilterID
+        {
+            get
+            {
+                Guid result;
+                Guid.TryParse(ParentFilterString, out result);
+                return result;
+            }
+        }
 
         /// <summary>
         /// The name of the filter
@@ -22,15 +51,86 @@ namespace ShipWorks.Warehouse.Configuration.Filters.DTO
         public string Name { get; set; }
 
         /// <summary>
+        /// The path of this filter in the filter tree
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
         /// The filter's definition
         /// </summary>
         [JsonProperty("definition")]
-        public FilterDefinition Definition { get; set; }
+        public JToken DefinitionJson { get; set; }
 
-        /// <summary>
-        /// This filter's children
-        /// </summary>
-        [JsonProperty("Children")]
-        public List<GetFiltersResponse> Children { get; set; }
+        [JsonIgnore]
+        public string DefinitionXML
+        {
+            get
+            {
+                using (var stringWriter = new StringWriter())
+                {
+                    using (XmlWriter xmlWriter = XmlWriter.Create(stringWriter))
+                    {
+                        xmlWriter.WriteStartElement("FilterDefinition");
+                        xmlWriter.WriteAttributeString("Target", "0");
+
+                        WriteGroupContainer(xmlWriter, DefinitionJson["rootContainer"]);
+
+                        xmlWriter.WriteEndElement();
+                    }
+                    return stringWriter.ToString();
+                }
+
+            }
+
+            set
+            {
+
+            }
+        }
+
+        public void WriteGroupContainer(XmlWriter writer, JToken groupContainer)
+        {
+            writer.WriteStartElement("JoinType");
+            writer.WriteAttributeString("value", groupContainer["joinType"].Value<string>());
+            writer.WriteEndElement();
+
+            writer.WriteStartElement("FirstGroup");
+            writer.WriteAttributeString("identifier", "Group");
+            {
+                writer.WriteStartElement("JoinType");
+                writer.WriteAttributeString("value", groupContainer["firstGroup"]["joinType"].Value<string>());
+                writer.WriteEndElement();
+
+                writer.WriteStartElement("Conditions");
+                {
+                    foreach (var condition in groupContainer["firstGroup"]["conditions"].Children<JObject>())
+                    {
+                        writer.WriteStartElement("Item");
+                        writer.WriteAttributeString("identifier", condition["identifier"].ToString().Replace(".Person.", "."));
+
+                        foreach (var key in condition.Properties())
+                        {
+                            if (!(key.Name.Equals("path", StringComparison.OrdinalIgnoreCase) || key.Name.Equals("identifier", StringComparison.OrdinalIgnoreCase)))
+                            {
+                                writer.WriteStartElement(char.ToUpper(key.Name[0]) + key.Name.Substring(1));
+                                writer.WriteAttributeString("value", key.Value.ToString());
+                                writer.WriteEndElement();
+                            }
+                        }
+
+                        writer.WriteEndElement();
+                    }
+                }
+                writer.WriteEndElement();
+            }
+            writer.WriteEndElement();
+
+            writer.WriteStartElement("SecondGroup");
+            if (groupContainer["secondGroup"]?.HasValues ?? false)
+            {
+                WriteGroupContainer(writer, groupContainer["secondGroup"]);
+            }
+            writer.WriteEndElement();
+        }
     }
 }
--- Code/ShipWorks/Warehouse/Configuration/Filters/HubFilterConfigurator.cs
+++ Code/ShipWorks/Warehouse/Configuration/Filters/HubFilterConfigurator.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Interapptive.Shared.ComponentRegistration;
 using Interapptive.Shared.Threading;
+using ShipWorks.Data.Model.EntityClasses;
 using ShipWorks.Filters;
+using ShipWorks.Filters.Content;
 using ShipWorks.Warehouse.Configuration.Filters.DTO;
 
 namespace ShipWorks.Warehouse.Configuration.Filters
@@ -9,9 +13,12 @@ namespace ShipWorks.Warehouse.Configuration.Filters
     /// <summary>
     /// Class to configure filters downloaded from the Hub
     /// </summary>
+    [Component]
     public class HubFilterConfigurator : IHubFilterConfigurator
     {
         private readonly IFilterHelper filterHelper;
+        private int progressIndex = 1;
+        private int totalFiltersToImport;
 
         /// <summary>
         /// Constructor
@@ -26,17 +33,83 @@ namespace ShipWorks.Warehouse.Configuration.Filters
         /// </summary>
         public void Configure(IEnumerable<GetFiltersResponse> filtersResponse, IProgressReporter filterProgress)
         {
-            foreach (var response in filtersResponse)
-            {
-                var newFilter = filterHelper.CreateFilterEntity(response.Name, response.Definition);
+            totalFiltersToImport = filtersResponse.Count();
 
-                if (response.Children.Any())
+            try
+            {
+                FilterLayoutContext.PushScope();
+
+                var ordersNode = FilterLayoutContext.Current.FindNode(BuiltinFilter.GetTopLevelKey(FilterTarget.Orders));
+                var newFilters = new List<(FilterEntity filter, Guid parentID)>();
+
+                foreach (var response in filtersResponse)
                 {
-                    newFilter.IsFolder = true;
+                    if (response.Path.Equals("root"))
+                    {
+                        ordersNode.Filter.HubFilterID = response.HubFilterID.ToString("N");
+                    }
+                    else
+                    {
+                        var definition = new FilterDefinition(response.DefinitionXML);
+
+                        var newFilter = filterHelper.CreateFilterEntity(response.Name, definition);
+                        newFilter.HubFilterID = response.HubFilterID.ToString("N");
+
+                        if (filtersResponse.Any(x => x.ParentFilterID == response.HubFilterID))
+                        {
+                            newFilter.IsFolder = true;
+                        }
+
+                        newFilters.Add((newFilter, response.ParentFilterID));
+                    }
                 }
 
-                // FilterLayoutContext.Current.AddFilter(newFilter);
+                var parentNodes = new List<FilterNodeEntity> { ordersNode };
+
+                do
+                {
+                    AddFilters(newFilters, ref parentNodes, filterProgress);
+                }
+                while (parentNodes.Any());
             }
+            catch (Exception ex)
+            {
+                /// wat
+            }
+            finally
+            {
+                FilterLayoutContext.PopScope();
+            }
+
+            filterProgress.Detail = "Done";
+            filterProgress.Completed();
+        }
+
+        /// <summary>
+        /// Add the filters to the database
+        /// </summary>
+        private void AddFilters(List<(FilterEntity filter, Guid parentID)> newFilters, ref List<FilterNodeEntity> parentNodes, IProgressReporter filterProgress)
+        {
+            var newParents = new List<FilterNodeEntity>();
+
+            foreach (var parent in parentNodes)
+            {
+                var parentID = Guid.Parse(parent.Filter.HubFilterID);
+
+                var children = newFilters.Where(x => x.parentID == parentID);
+
+                int childIndex = 0;
+
+                foreach (var child in children)
+                {
+                    filterProgress.Detail = $"Importing '{child.filter.Name}' ({progressIndex} of {totalFiltersToImport})";
+                    newParents.Add(FilterLayoutContext.Current.AddFilter(child.filter, parent, childIndex)[0]);
+                    childIndex++;
+                    progressIndex++;
+                }
+            }
+
+            parentNodes = newParents;
         }
     }
 }
