3222ea32086295988e5449ffd405915db246397a
Kevin Croke <k.croke@shipworks.com>
2021-04-16 11:34:55 -0500

    WORKS-1482: Added retry logic to cubiscan scale. Other minor code review changes

--- Code/Interapptive.Shared/IO/Hardware/Scales/ScaleCubiscanReader.cs
+++ Code/Interapptive.Shared/IO/Hardware/Scales/ScaleCubiscanReader.cs
@@ -13,6 +13,7 @@ namespace Interapptive.Shared.IO.Hardware.Scales
     public class ScaleCubiscanReader
     {
         private ILog log;
+        private static readonly string measureRequestString = $"{Convert.ToChar(2)}M{Convert.ToChar(3)}{Convert.ToChar(13)}{Convert.ToChar(10)}";
 
         /// <summary>
         /// Constructor
@@ -20,21 +21,21 @@ namespace Interapptive.Shared.IO.Hardware.Scales
         public ScaleCubiscanReader()
         {
             log = LogManager.GetLogger(typeof(ScaleCubiscanReader));
-        }  
+        }
 
         /// <summary>
         /// Requests dimensions from a Cubiscan and returns the result
         /// </summary>
         public ScaleReadResult ReadScale()
         {
-            var result = GetScaleReadResult().Result;
-            if (result.Success)
+            try
             {
+                var result = Functional.RetryAsync(GetScaleReadResult, 3, _ => true).Result;
                 return ReadCubiScan(result.Value);
             }
-            else
+            catch(Exception ex)
             {
-                log.Warn($"Unable to read weight from cubiscan scale: {result.Exception.Message}", result.Exception);
+                log.Warn($"Unable to read weight from cubiscan scale.", ex);
                 return ScaleReadResult.ReadError("Unable to read weight from cubiscan scale.", ScaleType.Cubiscan);
             }
         }
@@ -49,10 +50,10 @@ namespace Interapptive.Shared.IO.Hardware.Scales
                 return ScaleReadResult.ReadError($"Invalid response from Cubiscan. (Response: {value})", ScaleType.Cubiscan);
             }
 
-            double weight = double.Parse(value.Substring(35,6));
-            double length = double.Parse(value.Substring(12,5));
-            double width = double.Parse(value.Substring(19,5));
-            double height = double.Parse(value.Substring(26,5));
+            double weight = double.Parse(value.Substring(35, 6));
+            double length = double.Parse(value.Substring(12, 5));
+            double width = double.Parse(value.Substring(19, 5));
+            double height = double.Parse(value.Substring(26, 5));
 
             return ScaleReadResult.Success(weight, length, width, height, ScaleType.Cubiscan);
         }
@@ -63,32 +64,24 @@ namespace Interapptive.Shared.IO.Hardware.Scales
         /// <returns>Raw result from cubiscan</returns>
         private async Task<GenericResult<string>> GetScaleReadResult()
         {
-            try
+            using (TcpClient client = new TcpClient())
             {
-                using (TcpClient client = new TcpClient())
+                // todo: get this value from the configuration when we get to that story
+                await client.ConnectAsync("127.0.0.1", 1050).ConfigureAwait(false);
+
+                NetworkStream networkStream = client.GetStream();
+                string response;
+
+                using (StreamWriter writer = new StreamWriter(networkStream))
+                using (StreamReader reader = new StreamReader(networkStream))
                 {
-                    // todo: get this value from the configuration when we get to that story
-                    await client.ConnectAsync("127.0.0.1", 1050);
+                    writer.AutoFlush = true;
 
-                    NetworkStream networkStream = client.GetStream();
-                    string response;
-
-                    using (StreamWriter writer = new StreamWriter(networkStream))
-                    using (StreamReader reader = new StreamReader(networkStream))
-                    {
-                        writer.AutoFlush = true;
-
-                        string measurement = Convert.ToChar(2) + "M" + Convert.ToChar(3) + Convert.ToChar(13) + Convert.ToChar(10);
-                        await writer.WriteLineAsync(measurement);
-                        response = await reader.ReadLineAsync();
-                    }
-                    client.Close();
-                    return GenericResult.FromSuccess<string>(response);
-                }                
-            }
-            catch (Exception ex)
-            {
-                return GenericResult.FromError<string>(ex.Message);
+                    await writer.WriteLineAsync(measureRequestString).ConfigureAwait(false);
+                    response = await reader.ReadLineAsync().ConfigureAwait(false);
+                }
+                client.Close();
+                return GenericResult.FromSuccess<string>(response);
             }
         }
     }
