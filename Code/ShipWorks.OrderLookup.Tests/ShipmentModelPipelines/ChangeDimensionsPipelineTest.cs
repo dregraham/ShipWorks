28bab7828816ee7f020e5ca74896d254b701b281
Kevin Croke <k.croke@shipworks.com>
2021-04-21 17:22:16 -0500

    WORKS-1489: Add pipeline to change dimensions on OrderLookup mode

---
+++ Code/ShipWorks.OrderLookup.Tests/ShipmentModelPipelines/ChangeDimensionsPipelineTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
+using Interapptive.Shared.Messaging;
+using System.Reactive.Subjects;
+using Interapptive.Shared.Threading;
+using ShipWorks.Tests.Shared;
+using Xunit;
+using ShipWorks.Shipping.Services;
+using ShipWorks.OrderLookup;
+using Moq;
+using ShipWorks.Core.Messaging;
+using ShipWorks.Messaging.Messages;
+using Interapptive.Shared.IO.Hardware.Scales;
+using ShipWorks.OrderLookup.ShipmentModelPipelines;
+
+namespace ShipWorks.Shipping.UI.Tests.ShippingPanel.ObservableRegistrations
+{
+    public class ChangeDimensionsPipelineTest
+    {
+        private readonly AutoMock mock;
+        private readonly IMessenger messenger;
+        readonly Mock<IPackageAdapter> package;
+        readonly Mock<IOrderLookupShipmentModel> model;
+        readonly ChangeDimensionsPipeline testObject;
+
+        public ChangeDimensionsPipelineTest()
+        {
+            mock = AutoMockExtensions.GetLooseThatReturnsMocks();
+
+            messenger = new TestMessenger();
+            mock.Provide<IMessenger>(messenger);
+            mock.Provide<ISchedulerProvider>(new ImmediateSchedulerProvider());
+
+            package = mock.CreateMock<IPackageAdapter>();
+            model = mock.CreateMock<IOrderLookupShipmentModel>();
+            model.SetupGet(m => m.PackageAdapters)
+                .Returns(new[] { package.Object });
+
+            testObject = mock.Create<ChangeDimensionsPipeline>();
+        }
+
+        [Fact]
+        public void Register_AppliesDims_FromMessage()
+        {
+            testObject.Register(model.Object);
+            messenger.Send(new ChangeDimensionsMessage(this, ScaleReadResult.Success(1, 2, 3, 4, ScaleType.Cubiscan)));
+            package.VerifySet(p => p.DimsProfileID = 0);
+            package.VerifySet(p => p.DimsLength = 2);
+            package.VerifySet(p => p.DimsWidth = 3);
+            package.VerifySet(p => p.DimsHeight = 4);
+            package.VerifySet(p => p.ApplyAdditionalWeight = false);
+        }
+
+        [Fact]
+        public void Register_DoesNotApplyDims_WhenNoDimensions()
+        {
+            testObject.Register(model.Object);
+            messenger.Send(new ChangeDimensionsMessage(this, ScaleReadResult.Success(4, ScaleType.Cubiscan)));
+            package.VerifySet(p => p.DimsProfileID = It.IsAny<long>(), Times.Never);
+        }
+    }
+}
---
+++ Code/ShipWorks.OrderLookup/ShipmentModelPipelines/ChangeDimensionsPipeline.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using Interapptive.Shared.Threading;
+using ShipWorks.Core.Messaging;
+using ShipWorks.Messaging.Messages;
+using ShipWorks.Shipping.Services;
+
+namespace ShipWorks.OrderLookup.ShipmentModelPipelines
+{
+    /// <summary>
+    /// Pipeline for clearing the search box
+    /// </summary>
+    public class ChangeDimensionsPipeline : IOrderLookupShipmentModelPipeline
+    {
+        private readonly IMessenger messenger;
+        private readonly ISchedulerProvider schedulerProvider;
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ChangeDimensionsPipeline(
+            IMessenger messenger,
+            ISchedulerProvider schedulerProvider)
+        {
+            this.schedulerProvider = schedulerProvider;
+            this.messenger = messenger;
+        }
+
+        /// <summary>
+        /// Register the pipeline
+        /// </summary>
+        public IDisposable Register(IOrderLookupShipmentModel model) =>
+            messenger.OfType<ChangeDimensionsMessage>()
+                .ObserveOn(schedulerProvider.WindowsFormsEventLoop)
+                .Do(message => HandleChangeDimensionsMessage(model, message))
+                .Subscribe();
+
+        private void HandleChangeDimensionsMessage(IOrderLookupShipmentModel model, ChangeDimensionsMessage message)
+        {
+            if (message.ScaleReadResult.HasVolumeDimensions)
+            {
+                IPackageAdapter packageAdapter = model.PackageAdapters.First();
+
+                packageAdapter.DimsProfileID = 0;
+                packageAdapter.DimsLength = message.ScaleReadResult.Length;
+                packageAdapter.DimsWidth = message.ScaleReadResult.Width;
+                packageAdapter.DimsHeight = message.ScaleReadResult.Height;
+                packageAdapter.ApplyAdditionalWeight = false;
+            }
+        }
+    }
+}
