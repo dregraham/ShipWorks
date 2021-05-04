b1bd96fa313b7fbefc06d4e2d576d215c055918a
Kevin Croke <k.croke@shipworks.com>
2021-05-04 17:38:12 -0500

    WORKS-1490: Fix IP address validation

---
+++ Code/Interapptive.Shared.Tests/Net/HttpValidatorTest.cs
@@ -0,0 +1,38 @@
+﻿using Interapptive.Shared.Net;
+using Xunit;
+
+namespace Interapptive.Shared.Tests.Net
+{
+    public class HttpValidatorTest
+    {
+        readonly HttpValidator testObject;
+        
+        public HttpValidatorTest()
+        {
+            testObject = new HttpValidator();
+        }
+        
+        [Theory]
+        [InlineData("127.0.0.1", true)]
+        [InlineData("192.168.0.1", true)]
+        [InlineData("1.1.1.1", true)]
+        [InlineData("255.255.255.255", true)]
+        [InlineData(" 1.1.1.1 ", true)]
+        [InlineData("300.300.300.300", false)]
+        [InlineData("127.1.1", false)]
+        [InlineData("a.a.a.a", false)]
+        [InlineData("12345", false)]
+        [InlineData("1.1.1.1.1", false)]
+        [InlineData("null", false)]
+        [InlineData("", false)]
+        public void ValidateIpAddress(string ipAddress, bool expectedResult)
+        {
+            var result = testObject.ValidateIPAddress(ipAddress);
+            Assert.Equal(expectedResult, result.Success);
+            if (expectedResult)
+            {
+                Assert.Equal(result.Value, ipAddress.Trim());
+            }
+        }
+    }
+}
