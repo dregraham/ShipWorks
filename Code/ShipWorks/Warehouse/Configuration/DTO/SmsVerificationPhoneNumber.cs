2a47bcee8caa0fd2c90d02f0ab0c095896b38ec7
Kevin Croke <k.croke@shipworks.com>
2021-06-02 17:20:14 -0500

    WORKS-1724: Fix obfuscation issue

--- Code/ShipWorks/Warehouse/Configuration/DTO/SmsVerificationPhoneNumber.cs
+++ Code/ShipWorks/Warehouse/Configuration/DTO/SmsVerificationPhoneNumber.cs
@@ -1,7 +1,10 @@
-﻿namespace ShipWorks.Warehouse.Configuration.DTO
+﻿using Newtonsoft.Json;
+
+namespace ShipWorks.Warehouse.Configuration.DTO
 {
     public class SmsVerificationPhoneNumber
     {
+        [JsonProperty("smsVerifiedPhoneNumber")]
         public string SmsVerifiedPhoneNumber { get; set; }
     }
 }
