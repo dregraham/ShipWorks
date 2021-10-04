17e9a8610e211b57e2aa59f0d77001a76641cf05
danjjostauctane <daniel.jost@auctane.com>
2021-10-04 15:00:51 -0500

    WORKS-2193: Update StoreConfiguration DTOs

--- Code/ShipWorks/Warehouse/Configuration/Stores/DTO/StoreConfiguration.cs
+++ Code/ShipWorks/Warehouse/Configuration/Stores/DTO/StoreConfiguration.cs
@@ -40,6 +40,39 @@ namespace ShipWorks.Warehouse.Configuration.Stores.DTO
         /// The JSON serialized actions list
         /// </summary>
         public string ActionsPayload { get; set; }
+
+        /// <summary>
+        /// True if this store should be managed by Hub
+        /// </summary>
+        public bool ManagedInHub { get; set; }
+
+        /// <summary>
+        /// Store's address
+        /// </summary>
+        public Address Address { get; set; }
+
+        /// <summary>
+        /// True if this store should upload shipment details
+        /// </summary>
+        public bool UploadShipmentDetails { get; set; }
+    }
+
+    /// <summary>
+    /// Store configuration's address
+    /// </summary>
+    [Obfuscation]
+    public class Address 
+    {
+        public string City { get; set; }
+        public string StateProvCode { get; set; }
+        public string CountryCode { get; set; }
+        public string PostalCode { get; set; }
+        public string Company { get; set; }
+        public string Street1 { get; set; }
+        public string Street2 { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
     }
 
     /// <summary>
