c7fa77a0a3332525aa7c87e1421b66c3537e5a8a
Ezra Ellette <ezra.ellette@auctane.com>
2021-10-05 09:47:31 -0500

    WORKS-2683: add new Columns

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.3/9.3.2.0.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.3/9.3.2.0.sql
@@ -1,2 +1,11 @@
-﻿-- Forcing schema update
+﻿PRINT N'Altering [dbo].[UspsShipment]'
+GO
+IF COL_LENGTH(N'[dbo].[UspsShipment]', N'CustomsRecipientTIN') IS NULL
+ALTER TABLE [dbo].[UspsShipment] ADD [CustomsRecipientTIN] [nvarchar] (25) NULL
+GO
+
+PRINT N'Altering [dbo].[UspsProfile]'
+GO
+IF COL_LENGTH(N'[dbo].[UspsProfile]', N'CustomsRecipientTIN') IS NULL
+ALTER TABLE [dbo].[UspsProfile] ADD [CustomsRecipientTIN] [nvarchar] (25) NULL
 GO
