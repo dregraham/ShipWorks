340a14c2e9bde6e32ee5d59d5cfc1fbbc0c41de4
Maria Tripoli <meatripoli@gmail.com>
2021-10-13 16:42:28 -0500

    Works 2682: fixed a bug described in WORKS-2805

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.3/9.3.4.0.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.3/9.3.4.0.sql
@@ -5,10 +5,10 @@ IF COL_LENGTH(N'[dbo].[UpsShipment]', N'CustomsRecipientTIN') IS NULL
 ALTER TABLE [dbo].[UpsShipment] ADD [CustomsRecipientTIN] [nvarchar] (24) NULL
 GO
 IF COL_LENGTH(N'[dbo].[UpsShipment]', N'CustomsRecipientTINType') IS NULL
-ALTER TABLE [dbo].[UpsShipment] ADD [CustomsRecipientTINType] [int] NULL CONSTRAINT [DF_UserSettings_SingleScanConfirmationMode] DEFAULT ((0))
+ALTER TABLE [dbo].[UpsShipment] ADD [CustomsRecipientTINType] [int] NULL 
 GO
 IF COL_LENGTH(N'[dbo].[UpsShipment]', N'CustomsRecipientType') IS NULL
-ALTER TABLE [dbo].[UpsShipment] ADD [CustomsRecipientType] [int] NULL CONSTRAINT [DF_UserSettings_SingleScanConfirmationMode] DEFAULT ((0))
+ALTER TABLE [dbo].[UpsShipment] ADD [CustomsRecipientType] [int] NULL 
 GO
 
 PRINT N'ALTERING [dbo].[UpsProfile]'
@@ -22,3 +22,9 @@ GO
 IF COL_LENGTH(N'[dbo].[UpsProfile]', N'CustomsRecipientType') IS NULL
 ALTER TABLE [dbo].[UpsProfile] ADD [CustomsRecipientType] [int] NULL
 GO
+UPDATE ups
+SET ups.[CustomsRecipientTIN] = '', ups.[CustomsRecipientTINType] = 0, ups.[CustomsRecipientType] = 0
+from UpsProfile ups
+INNER JOIN ShippingProfile ship
+    on ups.ShippingProfileID = ship.ShippingProfileID
+where ship.ShipmentTypePrimary=1
