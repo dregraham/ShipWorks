bd475bc925f94b54f923ff28e836c737816384e6
Maria Tripoli <meatripoli@gmail.com>
2021-10-14 10:54:26 -0500

    Works 2805: updates based on code review

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.3/9.3.3.0.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.3/9.3.3.0.sql
@@ -4,6 +4,12 @@ IF COL_LENGTH(N'[dbo].[FedexShipment]', N'CustomsRecipientTINType') IS NULL
 ALTER TABLE [dbo].[FedexShipment] ADD [CustomsRecipientTINType] [int] NULL
 GO
 
+UPDATE fedexShipment
+SET fedexShipment.[CustomsRecipientTINType] = 0 
+FROM FedexShipment fedexShipment
+WHERE fedexShipment.[CustomsRecipientTINType] IS NULL
+GO
+
 PRINT N'Altering [dbo].[FedexProfile]'
 GO
 IF COL_LENGTH(N'[dbo].[FedexProfile]', N'CustomsRecipientTIN') IS NULL
@@ -12,10 +18,11 @@ GO
 IF COL_LENGTH(N'[dbo].[FedexProfile]', N'CustomsRecipientTINType') IS NULL
 ALTER TABLE [dbo].[FedexProfile] ADD [CustomsRecipientTINType] [int] NULL
 GO
-UPDATE fedex
-SET fedex.[CustomsRecipientTINType] = 0 , fedex.[CustomsRecipientTIN] = ''
-FROM FedexProfile fedex
-INNER JOIN ShippingProfile ship
-    ON fedex.ShippingProfileID = ship.ShippingProfileID
-WHERE ship.ShipmentTypePrimary = 1
+
+UPDATE fedexProfile
+SET fedexProfile.[CustomsRecipientTINType] = 0 , fedexProfile.[CustomsRecipientTIN] = ''
+FROM FedexProfile fedexProfile
+INNER JOIN ShippingProfile shipProfile
+    ON fedexProfile.ShippingProfileID = shipProfile.ShippingProfileID
+WHERE shipProfile.ShipmentTypePrimary = 1 AND fedexProfile.[CustomsRecipientTINType] IS NULL AND fedexProfile.[CustomsRecipientTIN] IS NULL
 GO
