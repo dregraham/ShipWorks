08fecba1b6461ed33d8919d8840cad0e7929980c
Kevin Croke <k.croke@shipworks.com>
2021-10-21 17:44:51 -0500

    Fix update script syntax

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.3/9.3.3.1.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.3/9.3.3.1.sql
@@ -1,6 +1,6 @@
 ﻿PRINT N'Altering [dbo].[Store]'
 GO
-IF COL_LENGTH(N' [dbo].[Store]', N'ManagedInHub') IS NULL
+IF COL_LENGTH(N'[dbo].[Store]', N'ManagedInHub') IS NULL
 ALTER TABLE [dbo].[Store] ADD
 	[ManagedInHub] [bit] NOT NULL CONSTRAINT [DF_Store_ManagedInHub] DEFAULT (0)
 GO
