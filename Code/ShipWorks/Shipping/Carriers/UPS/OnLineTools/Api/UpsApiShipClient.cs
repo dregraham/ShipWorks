b418c9a3b787d921d26dffed93f693a3e653c78f
Maria Tripoli <meatripoli@gmail.com>
2021-10-21 16:14:39 -0500

    Works 2682: code review SW-CR-161 changes

--- Code/ShipWorks/Shipping/Carriers/UPS/OnLineTools/Api/UpsApiShipClient.cs
+++ Code/ShipWorks/Shipping/Carriers/UPS/OnLineTools/Api/UpsApiShipClient.cs
@@ -242,9 +242,9 @@ namespace ShipWorks.Shipping.Carriers.UPS.OnLineTools.Api
             if (!ShipmentTypeManager.GetType(shipment).IsDomestic(shipment) && !string.IsNullOrWhiteSpace(ups.CustomsRecipientTIN))
             {
                 xmlWriter.WriteStartElement("VendorInfo");
-                xmlWriter.WriteElementString("VendorCollectIDTypeCode", EnumHelper.GetApiValue((UpsCustomsRecipientTINType) ups.CustomsRecipientTINType);
+                xmlWriter.WriteElementString("VendorCollectIDTypeCode", EnumHelper.GetApiValue((UpsCustomsRecipientTINType) ups.CustomsRecipientTINType));
                 xmlWriter.WriteElementString("VendorCollectIDNumber", StringUtility.Truncate(ups.CustomsRecipientTIN, 24));
-                xmlWriter.WriteElementString("ConsigneeType", EnumHelper.GetApiValue((UpsCustomsRecipientType) ups.CustomsRecipientType);
+                xmlWriter.WriteElementString("ConsigneeType", EnumHelper.GetApiValue((UpsCustomsRecipientType) ups.CustomsRecipientType));
                 xmlWriter.WriteEndElement();
             }
 
