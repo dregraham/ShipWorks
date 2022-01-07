a51032ae2885de55007465d39a3ab3c9768f0262
Tim <t.hughes@shipworks.com>
2022-01-07 16:02:22 -0600

    Added missing columns

---
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.5/9.5.0.1.sql
@@ -0,0 +1,22 @@
+﻿PRINT N'ALTERING [dbo].[DhlExpressProfile]'
+GO
+IF COL_LENGTH(N'[dbo].[DhlExpressProfile]', N'CustomsRecipientTin') IS NULL
+	ALTER TABLE [dbo].[DhlExpressProfile] ADD [CustomsRecipientTin] [nvarchar] (25) NULL
+GO
+IF COL_LENGTH(N'[dbo].[DhlExpressProfile]', N'CustomsTaxIdType') IS NULL
+	ALTER TABLE [dbo].[DhlExpressProfile] ADD [CustomsTaxIdType] [int] NULL 
+GO
+IF COL_LENGTH(N'[dbo].[DhlExpressProfile]', N'CustomsTinIssuingAuthority') IS NULL
+	ALTER TABLE [dbo].[DhlExpressProfile] ADD [CustomsTinIssuingAuthority] NVARCHAR (2) NULL 
+GO
+PRINT N'ALTERING [dbo].[DhlExpressShipment]'
+GO
+IF COL_LENGTH(N'[dbo].[DhlExpressShipment]', N'CustomsRecipientTin') IS NULL
+	ALTER TABLE [dbo].[DhlExpressShipment] ADD [CustomsRecipientTin] [nvarchar] (25) NULL
+GO
+IF COL_LENGTH(N'[dbo].[DhlExpressShipment]', N'CustomsTaxIdType') IS NULL
+	ALTER TABLE [dbo].[DhlExpressShipment] ADD [CustomsTaxIdType] [int] NULL 
+GO
+IF COL_LENGTH(N'[dbo].[DhlExpressShipment]', N'CustomsTinIssuingAuthority') IS NULL
+	ALTER TABLE [dbo].[DhlExpressShipment] ADD [CustomsTinIssuingAuthority] NVARCHAR (2) NULL 
+GO
