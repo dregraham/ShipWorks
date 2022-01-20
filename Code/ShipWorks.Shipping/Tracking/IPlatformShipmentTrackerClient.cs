58f8f2954206b7e763a304b83c37e2ebd963671a
Kevin Croke <k.croke@shipworks.com>
2022-01-20 12:07:43 -0600

    WORKS-3147: Add methods to IPlatformShipmentTrackerClient

--- Code/ShipWorks.Shipping/Tracking/IPlatformShipmentTrackerClient.cs
+++ Code/ShipWorks.Shipping/Tracking/IPlatformShipmentTrackerClient.cs
@@ -1,7 +1,22 @@
-﻿namespace ShipWorks.Shipping.Tracking
+﻿using System;
+using System.Collections.Generic;
+using ShipWorks.Stores.Warehouse;
+
+namespace ShipWorks.Shipping.Tracking
 {
+    /// <summary>
+    /// Send and get tracking number information
+    /// </summary>
     public interface IPlatformShipmentTrackerClient
     {
-        
+        /// <summary>
+        /// Send Shipment information to the hub
+        /// </summary>
+        void SendShipment(string trackingNumber, string carrierCode, string warehouseID);
+
+        /// <summary>
+        /// Get tracking information from the hub
+        /// </summary>
+        IEnumerable<ShipmentNotification> GetShipments(string WarehouseID, DateTime lastUpdateDate);
     }
 }
