f4567c55020db28b97096e9e859321acd56c7f54
John Ackerman <j.ackerman@shipworks.com>
2022-04-22 15:42:03 -0500

    WORKS-3521: Add DHL eCommerce Settings Page

---
+++ Code/ShipWorks.Shipping.UI/Carriers/DhlEcommerce/DhlEcommerceAccountEditorDlg.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Forms;
+using Autofac.Features.Indexed;
+using Interapptive.Shared.ComponentRegistration;
+using Interapptive.Shared.UI;
+using Interapptive.Shared.Utility;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using ShipWorks.Data.Model.Custom;
+using ShipWorks.Data.Model.EntityClasses;
+using ShipWorks.Shipping.Carriers;
+
+namespace ShipWorks.Shipping.UI.Carriers.DhlEcommerce
+{
+    /// <summary>
+    /// DHL eCommerce Account Editor Dialog
+    /// </summary>
+    [KeyedComponent(typeof(ICarrierAccountEditorDlg), ShipmentTypeCode.DhlEcommerce)]
+    public partial class DhlEcommerceAccountEditorDlg : Form, ICarrierAccountEditorDlg
+    {
+        private readonly DhlEcommerceAccountEntity account;
+        private readonly ICarrierAccountRetrieverFactory accountRetrieverFactory;
+        private readonly IMessageHelper messageHelper;
+        private readonly ICarrierAccountDescription accountDescription;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public DhlEcommerceAccountEditorDlg(ICarrierAccount account,
+            ICarrierAccountRetrieverFactory accountRetrieverFactory,
+            IMessageHelper messageHelper,
+            IIndex<ShipmentTypeCode, ICarrierAccountDescription> accountDescriptionFactory)
+        {
+            InitializeComponent();
+
+            var dhlEcommerceAccount = account as DhlEcommerceAccountEntity;
+            MethodConditions.EnsureArgumentIsNotNull(dhlEcommerceAccount, "dhlEcommerceAccount");
+
+            this.account = dhlEcommerceAccount;
+            this.accountRetrieverFactory = accountRetrieverFactory;
+            this.messageHelper = messageHelper;
+            this.accountDescription = accountDescriptionFactory[ShipmentTypeCode.Asendia];
+        }
+
+        /// <summary>
+        /// Initialization
+        /// </summary>
+        private void OnLoad(object sender, EventArgs e)
+        {
+            accountNumber.Text = $"{account.ClientId}-{account.PickupNumber}"; // TODO: DHLECommerce verify with product this is what we want to show here
+
+            description.Text = account.Description;
+
+            description.PromptText = account.Description;
+            contactInformation.LoadEntity(account.Address);
+        }
+
+        /// <summary>
+        /// The address content of the shipper has been edited
+        /// </summary>
+        private void OnPersonContentChanged(object sender, EventArgs e)
+        {
+            contactInformation.SaveToEntity();
+            description.PromptText = account.Description;
+        }
+
+        /// <summary>
+        /// User is ready to save the changes
+        /// </summary>
+        private void OnOK(object sender, EventArgs e)
+        {
+            try
+            {
+                if (description.Text.Trim().Length > 0)
+                {
+                    account.Description = description.Text.Trim();
+                }
+
+                contactInformation.SaveToEntity();
+                accountRetrieverFactory.Create(account.ShipmentType).Save(account);
+
+                DialogResult = DialogResult.OK;
+            }
+            catch (ORMConcurrencyException)
+            {
+                messageHelper.ShowError(this, "Your changes cannot be saved because another use has deleted the account.");
+
+                DialogResult = DialogResult.Abort;
+            }
+        }
+    }
+}
--- Code/ShipWorks/Shipping/Carriers/DhlEcommerce/DhlEcommerceServiceType.cs
+++ Code/ShipWorks/Shipping/Carriers/DhlEcommerce/DhlEcommerceServiceType.cs
@@ -113,17 +113,17 @@ namespace ShipWorks.Shipping.Carriers.DhlEcommerce
 
         US_DhlSMMarketingParcelGround = 19,
 
-        [Description("DHL SmartMail Parcel Return Light")]
-        [ApiValue("dhl_smartmail_parcel_return_light")]
-        US_DhlSmartMailParcelReturnLight = 20,
+        /* [Description("DHL SmartMail Parcel Return Light")]
+         [ApiValue("dhl_smartmail_parcel_return_light")]
+         US_DhlSmartMailParcelReturnLight = 20,
 
-        [Description("DHL SmartMail Parcel Return Plus")]
-        [ApiValue("dhl_smartmail_parcel_return_plus")]
-        US_DhlSmartMailParcelReturnPlus = 21,
+         [Description("DHL SmartMail Parcel Return Plus")]
+         [ApiValue("dhl_smartmail_parcel_return_plus")]
+         US_DhlSmartMailParcelReturnPlus = 21,
 
-        [Description("DHL SmartMail Parcel Return Ground")]
-        [ApiValue("dhl_smartmail_parcel_return_ground")]
-        US_DhlSmartMailParcelReturnGround = 22,
+         [Description("DHL SmartMail Parcel Return Ground")]
+         [ApiValue("dhl_smartmail_parcel_return_ground")]
+         US_DhlSmartMailParcelReturnGround = 22,*/
 
         [Description("DHL SM Parcel Expedited Max")]
         [ApiValue("dhl_sm_parcel_expedited_max")]
