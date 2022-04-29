bfcce2714033709efb1d86532a639e1df5bb85e6
John Ackerman <j.ackerman@shipworks.com>
2022-04-29 09:20:33 -0500

    WORKS-3505: Fix wait cursor when adding DHL eCommerce

--- Code/ShipWorks.Shipping.UI/Carriers/DhlEcommerce/DhlEcommerceSetupWizard.cs
+++ Code/ShipWorks.Shipping.UI/Carriers/DhlEcommerce/DhlEcommerceSetupWizard.cs
@@ -144,8 +144,11 @@ namespace ShipWorks.Shipping.UI.Carriers.DhlEcommerce
 
             try
             {
+                Application.UseWaitCursor = true;
                 Cursor.Current = Cursors.WaitCursor;
-                this.Enabled = false;
+                NextEnabled = false;
+                BackEnabled = false;
+                CanCancel = false;
 
                 var result = await shipEngineClient.ConnectDhlEcommerceAccount(new DhlEcommerceRegistrationRequest
                 {
@@ -191,7 +194,10 @@ namespace ShipWorks.Shipping.UI.Carriers.DhlEcommerce
             }
             finally
             {
-                this.Enabled = true;
+                NextEnabled = true;
+                BackEnabled = true;
+                CanCancel = true;
+                Application.UseWaitCursor = false;
                 Cursor.Current = Cursors.Default;
             }
         }
