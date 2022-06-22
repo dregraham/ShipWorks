377e5ac0bb078bb18e79df899ea1beaf131d8785
Tim <t.hughes@shipworks.com>
2022-06-22 16:16:03 -0500

    WORKS-3645: Needed to mark the terms as not being obfuscated

--- Code/ShipWorks.Shipping/Carriers/Amazon/SFP/Terms/DTO/AmazonGetTermsInfoResponse.cs
+++ Code/ShipWorks.Shipping/Carriers/Amazon/SFP/Terms/DTO/AmazonGetTermsInfoResponse.cs
@@ -1,10 +1,21 @@
-﻿namespace ShipWorks.Shipping.Carriers.Amazon.SFP.Terms.DTO
+﻿using System.Reflection;
+using Newtonsoft.Json;
+
+namespace ShipWorks.Shipping.Carriers.Amazon.SFP.Terms.DTO
 {
+    [Obfuscation(ApplyToMembers = true, Exclude = true, StripAfterObfuscation = false)]
     public class AmazonGetTermsInfoResponse
-    {   
+    {
+        [JsonProperty("amazonTermsVersion")]
         public string AmazonTermsVersion { get; set; }
+
+        [JsonProperty("amazonTermsAvailableDate")]
         public string AmazonTermsAvailableDate { get; set; }
+
+        [JsonProperty("amazonTermsDeadlineDate")]
         public string AmazonTermsDeadlineDate { get; set; }
+
+        [JsonProperty("amazonTermsUrl")]
         public string AmazonTermsUrl { get; set; }
     }
 }
