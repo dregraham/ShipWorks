7d4477f8d8e4a62fa7314557cba9eb8de6809ace
Daniel Habowski <daniel.habowski@auctane.com>
2022-12-23 14:25:30 +0100

    test compilation fix

--- Code/ShipWorks.Stores.Tests/Platforms/Amazon/AmazonPlatformDownloaderTest.cs
+++ Code/ShipWorks.Stores.Tests/Platforms/Amazon/AmazonPlatformDownloaderTest.cs
@@ -137,8 +137,8 @@ namespace ShipWorks.Stores.Tests.Platforms.Amazon
         /// Constructor
         /// </summary>
         public AmazonPlatformDownloaderChild(IProgressReporter progress, StoreEntity store, IStoreTypeManager storeTypeManager,
-            IStoreManager storeManager, Func<StoreEntity, IPlatformOrderWebClient> createWebClient) :
-            base(store, storeTypeManager, storeManager, createWebClient)
+            IStoreManager storeManager, IPlatformOrderWebClient platformOrderWebClient) :
+            base(store, storeTypeManager, storeManager, platformOrderWebClient)
         {
             Progress = progress;
         }
