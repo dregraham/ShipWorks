d3bfc27a67aa1ba1c8270fd8130d7805e2625c91
Daniel Habowski <daniel.habowski@auctane.com>
2023-01-17 13:54:14 +0100

    WORKS-3937: ShopifyCreateOrderSource form created

---
+++ Code/ShipWorks.Stores.UI/Platforms/Shopify/ShopifyCreateOrderSource.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ShipWorks.Stores.UI.Platforms.Shopify
+{
+    public partial class ShopifyCreateOrderSource : UserControl
+    {
+        public ShopifyCreateOrderSource()
+        {
+            InitializeComponent();
+        }
+    }
+}
