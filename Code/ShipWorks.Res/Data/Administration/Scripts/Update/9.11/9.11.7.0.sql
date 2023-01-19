5495eaeb91ac2d0cc7cb39ee8e84c6d776a39c16
Kevin Croke <kevin.croke@auctane.com>
2023-01-18 20:55:17 -0600

    WORKS-4015: Disable using platform for uploading Amazon shipments

---
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.11/9.11.7.0.sql
@@ -0,0 +1,7 @@
+﻿UPDATE a
+SET a.TaskIdentifier = 'AmazonShipmentUploadTask'
+FROM dbo.ActionTask AS a
+	OUTER APPLY a.TaskSettings.nodes('Settings/StoreID') AS m(c)
+	INNER JOIN dbo.Store 
+		s ON s.StoreID=m.c.value('@value', 'bigint') 
+WHERE a.TaskIdentifier = 'ApiShipmentUploadTask' AND s.TypeCode = 10
