ea692272fa79df12b44121ebdeb13622db323932
Kevin Croke <kevin.croke@auctane.com>
2023-02-02 16:07:18 -0600

    Fix unit tests that broke because we now first try to go through platform

--- Code/ShipWorks.Stores.Tests.Integration/Platforms/Etsy/EtsyOnlineUpdateCommandCreatorTest.cs
+++ Code/ShipWorks.Stores.Tests.Integration/Platforms/Etsy/EtsyOnlineUpdateCommandCreatorTest.cs
@@ -12,6 +12,7 @@ using ShipWorks.Startup;
 using ShipWorks.Stores.Content;
 using ShipWorks.Stores.Platforms.Etsy;
 using ShipWorks.Stores.Platforms.Etsy.OnlineUpdating;
+using ShipWorks.Stores.Platforms.Platform.OnlineUpdating;
 using ShipWorks.Tests.Shared;
 using ShipWorks.Tests.Shared.Database;
 using ShipWorks.Tests.Shared.EntityBuilders;
@@ -30,7 +31,7 @@ namespace ShipWorks.Stores.Tests.Integration.Platforms.Etsy
         private readonly EtsyStoreEntity store;
         private Mock<IEtsyWebClient> webClient;
         private readonly Mock<IMenuCommandExecutionContext> menuContext;
-        private readonly EtsyOnlineUpdateCommandCreator commandCreator;
+        private readonly PlatformUpdateCommandCreator commandCreator;
 
         public OnlineUpdateCommandCreatorTest(DatabaseFixture db, ITestOutputHelper output)
         {
@@ -46,7 +47,7 @@ namespace ShipWorks.Stores.Tests.Integration.Platforms.Etsy
 
             menuContext = context.Mock.Mock<IMenuCommandExecutionContext>();
 #pragma warning disable S3215 // "interface" instances should not be cast to concrete types
-            commandCreator = context.Mock.Container.ResolveKeyed<IOnlineUpdateCommandCreator>(StoreTypeCode.Etsy) as EtsyOnlineUpdateCommandCreator;
+            commandCreator = context.Mock.Container.ResolveKeyed<IOnlineUpdateCommandCreator>(StoreTypeCode.Etsy) as PlatformUpdateCommandCreator;
 #pragma warning restore S3215 // "interface" instances should not be cast to concrete types
 
             store = Create.Store<EtsyStoreEntity>(StoreTypeCode.Etsy)
@@ -62,181 +63,7 @@ namespace ShipWorks.Stores.Tests.Integration.Platforms.Etsy
                 .Set(x => x.TrackingNumber, "track-999")
                 .Set(x => x.Processed, true)
                 .Save();
-        }
-
-        [Fact]
-        public async Task UploadShippedStatus_MakesOneWebRequest_WhenOrderIsNotCombined()
-        {
-            OrderEntity order = CreateNormalOrder(1, "track-123", false);
-
-            menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { order.OrderID });
-
-            await commandCreator.OnUploadShippedStatus(menuContext.Object, store, true);
-
-            webClient.Verify(x => x.UploadStatusDetails(10, "Foo", (bool?) null, true));
-        }
-
-        [Fact]
-        public async Task UploadShippedStatus_MakesOneWebRequest_WhenOneOfTwoNotCombinedOrdersIsManual()
-        {
-            OrderEntity manualOrder = CreateNormalOrder(4, "track-456", true);
-            OrderEntity order = CreateNormalOrder(1, "track-123", false);
-
-            menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { manualOrder.OrderID, order.OrderID });
-
-            await commandCreator.OnUploadShippedStatus(menuContext.Object, store, true);
-
-            webClient.Verify(x => x.UploadStatusDetails(40, AnyString, (bool?) null, AnyBool), Times.Never);
-            webClient.Verify(x => x.UploadStatusDetails(10, "Foo", (bool?) null, true));
-        }
-
-        [Fact]
-        public async Task UploadShippedStatus_MakesTwoWebRequests_WhenOrderIsCombined()
-        {
-            OrderEntity order = CreateCombinedOrder(1, "track-123", Tuple.Create(2, false), Tuple.Create(3, false));
-
-            menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { order.OrderID });
-
-            await commandCreator.OnUploadShippedStatus(menuContext.Object, store, true);
-
-            webClient.Verify(x => x.UploadStatusDetails(20, "Foo", (bool?) null, true));
-            webClient.Verify(x => x.UploadStatusDetails(30, "Foo", (bool?) null, true));
-        }
-
-        [Fact]
-        public async Task UploadShippedStatus_MakesOneWebRequest_WhenOrderIsCombinedAndOneIsManual()
-        {
-            OrderEntity order = CreateCombinedOrder(1, "track-123", Tuple.Create(2, true), Tuple.Create(3, false));
-
-            menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { order.OrderID });
-
-            await commandCreator.OnUploadShippedStatus(menuContext.Object, store, true);
-
-            webClient.Verify(x => x.UploadStatusDetails(20, AnyString, (bool?) null, AnyBool), Times.Never);
-            webClient.Verify(x => x.UploadStatusDetails(30, "Foo", (bool?) null, true));
-        }
-
-        [Fact]
-        public async Task UploadShippedStatus_MakesThreeWebRequests_WhenBothCombinedAndNonCombinedAreUploaded()
-        {
-            OrderEntity normalOrder = CreateNormalOrder(1, "track-123", false);
-            OrderEntity combinedOrder = CreateCombinedOrder(4, "track-456", Tuple.Create(5, false), Tuple.Create(6, false));
-
-            menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { normalOrder.OrderID, combinedOrder.OrderID });
-
-            await commandCreator.OnUploadShippedStatus(menuContext.Object, store, true);
-
-            webClient.Verify(x => x.UploadStatusDetails(10, "Foo", (bool?) null, true));
-            webClient.Verify(x => x.UploadStatusDetails(50, "Foo", (bool?) null, true));
-            webClient.Verify(x => x.UploadStatusDetails(60, "Foo", (bool?) null, true));
-        }
-
-        [Fact]
-        public async Task UploadShippedStatus_ContinuesUploading_WhenFailuresOccur()
-        {
-            OrderEntity normalOrder = CreateNormalOrder(1, "track-123", false);
-            OrderEntity combinedOrder = CreateCombinedOrder(4, "track-456", Tuple.Create(5, false), Tuple.Create(6, false));
-            OrderEntity normalOrder2 = CreateNormalOrder(7, "track-789", false);
-
-            webClient.Setup(x => x.UploadStatusDetails(10, AnyString, (bool?) null, AnyBool))
-                .Throws(new EtsyException("Foo"));
-            webClient.Setup(x => x.UploadStatusDetails(50, AnyString, (bool?) null, AnyBool))
-                .Throws(new EtsyException("Foo"));
-
-            menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { normalOrder.OrderID, combinedOrder.OrderID, normalOrder2.OrderID });
-
-            await commandCreator.OnUploadShippedStatus(menuContext.Object, store, true);
-
-            webClient.Verify(x => x.UploadStatusDetails(60, "Foo", (bool?) null, true));
-            webClient.Verify(x => x.UploadStatusDetails(70, "Foo", (bool?) null, true));
-        }
-
-        [Fact]
-        public async Task UploadPaidStatus_MakesOneWebRequest_WhenOrderIsNotCombined()
-        {
-            OrderEntity order = CreateNormalOrder(1, "track-123", false);
-
-            menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { order.OrderID });
-
-            await commandCreator.OnUploadPaidStatus(menuContext.Object, store, true);
-
-            webClient.Verify(x => x.UploadStatusDetails(10, string.Empty, true, (bool?) null));
-        }
-
-        [Fact]
-        public async Task UploadPaidStatus_MakesOneWebRequest_WhenOneOfTwoNotCombinedOrdersIsManual()
-        {
-            OrderEntity manualOrder = CreateNormalOrder(4, "track-456", true);
-            OrderEntity order = CreateNormalOrder(1, "track-123", false);
-
-            menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { manualOrder.OrderID, order.OrderID });
-
-            await commandCreator.OnUploadPaidStatus(menuContext.Object, store, true);
-
-            webClient.Verify(x => x.UploadStatusDetails(40, AnyString, AnyBool, (bool?) null), Times.Never);
-            webClient.Verify(x => x.UploadStatusDetails(10, string.Empty, true, (bool?) null));
-        }
-
-        [Fact]
-        public async Task UploadPaidStatus_MakesTwoWebRequests_WhenOrderIsCombined()
-        {
-            OrderEntity order = CreateCombinedOrder(1, "track-123", Tuple.Create(2, false), Tuple.Create(3, false));
-
-            menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { order.OrderID });
-
-            await commandCreator.OnUploadPaidStatus(menuContext.Object, store, true);
-
-            webClient.Verify(x => x.UploadStatusDetails(20, string.Empty, true, (bool?) null));
-            webClient.Verify(x => x.UploadStatusDetails(30, string.Empty, true, (bool?) null));
-        }
-
-        [Fact]
-        public async Task UploadPaidStatus_MakesOneWebRequest_WhenOrderIsCombinedAndOneIsManual()
-        {
-            OrderEntity order = CreateCombinedOrder(1, "track-123", Tuple.Create(2, true), Tuple.Create(3, false));
-
-            menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { order.OrderID });
-
-            await commandCreator.OnUploadPaidStatus(menuContext.Object, store, true);
-
-            webClient.Verify(x => x.UploadStatusDetails(20, AnyString, AnyBool, (bool?) null), Times.Never);
-            webClient.Verify(x => x.UploadStatusDetails(30, string.Empty, true, (bool?) null));
-        }
-
-        [Fact]
-        public async Task UploadPaidStatus_MakesThreeWebRequests_WhenBothCombinedAndNonCombinedAreUploaded()
-        {
-            OrderEntity normalOrder = CreateNormalOrder(1, "track-123", false);
-            OrderEntity combinedOrder = CreateCombinedOrder(4, "track-456", Tuple.Create(5, false), Tuple.Create(6, false));
-
-            menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { normalOrder.OrderID, combinedOrder.OrderID });
-
-            await commandCreator.OnUploadPaidStatus(menuContext.Object, store, true);
-
-            webClient.Verify(x => x.UploadStatusDetails(10, string.Empty, true, (bool?) null));
-            webClient.Verify(x => x.UploadStatusDetails(50, string.Empty, true, (bool?) null));
-            webClient.Verify(x => x.UploadStatusDetails(60, string.Empty, true, (bool?) null));
-        }
-
-        [Fact]
-        public async Task UploadPaidStatus_ContinuesUploading_WhenFailuresOccur()
-        {
-            OrderEntity normalOrder = CreateNormalOrder(1, "track-123", false);
-            OrderEntity combinedOrder = CreateCombinedOrder(4, "track-456", Tuple.Create(5, false), Tuple.Create(6, false));
-            OrderEntity normalOrder2 = CreateNormalOrder(7, "track-789", false);
-
-            webClient.Setup(x => x.UploadStatusDetails(10, AnyString, AnyBool, (bool?) null))
-                .Throws(new EtsyException("Foo"));
-            webClient.Setup(x => x.UploadStatusDetails(50, AnyString, AnyBool, (bool?) null))
-                .Throws(new EtsyException("Foo"));
-
-            menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { normalOrder.OrderID, combinedOrder.OrderID, normalOrder2.OrderID });
-
-            await commandCreator.OnUploadPaidStatus(menuContext.Object, store, true);
-
-            webClient.Verify(x => x.UploadStatusDetails(60, string.Empty, true, (bool?) null));
-            webClient.Verify(x => x.UploadStatusDetails(70, string.Empty, true, (bool?) null));
-        }
+        }      
 
         [Fact]
         public async Task UploadDetails_MakesOneWebRequest_WhenOrderIsNotCombined()
@@ -245,8 +72,8 @@ namespace ShipWorks.Stores.Tests.Integration.Platforms.Etsy
 
             menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { order.OrderID });
 
-            await commandCreator.OnUploadTrackingDetails(menuContext.Object, store);
-
+            await commandCreator.OnUploadDetails(store, menuContext.Object);
+            
             webClient.Verify(x => x.UploadShipmentDetails(999, 10, "track-123", "other"));
         }
 
@@ -258,7 +85,7 @@ namespace ShipWorks.Stores.Tests.Integration.Platforms.Etsy
 
             menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { manualOrder.OrderID, order.OrderID });
 
-            await commandCreator.OnUploadTrackingDetails(menuContext.Object, store);
+            await commandCreator.OnUploadDetails(store, menuContext.Object);
 
             webClient.Verify(x => x.UploadShipmentDetails(AnyLong, 40, AnyString, AnyString), Times.Never);
             webClient.Verify(x => x.UploadShipmentDetails(999, 10, "track-123", "other"));
@@ -271,7 +98,7 @@ namespace ShipWorks.Stores.Tests.Integration.Platforms.Etsy
 
             menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { order.OrderID });
 
-            await commandCreator.OnUploadTrackingDetails(menuContext.Object, store);
+            await commandCreator.OnUploadDetails(store, menuContext.Object);
 
             webClient.Verify(x => x.UploadShipmentDetails(999, 20, "track-123", "other"));
             webClient.Verify(x => x.UploadShipmentDetails(999, 30, "track-123", "other"));
@@ -284,7 +111,7 @@ namespace ShipWorks.Stores.Tests.Integration.Platforms.Etsy
 
             menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { order.OrderID });
 
-            await commandCreator.OnUploadTrackingDetails(menuContext.Object, store);
+            await commandCreator.OnUploadDetails(store, menuContext.Object);
 
             webClient.Verify(x => x.UploadShipmentDetails(AnyLong, 20, AnyString, AnyString), Times.Never);
             webClient.Verify(x => x.UploadShipmentDetails(999, 30, "track-123", "other"));
@@ -298,7 +125,7 @@ namespace ShipWorks.Stores.Tests.Integration.Platforms.Etsy
 
             menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { normalOrder.OrderID, combinedOrder.OrderID });
 
-            await commandCreator.OnUploadTrackingDetails(menuContext.Object, store);
+            await commandCreator.OnUploadDetails(store, menuContext.Object);
 
             webClient.Verify(x => x.UploadShipmentDetails(999, 10, "track-123", "other"));
             webClient.Verify(x => x.UploadShipmentDetails(999, 50, "track-456", "other"));
@@ -319,7 +146,7 @@ namespace ShipWorks.Stores.Tests.Integration.Platforms.Etsy
 
             menuContext.SetupGet(x => x.SelectedKeys).Returns(new[] { normalOrder.OrderID, combinedOrder.OrderID, normalOrder2.OrderID });
 
-            await commandCreator.OnUploadTrackingDetails(menuContext.Object, store);
+            await commandCreator.OnUploadDetails(store, menuContext.Object);
 
             webClient.Verify(x => x.UploadShipmentDetails(999, 60, "track-456", "other"));
             webClient.Verify(x => x.UploadShipmentDetails(999, 70, "track-789", "other"));
