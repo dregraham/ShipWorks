285db0b0d351ca789b43026cad7723b8be3402e6
Nathaniel Preiss <nathaniel.preiss@auctane.com>
2023-02-13 23:51:09 -0600

    WORKS-3997: Fixing LLBGEN files

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.13/9.13.1.0.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.13/9.13.1.0.sql
@@ -1,6 +1,6 @@
 ﻿﻿PRINT N'ALTERING [dbo].[FedExShipment]'
 GO
 
-IF COL_LENGTH(N'[dbo].[FedExShipment]', N'ShipEngineCarrierID') IS NULL
+IF COL_LENGTH(N'[dbo].[FedExShipment]', N'ShipEngineLabelId') IS NULL
 	ALTER TABLE [dbo].[FedExShipment] ADD [ShipEngineLabelId] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 GO
