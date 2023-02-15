56f6ae0d870954ae6a70533ce1a03423c55ed6c2
Cody Hart <c.hart@shipworks.com>
2023-02-15 15:32:00 -0600

    Move smartpost sql update to its own file

---
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.13/9.13.6.0.sql
@@ -0,0 +1,4 @@
+﻿-- ShipEngine only supports a single SmartPostHub so we need a new field to hold that single value
+IF COL_LENGTH(N'[dbo].[FedExAccount]', N'SmartPostHub') IS NULL
+	ALTER TABLE [dbo].[FedExAccount] ADD [SmartPostHub] [int] NOT NULL DEFAULT (0)
+GO
