c8b44147afe24b42703e40ea8819d71b1d7f7753
Kevin Croke <kevin.croke@auctane.com>
2023-02-15 09:08:20 -0600

    WORKS-4038: Standardize build script numberring from after merge

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.13/9.13.1.0.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/9.13/9.13.1.0.sql
@@ -1,6 +1,17 @@
-﻿PRINT N'ALTERING [dbo].[FedExShipment]'
+﻿PRINT N'Altering [dbo].[FedExProfile]'
+GO
+IF COL_LENGTH(N'[dbo].[FedExProfile]', N'PayorCountryCode') IS NULL
+ALTER TABLE [dbo].[FedExProfile] ADD[PayorCountryCode] [nvarchar] (2) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+IF COL_LENGTH(N'[dbo].[FedExProfile]', N'PayorPostalCode') IS NULL
+ALTER TABLE [dbo].[FedExProfile] ADD[PayorPostalCode] [nvarchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 GO
 
-IF COL_LENGTH(N'[dbo].[FedExShipment]', N'ShipEngineLabelId') IS NULL
-	ALTER TABLE [dbo].[FedExShipment] ADD [ShipEngineLabelId] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+PRINT N'Updating FedEx Primary Profile'
+GO
+
+UPDATE fp
+SET [PayorCountryCode] = '', [PayorPostalCode] = ''
+FROM dbo.FedExProfile fp
+INNER JOIN dbo.ShippingProfile p ON p.ShippingProfileID = fp.ShippingProfileID
+WHERE [PayorCountryCode] IS NULL AND PayorPostalCode IS NULL AND p.ShipmentTypePrimary = 1
 GO
