5cea872a1c7f522a1234448d665d4f3211f08a82
wojciech-wojcik-metapack <wojciech.wojcik@auctane.com>
2023-07-07 12:19:53 +0200

    WORKS-4210: Add sql query for update FedEx profile table

--- Code/ShipWorks.Res/Data/Administration/Scripts/Update/10.0/10.0.5.0.sql
+++ Code/ShipWorks.Res/Data/Administration/Scripts/Update/10.0/10.0.5.0.sql
@@ -1,4 +1,14 @@
-﻿PRINT N'Altering [dbo].[FedExShipment]'
+﻿PRINT N'Altering [dbo].[FedExProfile]'
+GO
+
+IF COL_LENGTH(N'[dbo].[FedExProfile]', N'DeliveredDutyPaid') IS NULL
+BEGIN
+    ALTER TABLE [dbo].[FedExProfile]
+    ADD [DeliveredDutyPaid] [bit] NULL
+END
+GO
+
+PRINT N'Altering [dbo].[FedExShipment]'
 GO
 
 IF COL_LENGTH(N'[dbo].[FedExShipment]', N'DeliveredDutyPaid') IS NULL
