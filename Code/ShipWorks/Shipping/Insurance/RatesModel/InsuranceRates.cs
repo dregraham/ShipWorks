7244962b7b6ea4c4df302037946db4f7c97c8872
Marcin Adamski <marcin.adamski@metapack.com>
2023-07-10 10:25:19 +0200

    fix FedExUpsInternational insurance rate

--- Code/ShipWorks/Shipping/Insurance/RatesModel/InsuranceRates.cs
+++ Code/ShipWorks/Shipping/Insurance/RatesModel/InsuranceRates.cs
@@ -11,7 +11,7 @@ namespace ShipWorks.Shipping.Insurance.RatesModel
 	public class ShipworksRates
 	{
 		public decimal FedExUpsDomesticCost { get; set; } = 0.55m;
-		public decimal FedExUpsInternationalCost { get; set; } = 0.5m;
+		public decimal FedExUpsInternationalCost { get; set; } = 0.55m;
 		public decimal UspsDomesticCost { get; set; } = 0.75m;
 		public decimal iParcelCost { get; set; } = 0.75m;
 		public decimal UspsInternationalCost { get; set; } = 1.55m;
